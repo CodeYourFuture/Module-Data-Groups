1e3cdc7073cb3e43700fd3c7ea4f44b3c30acba3
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-16 15:12:26 +0000

    sprint1-refactor-includes

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,15 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
-      return true;
-    }
-  }
-  return false;
+    // if (list.includes(target)) {
+    //   return true;
+    // }
+    // else{
+    //   return false;
+    // }
+    return list.includes(target)
 }
+  
 
 module.exports = includes;
+
