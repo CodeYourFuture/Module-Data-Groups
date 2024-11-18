58a2c8e71e43a415fb9842e286692749fdc251ea
Armin <nouriarmin24@gmail.com>
2024-11-18 13:56:16 +0000

    debug and log house number

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -3,7 +3,6 @@
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
-
 const address = {
   houseNumber: 42,
   street: "Imaginary Road",
@@ -12,4 +11,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is houseNumber:`,address.houseNumber);
