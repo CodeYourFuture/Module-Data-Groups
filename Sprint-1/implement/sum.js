b8d2114b1cca67368c207b73643d531ee0f9d099
Amir200524 <amiraminia@yahoo.com>
2024-11-20 10:39:07 +0000

    function sum

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,12 @@
+
 function sum(elements) {
+  if (!Array.isArray(elements)) {
+    throw new Error("Input must be an array");
+  }
+
+  return elements
+    .filter(item => typeof item === "number") 
+    .reduce((total, num) => total + num, 0);
 }
 
 module.exports = sum;
