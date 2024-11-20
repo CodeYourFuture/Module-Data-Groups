777ec02f450168a190117e51d05e102fc9253a26
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-20 22:16:09 +0000

    sprint2-implement- lookup

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,11 @@
-function createLookup() {
+function createLookup(list) {
   // implementation here
+  let currency = {};
+  for(let i = 0; i < list.length; i++){
+    currency[list[i][0]] = list[i][1];
+  }
+  return currency;
 }
-
+console.log(createLookup([['US', 'USD'], ['CA', 'CAD']]));
 module.exports = createLookup;
+
