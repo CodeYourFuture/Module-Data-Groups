b09de2985897cf727ad760a71176f9051efd1d81
Amir200524 <amiraminia@yahoo.com>
2024-11-20 10:40:57 +0000

    sum.test

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -1,36 +1,27 @@
-/* Sum the numbers in an array
-
-In this kata, you will need to implement a function that sums the numerical elements of an array
-
-E.g. sum([10, 20, 30]), target output: 60
-E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical elements)
-*/
-
 const sum = require("./sum.js");
 
-// Acceptance Criteria:
+describe("sum", () => {
+  test("given an empty array, returns 0", () => {
+    expect(sum([])).toEqual(0);
+  });
 
-// Given an empty array
-// When passed to the sum function
-// Then it should return 0
-test.todo("given an empty array, returns 0")
+  test("given an array with just one number, returns that number", () => {
+    expect(sum([42])).toEqual(42);
+  });
 
-// Given an array with just one number
-// When passed to the sum function
-// Then it should return that number
+  test("given an array containing negative numbers, returns the correct total sum", () => {
+    expect(sum([-10, 20, -30, 40])).toEqual(20);
+  });
 
-// Given an array containing negative numbers
-// When passed to the sum function
-// Then it should still return the correct total sum
+  test("given an array with decimal/float numbers, returns the correct total sum", () => {
+    expect(sum([1.5, 2.5, 3.5])).toEqual(7.5);
+  });
 
-// Given an array with decimal/float numbers
-// When passed to the sum function
-// Then it should return the correct total sum
+  test("given an array containing non-number values, ignores the non-numerical values and returns the sum of the numerical elements", () => {
+    expect(sum([10, "hello", 20, {}, 30])).toEqual(60);
+  });
+  test("given an array with only non-number values, returns 0", () => {
+    expect(sum(["a", "b", {}, []])).toEqual(0);
+  });
+});
 
-// Given an array containing non-number values
-// When passed to the sum function
-// Then it should ignore the non-numerical values and return the sum of the numerical elements
-
-// Given an array with only non-number values
-// When passed to the sum function
-// Then it should return the least surprising value given how it behaves for all other inputs
