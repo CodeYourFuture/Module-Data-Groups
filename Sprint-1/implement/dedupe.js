123eb58bbcca62837a5e52082473c204133dc69b
Amir200524 <amiraminia@yahoo.com>
2024-11-20 10:20:27 +0000

    write a new function

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,10 @@
-function dedupe() {}
+function dedupe(array) {
+  if (!Array.isArray(array)) {
+    throw new Error("Input must be an array");
+  }
+ 
+  return Array.from(new Set(array));
+}
+
+module.exports = dedupe;
+
