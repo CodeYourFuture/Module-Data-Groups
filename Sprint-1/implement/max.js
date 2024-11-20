b8e0230e65d29acce9d0c596f0ccc2de927e0e27
Amir200524 <amiraminia@yahoo.com>
2024-11-20 10:27:44 +0000

    write findmax function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,11 @@
-function findMax(elements) {
+function findMax(numbers) {
+  if (!Array.isArray(numbers)) {
+    throw new Error("Input must be an array");
+  }
+
+  const validNumbers = numbers.filter(item => typeof item === "number");
+  return validNumbers.length > 0 ? Math.max(...validNumbers) : -Infinity;
 }
 
 module.exports = findMax;
+
