5554cc589f717a2f272eabacb669aa37a5ad3b93
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-20 21:56:18 +0000

    sprint2-implement- contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,18 @@
-function contains() {}
+function contains(dic , char) {
+    let keys = Object.keys(dic);
+    if(typeof dic != "object"){
+        return false;
+    }
+    if(Object.keys(dic).length == 0){
+        return false;
+    }
+    if(keys.includes(char)){
+        return true;
+    }
+    if(!keys.includes(char)){
+        return false;
+    }
+};
 
 module.exports = contains;
+
