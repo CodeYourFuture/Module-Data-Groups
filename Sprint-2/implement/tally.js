5f653e2c1a253d0c0bfdcfa60a6dbdafe6917600
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-21 21:16:05 +0000

    sprint2- implement- tally

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,17 @@
-function tally() {}
-
+function tally(list) {
+    let count = {};
+    if(list.length == 0){
+        return {};
+    }
+    if(!Array.isArray(list)){
+        throw new Error("invalid input, input must be an array");
+    }
+    else{
+        for (let item of list) {
+            count[item] = (count[item] || 0) + 1;
+        }
+    }return count;
+}
+console.log(tally(["a", "b", "c", "c", "b"]));
 module.exports = tally;
+
