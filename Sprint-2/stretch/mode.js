0fdc9beb1f9a4846b2db5e30ce1c71f41479f6d4
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-22 16:30:16 +0000

    sprint2-stretch - mode

--- Sprint-2/stretch/mode.js
+++ Sprint-2/stretch/mode.js
@@ -9,28 +9,40 @@
 // into smaller functions using the stages above
 
 function calculateMode(list) {
-  // track frequency of each value
-  let freqs = new Map();
+  // Stage 1: Track the frequency of each value
+  function freqq(list) {
+    let freqs = new Map();
 
-  for (let num of list) {
-    if (typeof num !== "number") {
-      continue;
+    for (let num of list) {
+      if (typeof num !== "number") {
+        continue; // Skip non-numeric values
+      }
+
+      freqs.set(num, (freqs.get(num) || 0) + 1);
     }
 
-    freqs.set(num, (freqs.get(num) || 0) + 1);
+    return freqs;
   }
 
-  // Find the value with the highest frequency
-  let maxFreq = 0;
-  let mode;
-  for (let [num, freq] of freqs) {
-    if (freq > maxFreq) {
-      mode = num;
-      maxFreq = freq;
+  // Stage 2: Find the value with the highest frequency
+  function maxfreqq(freqs) {
+    let maxFreq = 0;
+    let mode;
+
+    for (let [num, freq] of freqs) {
+      if (freq > maxFreq) {
+        mode = num;
+        maxFreq = freq;
+      }
     }
+
+    return maxFreq === 0 ? NaN : mode; // Return NaN if no mode is found
   }
 
-  return maxFreq === 0 ? NaN : mode;
+  // Execute the stages and return the result
+  const freqs = freqq(list); // Calculate frequency
+  return maxfreqq(freqs);    // Find the mode
 }
+
 
 module.exports = calculateMode;
