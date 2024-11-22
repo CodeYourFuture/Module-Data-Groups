114cb6b8fd736599b6d6f725416bbb5a4ca9395a
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-22 14:37:31 +0000

    sprint1-fix-median update

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -4,20 +4,16 @@
 // 1,2,3,4
 // 123456789
 function calculateMedian(list) {
+  list.sort();
   let median ;
-  let middleF ;
-  let middleC ;
   if(list.length % 2 == 0){
-    middleF = Math.floor(list.length / 2);
-    middleC = list[middleF];
-    median = (middleC + middleF) /2;
+    median = (list[Math.floor(list.length / 2 -1)] + list[Math.floor(list.length / 2)] ) /2;
   }
   else{
     median = list[Math.floor(list.length / 2)];
-
-    // median = list.splice(middleF, 1)[0];
     }
   return median;
 } 
+
 
 module.exports = calculateMedian;
