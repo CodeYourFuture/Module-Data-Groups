9b492d8e722a33e0ad618949304a8b074e0bdd0a
saqib javed <70890519+saqibjvd@users.noreply.github.com>
2024-11-23 14:33:39 +0000

    Sprint -1
    fix done

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,9 +3,21 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
+  // first we sort list in asc order e.g [1,2,3,4,5,6....etc]
+  list.sort((a,b) => a - b )
+
+  // divide length by 2 to get middle num/indx
   const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  // check for odd number
+  if(list.length % 2 !== 0) {
+    return list[middleIndex]
+  } else {
+    return (list[middleIndex -1] + list[middleIndex]) /2;
+
+  }
+ 
 }
+
+
 
 module.exports = calculateMedian;
