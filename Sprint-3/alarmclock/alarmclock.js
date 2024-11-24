875e3aac79bbf3986fcf8a1d1366813555f4fc51
Anna Fedyna <annafedyna03@gmail.com>
2024-11-24 22:10:42 +0000

    alarmclock added style and functionality

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,39 @@
-function setAlarm() {}
+
+function getZero(num) {
+  if (num >= 0 && num < 10) {
+    return "0" + num;
+  } else {
+    return num;
+  }
+}
+
+
+function setAlarm() {
+  const alarmInput = document.querySelector("#alarmSet");
+  const interval = setInterval(updateTimer, 1000);
+  let timeLeft = alarmInput.value;
+
+  if (isNaN(timeLeft) || timeLeft <= 0) {
+    return;
+  }
+  updateTimer();
+  function updateTimer() {
+    const remainingTime = document.querySelector(".time-left");
+
+    if (timeLeft <= 0) {
+      clearInterval(interval);
+      remainingTime.textContent = "00:00";
+      alarmInput.value = ''
+      playAlarm();
+      return;
+    }
+    let secondsRemaining = getZero(timeLeft % 60);
+    let minutesRemaining = getZero(Math.floor(timeLeft / 60));
+    remainingTime.textContent = `${minutesRemaining}:${secondsRemaining}`;
+
+    timeLeft -= 1;
+  } 
+}
 
 // DO NOT EDIT BELOW HERE
 
