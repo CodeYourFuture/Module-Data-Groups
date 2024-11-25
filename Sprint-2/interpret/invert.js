6c75e1b49deea5cff98e15d9d2c0c3295d4ecb01
Amir200524 <amiraminia@yahoo.com>
2024-11-25 13:54:11 +0000

    invert & jest test file

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -6,24 +6,42 @@
 
 // E.g. invert({x : 10, y : 20}), target output: {"10": "x", "20": "y"}
 
+//function invert(obj) { 
+ // const invertedObj = {};
+
+  //for (const [key, value] of Object.entries(obj)) {
+   // invertedObj.key = value;
+ // }
+
+ // return invertedObj;
+//}
+
+// a) What is the current return value when invert is called with { a : 1 }
+//{ key: 1 }
+
+
+// b) What is the current return value when invert is called with { a: 1, b: 2 }
+//{ key: 2 }
+
+
+// c) What is the target return value when invert is called with {a : 1, b: 2}
+//{ "1": "a", "2": "b" }
+
+
+// c) What does Object.entries return? Why is it needed in this program?
+//Object.entries() returns an array of key-value pairs from the object.
+//In this program, Object.entries() is used because it gives us both the key and value from the input object in an iterable form, which allows us to process them and swap their places in the inverted object.
+
+// d) Explain why the current return value is different from the target output
+//The issue is that the loop is using a hardcoded string "key" for the property of invertedObj. Instead of assigning the key dynamically from the input object (key), it is always setting key as the property name in invertedObj, leading to incorrect results.
+
+// e) Fix the implementation of invert (and write tests to prove it's fixed!)
 function invert(obj) {
   const invertedObj = {};
-
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
-  }
+    invertedObj[value] = key;  
 
   return invertedObj;
 }
 
-// a) What is the current return value when invert is called with { a : 1 }
-
-// b) What is the current return value when invert is called with { a: 1, b: 2 }
-
-// c) What is the target return value when invert is called with {a : 1, b: 2}
-
-// c) What does Object.entries return? Why is it needed in this program?
-
-// d) Explain why the current return value is different from the target output
-
-// e) Fix the implementation of invert (and write tests to prove it's fixed!)
+module.exports = invert;
---
+++ Sprint-2/interpret/invert.test.js
@@ -0,0 +1,23 @@
+const invert = require("./invert.js");
+
+describe("invert function", () => {
+  test("invert on an object with one key-value pair", () => {
+    expect(invert({ a: 1 })).toEqual({ "1": "a" });
+  });
+
+  test("invert on an object with multiple key-value pairs", () => {
+    expect(invert({ a: 1, b: 2 })).toEqual({ "1": "a", "2": "b" });
+  });
+
+  test("invert on an object with different values", () => {
+    expect(invert({ x: 10, y: 20 })).toEqual({ "10": "x", "20": "y" });
+  });
+
+  test("invert on an object with string keys and values", () => {
+    expect(invert({ a: "x", b: "y" })).toEqual({ "x": "a", "y": "b" });
+  });
+
+  test("invert on an empty object", () => {
+    expect(invert({})).toEqual({});
+  });
+});
