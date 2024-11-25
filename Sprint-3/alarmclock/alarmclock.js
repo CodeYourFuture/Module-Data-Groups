47a6e3947f2711e11678e1d086d16010eb9baf30
mohammed22471 <mohammed22471@outlook.com>
2024-11-25 21:30:49 +0000

    alarm clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,32 @@
-function setAlarm() {}
+function setAlarm() {
+    let inputValue = parseInt(document.getElementById("alarmSet").value, 10);
+    let tenSecond = inputValue;
+    if (inputValue < 0 || isNaN(inputValue)) {
+        alert("Invalid value");
+        return;
+    }
 
+    let intervalId = setInterval(function() {
+    	
+        if (inputValue >= 0) {
+        	
+            let minutes = Math.floor(inputValue / 60).toString().padStart(2, '0');
+            let seconds = (inputValue % 60).toString().padStart(2, '0');
+            document.getElementById("timeRemaining").innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+            inputValue--;
+
+            if (inputValue < 0) {
+            	if(tenSecond === 10){
+        			document.body.style.backgroundColor = 'lightblue'; // You can use any valid CSS color
+
+            	}
+                playAlarm();
+                clearInterval(intervalId);
+            }
+        }
+    }, 1000);
+}
+	
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
@@ -7,6 +34,7 @@ var audio = new Audio("alarmsound.mp3");
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
+
   });
 
   document.getElementById("stop").addEventListener("click", () => {
