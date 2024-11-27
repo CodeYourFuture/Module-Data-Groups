9df6b2ace8f2ebb47eb0bf2f94b9e288ea0ebcf8
saraamiiri <amirisara1993@gmail.com>
2024-11-27 09:26:32 +0000

    implement/max.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,13 @@
 function findMax(elements) {
-}
-
+      // Filter the array to include only numbers
+    const numbers = arr.filter(item => typeof item === 'number');
+    
+    // If there are no numbers in the array, return -Infinity
+    if (numbers.length === 0) {
+      return -Infinity;
+    }
+  
+    // Return the largest number in the array
+    return Math.max(...numbers);
+  }
 module.exports = findMax;
