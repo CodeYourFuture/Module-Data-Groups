30d15bcaa17d21c2d2a7016efe54471990eaea37
saraamiiri <amirisara1993@gmail.com>
2024-11-27 09:59:43 +0000

    implement/sum.test.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,12 @@
-function sum(elements) {
-}
+function sum(arr) {// sum.js
 
-module.exports = sum;
+      return arr.reduce((total, current) => {
+        // Only add current if it's a number (including integers and floats)
+        if (typeof current === 'number') {
+          return total + current;
+        }
+        return total;
+      }, 0);
+    }
+    
+    module.exports = sum;
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -5,15 +5,41 @@ In this kata, you will need to implement a function that sums the numerical elem
 E.g. sum([10, 20, 30]), target output: 60
 E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical elements)
 */
-
+// sum.test.js
 const sum = require("./sum.js");
+
+test("given an empty array, returns 0", () => {
+  expect(sum([])).toBe(0);
+});
+
+test("given an array with just one number, returns that number", () => {
+  expect(sum([5])).toBe(5);
+});
+
+test("given an array containing negative numbers, returns the correct total sum", () => {
+  expect(sum([10, -5, 3])).toBe(8);
+});
+
+test("given an array with decimal/float numbers, returns the correct total sum", () => {
+  expect(sum([10.5, 5.5, 2.0])).toBe(18);
+});
+
+test("given an array containing non-number values, ignores them and returns the sum of numerical elements", () => {
+  expect(sum(['hey', 10, 'hi', 60, 10])).toBe(80);
+});
+
+test("given an array with only non-number values, returns 0", () => {
+  expect(sum(['hey', 'hi', 'hello'])).toBe(0);
+});
+
+
+
 
 // Acceptance Criteria:
 
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
 
 // Given an array with just one number
 // When passed to the sum function
