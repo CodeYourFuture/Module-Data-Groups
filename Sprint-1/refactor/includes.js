626fac82f644e7db847f15ec86a9b8c48b872530
nadika <nadikazavodovska@gmail.com>
2024-11-27 17:25:04 +0000

    Rewrite function, using for of loop

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,20 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
+  // rewrite function with for of loop
+  for(const element of list){
     if (element === target) {
       return true;
     }
+    return false;
   }
-  return false;
+  // for (let index = 0; index < list.length; index++) {
+  //   const element = list[index];
+  //   if (element === target) {
+  //     return true;
+  //   }
+  // }
+  // return false;
 }
 
 module.exports = includes;
