6dcf560be6e92fc615c4d7fbb6bcebe1a5ec3eec
Alaa <aliqassabuk@gmail.com>
2024-11-27 18:29:04 +0000

    Moved changes from sprint-1 to sprint-2

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,9 @@
 // Predict and explain first...
-
+/*
+The program is attempting to iterate through the author object using a for...of loop. However, this results in an error because for...of is designed to iterate over iterable objects like arrays, string...
+but auther is plain object, so the loop fails.
+We have to use for...in for this object or  Object.values() to get an array of the values and then use for...of
+*/
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +15,14 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+// for (const value of author) {
+//   console.log(value);
+// }
+//Use for...in
+for (const key in author) {
+  console.log(author[key]);
+}
+//Use Object.values()
+for (const value of Object.values(author)) {
   console.log(value);
 }
--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,8 @@
 // Predict and explain first...
-
+/*
+it tries to convert the entire recipe object to a string. This results in a [object Object] output instead of the ingredients array being displayed line by line.
+The ingredients array needs to be joined with line breaks to display each ingredient on a new line.
+*/
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -10,6 +13,9 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
+// console.log(`${recipe.title} serves ${recipe.serves}
+//   ingredients:
+// ${recipe}`);
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients.join("\n")}`);
