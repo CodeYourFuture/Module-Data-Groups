0b0acb9e21656bcc85a5eb10d9f2fa5c49c9a557
saraamiiri <amirisara1993@gmail.com>
2024-11-27 09:06:18 +0000

    implement/dedupe.js

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,14 @@
-function dedupe() {}
+
+function dedupe(arr) {
+    const seen = new Set();
+    return arr.filter(item => {
+      if (!seen.has(item)) {
+        seen.add(item);
+        return true;
+      }
+      return false;
+    });
+  }
+  
+  module.exports = dedupe;
+  
