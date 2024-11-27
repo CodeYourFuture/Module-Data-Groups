c44a62ce6c28d756dbfc7024e421d1c28196d36e
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-27 22:38:39 +0000

    alarm clock 1

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,37 @@
-function setAlarm() {}
+var input_time = document.querySelector("#alarmSet");
+var title_time = document.querySelector("#titleTime");
+var timeRemaining = document.querySelector("#timeRemaining");
+
+function setAlarm() {
+  
+  let totalSeconds = parseInt(input_time.value, 10);
+  if (isNaN(totalSeconds) || totalSeconds <= 0) {
+    alert("Please enter a valid number of seconds.");
+    return;
+  }
+  
+  const title_second = input_time.value % 60;
+  const title_minute = (input_time.value - title_second) / 60;
+
+  title_time.innerText = `${title_minute.toString().padStart(2, "0")}:${title_second.toString().padStart(2, "0")}`;
+
+  let timer = setInterval(() =>{
+    let seconds = totalSeconds % 60;
+    let minutes = (totalSeconds - seconds) / 60;
+    timeRemaining.innerText = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    
+    if(totalSeconds <= 0){
+      clearInterval(timer);
+      playAlarm();
+    }
+    else{
+      totalSeconds--;
+    }
+    
+  }, 1000);
+
+
+}
 
 // DO NOT EDIT BELOW HERE
 
