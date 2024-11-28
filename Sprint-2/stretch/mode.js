6dda345d3e3d45e7d1a841dfa7113a03f0dfa44b
ArminNouri <nouriarmin24@gmail.com>
2024-11-28 11:10:16 +0000

    rewrite mode and till with test

--- Sprint-2/stretch/mode.js
+++ Sprint-2/stretch/mode.js
@@ -9,26 +9,16 @@
 // into smaller functions using the stages above
 
 function calculateMode(list) {
-  // track frequency of each value
-  let freqs = new Map();
+  const freqs = new Map();
 
-  for (let num of list) {
-    if (typeof num !== "number") {
-      continue;
-    }
+  list.forEach(
+    (num) => Number.isFinite(num) && freqs.set(num, (freqs.get(num) || 0) + 1)
+  );
 
-    freqs.set(num, (freqs.get(num) || 0) + 1);
-  }
-
-  // Find the value with the highest frequency
-  let maxFreq = 0;
-  let mode;
-  for (let [num, freq] of freqs) {
-    if (freq > maxFreq) {
-      mode = num;
-      maxFreq = freq;
-    }
-  }
+  return [...freqs.entries()].reduce(
+    (max, [num, freq]) => (freq > max.freq ? { num, freq } : max),
+    { num: NaN, freq: 0 }
+  ).num;
 
   return maxFreq === 0 ? NaN : mode;
 }
