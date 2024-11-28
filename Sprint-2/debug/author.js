535e0d7d37fb43d58e172c8feed1535d3722644f
saqib javed <70890519+saqibjvd@users.noreply.github.com>
2024-11-28 21:59:34 +0000

    Sprint-2
    Debug

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+// Answer: in the code below for...of loop trying loop through object that is causing error.
+// for...of loop is used to iterate over iterables (such as arrays, strings, maps) and not over objects directly.
+// instead use for...in loop or object.value
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +14,12 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+// Object.values() to iterate over values directly:
+for (const value of Object.values(author)) { 
   console.log(value);
+}
+
+//for...in loop to iterate over keys of an object:
+for(key in author) {
+  console.log(author[key])
 }
--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,8 +1,14 @@
 // Predict and explain first...
 
+// Answer ${recipe.title} serves ${recipe.serves}, will correctly print bruschetta serves 2
+// not printing recipe  instead printing [object object] - needs fixing
+
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
+
 // How can you fix it?
+// Answer: Use recipe.ingredients to log the ingredients separately and iterate over them.
+// use recipe.ingredients.join('\n') to join the ingredients into a single string with new lines.
 
 const recipe = {
   title: "bruschetta",
@@ -10,6 +16,6 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
+
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+  ingredients: ${recipe.ingredients.join("\n")}`);
