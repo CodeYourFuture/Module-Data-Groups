2d22aceb78219f66b6504ce42f22b84c981d18ee
ZabihollahNamazi <zabihnamazi@gmail.com>
2024-11-29 16:19:34 +0000

    commit 1

--- Sprint-3/todo-list/script.js
+++ Sprint-3/todo-list/script.js
@@ -1,8 +1,12 @@
+let list = document.getElementById("todo-list");
 function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
+  
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  for(let todo in todos){
+    list.innerHTML += `<li>${todos[todo].task}. <input type="checkbox" class="check-list"}> <button type="">🗑️</button></li>`;
+  }
 }
-
+document.getElementById("submit").addEventListener("click", addNewTodo);
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
@@ -17,9 +21,27 @@ function addNewTodo(event) {
   // The code below prevents the page from refreshing when we click the 'Add Todo' button.
   event.preventDefault();
   // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  let new_obj = {};
+  let input_text = document.querySelector("#input-text");
+  new_obj.task = input_text.value;
+  new_obj.completed = false;
+  todos.push(new_obj);
+  list.innerHTML = "";
+  populateTodoList(todos);
+
 }
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
   // Write your code here...
-}
+  let check_list = document.querySelectorAll('.check-list'); // input[type="checkbox"]
+  check_list.forEach((checkbox, index) => {
+    if (checkbox.checked) {
+      checkbox.parentElement.remove();
+      todos.splice(index, 1); // Remove from the todos array
+    }
+  });
+  }
+
+
+
