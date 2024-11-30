e5ffb0a58d6ca7317ac6ad1e59ebf77f04a919bf
AmirMontazeri <mr.amir.mti@gmail.com>
2024-11-30 11:08:52 +0000

    sprint1-implement-sum-completed

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,11 @@
-function sum(elements) {
+function findMax(arr) {
+  if (arr.length === 0) return -Infinity;
+
+  const validNumbers = arr.filter((item) => typeof item === "number");
+
+  if (validNumbers.length === 0) return -Infinity;
+
+  return Math.max(...validNumbers);
 }
 
-module.exports = sum;
+module.exports = findMax;
