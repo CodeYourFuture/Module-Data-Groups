08ba8395f4d394e213cdf00b9cf0f4ba931de83a
AmirMontazeri <mr.amir.mti@gmail.com>
2024-11-30 11:06:12 +0000

    sprint1-implement-max-completed

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,15 @@
 function findMax(elements) {
+  if (elements.length === 0) return -Infinity;
+
+  let max = -Infinity;
+
+  for (let i = 0; i < elements.length; i++) {
+    if (typeof elements[i] === "number") {
+      max = Math.max(max, elements[i]);
+    }
+  }
+
+  return max;
 }
 
 module.exports = findMax;
