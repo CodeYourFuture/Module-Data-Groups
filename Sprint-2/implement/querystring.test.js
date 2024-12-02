f09f00328e2b03788cc43229071d621c73b988e0
AmirMontazeri <mr.amir.mti@gmail.com>
2024-12-02 21:17:34 +0000

    sprint2-querystring-completed

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,10 +3,46 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
 test("parses querystring values containing =", () => {
   expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+    equation: "x=y+1",
+  });
+});
+
+test("parses empty query string", () => {
+  expect(parseQueryString("")).toEqual({});
+});
+
+test("parses querystring with multiple key-value pairs", () => {
+  expect(parseQueryString("key1=value1&key2=value2")).toEqual({
+    key1: "value1",
+    key2: "value2",
+  });
+});
+
+test("parses querystring with no value for a key", () => {
+  expect(parseQueryString("key1=")).toEqual({
+    key1: "",
+  });
+});
+
+test("parses querystring with no '=' for a key", () => {
+  expect(parseQueryString("key1")).toEqual({
+    key1: null,
+  });
+});
+
+test("parses querystring with special characters", () => {
+  expect(parseQueryString("key1=hello%20world&key2=special%26chars")).toEqual({
+    key1: "hello%20world",
+    key2: "special%26chars",
+  });
+});
+
+test("parses querystring with duplicated keys", () => {
+  expect(parseQueryString("key1=value1&key1=value2")).toEqual({
+    key1: "value2", // Last value overwrites previous ones
   });
 });
