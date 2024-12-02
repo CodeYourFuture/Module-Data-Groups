6393541eac46e5e3d0370a71db8666b870fa70ef
AmirMontazeri <mr.amir.mti@gmail.com>
2024-12-02 20:41:07 +0000

    sprint2-address-updated

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 
+// Answer: address[0] attempts to access a property of the address object using array-like indexing, which isn't valid for objects in JavaScript. Objects in JavaScript use keys (strings) to access properties
+
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -11,6 +13,5 @@ const address = {
   country: "England",
   postcode: "XYZ 123",
 };
-
 
 console.log(`My house number is ${address["houseNumber"]}`);
