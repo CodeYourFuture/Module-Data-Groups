86677b64f1eaa7f976fa9e5a5a52b8706ba86dc4
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-12-02 22:16:26 +0000

    Add test for the sum function to test different scenarios

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,43 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test("Given an empty array,  it should return 0", () => {
+  expect(sum([])).toEqual(0);
+});
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
+test("Given an array with just one number,  it should return that number", () => {
+  expect(sum([666])).toEqual(666);
+});
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
+test("Given an array containing negative numbers,  it return the correct total sum", () => {
+  expect(sum([-2, -5, -7, -44])).toEqual(-58);
+});
+
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
+test("Given an array with decimal/float numbers,  it return the correct total sum", () => {
+  expect(sum([2.04, 5.3, 7.5, 4.4])).toEqual(19.240000000000002);
+});
+
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+test("Given an array containing non-number values,  it ignore the non-numerical values and return the sum of the numerical elements", () => {
+  expect(sum(['Danial', 5.3, 'hello', 4.4])).toEqual(9.7);
+});
 
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test("Given an array containing only non-number values,  it ignore the non-numerical values and return 0", () => {
+  expect(sum(["Danial", '5.3', "hello", '4.4'])).toEqual(0);
+});
