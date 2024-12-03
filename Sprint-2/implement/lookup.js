7ed4d8201c4d1d6f9667d8441601e6cbea5ce2db
Alaa <aliqassabuk@gmail.com>
2024-12-03 09:02:03 +0000

    created a country currency code lookup for multiple codes

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,17 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  return countryCurrencyPairs.reduce((lookup, [countryCode, currencyCode]) => {
+    lookup[countryCode] = currencyCode;
+
+    return lookup;
+  }, {});
 }
 
 module.exports = createLookup;
+// console.log(
+//   createLookup([
+//     ["US", "USD"],
+//     ["CA", "CAD"],
+//     ["US", "USD"],
+//     ["US", "USN"],
+//   ])
+// );
