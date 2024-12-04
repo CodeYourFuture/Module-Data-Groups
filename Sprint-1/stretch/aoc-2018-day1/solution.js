c5ff7bdf130e88f30ab7fc516bcceff0188227c1
Armin <107566068+ArminNouri98@users.noreply.github.com>
2024-12-04 22:46:00 +0000

    Update solution.js
    add reading file input.txt

--- Sprint-1/stretch/aoc-2018-day1/solution.js
+++ Sprint-1/stretch/aoc-2018-day1/solution.js
@@ -1,3 +1,13 @@
+const filePath = './input.txt';
+
+// Read the file asynchronously
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+    calculateFrequency(data);
+});
 function calculateFrequency(changes) {
   let frequency = 0;
   for (const change of changes) {
