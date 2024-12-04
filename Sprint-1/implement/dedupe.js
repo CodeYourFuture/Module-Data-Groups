d679b58413a64302c604320ded6b42216d953996
donarbl <donarablanc@gmail.com>
2024-12-04 12:35:51 +0000

    first test with empty array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,8 @@
-function dedupe() {}
+function dedupe() {
+
+  
+}
+// Given an empty array
+// When passed to the dedupe function
+// Then it should return an empty array
+// test.todo("given an empty array, it returns an empty array");
