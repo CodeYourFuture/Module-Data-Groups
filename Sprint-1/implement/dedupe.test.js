a52b4918a04c8f5e29f4784bab97f95a97ee1032
ArminNouri <nouriarmin24@gmail.com>
2024-12-05 21:35:35 +0000

    fix test and more test

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -25,28 +25,33 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
-describe("calculateMedian", () => {
-  test("removes duplicates from an array with duplicates", () => {
-    expect(dedupe([1, 2, 2, 3, 4, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+
+describe("dedupe", () => {
+  test("removes duplicates from an array", () => {
+    expect(dedupe([2, 3, 2, 3, 5])).toEqual([2, 3, 5]);
   });
 
   test("returns the same array when all elements are unique", () => {
     expect(dedupe([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
   });
-  test("returns the same array when all elements are unique", () => {
+
+  test("handles an array of strings and removes duplicates", () => {
     expect(dedupe(["A", "A", "b", "b", "c"])).toEqual(["A", "b", "c"]);
   });
-  test("returns an empty array when input is an empty array", () => {
+
+  test("returns an empty array when the input is an empty array", () => {
     expect(dedupe([])).toEqual(
       "given an empty array, it returns an empty array"
     );
   });
 
-  test("returns undefined when input is undefined", () => {
-    expect(dedupe(undefined)).toBeUndefined();
+  test("returns undefined when the input is undefined", () => {
+    expect(dedupe(undefined)).toEqual(
+      "given an empty array, it returns an empty array"
+    );
   });
 
-  test("works with mixed data types in the array", () => {
+  test("works with mixed data types, treating them as unique values", () => {
     expect(dedupe([1, "1", 2, "2", 3, 3, "3"])).toEqual([
       1,
       "1",
