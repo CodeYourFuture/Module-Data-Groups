78a5292e65ab311e6f9ec8dc87dc7903e0359aa2
saraamiiri <amirisara1993@gmail.com>
2024-12-05 18:15:43 +0000

    Update sum.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -2,7 +2,7 @@ function sum(arr) {// sum.js
 
       return arr.reduce((total, current) => {
         // Only add current if it's a number (including integers and floats)
-        if (typeof current === 'number') {
+        if (typeof current === 'number'&&!isNaN(current)) {
           return total + current;
         }
         return total;
