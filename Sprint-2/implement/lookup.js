e349d7e8ea9cd45b24214854295d97c6514825d3
donarbl <donarablanc@gmail.com>
2024-12-05 14:32:39 +0000

    used Object.fromEntries() to pair

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,5 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {// is the parameter of the function 
+  return Object.fromEntries(countryCurrencyPairs);// Object.
 }
 
 module.exports = createLookup;
