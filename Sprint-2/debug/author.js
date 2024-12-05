f68933c30ab6fef4525b884bb3e9b365ba03acbb
donarbl <donarablanc@gmail.com>
2024-12-05 12:49:58 +0000

    used for... in loop to extract the values

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -11,6 +11,7 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
-  console.log(value);
+// for (const value of author) {// the problem is with for...of loop, it only works with arrays and strings that are iterables, the objects do not have an inherent order or iterable protocol (like arrays do). Instead use for... in loop to go through the keys
+for (const key in author){
+  console.log(author[key]);// the for...in goes through each key in the author object (first name etc), then we access the value linked to each key using author[key], that will print the value 
 }
