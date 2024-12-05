8edc8d174fb79e006261b6fa7866088668451dd8
donarbl <donarablanc@gmail.com>
2024-12-05 14:18:24 +0000

    checked object via if statement , and in operator

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,10 @@
-function contains() {}
+function contains(object, property) {// the function constains takes in two parameters: object and property
+  // Check if the first argument is an object (and not null)
+  if (typeof object !== 'object' || object === null) {//typeof object !== 'object' to check if the object is not of type object. Additionally, we check if the object is not null because typeof null also returns 'object', which is misleading.
+    return false;//f the object is not an object (like a string, number, etc.), we immediately return false.
+
+  }
+  return property in object;// in operator, property in object checks if the property exists in object, if it does,  it returns true, otherwise it's false
+}
 
 module.exports = contains;
