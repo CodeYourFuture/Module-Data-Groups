155896ed0e049ac162fabb500df14871485c1336
donarbl <donarablanc@gmail.com>
2024-12-05 12:42:31 +0000

    access the property via dot notation

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -4,12 +4,13 @@
 // but it isn't working...
 // Fix anything that isn't working
 
-const address = {
-  houseNumber: 42,
+const address = {// so this is an object that is defined as a address
+  houseNumber: 42,// the object has 5 properties, the syntax is correct, curly braces, the key words with columns
   street: "Imaginary Road",
   city: "Manchester",
   country: "England",
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+// console.log(`My house number is ${address[0]}`);// the problem is with address[0] that is looking for a property that has key 0 in the object. the address object has a key houseNumber that has a value of 42. thus, address[0] return undefined
+console.log(`My house number is ${address.houseNumber}`);// this corrects the return value, we are accessing via dot notation the actual name which is the houseNumber
