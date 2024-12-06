6908f24b2c2600f95f1f031da16b292878602781
saraamiiri <amirisara1993@gmail.com>
2024-12-06 22:10:21 +0000

    lookup.test.js

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,6 +1,15 @@
 const createLookup = require("./lookup.js");
+test("creates a country currency code lookup for multiple codes", () => {
+  const countryCurrencyPairs = [['US', 'USD'], ['CA', 'CAD']];
+  const result = createLookup(countryCurrencyPairs);
 
-test.todo("creates a country currency code lookup for multiple codes");
+  expect(result).toEqual({
+    'US': 'USD',
+    'CA': 'CAD'
+  });
+});
+
+
 
 /*
 
