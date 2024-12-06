d435014a992eb074e527273bc3525fe07311bc81
saraamiiri <amirisara1993@gmail.com>
2024-12-06 15:16:53 +0000

    contain.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,10 @@
-function contains() {}
-
-module.exports = contains;
+function contains(obj, key) {
+    if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
+      return false; // Return false if the parameter is not an object
+    }
+    
+    return obj.hasOwnProperty(key); // Checks if the object has the specified key
+  }
+  
+  module.exports = contains;
+  
