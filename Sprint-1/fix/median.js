7e3fcb8beec4e6df92e70c2bca43839f20be0b48
AFatmaa <fatma.arslantas@outlook.com>
2024-12-06 18:47:38 +0000

    Updated calculateMedian function

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -5,24 +5,23 @@
 function calculateMedian(list) {
 
   // 〰️ Create a copy of the input list to avoid modifying the original array
-  const copiedList = [...list];
+  const copiedList = [...list].sort((a, b) => a - b); // 〰️ Ensure the list is sorted
 
   // 〰️ Calculate the index of the middle element
   const middleIndex = Math.floor(copiedList.length / 2)
 
   // 〰️ If the list length is odd, return the middle element
   if (copiedList.length % 2 === 1) {
-    const median = copiedList.splice(middleIndex, 1)[0];
-    return median;
+    return copiedList[middleIndex]; // 〰️ Directly access the middle element;
   }
 
   // 〰️ If the list length is even, calculate the average of the two middle elements
-  const median1 = copiedList[middleIndex]
-  const median2 = copiedList[middleIndex -1] 
-  return (median1 + median2) / 2;
+  return (copiedList[middleIndex] + copiedList[middleIndex - 1]) / 2; 
 }
 
 console.log(calculateMedian([1, 2, 3, 4, 5, 6]));
 console.log(calculateMedian([1, 2, 3, 4, 5]));
+console.log(calculateMedian([1, 2, 6, 5, 4, 3]));
+console.log(calculateMedian([1, 2, 5, 4, 3]));
 
 module.exports = calculateMedian;
