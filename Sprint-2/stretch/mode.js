9dda208a03d07177a1148efdd69b3ffce040f1ca
HaftamuKebedew <haftamukebedew@gmail.com>
2024-12-07 04:22:22 +0000

    countFrequencies and findMode defined in mode files and   invoked in  the function calculatedMode

--- Sprint-2/stretch/mode.js
+++ Sprint-2/stretch/mode.js
@@ -8,21 +8,24 @@
 // refactor calculateMode by splitting up the code
 // into smaller functions using the stages above
 
-function calculateMode(list) {
-  // track frequency of each value
+
+function countFrequencies(list) {
   let freqs = new Map();
 
   for (let num of list) {
-    if (typeof num !== "number") {
+    if (typeof num !== "number") 
       continue;
-    }
-
     freqs.set(num, (freqs.get(num) || 0) + 1);
   }
 
-  // Find the value with the highest frequency
+  return freqs;
+}
+
+
+function findMode(freqs) {
   let maxFreq = 0;
   let mode;
+
   for (let [num, freq] of freqs) {
     if (freq > maxFreq) {
       mode = num;
@@ -31,6 +34,10 @@ function calculateMode(list) {
   }
 
   return maxFreq === 0 ? NaN : mode;
+}
+function calculateMode(list) {
+  const freqs = countFrequencies(list); // Track frequencies
+  return findMode(freqs); // Find and return the mode
 }
 
 module.exports = calculateMode;
