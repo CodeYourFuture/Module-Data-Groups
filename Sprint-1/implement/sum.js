f33a91d26c7b82c7e56d7a7bcd1ede9b7644ab99
HaftamuKebedew <haftamukebedew@gmail.com>
2024-12-07 02:37:14 +0000

    sum function defined

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,18 @@
 function sum(elements) {
+    if (!Array.isArray(elements)) {
+        throw new TypeError("Input must be an array.");
+      }
+    
+      let total = 0;
+    
+      for (let h = 0; h < elements.length; h++) {
+        const current = elements[h];
+        if (typeof current === 'number') {
+          total += current;
+        }
+      }
+    
+      return total;
 }
 
 module.exports = sum;
