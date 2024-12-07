d6193f66d2a3b6af94a39851f378715a4462fcea
HaftamuKebedew <haftamukebedew@gmail.com>
2024-12-07 03:30:52 +0000

    contains test case defined

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,20 +16,35 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
-
+test("contains on empty object returns false", () => {
+    expect(contains({}, 'h')).toBe(false);
+  });
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains with an existing property returns true", () => {
+    expect(contains({h: 1, k: 2}, 'h')).toBe(true);
+  });
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-
+test("contains with a non-existent property returns false", () => {
+    expect(contains({h: 1, k: 2}, 'b')).toBe(false);
+  });
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
-
+test("contains on an array returns false", () => {
+    expect(contains([], 'k')).toBe(false);
+  });
+  
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("contains on invalid parameters returns false", () => {
+    expect(contains(123, 'h')).toBe(false);  // number
+    expect(contains('string', 'h')).toBe(false);  // string
+    expect(contains(null, 'h')).toBe(false);  // null
+  });
+  
