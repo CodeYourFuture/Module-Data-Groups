082eaeaea9a11d75fb84a580ede5ba23c085de3c
bashirzadehdanial <bashirzadehdanial@gmail.com>
2024-12-08 15:34:06 +0000

    Implement till function and comprehensive tests for its functionality

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -4,37 +4,41 @@
 // When this till object is passed to totalTill
 // Then it should return the total amount in pounds
 
-// function totalTill(till) {
-//   let total = 0;
+function totalTill(till) {
+  let total = 0;
 
-//   for (const [coin, quantity] of Object.entries(till)) {
-//     total += coin * quantity;
-//   }
+  for (const [coin, quantity] of Object.entries(till)) {
+    const coinValue = parseInt(coin.replace("p", ""), 10);
+    total += coinValue * quantity;
+    
+  }
 
-//   return `£${total / 100}`;
-// }
-
-// const till = {
-//   "1p": 10,
-//   "5p": 6,
-//   "50p": 4,
-//   "20p": 10,
-// };
-// const totalAmount = totalTill(till);
-
-
-
-function totalTill(){
-  return {}
+  return `£${total / 100}`;
 }
 
+const till = {
+  "1p": 10,
+  "5p": 6,
+  "50p": 4,
+  "20p": 10,
+};
+const totalAmount = totalTill(till);
+
+console.log(totalAmount);
+
+
 // a) What is the target output when totalTill is called with the till object
+// The code will fail because the keys in the till object are strings. 
+// Multiplying a string by a number results in NaN.
 
 // b) Why do we need to use Object.entries inside the for...of loop in this function?
+// It returns an array of key-value pairs for the object, allowing us to iterate over both key and value.
 
 // c) What does coin * quantity evaluate to inside the for...of loop?
+// In each iteration prints NaN
 
 // d) Write a test for this function to check it works and then fix the implementation of totalTill
+
 
 
 
