196300081cd17b8285c993f7c187b1e724e484b6
Mohammed <mohammed22471@outlook.com>
2024-12-08 05:12:02 +0000

    Update contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,8 +1,11 @@
-function contains(obj, property) {
-  if (typeof obj !== 'object' || obj === null) {
-    throw new Error("First argument must be an object.");
-  }
-  return property in obj;
+function contains(checkObj, property) {
+    if (typeof checkObj !== 'object' || // Not an object
+        checkObj === null || // Null is not an object
+        Array.isArray(checkObj) // Arrays should be treated as invalid for this case
+    ) {
+        return false;
+    }
+    return checkObj.hasOwnProperty(property);
 }
 
 module.exports = contains;
