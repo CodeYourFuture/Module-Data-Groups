c16b4854f41cd837477aca8917d2e52fdee1de4d
saraamiiri <amirisara1993@gmail.com>
2024-12-08 11:09:07 +0000

    complite implement/tally.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,17 @@
-function tally() {}
+function tally(items) {
+    // Step 1: Input validation
+    if (!Array.isArray(items)) {
+      throw new Error("Input must be an array");
+    }
+  
+    // Step 2: Counting the frequency of each item
+    const result = {};
+    items.forEach(item => {
+      result[item] = (result[item] || 0) + 1;
+    });
+  
+    // Step 3: Return the result object
+    return result;
+}
 
 module.exports = tally;
