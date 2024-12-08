a9a069f03a660591bd7171c432f393896a53c429
saraamiiri <amirisara1993@gmail.com>
2024-12-08 11:37:34 +0000

    implement.invert.js

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -17,13 +17,33 @@ function invert(obj) {
 }
 
 // a) What is the current return value when invert is called with { a : 1 }
+//answer:{ key: 1 }
+
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
+//answer:{ key: 2 }
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
+//answer:{ "1": "a", "2": "b" }
+
 
 // c) What does Object.entries return? Why is it needed in this program?
 
+//Object.entries(obj) returns an array of [key, value] pairs from the object:
+
 // d) Explain why the current return value is different from the target output
+//In the loop, the code is using invertedObj.key = value instead of setting the value dynamically with invertedObj[value] = key. 
+//As a result, only the last key-value pair is stored with the same static key ("key").
+
 
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
+function invert(obj) {
+  const invertedObj = {};
+
+  for (const [key, value] of Object.entries(obj)) {
+    invertedObj[value] = key;
+  }
+
+  return invertedObj;
+}
+module.exports = invert;
