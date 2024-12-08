65eb2f4563cf2a873bef54e4ff1891bef6e1f0b7
saraamiiri <amirisara1993@gmail.com>
2024-12-08 11:01:35 +0000

    compliting implement/querystring.js

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,24 @@
 function parseQueryString(queryString) {
   const queryParams = {};
+
   if (queryString.length === 0) {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    // Find the first '=' to split between key and value
+    const separatorIndex = pair.indexOf("=");
+
+    if (separatorIndex === -1) {
+      // If there's no '=', treat it as a key with an empty value
+      queryParams[pair] = "";
+    } else {
+      const key = pair.slice(0, separatorIndex);
+      const value = pair.slice(separatorIndex + 1);
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -2,11 +2,56 @@
 // Unfortunately, it contains several bugs!
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
-
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
 test("parses querystring values containing =", () => {
   expect(parseQueryString("equation=x=y+1")).toEqual({
     "equation": "x=y+1",
   });
+});
+
+test("parses multiple key-value pairs", () => {
+  expect(parseQueryString("a=1&b=2")).toEqual({
+    "a": "1",
+    "b": "2",
+  });
+});
+
+test("parses querystring with multiple equals in value", () => {
+  expect(parseQueryString("a=1&b=x=y+1&c=3")).toEqual({
+    "a": "1",
+    "b": "x=y+1",
+    "c": "3",
+  });
+});
+
+test("parses querystring with empty value", () => {
+  expect(parseQueryString("a=&b=2")).toEqual({
+    "a": "",
+    "b": "2",
+  });
+});
+
+test("parses querystring with spaces in values", () => {
+  expect(parseQueryString("a=hello world&b=foo bar")).toEqual({
+    "a": "hello world",
+    "b": "foo bar",
+  });
+});
+
+test("parses querystring with special characters", () => {
+  expect(parseQueryString("a=%20&b=123")).toEqual({
+    "a": "%20",
+    "b": "123",
+  });
+});
+
+test("parses querystring with only one parameter", () => {
+  expect(parseQueryString("a=1")).toEqual({
+    "a": "1",
+  });
+});
+
+test("parses empty querystring", () => {
+  expect(parseQueryString("")).toEqual({});
 });
