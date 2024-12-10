cf02875156a871915d96c49c4d125c58cbdb8ea5
nadika <nadikazavodovska@gmail.com>
2024-12-10 16:37:55 +0000

    Get buttons, and image blocks

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -4,5 +4,7 @@ const images = [
     "./assets/cute-cat-c.jpg",
 ];
 
+const backwardBtn = document.getElementById("backward-btn");
+const forwardBtn = document.getElementById("forward-btn");
+const imgSlider = document.getElementById("carousel-img");
 
-// Write your code here
