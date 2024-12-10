02b231ecde26bc59dc97553e95ba2b52d3104999
nadika <nadikazavodovska@gmail.com>
2024-12-10 16:50:55 +0000

    Add eventListener for forward button

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -8,3 +8,10 @@ const backwardBtn = document.getElementById("backward-btn");
 const forwardBtn = document.getElementById("forward-btn");
 const imgSlider = document.getElementById("carousel-img");
 
+let index = 0;
+
+forwardBtn.addEventListener('click', () => {
+    index = (index + 1) % images.length;
+    imgSlider.src = images[index];
+})
+
