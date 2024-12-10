093ad63dd79eeb448a8033f441f6b5f8ab241865
Simpra11 <mrmabuya11@gmail.com>
2024-12-10 22:55:00 +0200

    Fixed my function so it can handle even length arrays

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,10 +3,21 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
+
   const middleIndex = Math.floor(list.length / 2);
-  const median = list.slice(middleIndex, middleIndex + 1)[0];
-  return median;
-}
+
+  if(list.length % 2 === 0){
+    const mid1 = list[middleIndex - 1];
+    const mid2 = list[middleIndex];
+    return (mid1 + mid2) / 2;
+  } else {
+    
+    return list[middleIndex];
+  }
+
+  }
+  
+
 
 module.exports = calculateMedian;
 
