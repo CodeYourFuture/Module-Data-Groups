8e66321361a4ff320579a1c5ed1fec4715458fda
nadika <nadikazavodovska@gmail.com>
2024-12-10 17:27:48 +0000

    Add eventListeners to autoForward, autoBackward, stop buttons

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -4,9 +4,12 @@ const images = [
     "./assets/cute-cat-c.jpg",
 ];
 
-const forwardBtn = document.getElementById("forward-btn");
-const backwardBtn = document.getElementById("backward-btn");
 const imgSlider = document.getElementById("carousel-img");
+const autoBackwardBtn = document.getElementById("auto-backward-btn");
+const backwardBtn = document.getElementById("backward-btn");
+const autoForwardBtn = document.getElementById("auto-forward-btn");
+const forwardBtn = document.getElementById("forward-btn");
+const stopBtn = document.getElementById("stop-btn");
 
 let index = 0;
 
@@ -14,13 +17,30 @@ function moveForward() {
     index = (index + 1) % images.length;
     imgSlider.src = images[index];
 }
-forwardBtn.addEventListener('click', moveForward);
 
-function moveBackforward() {
+function moveBackward() {
     index = (index - 1 + images.length) % images.length;
     imgSlider.src = images[index];
 }
-backwardBtn.addEventListener('click', moveBackforward);
+
+forwardBtn.addEventListener('click', moveForward);
+backwardBtn.addEventListener('click', moveBackward);
+
+let showInterval = null;
+
+autoBackwardBtn.addEventListener('click', () => {
+    clearInterval(showInterval);
+    showInterval = setInterval(moveForward, 5000);
+});
+
+autoForwardBtn.addEventListener('click', () => {
+    clearInterval(showInterval);
+    showInterval = setInterval(moveBackward, 5000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(showInterval);
+})
 
 
 
