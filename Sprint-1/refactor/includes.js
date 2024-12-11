58fc4f5687582dbe14ba7a4fcf3cc70fe8371095
Serna Malala <sm.sernamalala@gmail.com>
2024-12-11 17:59:44 +0200

    Implemented a for of loop in the given code

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,8 +1,8 @@
 // Refactor the implementation of includes to use a for...of loop
 
+//for of loop iterates over each element without needing to use the index to access a particular element
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
+  for (const element of list) {
     if (element === target) {
       return true;
     }
