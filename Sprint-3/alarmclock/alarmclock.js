ae98e344615a72a3406f46d52a62985b3a85643e
donarbl <donarablanc@gmail.com>
2024-12-11 15:17:29 +0000

    defind function

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,19 @@
-function setAlarm() {}
+function setAlarm() {
+  // this will get the input value and convert it to a number
+  const inputField = document.getElementById("alarmSet");// document.getElementById("alarmSet") it takes the input box where the user types the number of seconds
+  let timeRemaining = parseInt(inputField.value,10);// .value get the text inside the input box, parseInt(inputField.value, 10), converts the input which is a string into a number so that we can do arithmetics with it.
+
+  // find the heading where the time will be displayed
+  const timeDisplay = document.getElementById("timeRemaining");
+
+  // this function will help to format time as MM:SS
+  function formatTIme(seconds){
+    const minutes = Math.floor(seconds /60);
+    const secs =seconds % 60;
+    return `Time Remaining: ${String(minutes).padStart(2, "0")}:${String(secs).padStart(2,0)}`;
+    
+  }
+}
 
 // DO NOT EDIT BELOW HERE
 
