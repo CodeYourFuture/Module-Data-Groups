5578f853814e3d717cca4389c81e90c55db74133
Simpra11 <mrmabuya11@gmail.com>
2024-12-11 02:50:35 +0200

    Created a findMax function that is able to handle different cases and finds largest number

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,9 @@
 function findMax(elements) {
+    const filteredElements = elements.filter(item => typeof item === "number" && !isNaN(item));
+
+    return Math.max(...filteredElements);
 }
 
 module.exports = findMax;
+
+console.log(findMax([4, "Hi", 10, 3,"me", 11, 20]));
