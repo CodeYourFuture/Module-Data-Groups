3f9d828747549a35b8bd11d565ddad18204e15b2
Simpra11 <mrmabuya11@gmail.com>
2024-12-12 00:23:32 +0200

    Created a function that sums up the numerical elements of an array

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,12 @@
 function sum(elements) {
+    let total = 0;
+
+    for(let i = 0; i < elements.length; i++){
+        total += elements[i];
+    }
+    return total;
 }
 
 module.exports = sum;
+
+console.log(sum([20, 10, 5]));
