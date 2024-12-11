f617298329e32843914288154e22e478e7e93e29
Serna Malala <sm.sernamalala@gmail.com>
2024-12-11 17:02:35 +0200

    Created a function that takes a list and returns the maximum value in the list

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,29 @@
 function findMax(elements) {
+
+    let maxElement = -Infinity;
+
+    if (elements.length === 0) {
+        //return infinity if list is empty as stated in the instructions
+        return Infinity;
+    }
+
+    else {
+
+        for (let i = 0; i < elements.length; i++) {
+            if (!isNaN(elements[i])) {
+
+                if (Number(elements[i]) > maxElement) {
+                    maxElement = Number(elements[i]);
+
+                }
+            }
+
+        }
+
+        return maxElement
+    }
 }
 
+// console.log(findMax([30, 50, 10, 40]))
+// console.log(findMax(['hey', 10, 'hi', 60, 10]))
 module.exports = findMax;
