95c8176468e524022bbc832945fcfaf62682603e
Serna Malala <sm.sernamalala@gmail.com>
2024-12-11 17:30:02 +0200

    Created a sum function that adds numerical elements in an array

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,39 @@
 function sum(elements) {
+
+    let isNumberAvailable = false;
+    let sumValue = 0;
+
+    if (elements.length === 0) {
+        //return 0 if list is empty as stated in the instructions
+        return 0;
+    }
+
+    else {
+        //loop through element that is considered a number and add it to sumValue
+
+        for (let i = 0; i < elements.length; i++) {
+            if (!isNaN(elements[i])) {
+
+                isNumberAvailable = true;
+
+                sumValue += elements[i];
+
+            }
+
+        }
+        //if there are no numerical values
+        if (!isNumberAvailable) {
+            return -Infinity;
+        }
+
+        return sumValue
+    }
 }
+
+// console.log(sum([30, 50, 10, 40]))
+// console.log(sum(['hey', 10, 'hi', 60, 10]))
+// console.log(sum(['serna', 'malala', 'sernamalala', 'hello', 'annyeong', 'hola', 'bonjour']))
+
+
 
 module.exports = sum;
