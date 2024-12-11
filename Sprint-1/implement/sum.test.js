d9aabbf1b3b295902bb4e902c85695b4a6bea0e3
Simpra11 <mrmabuya11@gmail.com>
2024-12-12 01:10:06 +0200

    Created the rest of the sum test cases and they all passed

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -9,10 +9,33 @@ E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical
 const sum = require("./sum.js");
 
 describe("sum", () => {
-    test("given an empty array, it should returns 0", () => {
+
+    test("Given an empty array, it should returns 0", () => {
         expect(sum([])).toEqual(0);
     });
-})
+
+    test("Given an array with just one number, it should return that number", () => {
+        expect(sum([10])).toEqual(10);
+    });
+
+    test("Given an array containing negative numbers, it should still return the correct total sum", () => {
+        expect(sum([-3, -10, -60, -10])).toEqual(-83);
+    });
+
+    test("Given an array with decimal/float numbers, it should return the correct total sum", () => {
+        expect(sum([2.1, 34.9, 13.8, 4.7])).toEqual(55.5);
+    });
+
+    test("Given an array containing non-number values, it should ignore the non-numerical values and return the sum of the numerical elements", () => {
+        expect(sum(['hey', 10, 'hi', 60, 10])).toEqual(80);
+    });
+
+    test("Given an array with only non-number values, it should return the least surprising value given how it behaves for all other inputs", () => {
+        expect(sum(['A', '@', '&', 'd', '%'])).toEqual(0);
+    });
+
+
+});
 
 
 
