2b7c5ecf3ee2434fa93172a3763baf3233eeda4c
Serna Malala <sm.sernamalala@gmail.com>
2024-12-11 16:27:53 +0200

    created a duplicate function that takes lists as inputs and returns a list with no duplicates

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,21 @@
-function dedupe() {}
+function dedupe(list) {
+
+    let newList = [];
+    if (list.length === 0) {
+        //return the empty list if an empty list is given as an input
+        return list;
+
+    }
+
+    else {
+        //else take the list given and iterate through the list to find out out if any of the elements are not already in the newList
+
+        for (let i = 0; i < list.length; i++) {
+            if (!newList.includes(list[i])) {
+                newList.push(list[i])
+            }
+        }
+
+        return newList;
+    }
+}
