8c9b6aeda2ae3e56bd0ed419817cbe7d40c18268
donarbl <donarablanc@gmail.com>
2024-12-12 13:40:52 +0000

    improve lighthouse rendering by including the meta name description and color constract

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -1,19 +1,17 @@
-const newQuoteBtn = document.getElementById("new quote");
+const newQuoteBtn = document.getElementById("new-quote");
 const quoteBlock = document.getElementById("quote");
 const authorBlock = document.getElementById("author");
 
-function displayQuote(){
-  let pickedQuoteObject = pickFromArray(quotes);
-  let pickedQuoteArray = Object.entries(pickedQuoteObject);
-  let pickedQuoteString = pickedQuoteArray[0][1];
-  let pickedAuthorString = pickedQuoteArray[1][1];
-  quoteBlock.textContent = `${pickedQuoteString}`;
-  authorBlock.textContent =`${pickedAuthorString}`;
-
+function displayQuote() {
+  let chosenQuoteObject = pickFromArray(quotes);
+  let chosenQuoteArray = Object.entries(chosenQuoteObject);
+  let chosenQuoteString = chosenQuoteArray[0][1];
+  let chosenAuthorString = chosenQuoteArray[1][1];
+  quoteBlock.textContent = `"${chosenQuoteString}"`;
+  authorBlock.textContent = `${chosenAuthorString}`;
 }
 newQuoteBtn.addEventListener('click', displayQuote);
-window.addEventListener.apply('load', displayQuote)
-
+window.addEventListener('load', displayQuote);
 // DO NOT EDIT BELOW HERE
 
 // pickFromArray is a function which will return one item, at
