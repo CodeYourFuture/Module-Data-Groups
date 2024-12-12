eff75ff36b759329785df7da72a9af889ee27473
donarbl <donarablanc@gmail.com>
2024-12-12 10:57:50 +0000

    updated the code as previous was not working

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,24 +1,43 @@
 function setAlarm() {
-  // this will get the input value and convert it to a number
-  const inputField = document.getElementById("alarmSet");// document.getElementById("alarmSet") it takes the input box where the user types the number of seconds
-  let timeRemaining = parseInt(inputField.value,10);// .value get the text inside the input box, parseInt(inputField.value, 10), converts the input which is a string into a number so that we can do arithmetics with it.
+  const inputField = document.getElementById("alarmSet"); // Input field for seconds
+  const timeDisplay = document.getElementById("timeRemaining"); // Time display element
+  let inputValue = parseInt(inputField.value)//Get time in seconds
 
-  // find the heading where the time will be displayed
-  const timeDisplay = document.getElementById("timeRemaining");
-
-  // this function will help to format time as MM:SS
-  function formatTIme(seconds){
-    const minutes = Math.floor(seconds /60);// calculates the number of minutes in the totality of seconds
-    const secs =seconds % 60; // calculates the remaining seconds after converting to minutes
-    return `Time Remaining: ${String(minutes).padStart(2, "0")}:${String(secs).padStart(2,0)}`; // String(value).padStart(2, "0") formats numbers so that they always have 2 digits, like  1 -> 01
-
-    // sets initial display
-    timeDisplay.innerText = formatTIme(timeRemaining);
-
-    
-
+  if (isNaN(inputValue) || inputValue <= 0){
+    alert ("Please enter a valid number of seconds");
+    return;
   }
-}
+  // Function to format time as MM:SS
+  function getformatTime(seconds) {
+    const minutes = Math.floor(seconds / 60).toString().padStart(2, "0");
+    const secs = (seconds % 60).toString().padStart(2, "0");
+    return `${minutes}:${secs}`;
+  }
+
+  timeDisplay.textContent = `Time Remaining: ${getformatTime(inputField)}`
+
+  alarmInterval = setInterval (() =>{
+    inputField -= 1;
+
+    if (inputField <= 0){
+      clearInterval(alarmInterval);
+      timeDisplay.textContent = "Time remaining: 00:00";
+      playAlarm();
+      document.body.style.backgroundColour = "Blue";
+    }else {
+      timeDisplay.textContent = `Time remaining : ${getformatTime(inputField)}`;
+    }
+    },1000);
+  }
+  function pauseAlarm (){
+    audio.pause();
+    clearInterval(countdownInterval);
+  }
+
+
+
+
+
 
 // DO NOT EDIT BELOW HERE
 
