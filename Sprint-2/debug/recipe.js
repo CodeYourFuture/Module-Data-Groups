d57debe29b7a598981c4e736c0889379e50e6438
Iakub Dubachev <gorbone1@gmail.com>
2024-12-13 21:09:30 +0000

    recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,15 +1,13 @@
-// Predict and explain first...
-
-// This program should log out the title, how many it serves and the ingredients.
-// Each ingredient should be logged on a new line
-// How can you fix it?
-
 const recipe = {
   title: "bruschetta",
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
+// Explanation:
+// The issue was directly interpolating `recipe`, which converts the object to `[object Object]`.
+// To fix this, I needed access each property and loop over the `ingredients` array to format it correctly.
+
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+Ingredients:
+${recipe.ingredients.join("\n")}`);
