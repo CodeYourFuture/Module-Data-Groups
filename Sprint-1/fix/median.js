4798dc357b028cc1b1e8ac6d86f78eb68841ccd9
mgphone <pmn.blazer@gmail.com>
2024-12-13 12:42:40 +0000

    sprint 1 fix

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,9 +3,15 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
+  let isEven = list.length % 2 == 0;
   const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  if (isEven) {
+    const median = (list[middleIndex] + list[middleIndex - 1]) / 2;
+    return median;
+  } else {
+    const median = list[middleIndex];
+    return median;
+  }
 }
 
 module.exports = calculateMedian;
