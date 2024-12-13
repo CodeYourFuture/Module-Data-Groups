87a61bbffe6840e9874cd537f3882712ab433b6c
Iakub Dubachev <gorbone1@gmail.com>
2024-12-13 21:15:06 +0000

    lookup.js and lookup.test.js

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,35 +1,34 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+test("creates a country currency code lookup for multiple codes", () => {
+  const countryCurrencyPairs = [
+    ["US", "USD"],
+    ["CA", "CAD"],
+    ["GB", "GBP"],
+  ];
 
-/*
+  const expectedLookup = {
+    US: "USD",
+    CA: "CAD",
+    GB: "GBP",
+  };
 
-Create a lookup object of key value pairs from an array of code pairs
+  expect(createLookup(countryCurrencyPairs)).toEqual(expectedLookup);
+});
 
-Acceptance Criteria:
 
-Given
- - An array of arrays representing country code and currency code pairs
-   e.g. [['US', 'USD'], ['CA', 'CAD']]
+test("returns an empty object for an empty array", () => {
+  expect(createLookup([])).toEqual({});
+});
 
-When
- - createLookup function is called with the country-currency array as an argument
 
-Then
- - It should return an object where:
- - The keys are the country codes
- - The values are the corresponding currency codes
+test("throws an error if input is not an array", () => {
+  expect(() => createLookup(null)).toThrow("Input must be an array of arrays");
+});
 
-Example
-Given: [['US', 'USD'], ['CA', 'CAD']]
 
-When
-createLookup(countryCurrencyPairs) is called
-
-Then
-It should return:
- {
-   'US': 'USD',
-   'CA': 'CAD'
- }
-*/
+test("throws an error if a pair does not contain two strings", () => {
+  expect(() => createLookup([["US", 123]])).toThrow(
+    "Each pair must contain two strings"
+  );
+});
