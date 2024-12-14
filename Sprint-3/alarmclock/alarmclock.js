4f30d90d6c5fb7b3926c94ed945dd307eeb59bea
HaftamuKebedew <haftamukebedew@gmail.com>
2024-12-14 22:47:36 +0000

    alarm file updated to disply sound when it reaches 00:00

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,6 +1,42 @@
-function setAlarm() {}
 
-// DO NOT EDIT BELOW HERE
+function setAlarm() {
+  let countdown;
+  clearInterval(countdown);
+  document.body.style.backgroundColor = "";
+  const timeInput = parseInt(document.getElementById("alarmSet").value);
+
+  if (isNaN(timeInput) || timeInput <= 0) {
+    alert("Please enter a valid number of seconds.");
+    return;
+  }
+  let timeRemaining = timeInput;
+  if (timeRemaining === 10) {
+    triggerSpecialAlarm(); 
+  }
+  updateTimeDisplay(timeRemaining);
+  countdown = setInterval(() => {
+    timeRemaining--;
+    if (timeRemaining <= 0) {
+      clearInterval(countdown);
+      updateTimeDisplay(0);
+      playAlarm(); 
+    } else {
+      updateTimeDisplay(timeRemaining);
+    }
+  }, 1000);
+}
+
+function updateTimeDisplay(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  const secondsLeft = seconds % 60;
+  document.getElementById("timeRemaining").textContent = 
+    `Time Remaining: ${String(minutes).padStart(2, "0")}:${String(secondsLeft).padStart(2, "0")}`;
+}
+
+
+
+
+//No need to edit below
 
 var audio = new Audio("alarmsound.mp3");
 
