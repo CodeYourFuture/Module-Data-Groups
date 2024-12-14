1a0c475b2903fe84c20dde39621d21d8e4f6df3d
gai93003 <deng93003@gmail.com>
2024-12-14 15:36:44 +0000

    I made some changes based on the bugs

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 
+// It program will log error to the console since objects are not iterable like arrays.
+
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +13,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
