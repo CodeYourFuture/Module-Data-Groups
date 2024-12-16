c670711cf3b6547cb1e6c81ad2c90cab7f3c2f9c
Rashaad Ebrahim <rmebrahim@gmail.com>
2024-12-16 17:45:27 +0200

    sum function implemented and tests created.

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,11 @@
 function sum(elements) {
+  numericalElements = elements.filter((el) => typeof el === "number");
+  return numericalElements.length > 0
+    ? numericalElements.reduce((a, c) => a + c, 0)
+    : 0;
 }
 
 module.exports = sum;
+
+console.log(sum(["hey", 10, "hi", 60, 10])); // 80
+console.log(sum([])); // 0
