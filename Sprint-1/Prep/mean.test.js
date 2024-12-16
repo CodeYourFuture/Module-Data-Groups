02fc288969c63376315ed7e360eddf5b19b943c2
Emmanuel <esiziba71@gmail.com>
2024-12-16 19:15:32 +0200

    mean test case

---
+++ Sprint-1/Prep/mean.test.js
@@ -0,0 +1,7 @@
+test("calculates the mean of a list of numbers", () => {
+    const list = [3, 50, 7];
+    const currentOutput = calculateMean(list);
+    const targetOutput = 20;
+
+    expect(currentOutput).toEqual(targetOutput);
+});
