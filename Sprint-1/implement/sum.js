c39f85dd2f15745b09f37223afcb1d70cb92a0c7
Millena28 <millenakiflom06@gmail.com>
2024-12-16 12:58:26 +0000

    completed the refactor part

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -2,3 +2,10 @@ function sum(elements) {
 }
 
 module.exports = sum;
+ // Filter for numerical elements and sum them
+ return elements
+ .filter((element) => typeof element === "number")
+ .reduce((acc, num) => acc + num, 0); // Start with 0 as the initial sum
+}
+
+module.exports = sum;
