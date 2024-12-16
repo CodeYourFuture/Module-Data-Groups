7d83f5f5f5c1597c6f3d59cf83fafe35b1c66f75
AmirMontazeri <mr.amir.mti@gmail.com>
2024-12-16 18:47:11 +0000

    sprint1-refactor-includes.js-updated

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,5 +1,10 @@
 function includes(arr, target) {
-  return arr.includes(target);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = includes;
