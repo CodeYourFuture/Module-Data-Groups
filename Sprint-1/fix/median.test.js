56c230dba0f6097ac6557819fafb05714d2d18f6
Millena28 <millenakiflom06@gmail.com>
2024-12-16 13:25:45 +0000

    done the include part

--- Sprint-1/fix/median.test.js
+++ Sprint-1/fix/median.test.js
@@ -6,10 +6,9 @@
 
 const calculateMedian = require("./median.js");
 
+
 describe("calculateMedian", () => {
   test("returns the median for odd length array", () => {
-    expect(calculateMedian([1, 2, 3])).toEqual(2);
-    expect(calculateMedian([1, 2, 3, 4, 5])).toEqual(3);
     expect(calculateMedian([1, 2, 3])).toEqual(2);
     expect(calculateMedian([1, 2, 3, 4, 5])).toEqual(3);
   });
@@ -17,30 +16,8 @@ describe("calculateMedian", () => {
   test("returns the average of middle values for even length array", () => {
     expect(calculateMedian([1, 2, 3, 4])).toEqual(2.5);
     expect(calculateMedian([1, 2, 3, 4, 5, 6])).toEqual(3.5);
-    expect(calculateMedian([1, 2, 3, 4])).toEqual(2.5);
-    expect(calculateMedian([1, 2, 3, 4, 5, 6])).toEqual(3.5);
   });
 
-  test("doesn't modify the input", () => {
-    const list = [1, 2, 3];
-    calculateMedian(list);
-
-    expect(list).toEqual([1, 2, 3]);
-  });
-});
-const calculateMedian = require("./median.js");
-
-describe("calculateMedian", () => {
-
-  
-  test("returns the median for odd length array", () => {
-    expect(calculateMedian([1, 2, 3])).toEqual(2);
-    expect(calculateMedian([1, 2, 3, 4, 5])).toEqual(3);
-  });
-  test("returns the average of middle values for even length array", () => {
-    expect(calculateMedian([1, 2, 3, 4])).toEqual(2.5);
-    expect(calculateMedian([1, 2, 3, 4, 5, 6])).toEqual(3.5);
-  });
   test("doesn't modify the input", () => {
     const list = [1, 2, 3];
     calculateMedian(list);
