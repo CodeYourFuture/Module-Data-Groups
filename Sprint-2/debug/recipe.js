ee76ac4c83035f792055b14b56ef2cb170393dfc
Rashaad Ebrahim <rmebrahim@gmail.com>
2024-12-17 21:11:23 +0200

    debug section complete

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+    // the last variable in the template literal, ${recipe}, will return an objecy and not the ingredients.
+    // The variable is referring to the entire object not the specific item.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -12,4 +14,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients}`);
