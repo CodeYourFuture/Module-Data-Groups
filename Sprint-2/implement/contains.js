d7be78c2f6860eefd58ef42b254e48c08fbf7e89
Simpra11 <mrmabuya11@gmail.com>
2024-12-18 00:43:50 +0200

    Implemented a working function called contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,7 @@
-function contains() {}
+function contains(obj, property) {
+    return property in obj;
+}
 
 module.exports = contains;
+
+console.log(contains({name: "Mike", age: 25, city: "Cape Town"}, "age"));
