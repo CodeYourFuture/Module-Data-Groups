29e53eb74dc21b77e8d385d7d66780718dc3c611
Serna Malala <sm.sernamalala@gmail.com>
2024-12-17 18:20:02 +0200

    Thecreated a lookup function, that accepts an array of arrays

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,22 @@
-function createLookup() {
+function createLookup(input) {
   // implementation here
+
+  let currencyPairs = {};
+  //the input will be an array of arrays
+
+  for (const singleArray of input) {
+    if (singleArray.length === 2) {
+      //get a single array, loop through that and get the property and value
+      currencyPairs[singleArray[0]] = singleArray[1];
+    }
+    else {
+      console.error(`Invalid input ${singleArray}`);
+    }
+  }
+
+  return currencyPairs;
+
 }
 
+//console.log(createLookup([['US', 'USD'], ['CA', 'CAD']]));
 module.exports = createLookup;
