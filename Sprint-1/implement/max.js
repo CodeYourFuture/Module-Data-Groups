db6050b8805157de25bb252910b8a6ceb74acb79
Serna Malala <sm.sernamalala@gmail.com>
2024-12-17 09:27:02 +0200

    Updated the max function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,37 +1,32 @@
 function findMax(elements) {
 
     let maxElement = -Infinity;
-    let numberAvailable = false;
 
-    if (elements.length === 0) {
-        //return infinity if list is empty as stated in the instructions
-        return -Infinity;
-    }
 
-    else {
-        //loop through element that is considered a number
+    //loop through element that is considered a number
 
-        for (let i = 0; i < elements.length; i++) {
-            if (!isNaN(elements[i])) {
+    for (let i = 0; i < elements.length; i++) {
+        if (typeof elements[i] === "number" || !isNaN(elements[i])) {
 
-                numberAvailable = true;
-                //loop through each elemnt and compare the value against -Infinity
-                if (Number(elements[i]) > maxElement) {
-                    maxElement = Number(elements[i]);
-
-                }
+            let compareNumber = Number(elements[i]);
+            //loop through each elemnt and compare the value against -Infinity
+            if (compareNumber >= maxElement) {
+                maxElement = compareNumber;
             }
-
         }
-        if (!numberAvailable) {
-            return -Infinity;
-        }
-
-        return maxElement
     }
+
+    return maxElement
+
+
 }
 
 // console.log(findMax([30, 50, 10, 40]))
 // console.log(findMax(['hey', 10, 'hi', 60, 10]))
 //console.log(findMax(['serna', 'malala', 'sernamalala', 'hello', 'annyeong', 'hola', 'bonjour']))
+
+console.log(findMax(['serna', -25, 5.45, 'hello', 4.5, 98, 839, "840"]))
+console.log(findMax(['serna', -25, 5.45, 'hello', 4.5, 98, 839, [840]]))
+console.log(findMax(['serna', -25, 5.45, 'hello', 4.5, 98, 839, ["840"]]))
+
 module.exports = findMax;
