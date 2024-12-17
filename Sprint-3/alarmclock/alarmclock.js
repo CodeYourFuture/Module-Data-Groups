2ab39eb68804d86591d7be50d3eeb2fb3efde168
gai93003 <deng93003@gmail.com>
2024-12-17 03:55:37 +0000

    I made the recommended changes on the html file and created the functional code for the alarm clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,39 @@
-function setAlarm() {}
+let countDown;
+let remainingTime = 0;
+
+function setAlarm() {
+  remainingTime = parseInt(document.getElementById('alarmSet').value, 10);
+
+  if (isNaN(remainingTime) || remainingTime <= 0) {
+    alert('Please enter a valid number');
+    return;
+  }
+  else {
+    changeDisplay(remainingTime);
+    clearInterval(countDown);
+
+    countDown = setInterval(() => {
+      remainingTime--;
+
+      changeDisplay(remainingTime);
+
+      if(remainingTime <= 0) {
+        clearInterval(countDown);
+        playAlarm();
+      }
+    }, 1000);
+  }
+
+}
+
+const changeDisplay = (seconds) => {
+  const time = document.getElementById('time');
+
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+
+  time.textContent = `${String(minutes).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
