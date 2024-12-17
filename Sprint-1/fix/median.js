10901c5af5ff4dbf888e33c9c91a8c6db5a11e13
Serna Malala <sm.sernamalala@gmail.com>
2024-12-17 08:46:51 +0200

    Added a comparison function to sort the array in ascending order

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,18 +3,23 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
+  //to sort the array first slice the original array
+  //.sort in JS compares to elements against each other
+  //when subtraction is performed on them positive values remain first in line
+  //negative values go to the back
+  const sortedList = list.slice().sort((a, b) => a - b);
   const middleIndex = Math.floor(list.length / 2);
-  if (list.length % 2 == 0) {
+  if (sortedList.length % 2 == 0) {
     //to be able to handle an evenly lengthed list
     //need to extract the exact middle index and the one that comes right after it
     //DONT USE SPLICE IN THIS MANNER BECAUSE THE ARRAY IS ALTERED!!
     // const evenLengthMedian = l(list.splice(middleIndex, 1)[0] + list.splice(middleIndex+1, 1)[0]) / 2;
 
-    const evenLengthMedian = (list[middleIndex] + list[middleIndex - 1]) / 2;
+    const evenLengthMedian = (sortedList[middleIndex] + sortedList[middleIndex - 1]) / 2;
     return evenLengthMedian;
   }
   else {
-    const oddLengthMedian = list[middleIndex];
+    const oddLengthMedian = sortedList[middleIndex];
     return oddLengthMedian;
   }
 }
