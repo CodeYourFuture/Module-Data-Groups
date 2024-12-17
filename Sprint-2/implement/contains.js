e1513a233973f4cbc71e82aaf89b6d9bd3fb885e
Serna Malala <sm.sernamalala@gmail.com>
2024-12-17 17:55:40 +0200

    created a function that checks the property of a function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,18 @@
-function contains() {}
+function contains(objectGiven, element) {
+
+    //loop through the object property
+    for (const property in objectGiven) {
+        if (property === element) {
+            //if property is the same as the given element
+            return true;
+        }
+        return false;
+    }
+
+}
+
+console.log(contains({ a: 1, b: 2 }, 'a'));
+console.log(contains({ a: 1, b: 2 }, 'c'));
+console.log(contains(['a', 1, 'b', 2], 'c'));
 
 module.exports = contains;
