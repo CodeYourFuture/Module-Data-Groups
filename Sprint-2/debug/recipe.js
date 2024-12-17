0d081e20f140b7314d81a9b404228400c9c7b461
Serna Malala <sm.sernamalala@gmail.com>
2024-12-17 17:46:10 +0200

    I had to iterate through the array to print it out on each line

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 
+//the ingrediants in the array will not output, they are not being accesed properly
+
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -10,6 +12,13 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
+//need to access each array element.
+
+
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+  ingredients:`);
+
+//need to iterate through ingredients
+for (const singleIngredient of recipe.ingredients) {
+  console.log(`${singleIngredient}`);
+}
