d0fe351067566df732f69261c4a5616286cff6de
Rashaad Ebrahim <rmebrahim@gmail.com>
2024-12-17 21:43:34 +0200

    contains function implemented and tests created.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(object, property) {
+  for (const key of Object.keys(object)) {
+    if (key === property) return true;
+  }
+  return false;
+}
 
 module.exports = contains;
