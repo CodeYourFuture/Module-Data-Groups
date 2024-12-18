206053b641677e934991d0f488b77da1f9433bf7
gai93003 <deng93003@gmail.com>
2024-12-18 21:16:55 +0000

    I have worked on the tally function.

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -23,7 +23,11 @@ const tally = require("./tally.js");
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object", () => {
+  expect(tally(['a'])).toEqual({a: 1});
+  expect(tally(['a', 'a', 'a'])).toEqual({ a: 3 });
+  expect(tally(['a', 'a', 'b', 'c'])).toEqual({ a : 2, b: 1, c: 1 });
+});
 
 // Given an array with duplicate items
 // When passed to tally
