21ce380a3315ec110e85f39c6db17e45810a4462
Simpra11 <mrmabuya11@gmail.com>
2024-12-18 02:29:44 +0200

    Implemented a lookup function that works

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,12 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+   let lookup = {};
+
+   for(const[countryCode, currencyCode] of pairs){
+    lookup[countryCode] = currencyCode;
+   }
+   return lookup;
 }
 
 module.exports = createLookup;
+
+console.log(createLookup([['US', 'USD'], ['CA', 'CAD']]));
