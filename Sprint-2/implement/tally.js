b53382037ad4fa0cdd48829d31a7324f52da6f53
Rashaad Ebrahim <rmebrahim@gmail.com>
2024-12-18 15:27:45 +0200

    tally function implemented and tests created.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,11 @@
-function tally() {}
+function tally(listOfItems) {
+  console.log(typeof listOfItems)
+  if (typeof listOfItems !== 'object') throw new Error('Not a valid input')
+  const itemsCount = {};
+  for (const item of listOfItems) {
+    itemsCount[item] ? (itemsCount[item] += 1) : (itemsCount[item] = 1);
+  }
+  return itemsCount;
+}
 
 module.exports = tally;
--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -19,16 +19,34 @@ const tally = require("./tally.js");
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
+test("An array returns a count for each unique item", () => {
+  const currentOutput = tally(["a", "b", "c"]);
+  const targetOutput = { a: 1, b: 1, c: 1 };
+  expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("An empty array returns an empty object", () => {
+  const currentOutput = tally([]);
+  const targetOutput = {};
+  expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+test("An array with duplicate items returns counts for each unique item", () => {
+  const currentOutput = tally(["a", "a", "a", "b", "c", "c"]);
+  const targetOutput = { a: 3, b: 1, c: 2 };
+  expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("Given an invalid input should throw an error", () => {
+  const targetOutput = new Error("Not a valid input");
+  expect(() => tally("a, a, a, b, c, c")).toThrow(targetOutput);
+});
