94dc7954abd7574cd832b31c22ed66c0cd6a3b74
Serna Malala <sm.sernamalala@gmail.com>
2024-12-19 16:26:15 +0200

    Fixed the parseQueryString function to properly save the key value pairs to the objects

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,9 +1,17 @@
 function parseQueryString(queryString) {
+  //object to store the key value pairs
   const queryParams = {};
+
   if (queryString.length === 0) {
+    //return empty object for empty string
     return queryParams;
   }
-  const keyValuePairs = queryString.split("&");
+  //the info after the ? is needed
+  const neededInfo = queryString.split("?");
+  //the key value pairs are split on the needed info
+  //all the key value info will go to index 1 of the array
+  //further split it by the ampersand
+  const keyValuePairs = neededInfo[1].split("&");
 
   for (const pair of keyValuePairs) {
     const [key, value] = pair.split("=");
@@ -13,4 +21,7 @@ function parseQueryString(queryString) {
   return queryParams;
 }
 
+// console.log(parseQueryString("https://example.com/widgets?colour=blue&sort=newest"));
+// console.log(parseQueryString(""));
+// console.log(parseQueryString("https://example.com/widgets?colour=red"));
 module.exports = parseQueryString;
