7033499158369e93124bd0c51338839c98177f78
Simpra11 <mrmabuya11@gmail.com>
2024-12-20 00:02:43 +0200

    Wrote more test cases including an edge case and they all passed

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -1,7 +1,28 @@
 const invert = require("./invert");
 
+
 describe("invert function", () => {
-    test("Given an object with key-value pairs, it should swap the keys and values in the object", () => {
-        expect(invert({ x: 10, y: 20 })).toEqual({"10": "x", "20": "y"})
+    test("Given an object with multiple key-value pairs, it should swap the keys and values in the object", () => {
+        expect(invert({ x: 10, y: 20 })).toEqual({"10": "x", "20": "y"});
+    });
+
+    test("Given an object with a single key-value pair, it should swap the key and value in the object", () => {
+        expect(invert({x: 10})).toEqual({"10": "x"});
+    });
+
+    test("Given an empty object, it should return an empty object", () => {
+        expect(invert({})).toEqual({});
+    });
+
+    test("When we pass a string into invert, it should throw an error", () => {
+        expect(() => invert('I enjoy learning JavaScript')).toThrow(`Duplicate value found. Cannot invert.`);
+    });
+
+    test("When we pass duplicate values, it should throw an error", () => {
+        expect(() => invert({a: 1, b: 1})).toThrow(`Duplicate value found. Cannot invert.`);
+    });
+
+    test("Given an edge case with a different data type, it should swap the keys and values of the object ", () => {
+        expect(invert({ a: 1, b: '2', c: true })).toEqual({ '1': 'a', '2': 'b', true: 'c' });
     });
 });
