f1fa19a376af2ff2957ca5bcabf485876bedabab
Simpra11 <mrmabuya11@gmail.com>
2024-12-19 20:05:00 +0200

    Fixed the invert function, added an if statement so to handle errors and answered the questions as per requirement

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -10,20 +10,35 @@ function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    if (invertedObj[value] !== undefined) {
+      throw new Error(`Duplicate value found: ${value}. Cannot invert.`);
+    }
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
 
+console.log(invert({ a: 1, b: 2 }));
+
 // a) What is the current return value when invert is called with { a : 1 }
+
+// The current return value is {key: 1}
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
 
+//The current return value is {key: 2}
+
 // c) What is the target return value when invert is called with {a : 1, b: 2}
+
+// The target return value is {'1': 'a', '2': 'b'}
 
 // c) What does Object.entries return? Why is it needed in this program?
 
+// This method returns an array of [key, value] pairs from the object
+
 // d) Explain why the current return value is different from the target output
+
+//It's because we couldn't pass / swap key with invertObj using dot notation appending key instead of value 
 
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
