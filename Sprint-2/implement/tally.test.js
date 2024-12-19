b1471fbd52517fbc4f19c46b25f196dcf8f1d3fb
Simpra11 <mrmabuya11@gmail.com>
2024-12-19 18:56:44 +0200

    Implemented a number of test cases to my tally test file and they all passed

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -15,10 +15,22 @@ const tally = require("./tally.js");
  */
 
     describe("tally()", () => {
-        test("Given a tally function on an empty array, it should return an empty object", () => {
+        test("Given a tally function passed an array of items, it should return an empty object", () => {
             expect(tally([])).toEqual({});
         });
-    })
+
+        test("Given a tally function passed an array of items, it should return an object containing the count for each unique item", () => {
+            expect(tally(['a', 'b', 'a', 'c', 'b', 'd'])).toEqual({ a: 2, b: 2, c: 1, d: 1 });
+        });
+
+        test("Given an array with duplicate items , it should return counts for each unique item", () => {
+            expect(tally(['a', 'a', 'a', 'b', 'b'])).toEqual({ a: 3, b: 2 });
+        });
+
+        test("Given an invalid input like a string, it should throw an error", () => {
+            expect(() => tally('This is how JavaScript works')).toThrow("Invalid input");
+        });
+    });
 
 // Acceptance criteria:
 
