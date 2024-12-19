6065a8c1afe7a7b705ca1f170032f54eaa445e20
RahwaZeslusHaile <rahwazeslase@gmail.com>
2024-12-19 14:53:17 +0000

    alarm and qute

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,6 +1,39 @@
-function setAlarm() {}
 
-// DO NOT EDIT BELOW HERE
+
+function setAlarm() {
+  var inputTime = parseInt(document.getElementById("alarmSet").value);//to have the integer input
+  var countdown = inputTime;
+
+  if (isNaN(countdown) || countdown <= 0) {
+    alert("Please enter a valid time.");
+    return;
+  }
+
+  document.getElementById("timeRemaining").innerText = `Time Remaining: ${formatTime(countdown)}`;
+
+  function formatTime(seconds) {
+    var minutes = Math.floor(seconds / 60);
+    var remainingSeconds = seconds % 60;
+    return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+  }
+  
+
+  var interval = setInterval(() => {// I will have a function that runs at an interval
+    countdown--;
+    document.getElementById("timeRemaining").innerText = `Time Remaining: ${formatTime(countdown)}`;
+
+    if (countdown === 0) {
+      clearInterval(interval);
+      playAlarm();
+      // Optionally change background color
+      //document.body.style.backgroundColor = "red";
+      document.body.style.background = "linear-gradient(yellow, red)";
+
+
+    }
+  }, 1000);
+}
+
 
 var audio = new Audio("alarmsound.mp3");
 
@@ -21,5 +54,6 @@ function playAlarm() {
 function pauseAlarm() {
   audio.pause();
 }
+
 
 window.onload = setup;
