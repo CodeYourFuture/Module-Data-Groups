34681fd1f99837766a5c2896d7272acbbb9a812c
mgphone <pmn.blazer@gmail.com>
2024-12-19 01:10:14 +0000

    sprint 2 debug

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,9 +1,11 @@
 // Predict and explain first...
 
+// This will come undefined because that is object
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
-
+//solution
+//i add using "." object.houseNumber(key) and the value will come out
 const address = {
   houseNumber: 42,
   street: "Imaginary Road",
@@ -12,4 +14,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+//i think ingredients will display all of receipts and i believe i have to iterate receipt.ingredients
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -9,7 +10,6 @@ const recipe = {
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
-
+let receiptIngredients = recipe.ingredients.map((element) => element);
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+  ingredients: ${receiptIngredients}`);
