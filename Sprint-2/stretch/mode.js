0d91c4c4f6eea80a3ffc691f193e486169c68253
Serna Malala <sm.sernamalala@gmail.com>
2024-12-20 21:37:18 +0200

    I've refactored the mode function as to how I understood the instructions and created sub functions

--- Sprint-2/stretch/mode.js
+++ Sprint-2/stretch/mode.js
@@ -7,23 +7,30 @@
 
 // refactor calculateMode by splitting up the code
 // into smaller functions using the stages above
+function trackFrequency(listValue) {
+  //function to track the frequency
 
-function calculateMode(list) {
-  // track frequency of each value
   let freqs = new Map();
 
-  for (let num of list) {
+  //Object called freqs will be created which is a Map type object
+  for (let num of listValue) {
     if (typeof num !== "number") {
+      //if typeof is not a number continue to loop through the list
       continue;
     }
 
+    //set the number of the number being dealt with to 1 or an increment of 1
     freqs.set(num, (freqs.get(num) || 0) + 1);
   }
+  return freqs;
+}
 
-  // Find the value with the highest frequency
+function highestFrequency(list) {
+  // track frequency of each value
+  let currentFreqs = trackFrequency(list);
   let maxFreq = 0;
   let mode;
-  for (let [num, freq] of freqs) {
+  for (let [num, freq] of currentFreqs) {
     if (freq > maxFreq) {
       mode = num;
       maxFreq = freq;
@@ -33,4 +40,15 @@ function calculateMode(list) {
   return maxFreq === 0 ? NaN : mode;
 }
 
+function calculateMode(list) {
+  // Find the value with the highest frequency
+  return highestFrequency(list);
+
+
+}
+// const list = [1, 2, 2, 3, "a", 1, 3, 3];
+// console.log(calculateMode(list));
+
+// const nums = [2, 4, 1, 2, 3, 2, 1];
+// console.log(calculateMode(nums));
 module.exports = calculateMode;
