b233b3f9a9203ae4552289c91c964723fbc3bd99
Rashaad Ebrahim <rmebrahim@gmail.com>
2024-12-20 17:25:24 +0200

    When the user clicks the 'Set Alarm' button,the 'Time Remaining' title updates to show the entered number in mm:ss format

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,23 @@
-function setAlarm() {}
+const timeRemainingDisplay = document.querySelector("#timeRemaining");
+const alarmSetInput = document.querySelector("#alarmSet");
+
+// Buttons
+const stopAlarmBtn = document.querySelector("#stop");
+const setAlarmBtn = document.querySelector("#set");
+let inputValue, seconds, minutes, hours;
+
+function numberStartPadder(num, count = 2, padding = "0") {
+  return num.toString().padStart(count, padding);
+}
+
+console.log(numberStartPadder(7));
+
+function setAlarm() {
+  inputValue = Number(alarmSetInput.value);
+  seconds = numberStartPadder(inputValue % 60);
+  minutes = numberStartPadder(Math.floor(inputValue / 60));
+  timeRemainingDisplay.innerHTML = `Time Remaining: ${minutes}:${seconds}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -7,6 +26,7 @@ var audio = new Audio("alarmsound.mp3");
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
     setAlarm();
+    console.log("set");
   });
 
   document.getElementById("stop").addEventListener("click", () => {
