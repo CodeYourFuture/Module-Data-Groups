8183494beaaf4d6d6682270a990989534c83072c
Emmanuel <esiziba71@gmail.com>
2024-12-21 01:00:58 +0200

    Querystring function and test cases

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,14 +1,13 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
-    return queryParams;
-  }
-  const keyValuePairs = queryString.split("&");
 
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
-  }
+  if (!queryString) return queryParams;
+
+  queryString.split("&").forEach(pair => {
+      const [key, ...valueParts] = pair.split("=");
+      const value = valueParts.join("=");
+      queryParams[decodeURIComponent(key)] = decodeURIComponent(value);
+  });
 
   return queryParams;
 }
