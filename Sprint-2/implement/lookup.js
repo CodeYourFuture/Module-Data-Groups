689566b3b79cf2efc0ab8129aebc257bbf66d2ac
Emmanuel <esiziba71@gmail.com>
2024-12-20 15:52:43 +0200

    create lookup function

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,14 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  if (!Array.isArray(countryCurrencyPairs)) {
+    return {}; 
 }
+
+
+return countryCurrencyPairs.reduce((lookup, [country, currency]) => {
+    lookup[country] = currency; 
+    return lookup; 
+}, {});
+}
+
 
 module.exports = createLookup;
