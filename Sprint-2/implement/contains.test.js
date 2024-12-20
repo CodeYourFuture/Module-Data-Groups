6b8e7674bcadf86fddf60a47f26b9146bbcd4aba
Emmanuel <esiziba71@gmail.com>
2024-12-20 22:22:10 +0200

    contain test cases

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,24 +16,40 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
- 
+test.todo("contains a functioon"), () => {
+    const object = {g: 3, h: 6, j: 5}
+    expected(contains(object, 'z').ToEqual(false));
+    expected(contains(object, 'h').ToEqual(true));
+}
+
+   
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test.todo("contains on empty object returns false"), () => {
+    const object = {}
+    expected(contains(object, '').ToEqual(false));
+}
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-
+test.todo("contains an object with properties"), () => {
+    const object = {a: 1, b: 2}
+    expected(contains(object, 'b').ToEqual(true));
+}
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
-
+test.todo("contains an object with properties"), () => {
+    const object = {a: 1, b: 2}
+    expected(contains(object, 'z').ToEqual(false));
+}
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
 
-
-
-  
+test.todo("contains invalid parameters like an array returns false"), () => {
+    const object = [];
+    expect(contains(object, 'a')).toEqual(false);
+}
