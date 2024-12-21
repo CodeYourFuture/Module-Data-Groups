4b95ad5e475062e3be7e9bac37c5e7c0318f5171
Simpra11 <mrmabuya11@gmail.com>
2024-12-21 02:59:26 +0200

    Wrote more totalTill test cases and they all passed

--- Sprint-2/stretch/till.test.js
+++ Sprint-2/stretch/till.test.js
@@ -9,4 +9,16 @@ describe("totalTill()", () => {
             "20p": 10,
         })).toEqual("£4.40");
     });
-})
+
+    test("Given an invalid object of coins, it should throw an error 'Invalid coin format'", () => {
+        expect(() => totalTill({ "1p": 15, "10p": 5, "£1": 2 })).toThrow("Invalid coin format");
+    });
+
+    test("Given an empty object, it should return £0.00 ", () => {
+        expect(totalTill({})).toEqual("£0.00");
+    });
+
+    test("Given a single coin type object, it should return the total in pounds ", () => {
+        expect(totalTill({ "50p": 20 })).toEqual("£10.00");
+    });
+});
