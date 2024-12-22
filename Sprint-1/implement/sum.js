dd6b6c709ab150587505d31a8385bd667f36b1c8
Pooriya Ketabi <pooriya.ketabi@gmail.com>
2024-12-22 11:18:28 +0000

    Implement/sum Done

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,11 @@
 function sum(elements) {
+    let sum = 0;
+    for (let i = 0; i < elements.length; i += 1) {
+        if(typeof elements[i] == "number" && !isNaN(elements[i])) {
+            sum += elements[i];
+        }
+    }
+    return Math.round(sum * 1e10) / 1e10;
 }
 
 module.exports = sum;
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,52 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
 
+test("An empty array, should return 0", () => {
+    expect(sum([])).toBe(0);
+});
+
+// ---------------------------------------------------------------------------------------------
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
 
+test("An arrat with just one number, should return that number", () => {
+    expect(sum([20])).toBe(20);
+});
+
+// ---------------------------------------------------------------------------------------------
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
 
+test("An array with + and - numbers, should return correct total sum", () => {
+    expect(sum([20, -8, 7, -11, 3, 6])).toBe(17);
+});
+
+// ---------------------------------------------------------------------------------------------
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
 
+test("An array with decimal or float numbers, should return correct sum", () => {
+    expect(sum([20.3, 15.6, 71.8, 54.7])).toBe(162.4);
+});
+
+// ---------------------------------------------------------------------------------------------
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
 
+test("An array with numeric & non-numeric values, should ignore the non-numerics and return the sum of the numerical elements", () => {
+    expect(sum([14, -3, "pooriya", 16.3, "Hello World"])).toBe(27.3);
+});
+
+// ---------------------------------------------------------------------------------------------
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+
+test("An array with just non-numeric values, should return 0", () => {
+    expect(sum(["hello", "World", "pooriya", true])).toBe(0);
+});
