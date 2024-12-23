2f70e41eef3b5067d3ba0645989d9e6af19ce99b
Della-Bella <gisdellabella@uol.com.br>
2024-12-23 16:44:16 +0000

    Add Responsive code into CSS

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,41 @@ const books = [
   },
 ];
 
+// Function to display books
+function readingList(books) {
+  const bookList = document.getElementById("book-list");
+
+  // Create a div for each book
+
+  books.forEach((book) => {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
+
+    //background color
+    bookDiv.style.backgroundColor = book.alreadyRead ? "#d4edda" : "#f85c70";
+
+    //book title
+    const title = document.createElement("h3");
+    title.textContent = book.title;
+
+    //book author
+    const author = document.createElement("p");
+    author.textContent = `by ${book.author}`;
+
+    //book image
+    const img = document.createElement("img");
+    img.src = book.bookCoverImage;
+    img.alt = book.title;
+
+    // Append elements to the book div
+    bookDiv.appendChild(img);
+    bookDiv.appendChild(title);
+    bookDiv.appendChild(author);
+
+    // Append book div to the book list
+    bookList.appendChild(bookDiv);
+  });
+}
+
+// Call the function to display the books
+readingList(books);
