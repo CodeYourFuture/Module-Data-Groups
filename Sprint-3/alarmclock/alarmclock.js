97c96d89bd24367726baae691d31792e342c2767
mgphone <pmn.blazer@gmail.com>
2024-12-24 03:35:22 +0000

    time intrval,change min second, add background color

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,16 +1,43 @@
 function setAlarm() {
+  changeBackgroundColor("white");
   const maximumSecond = 3599;
   const inputEle = document.getElementById("alarmSet");
   const inputValue = inputEle.value;
   if (inputValue <= 0 || inputValue > maximumSecond) {
     alert("Please make sure your number between 1 to 3599");
     inputEle.value = "";
-  }
-  const displayMessage = document.querySelector("#timeRemaining");
-  displayMessage.innerText = `Something is coming ${inputValue}`;
-  input.addEventListener("keyup", checkingInput);
-}
+    return;
+  } else {
+    let remainingTime = inputValue;
+    const countDown = setInterval(function () {
+      const displayMessage = document.querySelector("#timeRemaining");
 
+      if (remainingTime > 0) {
+        displayMessage.innerText = `Time Remaining: ${formatTime(
+          remainingTime
+        )}`;
+        remainingTime--;
+      } else {
+        clearInterval(countDown);
+        displayMessage.innerText = "Time is up";
+        changeBackgroundColor("yellow");
+        playAlarm();
+      }
+    }, 1000);
+    input.addEventListener("keyup", checkingInput);
+  }
+}
+function changeBackgroundColor(color) {
+  document.body.style.backgroundColor = color;
+}
+function formatTime(inputSecond) {
+  const minutes = Math.floor(inputSecond / 60);
+  const second = inputSecond % 60;
+  return `${String(minutes).padStart(2, "0")}:${String(second).padStart(
+    2,
+    "0"
+  )}`;
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
