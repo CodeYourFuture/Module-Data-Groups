eb7e65d05ab42596cac55226d1bb405d821cc55c
Pooriya Ketabi <pooriya.ketabi@gmail.com>
2024-12-24 21:56:46 +0000

    Implement/tally.js Done

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,27 @@
-function tally() {}
+// function tally(listOfItems) {
+//     const itemCount = {}
+
+//     for (const item of listOfItems) {
+//         if (itemCount[item]) {
+//             itemCount[item] += 1;
+//         } else {
+//             itemCount[item] = 1;
+//         }
+//     }
+//     return itemCount;
+// }
+
+function tally(listOfItems) {
+    if (!Array.isArray(listOfItems)) {
+        throw new Error("Input must be an array!");
+    }
+
+    const itemCount = {}
+    
+    for (const item of listOfItems) {
+        itemCount[item] = (itemCount[item] || 0) + 1;
+    }
+    return itemCount;
+}
 
 module.exports = tally;
