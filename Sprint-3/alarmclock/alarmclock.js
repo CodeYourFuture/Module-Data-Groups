6a09367a4cc9b455588ae0f33a53fc4dce77d77b
mgphone <pmn.blazer@gmail.com>
2024-12-24 02:33:05 +0000

    check zero to maxium hour

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,15 @@
-function setAlarm() {}
+function setAlarm() {
+  const maximumSecond = 3599;
+  const inputEle = document.getElementById("alarmSet");
+  const inputValue = inputEle.value;
+  if (inputValue <= 0 || inputValue > maximumSecond) {
+    alert("Please make sure your number between 1 to 3599");
+    inputEle.value = "";
+  }
+  const displayMessage = document.querySelector("#timeRemaining");
+  displayMessage.innerText = `Something is coming ${inputValue}`;
+  input.addEventListener("keyup", checkingInput);
+}
 
 // DO NOT EDIT BELOW HERE
 
