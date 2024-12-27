658575bd4fb4b8caf1c0567199d9e2eabc7db863
Alireza1620 <alirezaseifishalamzari@gmail.com>
2024-12-27 14:07:46 +0000

    fxing the code in fix folder. the code was working only for lists with odd amount of numbers.

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,9 +3,21 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  // Sort the list in ascending order
+  const sortedList = list.slice().sort((a, b) => a - b);
+  const middleIndex = Math.floor(sortedList.length / 2);
+  
+  // Check if the length of the list is even or odd
+  if (sortedList.length % 2 === 0) {
+    // If even, return the average of the two middle numbers
+    return (sortedList[middleIndex - 1] + sortedList[middleIndex]) / 2;
+  } else {
+    // If odd, return the middle number
+    return sortedList[middleIndex];
+  }
 }
 
 module.exports = calculateMedian;
+
+
+// the code wasn't working because if the list has different amount of numbers, or to be specific, if the list has an even amount of numbers, the code will return the average of the two middle numbers, which is wrong. 
