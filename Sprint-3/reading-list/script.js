e76ee04eb371f7feac9615cb6feab048979ec71f
mgphone <pmn.blazer@gmail.com>
2024-12-27 18:47:01 +0000

    readinglist

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -20,4 +20,30 @@ const books = [
     bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
+function setup() {
+  const readingList = document.getElementById("reading-list");
+  books.map((book) => {
+    const listBook = document.createElement("li");
+    listBook.style.backgroundColor = book.alreadyRead ? "Red" : "Green";
+    listBook.style.height = "20%";
+    listBook.style.width = "40vw";
+    listBook.style.marginBottom = "10px";
+    //p tag inside li
+    const bookTitle = document.createElement("p");
+    bookTitle.textContent = book.title + " ";
+    //span tag inside p tag
+    const authorSpan = document.createElement("span");
+    authorSpan.textContent = book.author;
+    bookTitle.appendChild(authorSpan);
+    const img = document.createElement("img");
+    img.src = book.bookCoverImage;
+    img.alt = book.title;
+    img.style.height = "81%";
+    img.style.width = "auto";
+    listBook.appendChild(bookTitle);
+    listBook.appendChild(img);
 
+    readingList.appendChild(listBook);
+  });
+}
+window.onload = setup;
