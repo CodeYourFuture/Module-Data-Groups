b5402d5dd819ed0f8a238b060f7119469abf397f
Emmanuelgessessew <163059051+Emmanuelgessessew@users.noreply.github.com>
2024-12-29 23:40:02 +0000

    Fix issues in object property access, iteration, and formatting

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,8 @@
 // Predict and explain first...
+// The code attempts to log the ingredients array within the recipe object, but it uses ${recipe} in the string template. 
+// This will log the string representation of the entire recipe object, which isn't the desired behavior.
+//Instead, you want to explicitly log each ingredient on a new line. The code also doesn't include logic to format the ingredients array properly.
+
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -11,5 +15,5 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:
+${recipe.ingredients.join("\n")}`);
