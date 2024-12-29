cd95568112ad3b84b3e327d364ed01d9e8d4cb36
Serna Malala <sm.sernamalala@gmail.com>
2024-12-29 15:39:48 +0200

    I updated the alarm clock code and tried to adjust things to count

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,42 @@
-function setAlarm() {}
+function setAlarm() {
+  //access the input field
+  const inputValue = document.querySelector("#alarmSet").value;
+  //timeRemaning title
+  const timeRemaining = document.querySelector("#timeRemaining");
+  let min = parseInt(inputValue / 60);//minutes if input is above 60
+  let sec = inputValue % 60;//seconds left over after minutes
+
+  setInterval(() => {
+    if (min >= 1 && min < 10 && sec < 10) {
+      timeRemaining.textContent = `Time Remaining: ${min}:${sec}`;
+      if (sec > 0) {
+        sec--
+      }
+      else if (sec === 0 && min >= 1) {
+        min--;
+        sec = 60;
+      }
+
+    }
+    else if (inputValue > 60 && sec >= 10) {
+      timeRemaining.textContent = `Time Remaining: ${min}:${sec}`;
+      if (sec > 0) {
+        sec--
+      }
+      else if (sec === 0 && min >= 1) {
+        min--;
+        sec = 60;
+      }
+    }
+    else {
+      timeRemaining.innerHTML = `Time Remaining: 00:${sec}`;
+      if (sec > 0) {
+        sec--
+      }
+    }
+  }, 1000);
+}
+
 
 // DO NOT EDIT BELOW HERE
 
