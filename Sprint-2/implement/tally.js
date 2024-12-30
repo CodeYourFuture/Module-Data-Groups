cbd9c7cd062b735bb2efc783644bfd344fe2d6d2
Alireza1620 <alirezaseifishalamzari@gmail.com>
2024-12-30 22:07:19 +0000

    completing the function

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,14 @@
-function tally() {}
+function tally(items) {
+  // Validate input
+  if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
+
+  // Use reduce to build the frequency map
+  return items.reduce((acc, item) => {
+    acc[item] = (acc[item] || 0) + 1; // Increment the count for the item
+    return acc;
+  }, {});
+}
 
 module.exports = tally;
