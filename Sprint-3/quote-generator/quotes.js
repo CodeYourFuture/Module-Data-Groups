a8cccb49c8f499cdb7420afb1716fa86aea660fb
Emmanuel <esiziba71@gmail.com>
2024-12-30 14:48:37 +0200

    javascript functions(functionality) and css and html typos

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -1,5 +1,48 @@
-const qout
+const quoteText = document.querySelector(".quote"),
+quoteBtn = document.querySelector("button");
 
+authorName = document.querySelector(".name"),
+speechBtn = document.querySelector(".speech"),
+copyBtn = document.querySelector(".copy"),
+twitterBtn = document.querySelector(".twitter"),
+synth = speechSynthesis;
+
+function randomQuote() {
+  quoteBtn.classList.add("loading");
+  quoteBtn.innerText = "Loading Quote...";
+
+   
+  const randomQuoteObj = pickFromArray(quotes);
+  quoteText.innerText = randomQuoteObj.quote;
+  authorName.innerText = randomQuoteObj.author;
+
+  quoteBtn.classList.remove("loading");
+  quoteBtn.innerText = "New Quote";
+}
+
+
+speechBtn.addEventListener("click", ()=>{
+    if(!quoteBtn.classList.contains("loading")){
+        let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+        synth.speak(utterance);
+        setInterval(()=>{
+            !synth.speaking ? speechBtn.classList.remove("active") : speechBtn.classList.add("active");
+        }, 10);
+    }
+});
+
+copyBtn.addEventListener("click", ()=>{
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+
+twitterBtn.addEventListener("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
+
+quoteBtn.addEventListener("click", randomQuote);
+ 
+// Call randomQuote to display the first quote on page load
 
 // DO NOT EDIT BELOW HERE
 
@@ -494,3 +537,4 @@ const quotes = [
 ];
 
 // call pickFromArray with the quotes array to check you get a random quote
+
