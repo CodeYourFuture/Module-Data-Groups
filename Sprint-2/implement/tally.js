f863a3e526df1cb22c67e544d2977a2d7bebb9aa
Serna Malala <sm.sernamalala@gmail.com>
2024-12-31 21:22:14 +0200

    I've made changes to the function to be simpler by reducing the use of the if else statement

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,30 +1,23 @@
 function tally(inputArray) {
     //initialise an empty object
     let talliedResults = {};
-    //check if input is an array
-    if (Array.isArray(inputArray)) {
-        //for an empty array
-        if (inputArray.length === 0) {
-            return talliedResults;
-        }
-
-        for (const property of inputArray) {
-            //if this propert is already in the object increment it
-            if (talliedResults[property]) {
-                talliedResults[property] += 1;
-            }
-            else {
-                //if not initialize it as one
-                talliedResults[property] = 1;
-            }
-        }
-
-        return talliedResults;
-    }
-    else {
+    //check if input is not an array
+    if (!Array.isArray(inputArray)) {
         throw new Error("The input you provided is not an array");
+        //for an empty array
+        // if (inputArray.length === 0) {
+        //     return talliedResults;
+        // }
     }
+    for (const property of inputArray) {
+        //if this propert is already in the object increment it
+
+        talliedResults[property] = (talliedResults[property] || 0) + 1;
+    }
+
+    return talliedResults;
 }
+
 
 // console.log(tally([1, 2, 3, 1, 4, 1, 5, 2, 3, 4]));
 module.exports = tally;
