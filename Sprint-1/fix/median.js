6cbedaaff58e880e4e7c2fe423201f242dfcc59f
Emmanuel <esiziba71@gmail.com>
2025-01-01 20:44:43 +0200

    adding the sort method

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,15 +3,17 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
+  const sortedList = list.slice().sort((a, b) => a -b);
+
+  const middleIndex = Math.floor(sortedList / 2);
 
   if (list.length % 2 ===0){
-    const middleValue1 = list[middleIndex -1]
-    const middleValue2 = list[middleIndex];
+    const middleValue1 = sortedList[middleIndex -1]
+    const middleValue2 = sortedList[middleIndex];
     return (middleValue1 + middleValue2) / 2;
 
   } else {
-    return list[middleIndex];
+    return sortedList[middleIndex];
   }
 }
 
