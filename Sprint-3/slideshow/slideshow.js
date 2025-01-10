d5a6d3ba4516025243736dd1ecce787fdcd63f07
Serna Malala <sm.sernamalala@gmail.com>
2025-01-10 19:40:42 +0200

    implemented level 1 of the slideshow exercise

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,3 +6,45 @@ const images = [
 
 
 // Write your code here
+//LEVEL 1 implementation
+const body = document.querySelector("body");
+const image = document.getElementById("carousel-img");
+const forwardButton = document.getElementById("forward-btn");
+const backwardButton = document.getElementById("backward-btn");
+let index = 0;
+image.src = images[index];
+function carousel(imagesArray) {
+
+
+    body.style.textAlign = "center";
+    image.style.width = "50%";
+
+    forwardButton.addEventListener("click", function () {
+        if (index < imagesArray.length - 1) {
+            index++;
+            image.src = imagesArray[index];
+
+        }
+        else {
+            index = 0;
+            image.src = imagesArray[index];
+
+        }
+
+    })
+
+    backwardButton.addEventListener("click", function () {
+        if (index > 0) {
+            index--;
+            image.src = imagesArray[index];
+
+        }
+        else {
+            index = imagesArray.length - 1;
+            image.src = imagesArray[index];
+        }
+
+    })
+}
+
+carousel(images);
