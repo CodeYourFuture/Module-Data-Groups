34a000a4b9c5f5c2637ec842ed1d152a598f6376
RihannaP <114884466+RihannaP@users.noreply.github.com>
2025-01-13 21:43:34 +0000

    fix reviews

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,26 +1,35 @@
 function setAlarm() {
-  let timeRemaining = document.getElementById("alarmSet").value;
+  clearInterval();
+  //let timeRemaining = 0;
+
+  let inputTime = parseInt(document.getElementById("alarmSet").value);
+    if (isNaN(inputTime) || inputTime <= 0) {
+    alert("Please enter a valid time");
+      return;
+    }
+
+    timeRemaining = inputTime;
 
   function formatTime(seconds) {  //format time (MM : SS)
     const minutes = Math.floor(seconds / 60); //minutes
     const remainingSeconds = seconds % 60; // remaining seconds
     return `Time Remaining: ${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
   }
-      document.getElementById("timeRemaining").textContent = formatTime(timeRemaining);
+  document.getElementById("timeRemaining").textContent = formatTime(timeRemaining);
 
   ///counter for the clock
-  const countdown = setInterval(function() {
+  let countdown = setInterval(function() {
     if (timeRemaining <= 0) {
       //when the countdown is equal zero, play the alarm and stop the timer
       playAlarm();
-      clearInterval(countdown); // 〰️ Stop the countdown timer
+      clearInterval(countdown); // Stop the countdown timer
     } else {
       //decrease the remaining time by one second
       timeRemaining--;
 
       //Update the heading to reflect the new remaining time in MM:SS format
       document.getElementById("timeRemaining").textContent = formatTime(timeRemaining);
-}
+    }
   }, 1000);    // 1000 = 1 second > check every one second
 
 }
