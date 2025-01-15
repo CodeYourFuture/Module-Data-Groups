a5f1f84fe5f2126805c7ed3f2cd067b58942841a
mgphone <pmn.blazer@gmail.com>
2025-01-15 12:44:08 +0000

    change as review

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,42 +1,58 @@
+let countDown;
 function setAlarm() {
   changeBackgroundColor("white");
   const maximumSecond = 3599;
   const inputEle = document.getElementById("alarmSet");
-  const inputValue = inputEle.value;
-  if (inputValue <= 0 || inputValue > maximumSecond) {
+  const inputValue = parseInt(inputEle.value, 10);
+  const displayMessage = document.querySelector("#timeRemaining");
+
+  if (isNaN(inputValue) || inputValue <= 0 || inputValue > maximumSecond) {
     alert("Please make sure your number between 1 to 3599");
     inputEle.value = "";
     return;
-  } else {
-    let remainingTime = inputValue;
-    const countDown = setInterval(function () {
-      const displayMessage = document.querySelector("#timeRemaining");
-
-      if (remainingTime > 0) {
-        displayMessage.innerText = `Time Remaining: ${formatTime(
-          remainingTime
-        )}`;
-        remainingTime--;
-      } else {
-        clearInterval(countDown);
-        displayMessage.innerText = "Time is up";
-        changeBackgroundColor("yellow");
-        playAlarm();
-      }
-    }, 1000);
-    input.addEventListener("keyup", checkingInput);
   }
+  if (countDown) {
+    clearInterval(countDown);
+  }
+  let remainingTime = inputValue;
+  countDown = setInterval(function () {
+    if (remainingTime > 0) {
+      displayMessage.innerText = `Time Remaining: ${formatTime(remainingTime)}`;
+      remainingTime--;
+    } else {
+      clearInterval(countDown);
+      displayMessage.innerText = "Time is up";
+      inputEle.value = "";
+      changeBackgroundColor("yellow");
+      playAlarm();
+    }
+  }, 1000);
 }
+//add another stop alarm button event listener and I know this is not good approach for two eventlistener for one stop button but i can not edit for restriction
+document.getElementById("stop").addEventListener("click", () => {
+  stopAlarm();
+});
+function stopAlarm() {
+  const inputEle = document.getElementById("alarmSet");
+  const displayMessage = document.querySelector("#timeRemaining");
+
+  if (countDown) clearInterval(countDown);
+  displayMessage.innerText = "Time Remaining: 00:00 ";
+  inputEle.value = "";
+  changeBackgroundColor("white");
+  pauseAlarm();
+}
+
 function changeBackgroundColor(color) {
   document.body.style.backgroundColor = color;
 }
 function formatTime(inputSecond) {
   const minutes = Math.floor(inputSecond / 60);
   const second = inputSecond % 60;
-  return `${String(minutes).padStart(2, "0")}:${String(second).padStart(
-    2,
-    "0"
-  )}`;
+  return `${changePadTwo(minutes)}:${changePadTwo(second)}`;
+}
+function changePadTwo(inputString) {
+  return String(inputString).padStart(2, "0");
 }
 // DO NOT EDIT BELOW HERE
 
