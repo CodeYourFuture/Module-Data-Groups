8eb67a815bdd5245cc9fa5c33ea98d899901915d
Alireza1620 <alirezaseifi345@gmail.com>
2025-01-15 22:13:50 +0000

    Fixing the part deleted by mistake

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -48,6 +48,20 @@ function setAlarm() {
 }
 
 
+// DO NOT EDIT BELOW HERE
+
+var audio = new Audio("alarmsound.mp3");
+
+function setup() {
+  document.getElementById("set").addEventListener("click", () => {
+    setAlarm();
+  });
+
+  document.getElementById("stop").addEventListener("click", () => {
+    pauseAlarm();
+  });
+}
+
 function playAlarm() {
   audio.play();
 }
