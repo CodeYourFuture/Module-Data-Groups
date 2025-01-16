11574f860030be7c643483458371308c7e69a9e0
Della-Bella <gisdellabella@uol.com.br>
2025-01-16 00:18:26 +0000

    change path images and varavle to const and let

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -10,7 +10,7 @@
 
 // Write your code here
 
-let number_img = 1;
+/*let number_img = 1;
 const total_images = 5;
 
 function img_change_add() {
@@ -22,7 +22,7 @@ function img_change_add() {
    } else {
      number_img = 1; 
    }
-  img.src = "/Sprint-3/slideshow/assets/cute-cat-" + number_img + ".jpg";
+  img.src = "/assets/cute-cat-" + number_img + ".jpg";
 }
 
 function img_change_sub() {
@@ -33,5 +33,45 @@ function img_change_sub() {
  } else {
    number_img = total_images;
  }
- img.src = "/Sprint-3/slideshow/assets/cute-cat-" + number_img + ".jpg";
+ img.src = "/assets/cute-cat-" + number_img + ".jpg";
+}*/
+
+let number_img = 1;
+const total_images = 5;
+
+// Function to update the image source
+function updateImage() {
+  const img = document.getElementById("img");
+  img.src = `/Sprint-3/slideshow/assets/cute-cat-${number_img}.jpg`;
 }
+
+// Function to go forward
+function img_change_add() {
+  if (number_img < total_images) {
+    number_img += 1;
+  } else {
+    number_img = 1;
+  }
+  updateImage();
+}
+
+// Function to go backward
+function img_change_sub() {
+  if (number_img > 1) {
+    number_img -= 1;
+  } else {
+    number_img = total_images;
+  }
+  updateImage();
+}
+
+// Get buttons
+const forwardBtn = document.getElementById("forward-btn");
+const backwardBtn = document.getElementById("backward-btn");
+
+// Add event listeners
+forwardBtn.addEventListener("click", img_change_add);
+backwardBtn.addEventListener("click", img_change_sub);
+
+// Set the initial image
+updateImage();
