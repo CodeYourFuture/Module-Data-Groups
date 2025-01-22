d06c968b2c18819b2cbd8ccf5fce664b0d33c85d
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-01-22 07:12:28 +0000

    Fix findMax function: handle non-numeric values and invalid types

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -2,17 +2,19 @@
 //I have to use that filters number only the array to number
 //check every index is whether greater than the next index
 function findMax(elements) {
-    let smallestNumber = -Infinity;
-    for(let index=0; index < elements.length; index++){
-        const currentNumber = elements[index];
-        if (!isNaN(currentNumber) && currentNumber > smallestNumber){
-          smallestNumber = currentNumber ;
+    if (elements.length === 0) {
+        return null;  
     }
-  
-}
- return smallestNumber;
+
+    let maxNumber = -Infinity;
+    for (const currentNumber of elements) {
+        if (typeof currentNumber === 'number' && !isNaN(currentNumber) && currentNumber > maxNumber) {
+            maxNumber = currentNumber;
+        }
+    }
+
+    return maxNumber === -Infinity ? null : maxNumber;
 }
 
-console.log(findMax([2,4,7,12,6,0,28]));
 
 module.exports = findMax;
