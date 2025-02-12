f02b60aa4a8bba58805fb3c8f418a8a131a1d9cd
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-12 22:07:01 +0200

    implemented a function to sum elements in an array

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,10 @@
 function sum(elements) {
+  let total = 0;
+  for (let i = 0; i < elements.length; i++) {
+    total += elements[i];
+  }
+  return total;
 }
+console.log(sum([10, 10, 10]));
 
 module.exports = sum;
