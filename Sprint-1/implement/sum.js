9c1b6eb62e66d58259b37d9af0d52b3abf71e6c5
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-15 17:28:06 +0200

    updated the function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,10 +1,16 @@
 function sum(elements) {
   let total = 0;
   for (let i = 0; i < elements.length; i++) {
-    total += elements[i];
+    if (typeof elements[i] === "number") {
+      total += elements[i];
+      console.log(elements[i]);
+    }
   }
   return total;
 }
-console.log(sum([10, 10, 10]));
+/*console.log(sum([10, 10, 10]));
+console.log(sum([-2, -5, -9, -12]));
+console.log(sum([5.32, 6.59, 8.52, 7.2]));*/
+console.log(sum([65, 67, "month", 7]));
 
 module.exports = sum;
