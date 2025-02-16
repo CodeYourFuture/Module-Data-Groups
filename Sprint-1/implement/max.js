973b41a293198bb6326a116d141be9d265b8b1ad
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-16 22:06:02 +0200

    Implemented a function to calculate max

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,12 @@
 function findMax(elements) {
+  let maxNumber = -Infinity;
+  for (let i of elements) {
+    if (typeof i === "number" && !isNaN(i) && i > maxNumber) {
+      maxNumber = i;
+    }
+  }
+  return maxNumber;
 }
-
+console.log(findMax([30, 50, 10, "string", 40]));
+console.log(findMax(["number", 8, "string", 9]));
 module.exports = findMax;
