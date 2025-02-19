074025a63839a58901e45ab6d819f43c2c068c95
Aida Zhukova <aidazhukovaa@gmail.com>
2025-02-19 16:20:59 +0000

    median-done

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -3,9 +3,18 @@
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  const copyList = [...list];
+  if (copyList.length % 2 !== 0) {
+    const middleIndex = Math.floor(copyList.length / 2);
+    const median = copyList.splice(middleIndex, 1)[0];
+    return median;
+  } else {
+    const middleEvenIndex = copyList.length / 2 - 1;
+    const middleEvenIndex2 = copyList.length / 2;
+    const sum = list[middleEvenIndex] + list[middleEvenIndex2];
+    const evenNum = sum / 2;
+    return evenNum;
+  }
 }
 
 module.exports = calculateMedian;
