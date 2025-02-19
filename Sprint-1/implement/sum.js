243a495b2e395fa7096fc4ce8d767621274ea422
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-19 08:24:03 +0200

    added tests

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -8,9 +8,9 @@ function sum(elements) {
   }
   return total;
 }
-/*console.log(sum([10, 10, 10]));
+console.log(sum([10, 10, 10]));
 console.log(sum([-2, -5, -9, -12]));
-console.log(sum([5.32, 6.59, 8.52, 7.2]));*/
+console.log(sum([5.32, 6.59, 8.52, 7.2]));
 console.log(sum([65, 67, "month", 7]));
 
 module.exports = sum;
