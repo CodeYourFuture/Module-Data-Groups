3c3da715ec97af2b9d1c51ffb61384cf570ea13f
Ihoryan <ihorbolzhelarskyi78@gmail.com>
2025-02-24 23:06:08 +0000

    done refactor

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,9 +1,18 @@
 // Refactor the implementation of includes to use a for...of loop
 
+// function includes(list, target) {
+//   for (let index = 0; index < list.length; index++) {
+//     const element = list[index];
+//     if (element === target) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
+  for (let key of list) {
+    if (key === target) {
       return true;
     }
   }
