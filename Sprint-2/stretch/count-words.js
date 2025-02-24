122745d0d9092b18a756a59df8f353577e648a77
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-24 16:07:25 +0200

    Implemented a function that returns an object where the key is the words from the string and the values is the number of times the word appears

--- Sprint-2/stretch/count-words.js
+++ Sprint-2/stretch/count-words.js
@@ -20,9 +20,24 @@
 
 ## Advanced challenges
 
+
 1. Remove all of the punctuation (e.g. ".", ",", "!", "?") to tidy up the results
 
 2. Ignore the case of the words to find more unique words. e.g. (A === a, Hello === hello)
 
 3. Order the results to find out which word is the most common in the input
 */
+function countFrequency(str) {
+  str = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+  let result = [];
+  let words = str.split(" ");
+  //console.log(words);
+  for (let word of words) {
+    result[word] = result[word] ? result[word] + 1 : 1;
+  }
+  let sortedArray = Object.entries(result).sort((a, b) => b[1] - a[1]);
+  return Object.fromEntries(sortedArray);
+}
+
+console.log(countFrequency("aAaabbggshdkajfbshs"));
+console.log(countFrequency("My name is Evelyn Nomsa, I live in Cape Town"));
