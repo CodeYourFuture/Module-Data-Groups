db7563dcae689404db41eb07659643ae8a1fb23f
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-24 20:45:09 +0200

    Updated function to use for each method

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -3,10 +3,13 @@ const tally = (arr) => {
     throw new Error("Invalid input");
   }
   let result = {};
-  for (let i of arr) {
+  arr.forEach((element) => {
+    result[element] = result[element] ? result[element] + 1 : 1;
+  });
+  /*for (let i of arr) {
     result[i] = result[i] ? result[i] + 1 : 1;
-  }
+  }*/
   return result;
 };
-
+console.log(tally(["a", "a", "b", "c"]));
 module.exports = tally;
