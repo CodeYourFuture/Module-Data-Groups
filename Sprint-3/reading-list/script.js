14f223020c2c11a1656ace5c9f4fed4541a09a03
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-02-25 19:38:00 +0200

    Added background color to the li elements

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,10 +21,19 @@ const books = [
     bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
+
 const ulElement = document.getElementById("reading-list");
+
+const colors = ["#f4a261", "#2a9d8f", "#e76f51", "#264653", "#e9c46a"];
+
+let index = 0;
+
 for (let [key, value] of Object.entries(books)) {
   let liElement = document.createElement("li");
   liElement.innerHTML = `<p>${value.title} by ${value.author}</p>
                           <img src="${value.bookCoverImage}" alt="${value.title} cover" style="width:400px">`;
+  liElement.style.backgroundColor = colors[index % colors.length];
+  index++;
+
   ulElement.appendChild(liElement);
 }
