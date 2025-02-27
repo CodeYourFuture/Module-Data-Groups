f561a1fa2d585162d7b693325213d1ad61ec7971
eyuell21 <abrahameyuel4@gmail.com>
2025-02-27 18:56:40 +0000

    Fixed according to review

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,8 +1,8 @@
 function dedupe(arr) {
     let myArr = []
     myArr.push(arr[0])
-    for (let i = 0; i < arr.length; i++) {
-        if ((myArr.includes(arr[i]) == false)  && (arr.length > 0)) {
+    for (let i = 1; i < arr.length; i++) {
+        if ((myArr.includes(arr[i]) == false)) {
             myArr.push(arr[i])
         }
         if (arr.length == 0) return []
--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,24 +1,12 @@
 function sum(elements) {
-    let count = 0
-    if(elements.length == 0) return 0
-    else{
+    let total = 0
         for (let i = 0; i < elements.length; i++) {
             if (isNaN(elements[i]) == true) {
                 continue
             }
-            count+=elements[i]
+            total+=elements[i]
         }
-    }
-    return count
+    return total
 }
-
-let myArr = [0.1,0.2,0.3]
-let myArr1 = [1]
-let myArr2= ['x', 'abc', 'a', 'hello']
-let myArr3= ['x', 1, 3, 'hello']
-console.log(sum(myArr));
-console.log(sum(myArr1));
-console.log(sum(myArr2));
-console.log(sum(myArr3));
 
 module.exports = sum;
