8dec63235aa971ea17661f5bfbd027554404e79c
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-02-28 14:22:17 +0000

    better technique applied for tally function as suggested by the mentor

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -2,7 +2,9 @@ function tally(arr) {
   let newObj = {};
   for (let n of arr) {
     if (Object.keys(newObj).includes(n)) { // if n is found in my obj as a key increment its value by else create an item inside my obj with key of n and value 1.
-      newObj[n] += 1;  }
+      //newObj[n] += 1; this code works fine but for better technique it is replaced by the code in the next line
+      newObj[n] = (newObj[n]|| 0)+1; 
+    }
     else {
       newObj[n] = 1; }
   }
@@ -10,3 +12,8 @@ function tally(arr) {
 }
 console.log(tally(["a", "a", "a", "b"])); // { a: 3, b: 1 }
 module.exports = tally;
+
+//Note
+//newObj[n]=(newObj[n] || 0)+1;
+//Using || to get a value where the left of it is undefined is a generally useful technique
+
