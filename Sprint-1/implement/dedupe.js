8917f572553c25200b640963c66b09afa4c2eba1
loveth900 <ojay_love@yahoo.com>
2025-02-28 14:58:11 +0000

    an array was decleard

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,14 @@
-function dedupe() {}
+
+
+function dedupe(arr) {
+    const uniqueArray = [];
+    const seen = new setImmediate();
+
+    for (const item of arr) {
+      if (!seen.has(item)) {
+        uniqueArray.push(item);
+        seen.add(item);
+      }   
+    } 
+    return uniqueArray;
+}
