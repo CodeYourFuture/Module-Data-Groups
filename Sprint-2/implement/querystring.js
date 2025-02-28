1e041fb77837b7a26125d5cdb021d131ae9cb3cb
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-02-28 13:47:00 +0000

    implement query string

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -3,14 +3,19 @@ function parseQueryString(queryString) {
   if (queryString.length === 0) {
     return queryParams;
   }
-  const keyValuePairs = queryString.split("&");
-
+  const queryPart = queryString.split("?");
+  const keyValuePairs = queryPart[1].split("&");
   for (const pair of keyValuePairs) {
     const [key, value] = pair.split("=");
     queryParams[key] = value;
-  }
+    }
 
   return queryParams;
 }
 
 module.exports = parseQueryString;
+
+//MDN: note for my self alternate method inside the for loop
+//const [key, ...valueParts] = pair.split("=");
+// // const value = valueParts.join("="); // Join the remaining parts back together in case there are additional equals signs
+ 
--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -6,7 +6,5 @@
 const parseQueryString = require("./querystring.js")
 
 test("parses querystring values containing =", () => {
-  expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
-  });
+  expect(parseQueryString("https://example.com/widgets?colour=blue&sort=newest")).toEqual({ "colour":"blue" ,"sort":"newest"});
 });
