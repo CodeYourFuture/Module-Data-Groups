8ced2a12ff20d0355fef0a12f1f28b2167b6b521
Droid-An <andrei.filippov.aaf@gmail.com>
2025-02-28 12:25:08 +0000

    implemented function, that passes only 6 tests

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,12 @@
 function findMax(elements) {
+    console.log(elements.filter((num) => typeof num === 'number'))
+    elements.filter((num) => typeof num === 'number') == '[]'
+    if (elements.filter((num) => typeof num === 'number') == '[]') {
+        return "Enter valid numbers"
+    } else {
+        let filteredByNumber = elements.filter((num) => typeof num === 'number')
+        return Math.max(...filteredByNumber)
+    }
 }
 
 module.exports = findMax;
