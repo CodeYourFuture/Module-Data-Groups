72ddb6cd6b67d1d7adf9b0224ad0ee011852dbb4
loveth900 <ojay_love@yahoo.com>
2025-02-28 16:29:15 +0000

    the max was found

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,22 @@
 function findMax(elements) {
+    if (elements.length === 0) {
+     return undefined; // Return undefined if the array is empty
+
+
+}
+
+let max = elements[0]; // Start by assuming the first element is max
+
+
+for (let i = 1; i < elements.length; i++) {
+  if (elements[i] > max) {
+    max = elements[i]; // Update max if a larger number is found
+  }
+}
+
+return max; // Return the largest value found
+
 }
 
 module.exports = findMax;
+
