705722c33c26cb8917ab9d160172a5d36fd9bed4
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-02-28 12:08:25 +0000

    count words stretch exercise

--- Sprint-2/stretch/count-words.js
+++ Sprint-2/stretch/count-words.js
@@ -26,3 +26,17 @@
 
 3. Order the results to find out which word is the most common in the input
 */
+function countWords(inputTxt) {
+  let newObj = {};
+  let clnText =inputTxt.replace(/[.,!?]/g, "").toLowerCase();
+  let arrayStr = clnText.split(" ");
+  for (let word of arrayStr) {
+    if (newObj[word]) {
+      newObj[word]++;
+    } else {
+      newObj[word] = 1;
+    } }
+  return newObj;
+}
+
+console.log(countWords("you and me and you.")); // { you: 2, and: 2, me: 1 }
