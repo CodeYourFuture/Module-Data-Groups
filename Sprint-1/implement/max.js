45e8297c5ced5434bdef2c5ec0ecccf1073228ca
Amir200524 <amiraminia@yahoo.com>
2025-03-01 10:27:30 +0000

    max function and test case

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,10 @@
 function findMax(elements) {
+  const numericElements = elements.filter((el) => typeof el === "number");
+
+  if (numericElements.length === 0) return -Infinity;
+
+  return Math.max(...numericElements);
 }
 
 module.exports = findMax;
+
