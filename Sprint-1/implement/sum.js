0711a4a7f33cbd525f9ec45f0485ac58e51c842e
AmirMontazeri <mr.amir.mti@gmail.com>
2025-03-01 13:00:47 +0000

    sprint1-tasks-implement-sum.js-done

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,14 @@
 function sum(elements) {
-}
+    let count = 0;
+    elements.forEach((element) => { 
+      if (!isNaN(element) && element !== "") {
+        count += Number(element);
+      }
+    });
+    return count;
+  }
+  
+
+console.log(sum(["1", "2", 5, 5, 5, "3", "a", "hey"]));
 
 module.exports = sum;
