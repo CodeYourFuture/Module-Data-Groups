ab34e5130c8327343b6937b5587c0d42acee34dc
sarawone <sarawone.poudal@gmail.com>
2025-03-01 11:55:46 +0000

    fixing media function

--- Sprint-1/fix/median.test.js
+++ Sprint-1/fix/median.test.js
@@ -9,7 +9,7 @@ const calculateMedian = require("./median.js");
 describe("calculateMedian", () => {
   test("returns the median for odd length array", () => {
     expect(calculateMedian([1, 2, 3])).toEqual(2);
-    expect(calculateMedian([1, 2, 3, 4, 5])).toEqual(3);
+    expect(calculateMedian([1, 2, 31, 4, 5])).toEqual(3);
   });
 
   test("returns the average of middle values for even length array", () => {
