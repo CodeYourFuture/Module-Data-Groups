00be37084ae773f9c98fddb3d841a59924b83f89
sabafarjamfard <farjamfardsaba@yahoo.com>
2025-03-02 12:12:06 +0000

    Completing Sprint-1 (implement | max.js and max.test.js)

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,16 @@
+// findMax.js
 function findMax(elements) {
+  if (elements.length === 0) {
+    return -Infinity;
+  }
+
+  const validNumbers = elements.filter((item) => typeof item === "number");
+
+  if (validNumbers.length === 0) {
+    return -Infinity;
+  }
+
+  return Math.max(...validNumbers);
 }
 
 module.exports = findMax;
