7422bfba17d2392580fd3ed3d98ec0b9779c5170
Amir200524 <amiraminia@yahoo.com>
2025-03-02 09:30:18 +0000

    lookup function and test case

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,18 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+  if (!Array.isArray(pairs)) {
+    throw new Error("Input must be an array of arrays.");
+  }
+
+  const lookup = {};
+  for (const [country, currency] of pairs) {
+    if (typeof country !== "string" || typeof currency !== "string") {
+      throw new Error("Each pair must contain two strings.");
+    }
+    lookup[country] = currency;
+  }
+  return lookup;
 }
 
 module.exports = createLookup;
+
+
--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,6 +1,28 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+
+test("creates a country currency code lookup for multiple codes", () => {
+  const input = [['US', 'USD'], ['CA', 'CAD'], ['GB', 'GBP']];
+  const expectedOutput = { US: 'USD', CA: 'CAD', GB: 'GBP' };
+
+  expect(createLookup(input)).toEqual(expectedOutput);
+});
+
+test("returns an empty object for an empty array", () => {
+  expect(createLookup([])).toEqual({});
+});
+
+test("throws an error if input is not an array", () => {
+  expect(() => createLookup(null)).toThrow("Input must be an array of arrays.");
+  expect(() => createLookup("string")).toThrow("Input must be an array of arrays.");
+  expect(() => createLookup(42)).toThrow("Input must be an array of arrays.");
+});
+
+test("throws an error if elements are not valid pairs", () => {
+  expect(() => createLookup([['US']])).toThrow("Each pair must contain two strings.");
+  expect(() => createLookup([['US', 123]])).toThrow("Each pair must contain two strings.");
+});
+
 
 /*
 
