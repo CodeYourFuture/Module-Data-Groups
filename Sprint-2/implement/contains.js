cca712615769646f9cd0ec0b03c05942ed0d0821
loveth900 <ojay_love@yahoo.com>
2025-03-03 05:25:41 +0000

    the array fixed

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,7 @@
-function contains() {}
+function contains(arr, value) {
+    return arr.includes(value);
+}
 
 module.exports = contains;
+
+// we are checking if 'value' exist in an 'arr' array,so that the function can be exported in other files.
