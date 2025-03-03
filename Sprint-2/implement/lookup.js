545fdebc3ab13b362dd9b44233832cbb4417036c
loveth900 <ojay_love@yahoo.com>
2025-03-03 06:09:27 +0000

    the lookup found

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,20 @@
-function createLookup() {
-  // implementation here
+function createLookup(arr, key) {
+  const lookup = {}; 
+  // initialize an empty object to store the lookup table
+
+  // loop through each object in the input array.
+  Array.forEach(item => {
+  // assign the value of the specified key from the object as the lookup key.
+  // and store the  entire object as the corresponding value
+  lookup[item[key]] = item;
+
+  })
+
+  return lookup; 
+  // needed to return the completed lookup table.
+  
 }
 
 module.exports = createLookup;
+
+
