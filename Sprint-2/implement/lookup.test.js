0d23ffc437102c88260ab0827dc201d343d50e3a
loveth900 <ojay_love@yahoo.com>
2025-03-03 09:54:43 +0000

    the object were found

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,6 +1,3 @@
-const createLookup = require("./lookup.js");
-
-test.todo("creates a country currency code lookup for multiple codes");
 
 /*
 
@@ -33,3 +30,25 @@ It should return:
    'CA': 'CAD'
  }
 */
+  
+
+function createLookup(countryCurrencyPairs) {
+  const createLookup = {};
+  // create an empty object to hold the key-value pairs
+
+  // loop through each country-currency pair in the array
+  countryCurrencyPairs.forEach(pair => {
+    const [countryCode, currencyCode] = pair;
+  // Destructure each pair into countryCode and currencyCode
+  lookup[countryCode] = currencyCode;
+  // store the country code as the key and currency code as the value
+
+  });
+
+  return lookup; 
+
+  // return the constructed lookup object
+
+}
+
+module.exports = createLookup;
