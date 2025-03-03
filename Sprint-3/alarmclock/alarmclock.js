66090261b110a8b88beb9164034e7892c9319374
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-03-03 14:17:01 +0200

    Added comments to the code.

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -8,32 +8,35 @@ let totalSeconds = 0;
 let alarmRunning = false;
 
 function setAlarm() {
+  //set the seconds to the input value
   let seconds = alarmSetInput.value;
-  function updateDisplay() {
-    if (totalSeconds < 0) {
-      clearInterval(timer);
-      remainingTimeElement.innerHTML = "Time's up!";
-      playAlarm();
-      alarmRunning = false;
-      return;
-    }
+  //This checks to see if there is an alarm already running, if not, set the total seconds to input value.
+  if (!alarmRunning) {
+    totalSeconds = seconds;
+    //this prevents setting another alarm whilst there is an alarm running
+    alarmRunning = true;
+    //calling the updateDisplay function which shows the initial time and set the time to update every second.
+    updateDisplay();
+    timer = setInterval(updateDisplay, 1000);
 
-    let displayMinutes = Math.floor(totalSeconds / 60);
-    let displaySeconds = totalSeconds % 60;
-
-    displayMinutes = String(displayMinutes).padStart(2, "0");
-    displaySeconds = String(displaySeconds).padStart(2, "0");
-    remainingTimeElement.innerHTML = `Time Remaining: ${displayMinutes}:${displaySeconds}`;
-    totalSeconds--;
-  }
-
-  if (alarmRunning) {
-    clearInterval(timer);
-  } else {
-    if ((alarmRunning = true)) {
-      totalSeconds = seconds;
-      updateDisplay();
-      timer = setInterval(updateDisplay, 1000);
+    function updateDisplay() {
+      //check if total seconds is below 0, if yes it stops the countdown and displays time's up and plays sound.
+      if (totalSeconds < 0) {
+        clearInterval(timer);
+        remainingTimeElement.innerHTML = "Time's up!";
+        playAlarm();
+        //this resets alarm running and timer and exit the function
+        alarmRunning = false;
+        timer = null;
+        return;
+      }
+      //declaring variables, converting total seconds to display minutes and making sure that the minutes and seconds are displayed as 2 digits by using the pad function.
+      let displayMinutes = Math.floor(totalSeconds / 60);
+      let displaySeconds = totalSeconds % 60;
+      displayMinutes = String(displayMinutes).padStart(2, "0");
+      displaySeconds = String(displaySeconds).padStart(2, "0");
+      remainingTimeElement.innerHTML = `Time Remaining: ${displayMinutes}:${displaySeconds}`;
+      totalSeconds--;
     }
   }
 }
