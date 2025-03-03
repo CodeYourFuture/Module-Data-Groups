e35f5efd6efd473ff6f551b70ac59888f408e8e4
loveth900 <ojay_love@yahoo.com>
2025-03-03 04:53:59 +0000

    the data found

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,15 +1,24 @@
 // Predict and explain first...
 
+
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
 
+
 const address = {
+
   houseNumber: 42,
   street: "Imaginary Road",
   city: "Manchester",
   country: "England",
-  postcode: "XYZ 123",
+  postcode: "XYZ 123", 
+
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address[2]}`);
+console.log(`My house number is ${address.houseNumber}`);
+// The address object does not use numbers to access its data for an array. instead we must make use of stored information in key-value pairs.(houseNumber)
+
+
+
