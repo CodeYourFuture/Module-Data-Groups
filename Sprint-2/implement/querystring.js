d058b6b23c0756d5f1828866e7b8aa52c1efcd3f
AmirMontazeri <mr.amir.mti@gmail.com>
2025-03-04 13:23:55 +0000

    sprint2-tasks-implement-querystring.js-done

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,24 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
-    return queryParams;
-  }
+
+  if (!queryString) return queryParams;
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    const parts = pair.split("=");
+    const key = decodeURIComponent(parts[0]);
+    const value =
+      parts.length > 1 ? decodeURIComponent(parts.slice(1).join("=")) : "";
+
+    if (queryParams[key]) {
+      if (!Array.isArray(queryParams[key])) {
+        queryParams[key] = [queryParams[key]];
+      }
+      queryParams[key].push(value);
+    } else {
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
