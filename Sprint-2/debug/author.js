fd9ee9c0e7893c4124704fdd7d5d60a63d5394dc
Bahasyd <baha.syd@gmail.com>
2025-03-04 06:20:59 +0000

    object

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
-
+/*This is (author) an object that contains different data consisting of numbers, strings, 
+arrays, nested objects, or functions representing structured data with different properties 
+(like houseNumber, name of street). It is a set of date represented in key-value format.*/
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +13,7 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+ for (const value of Object.values(author)) {
   console.log(value);
-}
+ }
+
