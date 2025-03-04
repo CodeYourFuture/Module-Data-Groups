fb3b52e18e8ea0a0c9307eebfd73d741ce09dddd
Bahasyd <baha.syd@gmail.com>
2025-03-04 06:01:27 +0000

    new comment

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-/*This is an array that contains different data consisting of numbers and strings. */
+/*This is (address) an object that contains different data consisting of numbers 
+and strings representing structured data with different properties (like houseNumber, name of street).  */
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
