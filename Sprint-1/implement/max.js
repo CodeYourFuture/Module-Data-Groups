6487cb9da61719bdbbf3d39fc0e72bcd12231de7
sheetalkharab <sheetalrana89@gmail.com>
2025-03-04 10:27:46 +0000

    implement logic in findMax function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,14 @@
 function findMax(elements) {
+    //to filter non-numeric elements
+    const numericElement = elements.filter(element => typeof element === 'number'
+         && !isNaN(element));
+
+    // if  no element , return infinity
+    if(numericElement.length===0){
+        return -Infinity;
+    }  
+    // return max value from the filtered numeric elements
+    return  Math.max(...numericElement) ; 
 }
 
 module.exports = findMax;
