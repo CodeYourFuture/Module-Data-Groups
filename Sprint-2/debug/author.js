631dbc8a5b7981aedcce821b5fb9eb50c60eff17
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-03-04 22:12:58 +0000

    authorFixed

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,14 @@
 // Predict and explain first...
+/*
+some kind of error will occur because of the (for .. of) loop, it works in iterables such as array and strings not objects 
+since author is an object the code will not work as intended 
+when running the code type error occur
+for (const value of author) {
+                    ^
 
+TypeError: author is not iterable
+    at Object.<anonymous> 
+*/
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -10,7 +19,12 @@ const author = {
   age: 40,
   alive: true,
 };
-
+/*
 for (const value of author) {
   console.log(value);
+}
+*/
+// using (for .. in) loop to iterate over the keys of the object 
+for (const value in author){
+  console.log(author[value]);
 }
