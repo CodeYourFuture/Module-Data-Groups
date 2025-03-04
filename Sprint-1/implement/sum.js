5f5e4f367bdfb0ca2e2b5c847ba4fafd07469b04
sheetalkharab <sheetalrana89@gmail.com>
2025-03-04 10:28:46 +0000

    implement logic in sum function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,16 @@
 function sum(elements) {
+    if(!Array.isArray(elements)){// check input is not array, return 0
+        return 0;
+    }
+
+    let total = 0;
+
+    for (let i =0; i< elements.length; i++){
+        if(typeof elements[i]=== "number"){
+            total +=elements[i];
+        }
+    }
+    return total;
 }
 
 module.exports = sum;
