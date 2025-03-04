255cd45f1486581d6f169890cc6aab52dac1b2ba
Bahasyd <baha.syd@gmail.com>
2025-03-04 22:10:46 +0000

    empty

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,13 @@
-function contains() {}
+function contains(obj, key) {
+        if (typeof obj !== 'object' || obj === null) {
+    return false; 
+}
+return Object.prototype.hasOwnProperty.call(obj, key);
+    }
+    
+    console.log(contains({a: 1, b: 2}, 'a')); 
+    console.log(contains({a: 1, b: 2}, 'c')); 
+    console.log(contains({}, 'a')); 
+
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,20 +16,34 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
-
+test("a contains function should return true if the object contains the property, false otherwise", () => {
+  expect(contains({a: 1},'a')).toEqual(true);
+  expect(contains({a: 1}, 'b')).toEqual(false);
+});
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+    expect(contains({}, 'a')).toEqual(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-
+test("given an object with properties, contains should return true for an existing property", () => {
+    expect(contains({ a: 1 }, 'a')).toEqual(true);  
+});
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
-
+test("contains should return false for a non-existing property", () => {
+    expect(contains({ a: 1 }, 'b')).toEqual(false);
+});
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("contains should return false for invalid parameters", () => {
+    expect(contains([], 'a')).toEqual(false);     
+    expect(contains(null, 'a')).toEqual(false);   
+    expect(contains(undefined, 'a')).toEqual(false); 
+});
