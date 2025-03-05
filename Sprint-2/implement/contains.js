c066eaecc5a822c8ff7aa481f90f3c7f71f1af75
sheetalkharab <sheetalrana89@gmail.com>
2025-03-05 23:27:42 +0000

    change code of contains function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(obj, prop) {
+    if(typeof obj !== "object"|| obj === null || Array.isArray(obj)){
+        throw new Error("Invalid input: Expected an object");
+    }
+    return obj.hasOwnProperty(prop);
+}
 
 module.exports = contains;
