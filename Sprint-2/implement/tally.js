f502974c236c803fa57775c0d16740841977e867
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-03-05 22:29:19 +0000

    tallyFixed

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,13 @@
-function tally() {}
+function tally(arr) {
+    if(!Array.isArray(arr)){
+        throw new Error ("Input must be an array");
+    }
+    const result = {};
+ // Loop through the array to count occurrences of each item
+    for(let item of arr){
+        result[item] = (result[item] || 0) +1;
+    }
+    return result;
+}
 
 module.exports = tally;
