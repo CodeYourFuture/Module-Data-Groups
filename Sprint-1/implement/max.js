396c514007f3e43866b2bd9f0526be1175ae78e6
Droid-An <andrei.filippov.aaf@gmail.com>
2025-03-05 13:11:08 +0000

    removed unnecessary repetition from max.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,8 +1,6 @@
 function findMax(elements) {
   let filteredByNumber = elements.filter((num) => typeof num === "number");
-  if (filteredByNumber.length == 0 && elements.length == 0) {
-    return Math.max(...filteredByNumber);
-  } else if (filteredByNumber.length == 0 && elements.length > 0) {
+  if (filteredByNumber.length == 0 && elements.length > 0) {
     return "Enter valid numbers";
   } else {
     return Math.max(...filteredByNumber);
