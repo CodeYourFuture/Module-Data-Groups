9c70177f96ac0e2bb97a9119f5042966a3df733e
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-03-05 10:29:26 +0200

    Changed to arrow functions

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -2,7 +2,7 @@
 // Start by running the tests for this function
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
-function calculateMedian(list) {
+const calculateMedian = (list) => {
   const middleIndex = Math.floor(list.length / 2);
   const middleNumbers = list[middleIndex - 1] + list[middleIndex];
   const average = middleNumbers / 2;
@@ -10,6 +10,6 @@ function calculateMedian(list) {
   if (list.length % 2 === 0) {
     return average;
   } else return list[middleIndex];
-}
+};
 console.log(calculateMedian([1, 2, 3, 5, 8, 6]));
 module.exports = calculateMedian;
--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,6 +1,6 @@
-function dedupe(arr) {
+const dedupe = (arr) => {
   newArr = [...new Set(arr)];
   return newArr;
-}
+};
 console.log(dedupe(["Shoes", "Shirt", "Shoes", "Hat", "Shirt"]));
 module.exports = dedupe;
