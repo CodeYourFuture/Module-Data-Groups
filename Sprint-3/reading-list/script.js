3425f2bcf98960b43367a5682690d367b81f2136
eyuell21 <abrahameyuel4@gmail.com>
2025-03-05 01:50:23 +0000

    (JS) Created a function (readingList)

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -21,3 +21,37 @@ const books = [
   },
 ];
 
+////////////++++++Creating a function to display our array on the html page++++++++//////////////////
+
+
+function readingList(listId, arr) {
+
+  const myList = document.getElementById(listId);
+
+  arr.forEach(item => {  // Using fo each to iterate over our array of books
+
+    const li = document.createElement("li");  // creating an li element
+
+    const title = document.createElement('h3') //creating a h3 element for the title and appending it to li
+    title.textContent = item.title
+    li.appendChild(title)
+
+    const author = document.createElement('p') //creating a p element for the author and appending it to li
+    author.textContent = item.author
+    li.appendChild(author)
+
+    const bookCoverImage = document.createElement('img') //creating an img element for the book cover and appending it to li
+    bookCoverImage.src = item.bookCoverImage
+    li.appendChild(bookCoverImage)
+    
+    const alreadyReadStatus = item.alreadyRead;
+
+    alreadyReadStatus ?  li.style.backgroundColor = 'green' : li.style.backgroundColor = 'red' // adding a set dependent bg-color
+
+    myList.appendChild(li); // adding the li element to the parent ul element
+  })
+
+}
+
+readingList("reading-list", books);  // calling readingList 
+
