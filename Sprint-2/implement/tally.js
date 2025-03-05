777b86262d0001eb5b0fa1047e20e817348296b6
sheetalkharab <sheetalrana89@gmail.com>
2025-03-05 23:31:29 +0000

    write telly function logic

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,13 @@
-function tally() {}
+function tally(input) {
+    if(!Array.isArray(input)){
+        throw new Error("Invalid input: Expected an array");
+    }
+    const output ={};
+    for(const item of input){
+        output[item]= (output[item] || 0)+1;//count occurrence
+    }
+    
+   return output;
+}
 
 module.exports = tally;
