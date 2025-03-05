275ed66af21c34ead62b21eaf932eb3197276aac
sheetalkharab <sheetalrana89@gmail.com>
2025-03-05 23:31:05 +0000

    write test cases for parseQueryString function

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -10,3 +10,28 @@ test("parses querystring values containing =", () => {
     "equation": "x=y+1",
   });
 });
+
+test("parses simple querystring", () => {
+  expect(parseQueryString("name=Arnav&age=13")).toEqual({
+    name: "Arnav",
+    age: "13",
+  });
+});
+
+test("parses empty querystring  ", () => {
+  expect(parseQueryString(" ")).toEqual({});
+});
+
+test("parses querystring with value missing", () => {
+  expect(parseQueryString("key1=&key2=value")).toEqual({
+    key1:"",
+    key2: "value",
+  });
+});
+
+test("parses querystring contains multiple '=' ", () => {
+  expect(parseQueryString("key1=abc=123&key2=x=y=z")).toEqual({
+    key1: "abc=123",
+    key2: "x=y=z",
+  });
+});
