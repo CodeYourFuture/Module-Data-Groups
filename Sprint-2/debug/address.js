ea320fe7a61fe45768300690d48ab8c48a017a84
sabafarjamfard <farjamfardsaba@yahoo.com>
2025-03-05 09:03:09 +0000

    Fix-Sprint-2 (debug - address.js )

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,9 @@
 // Predict and explain first...
+//The code uses template literals (backticks with ${}) to insert the value of address.houseNumber into the string.
+//The address object contains several properties, including houseNumber which has the value 42.
+//The code accesses this property directly using address.houseNumber and dynamically places its value in the string.
+//As a result, the console log will output the string:
+//My house number is 42
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +17,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
