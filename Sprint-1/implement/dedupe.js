14048feb5e0cc5926f08927af248e83d0c404fa4
Elchascode <elchaschool@gmail.com>
2025-03-05 12:41:45 +0000

    implemented a function  and created an empty arr and then implemented a new Set object to eradicate duplicate from the arr. before returning the the original value.

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,9 @@
-function dedupe() {}
+function dedupe(arr) {
+ 
+  let uniqueArr = []; // create an empty array to store the unique values
+  uniqueArr = [...new Set(arr)]; // use the set object to remove duplicates and [...] means spread the values back into the empty array
+
+  return uniqueArr; // return the unique values
+}
+
+module.exports = dedupe;
