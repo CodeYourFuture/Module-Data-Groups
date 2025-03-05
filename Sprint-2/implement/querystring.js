00178f5b395df859aa3e9fe6b8b7062caa4bfff4
sheetalkharab <sheetalrana89@gmail.com>
2025-03-05 23:30:35 +0000

    changes in parseQueryString function

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,13 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
+  if (queryString.trim().length === 0) {//trim spaces and check for an empty space
     return queryParams;
   }
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    const [key, ...value] = pair.split("=");// keep elements after first '=
+    queryParams[key] = value.join("=");//rejoin multiple '=' in value
   }
 
   return queryParams;
