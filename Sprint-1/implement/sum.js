c26373c6ff95c14027673795cbd5c9577674adc2
Droid-An <andrei.filippov.aaf@gmail.com>
2025-03-05 13:37:14 +0000

    refactored sum.js file

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,14 +1,9 @@
 function sum(elements) {
   let filteredByNumber = elements.filter((num) => typeof num === "number");
-  sum = 0;
-  if (filteredByNumber.length == 0 && elements.length == 0) {
-    filteredByNumber.forEach((item) => {
-      sum += item;
-    });
-    return sum;
-  } else if (filteredByNumber.length == 0 && elements.length > 0) {
-    return "Enter valid numbers";
+  if (filteredByNumber.length == 0) {
+    return elements.length == 0 ? 0 : "Enter valid numbers";
   } else {
+    sum = 0
     filteredByNumber.forEach((item) => {
       sum += item;
     });
