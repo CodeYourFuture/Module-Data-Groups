afb33830b2f358f28ce5fe0419901c7319d1878c
aiah ibrahim <ayababikeribrahim@gmail.com>
2025-03-05 22:29:19 +0000

    tallyFixed

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -23,12 +23,29 @@ const tally = require("./tally.js");
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object", () => {
+    expect(tally([])).toEqual({});
+});
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+test("tally counts unique item correctly", () => {
+    expect(tally(['a','a','a'])).toEqual({a:3});
+    expect(tally(['a','b','a','c'])).toEqual({ a: 2, b: 1, c: 1 });
+});
 
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("tally throws an error when input is not an array", () => {
+    expect(() => tally("string")).toThrow("Input must be an array");
+    expect(() => tally(123)).toThrow("Input must be an array");
+    expect(() => tally({})).toThrow("Input must be an array");
+});
+
+
+
+test("tally with mixed data type", () => {
+    expect(tally(['a','a',1,2,1,1])).toEqual({a: 2, 1: 3, 2:1});
+});
