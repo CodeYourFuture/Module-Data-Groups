4897419b39343b66d92317558d7e4c19014d1070
sheetalkharab <sheetalrana89@gmail.com>
2025-03-05 23:24:23 +0000

    change in address object

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-
+// with the print statement it will not print house number as we are asking 0 th element of address not just houseNumber. 
+// so we will get undefined
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +13,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
