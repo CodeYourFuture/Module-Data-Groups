294a0a509c7ae08ec5de712963a3c1b625bd4396
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-03-05 10:53:40 +0200

    Changed the for loop to reduce method

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,12 +1,8 @@
 const sum = (elements) => {
-  let total = 0;
-  for (let i = 0; i < elements.length; i++) {
-    if (typeof elements[i] === "number") {
-      total += elements[i];
-      console.log(elements[i]);
-    }
-  }
-  return total;
+  return elements.reduce(
+    (acc, num) => (typeof num === "number" ? acc + num : acc),
+    0
+  );
 };
 console.log(sum([10, 10, 10]));
 console.log(sum([-2, -5, -9, -12]));
