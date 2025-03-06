fa8f33aa8fb1e1de84debc2a676a1be5947b4a62
Priscilla <cpjm0033@gmail.com>
2025-03-06 23:36:49 +0000

    adjusted the function

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// Objects (author) are not iterable unless the iterable protocol is used. Also for...of cannot be used to iterate over the properties of an object.
+// we have to use Object.keys or Object.entries, to iterate over the properties or entries of an object.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +13,7 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
+module.exports = author;
