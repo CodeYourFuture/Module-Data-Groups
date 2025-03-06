71066158b0b0bd91c53555024eaf3d93eb77195c
Ihoryan <ihorbolzhelarskyi78@gmail.com>
2025-03-06 13:25:32 +0000

    implemented setAlarm function

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,27 @@
-function setAlarm() {}
+const timeRemaining = document.querySelector(`#timeRemaining`);
+const alarmSet = document.querySelector(`#alarmSet`);
 
+function setAlarm() {
+  let inputValueToNumber = parseInt(alarmSet.value);
+  if (isNaN(inputValueToNumber) || inputValueToNumber <= 0) {
+    return alert(`Please enter correct number!`);
+  }
+  function displayChanges() {
+    let minutes = String(Math.floor(inputValueToNumber / 60)).padStart(2, `0`);
+    let seconds = String(inputValueToNumber % 60).padStart(2, `0`);
+    timeRemaining.textContent = `Time Remaining: ${minutes}:${seconds}`;
+  }
+  displayChanges();
+
+  let interval = setInterval(() => {
+    inputValueToNumber--;
+    if (inputValueToNumber <= 0) {
+      clearInterval(interval);
+      playAlarm();
+    }
+    displayChanges();
+  }, 1000);
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
