35ee493b414776e4d8e3bf71a33b29bbb5344922
Bahasyd <baha.syd@gmail.com>
2025-03-06 19:57:49 +0000

    parse query

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,11 +1,35 @@
-function parseQueryString(queryString) {
-  if (!queryString) return {};
+function parseQueryString(query) {
+  if (!query) return {};
 
-  return Object.fromEntries(
-    new URLSearchParams(queryString).entries()
-  );
+  if (query.startsWith("?")) {
+    query = query.slice(1);
+  }
+
+  return query.split("&").reduce((acc, pair) => {
+    let [key, ...values] = pair.split("=");
+    key = decodeURIComponent(key);
+    let value = values.length > 0 ? values.join("=") : null;
+  
+    if (value === "") {
+      value = null;
+    } else if (value !== null) {
+      value = decodeURIComponent(value);
+    }
+
+    if (acc[key] !== undefined) {
+      acc[key] = [].concat(acc[key], value);
+    } else {
+      acc[key] = value;
+    }
+
+    return acc;
+  }, {});
 }
+
 const queryString = "equation=x=y+1";
 console.log(parseQueryString(queryString));
 
+
 module.exports = parseQueryString;
+
+
