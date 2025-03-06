ec1fe1a584562b9944d22ecd68daed2f02e92862
Millena28 <millenakiflom06@gmail.com>
2025-03-06 11:37:51 +0000

    Fixed the implement part

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,12 +16,32 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+//test.todo("given an empty array, it returns an empty array");
+
+test("given an empty array should return an empty array", () => {
+    expect(dedupe([]).toEqual([]));
+
+});
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
 
+test("An array with no duplicates should return a copy of the original" , () => {
+   
+    expect(dedupe(["H","E","Y"]).toEqual(["H","E","Y"]));
+    expect(dedupe(["r","a","e"]).toEqual(["r","a","e"]));
+
+});
+
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
+
+test("An array should remove the duplicate values, preserving the first occurence " , () => {
+    expect(dedupe(["cat","dog","mouse","cat","bird"]).toEqual(["cat","dog","mouse","bird"]));
+    expect(dedupe(["hi","hey",4,"hello","hey",5]).toEqual(["hi","hey",4,"hello",5]));
+    expect(dedupe([5,1,2,4,3,4,5]).toEqual([5,1,2,4,3]));
+
+
+});
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,44 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test("given an empty array, returns 0" , () => {
+    expect (sum([])).toEqual(0);
+});
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
+test("given an array with just one number returns that number" , () => {
+    expect (sum([100])).toEqual(100);
+});
+
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
+test("given an  array with negative numbers returns the total sum number" , () => {
+    expect (sum([-1,-2,-3,-4,-5])).toEqual(-15);
+});
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
+test("given an array with decimal numbers returns the total sum number" , () => {
+    expect (sum([0.5,2.5,3.1,6.6])).toEqual(12.7);
+});
+
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+test("given an array containing non-numerical values returns the total sum number" , () => {
+    expect (sum(["hello",10,"yes",80])).toEqual(90);
+});
+
 
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test("given an array with non-numerical values returns the least surprising value" , () => {
+    expect (sum(["hello","code","your","future"])).toEqual(0);
+});
