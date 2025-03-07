3a8b17d2bda3afc671812acc4ec66883f1e794a2
sheetalkharab <sheetalrana89@gmail.com>
2025-03-07 13:46:09 +0000

    improvement in invert function

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -6,24 +6,45 @@
 
 // E.g. invert({x : 10, y : 20}), target output: {"10": "x", "20": "y"}
 
+// function invert(obj) {
+//   const invertedObj = {};
+
+//   for (const [key, value] of Object.entries(obj)) {
+//     invertedObj.key = value;
+//   }
+
+//   return invertedObj;
+// }
+
+// a) What is the current return value when invert is called with { a : 1 }
+// it will return { key: 1 }
+
+// b) What is the current return value when invert is called with { a: 1, b: 2 }
+// return {key: 2}
+
+// c) What is the target return value when invert is called with {a : 1, b: 2}
+// target value is  { "1": "a", "2": "b" }
+
+// c) What does Object.entries return? Why is it needed in this program?
+// it will return [ [ 'a', 1 ], [ 'b', 2 ] ]. It will allows us to iterate an object property using a for loop.
+
+// d) Explain why the current return value is different from the target output
+// because of the incorrect property assignment inside the loop
+// e) Fix the implementation of invert (and write tests to prove it's fixed!)
+
 function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key; // Correctly swap key and value
   }
 
   return invertedObj;
 }
+// test cases to varify the fix:
+console.log(invert({ a : 1 }));
+console.log(invert({ a : 1 , b : 2}));
+console.log(invert({ a : 10 , b : 20}));
+console.log(invert({ key1: "value1", key2: "value2" }));
+console.log(invert({}));
 
-// a) What is the current return value when invert is called with { a : 1 }
-
-// b) What is the current return value when invert is called with { a: 1, b: 2 }
-
-// c) What is the target return value when invert is called with {a : 1, b: 2}
-
-// c) What does Object.entries return? Why is it needed in this program?
-
-// d) Explain why the current return value is different from the target output
-
-// e) Fix the implementation of invert (and write tests to prove it's fixed!)
