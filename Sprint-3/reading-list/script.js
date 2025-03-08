a8be365cb98de0cf491f13c216885877b4e7c8df
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-03-08 12:40:17 +0000

    commits for the reading-list app

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -20,4 +20,18 @@ const books = [
     bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
-
+//a function which accept bookObj containing property of books as an array.
+function readingList(bookObj){
+  for (let i = 0; i < bookObj.length; i++) {
+    let bookTitle = bookObj[i].title; //for each iteration store the title in bookTitle
+    let bookAuthor = bookObj[i].author; //for each iteration store the author in bookAuthor
+    let bookCover = bookObj[i].bookCoverImage; //for each iteration store the url of book cover in bookCover
+    let ul = document.getElementById("reading-list"); // get the element from my document with id reading-list and let it be ul
+    let li = document.createElement("li"); //then create html element called li with "li" element value
+    ul.appendChild(li); //then append the created element li as a child in parent ul
+    li.innerHTML = `${bookTitle}${bookAuthor} <img src=${bookCover} alt="cover image">`; // then assign the innerHTML of our li element by the value given after = sign
+    bookObj[i].alreadyRead? (li.style.backgroundColor = "blue"): (li.style.backgroundColor = "red");
+    // if alreadyread is true assign blue as the background color of li if not red.
+  }
+}
+readingList(books);
