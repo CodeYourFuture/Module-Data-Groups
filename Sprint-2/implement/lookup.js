e178576a09f7c405c99fff2cc89dd0573200ca02
Priscilla <cpjm0033@gmail.com>
2025-03-08 12:38:53 +0000

    lookup function

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,15 @@
-function createLookup() {
+function createLookup(countryCurrencyPairs) {
   // implementation here
+  const lookup = {}; // Create an empty array
+  // Loop through each country-currency pair
+  for (let pair of countryCurrencyPairs) {
+    let country = pair[0]; // Extract country code
+    let currency = pair[1]; // Extract currency code
+
+    lookup[country] = currency; // Add the country as a key and currency as a value
+  }
+
+  return lookup;
 }
 
 module.exports = createLookup;
