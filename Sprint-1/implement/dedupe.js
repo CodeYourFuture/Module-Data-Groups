961c49a5227741622f91691353023bbb7296a982
Faithy4444 <faithy4444@gmail.com>
2025-03-08 19:29:26 +0200

    dedupe changes

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,7 +1,9 @@
-function dedupe(array) {
+/*function dedupe(array) {
     copyArray =[]
     array.map(x => !copyArray.includes(x) ? copyArray.push(x): "") 
     return copyArray 
-    }
-
-
+    }*/
+function dedupe(arr){
+    return [... new Set(arr)]
+}
+console.log(dedupe(['a','a','a','b','b','c', 'd']))
