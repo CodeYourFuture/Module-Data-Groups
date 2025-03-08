3e6f4149050071b24c725c85122761b5f1cfce87
sarawone <sarawone.poudal@gmail.com>
2025-03-08 15:07:11 +0000

    implement sum function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,70 @@
 function sum(elements) {
+    // if the array is empty 
+    if (elements.length===0)
+    {
+            return 0;
+    }
+
+    // if all array value are not non-numeric value
+    else if (elements.every(items => isNaN(items) === false)){
+        let total = 0;
+        for (let i=0;i < elements.length;i++)
+        {
+            total += elements[i];
+        }
+        return total;
+
+    }
+
+    // if the all values in array are all non-numeric number 
+    else if (elements.every(items => isNaN(items)) === true)
+    {
+        return "There is no numbers in this array"
+    }
+
+    // if the array has mix with non-numeric & numeric value 
+    else 
+    {
+       // filter the string from the array 
+        Filter_arr = elements.filter(s => typeof s === 'number');
+        
+        return sum(Filter_arr);
+
+    }
 }
 
 module.exports = sum;
+
+
+// for empty array 
+EmptArr = sum([]);
+console.log("The result is :",EmptArr);
+
+// for the array with one numeric value
+
+OneArr =  sum([7]);
+console.log("The result for array with one value",OneArr);
+
+// for the array with all positive values
+PosArr = sum([1,2,3]);
+console.log("The result for array with all positive values",PosArr);
+
+// for the array with all negative values
+
+NegArr = sum([-1,-2,-3]);
+console.log("The result for array with all negative values",NegArr);
+
+// for the array with mixed positive & negative values
+
+MixArr = sum ([1,-2,3,-3,8]);
+console.log("The result for array with both +&- values",MixArr);
+
+// for the array with mix num & sting 
+
+MixedArr = sum ([1,2,"a","b","c"]);
+console.log("The result for array mixed with number& string",MixedArr);
+
+// for the array with all string value 
+
+StrArr = sum(["a","b"]);
+console.log("The result for array with all string value",StrArr);
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,49 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test ("Get an empty array, return 0", () =>{
+    EmptArr = sum([]);
+    expect(EmptArr).toEqual(0);
+    
+})
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
 
+test ("Get an array with only one value,return that value", () => {
+    OneArr = sum([7]);
+    expect(OneArr).toEqual(7);
+})
+
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
 
+test ("Get an array with +&- numbers,return the correct value",() =>{
+    MixArr = sum([1,-2,3,4]);
+    expect(MixArr).toEqual(6);
+})
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
-
+test ("Get an array with decimal value,return the correct sum",() => {
+    DecArr = sum ([1.5,1.6]);
+    expect(DecArr).toEqual(3.1);
+})
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
 
+test ("Get an array mixed with non-number & number value, add the number values ", () => {
+    MixArr = sum (['a',1,2,3,'c']);
+    expect(MixArr).toEqual(6);
+})
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+
+test ("Get an array with all string values, return the array has no number message",() =>{
+    StrArr = sum (['a','b','c']);
+    expect(StrArr).toEqual('There is no numbers in this array');
+})
