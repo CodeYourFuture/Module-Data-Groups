c2bcdc2cd1768895de3233197266e1d1a6ea3ce8
Cristina Leascu <cristinaleascu@gmail.com>
2025-03-08 01:34:33 +0000

    debug | recipe - answer

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,9 @@
 // Predict and explain first...
 
+
+// Answer | It would not log out the ingredients, as the key and indexes for each ingredient were not called. 
+
+
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -12,4 +16,7 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients[0]}
+${recipe.ingredients[1]}
+${recipe.ingredients[2]}
+${recipe.ingredients[3]}`);
