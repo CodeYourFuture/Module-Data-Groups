e06321843020aaa20a38bb0e1710d3086bf1b628
Aida Zhukova <aidazhukovaa@gmail.com>
2025-03-08 19:24:45 +0000

    added set timer functionality

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,23 @@
-function setAlarm() {}
+let countdown;
+let timeLeft;
+let timeRemainingDisplay = document.getElementById("timeRemaining");
+
+function setAlarm() {
+  const input = document.getElementById("alarmSet").value;
+  let number = Number(input);
+  console.log(typeof number);
+
+  let intervalId = setInterval(() => {
+    console.log(number);
+    number = number - 1;
+    timeRemainingDisplay.textContent = `Time Remaining:${number}`;
+
+    if (number === 0) {
+      playAlarm();
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
