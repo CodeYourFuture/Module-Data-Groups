0e22a446b7286d1129991cf685a65be940ae2c9c
Droid-An <andrei.filippov.aaf@gmail.com>
2025-03-08 20:18:34 +0000

    made a timer that counts down

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,28 @@
-function setAlarm() {}
+function setAlarm() {
+  const input = document.querySelector("input");
+  time = input.valueAsNumber;
+  function changeDisplayTime(time) {
+    minutesRemaining = Math.floor(time / 60).toString();
+    paddedMinutesRemaining = minutesRemaining.padStart(2, "0");
+    secondsRemaining = (time % 60).toString();
+    paddedSecondsRemaining = secondsRemaining.padStart(2, "0");
+    const timeRemaining = `${paddedMinutesRemaining}:${paddedSecondsRemaining}`;
+    const charactersLeftP = document.querySelector("#timeRemaining");
+    charactersLeftP.innerText = `Time Remaining: ${timeRemaining}`;
+    
+  }
+  function deduction() {
+    changeDisplayTime(time)
+    if (time == 0) {
+      clearInterval(timerId)
+    }
+    --time
+  }
+  let timerId = setInterval(deduction, 1000)
+}
+function pauseAlarm() {
+  clearInterval(timerId)
+}
 
 // DO NOT EDIT BELOW HERE
 
