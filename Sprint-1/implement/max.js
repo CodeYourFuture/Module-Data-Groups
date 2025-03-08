937913c2c097560bc5c02928c8add3864036d7fc
Faithy4444 <faithy4444@gmail.com>
2025-03-08 19:54:24 +0200

    max

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,8 +1,9 @@
 function findMax(elements) {
-    let sorted = [...elements].sort((a, b) => a - b)
-    if (elements.length === 0)return "-Infinity"
-    else return sorted[sorted.length -1] 
+    let numericValues = elements.filter(el => typeof el === 'number');
+    if (numericValues.length === 0) return -Infinity;
+    
+    return Math.max(...numericValues);
 }
 
-console.log(findMax([]))
+console.log(findMax(['a', 10, 'b', 3]))
 module.exports = findMax;
