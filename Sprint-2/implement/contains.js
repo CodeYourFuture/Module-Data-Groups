ac4dea6184a0e66c69ceb82ff6e30dd492d43626
khalil-ali1 <khalil.ali.uk1@gmail.com>
2025-03-09 15:51:04 +0000

    finish implement/contain

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,11 @@
-function contains() {}
+function contains(object,key) {
+    return object.hasOwnProperty(key) && Array.isArray(object) === false
+}
+
+console.log(contains({ a: 1, b: 2 }, 'a'))
+console.log(contains([1, 2, 3], 1))
+console.log(contains({}))
+console.log(contains({ a: 1, b: 2 }, '1'))
+
 
 module.exports = contains;
