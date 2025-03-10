16eaf86eabe5d80978944678c16290511a596f0d
Droid-An <andrei.filippov.aaf@gmail.com>
2025-03-10 18:35:21 +0000

    made working forward button

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,34 @@
+const forwardButton = document.querySelector("#forward-btn");
+const backwardsButton = document.querySelector("#backward-btn");
+const carousel = document.querySelector("#carousel-img");
+
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
+  "./assets/daria-shatova-46TvM-BVrRI-unsplash.jpg",
+  "./assets/kate-stone-matheson-uy5t-CJuIK4-unsplash.jpg",
 ];
+let index = 1;
+
+function moveForward() {
+        carousel.setAttribute("src", images[index]);
+        if (index == (images.length) - 1) {
+            index = 0
+        } else {
+            index++
+        } 
+        console.log(index)
+        }
+
+    // for(let index = 0; index < images.length; index++) {
+    //     console.log(images[index])
+    //     carousel.setAttribute("src", images[index]);
+    //     if (index == images.length) {
+    //         index = -1
+    //     }
+    // }
 
 
-// Write your code here
+forwardButton.addEventListener("click", moveForward);
+backwardsButton.addEventListener("click", moveForward);
