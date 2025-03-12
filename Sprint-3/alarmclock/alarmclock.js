a6e41e363c6f437b454986f8b849ccd9eaddd5da
khalil-ali1 <khalil.ali.uk1@gmail.com>
2025-03-12 05:16:55 +0000

    finish sprint-3/alarmclock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,6 +1,32 @@
-function setAlarm() {}
+function getFormattedInput(inputInSeconds) {
+  const minutes = Math.floor(inputInSeconds / 60);
+  const seconds = inputInSeconds % 60;
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(seconds).padStart(2, '0');
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+function countdown(inputInSeconds) {
+  function updateAndContinue() {
+    if (inputInSeconds < 0) {
+      playAlarm();
+      document.body.style.backgroundColor = "red";
+      return;//break the loop when inputInSeconds reaches 0
+    }
+    timeRemaining.innerHTML = `Time Remaining: ${getFormattedInput(inputInSeconds)}`;
+    inputInSeconds--;
+    setTimeout(updateAndContinue, 1000);
+  }
+  updateAndContinue();
+}
 
+function setAlarm() {
+  let inputInSeconds = alarmSet.value;
+  const timeRemaining = document.querySelector("#timeRemaining");
+  timeRemaining.innerHTML = `Time Remaining: ${getFormattedInput(inputInSeconds)}`;
+ countdown(inputInSeconds);
+}
 // DO NOT EDIT BELOW HERE
+
 
 var audio = new Audio("alarmsound.mp3");
 
@@ -20,6 +46,7 @@ function playAlarm() {
 
 function pauseAlarm() {
   audio.pause();
+  document.body.style.backgroundColor = "white";
 }
 
 window.onload = setup;
