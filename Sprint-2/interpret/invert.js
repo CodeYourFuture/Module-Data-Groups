82b8d3e3313054467eec94e6266485b0246a742b
Cristina Leascu <cristinaleascu@gmail.com>
2025-03-12 21:41:12 +0000

    interpret | invert

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -10,20 +10,34 @@ function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
 
+const input = { x: 10, y: 20 };
+const expectedOutput = { 10: "x", 20: "y" };
+console.assert(
+  JSON.stringify(invert(input)) === JSON.stringify(expectedOutput),
+  "Test failed: When invert is passed this object, it should output { '10': 'x', '20': 'y' }"
+);
+
+console.log("Test passed if no assertion errors appear!");
+
+// We can also use console.log to test
+console.log(invert(input));
+
 // a) What is the current return value when invert is called with { a : 1 }
-
+// The returned values is : {key: 1}
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
-
+// The returned values is : {key: 2}
 // c) What is the target return value when invert is called with {a : 1, b: 2}
-
+// The target return is : { "1": "a", "2": "b" }
 // c) What does Object.entries return? Why is it needed in this program?
-
+// Object.entries is used to iterate over the key-value pairs in the object.
 // d) Explain why the current return value is different from the target output
+// The current return value is different from the target value, because the program does not swap keys with the values instead, the program incorrectly assigns a fixed property name "key" to the object.
 
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
+// Implementation done.
