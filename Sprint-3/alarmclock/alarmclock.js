7e8d18a2e2a1b0a4acad22dfb897ad06a8a0cd3b
Aida Zhukova <aidazhukovaa@gmail.com>
2025-03-12 21:54:05 +0000

    alarmclock done

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,21 +1,31 @@
-let countdown;
-let timeLeft;
 let timeRemainingDisplay = document.getElementById("timeRemaining");
+let stopButton = document.getElementById("stop");
 
+function transformSeconds(seconds) {
+  let min = Math.trunc(seconds / 60);
+  if (min < 10) {
+    min = `0${min}`;
+  }
+  let remainingSeconds = seconds % 60;
+  if (remainingSeconds < 10) {
+    remainingSeconds = `0${remainingSeconds}`;
+  }
+  return `${min}:${remainingSeconds}`;
+}
+56;
 function setAlarm() {
   const input = document.getElementById("alarmSet").value;
   let number = Number(input);
-  console.log(typeof number);
 
   let intervalId = setInterval(() => {
-    console.log(number);
-    number = number - 1;
-    timeRemainingDisplay.textContent = `Time Remaining:${number}`;
+    let timeInMMSS = transformSeconds(number);
+    timeRemainingDisplay.textContent = `Time Remaining: ${timeInMMSS}`;
 
     if (number === 0) {
       playAlarm();
       clearInterval(intervalId);
     }
+    number = number - 1;
   }, 1000);
 }
 
