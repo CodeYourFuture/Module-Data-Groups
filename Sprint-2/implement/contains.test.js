e8d5d966aee9de9e848cbb52bce10625e7c80716
loveth900 <ojay_love@yahoo.com>
2025-03-13 07:02:41 +0000

    fixed the function

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -1,47 +1,47 @@
-
+const contains = require("./contains.js");
 /*
 Implement a function called contains that checks an object contains a
 particular property
-
 E.g. contains({a: 1, b: 2}, 'a') // returns true
 as the object contains a key of 'a'
-
 E.g. contains({a: 1, b: 2}, 'c') // returns false
 as the object doesn't contains a key of 'c'
 */
-
 // Acceptance criteria:
-
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
-
 // Given an empty object
 // When passed to contains
 // Then it should return false
+test.todo("contains on empty object returns false");
+describe("", () =>{
+    test("Given an empty object", () =>{
+        expect(contains({}, "a")).toEqual(false)
+    });
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
 
+    test("Given an object with properties with an existing property name, it should return true", () =>{
+        expect(contains({a: 1, b: 2}, "a")).toEqual(true)
+    })
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
 
+    test("Given an object with properties with a non-existent property name, it should return false", () =>{
+        expect(contains({a: 1, b: 2}, "c")).toEqual(false)
+    })
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
-
-// contains.js
-function contains(obj, property) {
-    // Check that the input is an object, not an array.
-    if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
-        return false;
-    
-    }
-
-    // we need to verify if the specified property is present in the object.
-    return property in oby 
-}
-
-module.exports = contains;
+    test("Given invalid parameters like an array, it should return false ", () =>{
+        expect(contains(["a", "b"], "a")).toEqual(false);
+        expect(contains(2, "0")).toEqual(false);
+        expect(contains("a", "0")).toEqual(false);
+        expect(contains(true, "0")).toEqual(false);
+        expect(contains(null, "a")).toEqual(false);
+    })
+});
