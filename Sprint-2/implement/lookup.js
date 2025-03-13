d73172760bbaf7c0b1ca43b5b7e829aba401e8aa
loveth900 <ojay_love@yahoo.com>
2025-03-13 07:26:58 +0000

    corrected

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,20 +1,25 @@
-function createLookup(arr, key) {
-  const lookup = {}; 
-  // initialize an empty object to store the lookup table
 
-  // loop through each object in the input array.
-  Array.forEach(item => {
-  // assign the value of the specified key from the object as the lookup key.
-  // and store the  entire object as the corresponding value
-  lookup[item[key]] = item;
 
-  })
+function createLookup(countryCurrencyPairs) {
+  const createLookup = {};
+  // create an empty object to hold the key-value pairs
+
+  // loop through each country-currency pair in the array
+  countryCurrencyPairs.forEach(pair => {
+    const [countryCode, currencyCode] = pair;
+  // Destructure each pair into countryCode and currencyCode
+  lookup[countryCode] = currencyCode;
+  // store the country code as the key and currency code as the value
+
+  });
 
   return lookup; 
-  // needed to return the completed lookup table.
-  
+
+  // return the constructed lookup object
+
 }
 
 module.exports = createLookup;
+
 
 
--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -30,25 +30,22 @@ It should return:
    'CA': 'CAD'
  }
 */
-  
 
-function createLookup(countryCurrencyPairs) {
-  const createLookup = {};
-  // create an empty object to hold the key-value pairs
+function createLookup(arr, key) {
+  const lookup = {}; 
+  // initialize an empty object to store the lookup table
 
-  // loop through each country-currency pair in the array
-  countryCurrencyPairs.forEach(pair => {
-    const [countryCode, currencyCode] = pair;
-  // Destructure each pair into countryCode and currencyCode
-  lookup[countryCode] = currencyCode;
-  // store the country code as the key and currency code as the value
+  // loop through each object in the input array.
+  Array.forEach(item => {
+  // assign the value of the specified key from the object as the lookup key.
+  // and store the  entire object as the corresponding value
+  lookup[item[key]] = item;
 
-  });
+  })
 
   return lookup; 
-
-  // return the constructed lookup object
-
+  // needed to return the completed lookup table.
+  
 }
 
 module.exports = createLookup;
