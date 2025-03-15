41ee59705df54fb35321b6ba9e8e017f36ce8523
sarawone <sarawone.poudal@gmail.com>
2025-03-15 12:28:31 +0000

    changing the variable name to cameCase

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -26,7 +26,7 @@ function sum(elements) {
     else 
     {
        // filter the string from the array 
-        Filter_arr = elements.filter(s => typeof s === 'number');
+        filter_arr = elements.filter(s => typeof s === 'number');
         
         return sum(Filter_arr);
 
@@ -37,37 +37,37 @@ module.exports = sum;
 
 
 // for empty array 
-EmptArr = sum([]);
-console.log("The result is :",EmptArr);
+emptArr = sum([]);
+console.log("The result is :",emptArr);
 
 // for the array with one numeric value
 
-OneArr =  sum([7]);
-console.log("The result for array with one value",OneArr);
+oneArr =  sum([7]);
+console.log("The result for array with one value",oneArr);
 
 // for the array with all positive values
-PosArr = sum([1,2,3]);
-console.log("The result for array with all positive values",PosArr);
+posArr = sum([1,2,3]);
+console.log("The result for array with all positive values",posArr);
 
 // for the array with all negative values
 
-NegArr = sum([-1,-2,-3]);
-console.log("The result for array with all negative values",NegArr);
+negArr = sum([-1,-2,-3]);
+console.log("The result for array with all negative values",negArr);
 
 // for the array with mixed positive & negative values
 
-MixArr = sum ([1,-2,3,-3,8]);
-console.log("The result for array with both +&- values",MixArr);
+mixArr = sum ([1,-2,3,-3,8]);
+console.log("The result for array with both +&- values",mixArr);
 
 // for the array with mix num & sting 
 
-MixedArr = sum ([1,2,"a","b","c"]);
-console.log("The result for array mixed with number& string",MixedArr);
+mixedArr = sum ([1,2,"a","b","c"]);
+console.log("The result for array mixed with number& string",mixedArr);
 
 // for the array with all string value 
 
-StrArr = sum(["a","b"]);
-console.log("The result for array with all string value",StrArr);
+strArr = sum(["a","b"]);
+console.log("The result for array with all string value",strArr);
 
 testNan =  sum([NaN,1]);
 console.log("The result for Nan,1",testNan);
