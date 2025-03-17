4e9cc40a5841fe0c15f70ec0beae1f3372efe373
Elchacode <elchaschool@gmail.com>
2025-03-17 15:06:20 +0000

    refactored the code to log out the value of the object, as the previous syntax was wrong.

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -2,6 +2,8 @@
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
+// predictions: this would return undefined as line 14 should be const value in the object not of the object type. 
+// explanation: the code want to access the value but cant do so by declaring it as a const. we have to loop through the keys to get the value. 
 
 const author = {
   firstName: "Zadie",
@@ -11,6 +13,10 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
-  console.log(value);
+for (const key in author) { // this would iterate over the keys of the object. and the value would be the value of the key. 
+// Output: Zadie, Smith, writer, 40, true
+  
+  console.log(author[key]); // reason for using a bracket notation, as this would access the values in the object of the key. This would log out the values of the object.
 }
+
+
