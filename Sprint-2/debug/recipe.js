f723e634a0f9a4abc1f43f2c276bb1abfc67114b
Elchacode <elchaschool@gmail.com>
2025-03-17 16:09:39 +0000

    i refactored the giving object to log out the ingredient value in a new line. by implementing join() method.

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,7 +1,9 @@
 // Predict and explain first...
+// prediction: the output should be in the same line, and the ingredients should be separated by commas. but the output is not as expected.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
+// explanation: the output is not as expected because the ingredients are an array and we are trying to log out the object.
 // How can you fix it?
 
 const recipe = {
@@ -10,6 +12,12 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
+// console.log(`${recipe.title} serves ${recipe.serves}
+//   ingredients:
+// ${recipe}`);
+
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+  
+${recipe.ingredients.join("\n")}`); // we used .join() because the ingredients are inside an array. and we can only join the array with a comma and a space.
+
+
