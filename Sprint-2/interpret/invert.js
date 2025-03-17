65f65f26d933c6685f79d6930d6498b02f46d5bb
Elchacode <elchaschool@gmail.com>
2025-03-17 16:53:03 +0000

    refactored the function to swap the key and the value in the obj, the previous code only return the key and the value by default.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -10,20 +10,29 @@ function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
+console.log(invert({ a: 1 })); // { 1: 'a' }
 
-// a) What is the current return value when invert is called with { a : 1 }
+// a) What is the current return value when invert is called with { a : 1 } // {a: 1}
 
-// b) What is the current return value when invert is called with { a: 1, b: 2 }
+// b) What is the current return value when invert is called with { a: 1, b: 2 } // {a: 1, b: 2}
 
-// c) What is the target return value when invert is called with {a : 1, b: 2}
+// c) What is the target return value when invert is called with {a : 1, b: 2} // {"1": "a", "2": "b"}
 
-// c) What does Object.entries return? Why is it needed in this program?
+// c) What does Object.entries return? Why is it needed in this program?   // it needed because this method returns the key-value pair in an array of arrays, where each inner array contains a key-value pair. 
 
-// d) Explain why the current return value is different from the target output
+// d) Explain why the current return value is different from the target output // because the current implementation is only assigning the key to the value, not the value to the key. 
 
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
+// solution: 
+test("invert correctly swaps keys and values", () => {
+  expect(invert({ a: 1 })).toEqual({ 1: 'a' });
+  expect(invert({ a: 1, b: 2 })).toEqual({ a: 1, b: 2 });
+});
+// i refactored the invert function to swap keys and values: 
+// invertedObj[key] = value; // to invertedObj[value] = key;
+
