55a5a78877f3949d6515aca410756b1a7efdd20b
Elchacode <elchaschool@gmail.com>
2025-03-17 16:20:12 +0000

    implemented a function if a particular property is in an obj. if not return false.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,10 @@
-function contains() {}
+function contains(obj, key) {
+        if (obj == null || typeof obj !== 'object' || Array.isArray(obj)) { // implented a for loop which check if obj is === null or type of object is not object or array is not object. This is to check if the object is not an array or null.
+            return false; // then we return false.
+        }
+        return obj.hasOwnProperty(key); // else we return true if object hasOwnProperty of key. this is to check if the object has the key input. hasOwnProperty is a method that returns a boolean indicating whether the object has the specified property as its own property (as opposed to inheriting it).
+    }
+    
+
 
 module.exports = contains;
