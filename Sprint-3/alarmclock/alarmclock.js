e384ad4bf3143edb78f563c4c74749a62d8dc813
Mikiyas-STP <mikiyasgebremichaell@gmail.com>
2025-03-18 01:08:49 +0000

    This Commit is Done for The corrections Suggested by a Mentor on march 18, 2025 on alarm clock project

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,106 +1,62 @@
+var audio = new Audio("alarmsound.mp3");
+var countDown;
+let minutesVal;
+let secVal;
 function setAlarm() {
+  if (countDown) {
+    clearInterval(countDown);
+  }
   var textareaVal = document.getElementById("alarmSet").value;
-  let separatedValues =  textareaVal.split(':');
-  let minutesVal = parseInt(separatedValues[0]);
-  let secVal = parseInt(separatedValues[1]);
-  //document.getElementById("timeRemaining").innerHTML = `Time Remaining: ${minutesVal} : ${secVal}`;
-  if (isNaN(minutesVal) || isNaN(secVal) || minutesVal > 59 || minutesVal < 0 || secVal > 59 || secVal < 0 ) {
-    document.getElementById("timeRemaining").innerHTML ="Input Invalid: Provide a string in MM:SS where MM and SS are numbers";
+  if(textareaVal>0 && textareaVal<=60){
+    minutesVal = 0;
+    secVal = parseInt(textareaVal);
+  }
+  else if(textareaVal>60){
+    minutesVal = Math.floor(textareaVal/60);
+    secVal = parseInt(textareaVal%60);
+
+  }
+  else if (
+    isNaN(minutesVal) ||
+    isNaN(secVal) ||
+    minutesVal > 59 ||
+    minutesVal < 0 ||
+    secVal > 59 ||
+    secVal < 0
+  ) {
+    //document.getElementById("timeRemaining").innerHTML ="Input Invalid: Provide a string in MM:SS where MM and SS are numbers";
+    alert("Input Invalid: Provide a Whole Number OR Provide a string in MM:SS where MM and SS are numbers");
     return;
   }
-  var countDown = setInterval(function () {
-      if (minutesVal !== 0) {
-        if (secVal !== 0) {
-          secVal--;
-          if (secVal === 0) {
-            minutesVal = minutesVal - 1;
-            secVal = 59;
-          }
-        }
-      } else if (minutesVal === 0) {
-        if (secVal !== 0) {
-          secVal--;
-        } else if (secVal === 0) {
-          clearInterval(countDown);
-          document.body.style.backgroundColor = "red";
-          playAlarm();
-        }
-      }
-      document.getElementById("timeRemaining").innerHTML = `Time Remaining: ${String(minutesVal).padStart(2,"0")} : ${String(secVal).padStart(2,"0")}`;
-    }, 1000);
+  else  {
+    let separatedValues = textareaVal.split(":");
+    minutesVal = parseInt(separatedValues[0]);
+    secVal = parseInt(separatedValues[1]);
   }
   
-
-/*if (secVal !== 0) {
-      if (minutesVal !== 0) {
+  countDown = setInterval(function () {
+    if (minutesVal !== 0) {
+      if (secVal !== 0) {
         secVal--;
         if (secVal === 0) {
           minutesVal = minutesVal - 1;
-          secVal = secVal + 60;
-          secVal--;
-        }
-        if (minutesVal === 0 && secVal === 0) {
-          playAlarm();
+          secVal = 59;
         }
       }
-    }
-    else if(secVal !== 0){
-      if(minutesVal === 0){
+    } else if (minutesVal === 0) {
+      if (secVal !== 0) {
         secVal--;
-        if(secVal === 0){
-          playAlarm();
-        }
-      }
-    } */
-/*    else if (minutesVal !==0 && secVal !== 0){
-      if(minutesVal !== 0 & secVal === 0 ){
-        minutesVal=minutesVal-1;
-        secVal = secVal + 60;
-        for(let i = 0; i < 60; i++ )
-        {
-          secVal--;
-        }
-      }
-      else if(minutesVal !== 0 & secVal !== 0){
-        for(let i = 0; i < secVal ;i++){
-          secVal--;
-          if(secVal===0){
-            minutesVal = minutesVal - 1;
-            secVal = secVal + 60 ;
-            for(let i = 0; i < secVal ;i++){
-          secVal--;
-        }
-        minutesVal = minutesVal - 1 ;
-          }
+      } else if (secVal === 0) {
+        clearInterval(countDown);
+        document.body.style.backgroundColor = "red";
+        playAlarm();
       }
     }
-    else if (minutesVal === 0 & secVal === 0){
-      playAlarm();
-    }
-    }
-  } */
+    document.getElementById("timeRemaining").innerHTML = `Time Remaining: ${String(minutesVal).padStart(2,"0")} : ${String(secVal).padStart(2, "0")}`;
 
-/*
+      }, 1000);
+}
 
-function (){
-    if (minutesVal > 60 || minutesVal < 1){
-      clearInterval(countDown); //this means stop the countdown function
-      document.getElementById(
-        "timeRemaining"
-      ).innerHTML = "The Time is invalid provide time mm:ss format";
-    }
-    else{
-      minutesVal--;
-      document.getElementById(
-        "timeRemaining"
-      ).innerHTML = `Time Remaining: ${minutesVal} : ${secVal}`;
-      
-    }
-
-  }
-
-
-*/
 //Implement
 //User enter a number | clicked set alarm button  | Time Remaining ->updated to mm:ss
 //Alarm set with valid time | When one second passes time - remaining title should decrease by 1 second.
@@ -110,7 +66,13 @@ function (){
 //when loading for 1st time should show 00:00 and no alarm sound should should play.
 // DO NOT EDIT BELOW HERE
 
-var audio = new Audio("alarmsound.mp3");
+function playAlarm() {
+  audio.play();
+}
+
+function pauseAlarm() {
+  audio.pause();
+}
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
@@ -120,14 +82,6 @@ function setup() {
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
   });
-}
-
-function playAlarm() {
-  audio.play();
-}
-
-function pauseAlarm() {
-  audio.pause();
 }
 
 window.onload = setup;
