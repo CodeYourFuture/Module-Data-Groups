cc34f93a6974944c0cf7765c6a7c000d9fc4bad8
Segun <segunfolayan@yahoo.com>
2025-03-18 10:11:03 +0000

    SumtestUpdate1803

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,18 @@
 function sum(elements) {
+    let total = 0;
+  for (let Index=0; Index < elements.length; Index++){
+    if (typeof elements[Index] === "number"){
+        total+=elements[Index];
+    }
+    else {
+        total=total + 0;
+    }
+
+   }
+   return total;
+    
 }
 
-module.exports = sum;
+
+
+module.exports =sum;
