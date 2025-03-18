80bc289796851257f407b8c7c464f843d7b73fb3
Priscilla <cpjm0033@gmail.com>
2025-03-18 04:38:18 +0000

    made changes to the test

---
+++ prep/Sprint-1/fix/median.test.js
@@ -0,0 +1,27 @@
+// median.test.js
+
+// Someone has implemented calculateMedian but it isn't
+// passing all the tests...
+// Fix the implementation of calculateMedian so it passes all tests
+
+const calculateMedian = require("./median.js");
+
+describe("calculateMedian", () => {
+  test("returns the median for odd length array", () => {
+    expect(calculateMedian([1, 2, 3])).toEqual(2);
+    expect(calculateMedian([1, 2, 3, 4, 5])).toEqual(3);
+  });
+
+  test("returns the average of middle values for even length array", () => {
+    expect(calculateMedian([1, 2, 3, 4])).toEqual(2.5);
+    expect(calculateMedian([1, 2, 3, 4, 5, 6])).toEqual(3.5);
+  });
+
+  test("doesn't modify the input", () => {
+    const list = [1, 2, 3];
+    const copy = [...list]; // makes a copy of the list to make sure the input doesn't change when we call the function
+    calculateMedian(list);
+
+    expect(list).toEqual(copy);
+  });
+});
