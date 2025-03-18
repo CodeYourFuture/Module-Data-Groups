c1213764777f5037b728f7686c16d9bc8c5d9142
sab325 <sabshrestha324@gmail.com>
2025-03-18 14:43:37 +0000

    Alarm clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+function setAlarm() {
+  const inputValue = document.getElementById("alarmSet");
+  let secondsLeft = parseInt(inputValue.value);
+
+  if (isNaN(secondsLeft) || secondsLeft <= 0) {
+    alert("Please enter a valid number.");
+    return;
+  }
+
+  const heading = document.getElementById("timeRemaining");
+
+  function updateHeading() {
+    let minutes = Math.floor(secondsLeft / 60);
+    let seconds = secondsLeft % 60;
+    heading.textContent = `Time Remaining: ${minutes
+      .toString()
+      .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  }
+
+  updateHeading();
+
+  const countdown = setInterval(() => {
+    secondsLeft--;
+
+    if (secondsLeft <= 0) {
+      setInterval(countdown);
+      setAlarm();
+      alert("Time's up!");
+    }
+
+    updateHeading();
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
