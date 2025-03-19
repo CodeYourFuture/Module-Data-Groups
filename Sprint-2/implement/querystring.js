8bdafae4a11b4bd4d8f954213e700656910d0ad7
Priscilla <cpjm0033@gmail.com>
2025-03-19 16:29:08 +0000

    Function correction

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -6,13 +6,27 @@ function parseQueryString(queryString) {
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, ...valueParts] = pair.split("="); // Split only on the first "="
-    const value = valueParts.join("="); // Joins back the remaining value if it contained "="
+    const [encodedKey, ...valueParts] = pair.split("="); // Split only on the first "=" and values can contain "="
+    if (!encodedKey) continue; // if key is missing, skip.
 
-    queryParams[key] = value;
+    const key = decodeURIComponent(encodedKey);  // decodeURIComponent(encodedKey) makes sure that encoded characters (like %20 for spaces) are converted properly
+    const value = decodeURIComponent(valueParts.join("=") || ""); // valueParts.join("=") joins back any values that contains "=" or split by =, while || "" makes sure that if there are no value(s), it will be an empty string "".
+
+   
+    if (queryParams[key]) {
+      queryParams[key] = [].concat(queryParams[key], value); // queryParams[key] is converted into an array, and new values are added the existing one thereby handling duplicate keys.
+    } else {
+      queryParams[key] = value; // but if key appeared only once, the single value will be stored
+    }
   }
 
   return queryParams;
 }
 
 module.exports = parseQueryString;
+
+
+  
+
+  
+
