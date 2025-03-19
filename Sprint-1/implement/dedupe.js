290ecea26180e4a83fad4afb0087b6289e3ae22a
JanLlw <llwlojan@gmail.com>
2025-03-19 18:18:21 +0000

    fix 5 question in sprint 1

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,10 @@
-function dedupe() {}
+function dedupe(array) {
+    const newDedupe = [...new Set(array)];   
+    return newDedupe;
+}
+
+module.exports = dedupe;
+
+// console.log(dedupe(['a','a','a','b','b','c']));
+// console.log(dedupe(['a','a','a','b','b','c']));
+// console.log(dedupe([]));
