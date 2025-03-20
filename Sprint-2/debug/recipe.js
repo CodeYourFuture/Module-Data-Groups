f67fbdb755cc5f35e8cfa20a2d03f41c2526e459
Chi <181338252+Chi-Mb@users.noreply.github.com>
2025-03-20 17:42:40 +0000

    Fixed errors and completed debug section

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 
+// Putting recipe in the template string shows a default object text instead of its data.
+
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -11,5 +13,6 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+  Ingredients:
+  ${recipe.ingredients.join('\n')}`);
+  
