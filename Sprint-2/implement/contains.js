a52739c5744eee3b8060c9af178fbfe51ee4ea9a
sarawone <sarawone.poudal@gmail.com>
2025-03-21 23:36:07 +0000

    implement function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,30 @@
-function contains() {}
+function contains(o,s) {
+// if given argument is object
+if (typeof o === "object" && !Array.isArray(o))
+{
+   
+    return  (s in o);
+}
+else
+{
+    throw new Error ("The input is not an object");
+}
+
+}
+
+
+
+const person = {
+    name : "ricardo",
+    age : 22
+};
+
+const hobbies = ["football","read"];
+
+const favourite = {};
+
+let checking = contains(favourite);
+console.log(checking);
+
 
 module.exports = contains;
