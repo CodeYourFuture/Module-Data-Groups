a2695055e40adb81679d415905b851fb9fa56d5c
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-22 23:47:46 +0000

    Data Group - Sprint 1 - Implement - max.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,19 @@
 function findMax(elements) {
+  if (elements.length === 0) {
+    return -Infinity;
+  }
+
+  let max = -Infinity;
+
+  for (let i = 0; i < elements.length; i++) {
+    if (typeof elements[i] === "number" && !isNaN(elements[i])) {
+      if (elements[i] > max) {
+        max = elements[i];
+      }
+    }
+  }
+
+  return max;
 }
 
 module.exports = findMax;
