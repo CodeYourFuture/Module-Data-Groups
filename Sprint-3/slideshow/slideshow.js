13ca8bb62d3c8bd0e50c2036b721b92165bef6d1
Priscilla <cpjm0033@gmail.com>
2025-03-22 11:54:42 +0000

    slideshow function

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,33 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
+const carouselImage = document.getElementById("carousel-img");
+const backwardBtn = document.getElementById("backward-btn");
+const forwardBtn = document.getElementById("forward-btn");
+
+let currentImage = 0;
+
+function forward() {
+  // currentImage++;
+  currentImage = (currentImage+1)%images.length
+  // if (currentImage === images.length) {
+  //   currentImage = 0;
+  // }
+  carouselImage.src = images[currentImage];
+}
+
+function backward() {
+ currentImage = (currentImage - 1)%images.length
+ if (currentImage < 0) {
+  currentImage = images.length - 1
+ }
+  carouselImage.src = images[currentImage];
+}
+
+forwardBtn.addEventListener("click", forward);
+backwardBtn.addEventListener("click", backward);
 
 // Write your code here
