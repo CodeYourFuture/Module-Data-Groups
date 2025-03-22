fdd94adf92ba1dbcbd4faa8faa1a50192e0013b3
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-22 23:56:44 +0000

    Sprint 1 - Data Group - Implement sum.test.js

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,65 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test.todo("given an empty array, returns 0");
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
+test.todo("given an array with just one number, return that number");
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
+test.todo("given an array with negative numbers, return correct total sum");
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
+test.todo(
+  "given an array with decimal/float numbers, return correct total sum"
+);
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+test.todo(
+  "given an array containing non-number values, ignore non numeric and return correct total sum for numerical elements"
+);
 
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test.todo(
+  "given an array with only number values, return the least surprising value given how it behaves for all other inputs"
+);
+
+describe("sum", () => {
+  test("given an empty array, returns 0", () => {
+    expect(sum([])).toEqual(0);
+  });
+
+  test("given an array with just one number, return that number", () => {
+    expect(sum([5])).toEqual(5);
+    expect(sum([-3])).toEqual(-3);
+  });
+
+  test("given an array with negative numbers, return correct total sum", () => {
+    expect(sum([-1, -2, -3])).toEqual(-6);
+    expect(sum([-10, 5, -5])).toEqual(-10);
+  });
+
+  test("given an array with decimal/float numbers, return correct total sum", () => {
+    expect(sum([1.5, 2.5, 3.5])).toEqual(7.5);
+    expect(sum([-1.1, -2.2, 3.3])).toEqual(0);
+  });
+
+  test("given an array containing non-number values, ignore non numeric and return correct total sum for numerical elements", () => {
+    expect(sum([1, "a", 2, null, 3])).toEqual(6);
+    expect(sum([true, false, 4, "5", 6])).toEqual(10);
+  });
+
+  test("given an array with only non-number values, return the least surprising value given how it behaves for all other inputs", () => {
+    expect(sum(["a", true, null, undefined])).toEqual(0);
+  });
+});
