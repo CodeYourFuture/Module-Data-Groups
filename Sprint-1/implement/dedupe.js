378a74a6fedcf215a68aeb60d2a7e65bba46a266
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-22 13:47:11 +0000

    Data Group Sprint 1 - implement dedupe.js

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,20 @@
-function dedupe() {}
+function dedupe(arr) {
+  const uniqueArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let isDuplicate = false;
+
+    for (let j = 0; j < uniqueArray.length; j++) {
+      if (arr[i] === uniqueArray[j]) {
+        isDuplicate = true;
+      }
+    }
+
+    if (isDuplicate === false) {
+      uniqueArray.push(arr[i]);
+    }
+  }
+
+  return uniqueArray;
+}
+module.exports = dedupe;
