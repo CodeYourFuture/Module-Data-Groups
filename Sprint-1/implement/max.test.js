6e65ea44337eebce4e329d97300ad0e62431b830
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-22 23:34:52 +0000

    Data Group - Sprint 1 - Implement - max.test.js

--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -21,23 +21,73 @@ test.todo("given an empty array, returns -Infinity");
 // Given an array with one number
 // When passed to the max function
 // Then it should return that number
+test.todo("given an array with one number, returns that number");
 
 // Given an array with both positive and negative numbers
 // When passed to the max function
 // Then it should return the largest number overall
+test.todo(
+  "given an array with positive and negative number, returns the largest number overall"
+);
 
 // Given an array with just negative numbers
 // When passed to the max function
 // Then it should return the closest one to zero
+test.todo(
+  "given an array with negative number, returns the largest number which is closest to 0"
+);
 
 // Given an array with decimal numbers
 // When passed to the max function
 // Then it should return the largest decimal number
-
+test.todo(
+  "given an array with decimal numbers, returns the largest decimal number"
+);
 // Given an array with non-number values
 // When passed to the max function
 // Then it should return the max and ignore non-numeric values
-
+test.todo(
+  "given an array with non-number values, returns the max and ignore non-numeric values"
+);
 // Given an array with only non-number values
 // When passed to the max function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test.todo(
+  "given an array with non-number values, returns the least surprising value"
+);
+
+describe("findMax function", () => {
+  test("given an empty array, returns -Infinity", () => {
+    expect(findMax([])).toEqual(-Infinity);
+  });
+
+  test("given an array with one number, returns that number", () => {
+    expect(findMax([5])).toEqual(5);
+    expect(findMax([-10])).toEqual(-10);
+  });
+
+  test("given an array with positive and negative numbers, returns the largest number overall", () => {
+    expect(findMax([-1, 2, 3, -10])).toEqual(3);
+    expect(findMax([-100, 0, 50, -200])).toEqual(50);
+  });
+
+  test("given an array with negative numbers, returns the largest number which is closest to 0", () => {
+    expect(findMax([-5, -2, -8, -10])).toEqual(-2);
+    expect(findMax([-100, -50, -1, -20])).toEqual(-1);
+  });
+
+  test("given an array with decimal numbers, returns the largest decimal number", () => {
+    expect(findMax([1.5, 2.7, 3.9, 3.8])).toEqual(3.9);
+    expect(findMax([-1.1, -0.5, -0.9])).toEqual(-0.5);
+  });
+
+  test("given an array with non-number values, returns the max and ignores non-numeric values", () => {
+    expect(findMax([5, "a", null, 10, undefined, 3])).toEqual(10);
+    expect(findMax(["hello", 2, {}, 7, NaN, 1])).toEqual(7);
+  });
+
+  test("given an array with only non-number values, returns the least surprising value", () => {
+    expect(findMax(["a", {}, null, undefined])).toEqual(-Infinity);
+    expect(findMax([NaN, "hello", {}])).toEqual(-Infinity);
+  });
+});
