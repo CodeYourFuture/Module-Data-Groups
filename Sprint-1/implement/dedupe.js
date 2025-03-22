4f96112c1c5567b8cd5fa9ca1a31e67f0fb4ad9a
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-22 23:25:12 +0000

    Adding comments for readability and self understanding

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -4,12 +4,14 @@ function dedupe(arr) {
   for (let i = 0; i < arr.length; i++) {
     let isDuplicate = false;
 
+    // iterates over uniqueArray and checks if arr[i] is in that array
     for (let j = 0; j < uniqueArray.length; j++) {
       if (arr[i] === uniqueArray[j]) {
         isDuplicate = true;
       }
     }
 
+    // if arr[i] is not already in uniqueArray, add it to uniqueArray
     if (isDuplicate === false) {
       uniqueArray.push(arr[i]);
     }
@@ -17,4 +19,5 @@ function dedupe(arr) {
 
   return uniqueArray;
 }
+dedupe([1, 2, 3, 3, 4, 4, 5, 8, 8]);
 module.exports = dedupe;
