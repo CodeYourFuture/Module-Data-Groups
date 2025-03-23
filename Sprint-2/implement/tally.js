28d73e7260f7df8c18059e51c621b5a573dcd65d
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-23 15:08:07 +0000

    Data Groups|Sprint 2|Implement - tally.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,19 @@
-function tally() {}
+function tally(items) {
+  if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
+
+  const object = {};
+
+  for (let element of items) {
+    if (object[element]) {
+      object[element] += 1;
+    } else {
+      object[element] = 1;
+    }
+  }
+
+  return object;
+}
 
 module.exports = tally;
