47b5dce558ee3ec5b24a51e26b2d369e3a167794
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-23 14:21:22 +0000

    Sprint 2|Data Groups|Implement - lookup.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,11 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+  const lookup = {};
+
+  for (const pair of pairs) {
+    lookup[pair[0]] = pair[1];
+  }
+
+  return lookup;
 }
 
 module.exports = createLookup;
