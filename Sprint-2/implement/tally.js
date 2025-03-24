82cc395700dd5b23473f83e214b33258b37247de
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-24 22:31:26 +0000

    Refactoring code as per Dayeon's feedback to use object bracket notation that takes a variable as a key and logical OR operator

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -6,11 +6,12 @@ function tally(items) {
   const object = {};
 
   for (let element of items) {
-    if (object[element]) {
-      object[element] += 1;
-    } else {
-      object[element] = 1;
-    }
+    // if (object[element]) {
+    //   object[element] += 1;
+    // } else {
+    //   object[element] = 1;
+    // }
+    object[element] = (object[element] || 0) + 1; // Using object bracket notation that takes a variable as a key and logical OR operator
   }
 
   return object;
