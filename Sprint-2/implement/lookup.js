2e85723e98cbe82089447de42336e803004c7bd3
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-24 21:38:41 +0000

    Working on Feedback Refactoring function so when arguments are not an array, or array of arrays is not an array, function throws an error without going through whole code.

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,4 +1,17 @@
 function createLookup(pairs) {
+  //Checking if input is an array and throwing error if it's not
+  if (!Array.isArray(pairs)) {
+    throw new Error("Input must be an array.");
+  }
+
+  // Checking if every element inside the array is also an array (a pair) and throwing error if it's not
+  for (const pair of pairs) {
+    if (!Array.isArray(pair) || pair.length !== 2) {
+      throw new Error(
+        "Each element in the array must be an array with exactly two elements."
+      );
+    }
+  }
   const lookup = {};
 
   for (const pair of pairs) {
