c88f7d26c6cfd75c6f7596826fe40cab73aee3b5
hasandemiroz <64849417+hasandemiroz@users.noreply.github.com>
2025-03-24 01:23:30 +0000

    debug recipe file update and fix

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -3,6 +3,7 @@
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
+// We can use join() method to convert the ingredients array into a string, with each ingredient on a new line
 
 const recipe = {
   title: "bruschetta",
@@ -12,4 +13,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients.join('\n')}`);
