14e327f6bd3226562606b38935fc9c295ec2e4a7
hasandemiroz <64849417+hasandemiroz@users.noreply.github.com>
2025-03-24 01:13:27 +0000

    debug address file update and fix

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -4,12 +4,13 @@
 // but it isn't working...
 // Fix anything that isn't working
 
-const address = {
+const address = [{
   houseNumber: 42,
   street: "Imaginary Road",
   city: "Manchester",
   country: "England",
   postcode: "XYZ 123",
-};
+}];
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address[0].houseNumber}`);
+
