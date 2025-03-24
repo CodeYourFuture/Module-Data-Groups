2035e8212821c28418c3455571599279ea12bf15
Aida Zhukova <aidazhukovaa@gmail.com>
2025-03-24 11:51:25 +0000

    deleted changes for alarmclock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,33 +1,4 @@
-let timeRemainingDisplay = document.getElementById("timeRemaining");
-let stopButton = document.getElementById("stop");
-
-function transformSeconds(seconds) {
-  let min = Math.trunc(seconds / 60);
-  if (min < 10) {
-    min = `0${min}`;
-  }
-  let remainingSeconds = seconds % 60;
-  if (remainingSeconds < 10) {
-    remainingSeconds = `0${remainingSeconds}`;
-  }
-  return `${min}:${remainingSeconds}`;
-}
-56;
-function setAlarm() {
-  const input = document.getElementById("alarmSet").value;
-  let number = Number(input);
-
-  let intervalId = setInterval(() => {
-    let timeInMMSS = transformSeconds(number);
-    timeRemainingDisplay.textContent = `Time Remaining: ${timeInMMSS}`;
-
-    if (number === 0) {
-      playAlarm();
-      clearInterval(intervalId);
-    }
-    number = number - 1;
-  }, 1000);
-}
+function setAlarm() {}
 
 // DO NOT EDIT BELOW HERE
 
