9f85c59579385636a96ccba17ce69d6e8acfbe50
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-24 22:23:48 +0000

    Refactored code to handle encoded query string using decodeURIComponent

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -9,12 +9,15 @@ function parseQueryString(queryString) {
     // const [key, value] = pair.split("=");
     // queryParams[key] = value;
     const index = pair.indexOf("="); // Find the first "="
-    const key = pair.slice(0, index); // Extract key
-    const value = pair.slice(index + 1); // Extract everything after "="
+    // const key = pair.slice(0, index); // Extract key
+    //const value = pair.slice(index + 1); // Extract everything after "="
+    const key = decodeURIComponent(pair.slice(0, index)); // Extract and decode key
+    const value = decodeURIComponent(pair.slice(index + 1)); // Extract and decode value
     queryParams[key] = value;
   }
   return queryParams;
 }
 console.log(parseQueryString("equation=x=y+1"));
+console.log(parseQueryString("a%25b=c%26d"));
 
 module.exports = parseQueryString;
