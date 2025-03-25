266daab8eea344af3b812bcc09cc6152ad2c2ba6
Chi <181338252+Chi-Mb@users.noreply.github.com>
2025-03-25 02:34:19 +0000

    Update sum.js
    Included NaN filter

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,9 +1,6 @@
 function sum(elements) {
-    const sum = (elements) => {
-        const numericalValues = elements.filter(element => typeof element === 'number');
-        return numericalValues.reduce((total, current) => total + current, 0);
-          }
-    
-    }
+  const numericalValues = elements.filter(element => typeof element === 'number' && !isNaN(element));
+  return numericalValues.reduce((total, current) => total + current, 0);
+}
 
 module.exports = sum;
