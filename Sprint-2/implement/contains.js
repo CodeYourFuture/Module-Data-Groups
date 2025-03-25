0b08be9c1d05b3594bdcfe3db7b7e1cfc84ed5d6
AmirMontazeri <mr.amir.mti@gmail.com>
2025-03-25 10:03:07 +0000

    function implementation updated

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,5 +1,9 @@
-function contains(obj , key) {
+function contains(obj, key) {
+    if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
+      throw new Error("Invalid input: First argument must be an object.");
+    }
     return key in obj;
-}
-
-module.exports = contains;
+  }
+  
+  module.exports = contains;
+  
