7e39c5576165d293f21a589eaac318fd869e93b8
Andrei Navumau <tyziaa@gmail.com>
2025-03-28 12:04:23 -0400

    Update Solution and tests for Sprint 1 'fix median' task (#390)
    * Update solution and tests for Sprint 1 'fix' exercise with median
    
    * Add tests to check for errors and mixed arrays (advanced implementation of median)
    
    * Create two vesions of calculate median function: with throw error and with return null
    
    * Remove calculate median with throwing error and related tests

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -11,18 +11,33 @@
 // }
 
 // Explanation:
-// .splice() is a mutating method, so it updates the array in place.
-// to prevent this, you could take a copy of the array and use .splice(),
-// or you could use bracket notation to element at the desired index
+// The original implementation has several mistakes:
+// 1 - .splice() mutates or changes the original array. We don't want this to happen.
+// 2 - We actually don't need .splice() or any other method to find a median.
+// 3 - We must sort the array before we search for the median.
+// 4 - We should check if the 'list' parameter actually has numbers, otherwise try to filter
+// out all non-numeric values and throw an error or return null if there are no numeric values.
+
+
+// Example of a function calculating median and returning null in case no numeric values provided.
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-
-  if (list.length % 2 === 0) { // use the remainder operator (%) to check if the array length is even
-    return (list[middleIndex - 1] + list[middleIndex]) / 2;
+  if (!Array.isArray(list)) {
+    return null;
   }
+  const onlyNumberList = list.filter(item => typeof item === 'number' && !isNaN(item));
 
-  return list[middleIndex];
+  if (!onlyNumberList.length) {
+    return null;
+  } else {
+    const sortedList = onlyNumberList.sort((current, next) => current - next);
+    const indexNearMiddleOfArray = Math.floor(sortedList.length / 2);
+    if (sortedList.length % 2 === 0) {
+      return (sortedList[indexNearMiddleOfArray - 1] + sortedList[indexNearMiddleOfArray]) / 2;
+    } else {
+      return sortedList[indexNearMiddleOfArray];
+    }
+  }
 }
 
 module.exports = calculateMedian;
