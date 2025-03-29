b9eba6463e6f7dd0a1db2e310b7b3dceb6d70c01
MarynaRomashca <marynarmnv@gmail.com>
2025-03-29 14:27:40 +0000

    Maryna Romanova - Sprint 2.0

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,4 @@
-// Predict and explain first...
+// Predict and explain first... - I don't see the return, I think it's a problem. I would change [0] to (houseNumber), it's will show undefined error
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -10,6 +10,10 @@ const address = {
   city: "Manchester",
   country: "England",
   postcode: "XYZ 123",
+
 };
 
 console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address["houseNumber"]}`);
+
+//I had mistake in my prediction, it does't need return because it's not a function. 
