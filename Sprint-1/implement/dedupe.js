04babdc4d1ac9ec747cb54b13771cf3cd5c1dd6d
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-29 12:47:04 +0000

    Refactored code using Set as per reviewer's suggestion

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,23 +1,29 @@
+// function dedupe(arr) {
+//   const uniqueArray = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let isDuplicate = false;
+
+//     // iterates over uniqueArray and checks if arr[i] is in that array
+//     for (let j = 0; j < uniqueArray.length; j++) {
+//       if (arr[i] === uniqueArray[j]) {
+//         isDuplicate = true;
+//       }
+//     }
+
+//     // if arr[i] is not already in uniqueArray, add it to uniqueArray
+//     if (isDuplicate === false) {
+//       uniqueArray.push(arr[i]);
+//     }
+//   }
+
+//   return uniqueArray;
+// }
+// Refactored code using SET
 function dedupe(arr) {
-  const uniqueArray = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    let isDuplicate = false;
-
-    // iterates over uniqueArray and checks if arr[i] is in that array
-    for (let j = 0; j < uniqueArray.length; j++) {
-      if (arr[i] === uniqueArray[j]) {
-        isDuplicate = true;
-      }
-    }
-
-    // if arr[i] is not already in uniqueArray, add it to uniqueArray
-    if (isDuplicate === false) {
-      uniqueArray.push(arr[i]);
-    }
-  }
-
-  return uniqueArray;
+  return [...new Set(arr)]; // Convert Set back to an array
 }
-dedupe([1, 2, 3, 3, 4, 4, 5, 8, 8]);
+
+console.log(dedupe([1, 2, 3, 3, 4, 4, 5, 8, 8]));
+
 module.exports = dedupe;
