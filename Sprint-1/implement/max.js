8cead77c301fa2ddbc96c4e0a4dce4b2e5638d05
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-03-29 12:50:08 +0000

    Refactored code using Math.max

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,19 +1,28 @@
+// function findMax(elements) {
+//   if (elements.length === 0) {
+//     return -Infinity;
+//   }
+
+//   let max = -Infinity;
+
+//   for (let i = 0; i < elements.length; i++) {
+//     if (typeof elements[i] === "number" && !isNaN(elements[i])) {
+//       if (elements[i] > max) {
+//         max = elements[i];
+//       }
+//     }
+//   }
+
+//   return max;
+// }
+
+//Refactored code using Math.max
 function findMax(elements) {
-  if (elements.length === 0) {
-    return -Infinity;
-  }
+  const validNumbers = elements.filter(
+    (num) => typeof num === "number" && !isNaN(num)
+  );
 
-  let max = -Infinity;
-
-  for (let i = 0; i < elements.length; i++) {
-    if (typeof elements[i] === "number" && !isNaN(elements[i])) {
-      if (elements[i] > max) {
-        max = elements[i];
-      }
-    }
-  }
-
-  return max;
+  return validNumbers.length ? Math.max(...validNumbers) : -Infinity;
 }
 
 module.exports = findMax;
