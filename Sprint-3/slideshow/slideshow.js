37362f50e23e9de4eb6e2812e96fe734ab8828d7
Chi <181338252+Chi-Mb@users.noreply.github.com>
2025-04-05 18:25:16 +0100

    Updated the js file

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,3 +6,50 @@ const images = [
 
 
 // Write your code here
+
+
+let currentIndex = 0;
+let intervalId = null;
+
+const imageElement = document.getElementById("carousel-image");
+const backBtn = document.getElementById("back");
+const forwardBtn = document.getElementById("forward");
+const autoBackBtn = document.getElementById("auto-back");
+const autoForwardBtn = document.getElementById("auto-forward");
+const stopBtn = document.getElementById("stop");
+
+function updateImage() {
+  imageElement.src = images[currentIndex];
+}
+
+backBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateImage();
+});
+
+forwardBtn.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateImage();
+});
+
+autoForwardBtn.addEventListener("click", () => {
+  stopSlide();
+  intervalId = setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+  }, 2000);
+});
+
+autoBackBtn.addEventListener("click", () => {
+  stopSlide();
+  intervalId = setInterval(() => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateImage();
+  }, 2000);
+});
+
+stopBtn.addEventListener("click", stopSlide);
+
+function stopSlide() {
+  clearInterval(intervalId);
+}
