4225e942f8f66fdb04299b14ff17f56e2721a6da
hasandemiroz <64849417+hasandemiroz@users.noreply.github.com>
2025-04-05 01:02:45 +0100

    implement | max files update

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,13 @@
 function findMax(elements) {
+    const numbers = elements.filter(element => typeof element === "number");
+    
+    if(numbers.length === 0){
+        return -Infinity;
+    }else if(numbers.length === 1){
+        return numbers[0];
+    }else{
+        return Math.max(...numbers);
+    }
 }
 
 module.exports = findMax;
