23805bda95bd282e2a236bed4c69218d2f0e1e77
Segun <segunfolayan@yahoo.com>
2025-04-20 06:11:15 +0100

    FixingSum.JSBasedOnFeedback

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,18 +1,17 @@
-function sum(elements) {
-    let total = 0;
-  for (let Index=0; Index < elements.length; Index++){
-    if (typeof elements[Index] === "number"){
-        total+=elements[Index];
-    }
-    else {
-        total=total + 0;
-    }
-
-   }
-   return total;
-    
-}
-
 
 
 module.exports =sum;
+
+function sum(elements) {
+    let total = 0;
+    for (let Index = 0; Index < elements.length; Index++) {
+        if (typeof elements[Index] === "number") {
+            total += elements[Index];
+        } else {
+            total = total + 0;
+        }
+    }
+    return parseFloat(total.toFixed(10));
+}
+
+module.exports = sum;
