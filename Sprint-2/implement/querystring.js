588e028d152b68cab9d6e33a74bbc641e96abc77
Segun <segunfolayan@yahoo.com>
2025-04-20 03:50:09 +0100

    ChangesMadeForURLDEcoding

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,15 +1,24 @@
+
+module.exports = parseQueryString;
+
 function parseQueryString(queryString) {
   const queryParams = {};
   if (queryString.length === 0) {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, ...value] = pair.split("=");
-    const newvalue = value.join("="); 
-    queryParams[key] = newvalue;
+    // Decode percent-encoded characters in the query string
+    const decodedPair = decodeURIComponent(pair);
 
+    // Split the decoded pair into key and value
+    const [key, ...value] = decodedPair.split("=");
+    const newValue = value.join("=");
+
+    // Store the decoded key and value in the queryParams object
+    queryParams[key] = newValue;
   }
 
   return queryParams;
