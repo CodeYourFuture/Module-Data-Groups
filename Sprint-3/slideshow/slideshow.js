ab78d672b859164b7ba3e224ba9010d222fb8696
Samunta Sunuwar <153233353+Samunta@users.noreply.github.com>
2025-04-22 00:20:00 +0100

    Adding scope for forward and backward carousel

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,22 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
-
 // Write your code here
+
+let currentIndex = 0;
+
+const carouselImg = document.getElementById("carousel-img");
+carouselImg.setAttribute("src", images[currentIndex]);
+
+document.getElementById("forward-btn").addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  carouselImg.setAttribute("src", images[currentIndex]);
+});
+
+document.getElementById("backward-btn").addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  carouselImg.setAttribute("src", images[currentIndex]);
+});
