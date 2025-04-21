848be8ccc8534839daacb0f10e0fb1f3c19b5297
Priscilla <cpjm0033@gmail.com>
2025-04-21 08:13:25 +0100

    Added a function to render book list

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -1,4 +1,5 @@
 // for the tests, do not modify this array of books
+
 const books = [
   {
     title: "The Design of Everyday Things",
@@ -21,3 +22,30 @@ const books = [
   },
 ];
 
+function readingList(books) {
+  const list = document.getElementById("reading-list");
+
+  books.forEach((book) => {
+    const listItem = document.createElement("li");
+    listItem.className = "book-item";
+    listItem.style.backgroundColor = book.alreadyRead ? "#c8f7c5" : "#f7c5c5";
+
+    const title = document.createElement("h3");
+    title.textContent = book.title;
+
+    const author = document.createElement("p");
+    author.textContent = `by ${book.author}`;
+
+    const image = document.createElement("img");
+    image.src = book.bookCoverImage;
+    image.alt = `${book.title} cover`;
+    image.style.width = "100px";
+
+    listItem.appendChild(image);
+    listItem.appendChild(title);
+    listItem.appendChild(author);
+    list.appendChild(listItem);
+  });
+}
+
+readingList(books);
