3990dd8999c7f673c43f7fa46cc6f66f6f8ac3e2
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-06-10 18:01:53 +0100

    Fix tests: Make median function pass all edge cases

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,21 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
+    if (!Array.isArray(list)) return null;
+     if (list.length === 0) return null;
+
+  const numberOnly = list.sort().filter(element=>typeof element === "number")
+       if (numberOnly.length === 0) return null;
+
+  if(numberOnly.length % 2 ===0){
+const middleIndexsSum = numberOnly.splice((numberOnly.length/2-1),2).reduce((sum,element)=>sum + element,0) 
+const median = middleIndexsSum/2
+return median
+  }
+const middleIndex = Math.floor(numberOnly.length / 2);
+  const median = numberOnly.splice(middleIndex, 1)[0];
   return median;
 }
+
 
 module.exports = calculateMedian;
