8ffa6d2f8ab6b88bd387cf0a1dbf651cb8c9ef79
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2025-06-10 10:48:57 +0100

    update fix

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,22 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  if (!Array.isArray(list)) return null;
+
+  let filtered = list.filter(
+    (element) => typeof element === "number" && !isNaN(element)
+  );
+  if (filtered.length === 0) return null;
+
+  let sorted = filtered.sort((a, b) => a - b);
+  let mid = Math.floor(sorted.length / 2);
+
+  if (sorted.length % 2 === 0) {
+    return (sorted[mid - 1] + sorted[mid]) / 2;
+  } else {
+    return sorted[mid];
+  }
 }
 
+console.log(calculateMedian(["apple",30,20,40,50]))
 module.exports = calculateMedian;
