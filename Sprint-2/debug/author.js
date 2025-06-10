a651677647140fa10db22e4d50889b8bae07d34d
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-06-10 21:53:27 +0100

    fix: use for...in to log object property values correctly

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,20 @@
 // Predict and explain first...
-
+/*
+It will throw an error because of the for loop "const value of author" ,for ... of is used for iterable objects like array,strings...
+and after changing the for... of to for...in the program still currently attempts to log out all the property of the object
+which is 
+         firstName
+         lastName
+         occupation
+         age
+         alive
+but the program is needed to log out all the property values in the object
+=>    Zadie
+      Smith
+      writer
+      40
+      true
+*/
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +26,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
-  console.log(value);
+for (const key in author) {
+  console.log(author[key]);
 }
