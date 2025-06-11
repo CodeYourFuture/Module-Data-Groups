f4e9b5cd133378cda045cd54b43949707960c1de
rivakutu <revivemmapfumo@gmail.com>
2025-06-11 23:22:27 +0200

    Simplifies recipe logging and fixes ingredients

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,15 +1,15 @@
 // Predict and explain first...
+// the ingredients wont be printed at all because the we didnt call it correctly
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
+// by typing recipe.ingredients
 
 const recipe = {
   title: "bruschetta",
   serves: 2,
-  ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
+  ingredients: ["\nolive oil", "tomatoes\n", "salt\n", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves} ${recipe.ingredients}`);
