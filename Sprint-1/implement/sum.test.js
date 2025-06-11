8f80b8225aef92a9ac2d92732a145b5f605dc818
rivakutu <revivemmapfumo@gmail.com>
2025-06-11 15:01:17 +0200

    Updates tests to use `toEqual` instead of `toBe`

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -17,7 +17,7 @@ test("given empty array, returns 0", () => {
   const input = [];
   const expected = 0;
   const result = sum(input);
-  expect(result).toBe(expected);
+  expect(result).toEqual(expected);
 });
 
 // Given an array with just one number
@@ -27,7 +27,7 @@ test("given an array with one number, returns that number", () => {
   const input = [1];
   const expected = 1;
   const result = sum(input);
-  expect(result).toBe(expected);
+  expect(result).toEqual(expected);
 });
 
 // Given an array containing negative numbers
@@ -37,7 +37,7 @@ test("given an array with negative numbers, returns sum", () => {
   const input = [-1, -4, -2, -6];
   const expected = 11;
   const result = sum(input);
-  expect(result).toBe(expected);
+  expect(result).toEqual(expected);
 });
 
 // Given an array with decimal/float numbers
@@ -47,7 +47,7 @@ test("given an array with decimal or float numbers, returns sum", () => {
   const input = [1.3, 1.6, 1.4, 2.1];
   const expected = 6.4;
   const result = sum(input);
-  expect(result).toBe(expected);
+  expect(result).toEqual(expected);
 });
 
 // Given an array containing non-number values
@@ -57,7 +57,7 @@ test("given an array with non numeric numbers, returns sum of numbers", () => {
   const input = [1, 3, "hi", "hello", null, 2];
   const expected = 3.3;
   const result = sum(input);
-  expect(result).toBe(expected);
+  expect(result).toEqual(expected);
 });
 
 // Given an array with only non-number values
@@ -67,5 +67,5 @@ test("given an array with non numeric values, returns undefined", () => {
   const input = ["hi", "hello", null, "hello-you"];
   const expected = undefined;
   const result = sum(input);
-  expect(result).toBe(expected);
+  expect(result).toEqual(expected);
 });
