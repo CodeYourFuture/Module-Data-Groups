3cf0d5d53617bc197da753de782807e7bbaebecb
Rashaad Ebrahim <rmebrahim@gmail.com>
2025-06-11 12:33:21 +0200

    debug section complete

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+  // An object is not iterable and a for of loop cannot be used on the object the way it has below.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +12,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+  // the last variable in the template literal, ${recipe}, will return an object and not the ingredients.
+  // The variable is referring to the entire object not the specific item.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -10,6 +12,4 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves} \ningredients: \n${recipe.ingredients.join('\n')}`);
