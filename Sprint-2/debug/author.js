c2c32a8088b4973405daf3b7345292bd2e569d55
rivakutu <revivemmapfumo@gmail.com>
2025-06-11 23:03:37 +0200

    Revert "Fixes object property logging"
    This reverts commit f1df73a8842b20508deccfbb1cb13662a78f1c01.

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -2,8 +2,7 @@
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
-// the program is not running because author is not an array hence its not iterable
-// the best way is to call them individually using the dot notation
+
 const author = {
   firstName: "Zadie",
   lastName: "Smith",
@@ -12,8 +11,6 @@ const author = {
   alive: true,
 };
 
-console.log(author.firstName);
-console.log(author.lastName);
-console.log(author.occupation);
-console.log(author.age);
-console.log(author.alive);
+for (const value of author) {
+  console.log(value);
+}
