34b19def6ffd76032e7445591138fed084cc7a70
rivakutu <revivemmapfumo@gmail.com>
2025-06-13 18:03:42 +0200

    Improves query string parsing
    Updates the query string parser to correctly handle key-value pairs where the '=' character appears multiple times within the value.
    Now uses `indexOf` to find the first occurrence of '=' to split keys and values.

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -3,14 +3,16 @@ function parseQueryString(queryString) {
   if (queryString.length === 0) {
     return queryParams;
   }
-  const keyValuePairs = queryString.split("&");
+  const keyValuePairs = queryString.split("&"); // if multiple pairs exist split at the
 
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+  for (const keyValuePair of keyValuePairs) {
+    const index = keyValuePair.indexOf("="); // Finding the first occurrence of '='
+    if (index !== -1) {
+      const key = keyValuePair.substring(0, index);
+      const value = keyValuePair.substring(index + 1);
+      queryParams[key] = value;
+    }
   }
-
   return queryParams;
 }
-
 module.exports = parseQueryString;
