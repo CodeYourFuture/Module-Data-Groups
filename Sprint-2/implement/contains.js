0809ad05d1f21641407cbdc6b027271809f72337
rivakutu <revivemmapfumo@gmail.com>
2025-06-15 17:56:20 +0200

    The function now validates the input to ensure it is a valid object.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,10 @@
-function contains() {}
+function contains(obj, propertyName) {
+  // Validating if input is an object
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    throw new TypeError("Input must be an object");
+  }
 
+  // checking if property exists inside the object
+  return propertyName in obj;
+}
 module.exports = contains;
