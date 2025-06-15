6e6aebce348e5eae414a97cb862603407b58e88d
rivakutu <revivemmapfumo@gmail.com>
2025-06-15 18:35:11 +0200

    Implements lookup creation

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,15 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  if (!Array.isArray(countryCurrencyPairs)) {
+    throw new TypeError("Input must be country: currency pairs");
+  }
+
+  return countryCurrencyPairs.reduce((lookup, [country, currency]) => {
+    if (typeof country !== "string" || typeof currency !== "string") {
+      throw new TypeError("Only strings entries are allowed");
+    }
+    lookup[country] = currency;
+    return lookup;
+  }, {});
 }
 
 module.exports = createLookup;
