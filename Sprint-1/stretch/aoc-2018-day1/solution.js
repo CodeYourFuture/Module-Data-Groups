5d1e218daeb55747c489abee3897cd33733d17b1
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-06-16 13:05:22 +0100

    chore: solved Advent of Code challenge

---
+++ Sprint-1/stretch/aoc-2018-day1/solution.js
@@ -0,0 +1,6 @@
+
+const fs = require('fs');
+
+const data = fs.readFileSync('./input.txt', { encoding: 'utf8', flag: 'r' });
+const sumNumFileArray = data.split("\n").reduce(((sum,num)=> sum =sum + Number(num)),0)
+console.log(sumNumFileArray);//595
