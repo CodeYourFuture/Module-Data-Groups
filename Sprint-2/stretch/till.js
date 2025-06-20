7fb94314f1123250503627b698d0c6b74ae3a9a4
rivakutu <revivemmapfumo@gmail.com>
2025-06-20 14:05:42 +0200

    function now performs computations accurately

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -6,12 +6,15 @@
 
 function totalTill(till) {
   let total = 0;
-
+if (typeof till !== "object" || Array.isArray(till) || till === null) {
+  throw new Error("Input must be an object");
+}
   for (const [coin, quantity] of Object.entries(till)) {
-    total += coin * quantity;
+    const valueInPence = Number(coin.replace("p", ""));
+    total += valueInPence * quantity;
   }
 
-  return `£${total / 100}`;
+  return `£${(total / 100).toFixed(2)}`;
 }
 
 const till = {
@@ -23,9 +26,14 @@ const till = {
 const totalAmount = totalTill(till);
 
 // a) What is the target output when totalTill is called with the till object
+// £4.40
 
 // b) Why do we need to use Object.entries inside the for...of loop in this function?
+//Object.entries() is used to loop over both the keys and values of an object
 
 // c) What does coin * quantity evaluate to inside the for...of loop?
+//  coin * quantity evaluates to Nan(Not A Number)
 
 // d) Write a test for this function to check it works and then fix the implementation of totalTill
+
+module.exports = totalTill;
