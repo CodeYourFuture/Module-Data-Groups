07bea5b50b1321c55c7242ff61e367e0c770cb8c
TarasMykytiuk <tarasmykytiukw@gmail.com>
2025-06-20 19:31:01 +0300

    sprint_1 folder fix

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,46 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
+  //if list not and array return null, 
+  // this check first because no sense to check else if list not array
+  if (!(list instanceof Array)) {
+    return null;
+  }
+  // if no numbers in list return null
+  if (!containsNumbersCheck(list)) {
+    return null;
+  }
+  let sortedCopy = makeSortedCopyOfArr(list);
+  let middleIndex = Math.floor(sortedCopy.length / 2);
+  let median;
+  //to avoid modification of original copy off list created in separate function
+
+  if (sortedCopy.length % 2 == 1) {
+    median = sortedCopy.splice(middleIndex, 1)[0];
+  } else {// if list length not even number
+    median = (sortedCopy[middleIndex] + sortedCopy[middleIndex - 1]) / 2;
+  }
   return median;
+}
+
+function makeSortedCopyOfArr(arr) {
+  let sortedCopy = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == 'number') {
+      sortedCopy.push(arr[i]);
+    }
+  }
+  sortedCopy.sort();
+  return sortedCopy;
+}
+
+function containsNumbersCheck(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof (arr[i]) == 'number') {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = calculateMedian;
