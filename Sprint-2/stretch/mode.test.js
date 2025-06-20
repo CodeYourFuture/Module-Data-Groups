3bc0e1e1b78d878cd60e192ab04c43f6cfbc7444
rivakutu <revivemmapfumo@gmail.com>
2025-06-20 11:36:27 +0200

    more edge cases implemented

--- Sprint-2/stretch/mode.test.js
+++ Sprint-2/stretch/mode.test.js
@@ -13,20 +13,20 @@ const calculateMode = require("./mode.js");
 
 describe("calculateMode()", () => {
   test("returns the most frequent number in an array", () => {
-    const nums = [2, 4, 1, 2, 3, 2, 1];
-
-    expect(calculateMode(nums)).toEqual(2);
+    expect(calculateMode([2, 4, 1, 2, 3, 2, 1])).toEqual(2);
+    expect(calculateMode([4, 4, 1, 2, 3, 2, 4])).toEqual(4);
+    expect(calculateMode([3, 4, 1, 5, 3, 1, 1])).toEqual(1);
   });
 
   test("returns the first mode in case of multiple modes", () => {
-    const nums = [1, 2, 2, 3, 3];
-
-    expect(calculateMode(nums)).toEqual(2);
+    expect(calculateMode([1, 2, 2, 3, 3])).toEqual(2);
+    expect(calculateMode([1, 1, 2, 7, 7])).toEqual(1);
+    expect(calculateMode([1, 5, 5, 3, 3])).toEqual(5);
   });
 
   test("ignores non-number values", () => {
-    const nums = [1, 3, "2", 2, 3, null];
-
-    expect(calculateMode(nums)).toEqual(3);
+    expect(calculateMode([1, 3, "2", 2, 3, null])).toEqual(3);
+    expect(calculateMode([1, 3, "2", undefined, 3, null])).toEqual(3);
+    expect(calculateMode(["2", undefined, 1, null])).toEqual(1);
   });
 });
