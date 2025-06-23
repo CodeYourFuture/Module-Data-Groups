644c523daf8631ce7fe4ffea17e72db35a2dc5c1
Nataliia74 <volkov.nataliia@gmail.com>
2025-06-23 22:13:24 +0100

    findMax function and testing it

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,24 @@
 function findMax(elements) {
+  if (!Array.isArray(elements)) {
+    return null;
+  }
+  const new_elements = elements.filter(
+    (item) => typeof item === "number" && !isNaN(item)
+  );
+  if (new_elements.length === 0) {
+    return -Infinity;
+  }
+  let max_element = new_elements[0];
+  for (let i = 1; i < new_elements.length; i++) {
+    if (new_elements[i] > max_element) {
+      max_element = new_elements[i];
+    }
+  }
+  console.log(max_element);
+  return max_element;
 }
+
+const arr = [1, 3, 5, 7];
+console.log(findMax(arr));
 
 module.exports = findMax;
