580aede1e3b7fb39c8b0f4220f3b9a5d5c1a669e
Nataliia74 <volkov.nataliia@gmail.com>
2025-06-23 19:48:03 +0100

    programm with function calculateMedian()

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,32 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  if (!Array.isArray(list)) {
+    // check whether the input is array
+    return null;
+  }
+  const new_list = list.filter(
+    (item) => typeof item === "number" && !isNaN(item) // filter input and create an array that include just numbers
+  );
+  if (new_list.length === 0) {
+    // check if array is empty, excluded any non-numeric items
+    return null;
+  }
+  const sortedList = new_list.slice().sort((a, b) => a - b); //sort elements inside array in ascending order to calculate median
+  const middleIndex = Math.floor(sortedList.length / 2); // calculate middle index array element
+  if (sortedList.length % 2 === 0) {
+    //check if array has even quantity elements
+    const median = (sortedList[middleIndex - 1] + sortedList[middleIndex]) / 2; // calculate median
+    return median;
+  } else {
+    return sortedList[middleIndex];
+  }
 }
+
+const arr = ["apple", null, undefined];
+calculateMedian(arr);
+console.log(arr);
+
+console.log(calculateMedian(arr));
 
 module.exports = calculateMedian;
