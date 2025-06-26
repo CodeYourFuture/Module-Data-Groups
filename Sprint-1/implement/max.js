a9dab4a646350e4c711c27886139f3dd48c814e2
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-26 21:27:20 +0100

    added a comment

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,8 +1,11 @@
 function findMax(elements) {
     //if element is empty returns infinity
-    if(elements=[]) return-Infinity;
+    if(elements.length===0) return-Infinity;
+    //if the array has only one number it returns the number
+    if(elements=>typeof elements==="number"&& elements.length===1) return elements[0]
     //excludes all non-numbers and sorts the array in ascending order
     const omnlyNumbers=elements.filter(item=>typeof item==="number").sort((a,b)=>a-b);
+    
     //it retrieves the last element of the array
     const largestNumber=omnlyNumbers.splice(omnlyNumbers.length-1)[0];
     return largestNumber;
