3ea82dcdd13313a654594b08d3d2a4d5b03d74ae
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-26 21:53:03 +0100

    changed an arrow function to a condition

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -2,7 +2,7 @@ function findMax(elements) {
     //if element is empty returns infinity
     if(elements.length===0) return-Infinity;
     //if the array has only one number it returns the number
-    if(elements=>typeof elements==="number"&& elements.length===1) return elements[0]
+    if(typeof elements[0]==="number"&& elements.length===1) return elements[0]
     //excludes all non-numbers and sorts the array in ascending order
     const omnlyNumbers=elements.filter(item=>typeof item==="number").sort((a,b)=>a-b);
     
@@ -10,6 +10,6 @@ function findMax(elements) {
     const largestNumber=omnlyNumbers.splice(omnlyNumbers.length-1)[0];
     return largestNumber;
 }
-console.log(findMax([9,4,-106]))
-
+console.log(findMax([-9,-45,-6]))
+//findMax([-9, -45, -6]);
 module.exports = findMax;
