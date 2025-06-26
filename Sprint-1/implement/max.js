e19878f6fc76dc98e182c88f255b9f10d3861490
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-26 20:13:48 +0100

    wrote the function to filter out non-numbers and find the maximum element of the array

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,8 @@
 function findMax(elements) {
+    const omnlyNumbers=elements.filter(item=>typeof item==="number").sort((a,b)=>a-b);
+    const largestNumber=omnlyNumbers.splice(omnlyNumbers.length-1)[0];
+    return largestNumber;
 }
+console.log(findMax([9,4,"r"]))
 
 module.exports = findMax;
