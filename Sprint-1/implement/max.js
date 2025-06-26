77a1518f96b6670dbfe07792e7369352e1fb1a1f
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-26 20:16:11 +0100

    added comments

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,5 +1,7 @@
 function findMax(elements) {
+    //excludes all non-numbers and sorts the array in ascending order
     const omnlyNumbers=elements.filter(item=>typeof item==="number").sort((a,b)=>a-b);
+    //it retrieves the last element of the array
     const largestNumber=omnlyNumbers.splice(omnlyNumbers.length-1)[0];
     return largestNumber;
 }
