19934d34fc2ff50adce227823d2c935ad29ab2a6
Malusi Skunyana <malusiskunyana@gmail.com>
2025-06-26 22:13:55 +0200

    wrote tests for sum function

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,41 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test("sum([]) should return 0", () => {
+  expect(sum([])).toBe(0);
+});
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
+test("sum([5]) should return 5", () => {
+  expect(sum([5])).toBe(5);
+});
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
+test("sum([-5, -10, -15]) should return -30", () => {
+  expect(sum([-5, -10, -15])).toBe(-30);
+});
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
+test("sum([1.5, 2.5, 3.5]) should return 7.5", () => {
+  expect(sum([1.5, 2.5, 3.5])).toBe(7.5);
+});
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+test("sum(['hey', 10, 'hi', 60, 10]) should return 80", () => {
+  expect(sum(['hey', 10, 'hi', 60, 10])).toBe(80);
+});
 
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test("sum(['hey', 'hi']) should return 0", () => {
+  expect(sum(['hey', 'hi'])).toBe(0);
+});
