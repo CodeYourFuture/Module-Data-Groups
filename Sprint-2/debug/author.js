d80a603f8824033cc04aa4d0fe24cd1fa85ca858
Nataliia74 <volkov.nataliia@gmail.com>
2025-06-26 22:54:10 +0100

    debug tasks

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,9 @@
 // Predict and explain first...
 
+// Firstly I think that I can get access to object values as I usually get to the array elements.
+// But I got error. Now I understood and learnt that to get access to object properties we need special
+// use method Object.keys, Object.values, Object.entries (to access both).
+
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +15,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,9 @@
 // Predict and explain first...
 
+// Here we have nested object, to be precise array under key ingredients.
+// To get access to array we use dot notation with ingredients key. But each ingredient
+// should be on a separate line. That mean that we need loop through this array.
+
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -11,5 +15,11 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+    ingredients: `);
+for (const ingredient of recipe.ingredients) {
+  console.log(`${ingredient}`);
+}
+
+// console.log(`${recipe.title} serves ${recipe.serves}
+// ingredients:
+// ${ingredient}`);
