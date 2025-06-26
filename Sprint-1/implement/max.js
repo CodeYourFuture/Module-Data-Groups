cc668a4c120dcec168a3e362bfcc3660e3ed6229
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-26 22:11:15 +0100

    added a comment

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,14 +1,16 @@
 function findMax(elements) {
-    //if element is empty returns infinity
-    if(elements.length===0) return-Infinity;
-    //if the array has only one number it returns the number
-    if(typeof elements[0]==="number"&& elements.length===1) return elements[0]
-    //excludes all non-numbers and sorts the array in ascending order
-    const omnlyNumbers=elements.filter(item=>typeof item==="number").sort((a,b)=>a-b);
-    
-    //it retrieves the last element of the array
-    const largestNumber=omnlyNumbers.splice(omnlyNumbers.length-1)[0];
-    return largestNumber;
+  //if element is empty returns infinity
+  if (elements.length === 0) return -Infinity;
+  //excludes all non-numbers and sorts the array in ascending order
+  const omnlyNumbers = elements
+    .filter((item) => typeof item === "number")
+    .sort((a, b) => a - b);
+    //if the array has only one element and is a number it prints that number
+    if (typeof omnlyNumbers[0] === "number" && omnlyNumbers.length === 1)
+      return omnlyNumbers[0];
+  //it retrieves the last element of the array
+  const largestNumber = omnlyNumbers.splice(omnlyNumbers.length - 1)[0];
+  return largestNumber;
 }
 console.log(findMax([-9,-45,-6]))
 //findMax([-9, -45, -6]);
