aed56e7c6059ba14fac01cb39cd1c0c2a58a9b80
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-27 10:58:57 +0100

    if the array is empty it returns an empty array also it checks if it is an array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,11 @@
-function dedupe() {}
+function dedupe(duplicate) {
+if (!Array.isArray(duplicate)) return "not an array";
+if(duplicate.length===0)return []
+
+}
+console.log(dedupe([]))
+
+
+
+
+module.exports = dedupe;
