c5af19a535deca2e851b06328f52fb2138862ccc
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-27 11:40:01 +0100

    declared a variable with the same  value of duplicate but with only unique elements,checked if the array has any duplicates and if it doesnt it returns a copy of itself,added comments

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,6 +1,12 @@
 function dedupe(duplicate) {
+    //it checks that the parameter is an array
 if (!Array.isArray(duplicate)) return "not an array";
-if(duplicate.length===0)return []
+//if the array is empty it returns an empty array
+if(duplicate.length===0)return [];
+//declares a variable with the same  value of duplicate but with only unique elements
+const uniqueDuplicate=[...new Set(duplicate)];
+//it checks if the array has any duplicates and if it doesnt it returns a copy of itself
+if(uniqueDuplicate.length===duplicate.length)return [...duplicate];
 
 }
 console.log(dedupe([]))
