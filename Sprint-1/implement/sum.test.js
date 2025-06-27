a3a9acc83025d561619d95e04e852f79443e56c3
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-27 08:52:29 +0100

    test sum with single number array

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -20,6 +20,9 @@ expect(sum([])).toBe(0);
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
+it("array with just one number returns that number",()=>{
+    expect(sum([1])).toBe(1);
+})
 
 // Given an array containing negative numbers
 // When passed to the sum function
