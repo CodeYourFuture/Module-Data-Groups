d94f27b57394ba1a65629269195435d07d827342
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-27 11:58:24 +0100

    checks if the array has any duplicates if it does it returns an array which contains only unique elements without duplicates

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -6,8 +6,12 @@ if(duplicate.length===0)return [];
 //declares a variable with the same  value of duplicate but with only unique elements
 const uniqueDuplicate=[...new Set(duplicate)];
 //it checks if the array has any duplicates and if it doesnt it returns a copy of itself
-if(uniqueDuplicate.length===duplicate.length)return [...duplicate];
-
+if(uniqueDuplicate.length===duplicate.length){
+    return [...duplicate];
+// if it does it returns an array which contains only unique elements without duplicates
+} else{
+    return uniqueDuplicate;
+}
 }
 console.log(dedupe([]))
 
