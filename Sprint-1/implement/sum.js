a41f828728befedec0806fc7d0d9fc965d13ac8b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-27 09:45:22 +0100

    added !Number.isNaN(item) to line 6

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,12 +1,14 @@
 function sum(elements) {
   //it checks if the parameter is an array, if not it returns the message 'not an array'
   if (!Array.isArray(elements)) return "not an array";
-  //it filters out all non-numbers
-  const elementsFiltered = elements.filter((item) => typeof item ==="number");
+  
+  //it filters out all non-numbers including NaN
+  const elementsFiltered = elements.filter((item) => typeof item ==="number"&& !Number.isNaN(item));
   //if the array is empty it returns 0
   if (elementsFiltered.length === 0) return 0;
   //if the array has only one number it returns that number
   if (elementsFiltered.length === 1) return elementsFiltered[0];
+  
   //declares a variable and sets its value to 0
   let sumArray=0;
   //goes through every element of the array and adds it up
