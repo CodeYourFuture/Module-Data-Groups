76b624de0f1737d7b5a2239a5b1b3e99d54d182e
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-27 08:54:08 +0100

    return number when array has a single number

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,6 +1,12 @@
 function sum(elements) {
-    if(!Array.isArray(elements))return "not an array";
-    if(elements.length===0)return 0;
+  //it checks if the parameter is an array, if not it returns the message 'not an array'
+  if (!Array.isArray(elements)) return "not an array";
+  //it filters out all non-numbers
+  const elementsFiltered = elements.filter((item) => typeof item == "number");
+  //if the array is empty it returns 0
+  if (elementsFiltered.length === 0) return 0;
+  //if the array has only one number it returns that number
+  if (elementsFiltered.length === 1) return elementsFiltered[0];
 }
 
 module.exports = sum;
