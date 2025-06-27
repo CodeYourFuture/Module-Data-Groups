88a50bf6a841078e66d06fe01fdd8587423a3a57
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-27 13:57:49 +0100

    replaced the for loop with a for..of loop

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -2,12 +2,7 @@
 
 function includes(list, target) {
   //keeps looping through list
-  for (let index = 0; index < list.length; index++) {
-    //declares a new variable each time it goes through the loop 
-    // called element and it assigns it the value of the index which the loop 
-    //goes through at that time
-    const element = list[index];
-    //it checks if any of the indexes in the array is identical to the target
+  for (const element of list){
     if (element === target) {
       return true;
     }
