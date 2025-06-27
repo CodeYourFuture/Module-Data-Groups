322348f9821f420fc5b6e612f0ca7aebec33dec0
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-27 13:39:54 +0100

    added comments

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,8 +1,13 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
+  //keeps looping through list
   for (let index = 0; index < list.length; index++) {
+    //declares a new variable each time it goes through the loop 
+    // called element and it assigns it the value of the index which the loop 
+    //goes through at that time
     const element = list[index];
+    //it checks if any of the indexes in the array is identical to the target
     if (element === target) {
       return true;
     }
