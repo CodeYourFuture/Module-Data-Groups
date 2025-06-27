8d2d7bd48a87cd99f13f2c88e78591558c18937e
Reza Jahanimir <jahanimir.r@gmail.com>
2025-06-27 11:40:09 +0100

    removes deduplicate  elements in a array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,22 @@
-function dedupe() {}
+function dedupe(array) {
+  // Validate input
+  if (!Array.isArray(array)) return [];
+
+  // set is a collection of unique values
+  // It will automatically handle duplicates for us
+  const seen = new Set(); // {}
+  const result = [];
+
+  // Iterate through the array
+  // If the item is not in the set, add it to both the set and the result array
+  for (const item of array) {
+    if (!seen.has(item)) {
+      seen.add(item);
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+module.exports = dedupe;
