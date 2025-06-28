4b0a6c4b3e980478aece5cbbbb1c16db99f32e40
sheida-shab <shabankary@gmail.com>
2025-06-28 12:48:02 +0100

    implement small function

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -5,10 +5,37 @@
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
+
+function isOddItems(listLength){
+  if (listLength % 2 === 0) {
+    return false;
+  } else {
+   return true;
+  }
+}
+
+function calculateMedianOddCount(oddList){
+  const middleIndex = Math.floor(oddList.length / 2);
+  return oddList.splice(middleIndex, 1)[0];
+   
+}
+
+function calculateMedianEvenCount(evenList) {
+  const mid1 = evenList[evenList.length / 2 - 1];
+  const mid2 = evenList[evenList.length / 2];
+  return (mid1 + mid2) / 2;
+  
+}
+
+
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  list=list.sort();
+  if (isOddItems(list.length)){
+   return calculateMedianOddCount(list);
+  }
+  else{
+    return calculateMedianEvenCount(list);
+  }
 }
 
 module.exports = calculateMedian;
