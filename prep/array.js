808cd40e0a68bb4b797d50c21ee7ec811a69ceec
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-28 09:56:32 +0100

    Declare array variables.

---
+++ prep/array.js
@@ -0,0 +1,9 @@
+// declaring array literal as follows
+
+const items = [4.6, 5.9, 6.99];
+
+// In JavaScript, we can use square bracket notation to access specific elements in the array using an index.
+
+items[0]; // evaluates to 4.6
+items[1]; // evaluates to 5.9
+items[2]; // evaluates to 6.99
