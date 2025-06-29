78dc8009b02f386f37e7bf1e4d4e275475e3c813
Nataliia74 <volkov.nataliia@gmail.com>
2025-06-29 09:07:43 +0100

    tally function and testing it

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,19 @@
-function tally() {}
+function tally(arr) {
+  let object = {};
+  if (!Array.isArray(arr)) {
+    throw new Error("This is not an array");
+  } else if (arr.length < 1) {
+    return {};
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      const some_value = arr[i];
+      object[some_value] = (object[some_value] || 0) + 1;
+    }
+    return object;
+  }
+}
+
+const result = tally([1, 2, 3, 1, 2, 3, 6]);
+console.log(result);
 
 module.exports = tally;
