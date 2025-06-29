44ac4ae51b39a3970d5c2af331d665b772b014e9
mo-muchunu <mo.muchunu@gmail.com>
2025-06-29 18:09:13 +0100

    Implement 'findMax' function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,13 @@
 function findMax(elements) {
+    if (!Array.isArray(elements)) return -Infinity; // checks if input is an array.
+
+    
+    const numbersOnly = elements.filter( // Filter out non-numeric values
+        item => typeof item === "number" && !isNaN(item) // Ensures all elements are numbers not strings, objects, NaN
+    );
+
+    // Use Math.max with spread operator to find the largest number
+    return numbersOnly.length > 0 ? Math.max(...numbersOnly) : -Infinity;
 }
 
 module.exports = findMax;
