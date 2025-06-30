06e0e0b4c04d127d692d15d5ae921d84f28ae4bf
mo-muchunu <mo.muchunu@gmail.com>
2025-06-30 03:03:21 +0100

    Implemented 'dedupe' function to remove duplicates from array while preserving order

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,21 @@
-function dedupe() {}
+function dedupe(values) {
+    // Check if the input is an array- if not returns empty array to avoid errors.
+    if (!Array.isArray(values)) return [];
+
+    const seen = new Set();        // Using Set to track values that have already been seen.
+    const result = [];             // Empty array to store values without duplicates.
+
+    for (const value of values) {  // Iterate through each value in the input array.
+        if (!seen.has(value)) {    // Check if the value has not already been seen/ not in the Set yet
+            seen.add(value);       // Mark value as seen and add to the Set if new.
+            result.push(value);    // Add it to the result array, keeping only the first time a unique item appears.
+        }
+    }
+
+    return result;                 // After looping through the whole array, return a new array with duplicates removed, preserving order.
+
+}
+module.exports = dedupe;
+
+
+
