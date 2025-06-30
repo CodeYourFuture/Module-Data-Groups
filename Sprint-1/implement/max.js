f62de65cd784c5a6085f6486e194d6fe1f30169b
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-30 15:35:49 +0100

    max.js implement

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,24 @@
 function findMax(elements) {
+  // check if the input is an array; if not return -Infinity
+  if (!Array.isArray(elements)) return -Infinity;
+
+  //initialize max with the smallest possible number value
+  let max = -Infinity;
+
+  //loop through each element in the array
+  for (let i = 0; i < elements.length; i++) {
+    const num = elements[i];
+
+    // check if the current element is finite number
+    if (typeof num === "number" && Number.isFinite(num)) {
+      //update max if the current number is greater than the current max
+      if (num > max) {
+        max = num;
+      }
+    }
+  }
+  //return max number if found or -Infinite if non found
+  return max;
 }
 
 module.exports = findMax;
