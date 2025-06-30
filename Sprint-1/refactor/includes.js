8765a90ba8ea1a33bd8f196e12d2aa7832a75364
mo-muchunu <mo.muchunu@gmail.com>
2025-06-30 04:14:03 +0100

    Refactored 'includes' function for readability

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,22 @@
 // Refactor the implementation of includes to use a for...of loop
 
+// Implementation of includes using the built-in Array.includes method.
+// Simplified- by reducing the amount of code, offers improved readability as it's clear and easy to understand.
+
+function includes(list, target) {   // Takes two parameters--'list'- an array to search, 'target'- value to check for
+  return list.includes(target);     // 'true' if `target` is found, and 'false' if not.
+}
+
+module.exports = includes;
+
+/*
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
+  for (const item of list) {
+    if (item === target) {
       return true;
     }
   }
   return false;
 }
+*/
 
-module.exports = includes;
