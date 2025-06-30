1c8c954e77c6078655df4a45c06deac121738d9f
mo-muchunu <mo.muchunu@gmail.com>
2025-06-30 01:37:43 +0100

    Implemented 'sum' function to handle various  inputs.

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,10 @@
 function sum(elements) {
-}
+    if (!Array.isArray(elements)) // If input is not an array, return 0.
+        return 0; 
 
+  return elements // Filter to include only real numbers and then sum them
+    .filter(item => typeof item === "number" && !isNaN(item))
+    .reduce((acc, curr) => acc + curr, 0); // Using reduce to add up the filtered numbers, starting from 0.
+}
 module.exports = sum;
+  
