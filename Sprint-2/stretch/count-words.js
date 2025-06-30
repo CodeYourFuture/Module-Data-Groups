12f733539b87778c49b41c2960a8c19c4597437c
Nataliia74 <volkov.nataliia@gmail.com>
2025-06-30 13:45:43 +0100

    count-words function

--- Sprint-2/stretch/count-words.js
+++ Sprint-2/stretch/count-words.js
@@ -26,3 +26,23 @@
 
 3. Order the results to find out which word is the most common in the input
 */
+function countWords(str) {
+  let obj = {}; // create an empty object
+
+  const arr = str
+    .toLowerCase() // make a string lowercase completely
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "") // delete any regex symbols from strings
+    .split(" "); // convert string to an array by splitting the string into words by using " " as a delimiter
+
+  for (const word of arr) {
+    // looping through the array
+    obj[word] = (obj[word] || 0) + 1; // fill up the empty object with keys (words) and values (the number of times the word appears in the value)
+  }
+  let entries = Object.entries(obj); // convert oject into an array to be able to sort it
+  let sorted_obj = entries.sort((a, b) => b[1] - a[1]); // sorting entries in descending order
+
+  return Object.fromEntries(sorted_obj); // convert the sorted array into object again
+}
+
+const str = "we meet Dan,,, to meet Dan Dan dan,";
+console.log(countWords(str));
