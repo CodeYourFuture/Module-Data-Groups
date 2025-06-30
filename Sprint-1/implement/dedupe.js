e51b6aff1bd4c5102716f0faa6758529a5e20ce8
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-06-30 12:23:28 +0100

    dedupe.js Implement

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,21 @@
-function dedupe() {}
+function dedupe(arr) {
+    const result = [];
+    let resultArray = 0;
+
+for (let i= 0; i< arr.length; i++){
+    let isDuplicate = false;
+
+for (let j = 0; j < resultArray; j++){
+    if(arr[i]===result[j]) {
+        isDuplicate = true;
+        break;
+    }
+} 
+if (!isDuplicate) {
+    result[resultArray] = arr[i];
+    resultArray++;
+}   
+} 
+return result;   
+}
+module.exports = dedupe;
