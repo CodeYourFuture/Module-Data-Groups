897cf60dc1b0fa480d5429677f8fc9e34aeacd63
galyna-k <hkzlvs@gmail.com>
2025-07-01 22:30:43 +0100

    Implement contains function and add comprehensive tests
    - add the contains function to check if a key exists in an object;
    - create test  cases  to cover various scenarios, including empty objects, missing keys, and invalid input types

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(obj, key) {
+  if (obj && typeof obj === "object") {
+    return key in obj;
+  }
+  return false;
+}
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,20 +16,58 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
+describe("containsKey", () => {
+  test("given a contains function, should return true if the object contains the property, false otherwise", () => {
+    const currentOutput = contains({ name: "John", surname: "White" }, "surname");
+    const targetOutput = true;
 
-// Given an empty object
-// When passed to contains
-// Then it should return false
-test.todo("contains on empty object returns false");
+    expect(currentOutput).toBe(targetOutput);
+  });
 
-// Given an object with properties
-// When passed to contains with an existing property name
-// Then it should return true
+  // Given an empty object
+  // When passed to contains
+  // Then it should return false
+  test("contains on empty object returns false", () => {
+    const currentOutput = contains({}, "a");
+    const targetOutput = false;
 
-// Given an object with properties
-// When passed to contains with a non-existent property name
-// Then it should return false
+    expect(currentOutput).toBe(targetOutput);
+  });
 
-// Given invalid parameters like an array
-// When passed to contains
-// Then it should return false or throw an error
+  // Given an object with properties
+  // When passed to contains with an existing property name
+  // Then it should return true
+  test("should returns true when the object contains the property", () => {
+    const currentOutput = contains({ a: 1, b: 2, c: 3 }, "c");
+    const targetOutput = true;
+
+    expect(currentOutput).toBe(targetOutput);
+  });
+
+  // Given an object with properties
+  // When passed to contains with a non-existent property name
+  // Then it should return false
+  test("should returns false when the object doesn't exist in the property", () => {
+    const currentOutput = contains({ a: 1, b: 2, c: 3 }, "d");
+    const targetOutput = false;
+
+    expect(currentOutput).toBe(targetOutput);
+  });
+
+  // Given invalid parameters like an array
+  // When passed to contains
+  // Then it should return false or throw an error
+  [
+    { input: [[], "a"], expected: false },
+    { input: [null, "a"], expected: false },
+    { input: [undefined, "a"], expected: false },
+    { input: [123, "a"], expected: false },
+    { input: ["string", "a"], expected: false },
+  ].forEach(({ input, expected }) => {
+    it(`returns ${expected} when called with ${JSON.stringify(
+      input[0]
+    )} and key '${input[1]}'`, () => {
+      expect(contains(input[0], input[1])).toBe(expected);
+    });
+  });
+});
