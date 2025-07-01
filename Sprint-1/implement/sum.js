9009a3025b2701f60dd464ba9d4cfaf8c5fc5378
AdnaanA <thetriplex247@gmail.com>
2025-07-01 18:04:29 +0100

    modified:   Sprint-1/implement/sum.js 	modified:   Sprint-1/implement/sum.test.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,22 @@
 function sum(elements) {
+    if (elements.length === 0) {
+        return 0; // Return 0 for an empty array
+    }
+
+    // Filter out non-numeric elements
+    elements = elements.filter(element => typeof element === 'number');
+    
+    if (elements.length === 0) {
+        return 0; // If all elements are non-numeric, return 0
+    }
+
+    // Sum the numeric elements
+    let total = 0;
+    for (let i = 0; i < elements.length; i++) {
+        total += elements[i];
+    }
+
+    return total; // Return the total sum
 }
 
 module.exports = sum;
