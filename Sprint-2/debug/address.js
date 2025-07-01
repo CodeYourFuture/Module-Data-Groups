33de38f685ef5653f63c32e4916f36330a9fe63c
Reza Jahanimir <jahanimir.r@gmail.com>
2025-07-01 11:04:55 +0100

    This code log out the houseNumber from the address object

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,9 @@
 // Predict and explain first...
+// inside the string template the way the calling the dic is different.
+// The code is trying to access the `houseNumber` property of the `address` object
+// using an array index notation, which is incorrect.
+// Instead, it should use dot notation or bracket notation with the property name as a string.
+
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +17,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address["houseNumber"]}`);
