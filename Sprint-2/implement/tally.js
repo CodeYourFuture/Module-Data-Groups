bca87b3fbcaf5cea19ab723e1622a6d28d0bd84f
Reza Jahanimir <jahanimir.r@gmail.com>
2025-07-01 11:16:22 +0100

    a function that will take a list of items and count the frequency of each item

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,30 @@
-function tally() {}
+function tally(items) {
+  if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
+
+  const result = {};
+
+  // Build array of unique items
+  const uniqueItems = [...new Set(items)];
+
+  // Loop through each unique item
+  for (let i = 0; i < uniqueItems.length; i++) {
+    const currentItem = uniqueItems[i];
+    let count = 0;
+
+    // Count how many times it appears in the original array
+    for (let j = 0; j < items.length; j++) {
+      if (items[j] === currentItem) {
+        count++;
+      }
+    }
+
+    // Add the item and count to result
+    result[currentItem] = count;
+  }
+
+  return result;
+}
 
 module.exports = tally;
