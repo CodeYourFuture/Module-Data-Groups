703e4f3bc00b18bc29ce7b3b2fe0e76d03148ee0
galyna-k <hkzlvs@gmail.com>
2025-07-01 21:57:04 +0100

    fix debug's files
    - corrected property access in address.js to use key instead of index;
    - fixed author.js to iterate over object values using Object.values;
    - updated recipe.js to properly log the ingredients array instead of the object itself

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,8 +1,12 @@
 // Predict and explain first...
+// bruschetta serves 2 ingredients: [object Object]
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
+
+// ${recipe} tries to print the whole object, which converts to "[object Object]".
+// To show ingredients, need access by using recipe.ingredients.
 
 const recipe = {
   title: "bruschetta",
@@ -12,4 +16,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients}`);
