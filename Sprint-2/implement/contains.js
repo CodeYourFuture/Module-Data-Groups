5bba410c882b8499dc0e172ed525d8d13741eb0b
Reza Jahanimir <jahanimir.r@gmail.com>
2025-07-01 11:12:54 +0100

    Implement a function called contains that checks an object contains a particular property

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,9 @@
-function contains() {}
+function contains(obj, key) {
+    if (typeof obj !== "object" || obj === null || Array.isArray(obj)){
+        throw new Error("Invalid input");
+    }
+
+    return Object.hasOwn(obj, key); 
+}
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,16 +20,31 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test('Given an empty object, it should return false', () => {
+    const obj = {}
+    expect(contains(obj, 'anykey')).toEqual(false)
+})
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test('passing an obj and the key we looking for, it should return true', () => {
+    const obj = {a: 1, b: 2};
+    expect(contains({a: 1, b: 2}, 'c')).toBe(true)
+});
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("When passed to contains with a non-existent property name, it should return false", () => {
+    const obj = {a: 1, b: 2};
+    expect(contains({a: 1, b: 2}), 'c').toBe(false);
+});
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("Given invalid parameters like an array, it should return false or throw an error", () => {
+    const list = [1, 2, 3]
+    expect(contains(list, "key")).toThrow("Invalid input")
+});
