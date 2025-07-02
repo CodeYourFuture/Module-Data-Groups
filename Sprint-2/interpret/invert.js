c0fea7be32710a6d0b2f8f72921a105a1ecc5154
galyna-k <hkzlvs@gmail.com>
2025-07-02 11:08:17 +0100

    update interpret/invert
    - fix invert function to use bracket notation for key-value swapping;
    - add unit tests to verify correct inversion for various input cases.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -10,20 +10,30 @@ function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
 
+module.exports = invert;
+
 // a) What is the current return value when invert is called with { a : 1 }
+  // invert({ a: 1 }) -> output: { "1": "a" }
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
+  // invert({ a: 1, b: 2 }) -> output: { "1": "a", "2": "b" }
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
+  // return: { "1": "a", "2": "b" }
 
-// c) What does Object.entries return? Why is it needed in this program?
+// d) What does Object.entries return? Why is it needed in this program?
+  // Object.entries({ a: 1, b: 2 }) -> return: [['a', 1], ['b', 2]].
+  // It's needed to loop over each [key, value] pair to swap them.
 
-// d) Explain why the current return value is different from the target output
+// e) Explain why the current return value is different from the target output
+  // If written like invertedObj.key = value, it would create a property literally named "key".
+  // This need to be fixed by using bracket notation: invertedObj[value] = key.
 
-// e) Fix the implementation of invert (and write tests to prove it's fixed!)
+// f) Fix the implementation of invert (and write tests to prove it's fixed!)
+  // Fixed at the line 13 by swapping key and value with bracket notation invertedObj[value] = key.
