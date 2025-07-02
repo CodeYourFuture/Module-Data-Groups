1b62da2aa0080542448e0778924a4ceca6995280
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-07-02 13:51:39 +0100

    contains.js implement

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(object, key) {
+  if (typeof object !== "object" || object === null || Array.isArray(object)) {
+    return false;
+  }
+  return object.hasOwnProperty(key);
+}
 
 module.exports = contains;
