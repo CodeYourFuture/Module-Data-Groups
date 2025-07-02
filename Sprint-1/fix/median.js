89eebb611ad1b572b5c1fbe2eef0161fc75dc2df
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2025-07-02 11:48:28 +0100

    1. Fixed median.js and median.test.js / Sprint-1 MODULE-DATA-GROUPS

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,29 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  // First of all, we check if list is an array.
+  if (!Array.isArray(list)) {
+    return null
+  } 
+ // the next code from 13 to 16create a new Array called listFiltered checking if the elements are numbers and no NaN>
+  const listFiltered = list.filter(checkElement);
+   function checkElement(n){
+    return typeof n === "number" && n != isNaN;
+   }
+   // return null if there are not numbers in the list.
+  if (listFiltered.length == 0) {
+  return null
+  }
+  // sort the list
+  listFiltered.sort();
+  const middleIndex = Math.floor(listFiltered.length / 2);
+  // check for even or odd amount of numbers in the list
+   if (listFiltered.length % 2 == 0) {
+    const median = (listFiltered[middleIndex - 1] + listFiltered[middleIndex]) / 2;
+    return median 
+  } else { 
+    const median = listFiltered.splice(middleIndex, 1)[0];
+    return median 
+  }
 }
-
-module.exports = calculateMedian;
+module.exports = {calculateMedian}
