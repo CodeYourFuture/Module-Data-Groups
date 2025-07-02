0494f5022b9de1165a87a287addd5cb7c17b3640
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-07-02 19:41:51 +0100

    lookup.js implement

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,16 @@
-function createLookup() {
-  // implementation here
+function createLookup(countCurr) {
+  if (!Array.isArray(countCurr)) return;
+
+  const findCurrency = {};
+
+  for (const pair of countCurr) {
+    const [country, currency] = pair;
+
+    if (typeof country === "string" && typeof currency === "string") {
+      findCurrency[country] = currency;
+    }
+  }
+  return findCurrency;
 }
 
 module.exports = createLookup;
