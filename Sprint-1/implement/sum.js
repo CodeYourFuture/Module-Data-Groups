fe18c0e542c702f3ca3accc2225a00074c99336e
sheida-shab <shabankary@gmail.com>
2025-07-02 21:30:31 +0100

    improving function and passed 4 test

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -3,6 +3,64 @@ function sum(elements) {
     if (elements.length === 0) {
         return 0;
     }
+    if (allNumericArray(elements)){
+        return sumOfArray(elements);
+
+    }
+
 }
+
+
+//detect numeric values 
+function allNumericArray(myArray) {
+    for (let i=0;i<myArray.length;i++){
+        if (typeof myArray[i] !== "number") {
+          return false;
+        }
+    }
+    return true;
+}
+
+ //find the first number element in an array
+ //input : an array
+ //output: first number element    
+ function findFirstNumberElement(myArray){
+    for (let i=0;i<myArray.length;i++){
+        if (typeof myArray[i] === "number"){
+            return myArray[i];
+        }
+    }
+
+ }   
+ 
+ 
+ //detect all non-numeric value array
+ function allNoneNumericArray(myArray){
+    for (let i=0;i<myArray.length;i++){
+        if (typeof myArray[i] === "number"){
+            return false;
+        }
+    }
+    return true;
+
+ }
+
+ function findMaxNoneNumeric(myArray,maxElement){
+    for (let i=0;i<myArray.length;i++){
+        if (myArray[i] > maxElement) {
+            maxElement = myArray[i];
+           }
+     }
+    return maxElement;
+}
+
+function sumOfArray(myArray){
+    let total=0;
+    for(i=0;i<myArray.length;i++){
+       total = total + myArray[i];
+    }
+    return total;
+}
+
 
 module.exports = sum;
