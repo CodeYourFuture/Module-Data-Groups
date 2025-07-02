ff8508b611e18396200c8332881d1134e31323ca
sheida-shab <shabankary@gmail.com>
2025-07-02 21:05:28 +0100

    writing the test cases

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,8 @@
 function sum(elements) {
+    //for empty array input
+    if (elements.length === 0) {
+        return 0;
+    }
 }
 
 module.exports = sum;
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,41 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test("given an empty array, returns 0",() =>{
+    expect(sum([])).toEqual(0);
+}
+)
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
-
+test("given an array with just one number, returns that number", () => {
+  expect(sum([78])).toEqual(78);
+});
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
+test("given an array containing negative numbers, returns the correct total sum", () => {
+  expect(sum([1,3,4,-6])).toEqual(2);
+});
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
+test("given an array with decimal/float numbers, returns the correct total sum", () => {
+  expect(sum([1.7, 0.3, 4.5, 0.08])).toEqual(6.58);
+});
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+test("given an array containing non-number values, returns the sum of the numerical elements", () => {
+  expect(sum(["hey", 10, "hi", 60, 10])).toEqual(80);
+});
 
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test("given an array with only non-number values, returns the least surprising value given how it behaves for all other inputs", () => {
+  expect(sum(["hey","test" , "hi", "first"])).toEqual("heytesthifirst");
+});
