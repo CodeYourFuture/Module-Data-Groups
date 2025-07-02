03078eb323a8bb0e018d072ac39ed838dc83b1ad
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-07-02 20:00:46 +0100

    Debug exercises are completed.

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -3,6 +3,8 @@
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
+// address is not an array so the index can't use to retrieve data from address object.
+// We should use address.houseNumber.
 
 const address = {
   houseNumber: 42,
@@ -12,4 +14,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -4,12 +4,16 @@
 // Each ingredient should be logged on a new line
 // How can you fix it?
 
+// There is a recipe object and inside the recipe, ingredient property is an array.
+// We should use for loop to log out the ingredients. 
+
 const recipe = {
   title: "bruschetta",
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves}`+'\n'+'Ingredients:');
+for (const ingredient of recipe.ingredients) {
+  console.log(`- ${ingredient}`);
+}
