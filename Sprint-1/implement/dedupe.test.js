be126903e8819f409d648f29b1443420ade2180e
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-07-03 23:48:36 +0100

    feat: Implement dedupe function and add tests.

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,12 +16,22 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+describe('dedupe function' , () => {
+test("given an empty array, it returns an empty array", ()=>{
+expect(dedupe([])).toEqual([])
+});
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
-
+test('array with no duplicates should return a copy of the original array',() => {
+expect(dedupe([1,3,6])).toEqual([1,3,6]);
+});
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
+test('array with strings or numbers should remove the duplicate values, preserving the first occurence of each element', () => {
+expect(dedupe(['a','k','k','c','c'])).toEqual(['a','k','c']);
+expect(dedupe([1,1,1,3,4,4,])).toEqual([1,3,4,])
+});
+});
