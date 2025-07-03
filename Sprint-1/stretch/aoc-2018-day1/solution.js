b42a7358a38aa5b71b24717b809ff6cdd8ef91c4
sheida-shab <shabankary@gmail.com>
2025-07-03 18:26:41 +0100

    Day 2-1st puzzle -function calculateArea

--- Sprint-1/stretch/aoc-2018-day1/solution.js
+++ Sprint-1/stretch/aoc-2018-day1/solution.js
@@ -1,18 +1,61 @@
-function readInputFromFile(textFile){
+function day1Puzzle1(textFile) {
   const fs = require("fs");
   const inputFile = fs.readFileSync(textFile, "utf8");
   const lines = inputFile.split("\n");
-  let countOP=0;
-  let countCP=0;
+  let countOP = 0;
+  let countCP = 0;
   //console.log(inputFile.length);
-  for (let i=0;i<inputFile.length;i++){
-    if (inputFile[i]==="("){
-        countOP++ 
-    }else{
-        countCP++
+  for (let i = 0; i < inputFile.length; i++) {
+    if (inputFile[i] === "(") {
+      countOP++;
+    } else {
+      countCP++;
     }
   }
-  console.log(countOP-countCP);
+  return (countOP - countCP);
 }
 
-readInputFromFile("Day1.txt");
+
+function day1Puzzle2(textFile) {
+    const fs = require("fs");
+    const inputFile = fs.readFileSync(textFile, "utf8");
+    const lines = inputFile.split("\n"); 
+    let floorCount= 0;
+    for (let i = 0; i < inputFile.length; i++) {
+      if (inputFile[i] === "(") {
+        floorCount++;
+      } else {
+        floorCount--;
+      }
+      if (floorCount==-1){
+        return i+1;
+      }
+    }
+}
+
+function day2Puzzle1(textFile) {
+  const fs = require("fs");
+  const inputFile = fs.readFileSync(textFile, "utf8");
+  const lines = inputFile.split("\n");
+ 
+  for (let i = 0; i < inputFile.length; i++) {
+    
+  }
+}
+
+function calculateArea(dimension){
+  let dimensionArray=dimension.split("x");
+  let s1 = parseInt(dimensionArray[0]) * parseInt(dimensionArray[1]);
+  let s2 = parseInt(dimensionArray[0]) * parseInt(dimensionArray[2]);
+  let s3 = parseInt(dimensionArray[1]) * parseInt(dimensionArray[2]);
+
+  let extraPaper = Math.min(s1,s2,s3);
+  let totalWrapPaper=(2*s1)+(2*s2)+(2*s3)+extraPaper
+  return totalWrapPaper;;
+   
+}
+
+
+console.log(day1Puzzle1("Day1.txt"));
+console.log(day1Puzzle2("Day1.txt"));
+console.log(day2Puzzle1("Day2.txt"));
