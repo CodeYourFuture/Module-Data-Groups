a6d7d4adecd9352a68affb3e06fc866d0c50ac96
[Hibo Sharif] <[hibo.sharif87@gmail.com]git config --global --list>
2025-07-03 15:22:26 +0100

    querystring.test.js implement

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,10 +3,42 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
 test("parses querystring values containing =", () => {
   expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+    equation: "x=y+1",
+  });
+});
+
+test("handles multiple key and value pairs", () => {
+  expect(parseQueryString("city=New%20Jersey&age=60")).toEqual({
+    city: "New Jersey",
+    age: "60",
+  });
+});
+
+test(" handles empty string", () => {
+  expect(parseQueryString("")).toEqual({});
+});
+
+test("decodes + as spaces and % ", () => {
+  expect(
+    parseQueryString("occasion=happy%20birthday&gift=mobile%20phone")
+  ).toEqual({
+    occasion: "happy birthday",
+    gift: "mobile phone",
+  });
+});
+
+test("handles key without value", () => {
+  expect(parseQueryString("city")).toEqual({
+    city: "",
+  });
+});
+
+test("handles key with empty value", () => {
+  expect(parseQueryString("city=")).toEqual({
+    city: "",
   });
 });
