daf8b848e6eafe91ec892268ba39498d5aad837e
sheida-shab <shabankary@gmail.com>
2025-07-03 20:32:37 +0100

    implement Day2 2nd Puzzle  function

--- Sprint-1/stretch/aoc-2018-day1/solution.js
+++ Sprint-1/stretch/aoc-2018-day1/solution.js
@@ -33,18 +33,18 @@ function day1Puzzle2(textFile) {
     }
 }
 
-function day2Puzzle1(textFile) {
+function day2Puzzle1_totalWrappingPaper(textFile) {
   const fs = require("fs");
   const inputFile = fs.readFileSync(textFile, "utf8");
   const lines = inputFile.split("\n");
   let totalWrapPaper=0
   for (let i = 0; i < lines.length; i++) {
-    totalWrapPaper += calculateArea(lines[i]);
+    totalWrapPaper += calculateWrapingPaper(lines[i]);
   }
    return totalWrapPaper;
 }
 
-function calculateArea(dimension){
+function calculateWrapingPaper(dimension){
   let dimensionArray=dimension.split("x");
   let s1 = parseInt(dimensionArray[0]) * parseInt(dimensionArray[1]);
   let s2 = parseInt(dimensionArray[0]) * parseInt(dimensionArray[2]);
@@ -56,7 +56,29 @@ function calculateArea(dimension){
    
 }
 
+function day2Puzzle2_totalRibbon(textFile) {
+  const fs = require("fs");
+  const inputFile = fs.readFileSync(textFile, "utf8");
+  const lines = inputFile.split("\n");
+  let totalRibbon = 0;
+  for (let i = 0; i < lines.length; i++) {
+    totalRibbon += calculateRibbon(lines[i]);
+  }
+  return totalRibbon;
+
+}
+
+function calculateRibbon(dimension) {
+  let dimensionArray = dimension.split("x");
+  let d1 = parseInt(dimensionArray[0]);
+  let d2 = parseInt(dimensionArray[1]);
+  let d3 = parseInt(dimensionArray[2]);
+  let wrapRibbon=(d1*2)+(d2*2);
+  let bowRibbon=d1*d2*d3;
+  return wrapRibbon + bowRibbon;
+}
 
 console.log(day1Puzzle1("Day1.txt"));
 console.log(day1Puzzle2("Day1.txt"));
-console.log(day2Puzzle1("Day2.txt"));
+console.log(day2Puzzle1_totalWrappingPaper("Day2.txt"));
+console.log(day2Puzzle2_totalRibbon("Day2.txt"));
