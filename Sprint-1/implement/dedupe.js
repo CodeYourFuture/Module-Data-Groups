df0b19c9cc18675a91acf0b2676411dbf325d78f
Declan Williams <declanwilliams793@gmail.com>
2025-07-04 23:36:11 +0100

    feat: implement for...of loop to iterate through input array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,4 +1,8 @@
 function dedupe() {
-    // makes a new array to store non duplicate values
-    const newArray = [];
+  // makes a new array to store non duplicate values
+  const newArray = [];
+
+  // loops over each item in the original array
+  for (const item of array) {
+  }
 }
