8f6ddf1d854bcdd624e8e2a771d24ac027ff1f90
mo-muchunu <mo.muchunu@gmail.com>
2025-07-04 05:31:20 +0100

    Debugged and fixed 'address', 'author', and 'recipe' code to correctly access object properties

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,3 @@
-// Predict and explain first...
-
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +10,13 @@ const address = {
   postcode: "XYZ 123",
 };
 
+/* =======> Predict and explain first...
+The code will not work because it is attempting to access the address object using an array index, which is incorrect.
+
 console.log(`My house number is ${address[0]}`);
+
+=======> To fix:
+Instead, we can access the properties of an object using either dot or bracket notation.
+*/
+
+console.log(`My house number is ${address.houseNumber}`);
--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,3 @@
-// Predict and explain first...
-
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +9,20 @@ const author = {
   alive: true,
 };
 
+/* =======> Predict and explain first...
+This program will not work because 'author' is an object, not an array, so it is not iterable.
+Running the code will throw an error. 
+
 for (const value of author) {
   console.log(value);
+}
+  
+=======> To fix:
+One way to log out all the property values in the object is to access the keys 
+with a 'for...in' loop. Then, use bracket notation to log each value.
+*/
+
+
+for (const key in author) { // Iterate over the property names [keys] of the author object.
+  console.log(author[key]); // Use bracket notation to access and log each property value.
 }
