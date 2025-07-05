87aa2f848a1090c1c42c8f708d10d51d9a1dc28b
Declan Williams <declanwilliams793@gmail.com>
2025-07-05 13:08:24 +0100

    Fix console log to correctly display house number from address object

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -17,4 +17,5 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+// console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address["houseNumber"]}`); // corrected to print the house number from the address object
