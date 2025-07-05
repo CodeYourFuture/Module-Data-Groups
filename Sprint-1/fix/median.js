7a1563093554109c4092a256b57ae83b6b7fcad0
Anuar Duisenbek <coolboy949494@gmail.com>
2025-07-05 13:54:34 +0100

    solved: fix-median

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,27 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  //Array validation
+  if (!Array.isArray(list)) return null;
+
+  //Filter Only Numeric Values
+  const onlyNumbers = list.filter(Number.isFinite);
+
+  //Handle No Numbers Case
+  if (onlyNumbers.length === 0) return null;
+
+  //sorting Numeric values
+  const sorted = onlyNumbers.slice().sort((a, b) => a - b);
+
+  //Find middle index
+  const mid = Math.floor(sorted.length / 2);
+
+  //The median calculation
+  if (sorted.length % 2 === 0) {
+    return (sorted[mid - 1] + sorted[mid]) / 2;
+  } else {
+    return sorted[mid];
+  }
 }
 
 module.exports = calculateMedian;
