cf7cd0580d1eb24d2236d907d0fa8db31ed7a689
AdnaanA <thetriplex247@gmail.com>
2025-07-05 14:14:35 +0100

    modified:   Sprint-2/debug/address.js 	modified:   Sprint-2/implement/contains.js 	modified:   Sprint-2/implement/contains.test.js 	new file:   package-lock.json 	new file:   package.json

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,16 @@
-function contains() {}
+function contains() {
+    /**
+     * Checks if the given array contains the specified value.
+     * @param {Array} arr - The array to check.
+     * @param {*} value - The value to search for.
+     * @returns {boolean} - Returns true if the value is found, otherwise false.
+     */
+    return function(arr, value) {
+        if (!Array.isArray(arr)) {
+        throw new TypeError('First argument must be an array');
+        }
+        return arr.includes(value);
+    };
+}
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -21,6 +21,9 @@ as the object doesn't contains a key of 'c'
 // When passed to contains
 // Then it should return false
 test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+  expect(contains({}, 'a')).toBe(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
