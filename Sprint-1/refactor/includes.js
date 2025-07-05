849a7e8613f39b466f1940079aaecdc3035d47b9
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-07-05 12:27:56 +0100

    Refactor the `includes` function to use a for .. of loop passing all tests.

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,22 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
+  if (list.length === 0) {
+    return false;
+  }
+  for (const item of list) {
+    if (item === target) {
       return true;
     }
   }
   return false;
 }
+
+console.log(includes([5, 6, null], 4));
+console.log(includes(["a", "b", "m"], "m"));
+console.log(includes(["a", "b", "m"], 2));
+console.log(includes([1, 2, 2, 3], 2));
+console.log(includes([null], null));
+console.log(includes([], 2));
 
 module.exports = includes;
