b07f167082d4a92be72b0f50d25c9b689c1b01de
Declan Williams <declanwilliams793@gmail.com>
2025-07-05 11:15:35 +0100

    add unit tests for dedupe function to ensure it removes duplicates while preserving first occurrences

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -30,3 +30,8 @@ test("given an array with no duplicates, it returns a copy of the original array
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
+test("given an array with duplicates it removes any duplicates and preserves the first instance of each element", () => {
+  expect(dedupe(['a','a','a','b','b','c'])).toEqual(['a','b','c']);
+  expect(dedupe([5, 1, 1, 2, 3, 2, 5, 8])).toEqual([5, 1, 2, 3, 8]);
+  expect(dedupe([1, 2, 1])).toEqual([1, 2]);
+});
