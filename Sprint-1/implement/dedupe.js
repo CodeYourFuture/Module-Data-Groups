45c2ff0621002a70663a352a4810b2a1fea4398c
Anuar Duisenbek <coolboy949494@gmail.com>
2025-07-05 14:55:17 +0100

    solved: implement-dedupe.js

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,11 @@
-function dedupe() {}
+function dedupe(arr) {
+  const result = [];
+  for (let item of arr) {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+}
+
+module.exports = dedupe;
