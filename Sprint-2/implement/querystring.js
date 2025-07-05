154a4ffabac13732dc2ccd064ce1189f491c9ec1
mo-muchunu <mo.muchunu@gmail.com>
2025-07-05 06:25:15 +0100

    Implemented 'parseQueryString' function and numerous test cases covering various input formats

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,16 @@
-function parseQueryString(queryString) {
-  const queryParams = {};
-  if (queryString.length === 0) {
-    return queryParams;
-  }
-  const keyValuePairs = queryString.split("&");
+function parseQueryString(query) {
+  if (!query) return {};                                        // Return an empty object if input is empty, null, or undefined. 
 
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
-  }
+  return Object.fromEntries(                                    // Using Object.fromEntries to convert array of [key, value] pairs into an object
+    query.split("&").map((pair) => {                            // Split the query string into individual key-value pairs
 
-  return queryParams;
+
+      const [key, ...rest] = pair.split("=");                   // Split each pair at the first '='
+      const value = rest.length > 0 ? rest.join("=") : "";      // If at least one after the first '=', join values with '=' as separator or default to empty string
+      return [key, value];                                      
+    })
+  );
+
 }
 
 module.exports = parseQueryString;
