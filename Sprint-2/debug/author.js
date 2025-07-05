fc5ec4ad03a7d39ce31ba75a3d8342af8d479025
sedazam <sedazam@gmail.com>
2025-07-05 06:30:44 +0100

    modified: changed author.values[author]   Sprint-2/debug/author.js

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,10 @@
 // Predict and explain first...
 
+// This code is trying to log out the values of the properties in the `author` object.
+// However, the `for...of` loop is designed to iterate over iterable objects like arrays
+// or strings, not over objects. Since `author` is an object, the loop will not work as intended,
+// and it will throw a TypeError because objects are not iterable by default.
+
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +16,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of author.values[author]) {
   console.log(value);
 }
