f6fee7b35591e450329b78d511e291aa3b400730
sedazam <sedazam@gmail.com>
2025-07-06 23:03:00 +0100

    modified: Added comments to Sprint-2/debug/address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -4,6 +4,12 @@
 // but it isn't working...
 // Fix anything that isn't working
 
+// The code is trying to log the `houseNumber` property from the `address` object.
+// However, the `address` object is not defined in the provided code snippet.
+// To fix this, we need to define the `address` object first and then log the
+// `houseNumber` property correctly.
+// The code is also using a template literal to log the value, which is correct.
+// Let's define the `address` object and log the `houseNumber` property.
 const address = {
   houseNumber: 42,
   street: "Imaginary Road",
