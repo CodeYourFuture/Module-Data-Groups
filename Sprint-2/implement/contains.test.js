9b4359602526870ee1a5d586591aa5fc05324df9
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-07 18:28:59 +0100

    copied the first test in to the third as the requirements are similar

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -22,12 +22,16 @@ test("returns true if the object contains the property, false otherwise", ()=>{
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false",()=>{
+    expect(contains({})).toEqual(false)
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-
+test("returns true if the object contains the property, false otherwise", () => {
+  expect(contains({ age: 25 }, "age")).toEqual(true);
+});
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
