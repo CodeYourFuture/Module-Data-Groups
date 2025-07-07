e940258c4690ecad545e70260597b4bbee51afac
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-07 15:03:09 +0100

    Write test cases and Implement the function to passed the tests

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,19 @@
-function dedupe() {}
+function dedupe(array) {
+    let arrayCopy = [];
+    if (array.length == 0) {
+        return [];
+    }
+    else if (array.length != 0) {
+        array.forEach(element => {
+            if (!arrayCopy.includes(element)) {
+                arrayCopy.push(element);    
+        }
+    });
+        return arrayCopy;
+    }
+
+
+}
+
+console.log(dedupe(["a", "b", "c"]))
+module.exports = dedupe;
