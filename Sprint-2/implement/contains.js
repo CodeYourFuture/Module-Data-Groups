a8d12eb7f5df94bbccab97e57eb110e127de8ff9
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-07 18:12:38 +0100

    wrote code to return true if the object contains the property, false otherwise

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,10 @@
-function contains() {}
+function contains(obj,prop) {
+//it checks if the key part of the object's property includes the second parameter
+    if(Object.keys(obj).includes(prop)){
+        return true
+    } else {
+        return false}
+
+}
 
 module.exports = contains;
