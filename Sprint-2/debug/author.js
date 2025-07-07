bc476c9c88d322a8cbd13bce0a1125ee0694fe75
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-07-07 23:01:42 +0100

    Fix: Use Object.values to iterate over object values correctly

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// The code doesn't work because you can't use the "for...of" loop directly on an object.
+// In JavaScript, objects are not iterable like arrays are.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +13,7 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
+
