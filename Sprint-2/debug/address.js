cc0c7b72e09d4daa3bb63e8c265d2a64edd7a4c9
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-07-07 12:46:28 +0100

    fixed syntax to access value for "houseNumber" key

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// prediction before running code: This leads to an error as this isn't an array where values accessed via index/position. values should be accessed via keys/properties in objects.
+//prediction after running code: logged out "undefined" for ${address[0]}` as this expression can't be matched with any property inside the object
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +14,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
