550017fac6cda2af26a941a8ddfdbfad3fac9bc0
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-07-07 21:42:03 +0100

    fix: use correct syntax to get house number from object

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+// This line of code does not function correctly because it is attempting to access the house number as if it were an array 
+// using the index value [0], but the "address" variable is an object, not an array.
+//  In order to access the house number, one would must utilize the address.houseNumber variable.
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -13,3 +16,7 @@ const address = {
 };
 
 console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`); // This will correctly print house number is 42
+
+
+
