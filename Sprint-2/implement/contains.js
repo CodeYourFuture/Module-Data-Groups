1f1dc5cff4210f696fdbdf1e9a190794ef3866e4
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-07 18:58:10 +0100

    added coment

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,14 +1,13 @@
 function contains(obj,prop) {
-    if (typeof obj != "object") return false;
-    //if the object has no keys , returns false
-    if (Object.keys(obj).length === 0) return false; 
-//it checks if the key part of the object's property includes the second parameter
-    if(Object.keys(obj).includes(prop))  {return true}
-    return false;
-    
-    
-     
-
+  //return false if the parameter is not an object
+  if (typeof obj != "object") return false;
+  //if the object has no keys , returns false
+  if (Object.keys(obj).length === 0) return false;
+  //it checks if the key part of the object's property includes the second parameter
+  if (Object.keys(obj).includes(prop)) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = contains;
