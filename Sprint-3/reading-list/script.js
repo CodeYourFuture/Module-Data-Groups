b21124083968f52909bb05690520c66d44c3846e
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-07-07 14:58:43 +0100

    Implement reading list: fetch books, display with images and styled bullets

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -1,23 +1,65 @@
-// for the tests, do not modify this array of books
-const books = [
-  {
-    title: "The Design of Everyday Things",
-    author: "Don Norman",
-    alreadyRead: false,
-    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780465050659.jpg",
-  },
-  {
-    title: "The Most Human Human",
-    author: "Brian Christian",
-    alreadyRead: true,
-    bookCoverImage:
-      "https://images-na.ssl-images-amazon.com/images/I/41m1rQjm5tL._SX322_BO1,204,203,200_.jpg",
-  },
-  {
-    title: "The Pragmatic Programmer",
-    author: "Andrew Hunt",
-    alreadyRead: true,
-    bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
-  },
-];
+// For the tests, do not modify this array of books
+let books = [];
+
+fetch('books.json')
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Books are not fetched correctly");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    books = data;
+    renderReadingList(); 
+  })
+  .catch((err) => {
+    document.getElementById("reading-list").textContent = "Could not load books.";
+    console.error("Error encountered:", err);
+  });
+
+
+const renderReadingList = () => {
+  const colors = ["#f9c2ff", "#c2f0fc", "#ffe5b4", "#d0f0c0", "#f4cccc"];
+  const mainWrapper = document.getElementById("content");
+  const readingList = document.getElementById("reading-list");
+  mainWrapper.style.backgroundColor = "#51e2f5"
+  mainWrapper.style.width = "100%"
+  mainWrapper.style.height = "100%"
+
+  books.forEach((book, index) => {
+    const li = document.createElement("li");
+    li.style.margin = "1rem 10rem";
+
+    const wrapper = document.createElement("div");
+    wrapper.style.display = "flex";
+    wrapper.style.alignItems = "flex-start";
+    wrapper.style.gap = "1rem";
+    wrapper.style.backgroundColor = colors[index % colors.length];
+    wrapper.style.padding = "2rem";
+    wrapper.style.borderRadius = "12px";
+    wrapper.style.width = "100%";
+
+  
+    const img = document.createElement("img");
+    img.src = book.bookCoverImage;
+    img.alt = `Cover of ${book.title}`;
+    img.style.width = "100px";  
+    img.style.borderRadius = "8px";
+
+    const bookInfo = document.createElement("div");
+    bookInfo.style.fontSize = "1.5rem";
+    bookInfo.innerHTML = `
+      <strong>${book.title}</strong><br>
+      by ${book.author}<br>
+      <em>${book.alreadyRead ? " Already read" : "To read"}</em>
+    `;
+
+    wrapper.appendChild(img);   
+    wrapper.appendChild(bookInfo); 
+
+    li.appendChild(wrapper);
+    readingList.appendChild(li);
+  });
+
+};
 
