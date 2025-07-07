22695204dc5451836883a4d4c80c92b02c923d41
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-07 18:32:10 +0100

    changed 2 if statements ,one to check if the function returns false for an empty object as it was initially wrong, and the other i deleted the else as it was uneccessarry because the return is a boolean

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,9 +1,10 @@
 function contains(obj,prop) {
+    //if the object has no keys , returns false
+    if (Object.keys(obj).length === 0) return false; 
 //it checks if the key part of the object's property includes the second parameter
-    if(Object.keys(obj).includes(prop)){
-        return true
-    } else {
-        return false}
+    if(Object.keys(obj).includes(prop))  return true
+    
+     
 
 }
 
