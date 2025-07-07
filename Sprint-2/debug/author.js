aa848542749ecb09c2c09b9d466946a7a13e009a
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-07-07 13:05:10 +0100

    object now iterable & logs out all values.

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// prediction before running code: because accessing values of objects should've been done via dot notation or the square brackets method. 
+//prediction after running code: the "for.. of" method only works on iterable objects excluding "plain objects". "For...in" method could be used instead.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +13,7 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
-  console.log(value);
+for (const value in author) {
+  console.log(author[value]);
 }
+
