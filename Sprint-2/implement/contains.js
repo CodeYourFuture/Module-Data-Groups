e11e2a8f0b21cad481b3971167f5a26b1870f243
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-07 18:39:59 +0100

    added line to return false if the property is not included in the object

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -2,7 +2,8 @@ function contains(obj,prop) {
     //if the object has no keys , returns false
     if (Object.keys(obj).length === 0) return false; 
 //it checks if the key part of the object's property includes the second parameter
-    if(Object.keys(obj).includes(prop))  return true
+    if(Object.keys(obj).includes(prop))  {return true}
+    return false;
     
      
 
