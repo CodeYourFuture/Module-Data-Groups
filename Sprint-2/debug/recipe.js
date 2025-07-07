9fe323fec7b58946d2f52e9907e6d290d89b5fe5
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-07-07 13:38:11 +0100

    fixed bug & each ingredient displayed in new line

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 // prediction before running code: this template literal expression: "${recipe}" won't give the expected results for the "ingredients" part.
-// observations after running code: ${recipe}` evaluates to [object Object]
+// observations after running code: ${recipe}` evaluates to [object Object]. This is because template literal ALONE converts an object into a string with unreadable properties.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -12,6 +12,4 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves} ingredients:\n${recipe.ingredients.join("\n")}`);
