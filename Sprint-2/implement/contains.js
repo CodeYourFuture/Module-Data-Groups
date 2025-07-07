cfcb972312f72bdcd70f9cdcc409c2d55008e9d2
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-07-07 15:49:57 +0100

    contains function now meets requirements

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,14 @@
-function contains() {}
+function contains(object, property) {
+    if (typeof object !== "object" || object == null || Array.isArray(object)) {
+        return false;
+    }
+    if (Object.keys(object).length === 0) {
+        return false;
+    } else {
+        return object.hasOwnProperty(property); 
+    }
+    
+
+}
 
 module.exports = contains;
