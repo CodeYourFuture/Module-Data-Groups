a72d182dd5253e96274abe541e85d5f844a4d49c
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-07 16:10:11 +0100

    added .ingredients to ${recipe}

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,7 +1,7 @@
 // Predict and explain first...
 
 // This program should log out the title, how many it serves and the ingredients.
-// Each ingredient should be logged on a new line
+// Each ingredient should be logged on a new line....we will get instead of ingredients the whole recipe
 // How can you fix it?
 
 const recipe = {
@@ -12,4 +12,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients}`);
