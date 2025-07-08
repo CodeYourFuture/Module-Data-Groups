47ece2cb47f26c44304410bd0d3335d33ee8eed4
Sara Tahir <saratahir34@gmail.com>
2025-07-08 07:30:44 +0100

    Wrote the function. Discussed 2 methods to update the function.

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -10,6 +10,15 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves}\ningredients:`); // add a line break
+recipe.ingredients.forEach((ingredient) => { //iterate over ear ingredient in ingredients.
+  console.log(ingredient);
+})
+/* This will log the whole object recipe rather than each ingredient on a different line.
+We need to access each item of ingredients from the array seperately.Iterate over them.
+we could also use 
+console.log(`${recipe.title} serves ${recipe.serves}\ningredients:`);
+
+for (let i = 0; i < recipe.ingredients.length; i++) {
+  console.log(recipe.ingredients[i]);
+}*/ 
