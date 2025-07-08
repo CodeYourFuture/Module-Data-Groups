65595b462aca25b109f0e84a248766ce29cf44bc
Malusi Skunyana <malusiskunyana@gmail.com>
2025-07-08 20:34:39 +0200

    implemented the contains function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,21 @@
-function contains() {}
+function contains() {
+    // Check if the first argument is an object
+    if (typeof arguments[0] !== 'object' || arguments[0] === null) {
+        return false; // Return false for non-object types
+    }
+
+    // If the first argument is an array, return false
+    if (Array.isArray(arguments[0])) {
+        return false;
+    }
+    
+    // Check if the second argument is a string
+    if (typeof arguments[1] !== 'string') {
+        return false; // Return false for non-string property names
+    }
+    
+    // Check if the object contains the property
+    return Object.prototype.hasOwnProperty.call(arguments[0], arguments[1]);
+}
 
 module.exports = contains;
