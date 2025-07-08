c161a42b8860966fa076a162d9050fff66c43b70
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-08 16:02:30 +0100

    splits the query with multiple '=' and returns empty object for empty query

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,19 @@
 function parseQueryString(queryString) {
-  const queryParams = {};
-  if (queryString.length === 0) {
+  const queryParams = {};                                        //declares a variable with the value of an empty object
+  if (queryString.length === 0) {                                //if the querry string is empty return empty object
     return queryParams;
   }
-  const keyValuePairs = queryString.split("&");
-
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
-  }
-
+  const [key, ...rest] = queryString.split("=");                 //splits the queystring in to arrays where it finds'=', assigns the first part as the value for key
+  const value = rest.join("=");                              //it joins the two arrays from 'rest' with '=' resulting in a string
+  queryParams[key] = value;                                  //adds the key:value pair to the object
+  
   return queryParams;
 }
+
+
+
+console.log(parseQueryString("equation=x=y+1"));
+
+
 
 module.exports = parseQueryString;
