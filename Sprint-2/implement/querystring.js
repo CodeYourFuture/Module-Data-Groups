535764fbca9e329f18a30b7743eb03e469617574
Malusi Skunyana <malusiskunyana@gmail.com>
2025-07-08 23:16:16 +0200

    updated the parseQueryString function

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -6,8 +6,12 @@ function parseQueryString(queryString) {
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    const [key, ...rest] = pair.split("=");
+    const value = rest.join("=");
+    // Decode key and value to handle special characters
+    const decodedKey = decodeURIComponent(key);
+    const decodedValue = decodeURIComponent(value);
+    queryParams[decodedKey] = decodedValue;
   }
 
   return queryParams;
