26e8cd8bb8606f184fd96b956c4911286e977d96
Sisu860 <meharisisay6@gmail.com>
2025-07-08 21:39:24 +0100

    Add check for empty input.txt file to handle empty file error

--- Sprint-1/stretch/aoc-2018-day1/solution.js
+++ Sprint-1/stretch/aoc-2018-day1/solution.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const filePath = 'input.txt';
+try {
 const content = fs.readFileSync(filePath, 'utf8');
 const frequencyChanges = content
   .split('\n')
@@ -11,3 +12,6 @@ const frequencyChanges = content
 const total = frequencyChanges.reduce((a, b) => a + b, 0);
 
 console.log('Total frequency:', total);
+}catch (err) {
+  console.error('Error reading file:', err);
+}
