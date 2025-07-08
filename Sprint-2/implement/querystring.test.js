af97b62f79a0377b5e942381672d2b2f9eccf03a
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-07-08 15:22:21 +0100

    Add tests for parseQueryString including edge cases

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,10 +3,28 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
-test("parses querystring values containing =", () => {
+test("Parses key-value pair with '=' in the value", () => {
   expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+    equation: "x=y+1",
   });
 });
+
+test("Parses multiple key-value pairs", () => {
+  expect(parseQueryString("name=Ali&age=30")).toEqual({
+    name: "Ali",
+    age: "30",
+  });
+});
+
+test("Handles empty query string", () => {
+  expect(parseQueryString("")).toEqual({});
+});
+
+test("Handles value with multiple '=' characters", () => {
+  expect(parseQueryString("code=a=b=c")).toEqual({
+    code: "a=b=c",
+  });
+});
+
