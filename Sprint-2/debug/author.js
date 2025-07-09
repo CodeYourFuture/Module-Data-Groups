8cfea902bbd627f4af931375fba733ad3112c2f6
HilolaRustamova <mohlaroy86@gmail.com>
2025-07-09 14:29:24 +0100

    author.js file debuged and tested

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,10 @@
 // Predict and explain first...
 
+//This code tries to use a for...of loop on an object, but objects are not iterable by default.
+//for...of works on iterables like arrays, strings, maps, sets — not plain objects.
+//Running this will throw:
+//TypeError: author is not iterable
+
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +16,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
