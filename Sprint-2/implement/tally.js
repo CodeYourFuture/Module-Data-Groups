917f2ce13940169f2a1e05d5c18a7c84886f242f
Malusi Skunyana <malusiskunyana@gmail.com>
2025-07-09 21:21:00 +0200

    implemented the tally function

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,15 @@
-function tally() {}
+function tally(items) {
+    if (!Array.isArray(items)) {
+        throw new Error("Invalid input");
+    }
+
+    return items.reduce((acc, item) => {
+        if (typeof item !== 'string') {
+            throw new Error("Invalid input");
+        }
+        acc[item] = (acc[item] || 0) + 1;
+        return acc;
+    }, {});
+}
 
 module.exports = tally;
