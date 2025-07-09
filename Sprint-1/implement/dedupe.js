29b76c33507174a65915542df4c8576ae706afc2
Peter Lui <pgmlui@gmail.com>
2025-07-09 11:34:35 +0100

    uploading progress

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,12 @@
-function dedupe() {}
+function dedupe(arr) {
+    // Create a Set to get unique values
+    const uniqueSet = new Set(arr);
+    
+    // Clear the input array
+    arr.length = 0;
+    
+    // Repopulate the input array with unique values
+    arr.push(...uniqueSet);
+    
+    return arr;
+}
