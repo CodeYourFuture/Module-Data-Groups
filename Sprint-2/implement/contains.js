134242198610121ea50e07cb45f0f04171e41920
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-07-09 16:18:28 +0100

    Fix: create contains function and implemented tests.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,26 @@
-function contains() {}
+const containObject = {
+first_name : 'john',
+surname: 'madim',
+date_of_birth : 1986,
+place_of_birth : 'newYork'
+}
 
+function contains(obj,propertyName) {
+        if ( typeof obj !== 'object'|| obj === null || Array.isArray(obj)){
+        throw new Error ('Invalid first argument parameter, need to be an object.')
+    }
+    if (Object.keys(obj).length ===0){
+        return false
+    }
+
+    const keyObject = Object.keys(obj)
+    const hasPropertyName = keyObject.includes(propertyName)
+    return hasPropertyName
+    
+}
+
+console.log(contains({first_name :'john'}, 'first_name'))
+console.log(contains({place_of_birth :'newYork'}, 'first_name'))
+console.log(contains({}, 'surname'))
+console.log(contains('hello', 'surname'))
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -12,24 +12,55 @@ as the object doesn't contains a key of 'c'
 */
 
 // Acceptance criteria:
-
+describe('conatains function',() => {
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
 
+test('should return true if the object contains the property', () => {
+        const testObject = { a: 1, b: 2 };
+        expect(contains(testObject, 'a')).toBe(true);
+        expect(contains(testObject, 'b')).toBe(true);
+        expect(contains({ surname: 'madin', date_of_birth: 1986 }, 'surname')).toBe(true)
+});
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("should return false when passed an empty object", () => {
+        expect(contains({}, 'surname')).toBe(false);
+        expect(contains({}, 'anyProperty')).toBe(false);
+    });
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-
+test('should return true if the object contains the property', () => {
+        const testObject = { a: 1, b: 2 };
+        expect(contains(testObject, 'a')).toBe(true);
+        expect(contains(testObject, 'b')).toBe(true);
+        expect(contains({ first_name: 'john', date_of_birth: 1986 }, 'first_name')).toBe(true)
+});
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
-
+test('should return false if the object does not contain the property', () => {
+        const testObject = { a: 1, b: 2 };
+        expect(contains(testObject, 'c')).toBe(false);
+        expect(contains(testObject, 'x')).toBe(false);
+        expect(contains({ surname: 'madin', date_of_birth: 1986 }, 'place_of_birth')).toBe(false);
+});
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+
+test('should throw an error for invalid parameters like an array', () => {
+        expect(() => contains([], 'surname')).toThrow('Invalid parameter: The first argument must be a non-null, non-array object.')
+
+        expect(() => contains(null, 'first_name')).toThrow('Invalid parameter: The first argument must be a non-null, non-array object.')
+        expect(() => contains(123, 'first_name')).toThrow('Invalid parameter: The first argument must be a non-null, non-array object.');
+
+        // Test with undefined
+        expect(() => contains(undefined, 'surname')).toThrow('Invalid parameter: The first argument must be a non-null, non-array object.');
+})
+
+});
