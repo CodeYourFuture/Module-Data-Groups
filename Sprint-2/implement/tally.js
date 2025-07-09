f91ae9b8a6f208f9aabcdd8c4fdc54c0d03a57eb
Sara Tahir <saratahir34@gmail.com>
2025-07-09 11:18:57 +0100

    wrote test cases and tally function.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,21 @@
-function tally() {}
+function tally(items) {
+  if (!Array.isArray(items)) {
+    throw new Error("Input should be an array");
+  }
 
+  const result = {};
+
+  for (const item of items) {
+    if (result[item]) {
+      result[item]++;
+    } else {
+      result[item] = 1;
+    }
+  }
+
+  return result;
+}
 module.exports = tally;
+//takes and array of items
+//item are keys
+// how many times that item is repeated in the array- count of that item(key) is the value of each key 
