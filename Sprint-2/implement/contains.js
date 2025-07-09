95b65e76f24c39cf38852ee34a63f01849e49904
HilolaRustamova <mohlaroy86@gmail.com>
2025-07-09 21:36:28 +0100

    contains function made and tested

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,9 @@
-function contains() {}
+function contains(obj, prop) {
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    throw new Error("Invalid input: must be a non-null object");
+  }
+
+  return obj.hasOwnProperty(prop);
+}
 
 module.exports = contains;
