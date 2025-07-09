05d5b0180e15baac8493f6d44f3e7fdaea7e2341
Declan Williams <declanwilliams793@gmail.com>
2025-07-09 18:21:13 +0100

    implemented findMax function to return the maximum number from an array

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -10,6 +10,17 @@ function findMax(elements) {
             numbersOnly.push(item);
         }
     }
-}
+    // start with the first number in the "numbersOnly" array
+    let max = numbersOnly[0];
 
+    // loop for the largest number in the "numbersOnly" array
+    for (const num of numbersOnly) {
+        if (num > max) {
+            max = num;
+        }
+    }
+    return max;
+}
+console.log(findMax([30, 50, 10, 40])); // 50
+console.log(findMax(['hey', 10, 'hi', 60, 10])); // 60
 module.exports = findMax;
