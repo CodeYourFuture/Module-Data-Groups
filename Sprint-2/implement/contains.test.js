e8aa86b05f7f1ad9fd650783ea233cfcfb3b4d96
Sisu860 <meharisisay6@gmail.com>
2025-07-09 19:30:32 +0100

    Add contains function with input validation and related tests

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,20 +16,33 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
-
+test("contains with an existing property name returns true", () => {
+    expect(contains ({a: 1, b: 2}, "a")).toBe(true);
+});
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+    expect (contains({}, "a")).toBe(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test("contains with an existing property name", () => {
+    expect(contains({a: 1, b: 2}, "b")).toBe(true);
+});
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("contains with a non-existent property name", () => {
+  expect(contains({a: 1, b: 2}, "c")).toBe(false);
+});
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("contains throws an error for invalid input like an array", () => {
+    expect(() => contains(["a", "b"], "a")).toThrow("Invalid input: expected an object");
+});
