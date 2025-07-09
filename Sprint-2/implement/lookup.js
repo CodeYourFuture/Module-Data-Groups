fc94a7505a24ef0a0404fe9f9f1ea02de657757d
sedazam <sedazam@gmail.com>
2025-07-09 19:52:46 +0100

    modified:   Sprint-2/implement/lookup.js 	modified:   Sprint-2/implement/lookup.test.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,27 +1,9 @@
 function createLookup(countryCurrencyPairs) {
-  if (
-    !Array.isArray(countryCurrencyPairs) ||
-    countryCurrencyPairs.length === 0
-  ) {
-    throw new Error("Input must be a non-empty array of pairs");
-  }
-
   const result = {};
 
   for (let i = 0; i < countryCurrencyPairs.length; i++) {
     const pair = countryCurrencyPairs[i];
-
-    if (!Array.isArray(pair) || pair.length !== 2) {
-      throw new Error(
-        "Each pair must be a pair of countryCode and currencyCode"
-      );
-    }
-
     const [countryCode, currencyCode] = pair;
-
-    if (typeof countryCode !== "string" || typeof currencyCode !== "string") {
-      throw new Error("Both country code and currency code must be strings");
-    }
 
     result[countryCode] = currencyCode;
   }
@@ -29,13 +11,11 @@ function createLookup(countryCurrencyPairs) {
   return result;
 }
 
-const data = [
-  ["US", "USD"],
-  ["CA", "CAD"],
-];
-
-const lookup = createLookup(data);
-
-console.log(lookup); // { US: 'USD', CA: 'CAD' }
+console.log(
+  createLookup([
+    ["US", "USD"],
+    ["CA", "CAD"],
+  ])
+);
 
 module.exports = createLookup;
