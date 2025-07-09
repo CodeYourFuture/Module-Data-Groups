b7d7b0dcbbe9a9b29645dc4b0868eb60c34d065a
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-07-09 20:37:25 +0100

    fix: implement the lookup function and  write the test case.

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,14 @@
-function createLookup() {
-  // implementation here
+const countryCurrency = [["MAR","MAD"],["UK","GBP"],["US","USD"]]
+
+
+function createLookup(dataCurrency) {
+  
+ return dataCurrency.reduce((acc,entry) => {
+  const [countryCode,currencyCode]=entry
+  acc[countryCode]= currencyCode
+  return acc
+ },{})
 }
 
+console.log(createLookup(countryCurrency))
 module.exports = createLookup;
