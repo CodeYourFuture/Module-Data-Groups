128a345be3bd0de35f678f1586d88c5bf394e5b4
Declan Williams <declanwilliams793@gmail.com>
2025-07-09 17:55:55 +0100

    feat: implement findMax function to filter numbers from an array

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,15 @@
 function findMax(elements) {
+    // makes an array to hold only numbers
+    const numbersOnly = []
+
+    // to loop through each element in the array
+    for (const item of elements){
+        
+        // if the item is a number push it to the numbersOnly array
+        if (typeof item === `number`){
+            numbersOnly.push(item);
+        }
+    }
 }
 
 module.exports = findMax;
