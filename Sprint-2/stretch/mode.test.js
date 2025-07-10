d93e8e56b628142655285c2f8fb6f354814e1996
rivakutu <revivemmapfumo@gmail.com>
2025-07-10 13:27:11 +0200

    handles non numerical arrays and interleaving modes well

--- Sprint-2/stretch/mode.test.js
+++ Sprint-2/stretch/mode.test.js
@@ -22,11 +22,14 @@ describe("calculateMode()", () => {
     expect(calculateMode([1, 2, 2, 3, 3])).toEqual(2);
     expect(calculateMode([1, 1, 2, 7, 7])).toEqual(1);
     expect(calculateMode([1, 5, 5, 3, 3])).toEqual(5);
+    expect(calculateMode([5, 3, 3, 1, 5])).toEqual(5);
+    expect(calculateMode([9, 2, 4, 5, 3, 3, 4, 1, 1, 2])).toEqual(2);
   });
 
   test("ignores non-number values", () => {
     expect(calculateMode([1, 3, "2", 2, 3, null])).toEqual(3);
     expect(calculateMode([1, 3, "2", undefined, 3, null])).toEqual(3);
     expect(calculateMode(["2", undefined, 1, null])).toEqual(1);
+    expect(calculateMode("a", true, null, "b", undefined)).toBeNaN();
   });
 });
