0cd5b2db67c448a4284f511e87e6f8c9cd4be4f1
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-07-10 16:10:40 +0100

    Fix: implement the tally function and writing the tests.

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -13,22 +13,34 @@ const tally = require("./tally.js");
  * tally(['a', 'a', 'a']), target output: { a: 3 }
  * tally(['a', 'a', 'b', 'c']), target output: { a : 2, b: 1, c: 1 }
  */
+describe('tally function tests', () => {
 
 // Acceptance criteria:
 
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
-
+test('When passed an array of item, then it should return an object containing the count for each unique item',() =>{
+    expect(tally(['a','o,','h'])).toEqual({a:1, o:1,h:1})
+})
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object",() => {
+    expect(tally([])).toEqual({})
+});
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
-
+test(' Given an array with duplicate item, should return counts for each unique item',() =>{
+    expect(tally(['a','o,','o'])).toEqual({a:1, o:2})
+})
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test('  Given an invalid input like a string, should throw an error',() =>{
+    expect(() => tally('Hello World')).toThrow('Error: Parameter must be an array.')
+
+})
+})
