5d3b005512b17cf6660091bc30e1c105a1e31bba
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-10 10:09:39 +0100

    tally on an empty array returns an empty object, and tally on an array returns an object  containing the count for each unique item

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -19,15 +19,20 @@ const tally = require("./tally.js");
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
-
+test( "tally on an array returns an object  containing the count for each unique item",()=>{
+    expect(tally(['b','b'])).toEqual({b:2})
+});
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object",()=>{
+    expect(tally([])).toEqual({})
+});
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+
 
 // Given an invalid input like a string
 // When passed to tally
