453bfe995b4ac675d166a33e07548f62d94dc1fd
Malusi Skunyana <malusiskunyana@gmail.com>
2025-07-10 13:59:53 +0200

    refactored calculateMode

--- Sprint-2/stretch/mode.js
+++ Sprint-2/stretch/mode.js
@@ -9,24 +9,29 @@
 // into smaller functions using the stages above
 
 function calculateMode(list) {
-  // track frequency of each value
-  let freqs = new Map();
+  const freqs = getFrequencies(list);
+  return findModeFromFrequencies(freqs);
+}
+
+function getFrequencies(list) {
+  const freqs = new Map();
 
   for (let num of list) {
-    if (typeof num !== "number") {
-      continue;
-    }
-
+    if (typeof num !== "number") continue;
     freqs.set(num, (freqs.get(num) || 0) + 1);
   }
 
-  // Find the value with the highest frequency
+  return freqs;
+}
+
+function findModeFromFrequencies(freqs) {
   let maxFreq = 0;
   let mode;
+
   for (let [num, freq] of freqs) {
     if (freq > maxFreq) {
-      mode = num;
       maxFreq = freq;
+      mode = num;
     }
   }
 
