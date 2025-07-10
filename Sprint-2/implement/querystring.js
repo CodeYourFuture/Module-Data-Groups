b6994d24be9e1bf03328c6f0a4ac85c6c31f0ca6
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-10 20:20:30 +0100

    added some comments

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,19 +1,21 @@
 function parseQueryString(queryString) {
   const queryParams = {};                                        //declares a variable with the value of an empty object
-  if (queryString.length === 0) {                                //if the querry string is empty return empty object
+  if (queryString.length === 0) {                                //if the query string is empty return empty object
     return queryParams;
   }
-  const [key, ...rest] = queryString.split("=");                 //splits the queystring in to arrays where it finds'=', assigns the first part as the value for key
-  const value = rest.join("=");                              //it joins the two arrays from 'rest' with '=' resulting in a string
-  queryParams[key] = value;                                  //adds the key:value pair to the object
-  if (value.length===0)return { [key]: "missing value" };
-  if (key.length===0) return {"missing key":value,}
+  
+  const [key, ...rest] = queryString.split("=");                 //splits the querystring in to arrays where it finds'=', assigns the first part as the value for key
+   const value = rest.join("=");                              //it joins the two arrays from 'rest' with '=' resulting in a string
+   queryParams[key] = value;                                  //adds the key:value pair to the object
+   if (value.length===0)return { [key]: "missing value" };    // checks for missing value
+   if (key.length===0) return {"missing key":value,}           // checks for missing value
+  
   return queryParams;
 }
 
 
 
-console.log(parseQueryString("=o"));
+console.log(parseQueryString("letter=o&age=34"));
 
 
 
