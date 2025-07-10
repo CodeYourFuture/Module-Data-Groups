779e97e91cae08115b6cc95764fe90830b05e289
AdnaanA <thetriplex247@gmail.com>
2025-07-10 12:48:53 +0100

    modified:   Sprint-2/debug/address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,10 @@
 // Predict and explain first...
+// Should trow an error because `author` is an object, not an iterable.
+
+// The is trying to display the houseNumber property of the address object.
+// However, the code is using the wrong syntax to access the property.
+// The correct way to access a property in an object is using either dot notation or bracket notation
+// but the code is using an incorrect syntax that does not access the property correctly.
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,5 +18,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-//console.log(`My house number is ${address[houseNumber]}`);
 console.log(`My house number is ${address["houseNumber"]}`);
