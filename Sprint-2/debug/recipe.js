1a1c5c7cab36c3d15d69e3c9436179caaa978a0f
AdnaanA <thetriplex247@gmail.com>
2025-07-10 13:15:17 +0100

    modified:   Sprint-2/debug/recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,12 @@
 // Predict and explain first...
+// This code is intended to log out the details of a recipe object.
+// However, it incorrectly attempts to log the entire recipe object as a string,
+// which will not format the ingredients correctly.
+// The expected output should include the title, the number of servings, and each ingredient on a
+// new line, but the current code will not achieve that.
+// To fix it, we need to iterate over the ingredients array and log each ingredient on a
+// new line. We can use the `join` method to format the ingredients correctly.
+
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -10,6 +18,7 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
+
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:
+- ${recipe.ingredients.join("\n- ")}`);
