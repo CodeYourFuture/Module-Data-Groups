43145bea4fb70d67d3eb3ca7b284f660e72ecedd
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-10 10:12:34 +0100

    created empty object, adds each unique item with count to the object, returns empty object if the array is empty

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,14 @@
-function tally() {}
+function tally(arr) {
+     
+const newObj={} ;
+if (arr.length === 0) return newObj;
+for(const item of arr){
+(newObj[item]=(newObj[item] || 0)+1)
+}
+return newObj;
+}
+
+console.log(tally(['a','a','t']))
+
 
 module.exports = tally;
