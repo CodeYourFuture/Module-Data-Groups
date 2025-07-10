df25099bf606f9f4e75e89b7028e428c78b4dfad
AdnaanA <thetriplex247@gmail.com>
2025-07-10 17:51:27 +0100

    modified:   Sprint-2/implement/tally.js 	modified:   Sprint-2/implement/tally.test.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,15 @@
-function tally() {}
+function tally(input) {
+  if (!Array.isArray(input)) {
+    throw new Error("Input must be an array");
+  }
+
+  const result = {};
+
+  for (const item of input) {
+    result[item] = (result[item] || 0) + 1;
+  }
+
+  return result;
+}
 
 module.exports = tally;
