c8db9810aa68773bc170456fc5b629557795034c
ike-agu <ikennaww@yahoo.fr>
2025-07-11 17:13:27 +0100

    Implemented findMax function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,5 +1,11 @@
 function findMax(elements) {
-  
+  if(elements === 0){
+    return -Infinity;
+  }else{
+    return Math.max(...elements)
+  }
 }
 
+// console.log(findMax([1, 2, 3, 4,10]));
+// console.log(findMax([]));
 module.exports = findMax;
