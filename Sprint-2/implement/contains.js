b96968fc57a9dfda8781d10ef994d631a539661c
Declan Williams <declanwilliams793@gmail.com>
2025-07-11 12:50:49 +0100

    Fix type check for object in contains function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,7 +1,5 @@
 function contains(obj, key) {
     // check if "obj" is an object and not an array and we are working with plain objects
-    if (typeof obj !== object || Array.isArray(obj)) return false;
-    
+    if (typeof obj !== "object" || Array.isArray(obj)) return false;
 }
-
 module.exports = contains;
