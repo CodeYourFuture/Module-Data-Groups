552dbc7cf635731e756ac6bb142198fb726b7fbb
mirabellemorah <myrakuti@gmail.com>
2025-07-11 01:49:04 +0100

    Implement done

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,8 @@
-function dedupe() {}
+function dedupe(list) {
+  return [...new Set(list)];
+}
+console.log(dedupe(['a','a','b','c','c'])); // returns ['a', 'b', 'c']
+console.log(dedupe([5, 1, 1, 2, 3, 2, 5, 8])); // returns [5, 1, 2, 3, 8]
+console.log(dedupe([])); // returns []
+
+module.exports = dedupe;
--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,21 @@
-function sum(elements) {
+function sum(list) {
+    // Remember this filters the list to only include numbers and ignore strings or other types
+    const filterList = list.filter(
+        (el) => typeof el === "number" && !isNaN(el)
+    );
+    
+    // If there are no numbers, return 0
+    if (filterList.length === 0) {
+        return 0;
+    }
+    // If the sum is negative, return 0; otherwise, return the sum
+    const total = filterList.reduce((acc, curr) => acc + curr, 0);
+    return total;
 }
+
+console.log(sum([10, 20, 30])); // 60
+console.log(sum(['hey', 10, 'hi', 60, 10])); // 80
+console.log(sum([])); // 0
+console.log(sum([-10, -20, 30])); // 0
 
 module.exports = sum;
