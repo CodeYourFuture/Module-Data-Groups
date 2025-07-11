c0fc206a8cdc0717106fcbfbf79c5175891c9fa3
Amy-Rose <amyrlcollins@gmail.com>
2025-07-11 16:43:05 +0100

    implement, contains: wrote tests then code, tests are passing

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,16 +20,48 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+    const emptyObj = {};
+    const prop3 = 'beans';
+    expect(contains(emptyObj,prop3)).toEqual(false);
+});
+
+const obj = {name: 'Amy-Rose', age: 28, favMusic: 'Smashing Pumpkins', uhOh: 0};
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test("should return true if the object contains the property", () => {
+    const prop1 = 'favMusic';
+    expect(contains(obj,prop1)).toEqual(true);
+})
+
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
 
+test("should return false if the object doesn't contain the property", () => {
+    const prop2 = 'favColour';
+    expect(contains(obj,prop2)).toEqual(false);
+})
+
+
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+
+test("invalid input should return false", () => {
+    const prop4 = [1,2,3,4];
+    expect(contains(obj, prop4)).toEqual(false);
+})
+
+
+// Given an object that contains falsey values
+// When passed to contains with an existing property
+// Then it should still return true
+
+test("for a property that does exist in the object, contains should return true even if the value in the key-value pair is falsey", () => {
+    const prop5 = 'uhOh';
+    expect(contains(obj,prop5)).toEqual(true);
+})
