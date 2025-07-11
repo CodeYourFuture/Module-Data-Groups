228755f2497ba14c5c7174171475bfb3d1c6ba3b
Declan Williams <declanwilliams793@gmail.com>
2025-07-11 12:54:02 +0100

    fix: add missing return false to handle cases where key is not found

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,5 +1,7 @@
 function contains(obj, key) {
     // check if "obj" is an object and not an array and we are working with plain objects
     if (typeof obj !== "object" || Array.isArray(obj)) return false;
+
+    return false; // if no object or key is found return false
 }
 module.exports = contains;
