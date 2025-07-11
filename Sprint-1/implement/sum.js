bda9ea15bbfc78fdef0e7168ea5bad9dc9034a83
ike-agu <ikennaww@yahoo.fr>
2025-07-11 19:04:41 +0100

    Implemented Sum function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,17 @@
 function sum(elements) {
+  let onlyNumbers = [];
+  //Extract numbers from the Array incase array is mixed
+  for (let i = 0; i < elements.length; i++) {
+    if (typeof elements[i] === "number") {
+      onlyNumbers.push(elements[i]);
+    }
+  }
+  //Sum numbers in array
+  let sumOfArray = onlyNumbers.reduce((a, b) => a + b);
+  return sumOfArray;
 }
+
+// console.log(sum([10, 20, 30]));
+// console.log(sum(["hey", 10, "hi", 60, 10]));
 
 module.exports = sum;
