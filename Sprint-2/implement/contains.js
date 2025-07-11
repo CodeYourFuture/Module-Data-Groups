ac0ffda8ed6fd46e2b9e94a95f676465a7b42fbe
Declan Williams <declanwilliams793@gmail.com>
2025-07-11 12:30:25 +0100

    implemented parameters to contains function and to check if obj is an object and not an array

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,7 @@
-function contains() {}
+function contains(obj, key) {
+    // check if "obj" is an object and not an array and we are working with plain objects
+    if (typeof obj !== object || Array.isArray(obj)) return false;
+    
+}
 
 module.exports = contains;
