aed7949042ca94014888afc2e4aa6472c14b7bfb
ike-agu <ikennaww@yahoo.fr>
2025-07-11 14:31:43 +0100

    Implemented dedupe function and implemented first test case if array is empty should return empty array.

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,11 +16,16 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+test("given an empty array, it returns an empty array", () => {
+  let emptyArr = []
+  let result = dedupe(emptyArr);
+  expect(result).toEqual([]);
+});
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
+
 
 // Given an array with strings or numbers
 // When passed to the dedupe function
