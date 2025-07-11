499b60afe1fc1f46c93991516e6b06197ff3762f
HilolaRustamova <mohlaroy86@gmail.com>
2025-07-11 23:19:28 +0100

    tally.js function made and tested

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,23 @@
-function tally() {}
+function tally(items) {
+  // Validate input: must be an array
+  if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
+
+  // Create an object to hold counts
+  const counts = {};
+
+  // Loop through each item and count occurrences
+  for (const item of items) {
+    if (counts[item]) {
+      counts[item] += 1;
+    } else {
+      counts[item] = 1;
+    }
+  }
+
+  return counts;
+}
 
 module.exports = tally;
+
