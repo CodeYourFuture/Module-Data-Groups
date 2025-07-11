987ba347e081665bfd66fa587d394d4a29d4872c
Nataliia74 <volkov.nataliia@gmail.com>
2025-07-11 15:44:48 +0100

    added different ways to solve the task, added new edge cases that led to fixing the function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,12 +1,31 @@
 function contains(object, property) {
-  for (const key of Object.keys(object)) {
-    if (key === property) {
-      console.log(`${object[key]}`);
-      return true;
-    }
+  if (typeof object === "object" && object !== null && !Array.isArray(object)) {
+    console.log(Object.hasOwn(object, property));
+    return Object.hasOwn(object, property);
   }
   return false;
 }
+
+// function contains(object, property) {
+//   if (typeof object === "object" && object !== null && !Array.isArray(object)) {
+//     for (const key in object) {
+//       if (key === property) {
+//         //console.log(`${object[key]}`);
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+// function contains(object, property) {
+//   for (const key of Object.keys(object)) {
+//     if (key === property) {
+//       console.log(`${object[key]}`);
+//       return true;
+//     }
+//   }
+//   return false;
 
 const profile = {
   name: "Gita",
@@ -14,7 +33,7 @@ const profile = {
   school: "Leith Academy",
 };
 
-const result = contains(profile, "age");
-console.log(result);
+//const result = contains([1, 2, "age"], "age");
+//console.log(result);
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -22,47 +22,51 @@ describe("edge cases of the contains function", () => {
     const targetOutput = true;
     expect(currentOutput).toEqual(targetOutput);
   });
-});
 
-// Given an empty object
-// When passed to contains
-// Then it should return false
-describe("edge cases of the contains function", () => {
+  // Given an empty object
+  // When passed to contains
+  // Then it should return false
   test("contains on empty object returns false", () => {
     const currentOutput = contains({}, "property");
     const targetOutput = false;
     expect(currentOutput).toEqual(targetOutput);
   });
-});
 
-// Given an object with properties
-// When passed to contains with an existing property name
-// Then it should return true
-describe("edge cases of the contains function", () => {
-  test("contains an object and property and returns true", () => {
-    const currentOutput = contains({ property: "value" }, "property");
-    const targetOutput = true;
-    expect(currentOutput).toEqual(targetOutput);
-  });
-});
-
-// Given an object with properties
-// When passed to contains with a non-existent property name
-// Then it should return false
-describe("edge cases of the contains function", () => {
+  // Given an object with properties
+  // When passed to contains with a non-existent property name
+  // Then it should return false
   test("contains an object and property and returns true", () => {
     const currentOutput = contains({ property: "value" }, "lava");
     const targetOutput = false;
     expect(currentOutput).toEqual(targetOutput);
   });
-});
 
-// Given invalid parameters like an array
-// When passed to contains
-// Then it should return false or throw an error
-describe("edge cases of the contains function", () => {
+  // Given invalid parameters like an array
+  // When passed to contains
+  // Then it should return false or throw an error
   test("contains an object and property and returns true", () => {
     const currentOutput = contains([], "property");
+    const targetOutput = false;
+    expect(currentOutput).toEqual(targetOutput);
+  });
+
+  // Don't forget to fill in the expect return values
+  test("contains a null instead of object and property and returns false", () => {
+    const currentOutput = contains(null, "foo");
+    const targetOutput = false;
+    expect(currentOutput).toEqual(targetOutput);
+  });
+
+  // Don't forget to fill in the expect return values
+  test("contains an array instead of object and property and returns false", () => {
+    const currentOutput = contains([6, 6, 7], "1");
+    const targetOutput = false;
+    expect(currentOutput).toEqual(targetOutput);
+  });
+
+  // Don't forget to fill in the expect return values
+  test("contains an string instead of object and property and returns false", () => {
+    const currentOutput = contains("foo", "foo");
     const targetOutput = false;
     expect(currentOutput).toEqual(targetOutput);
   });
