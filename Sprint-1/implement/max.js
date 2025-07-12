a2d7be34a76109acf93f4d173c6837bb6e0818dc
Shannon <shannond050@outlook.com>
2025-07-12 16:27:57 +0200

    max update

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,6 @@
-function findMax(elements) {
+function findMax(arr) {
+  const numbersOnly = arr.filter((item) => typeof item === "number");
+  return Math.max(...numbersOnly);
 }
 
 module.exports = findMax;
