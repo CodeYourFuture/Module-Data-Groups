2ed03e4d9373d504e62969217e2e650320145685
zohrehKazemianpour <129424353+zohrehKazemianpour@users.noreply.github.com>
2025-07-12 12:31:48 +0100

    refactor parseQueryString: properly handeling empty prams and decode URL encoded values

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,21 +1,27 @@
 function parseQueryString(queryString) {
   const result = {};
-  if (queryString.length === "") {
+  if (queryString.trim().length === 0) {
     return result;
   }
   const pairs = queryString.split("&");
 
-  for (let i = 0; i <pairs.length; i++) {
-    const pair = pairs[i];
-    const firstEqualSignIndex =pair.indexOf("=");
-    let key = pair.slice(0,firstEqualSignIndex);
-    let value = pair.slice(firstEqualSignIndex + 1);
-    result[key] = value;
+  for (const pair of pairs) {
+   
+    if (!pair.includes("=")) {
+      result[pair] = null;
+    } else {
 
+      const firstEqualIndex = pair.indexOf("=");
+      const key = pair.slice(0, firstEqualIndex);
+      let value = pair.slice(firstEqualIndex + 1);
+            value = value === "" ? null : decodeURIComponent(value);
+            result[key] = value;
+
+    }
 
   }
 
   return result;
 }
-console.log(parseQueryString("equation = x = y + 1"));
+console.log(parseQueryString(""));
 module.exports = parseQueryString;
