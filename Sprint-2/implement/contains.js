025f19c104643d87a1a3b24bd0ecce4be3273f40
HassanOHOsman <hassan.oh.osman@gmail.com>
2025-07-12 14:15:54 +0100

    removed code lines that don't add anything.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -2,13 +2,10 @@ function contains(object, property) {
     if (typeof object !== "object" || object == null || Array.isArray(object)) {
         return false;
     }
-    if (Object.keys(object).length === 0) {
-        return false;
-    } else {
+
         return object.hasOwnProperty(property); 
     }
     
 
-}
 
 module.exports = contains;
