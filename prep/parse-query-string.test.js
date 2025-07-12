8ddfc15bea904b12390cdf2544e26f060b6e1ade
mirabellemorah <myrakuti@gmail.com>
2025-07-12 09:06:28 +0100

    New commit

---
+++ prep/parse-query-string.test.js
@@ -0,0 +1,9 @@
+const parseQueryString = require("./parse-query-string");
+
+test("given a query string with no query parameters, returns an empty object", function () {
+  const input = "";
+  const currentOutput = parseQueryString(input);
+  const targetOutput = {};
+
+  expect(currentOutput).toEqual(targetOutput);
+});
