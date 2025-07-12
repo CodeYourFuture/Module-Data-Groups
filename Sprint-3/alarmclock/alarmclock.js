bc18306452decd70fc115c90b35baea769a992f0
AdnaanA <thetriplex247@gmail.com>
2025-07-12 12:11:34 +0100

    modified:   Sprint-3/alarmclock/alarmclock.js 	modified:   Sprint-3/alarmclock/index.html

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,28 @@
-function setAlarm() {}
+function setAlarm() {
+  const input = document.getElementById("alarmSet");
+  let seconds = Number(input.value);
 
+  updateAlarmDisplay(seconds);
+  // Set a timeout to play the alarm after the specified seconds
+  // Note: This will not work if the page is closed or refreshed]
+  countdownInterval = setInterval(() => {
+    seconds--;
+    if (seconds <= 0) {
+      clearInterval(countdownInterval);
+      updateAlarmDisplay(0);
+      playAlarm();
+    } else {
+      updateAlarmDisplay(seconds);
+    }
+  }, 1000);
+}
+
+function updateAlarmDisplay(seconds) {
+  const display = document.getElementById("timeRemaining");
+  const minutes = Math.floor(seconds / 60).toString().padStart(2, '0');
+  const secs = (seconds % 60).toString().padStart(2, '0');
+  display.textContent = `Time Remaining: ${minutes}:${secs}`;
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
