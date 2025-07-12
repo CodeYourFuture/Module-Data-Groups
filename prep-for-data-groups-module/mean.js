7c847c5cf92f9729e16937ea594fb7bb90131d07
ike-agu <ikennaww@yahoo.fr>
2025-07-12 19:44:04 +0100

    fixed the function to find the mean of a list based on array length

--- prep-for-data-groups-module/mean.js
+++ prep-for-data-groups-module/mean.js
@@ -4,14 +4,17 @@ Given an array of numbers
 When we call calculateMean with the array of numbers
 Then we get the mean.
  */
-const list = [3,50,7];
+
 function calculateMean(list){
   let total = 0;
 
   for(const item of list){
-    total += item/3
+    total += item/list.length
   }
   return total;
 }
+
+console.log(calculateMean([3, 50, 7]));
+console.log(calculateMean([3, 10, 7,20]));
 
 module.exports = calculateMean;
