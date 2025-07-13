87fddcf470d514719fff091148840c18fabd4eed
avatarit <salah@ait.ps>
2025-07-13 01:29:46 +0100

    Fix recipe logging to correctly format ingredients output

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,9 @@
 // Predict and explain first...
+// This code is intended to log out the title, how many it serves, and the ingredients of a recipe.
+// However, it is not working correctly because the `console.log` statement is trying to log
+// the entire `recipe` object directly, which will not format the ingredients as expected.
+// Instead, we need to iterate over the `ingredients` array and log each ingredient on a new line.
+// To fix this, we can use a template literal to format the output and use `join` to create a string of ingredients separated by new lines.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -10,6 +15,6 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves} 
+ingredients: 
+${recipe.ingredients.join('\n')}`);
