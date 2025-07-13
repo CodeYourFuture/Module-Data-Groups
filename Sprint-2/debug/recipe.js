0f0d3342a178109ae5822e569bbae8866c7bb9b5
Shannon <shannond050@outlook.com>
2025-07-13 19:23:20 +0200

    Debug update

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,8 +1,8 @@
 // Predict and explain first...
-
+//will logout every ingredients on same line
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
-// How can you fix it?
+// How can you fix it? Add line breaks
 
 const recipe = {
   title: "bruschetta",
@@ -10,6 +10,6 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
+console.log(`${recipe.title}\n serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients.join("\n")}`);
