8e2c42ab236faf4ecb32e29b6ea55b0b98aaa3ef
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2025-07-14 10:29:13 +0100

    Implement dedupe function and write tests to ensure it removes duplicates

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,11 @@
-function dedupe() {}
+function dedupe(list) {
+  const newArray = [];
+
+  for (const item of list) {
+    if (!newArray.includes(item)) {
+      newArray.push(item);
+    }
+  }
+  return newArray;
+}
+module.exports = dedupe;
