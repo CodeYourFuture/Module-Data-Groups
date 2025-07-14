c6b49fb3748873956a601b4318a12c8de634716d
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-07-14 21:23:21 +0100

    Update reading list to color books based on read status (green/red)

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -1,32 +1,35 @@
 
 let books = [];
 
-fetch('books.json')
-  .then((res) => {
+async function fetchBooks() {
+  try {
+    const res = await fetch('books.json');
     if (!res.ok) {
       throw new Error("Books are not fetched correctly");
     }
-    return res.json();
-  })
-  .then((data) => {
+
+    const data = await res.json();
     books = data;
-    renderReadingList(); 
-  })
-  .catch((err) => {
+    renderReadingList();
+  } catch (err) {
     document.getElementById("reading-list").textContent = "Could not load books.";
     console.error("Error encountered:", err);
-  });
+  }
+}
+
+fetchBooks();
+
 
 
 const renderReadingList = () => {
-  const colors = ["#f9c2ff", "#c2f0fc", "#ffe5b4", "#d0f0c0", "#f4cccc"];
   const mainWrapper = document.getElementById("content");
   const readingList = document.getElementById("reading-list");
-  mainWrapper.style.backgroundColor = "#51e2f5"
-  mainWrapper.style.width = "100%"
-  mainWrapper.style.height = "100%"
+   readingList.style.listStyle = "none";
+  mainWrapper.style.backgroundColor = "#51e2f5";
+  mainWrapper.style.width = "100%";
+  mainWrapper.style.height = "100%";
 
-  books.forEach((book, index) => {
+  books.forEach((book) => {
     const li = document.createElement("li");
     li.style.margin = "1rem 10rem";
 
@@ -34,16 +37,16 @@ const renderReadingList = () => {
     wrapper.style.display = "flex";
     wrapper.style.alignItems = "flex-start";
     wrapper.style.gap = "1rem";
-    wrapper.style.backgroundColor = colors[index % colors.length];
     wrapper.style.padding = "2rem";
     wrapper.style.borderRadius = "12px";
     wrapper.style.width = "100%";
 
-  
+    wrapper.style.backgroundColor = book.alreadyRead ? "green" : "red";
+
     const img = document.createElement("img");
     img.src = book.bookCoverImage;
     img.alt = `Cover of ${book.title}`;
-    img.style.width = "100px";  
+    img.style.width = "100px";
     img.style.borderRadius = "8px";
 
     const bookInfo = document.createElement("div");
@@ -51,15 +54,15 @@ const renderReadingList = () => {
     bookInfo.innerHTML = `
       <strong>${book.title}</strong><br>
       by ${book.author}<br>
-      <em>${book.alreadyRead ? " Already read" : "To read"}</em>
+      <em>${book.alreadyRead ? "Already read" : "To read"}</em>
     `;
 
-    wrapper.appendChild(img);   
-    wrapper.appendChild(bookInfo); 
+    wrapper.appendChild(img);
+    wrapper.appendChild(bookInfo);
 
     li.appendChild(wrapper);
     readingList.appendChild(li);
   });
-
 };
+
 
