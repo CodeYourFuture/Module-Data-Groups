bac5949081442c370df9dbf750b746c16783c111
AdnaanA <thetriplex247@gmail.com>
2025-07-14 11:17:10 +0100

    modified:   Sprint-2/debug/address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,11 @@
 // Predict and explain first...
+// This code is intended to log the house number from the address object.
+// The expected output is: "My house number is 42"
+// However, if there are any issues with the code, it may not log correctly.
+// The code uses bracket notation to access the houseNumber property,
+// but it incorrectly uses an index (0) instead of the property name.
+// The correct way to access the property is by using the property name as a string.
+
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
