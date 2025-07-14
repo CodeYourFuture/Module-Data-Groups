f2b62558b4b4c980a2036ef52ca9d20cd5d27311
Farah-Stu <selma.salam45@gmail.com>
2025-07-15 00:32:28 +0100

    fixing the address issue in js file

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-
+// The code attempts to log out the houseNumber using[0] which is index. In this case, it will return undefined. Because
+// values in the object are not accessed by index but by property name.
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +13,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}.`);
