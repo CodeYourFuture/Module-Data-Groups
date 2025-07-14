fead65649948b27fe84f896c64408c0f8e559f92
AdnaanA <thetriplex247@gmail.com>
2025-07-14 11:26:24 +0100

    modified:   Sprint-2/debug/recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,11 @@
 // Predict and explain first...
+// this program should log out the title, how many it serves and the ingredients.
+// Each ingredient should be logged on a new line
+// However, it does not work as expected.
+// It logs the title and serves correctly, but the ingredients are not displayed properly.
+// The issue is that the ingredients are being logged as an object instead of each ingredient on a new line.
+// To fix this, we need to iterate over the ingredients array and log each ingredient separately
+
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
