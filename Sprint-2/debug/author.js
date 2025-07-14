3a76784e2d077700bba34e0cf8fb5f6474d23e71
AdnaanA <thetriplex247@gmail.com>
2025-07-14 11:22:08 +0100

    modified:   Sprint-2/debug/author.js

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,10 @@
 // Predict and explain first...
+// The code below is intended to log out all the property values of the `author` object.
+// However, it will not work as expected because the `for...of` loop is designed
+// to iterate over iterable objects like arrays or strings, not over objects.
+// To fix this, we can use `Object.values()` to get an array of the object's
+// property values, which can then be iterated over with a `for...of` loop
+
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
