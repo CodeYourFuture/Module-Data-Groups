7e8f294603467fa49bb447188feb493c8b6d98ef
ike-agu <ikennaww@yahoo.fr>
2025-07-15 19:44:52 +0100

    added descriptive comments for future reference

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,12 +1,14 @@
 function contains(obj, property) {
-
-  if(Object.keys(obj).length === 0){
+  //return false if object is empty
+  if (Object.keys(obj).length === 0) {
     return false;
   }
-
+  //throw error if parameters are invalid
   if (typeof obj !== "object" || Array.isArray(obj) || obj === null) {
     throw new Error("Not a valid input");
   }
+  //check that the obj contains a property, and the property is not inherited.
+  //N:B Object.hasOwn() strictly check for own properties and not inherited ones.
   if (Object.hasOwn(obj, property)) {
     return true;
   } else {
