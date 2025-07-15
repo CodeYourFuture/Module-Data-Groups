a20b4040eb7990d206d94755d2ab4074ce2403d2
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-15 23:41:55 +0100

    Extract coin values with slice, and add test case

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -8,24 +8,31 @@ function totalTill(till) {
   let total = 0;
 
   for (const [coin, quantity] of Object.entries(till)) {
-    total += coin * quantity;
+    total += coin.slice("0", coin.length - 1) * quantity;
   }
 
   return `£${total / 100}`;
 }
 
 const till = {
-  "1p": 10,
-  "5p": 6,
-  "50p": 4,
-  "20p": 10,
+  "1p": 10, 
+  "5p": 6, 
+  "50p": 4, 
+  "20p": 10, 
 };
 const totalAmount = totalTill(till);
 
+
 // a) What is the target output when totalTill is called with the till object
+    // is to return the total amounts in pounds which is in this example qual to £4.4
 
 // b) Why do we need to use Object.entries inside the for...of loop in this function?
+    // to get each key-value pair alone and assign key to be = coin and value = quantity
 
 // c) What does coin * quantity evaluate to inside the for...of loop?
+    //  multiplication of the key by the value
 
 // d) Write a test for this function to check it works and then fix the implementation of totalTill
+const input = totalTill({"1p": 10})
+const targetOutput = "£0.1"
+console.assert(input === targetOutput, "It should return £0.1")
