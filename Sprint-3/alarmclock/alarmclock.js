e88114777b7b5d45352ea85f1b865cdfd02c89ec
TarasMykytiuk <tarasmykytiukw@gmail.com>
2025-07-16 00:42:13 +0300

    sprint_3 alarm clock app

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,27 @@
-function setAlarm() {}
+function setAlarm() {
+  let timerDom = document.getElementById("timeRemaining");
+  let timeInputValue = document.getElementById("alarmSet").value;
+  setTimer(timerDom, formatTime(timeInputValue));
+  let interval = setInterval(function () {
+    timeInputValue--;
+    setTimer(timerDom, formatTime(timeInputValue));
+    if (timeInputValue == 0) {
+      playAlarm();
+      clearInterval(interval);
+    }
+  }, 1000);
+}
+
+function formatTime(seconds) {
+  let minutes = Math.trunc(seconds / 60);
+  let remainingSeconds = seconds % 60;
+  let formattedTime = minutes.toString().padStart(2, '0') + ':' + remainingSeconds.toString().padStart(2, '0');
+  return formattedTime;
+}
+
+function setTimer(timerDon, timeInputValue) {
+  timerDon.textContent = "Time Remaining: " + String(timeInputValue);
+}
 
 // DO NOT EDIT BELOW HERE
 
