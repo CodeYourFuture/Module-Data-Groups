2d6731965e60a26eb822650d04bf72de4cfe53a1
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-07-15 12:32:05 +0100

    removing unnecessary variable assignment

--- Sprint-1/stretch/aoc-2018-day1/solution.js
+++ Sprint-1/stretch/aoc-2018-day1/solution.js
@@ -2,5 +2,5 @@
 const fs = require('fs');
 
 const data = fs.readFileSync('./input.txt', { encoding: 'utf8', flag: 'r' });
-const sumNumFileArray = data.split("\n").reduce(((sum,num)=> sum =sum + Number(num)),0)
+const sumNumFileArray = data.split("\n").reduce(((sum,num)=> sum + Number(num)),0)
 console.log(sumNumFileArray);//595
