eb0812e46537d5a1fc220392bffba4fcc305c9ca
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-15 12:31:44 +0100

    Implemetn 'createLookup' function and create test cases.

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,15 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  if (countryCurrencyPairs.length == 0){
+    return "array is empty"
+  }
+  console.log(countryCurrencyPairs.length)
+  let countryCurrencyObject = {}
+  for (const array of countryCurrencyPairs){
+    countryCurrencyObject[array[0]] = array[1];
+  }
+  return countryCurrencyObject;
 }
+
+console.log(createLookup([]));
 
 module.exports = createLookup;
--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -33,3 +33,27 @@ It should return:
    'CA': 'CAD'
  }
 */
+
+test("throws error when createLookup is given an empty array", () => {
+  const input = [];
+  const currentOutput = createLookup(input);
+  const targetOutput = "array is empty";
+  expect(currentOutput).toStrictEqual(targetOutput)
+
+})
+
+test("handel the case when createLookup has only one array inside an array", () => {
+  const input = [['US', 'USD']];
+  const currentOutput = createLookup(input);
+  const targetOutput = {'US': 'USD'};
+  expect(currentOutput).toStrictEqual(targetOutput)
+
+})
+
+test("handel the case when createLookup has more than one array inside an array", () => {
+  const input = [['US', 'USD'], ['CA', 'CAD']];
+  const currentOutput = createLookup(input);
+  const targetOutput = {'US': 'USD', 'CA': 'CAD'};
+  expect(currentOutput).toStrictEqual(targetOutput)
+
+})
