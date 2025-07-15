8259a3d015ec362099fbad67c294a97e5513d4ca
ike-agu <ikennaww@yahoo.fr>
2025-07-16 00:44:31 +0100

    added descriptive comment for my future reference

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,12 +1,13 @@
 function createLookup(countryCodes) {
   // implementation here
   const countryCodeAndCurrency = {};
+  //Iterating over the array to access the inner arrays and assign the item as key value pairs
   for(const item of countryCodes){
     countryCodeAndCurrency[item[0]] = item[1]
   }
   return countryCodeAndCurrency;
 }
-console.log(createLookup([["US", "USD"],["CA", "CAD"]])
+// console.log(createLookup([["US", "USD"],["CA", "CAD"]])
 );
 
 module.exports = createLookup;
