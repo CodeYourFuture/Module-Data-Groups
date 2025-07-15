a5cf54958dd47cc84febbd6176125583def73aa3
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-15 17:00:33 +0100

    Implement 'tally' function to count the frequency of each item in an array and add test cases

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,16 @@
-function tally() {}
+function tally(arrayElements) {
+    const tallyObject = {}
+    for (const item of arrayElements) {
+        if (tallyObject[item]) {
+            tallyObject[item] += 1;
+        }
+        else {
+            tallyObject[item] = 1;
+        }
+    }
+    return tallyObject;
+}
 
+console.log(tally(['a', 'a', 'b', 'a']))
 module.exports = tally;
+
--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -23,11 +23,33 @@ const tally = require("./tally.js");
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object", () => {
+    const input = []
+    const currentOutput = tally(input)
+    const targetOutput = {}
+    expect(currentOutput).toEqual(targetOutput)
+});
+
+// Given an array with one item
+// When passed to tally
+// Then it should return counts of the item, which is 1
+test("tally on an array with one item should return counts of the item, which is 1", () => {
+    const input = ['a', 'a']
+    const currentOutput = tally(input)
+    const targetOutput = { a : 2}
+    expect(currentOutput).toEqual(targetOutput)
+});
+
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+test("tally on an array with duplicate items should return counts for each unique item", () => {
+    const input = ['a', 'a', 'a', 'b', 'c', 'd', 'b']
+    const currentOutput = tally(input)
+    const targetOutput = { a : 3, b: 2, c: 1, d: 1 }
+    expect(currentOutput).toEqual(targetOutput)
+});
 
 // Given an invalid input like a string
 // When passed to tally
