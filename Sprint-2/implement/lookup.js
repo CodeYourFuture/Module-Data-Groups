9e7e4141122c1e233eaa78309dc0030c2913be07
ike-agu <ikennaww@yahoo.fr>
2025-07-16 00:31:06 +0100

    created a create look up function and a test case multiple country code and currency lookup

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,12 @@
-function createLookup() {
+function createLookup(countryCodes) {
   // implementation here
+  const countryCodeAndCurrency = {};
+  for(const item of countryCodes){
+    countryCodeAndCurrency[item[0]] = item[1]
+  }
+  return countryCodeAndCurrency;
 }
+console.log(createLookup([["US", "USD"],["CA", "CAD"]])
+);
 
 module.exports = createLookup;
