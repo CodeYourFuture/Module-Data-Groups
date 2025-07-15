4e6e1fd29bce6f9571a4265d00bfe298f6c9f7c1
ike-agu <ikennaww@yahoo.fr>
2025-07-15 16:22:26 +0100

    fixed the recipe object in recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+//In this code, recipe.title and recipe.serves will execute as expected but ingredients: recipe will not execute as expected because we are printing out the entire recipe object. Instead What we want to do is log each ingredient on a new line.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -11,5 +12,7 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:`);
+for(const ingredient of recipe.ingredients){
+  console.log(`- ${ingredient}`)
+}
