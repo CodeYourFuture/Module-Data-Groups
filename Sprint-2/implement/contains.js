4f10acac9c76273fa64e9656ce09c59899586bd9
Farah-Stu <selma.salam45@gmail.com>
2025-07-15 23:37:42 +0100

    updating the contains file

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,13 @@
-function contains() {}
+function contains(obj, key) {
+    if (
+        typeof obj!== 'object' ||
+        obj === null ||
+        Array.isArray(obj)
+    ) {
+        return false;
+    }
+
+    return key in obj;
+}
 
 module.exports = contains;
