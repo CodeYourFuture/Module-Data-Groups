980a5a72cff73026719267413072cc9e1b4c15fc
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-16 02:39:03 +0100

    fixed the code using 'join('\n')' on the ingrediants array.

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -5,6 +5,9 @@
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
+// The code is not correctly logging the ingredients on separate lines.
+// To fix this, we can use `join('\n')` on the ingredients array to format them correctly.
+// Additionally, we need to ensure that the template literal is formatted correctly.
 
 const recipe = {
   title: "bruschetta",
@@ -14,4 +17,5 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients.join('\n')}`);
+// This code will now correctly log the title, number of servings, and each ingredient on a new line.
