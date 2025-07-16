96380c1726f7761b2762572afa1632204f44567c
Farah-Stu <selma.salam45@gmail.com>
2025-07-16 19:51:50 +0100

    updating the lookup file

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,12 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  const lookup= {};
+  for (let pair of countryCurrencyPairs){
+    const country = pair[0];
+    const currency = pair[1];
+    lookup[country]= currency;
+    
+  } 
+  return lookup;
 }
 
 module.exports = createLookup;
