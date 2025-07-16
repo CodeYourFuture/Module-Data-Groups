8d01d8369df8a5123f74ee54a3f6679a7eef2e01
sheida-shab <shabankary@gmail.com>
2025-07-16 23:27:40 +0100

    flash page color using set interval and global var

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,3 +1,4 @@
+var flashColor; 
 function setAlarm() {
   const userInput=document.getElementById("alarmSet");
   
@@ -45,13 +46,87 @@ function setup() {
 function playAlarm() {
   audio.play();
   document.getElementById("stop").style.backgroundColor = "rgb(191, 115, 47)";
-   document.body.style.backgroundColor = "rgb(218, 178, 119)";
+  document.body.style.backgroundColor = "rgb(218, 178, 119)";
+   let flashColor=setInterval(() => {
+    if(document.body.style.backgroundColor === "rgb(218, 178, 119)"){
+      document.body.style.backgroundColor = "white";
+    }else{
+      document.body.style.backgroundColor = "rgb(218, 178, 119)";
+    }
+    
+   }, 500);
 }
 
 function pauseAlarm() {
   audio.pause();
   document.getElementById("stop").style.backgroundColor = "rgb(239, 239, 239)";
+  clearInterval(flashColor);
+  document.body.style.backgroundColor = "white";
+}
+
+window.onload = setup;function setAlarm() {
+  const userInput=document.getElementById("alarmSet");
   
+  let inputTime=userInput.value;
+  if (inputTime <= 0 || isNaN(inputTime)) {
+    alert("invalid input!!!");
+    return;
+  }
+  updateDisplayTime(inputTime);
+  let secondsInterval=setInterval(() => {
+    inputTime--;
+    updateDisplayTime(inputTime);
+    if ((inputTime === 0)) {
+      playAlarm();
+      clearInterval(secondsInterval);
+           
+    } 
+  }, 1000);
+ 
+}
+
+
+
+function updateDisplayTime(inputTime){
+  const timeRemain = document.getElementById("timeRemaining");
+  let min = String(Math.floor(inputTime / 60)).padStart(2, "0");
+  let sec = String(inputTime % 60).padStart(2, "0");
+  timeRemain.textContent = `Time Remaining: ${min}:${sec}`;
+}
+// DO NOT EDIT BELOW HERE
+
+var audio = new Audio("alarmsound.mp3");
+
+function setup() {
+  document.getElementById("set").addEventListener("click", () => {
+    setAlarm();
+  });
+
+  document.getElementById("stop").addEventListener("click", () => {
+    pauseAlarm();
+   
+  });
+}
+
+function playAlarm() {
+  audio.play();
+  document.getElementById("stop").style.backgroundColor = "rgb(191, 115, 47)";
+  document.body.style.backgroundColor = "rgb(218, 178, 119)";
+    flashColor=setInterval(() => {
+    if(document.body.style.backgroundColor === "rgb(218, 178, 119)"){
+      document.body.style.backgroundColor = "white";
+    }else{
+      document.body.style.backgroundColor = "rgb(218, 178, 119)";
+    }
+    
+   }, 500);
+}
+
+function pauseAlarm() {
+  audio.pause();
+  document.getElementById("stop").style.backgroundColor = "rgb(239, 239, 239)";
+  clearInterval(flashColor);
+  document.body.style.backgroundColor = "white";
 }
 
 window.onload = setup;
