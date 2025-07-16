a02193053ed582d5cb9757c2a3d6bf9a01ae12c9
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-16 02:29:33 +0100

    predicted the out put and explained the code.

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// I think this code will log out the title, how many it serves, and the ingredients.
+// However, it will not log each ingredient on a new line because the `recipe` object
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
