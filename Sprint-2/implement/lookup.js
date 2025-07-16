beb5da906a9bdbe0817e3887e6c04518a0d87cdd
Amy-Rose <amyrlcollins@gmail.com>
2025-07-16 10:44:25 +0100

    implement, lookup: code working with a for-of loop

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,14 +1,41 @@
 function createLookup(arr) {
 
-  let i =0;
-  while (i<arr.length) {
-    if(arr[i].length !== 2) {
+  for (let i of arr) {
+    if (i.length!==2){
       throw new Error("Inner arrays must have length 2 to be used for object creation");
-    };
-    i++;
+    }
   }
 
   return Object.fromEntries(arr);
 }
 
 module.exports = createLookup;
+
+
+
+/*
+// FUNCTION HISTORY
+
+// with a while loop
+
+let i =0;
+while (i<arr.length) {
+  if(arr[i].length !== 2) {
+    throw new Error("Inner arrays must have length 2 to be used for object creation");
+  };
+  i++;
+}
+
+
+
+// with a for...of loop
+
+for (let i of arr) {
+  if (i.length!==2){
+    throw new Error("Inner arrays must have length 2 to be used for object creation");
+  }
+}
+
+
+
+*/
