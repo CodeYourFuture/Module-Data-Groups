60d66375c60b1d405481f3a4f9828e904057fe08
Innocent_Niwatwine <innocent.pare3@gmail.com>
2025-07-16 08:51:23 +0300

    invert.js

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -5,25 +5,32 @@
 // Then it should swap the keys and values in the object
 
 // E.g. invert({x : 10, y : 20}), target output: {"10": "x", "20": "y"}
-
 function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
-  }
+  
 
+  for (const [key, value] of Object.entries(obj)) {
+    invertedObj[value] = key;
+  }
+  return invertedObj;
+  }
   return invertedObj;
 }
+console.log(invert({ a: 1, b: 2 })); 
 
 // a) What is the current return value when invert is called with { a : 1 }
+// { key: 1 }
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
-
+// { key: 2 }
 // c) What is the target return value when invert is called with {a : 1, b: 2}
+// { '1': 'a', '2': 'b' }
 
 // c) What does Object.entries return? Why is it needed in this program?
 
 // d) Explain why the current return value is different from the target output
-
+// it is different because current code is not doing what we was expecting.
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
+module.exports = invert;
