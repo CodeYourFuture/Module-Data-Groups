7500e6d33db38b6927438a3a3154f06b707582d8
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-16 02:21:14 +0100

    explained why the  'for ...of' loop is not iterating over the object.

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,10 +1,16 @@
 // Predict and explain first...
-// I think this code will not work as expected because the `for...of` loop is trying to iterate over an object.
+// I think this code will not work as expected because the `for...of` 
+// loop is trying to iterate over an object.
 // Objects cannot be iterated directly with `for...of`.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
+//JavaScript objects like author aren’t directly iterable with a for...of loop.
+//for...of is designed to work with iterable objects like arrays, strings, maps, 
+// etc. Since author is a plain object, trying to iterate over it like that throws a TypeError.
+// To fix this, we can use `Object.values()` to get an array of the object's values
+//  and then iterate over that array.
 
 const author = {
   firstName: "Zadie",
