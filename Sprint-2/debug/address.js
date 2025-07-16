1977a31b66386d17906e17f262f6fc645d1bdcb1
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-16 01:51:08 +0100

    predicted and explained the out put of the code. and the reasons.

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// I think this code will log out "My house number is undefined"
+// because address[0] is trying to access an index that doesn't exist in the object.
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
