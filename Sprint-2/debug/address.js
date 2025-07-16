849459531e080298e762681d16edf24a9a17ce18
iTeddy16 <tewodros.bekere16@gmail.com>
2025-07-16 19:09:18 +0100

    Fix access method for houseNumber in address object for M-D-G/Sprint-2/debug/address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+// In this case address is not an array and has no key index, its output will be undefined. address is an object and to get the correct output we have to access the property using its key name.
+// The correct way to access the houseNumber property is by using address.houseNumber.
+
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +15,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
