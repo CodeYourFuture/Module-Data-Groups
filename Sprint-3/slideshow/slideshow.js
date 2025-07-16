bca6e25ea1e5620f336a5d897f116e0cddc51b0e
Reza-Jahanimir <jahanimir.r@gmail.com>
2025-07-16 19:36:25 +0100

    first attempt to code with out testing

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,3 +6,46 @@ const images = [
 
 
 // Write your code here
+
+let currentIndex = 0;
+const image = document.querySelector("#carousel-img");
+
+
+// Displays the image at the given index from the images array by updating the src of the image element.
+function showImage(index) {
+  image.src = images[index];
+}
+
+
+// Increments the currentIndex by 1 to move forward in the image list.
+// If it's the last image, it wraps back to the first using modular arithmetic (%).
+function moveImageForward() {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+}
+
+
+// Decrements the currentIndex to go back to the previous image.
+// If it's the first image, it wraps around to the last one using modular arithmetic to stay within bounds.
+function moveImageBackward() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+}
+
+
+function setup() {
+
+    document.querySelector("#backward-btn").addEventListener("click", ( ) => {
+        moveImageBackward()
+    });
+
+    document.querySelector("#forward-btn").addEventListener("click", ( ) => {
+        moveImageForward()
+    });
+
+    // Show first image on load
+    showImage(currentIndex);
+
+};
+
+setup();
