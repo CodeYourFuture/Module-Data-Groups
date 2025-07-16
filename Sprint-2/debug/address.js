eebd01575f376dd737ae6a66c65c5952da920712
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-16 01:55:55 +0100

    fixed the code and tested the result.

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -14,4 +14,5 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
+// The code is already correct and will log the house number as expected.
