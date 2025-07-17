7b8a20bd63cc263f3253c1fd5bbe0e84b6c4e205
ike-agu <ikennaww@yahoo.fr>
2025-07-17 15:34:44 +0100

    Implemented tally function and completed test cases

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,30 @@
-function tally() {}
+function tally(list) {
+  const countItem = {};
 
+  if (!Array.isArray(list)) {
+    throw new Error("Your input is invalid!");
+  }
+
+  if(list.length === 0){
+    return {}
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    let currentItem = list[i];
+    if (countItem[currentItem]) {
+      countItem[currentItem] = countItem[currentItem] + 1;
+    } else {
+      countItem[currentItem] = countItem[currentItem] = 1;
+    }
+  }
+  return countItem;
+}
+// // let call1 = tally("");
+// let call2 = tally(["a", "a", "b", "c"]);
+// let call3 = tally(["a", "a", "a", "b", "z", "z"]);
+// let call4 = tally([]);
+// // console.log(call1);
+// console.log(call2);
+// console.log(call3);
+// console.log(call4);
 module.exports = tally;
--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -19,16 +19,28 @@ const tally = require("./tally.js");
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
+test("When passed an array of items, returns an object containing the count for each unique item", () => {
+  expect(tally(["a"])).toEqual({ a: 1 });
+  expect(tally(["a", "a", "a"])).toEqual({ a: 3 });
+});
 
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object",() => {
+  expect(tally([])).toEqual({})
+});
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+test("When passed an array with duplicate items, returns count for each unique item", () => {
+  expect(tally(["a", "a", "b", "c"])).toEqual({ a: 2, b: 1, c: 1 });
+});
 
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("When invalid input like string, throw an error ", () => {
+  expect(() => tally("")).toThrow("Your input is invalid!");
+});
