5eed41160c448a02986f0d878fcf25e1dde369ab
Farah-Stu <selma.salam45@gmail.com>
2025-07-17 19:32:00 +0100

    updating tally.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,16 @@
-function tally() {}
+function tally(arr) {
+    if (!Array.isArray(arr)) {
+    throw new Error("Input must be an array");
+}
+    const result = {};
+    if (arr.length === 0) {
+        return {};
+    }
+    arr.forEach(item =>{
+    result[item]=(result[item] || 0) + 1;
+});
+
+    return result;
+}
 
 module.exports = tally;
