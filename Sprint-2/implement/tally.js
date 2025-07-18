60a6ee6cbcfd161e4b0c36ab8182debbda6f4866
c-mayamba <chrismayamba@gmail.com>
2025-07-18 16:53:29 +0200

    tally implement solved

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,19 @@
-function tally() {}
+function tally(array) {
+  if (!Array.isArray(array)) {
+    throw new Error("Input must be an array");
+  }
+
+  const result = {};
+
+  for (const item of array) {
+    if (result[item]) {
+      result[item] = result[item] + 1;
+    } else {
+      result[item] = 1;
+    }
+  }
+
+  return result;
+}
 
 module.exports = tally;
