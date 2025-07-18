16245db57586b668b49b980c7799fae63bab82c6
Jamal Laqdiem <jlaqdiem@gmail.com>
2025-07-18 13:51:56 +0100

    done

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,18 +1,4 @@
 function sum(elements) {
-  let initialValue = false;
-  let initialAccumulator = 0;
-
-  for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-    if (typeof element === "number" && Number.isFinite(element)) {
-      initialAccumulator = element;
-      initialValue = true;
-      break;
-    }
-  }
-  if (!initialValue) {
-    return 0;
-  }
 
   let filterNumbers = elements.filter(
     (element) => typeof element === "number" && Number.isFinite(element)
