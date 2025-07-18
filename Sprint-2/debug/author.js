dd1a25da572865d7e8a7d31276b1bfbea935851f
c-mayamba <chrismayamba@gmail.com>
2025-07-18 13:14:25 +0200

    author debug solved

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,7 +1,10 @@
 // Predict and explain first...
+// - I predict that it won't work, because the variable 'value' isn't defined.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
+// - I was wrong, the error that it gives is that the variable 'author' is not iterable.
+// Checking for a solution...
 
 const author = {
   firstName: "Zadie",
@@ -11,6 +14,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
