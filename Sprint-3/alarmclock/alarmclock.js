5879ef2a5fb5d015c31d4de6b99883812fb164fc
sheida-shab <shabankary@gmail.com>
2025-07-18 21:56:28 +0100

    delete  repeated functions and fix the bug of click set button multiple time

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,85 +1,26 @@
-var flashColor; 
+var flashColor, secondsInterval, inputTime; 
+
 function setAlarm() {
   const userInput=document.getElementById("alarmSet");
   
-  let inputTime=userInput.value;
+  inputTime=parseInt(userInput.value);
   if (inputTime <= 0 || isNaN(inputTime)) {
     alert("invalid input!!!");
     return;
   }
-  updateDisplayTime(inputTime);
-  let secondsInterval=setInterval(() => {
-    inputTime--;
-    updateDisplayTime(inputTime);
-    if ((inputTime === 0)) {
-      playAlarm();
-      clearInterval(secondsInterval);
-           
-    } 
-  }, 1000);
- 
-}
-
-
-
-function updateDisplayTime(inputTime){
-  const timeRemain = document.getElementById("timeRemaining");
-  let min = String(Math.floor(inputTime / 60)).padStart(2, "0");
-  let sec = String(inputTime % 60).padStart(2, "0");
-  timeRemain.textContent = `Time Remaining: ${min}:${sec}`;
-}
-// DO NOT EDIT BELOW HERE
-
-var audio = new Audio("alarmsound.mp3");
-
-function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
-  });
-
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
-   
-  });
-}
-
-function playAlarm() {
-  audio.play();
-  document.getElementById("stop").style.backgroundColor = "rgb(191, 115, 47)";
-  document.body.style.backgroundColor = "rgb(218, 178, 119)";
-   let flashColor=setInterval(() => {
-    if(document.body.style.backgroundColor === "rgb(218, 178, 119)"){
-      document.body.style.backgroundColor = "white";
-    }else{
-      document.body.style.backgroundColor = "rgb(218, 178, 119)";
-    }
-    
-   }, 500);
-}
-
-function pauseAlarm() {
-  audio.pause();
-  document.getElementById("stop").style.backgroundColor = "rgb(239, 239, 239)";
-  clearInterval(flashColor);
-  document.body.style.backgroundColor = "white";
-}
-
-window.onload = setup;function setAlarm() {
-  const userInput=document.getElementById("alarmSet");
   
-  let inputTime=userInput.value;
-  if (inputTime <= 0 || isNaN(inputTime)) {
-    alert("invalid input!!!");
-    return;
-  }
-  updateDisplayTime(inputTime);
-  let secondsInterval=setInterval(() => {
-    inputTime--;
-    updateDisplayTime(inputTime);
-    if ((inputTime === 0)) {
-      playAlarm();
+  clearInterval(secondsInterval); 
+  clearInterval(flashColor);
+  pauseAlarm();
+  let remainingTime = inputTime;
+  updateDisplayTime(remainingTime);
+ 
+    secondsInterval=setInterval(() => {
+      remainingTime--;
+    updateDisplayTime(remainingTime);
+    if (remainingTime === 0) {
       clearInterval(secondsInterval);
-           
+      playAlarm();
     } 
   }, 1000);
  
@@ -129,4 +70,6 @@ function pauseAlarm() {
   document.body.style.backgroundColor = "white";
 }
 
-window.onload = setup;
+ window.onload = setup;
+
+
