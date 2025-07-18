47c438d69d42f71e7fe200eff84fbf214fa6db31
Emilianouz <135679131+Emilianouz@users.noreply.github.com>
2025-07-18 09:44:31 +0100

    Slideshow app: HTML:style tag added | slideshow.js: basicChallenge with buttons | style.css.

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -3,6 +3,40 @@ const images = [
     "./assets/cute-cat-b.jpg",
     "./assets/cute-cat-c.jpg",
 ];
+function setup(){
+buttonBack = document.getElementById("backward-btn");
+buttonForward = document.getElementById("forward-btn");
+buttonForward.addEventListener("click", forwardImg);
+buttonBack.addEventListener("click", backwardImg);
+}
+let flag = 0
+img = document.getElementById("carousel-img");
 
+function backwardImg(){
+    if (flag>0){
+        flag -= 1;
+    } else {
+        flag = images.length - 1;
+    }
+    img.src = images[flag];
+}
+function forwardImg(){
+    if (flag < images.length-1){
+        flag += 1;
+    } else {
+        flag = 0;
+    }
+    img.src = images[flag];
+}
+
+ 
+window.onload = setup;
+/**
+  ****
+function stopAuto(){
+    clearInterval(interval) 
+interval = setInterval(moveBackward, 4000);
+    
+    */
 
 // Write your code here
