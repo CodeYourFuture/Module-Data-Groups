f3dac2dc032d336b5759e0eaa5e4533255b4dfc2
Declan Williams <declanwilliams793@gmail.com>
2025-07-19 13:59:11 +0100

    Add event listener for DOMContentLoaded to initialize quote button functionality

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -507,3 +507,8 @@ const randomQuote = pickFromArray(quotes);
 quoteElements.textContent = randomQuote.quote;
 authorElements.textContent = randomQuote.author;
 }
+// use "DOMContentloaded" to ensure all html elements are loaded before updating the DOM to prevent returning null
+document.addEventListener("DOMContentLoaded", function(){
+  const newQuoteButton = document.getElementById("new-quote"); // gets the html element for the button new-quote
+  newQuoteButton.addEventListener("click", displayRandomQuote); // shows a a random new quote each time the buttons clicked
+});
