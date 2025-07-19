f3ca5971e13cc826a1981f55280001cbe775b2e5
bluejay600 <waleedbluejay7@gmail.com>
2025-07-19 14:38:15 +0100

    1. set condition if the input is invalid or zero , do nothing. 2. added a function to clear any existing interval and another one to update display immediately to the counddown function which is set interval.

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,12 +1,35 @@
 let countdown;
 let remainingTime = 0;
 
-function setAlarm() {}
-//this fuction called when (when set alarm or clicked the set button
+function setAlarm() {
 
   // Get input value in seconds
-const input = document.getElementById("alarmSet").value;
+  const input = document.getElementById("alarmSet").value;
   remainingTime = parseInt(input, 0);
+
+  // If input is invalid or zero, do nothing
+  if (isNaN(remainingTime) || remainingTime <= 0) return;
+
+  // Clear any existing interval
+  clearInterval(countdown);
+
+  // Update display immediately
+  updateDisplay(remainingTime);
+
+  // Start countdown
+  countdown = setInterval(() => {
+    remainingTime--;
+    updateDisplay(remainingTime);
+
+    if (remainingTime <= 0) {
+      clearInterval(countdown);
+      playAlarm();
+    }
+  }, 1000);
+}
+}
+
+//this fuction called when (when set alarm or clicked the set button
 
 // DO NOT EDIT BELOW HERE
 
