c0e60f342b0638581c300e0adbd933acb1fa2192
iTeddy16 <tewodros.bekere16@gmail.com>
2025-07-19 11:15:52 +0100

    update M-D-G/Sprint-2/debug/ recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,8 +1,13 @@
 // Predict and explain first...
+// - it didn't gives us a correct out put for the key "ingredients". Because we are trying to log an array directly, which will not format the ingredients correctly.
+
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
+// - to fix this we use this call "recipe.ingredients" and use a new line for ech ingredient list.  
+
+
 
 const recipe = {
   title: "bruschetta",
@@ -12,4 +17,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients.join('\n')}`);
