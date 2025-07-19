157276cc1dc4c0d5ca4bdae2696e0bf30766968e
Sisu860 <meharisisay6@gmail.com>
2025-07-19 23:56:54 +0100

    Update createLookup to return object from key-value pair using Object.fromEntries

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,6 +1,9 @@
 function createLookup(key, value) {
-  return Object.fromEntries(key, value);
+  return Object.fromEntries([[key, value]]);
   // implementation here
+  // This function should create an object with one key-value from the provided array by taking as parameter key and value
+  // The keys are the country codes
+  // The values are the corresponding currency codes
 
 }
 
