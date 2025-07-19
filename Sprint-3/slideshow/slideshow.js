2f38ca669d0201f8fd86567ed1670164ce791972
Sisu860 <meharisisay6@gmail.com>
2025-07-19 23:18:33 +0100

    modified:   Sprint-3/slideshow/index.html 	modified:   Sprint-3/slideshow/slideshow.js 	modified:   Sprint-3/slideshow/style.css

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,3 +6,26 @@ const images = [
 
 
 // Write your code here
+let currentIndex = 0;
+const imaEl = document.getElementById("carousel-img");
+const forwardBtn = document.getElementById("forward-btn");
+const backwardBtn = document.getElementById("backward-btn");
+
+
+// first lets do the forward button
+forwardBtn.addEventListener("click", function() {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    imaEl.src = images[currentIndex];
+});
+
+// now the backward button
+backwardBtn.addEventListener("click", function() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+    imaEl.src = images[currentIndex];
+});
