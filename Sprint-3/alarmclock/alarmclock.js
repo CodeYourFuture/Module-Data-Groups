9944a64cb301cdebcb292e97a9b4ada93497490b
Farah-Stu <selma.salam45@gmail.com>
2025-07-19 16:05:32 +0100

    updating alarm clock js file

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,22 @@
-function setAlarm() {}
+function setAlarm() {
+  const title = document.getElementById("timeRemaining");
+  let time =10;
+  const timerId = setInterval(() => {
+    time--;
+    const minutes=Math.floor(time / 60);
+    const seconds= time%60;
 
+    const formattedMinutes= minutes.toString().padStart(2, '0');
+    const formattedSeconds= seconds.toString().padStart(2, '0');
+
+    const formattedTime = `${formattedMinutes}: ${formattedSeconds}`;
+    title.textContent = `Time Remaining : ${formattedTime}`;
+    if (time<=0){
+      clearInterval(timerId);
+      playAlarm();
+    }
+}, 1000);
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
