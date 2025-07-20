4fb37a23721d9fbff55db0c88d6bbffffec97c30
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-20 09:25:32 +0100

    Removing the console.log() functions from the code

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -2,14 +2,11 @@ function createLookup(countryCurrencyPairs) {
   if (countryCurrencyPairs.length == 0){
     return "array is empty"
   }
-  console.log(countryCurrencyPairs.length)
   let countryCurrencyObject = {}
   for (const array of countryCurrencyPairs){
     countryCurrencyObject[array[0]] = array[1];
   }
   return countryCurrencyObject;
 }
-
-console.log(createLookup([]));
 
 module.exports = createLookup;
