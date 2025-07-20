7cfaa761aab240707e1f10eeb216aaa243b210fc
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-20 09:22:50 +0100

    Adding the explanation and updating the console.log.

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,14 @@
 // Predict and explain first...
 
+/*
+At he beginning the code will logs the following:
+bruschetta serves 2, 
+ ingredients: 
+olive oil,tomatoes,salt,pepper
+
+To fix this we need to use .join("\n") to join the ingredients by new line.
+*/
+
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -10,6 +19,5 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
+console.log(`${recipe.title} serves ${recipe.serves}, the ingredients required are:
 ${recipe.ingredients.join("\n")}`);
