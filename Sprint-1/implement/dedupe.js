ba8105d6bd11db2e10803b4a310c4ecffd5379f9
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-20 09:49:11 +0100

    refactor: format code for better readability by adding spacing

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,17 +1,23 @@
 function dedupe(duplicate) {
-    //it checks that the parameter is an array
-if (!Array.isArray(duplicate)) return "not an array";
-//if the array is empty it returns an empty array
-if(duplicate.length===0)return [];
-//declares a variable with the same  value of duplicate but with only unique elements
-const uniqueDuplicate=[...new Set(duplicate)];
-//it checks if the array has any duplicates and if it doesnt it returns a copy of itself
-if(uniqueDuplicate.length===duplicate.length){
-    return [...duplicate];
-// if it does it returns an array which contains only unique elements without duplicates
-} else{
-    return uniqueDuplicate;
-}
+        //it checks that the parameter is an array
+    if (!Array.isArray(duplicate)) return "not an array";
+
+
+        //if the array is empty it returns an empty array
+    if(duplicate.length===0)return [];
+
+
+        //declares a variable with the same  value of duplicate but with only unique elements
+    const uniqueDuplicate=[...new Set(duplicate)];
+
+
+        //it checks if the array has any duplicates and if it doesnt it returns a copy of itself
+    if(uniqueDuplicate.length===duplicate.length){
+        return [...duplicate];
+        // if it does it returns an array which contains only unique elements without duplicates
+    } else{
+        return uniqueDuplicate;
+    }
 }
 console.log(dedupe([]))
 
