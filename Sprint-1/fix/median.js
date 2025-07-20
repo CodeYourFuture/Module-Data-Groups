516cc4bd41bcc035b347338da45f04d45f76096f
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-20 09:49:26 +0100

    fix: correct comments and formatting in median calculation function

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -1,6 +1,6 @@
 // Fix this implementation
 // Start by running the tests for this function
-// If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
+// If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory.
 
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
@@ -9,7 +9,7 @@ function calculateMedian(list) {
   //it checks if the parameter is an array, if not returns null
   if (!Array.isArray(list)) return null;
   //makes a copy of the parameter so the parameter  doesnt get modified while we work with it,and its sorting it ascending
-  const copy=[...list].sort((a,b)=> a-b);
+  //const copy=[...list].sort((a,b)=> a-b);
   
   
   //filter out non-numeric and sort remaining numbers
@@ -19,7 +19,7 @@ function calculateMedian(list) {
     return null;
   }
    // gets the middle of filteredNumbers
-   const middleNumber=Math.floor(filteredNumbers.length/2);
+   const middleNumber = Math.floor(filteredNumbers.length / 2);
    //calculates the median if filteredNumbers is even
    if(filteredNumbers.length%2===0){
     return (filteredNumbers[middleNumber-1]+filteredNumbers[middleNumber])/2;
