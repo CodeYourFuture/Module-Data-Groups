90d74c507a76494cd06500ee831076e41c59c11f
mo-muchunu <mo.muchunu@gmail.com>
2025-07-20 08:31:17 +0100

    Added decoding logic using decodeURIComponent for query string keys and values

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,15 @@
 function parseQueryString(query) {
-  if (!query) return {};                                        // Return an empty object if input is empty, null, or undefined. 
+  if (!query) return {};                                            // Return an empty object if input is empty, null, or undefined. 
 
-  return Object.fromEntries(                                    // Using Object.fromEntries to convert array of [key, value] pairs into an object
-    query.split("&").map((pair) => {                            // Split the query string into individual key-value pairs
+  return Object.fromEntries(                                        // Using Object.fromEntries to convert array of [key, value] pairs into an object
+    query.split("&").map((pair) => {                                // Split the query string into individual key-value pairs
 
 
-      const [key, ...rest] = pair.split("=");                   // Split each pair at the first '='
-      const value = rest.length > 0 ? rest.join("=") : "";      // If at least one after the first '=', join values with '=' as separator or default to empty string
-      return [key, value];                                      
+      const [key, ...rest] = pair.split("=");                       // Split each pair at the first '='
+      const value = rest.length > 0 ? rest.join("=") : "";          // If at least one after the first '=', join values with '=' as separator or default to empty string
+      return [decodeURIComponent(key), decodeURIComponent(value)];  // Decode both key and value                                      
     })
   );
-
 }
 
 module.exports = parseQueryString;
