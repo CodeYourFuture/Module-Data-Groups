312832e3d738c957f2f3677f3e68a59dc9d7ecdb
Hendrine Zeraua <puumotu@hotmail.com>
2025-07-21 13:00:09 +0100

    feat: implement 'contains' function and add corresponding unit tests

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,10 @@
-function contains() {}
-
-module.exports = contains;
+function contains(obj, key) {
+    if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
+      return false; // invalid input — not a plain object
+    }
+  
+    return Object.prototype.hasOwnProperty.call(obj, key);
+  }
+  
+  module.exports = contains;
+  
