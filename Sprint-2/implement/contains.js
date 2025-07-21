ca0427e5087e926a1d01b11c838b899e95820dd3
emladeno <mladenova.elvira@yahoo.com>
2025-07-21 10:51:15 +0100

    fixed indentation code

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,19 +1,17 @@
 function contains(obj, key) {
-if (typeof obj !== 'object' || obj === null) {
-        throw new Error('Invalid input: must be an object');
-    }
-    
-    if (Array.isArray(obj)) {
-        return false;
-    }
-    
-    if (typeof key !== 'string') {
-        return false;
-    }
-    
+  if (typeof obj !== "object" || obj === null) {
+    throw new Error("Invalid input: must be an object");
+  }
 
-    return obj.hasOwnProperty(key);
+  if (Array.isArray(obj)) {
+    return false;
+  }
+
+  if (typeof key !== "string") {
+    return false;
+  }
+
+  return obj.hasOwnProperty(key);
 }
-
 
 module.exports = contains;
