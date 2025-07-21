5be14ef6a70e673510834578de7965f32e24e2cf
Shannon <shannond050@outlook.com>
2025-07-21 21:30:35 +0200

    contains update

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,14 @@
-function contains() {}
+function contains(obj, prop) {
+  if (
+    obj != null &&
+    typeof obj === "object" &&
+    !Array.isArray(obj) &&
+    Object.prototype.hasOwnProperty.call(obj, prop)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = contains;
