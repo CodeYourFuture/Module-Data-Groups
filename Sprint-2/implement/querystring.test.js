d71ddeaa7984add51f4bcdede573fb5124a35f83
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-22 15:37:24 +0100

    added a test to parses querystring with multiple values.

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,10 +3,18 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
+
+
 const parseQueryString = require("./querystring.js")
 
 test("parses querystring values containing =", () => {
   expect(parseQueryString("equation=x=y+1")).toEqual({
     "equation": "x=y+1",
+  });
+});
+test("parses querystring with multiple values", () => {
+  expect(parseQueryString("a=1&b=2")).toEqual({
+    "a": "1",
+    "b": "2",
   });
 });
