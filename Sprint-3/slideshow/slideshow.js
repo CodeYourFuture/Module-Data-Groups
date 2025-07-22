9b3910df178011846558b45372433622753a6418
Sara Tahir <saratahir34@gmail.com>
2025-07-22 12:25:37 +0100

    Added functionality for forward and backward button

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,33 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
-];
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
+  "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?auto=format&fit=crop&w=800&q=80",
+]
 
 
-// Write your code here
+let currentIndex = 0;
+const carouselImg = document.getElementById("carousel-img");
+
+function showNextImage() {
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0; // wrap to start
+  }
+  carouselImg.src = images[currentIndex];
+}
+ document.getElementById("forward-btn").addEventListener("click",function(){
+    showNextImage()
+
+ })
+ function showPreviousImage() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;  // assign the last index to currentIndex
+  }
+  carouselImg.src = images[currentIndex];
+}
+ document.getElementById("backward-btn").addEventListener("click",function(){
+    showPreviousImage()
+
+ })
