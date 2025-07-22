521a882e8d8cd42a16560cdb76500715e5998a1b
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-23 00:14:14 +0100

    implemented the function for tally that except only array input.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,11 @@
-function tally() {}
+function tally(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("Input must be an array");
+  }
+  return arr.reduce((acc, item) => {
+    acc[item] = (acc[item] || 0) + 1;
+    return acc;
+  }, {});
+}
 
 module.exports = tally;
