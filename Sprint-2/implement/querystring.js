08b851a2525bdef44690733045afd14676a88a1e
Shannon <shannond050@outlook.com>
2025-07-22 20:38:01 +0200

    querystring update

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -6,8 +6,26 @@ function parseQueryString(queryString) {
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    const seperatorIndex = pair.indexOf("=");
+
+    let key, value;
+    if (seperatorIndex === -1) {
+      key = decodeURIComponent(pair);
+      value = "";
+    } else {
+      key = decodeURIComponent(pair.slice(0, seperatorIndex));
+      value = decodeURIComponent(pair.slice(seperatorIndex + 1));
+    }
+
+    if (queryParams.hasOwnProperty(key)) {
+      if (Array.isArray(queryParams[key])) {
+        queryParams[key].push(value); // Already an array, just push new value
+      } else {
+        queryParams[key] = [queryParams[key], value]; // Convert to array
+      }
+    } else {
+      queryParams[key] = value; // First time seeing this key
+    }
   }
 
   return queryParams;
