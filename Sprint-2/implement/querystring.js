c2ed514a05a181202b8baab54c60a1842f9cff01
iTeddy16 <tewodros.bekere16@gmail.com>
2025-07-22 15:28:53 +0100

    Refactor parseQueryString function for improved handling of key-value pairs and update tests for accuracy for M-D-G/Sprint-2/implement/querystring.js

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,40 @@
-function parseQueryString(queryString) {
+ function parseQueryString(queryString) {
   const queryParams = {};
+
   if (queryString.length === 0) {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    const index = pair.indexOf("=");
+
+    let key, value;
+
+    if (index !== -1) {
+      key = decodeURIComponent(pair.substring(0, index));
+      value = decodeURIComponent(pair.substring(index + 1));
+    } else {
+      key = decodeURIComponent(pair);
+      value = "";
+    }
+
+    if (queryParams.hasOwnProperty(key)) {
+      if (Array.isArray(queryParams[key])) {
+        queryParams[key].push(value);
+      } else {
+        queryParams[key] = [queryParams[key], value];
+      }
+    } else {
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
 }
+
+
+
 
 module.exports = parseQueryString;
