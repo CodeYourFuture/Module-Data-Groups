7bd8faa47a04e31d375541ab46cb78969c3a3fa6
Waleed Yahya <waleedbluejay7@gmail.com>
2025-07-22 14:58:43 +0100

    implemented a lookup function .

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,10 @@
-function createLookup() {
+function createLookup(pairs) {
   // implementation here
+  const result = {};
+  for (const [country, currency] of pairs) {
+    result[country] = currency;
+  }
+  return result;
 }
 
 module.exports = createLookup;
