063aae8aef48e16347c99a13847c00826451b41f
iTeddy16 <tewodros.bekere16@gmail.com>
2025-07-22 15:40:11 +0100

    Implement tally function to count item frequencies and add corresponding tests for M-D-G/Sprint-2/implement/tally.js

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -20,15 +20,28 @@ const tally = require("./tally.js");
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
 
+  test('returns correct count for mixed items', () => {
+    expect(tally(['a', 'a', 'b', 'c'])).toEqual({ a: 2, b: 1, c: 1 });
+  });
+
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+
+test('returns an empty object for an empty array', () => {
+    expect(tally([])).toEqual({});
+  });
+
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
 
+  test('returns correct count for repeated items', () => {
+    expect(tally(['a', 'a', 'a'])).toEqual({ a: 3 });
+  });
+
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+
