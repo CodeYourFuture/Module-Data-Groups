8238da957484c8acbc9224e6d1372bd1144984ca
roja alagurajan <alagurajan.roja@gmail.com>
2025-07-24 10:12:56 +0100

    alaram_clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,39 @@
-function setAlarm() {}
+let countdownInterval; 
+let timeRemaining = 0; 
+
+function setAlarm() {
+  clearInterval(countdownInterval);
+
+  const inputField = document.getElementById("alarmSet");
+  timeRemaining = parseInt(inputField.value, 10);
+
+  if (isNaN(timeRemaining) || timeRemaining <= 0) {
+    alert("Please enter a valid number greater than 0.");
+    return;
+  }
+
+  updateTitle();
+
+  countdownInterval = setInterval(() => {
+    timeRemaining--;
+
+    if (timeRemaining <= 0) {
+      clearInterval(countdownInterval);
+      playAlarm(); 
+      updateTitle(); 
+    } else {
+      updateTitle(); 
+    }
+  }, 1000);
+}
+
+function updateTitle() {
+  const title = document.getElementById("timeRemaining");
+  const minutes = String(Math.floor(timeRemaining / 60)).padStart(2, "0");
+  const seconds = String(timeRemaining % 60).padStart(2, "0");
+  title.textContent = `Time Remaining: ${minutes}:${seconds}`;
+}
+
 
 // DO NOT EDIT BELOW HERE
 
