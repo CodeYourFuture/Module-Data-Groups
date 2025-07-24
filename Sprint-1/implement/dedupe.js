6b582d3f1c1d937cbdba6b622b0637f2b608d2a9
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-24 16:09:38 +0100

    fix: remove unnecessary blank line in dedupe function

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -3,7 +3,6 @@ function dedupe(duplicate) {
   //it checks that the parameter is an array
   if (!Array.isArray(duplicate)) return "not an array";
 
-
   //declares a variable with the same  value of duplicate but with only unique elements
   const uniqueDuplicate=[...new Set(duplicate)];
 
