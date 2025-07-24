9539ce3b10fbf4195bbd0feba209a6673ef4aa5d
Declan Williams <declanwilliams793@gmail.com>
2025-07-24 13:30:55 +0100

    implement input validation for tally function to ensure array input

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,9 @@
-function tally() {}
+function tally(array) {
+    // ensures that the input is an array.
+    if (!Array.isArray(array)){
+        // throw an error if input is not an array
+        throw new Error("expected input to be an array");
+    }
+}
 
 module.exports = tally;
