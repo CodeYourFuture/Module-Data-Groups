66e679dddf39ba9a0b13f7562caf23243d36f4c3
galyna-k <hkzlvs@gmail.com>
2025-07-24 13:55:04 +0100

    Refactor formatTime in alarmclock.js
    - move padStart logic to a helper function to improve readability;
    - remove duplication in formatting minutes and seconds

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -20,11 +20,12 @@ function setAlarm() {
   let timeLeft = alarmTime;
 
   function formatTime(time) {
-    let minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    return `${minutes.toString().padStart(2, "0")}:${seconds
-      .toString()
-      .padStart(2, "0")}`;
+    const pad = (num) => num.toString().padStart(2, "0");
+
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    return `${pad(minutes)}:${pad(seconds)}`;
   }
 
   function updateTitle(time) {
