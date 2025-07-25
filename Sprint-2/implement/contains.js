854677a281676355e07ed4ed35b825cdf8a6ed9b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-25 17:16:10 +0100

    added null and undefined check

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,5 +1,6 @@
 function contains(obj,prop) {
 
+  if(obj===null || obj=== undefined) return false;
   //it checks if the key part of the object's property includes the second parameter
   if (Object.hasOwn(obj,prop)) {
     return true;
@@ -8,3 +9,4 @@ function contains(obj,prop) {
 }
 
 module.exports = contains;
+
