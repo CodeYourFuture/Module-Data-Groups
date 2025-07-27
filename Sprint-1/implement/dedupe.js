f8c3fa3e99fdd5a8f2797674f767cfba018edf32
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-07-27 20:18:35 +0100

    dedupe is updated

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,7 +1,7 @@
 function dedupe(arr) {
   const resultArray = [];
 
-  if (!Array.isArray(arr) || arr === null ) return null;
+  if (!Array.isArray(arr)) return null;
   if (arr.length == 0) return [];
   for (let i = 0; i < arr.length; i++) {
     if (!resultArray.includes(arr[i])) resultArray.push(arr[i]);
