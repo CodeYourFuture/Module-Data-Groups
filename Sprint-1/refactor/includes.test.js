00ab599e1d5cb0502d6e18ed07c58dba402a306f
hmkavut <63113851+hmkavut@users.noreply.github.com>
2025-07-27 16:23:22 +0100

    misunderstanding

--- Sprint-1/refactor/includes.test.js
+++ Sprint-1/refactor/includes.test.js
@@ -1,39 +1,38 @@
 // Refactored version of includes should still pass the tests below:
 
 const includes = require("./includes.js");
-describe("find Max", () => {
-  it("returns true when target is in array", () => {
-    const currentOutput = includes(["a", "b", "c", "d"], "c");
-    const targetOutput = true;
 
-    expect(currentOutput).toEqual(targetOutput);
-  });
+test("returns true when target is in array", () => {
+  const currentOutput = includes(["a", "b", "c", "d"], "c");
+  const targetOutput = true;
 
-  it("returns false when target not in array", () => {
-    const currentOutput = includes([1, 2, 3, 4], "a");
-    const targetOutput = false;
+  expect(currentOutput).toEqual(targetOutput);
+});
 
-    expect(currentOutput).toEqual(targetOutput);
-  });
+test("returns false when target not in array", () => {
+  const currentOutput = includes([1, 2, 3, 4], "a");
+  const targetOutput = false;
 
-  it("returns true when the target is in array multiple times", () => {
-    const currentOutput = includes([1, 2, 2, 3], 2);
-    const targetOutput = true;
+  expect(currentOutput).toEqual(targetOutput);
+});
 
-    expect(currentOutput).toEqual(targetOutput);
-  });
+test("returns true when the target is in array multiple times", () => {
+  const currentOutput = includes([1, 2, 2, 3], 2);
+  const targetOutput = true;
 
-  it("returns false for empty array", () => {
-    const currentOutput = includes([]);
-    const targetOutput = false;
+  expect(currentOutput).toEqual(targetOutput);
+});
 
-    expect(currentOutput).toEqual(targetOutput);
-  });
+test("returns false for empty array", () => {
+  const currentOutput = includes([]);
+  const targetOutput = false;
 
-  it("searches for null", () => {
-    const currentOutput = includes(["b", "z", null, "a"], null);
-    const targetOutput = true;
+  expect(currentOutput).toEqual(targetOutput);
+});
 
-    expect(currentOutput).toEqual(targetOutput);
-  });
+test("searches for null", () => {
+  const currentOutput = includes(["b", "z", null, "a"], null);
+  const targetOutput = true;
+
+  expect(currentOutput).toEqual(targetOutput);
 });
