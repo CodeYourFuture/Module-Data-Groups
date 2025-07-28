526aa3cdc4b54bc5aeae4e2fddfb625fd7f7c8dd
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-07-28 01:09:12 +0100

    created a sum function which gives a total by adding all the numeric value within an array

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,15 @@
 function sum(elements) {
+  let sortedElements = elements.filter(
+    (value) => typeof value === "number" && !isNaN(value)
+  );
+  if (sortedElements.length === 0 && elements.length > 0) {
+    return NaN;
+  }
+  let total = 0;
+  for (let i = 0; i < sortedElements.length; i++) {
+    total += sortedElements[i];
+  }
+  return total;
 }
 
 module.exports = sum;
