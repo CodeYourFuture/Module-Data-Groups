d5776807ce033571a1c55b2e8e58ab0cfa31f5cc
Ahmad <ahmadehsas14345@gmail.com>
2025-07-28 17:26:21 +0100

    Implemented the deduplicate function  with given arrays and passed the tests cases.

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,15 @@
-function dedupe() {}
+function dedupe(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
+
+  const result = [];
+  for (let item of array) {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+}
+
+module.exports = dedupe;
