88afd1b565b60b1ef1ba0db17dbb2e63fb7685ee
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2025-07-28 13:39:20 +0100

    implement contains function and add tests for various edge cases

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,17 @@
-function contains() {}
+function contains(obj, key) {
+  if (
+    typeof obj !== "object" ||
+    obj === null ||
+    Object.keys(obj).length === 0
+  ) {
+    return false;
+  }
+  for (let keyInObj in obj) {
+    if (keyInObj === key) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = contains;
