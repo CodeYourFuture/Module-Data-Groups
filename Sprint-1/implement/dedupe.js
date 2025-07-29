f4d46ba6fbfc294e05c618af08738fde7921347a
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-07-29 01:20:02 +0100

    created dedupe function which removes duplicate from an array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,12 @@
-function dedupe() {}
+function dedupe(list) {
+  let result = [];
+
+  for (let item of list) {
+    if (!result.includes(item)) {
+      result.push(item);
+    }
+  }
+  return result;
+}
+
+console.log(dedupe([1, 1, 1, 2, 3, 4, 4, 5]));
