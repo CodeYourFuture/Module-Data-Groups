bb2d1e3036112e937d149c4dcc1050cc5a6c62c8
AdiyahFarhan <adiyafarhan110@gmail.com>
2025-07-30 21:50:11 +0100

    Implements the function contains() that checks an object contains a particular property

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(obj, is_property_present) {
+  if (Object.keys(obj).length === 0 && obj.constructor === Object) return false;
+  else if (is_property_present in obj) return true;
+  else !obj || typeof obj !== "object";
+  return false;
+}
 
 module.exports = contains;
