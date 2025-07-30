a4d5622b741ab97498eef7a917d7d2e3c034c915
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-31 00:37:18 +0100

    fix the Indentation

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,19 +1,16 @@
 function dedupe(array) {
-    let arrayCopy = [];
-    if (array.length == 0) {
-        return [];
-    }
-    else if (array.length != 0) {
-        array.forEach(element => {
-            if (!arrayCopy.includes(element)) {
-                arrayCopy.push(element);    
-        }
+  let arrayCopy = [];
+  if (array.length == 0) {
+    return [];
+  } else if (array.length != 0) {
+    array.forEach((element) => {
+      if (!arrayCopy.includes(element)) {
+        arrayCopy.push(element);
+      }
     });
-        return arrayCopy;
-    }
-
-
+    return arrayCopy;
+  }
 }
 
-console.log(dedupe(["a", "b", "c"]))
+console.log(dedupe(["a", "b", "c"]));
 module.exports = dedupe;
