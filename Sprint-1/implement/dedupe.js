1ed85d43241e364499089caaae8afa0043b4eb1d
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-31 17:37:15 +0100

    remove unnecessary else block after return for cleaner control flow

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -2,15 +2,13 @@ function dedupe(array) {
   let arrayCopy = [];
   if (array.length == 0) {
     return [];
-  } else if (array.length != 0) {
-    array.forEach((element) => {
-      if (!arrayCopy.includes(element)) {
-        arrayCopy.push(element);
-      }
-    });
-    return arrayCopy;
   }
+  array.forEach((element) => {
+    if (!arrayCopy.includes(element)) {
+      arrayCopy.push(element);
+    }
+  });
+  return arrayCopy;
 }
 
-console.log(dedupe(["a", "b", "c"]));
 module.exports = dedupe;
