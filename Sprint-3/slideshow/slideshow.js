b9a3663475702a145a4d709a3e3ef0301018e001
iTeddy16 <tewodros.bekere16@gmail.com>
2025-07-31 17:34:12 +0100

    add a title, css and js  to the image carousel to implement navigation functionality for manual  Backward and forward

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -3,6 +3,34 @@ const images = [
     "./assets/cute-cat-b.jpg",
     "./assets/cute-cat-c.jpg",
 ];
-
-
 // Write your code here
+
+let currentIndex = 0;
+let intervalId = null;
+
+const imageElement = document.getElementById("carousel-image");
+
+function updateImage() {
+  imageElement.src = images[currentIndex]; 
+}
+
+function nextImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateImage();
+}
+
+function previousImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length; // Go to previous, wrap to end
+  updateImage();
+}
+
+//forwardBtn.addEventListener("click", nextImage);
+document.getElementById("forward").addEventListener("click", function () {
+  nextImage();
+});
+
+document.getElementById("back").addEventListener("click", function () {
+  previousImage();
+});
+
+updateImage();
