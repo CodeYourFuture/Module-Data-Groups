40de9deb2e6311d27e1cbe73d5a0027f2f2a3b89
iTeddy16 <tewodros.bekere16@gmail.com>
2025-07-31 18:24:48 +0100

    Add more control buttons Auto forward,backward and stop and display current image number.

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -8,10 +8,12 @@ const images = [
 let currentIndex = 0;
 let intervalId = null;
 
+const indexDisplay = document.getElementById("image-index"); 
 const imageElement = document.getElementById("carousel-image");
 
 function updateImage() {
   imageElement.src = images[currentIndex]; 
+  indexDisplay.textContent = `${currentIndex + 1} `;
 }
 
 function nextImage() {
@@ -20,17 +22,46 @@ function nextImage() {
 }
 
 function previousImage() {
-  currentIndex = (currentIndex - 1 + images.length) % images.length; // Go to previous, wrap to end
+  currentIndex = (currentIndex - 1 + images.length) % images.length; 
   updateImage();
 }
 
-//forwardBtn.addEventListener("click", nextImage);
-document.getElementById("forward").addEventListener("click", function () {
-  nextImage();
+function AutoPlay(direction) {
+  stopAutoPlay();
+
+  intervalId = setInterval(function () {
+    if (direction === "forward") {
+      nextImage();
+    } else {
+      previousImage();
+    }
+  }, 1000); // Every 1 seconds
+}
+
+function stopAutoPlay() {
+  clearInterval(intervalId);
+        
+  currentIndex = 0;          // Reset index
+  updateImage();  
+}
+
+document.getElementById("auto-back").addEventListener("click", function () {
+  AutoPlay("back");
 });
 
 document.getElementById("back").addEventListener("click", function () {
   previousImage();
 });
+
+document.getElementById("forward").addEventListener("click", function () {
+  nextImage();
+});
+
+document.getElementById("auto-forward").addEventListener("click", function () {
+  AutoPlay("forward");
+});
+
+document.getElementById("stop").addEventListener("click", stopAutoPlay);
+
 
 updateImage();
