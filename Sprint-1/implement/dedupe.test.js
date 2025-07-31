c29e0ab3b3d2a62ff4ffe24dc9758e6fc5332a1e
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-31 01:21:45 +0100

    update the test to check the reference of the returned array is different from the input array

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,25 +16,22 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
 test("given an empty array, it returns an empty array", () => {
     expect(dedupe([])).toEqual([]);
 });
-
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
-test.todo("Given an array with no duplicates, it return a copy of the original array");
-test("Given an array with no duplicates, it return a copy of the original array", () => {
-    expect(dedupe(["a", "b", "c"])).toEqual(["a", "b", "c"]);
+test("Given an array with no duplicates, it returns a copy of the original array", () => {
+    const input = ["a", "b", "c"];
+    const output = dedupe(input);
+    expect(input).not.toBe(output);
+    expect(input).toEqual(output);
 });
 // Given an array with strings or numbers
 // When passed to the dedupe function
-// Then it should remove the duplicate values, preserving the first occurence of each element
-test.todo("Given an array with strings or numbers, it remove the duplicate values, preserving the first occurence of each element");
+// Then it should remove the duplicate values, preserving the first occurrence of each element
 test("Given an array with strings or numbers, it return an array with no duplicate ", () => {
     expect(dedupe(["a", "a", "b", "c", "a", "d"])).toEqual(["a", "b", "c", "d"]);
     expect(dedupe([1, 2, 2, 3, 4, 5, 6, 6, 9])).toEqual([1, 2, 3, 4, 5, 6, 9]);
-    
-
 });
