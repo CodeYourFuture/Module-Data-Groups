e46a7f33a4db4b565a3b04e5780ddc1ba8669e3a
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-31 14:05:32 +0100

    refactor the function to handle the test cases correctly and handling floating numbers

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,18 +1,24 @@
 function sum(elements) {
-    if (elements.length == 0) {
-        return 0;
-    }
-    else if (elements.length == 1) {
-        return sum[0];
-    }
-    else if (elements.length > 1) {
-        const numbersOnly = elements.filter(element => typeof element === 'number');
-        if (numbersOnly.length == 0) {
-            return Infinity;
-        }
-        const sumValues = numbersOnly.reduce((accumulator, currentValue) => accumulator + currentValue);
-        return sumValues;
-    }
+  if (elements.length === 0) {
+    return 0;
+  }
+  const numbersOnly = elements.filter((element) => Number.isFinite(element));
+  if (numbersOnly.length === 0) {
+    return Infinity;
+  }
+  return numbersOnly.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
 }
-console.log(sum([-1, -5, -3]));
-module.exports = sum;
+/**
+ * Check if the difference between summation value and expected value
+ * is within the range of the tolerance
+ */
+function closeEnough(a, b, tolerance = 0.0001) {
+  return Math.abs(a - b) < tolerance;
+}
+
+console.log(sum([0.005, 0.6, 1.2]));
+
+module.exports = { sum, closeEnough };
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -6,22 +6,21 @@ E.g. sum([10, 20, 30]), target output: 60
 E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical elements)
 */
 
-const sum = require("./sum.js");
-
+const { sum, closeEnough } = require("./sum.js");
 // Acceptance Criteria:
 
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
 test("Given an empty array, should return 0", () => {
-    expect(sum([])).toEqual(0)
+    expect(sum([])).toEqual(0);
 });
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
 test("Given an array with just one number, should return that number", () => {
-    expect(sum([1])).toEqual(sum[0])
+    expect(sum([1])).toEqual(1);
 });
 
 // Given an array containing negative numbers
@@ -29,8 +28,8 @@ test("Given an array with just one number, should return that number", () => {
 // Then it should still return the correct total sum
 test("Given an array containing negative numbers, should still return the correct total sum", () => {
     const input = [-1, -5, -3];
-    const output = -9
-    expect(sum(input)).toEqual(output)
+    const output = -9;
+    expect(sum(input)).toEqual(output);
 });
 
 // Given an array with decimal/float numbers
@@ -38,8 +37,9 @@ test("Given an array containing negative numbers, should still return the correc
 // Then it should return the correct total sum
 test("Given an array with decimal/float numbers, should return the correct total sum", () => {
     const input = [1.5, 2.4, 3.5];
-    const output = 7.4
-    expect(sum(input)).toEqual(output)
+    const result = sum(input);
+    const expected = 7.4;
+    expect(closeEnough(result, expected)).toBe(true);  
 });
 
 // Given an array containing non-number values
@@ -47,8 +47,8 @@ test("Given an array with decimal/float numbers, should return the correct total
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
 test("Given an array containing non-number values, should return the sum of the numerical elements only", () => {
     const input = [1.5, "2.4", 3.5, "hello", 2];
-    const output = 7
-    expect(sum(input)).toEqual(output)
+    const output = 7;
+    expect(sum(input)).toEqual(output);
 });
 
 // Given an array with only non-number values
@@ -57,6 +57,6 @@ test("Given an array containing non-number values, should return the sum of the 
 
 test("Given an array with only non-number values, should return Infinity", () => {
     const input = ["2.4", "hello", null, undefined];
-    const output = Infinity
-    expect(sum(input)).toEqual(output)
+    const output = Infinity;
+    expect(sum(input)).toEqual(output);
 });
