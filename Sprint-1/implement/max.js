d3b97e0fafd5d991f9af681bc8cadf94df24edd9
3bdullah-saleh <abdullahedu0@gmail.com>
2025-07-31 17:54:02 +0100

    handle empty numeric array by returning -Infinity in findMax function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,15 +1,16 @@
 /**
  * find the largest numerical element of an array.
  */
-
 function findMax(elements) {
-    // Function will return '-Infinity', If the array is empty or has only non numeric value.
-    // If the array has only one element, function will return that element as the largest value.
-    // If the array has only negative value, function will return the closest number to zero.
-    // Otherwise, function will return the largest value.
-    const numbersOnly = elements.filter(element => Number.isFinite(element));
-    return  Math.max(...numbersOnly);
-
-}
+  // Function will return '-Infinity', If the array is empty or has only non numeric value.
+  // If the array has only one element, function will return that element as the largest value.
+  // If the array has only negative value, function will return the closest number to zero.
+  // Otherwise, function will return the largest value.
+  const numbersOnly = elements.filter((element) => Number.isFinite(element));
+  if (numbersOnly.length === 0) {
+    return -Infinity;
+  }
+  return Math.max(...numbersOnly);
+};
 
 module.exports = findMax;
