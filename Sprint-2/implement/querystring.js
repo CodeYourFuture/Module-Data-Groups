5d789bc21a4389c8839bfaf4a3f3cbde11e6dbcc
roja alagurajan <alagurajan.roja@gmail.com>
2025-07-31 12:15:54 +0100

    new updateds

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,9 +1,9 @@
-
 function parseQueryString(queryString) {
   const queryParams = {};
   if (queryString.length === 0) {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (let i = 0; i < keyValuePairs.length; i++) {
@@ -11,16 +11,17 @@ function parseQueryString(queryString) {
     const firstEqualIndex = pair.indexOf("=");
 
     if (firstEqualIndex === -1) {
-      // If there is no equal sign, treat the whole pair as a key with an empty value
-      queryParams[pair] = "";
+      // If there is no equal sign, decode the key and assign empty value
+      const key = decodeURIComponent(pair);
+      queryParams[key] = ""; // ✅ use decoded key
     } else {
-      const key = pair.slice(0, firstEqualIndex);
-      const value = pair.slice(firstEqualIndex + 1);
+      const key = decodeURIComponent(pair.slice(0, firstEqualIndex));
+      const value = decodeURIComponent(pair.slice(firstEqualIndex + 1));
       queryParams[key] = value;
-      }
+    }
   }
 
   return queryParams;
 }
-console.log(parseQueryString("equation=x=y+1")); // { equation: 'x=y+1' }
+console.log(parseQueryString("equation=x%3Dy%2B1"));
 module.exports = parseQueryString;
