ce0ebff09d20c1aef15bff85bf2811c2c2720927
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-08-01 15:56:30 +0100

    fixed the bug in the code and explained the reason for it

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-
+// there is a bug in this code, to access  values of ingredients we need to use dot notation (recipe.ingredients) to access it values.
+// In order to print out ingredients in seperate line we can then use for of loop.
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -11,5 +12,7 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:`);
+for (value of recipe.ingredients) {
+  console.log(value);
+}
