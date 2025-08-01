c0879fa45eeb435bcf6cb51a029954db57ec8389
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-08-01 21:44:17 +0100

    implemented a function "contains" based on specific requirements and also created tests using jest for it

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,6 @@
-function contains() {}
+function contains(obj, key) {
+  return Object.hasOwn(obj, key);
+}
 
+console.log(contains([1, 2, 3, 4], "ingredients"));
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,16 +20,39 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+  const currentOutput = contains({});
+  const targetOutput = false;
+  expect(currentOutput).toBe(targetOutput);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-
+test("given an object with properties when passed to contains with existing propery name, it returns true", () => {
+  const currentOutput = contains(
+    { firstName: "Mansoor", organisation: "CYF" },
+    "firstName"
+  );
+  const targetOutput = true;
+  expect(currentOutput).toBe(targetOutput);
+});
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
-
+test("Given an object with properties and when it passed to contains with non-existent property name then it should return false", () => {
+  const currentOutput = contains(
+    { firstName: "Mansoor", organisation: "CYF" },
+    "Name"
+  );
+  const targetOutput = false;
+  expect(currentOutput).toBe(targetOutput);
+});
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("given invalid parameters like an array when passed to contains, it returns false or throw an error", () => {
+  const currentOutput = contains([1, 2, 3, 4], "hello");
+  targetOutput = false;
+  expect(currentOutput).toBe(targetOutput);
+});
