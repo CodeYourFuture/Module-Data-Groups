e622cb5c4fc47ca0e733d187336d05dd2a2d180c
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-08-01 15:31:35 +0100

    fixed the bug in the program and explained why it occurs.

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-
+// it doesnt work as objects are not iteraable and there for we cant use for loops.
+// But by using object.values allows us to get the value within an object and store it in an array and then we can use for of loop.
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +12,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
