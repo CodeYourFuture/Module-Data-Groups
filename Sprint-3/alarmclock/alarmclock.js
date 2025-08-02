4971d4fb8c97f6eecf1f14606abcd371c2617cdc
Dawud Vermeulen <127528069+Dave-Vermeulen@users.noreply.github.com>
2025-08-02 10:34:14 +0000

    alarmclock passes tests. added comments to explain cause it felt bare.

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+let countdown; // just to make it avails for later
+let isPaused = false; // start off false so the timer is not paused
+let timeRemaining; // time remaining in seconds
+
+function setAlarm() {
+  const inputField = document.getElementById("alarmSet"); //get input
+  timeRemaining = parseInt(inputField.value); //set time remaining in seconds
+
+  const updateTitle = () => {
+    const minutes = String(Math.floor(timeRemaining/60)).padStart(2, '0'); // minutes
+    const seconds = String(timeRemaining % 60).padStart(2, '0'); // seconds
+    document.getElementById("timeRemaining").innerText = `Time Remaining: ${minutes}:${seconds}`;
+  };
+
+  updateTitle();
+
+  countdown = setInterval(() => {
+   if (!isPaused){ 
+    timeRemaining = timeRemaining - 1;
+    updateTitle();
+
+    if (timeRemaining <= 0) {
+      clearInterval(countdown);
+      playAlarm();
+    }
+  }
+  }, 1000);
+}
+
+function pauseTimer() {
+  isPaused = !isPaused;
+  document.getElementById("pause").innerText = isPaused ? "Resume Timer" : "Pause Timer";
+}
 
 // DO NOT EDIT BELOW HERE
 
