d5ca4ab50a304e941c7d5212bb3dd893f63cfcc3
Ahmad <ahmadehsas14345@gmail.com>
2025-08-06 00:10:44 +0100

    Fixed the code address object and used dot notation to access the houseNumber property.

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+// The bellow code should be in array format.
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +13,7 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+//console.log(`My house number is ${address[0]}`);
+
+// Here we use dot notation to access the houseNumber property
+console.log(`My house number is ${address.houseNumber}`);
