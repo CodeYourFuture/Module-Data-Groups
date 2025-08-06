e1af6b748ffe9b9d0a92c4fb36325a7221246a88
Hendrine Zeraua <puumotu@hotmail.com>
2025-08-06 14:18:20 +0100

    Use decodeURIComponent to handle percent-encoded characters in query strings

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,3 +1,4 @@
+// Parses a query string into an object, decoding percent-encoded characters in keys and values
 function parseQueryString(queryString) {
   const queryParams = {};
   if (queryString.length === 0) {
@@ -8,15 +9,24 @@ function parseQueryString(queryString) {
 
   for (const pair of keyValuePairs) {
     const indexOfEquals = pair.indexOf("=");
+
     if (indexOfEquals === -1) {
-      queryParams[pair] = "";
+      const decodedKey = decodeURIComponent(pair);
+      queryParams[decodedKey] = "";
       continue;
     }
+
     const key = pair.slice(0, indexOfEquals);
     const value = pair.slice(indexOfEquals + 1);
-    queryParams[key] = value;
+
+    const decodedKey = decodeURIComponent(key);
+    const decodedValue = decodeURIComponent(value);
+
+    queryParams[decodedKey] = decodedValue;
   }
 
   return queryParams;
 }
+
 module.exports = parseQueryString;
+
