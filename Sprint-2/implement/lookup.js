22c40940d0afbe40228e84c97f763f4746a4a222
Ahmad <ahmadehsas14345@gmail.com>
2025-08-07 14:51:34 +0100

    Implemented the createLookup function and passed the test.

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,16 @@
-function createLookup() {
-  // implementation here
+function createLookup(codePairs) {
+  const lookup = codePairs.reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {});
+  return lookup;
 }
+
+console.log(
+  createLookup([
+    ["US", "USD"],
+    ["CA", "CAD"],
+  ])
+);
 
 module.exports = createLookup;
