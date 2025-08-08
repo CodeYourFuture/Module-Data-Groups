aa425682ce4f18489d711f8ac90d56e0a5575f81
Ahmad <ahmadehsas14345@gmail.com>
2025-08-08 22:36:23 +0100

    Implemented the function called tally and passed tests.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,19 @@
-function tally() {}
+function tally(array) {
+  if (!Array.isArray(array)) {
+    throw new Error("Input must be an array");
+    // check if the input ia an array. if not, throw an error.
+  }
+  const tally = {};
+
+  for (const item of array) {
+    tally[item] = (tally[item] || 0) + 1;
+    // If the item already exists in tally, increase its count by 1.
+    // If it doesn't exist, start from 1.
+  }
+
+  return tally;
+}
+
+console.log(tally(["a", "a", "a"]));
 
 module.exports = tally;
