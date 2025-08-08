1231d9c2e9b09f37a6183274859a88e22261a518
Ahmad <ahmadehsas14345@gmail.com>
2025-08-08 17:08:07 +0100

    Fixed the implementation for this test and wrote tests.

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,10 +3,21 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
 test("parses querystring values containing =", () => {
-  expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+  expect(parseQueryString("equation=x=y+1")).toStrictEqual({
+    equation: "x=y+1",
+  });
+});
+
+test("returns empty object for empty query string", () => {
+  expect(parseQueryString("")).toStrictEqual({});
+});
+
+test("parses multiple parameters", () => {
+  expect(parseQueryString("name=John&age=30")).toStrictEqual({
+    name: "John",
+    age: "30",
   });
 });
