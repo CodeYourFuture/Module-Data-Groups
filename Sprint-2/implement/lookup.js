7238ca1a9861e2ae7c45b3d4954ad1f7d1119bca
Waleed Yahya <waleedbluejay7@gmail.com>
2025-08-12 03:07:06 +0100

    fixed the implementation of the function.

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,10 +1,17 @@
 function createLookup(pairs) {
   // implementation here
-  const result = {};
-  for (const [country, currency] of pairs) {
-    result[country] = currency;
+    if (!Array.isArray(pairs)) {
+    throw new Error("Input must be an array of [key, value] pairs");
   }
-  return result;
+
+  return pairs.reduce((acc, pair) => {
+    if (!Array.isArray(pair) || pair.length !== 2) {
+      throw new Error("Each item must be an array with exactly 2 elements");
+    }
+    const [countryCode, currencyCode] = pair;
+    acc[countryCode] = currencyCode;
+    return acc;
+  }, {});
 }
 
 module.exports = createLookup;
