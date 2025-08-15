f780923d8a1f0fbc3ffa5efb68a286ffc7634140
Nahom Mesfin <mnahom545@gmail.com>
2025-08-15 12:54:18 +0100

    contains key test

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,20 +16,39 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
+describe("contains", () => {
+    test("returns true if the object contains the property, false otherwise", () => {
+        expect(contains({CYF:3,ITP:5}, "ITP")).toBe(true);
+    })
 
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+    expect(contains({}, "a")).toBe(false);
+  });
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-
+  test("returns true for an existing property", () => {
+    expect(contains({ a: 1, b: 2 }, "a")).toBe(true);
+    expect(contains({ x: 10, y: 20 }, "y")).toBe(true);
+  });
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+  test("returns false for a non-existent property", () => {
+    expect(contains({ a: 1, b: 2 }, "c")).toBe(false);
+    expect(contains({ x: 10, y: 20 }, "z")).toBe(false);
+  });
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+ test("returns false for invalid parameters like an array", () => {
+    expect(contains([], "a")).toBe(false);
+    expect(contains(null, "a")).toBe(false);
+    expect(contains("string", "a")).toBe(false);
+  });
+});
