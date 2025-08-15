6425fff8480253c5926c1d2e6e7a5b899074c5e0
Mansoor Munawar <mansoor.munawar448@gmail.com>
2025-08-15 11:28:14 +0100

    refactor contains function to handle 'length' key for arrays

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,6 +1,9 @@
 function contains(obj, key) {
+  if (Array.isArray(obj) && key === "length") {
+    return false;
+  }
   return Object.hasOwn(obj, key);
 }
 
-console.log(contains([1, 2, 3, 4], "ingredients"));
+console.log(contains([1, 2, 3, 4], "length"));
 module.exports = contains;
