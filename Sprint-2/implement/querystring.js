ec864238b58fbf478a57eb6d1fddc2b37bddb5bd
AjueborChukwuemeke <chukwuemekeajuebor247@gmail.com>
2025-08-16 19:10:00 +0100

    implement_querystring

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -5,8 +5,21 @@ function parseQueryString(queryString) {
   }
   const keyValuePairs = queryString.split("&");
 
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+  for (let i = 0; i < keyValuePairs.length; i++) {
+    const pair = keyValuePairs[i];
+    const equalIndex = pair.indexOf("=");
+
+    let key, value;
+
+    if (equalIndex === -1) {
+      key = pair;
+      value = "";
+    } // if no "=" is found key is assigned with a value of empty string
+    else {
+      key = pair.substring(0, equalIndex);
+      value = pair.substring(equalIndex + 1);
+    } // Split into key and value for as many "=" in the value using substring
+
     queryParams[key] = value;
   }
 
--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -6,7 +6,28 @@
 const parseQueryString = require("./querystring.js")
 
 test("parses querystring values containing =", () => {
-  expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+  expect(parseQueryString("equation=x=y+1")).toEqual({"equation": "x=y+1"})
+});
+test("returns empty object for empty query string", () => {
+  expect(parseQueryString("")).toEqual({});
+});
+
+test("parses single key=value pair", () => {
+  expect(parseQueryString("a=1")).toEqual({ a: "1" });
+});
+
+test("parses multiple key=value pairs", () => {
+  expect(parseQueryString("a=1&b=2&c=3")).toEqual({
+    a: "1",
+    b: "2",
+    c: "3",
   });
+});
+
+test("parses key without value (assigns empty string)", () => {
+  expect(parseQueryString("a=&b=2")).toEqual({a: "", b: "2"});
+});
+
+test("handles key with no '=' at all", () => {
+  expect(parseQueryString("hello")).toEqual({hello: ""});
 });
