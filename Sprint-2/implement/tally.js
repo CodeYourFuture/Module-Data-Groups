34a3801cb004d4a4f8f1e4309499ee483ff2996c
AjueborChukwuemeke <chukwuemekeajuebor247@gmail.com>
2025-08-16 18:10:52 +0100

    implement_tally

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,21 @@
-function tally() {}
+function tally(arr) {
+    if (!Array.isArray(arr)) {
+    throw new Error("Input an array!");
+  }
+
+  const counts = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    if (counts[item]) {
+      counts[item] += 1;
+    } else {
+      counts[item] = 1;
+    }// if/else ensures add to the count of an item if it has been
+    // counted in a previous iteration or start at 1.  
+  }
+
+  return counts;
+}
 
 module.exports = tally;
