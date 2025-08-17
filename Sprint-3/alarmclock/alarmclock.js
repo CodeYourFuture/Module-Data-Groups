926063ef2984a9d8125f8cc2812375eddad681ed
AjueborChukwuemeke <chukwuemekeajuebor247@gmail.com>
2025-08-17 18:07:00 +0100

    implementation

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,51 @@
-function setAlarm() {}
+//this variable holds the time independent of the function
+let Timer = null;
+
+function setAlarm() {
+  //this pulls the set time from whatever is entered as input on the html page
+  let inputValue = document.getElementById("alarmSet").value;
+
+  let remainingTime = Number(inputValue);
+  
+  if (isNaN(remainingTime) || remainingTime <= 0) {
+    return;
+  }
+
+  //Time on screen in MM:SS format
+  updateTimerDisplay(remainingTime);
+  
+  if (Timer !== null) {
+    clearInterval(Timer);
+  }
+  
+  Timer = setInterval(function () {
+    remainingTime = remainingTime - 1; //timer is reduced by 1 second
+    
+    updateTimerDisplay(remainingTime); //then updated
+
+    // When timer hits zero, stop timer and play alarm
+    if (remainingTime <= 0) {
+      clearInterval(Timer);
+      playAlarm();
+    }
+  }, 1000);
+}
+
+
+function updateTimerDisplay(seconds) {
+  let minutes = Math.floor(seconds / 60);
+  let secs = seconds % 60;
+
+  // Puts the figures in strings and in a clock-like format
+  let minuteStr = String(minutes).padStart(2, "0");
+  let secondStr = String(secs).padStart(2, "0");
+
+  // Update the heading text
+  document.getElementById(
+    "timeRemaining"
+  ).innerText = `Time Remaining: ${minuteStr}:${secondStr}`;
+
+}
 
 // DO NOT EDIT BELOW HERE
 
