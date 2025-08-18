af330b4cb8325a5d78c49bcb041226fd22b94f6f
JenniferIsidienu <jennieisidienu@gmail.com>
2025-08-18 14:10:56 +0100

    Sprint3 Alarm clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+  let countdownTimer;
+  let timeRemaining = 0;
+
+function setAlarm() {
+  const input = document.getElementById("alarmSet");
+  timeRemaining = Number(input.value);
+
+  updateDisplay(timeRemaining);
+  clearInterval(countdownTimer);
+
+  countdownTimer = setInterval(function () {
+    timeRemaining--;
+
+    if (timeRemaining <= 0) {
+      clearInterval(countdownTimer);
+      updateDisplay(0);
+      playAlarm();
+    } else {
+      updateDisplay(timeRemaining);
+    }
+  }, 1000);
+}
+function updateDisplay(seconds) {
+  const heading = document.getElementById("timeRemaining");
+
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+
+  const minuteStr = String(minutes).padStart(2, '0');
+  const secondStr = String(secs).padStart(2, '0');
+
+  heading.innerText = `Time Remaining: ${minuteStr}:${secondStr}`; 
+  }
 
 // DO NOT EDIT BELOW HERE
 
