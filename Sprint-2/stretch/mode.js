3b8b32230c8ba90e8cc5b7ab5f9c5f1b237fd8d9
Samiuk <lahlahhoussam80@gmail.com>
2025-09-13 14:40:31 +0100

    refactor: split calculateMode into smaller functions

--- Sprint-2/stretch/mode.js
+++ Sprint-2/stretch/mode.js
@@ -1,28 +1,22 @@
-// You are given an implementation of calculateMode
-
-// calculateMode's implementation can be broken down into two stages:
-
-// Stage 1. One part of the code tracks the frequency of each value
-// Stage 2. The other part finds the value with the highest frequency
-
-// refactor calculateMode by splitting up the code
-// into smaller functions using the stages above
-
-function calculateMode(list) {
-  // track frequency of each value
+// Stage 1. Track frequency of each value
+function buildFrequencyMap(list) {
   let freqs = new Map();
 
   for (let num of list) {
     if (typeof num !== "number") {
-      continue;
+      continue; // skip non-number values
     }
-
     freqs.set(num, (freqs.get(num) || 0) + 1);
   }
 
-  // Find the value with the highest frequency
+  return freqs;
+}
+
+// Stage 2. Find the value with the highest frequency
+function findMode(freqs) {
   let maxFreq = 0;
   let mode;
+
   for (let [num, freq] of freqs) {
     if (freq > maxFreq) {
       mode = num;
@@ -33,4 +27,11 @@ function calculateMode(list) {
   return maxFreq === 0 ? NaN : mode;
 }
 
+// Main function
+function calculateMode(list) {
+  const freqs = buildFrequencyMap(list);
+  return findMode(freqs);
+}
+
 module.exports = calculateMode;
+
