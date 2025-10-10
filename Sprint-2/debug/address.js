cd4cb2b536183dd3e3644890b816014940eb5f99
shreefAhmedM <livecoding.33@gmail.com>
2025-10-10 19:20:58 +0100

    refactoring adddress.js file

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,9 +1,7 @@
 // Predict and explain first...
-
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
-
 const address = {
   houseNumber: 42,
   street: "Imaginary Road",
@@ -15,6 +13,7 @@ const address = {
 // console.log(`My house number is ${address[0]}`);
 // is trying to access a property using address[0] and address is an object, not an array.
 // it will log undefined.
-// to access the houseNumber property, we should use dot notation or bracket notation with string.
+// to access the houseNumber property, we should use dot notation or bracket notation with string,and either
+// way using  object.key or object["key"]
 console.log(`My house number is ${address.houseNumber}`);
 console.log(`My house number is ${address["houseNumber"]}`);
