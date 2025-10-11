8bd2a03547e028dd4e7184c9860bf12af0115700
shreefAhmedM <livecoding.33@gmail.com>
2025-10-11 12:56:31 +0100

    implementing function that takes all a list of items and count the freguency of each items in tally.js file

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,11 @@
-function tally() {}
-
+function tally(arr) {
+  const output = {};
+  if (!Array.isArray(arr)) {
+    throw new Error("Input must be an array");
+  }
+  arr.forEach((element) => {
+    output[element] = (output[element] || 0) + 1;
+  });
+  return output;
+}
 module.exports = tally;
--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -19,16 +19,26 @@ const tally = require("./tally.js");
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
-
+test("Given a function tally(['a']), target output should be { a: 1 }", () => {
+  expect(tally(["a", "a", "a"])).toEqual({ a: 3 });
+});
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
 test.todo("tally on an empty array returns an empty object");
-
+test("tally on an empty array returns an empty object, target output should be {}", () => {
+  expect(tally([])).toEqual({});
+});
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+test("Given a function tally(['a']), target output should be { a: 1 }", () => {
+  expect(tally(["a", "a", "b", "c"])).toEqual({ a: 2, b: 1, c: 1 });
+});
 
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("Given an invalid input like a string,Then it should throw an error", () => {
+  expect(() => tally("string")).toThrow("Input must be an array");
+});
