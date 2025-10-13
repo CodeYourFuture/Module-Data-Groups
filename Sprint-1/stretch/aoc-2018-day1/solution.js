88158136ed0f8438ef799773c02dff16bd019107
TzeMingHo <tzemingho@gmail.com>
2025-10-13 10:33:30 +0100

    updated solution.js

--- Sprint-1/stretch/aoc-2018-day1/solution.js
+++ Sprint-1/stretch/aoc-2018-day1/solution.js
@@ -1,10 +1,22 @@
 const fs = require("fs");
+const path = require("path");
 
-// read the file
-let frequencies = fs.readFileSync("input.txt", "utf-8");
+function solution() {
+  // set the file path
+  const filePath = path.join(__dirname, "input.txt");
 
-// split the numbers by next line and covert them into Number
-frequencies = frequencies.split("\n").map((num) => Number(num));
+  // read the file
+  let frequencies = fs.readFileSync(filePath, "utf-8");
 
-// reduce to get the total = 529
-console.log(frequencies.reduce((acc, num) => acc + num, 0));
+  // split the numbers by next line and covert them into Number
+  frequencies = frequencies.split("\n").map((num) => Number(num));
+
+  // reduce to get the total = 529
+  const total = frequencies.reduce((acc, num) => acc + num, 0);
+
+  return total;
+}
+
+console.log(solution());
+
+module.exports = solution;
