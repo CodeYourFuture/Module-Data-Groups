cf3676ff6e21cfddbb243b692643025331751af5
shreefAhmedM <livecoding.33@gmail.com>
2025-10-14 14:04:42 +0100

    implementing and built alarm clock app

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,34 @@
-function setAlarm() {}
+let alarmTime;
+let countDown;
+function setAlarm() {
+  const timeRemaining = document.getElementById("timeRemaining");
+  const alarmSet = document.getElementById("alarmSet").value;
+  alarmTime = parseInt(alarmSet);
+  if (countDown) {
+    clearInterval(countDown);
+  }
+  countDown = setInterval(startCount, 1000);
+  updateDisplay(timeRemaining);
+}
+function startCount() {
+  if (alarmTime > 0) {
+    alarmTime--;
+    updateDisplay(document.getElementById("timeRemaining"));
+  } else {
+    clearInterval(countDown);
+    playAlarm();
+    document.body.style.background = "#e14343";
+  }
+}
 
+function updateDisplay(timeRemainingElement) {
+  let minutes = Math.floor(alarmTime / 60);
+  let seconds = alarmTime % 60;
+  timeRemainingElement.innerHTML = `Time Remaining: ${String(minutes).padStart(
+    2,
+    "0"
+  )}:${String(seconds).padStart(2, "0")}`;
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
