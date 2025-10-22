d2e4bd25ac2766c692b123a87a61672ea1adef02
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-10-22 01:34:31 +0100

    created the function dedupe & sat test cases

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,7 @@
-function dedupe() {}
+function dedupe(arr) {
+    const newArr = new Set(arr); // Using `new Set()` to store only unique values in newArr
+    return Array.from(newArr); // return athe new array using `Array.from()`
+}
+
+console.log(dedupe(['a','V','a','b','b','c'])); // ['a','b','c']
+module.exports = dedupe;
