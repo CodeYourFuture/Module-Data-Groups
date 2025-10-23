98abdd3f320c4d3bc29b10bebdcc0f9af9d855f0
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-10-23 10:50:34 +0100

    refactored includes() in includes.js

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,9 @@
 // Refactor the implementation of includes to use a for...of loop
 
-function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
-      return true;
-    }
-  }
-  return false;
+const includes = (list, target) =>{
+  for (item of list)
+      if ( item == target ) return true;
+  return false; 
 }
 
 module.exports = includes;
