6e09b6448d76fef3e0693cfe6867562779773f23
iswat <ibeetek@gmail.com>
2025-10-23 12:07:07 +0100

    Add initial mean.js and mean.test.js files

---
+++ prep/mean.js
@@ -0,0 +1,9 @@
+function calculateMean(list) {
+    let total = 0;
+     
+    for (const item of list) {
+        total += item;
+    }
+}
+
+calculateMean([1,2,3,4,5]);
---
+++ prep/mean.test.js
@@ -0,0 +1,7 @@
+Test("Calculate the average of a list of numbers", () => {
+  const list = [3, 50, 7];
+  const currentOutput = calculateMean(list);
+  const targetOutput = 20; // 20 is (3 + 50 + 7) / 3
+  
+  expect(currentOutput).toEqual(targetOutput);
+});
