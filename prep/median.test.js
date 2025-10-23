49b82d5e422e6703f028ff69669b697aea6145a7
iswat <ibeetek@gmail.com>
2025-10-23 12:30:58 +0100

    Write a test for the function in the median.js file.

---
+++ prep/median.test.js
@@ -0,0 +1,7 @@
+test("Calculate the median of the list of numbers", () => {
+    list = [10, 20, 30, 40, 50];
+    const currentOutput = calaculateMedian(list);
+    const targetOutput = 30;
+
+    expect(currentOutput).toEqual(targetOutput);
+})
