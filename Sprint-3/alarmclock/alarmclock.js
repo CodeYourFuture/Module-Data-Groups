82a89ff4c4788fe80fc9a6ea50749107e3c5e2cf
TzeMingHo <tzemingho@gmail.com>
2025-10-23 18:00:57 +0100

    completed the tests and its function, added some styling

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,33 @@
-function setAlarm() {}
+function calculateAndUpdate(seconds, timeRemaining) {
+  let mm = String(Math.floor(seconds / 60)).padStart(2, 0);
+  let ss = String(seconds % 60).padStart(2, 0);
+  timeRemaining.innerText = `Time Remaining: ${mm}:${ss}`;
+}
+
+function setAlarm() {
+  let seconds = document.getElementById("alarmSet").value;
+  let timeRemaining = document.getElementById("timeRemaining");
+  const alarmInputField = document.getElementById("alarmInputField");
+
+  if (seconds > 0) {
+    calculateAndUpdate(seconds, timeRemaining);
+
+    const intervalId = setInterval(() => {
+      seconds -= 1;
+      calculateAndUpdate(seconds, timeRemaining);
+
+      if (seconds == 0) {
+        clearInterval(intervalId);
+        playAlarm();
+        document.body.classList.add("flash-background");
+      }
+    }, 1000);
+  }
+}
+
+document.getElementById("stop").addEventListener("click", () => {
+  document.body.classList.remove("flash-background");
+});
 
 // DO NOT EDIT BELOW HERE
 
