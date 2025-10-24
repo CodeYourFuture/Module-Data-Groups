22abc47946189c101917a77af63b9c1e7fc1bd83
iswat <ibeetek@gmail.com>
2025-10-24 16:07:16 +0100

    Import calculateMean function from mean.js

--- prep/mean.test.js
+++ prep/mean.test.js
@@ -1,4 +1,6 @@
-Test("Calculate the average of a list of numbers", () => {
+const calculateMean = require("./mean");
+
+test("Calculate the average of a list of numbers", () => {
   const list = [3, 50, 7];
   const currentOutput = calculateMean(list);
   const targetOutput = 20; // 20 is (3 + 50 + 7) / 3
