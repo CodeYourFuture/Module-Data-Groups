935e47ed28c003f1ecdbe7ca2bb8d3818a3de012
iswat <ibeetek@gmail.com>
2025-10-24 10:25:37 +0100

    Add a return statement to the function calculateMean

--- prep/mean.js
+++ prep/mean.js
@@ -4,6 +4,9 @@ function calculateMean(list) {
     for (const item of list) {
         total += item;
     }
+    console.log("the total of the array is", total);
+    console.log("the element has", list.length, "elements");
+    return total / list.length;
 }
 
 calculateMean([1,2,3,4,5]);
