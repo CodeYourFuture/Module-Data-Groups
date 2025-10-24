e0bed316dfe739315cf391e660e7ef7ca1f75506
iswat <ibeetek@gmail.com>
2025-10-24 16:07:36 +0100

    Import calculateMedian function from median.js

--- prep/median.test.js
+++ prep/median.test.js
@@ -1,7 +1,13 @@
+const calaculateMedian = require("./median");
+
 test("Calculate the median of the list of numbers", () => {
-    list = [10, 20, 30, 40, 50];
+    const list = [10, 20, 30, 40, 50];
     const currentOutput = calaculateMedian(list);
     const targetOutput = 30;
 
     expect(currentOutput).toEqual(targetOutput);
 })
+
+// test("should not mutate the list of numbers", () => {
+//     const list = 
+// })
