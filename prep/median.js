4fe7d13636b6365c327ea6f75ab734203000f15f
iswat <ibeetek@gmail.com>
2025-10-26 19:25:59 +0000

    Comment out function that mutates array with splice

--- prep/median.js
+++ prep/median.js
@@ -1,8 +1,18 @@
-function calculateMedian(list) {
-    const middleIndex = Math.floor(list.length / 2);
-    const median = list.splice(middleIndex, 1)[0];
+// How to calculate the median of an array by mutating the array
 
-    return median;
-}
+// function calculateMedian(list) {
+//   const middleIndex = Math.floor(list.length / 2);
+//   const median = list.splice(middleIndex, 1)[0];
+//   console.log("middleIndex", middleIndex);
+//   console.log(list);
+//   return median;
+// }
 
+// const actualOutput = calculateMedian([1, 2, 3]);
+// console.log("median", actualOutput);
 module.exports = calculateMedian;
+
+// How to calculate the median of an array without muatating the array
+function calculateMedian(list) {
+  const arrayLength = list.length;
+}
