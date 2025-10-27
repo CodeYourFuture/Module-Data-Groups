3449f32621c5f610dad32f16f2d07e3c271cbf68
iswat <ibeetek@gmail.com>
2025-10-27 11:03:06 +0000

    Add Jest test case to verify that the function implementation does not mutate the array

--- prep/median.test.js
+++ prep/median.test.js
@@ -1,13 +1,17 @@
-const calaculateMedian = require("./median");
+const calculateMedian = require("./median");
 
 test("Calculate the median of the list of numbers", () => {
-    const list = [10, 20, 30, 40, 50];
-    const currentOutput = calaculateMedian(list);
-    const targetOutput = 30;
+  const list = [10, 20, 30, 40, 50];
+  const currentOutput = calculateMedian(list);
+  const targetOutput = 30;
 
-    expect(currentOutput).toEqual(targetOutput);
-})
+  expect(currentOutput).toEqual(targetOutput);
+});
 
-// test("should not mutate the list of numbers", () => {
-//     const list = 
-// })
+test("should not modify the input", () => {
+  const list = [1, 2, 3];
+  calculateMedian(list);
+
+  expect(list).toEqual([1, 2, 3]);
+});
+
