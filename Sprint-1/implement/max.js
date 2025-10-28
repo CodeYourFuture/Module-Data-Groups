4938239b3a26710b68ae0d3615aa4ca16d5b2c63
Della Bella <gisdellabella@uol.com.br>
2025-10-28 22:46:02 +0000

    Max Excercise

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,9 @@
-function findMax(elements) {
+function findMax(numbersList) {
+    let onlyNumbers = numbersList.filter((item) => typeof item === "number");
+ let currentMax = Math.max(...onlyNumbers); ;
+  return currentMax;
 }
+
+console.log(findMax([1, "apple", 5, 0, "banana", 3]));
 
 module.exports = findMax;
