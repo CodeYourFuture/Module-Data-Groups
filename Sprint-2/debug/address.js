d6b4ea3598b7fe0caae5a347915840b04aea55b2
Imran Mohamed <info@imranbmohamed.com>
2025-10-29 10:53:07 +0000

    completed tasks set out in debug folder

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
 
+//The code will not log the house number as it is using the wrong syntax to access the property.
+
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +14,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
