e90d9dd16de5d68acaa6609b0559c94a44db80d1
AdnaanA <thetriplex247@gmail.com>
2025-10-30 11:00:59 +0000

    fixed function and added test-cases for tally

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,18 @@
-function tally() {}
+function tally(items) {
+  if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
 
+  const result = {};
+
+  for (const item of items) {
+    if (result[item]) {
+      result[item]++;
+    } else {
+      result[item] = 1;
+    }
+  }
+
+  return result;
+}
 module.exports = tally;
