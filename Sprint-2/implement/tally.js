2556038fcb8a15406d5654b332f1539076cc76d6
Imran Mohamed <imran.b.mohamed@gmail.com>
2025-11-01 14:21:44 +0000

    implement tally function and corresponding tests for various cases

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,12 @@
-function tally() {}
+function tally(arr) {
+    if (!Array.isArray(arr) || arguments.length !== 1) {
+        throw new Error("Input must be an array");
+    }
+    const result = {};
+    for (const item of arr) {
+        result[item] = (result[item] || 0) + 1;
+    }
+    return result;
+}
 
 module.exports = tally;
