88176351e6e494bb24aa8f771ee84535f5d490b3
Fares Bakhet <faresak47roo@gmail.com>
2025-11-04 19:30:35 +0000

    Implement findMax function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,23 @@
-function findMax(elements) {
+function findMax(num) {
+  for (let i = 0; i < num.length; i++) {
+    if (num.length === 0) {
+      return -Infinity;
+    }
+    if (num.length === 1) {
+      return num[0];
+    }
+    if (Math.sign(num) === -1 && num.length > 1) {
+      return Math.max(...num);
+    }
+    if (num.isDecimal === true) {
+      return Math.max(...num);
+    }
+    if (typeof num[i] !== "number") {
+      num.splice(i, 1);
+      i--;
+    }
+  }
+  return Math.max(...num);
 }
 
 module.exports = findMax;
