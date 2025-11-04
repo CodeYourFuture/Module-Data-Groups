f2036d32a420bea434788c7ccd41a5a540077b14
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-11-04 10:32:23 +0000

    declared var inside for..of loop

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,9 +1,11 @@
 // Refactor the implementation of includes to use a for...of loop
 
+// "use strict";
 const includes = (list, target) =>{
-  for (item of list)
+  for (const item of list)
       if ( item == target ) return true;
   return false; 
 }
 
+console.log(includes([1,2,3,4,5], 3));
 module.exports = includes;
