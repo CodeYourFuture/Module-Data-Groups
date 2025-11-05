61a84cea5f5ac1ed271774c02582def633a4efcb
Fares Bakhet <faresak47roo@gmail.com>
2025-11-05 17:26:52 +0000

    Implementing a sum function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,15 @@
+const { array, string } = require("yargs");
+
 function sum(elements) {
+  let totalSum = 0;
+  for (let i = 0; i < elements.length; i++) {
+    if (typeof elements[i] === "number") {
+      totalSum += elements[i];
+    }
+  }
+  return totalSum;
 }
+
+console.log(sum(["cow", "true", "ana"]));
 
 module.exports = sum;
