54e1a28b410da1df8581df79963d6fb130c2a364
iswat <ibeetek@gmail.com>
2025-11-05 13:54:18 +0000

    Add implementation for when empty array is passed as input

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -2,6 +2,10 @@ function dedupe(arrayInput) {
    if (!Array.isArray(arrayInput)) {
     throw new TypeError("Input must be an array");
    }
+
+   if (arrayInput.length === 0) {
+    return [];
+   }
 }
 
 console.log(dedupe("food"));
