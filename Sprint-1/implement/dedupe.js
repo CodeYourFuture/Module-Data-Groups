ae9fb0ecc9cee19079f6c6c6004b570d61c2b0c1
iswat <ibeetek@gmail.com>
2025-11-05 13:49:06 +0000

    Add input validation for non-array values

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,6 +1,9 @@
 function dedupe(arrayInput) {
-   
+   if (!Array.isArray(arrayInput)) {
+    throw new TypeError("Input must be an array");
+   }
 }
 
+console.log(dedupe("food"));
 
 module.exports = dedupe;
