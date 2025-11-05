b43986ff1be0fee2b28de5b703b4d7248831d814
Della Bella <gisdellabella@uol.com.br>
2025-11-05 23:39:29 +0000

    Recipes Object Practice

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,15 +1,23 @@
 // Predict and explain first...
-
+// the code try to print a array in a single line of ingredients and also has 
+// syntax erros 
 // This program should log out the title, how many it serves and the ingredients.
+// I need to get the values but firnt need to make the object interable=use Object.value 
 // Each ingredient should be logged on a new line
 // How can you fix it?
 
 const recipe = {
-  title: "bruschetta",
-  serves: 2,
-  ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
+title: "bruschetta",
+serves: 2,
+ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+let menuList = Object.values(recipe.ingredients);
+
+console.log (`${recipe.title}`);
+ console.log (`serves ${recipe.serves}`);
+
+
+for (const ingredientName of menuList) {
+console.log(ingredientName);
+}
