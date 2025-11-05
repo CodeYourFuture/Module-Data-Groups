66d17135887247385cecb0a90a6e76d570971d22
basel <adoumbasel@gmail.com>
2025-11-05 19:06:54 +0100

    doubleNumber eercise

--- prep/doubleNumber.js
+++ prep/doubleNumber.js
@@ -1,14 +1,24 @@
 // Can you fix this code?
-function doubleAllNumbers() {
-  let doubledNumbers;
+// function doubleAllNumbers(numbers) {
+//   let doubledNumbers= []
 
+//   for (let n of numbers) {
+//     doubledNumbers.push(n * 2);
+//   }
+
+//   return doubledNumbers;
+// }
+
+// const myNums = [10, 20, 30];
+// console.log(doubleAllNumbers(myNums));
+
+// make it better code
+
+function doubleAllNumbers(numbers) {
+  let doubledNumbers = [];
   for (let n of numbers) {
     doubledNumbers.push(n * 2);
   }
-
   return doubledNumbers;
 }
-
-const myNums = [10, 20, 30];
-doubleAllNumbers(myNums);
-console.log(myNums);
+module.exports = doubleAllNumbers;
