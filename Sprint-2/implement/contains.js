280a02cbfdc31c003c668a2eb4b4602939ae2464
shreefAhmedM <livecoding.33@gmail.com>
2025-11-06 10:22:28 +0000

    renaming parameter name to be more meaningful

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,5 +1,5 @@
-function contains(obj, prop) {
-  return obj && typeof obj === "object" && prop in obj;
+function contains(obj, propertyName) {
+  return obj && typeof obj === "object" && propertyName in obj;
 }
 // console.log(contains(({ a: 1, b: 2 }, "c")));
 module.exports = contains;
