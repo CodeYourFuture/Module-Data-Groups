11e30f5fccbd7900ee868dc9fadf058fa334db78
Baba05206 <babatunde.yusuf@outlook.com>
2025-11-06 18:05:48 +0000

    Add initial implementation of mean calculation and tests

---
+++ prep/mean.js
@@ -0,0 +1,37 @@
+/*
+Let’s consider a list of prices in a bill:
+4.6, 5.03, 7.99, 8.01
+instead of writing the  like below
+const price0 = 4.6;
+const price1 = 5.03;
+const price2 = 7.99;
+const price3 = 8.01;
+We can write it as an array literal
+const items = [4.6, 5.03, 7.99, 8.01];
+
+The Array object, as with arrays in other programming languages, enables storing a collection
+of multiple items under a single variable name, and has members for performing common array operations.
+Arrays can store items of any type & multiple pieces of information.
+
+In JavaScript, we can use [] notation to access specific elements in the array using index numbers.
+The index numbers start from 0.
+
+
+const items = [4.6, 5.03, 7.99, 8.01];
+console.log(items[0]); // 4.6
+console.log(items[1]); // 5.03
+console.log(items[2]); // 7.99
+console.log(items[3]); // 8.01
+// Accessing elements using index numbers
+*/
+const items = [4.6, 5.03, 7.99, 8.01];
+function calculateMean(list) {
+  // Calculate the sum of all elements in the array
+  const sum = list.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  // Calculate the mean by dividing the sum by the number of elements
+  const mean = sum / list.length;
+  return mean;
+}
---
+++ prep/mean.test.js
@@ -0,0 +1,12 @@
+/*
+test("does something as described below", () => {   
+    // test implementation goes here    
+});
+*/
+test("calculates the mean of a list of numbers", () => {
+  const list = [3, 50, 7];
+  const currentOutput = calculateMean(list);
+  const targetOutput = 20;
+
+  expect(currentOutput).toEqual(targetOutput); // 20 is (3 + 50 + 7) / 3
+});
