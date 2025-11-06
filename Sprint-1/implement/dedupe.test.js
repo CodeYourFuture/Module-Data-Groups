6e91a426e6cea3250bf45f806636699c6aafb242
iswat <ibeetek@gmail.com>
2025-11-06 22:44:17 +0000

    Change deduplicate back to dedupe

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -2,9 +2,9 @@ const dedupe = require("./dedupe.js");
 /*
 Dedupe Array
 
-📖 Dedupe means **deduplicate**
+📖 Dedupe means **dedupe**
 
-In this kata, you will need to deduplicate the elements of an array
+In this kata, you will need to dedupe the elements of an array
 
 E.g. dedupe(['a','a','a','b','b','c']) target output: ['a','b','c']
 E.g. dedupe([5, 1, 1, 2, 3, 2, 5, 8]) target output: [5, 1, 2, 3, 8]
