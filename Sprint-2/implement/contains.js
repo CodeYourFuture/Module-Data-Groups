95f9e07748f362bd1a1f34570d570df15d2a8899
Della Bella <gisdellabella@uol.com.br>
2025-11-09 15:40:39 +0000

    Contains/ test / Excercise

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,13 @@
-function contains() {}
+function contains(someObject, propertyName) {
+     let whatTheRobotFound = someObject[propertyName];
+if (someObject[propertyName] === undefined) {
+  return false;
+} else {
+  return true;
+};
+};
 
 module.exports = contains;
+
+// Implement a function called contains that checks an object contains a
+// particular property
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,16 +20,39 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false",() =>{ 
+    const inputEmpty = [];
+    const result = contains(inputEmpty);
+    expect(result).toEqual(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test("Given an object with properties when pass to contains it returns true", () => {
+const myObject = {
+  name: "Alice",
+  age: 30,
+  city: "London",
+};
+  const result = contains(myObject, "name");
+  expect(result).toEqual(true);
+});
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("give a object with proprieties when pass contains with non-exist proprity name it should return false", () => {
+const inputProprieties2 = ["f", "g", "h"];
+const result = contains(inputProprieties2);
+expect (result).toEqual(false);
+});
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("invalid parameters  should return false or throw an error", () => {
+  const invalidParameters = ("f", "g", "h");
+  const result = contains(invalidParameters);
+  expect(result).toEqual(false);
+});
