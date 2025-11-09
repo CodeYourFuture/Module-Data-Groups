6e3bb1f26309a79f717550dce0cf66335aa3cf3a
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-11-09 10:37:21 +0000

    Sumarry:update calulateMedian implementation ...checked if input is an array,otherwise return null..........Filtered out non-numeric element .......Added a check for emty numeric arrays ....Cloned the array to prevent mutation(avoid side effects)......Sorted numbers in ascending order.......Calculated the median for both even and odd length

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,27 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  // must be an array
+  if (!Array.isArray(list)) return null;
+  //filter out non-numeric values
+
+  const numericList = list.filter(
+    (item) => typeof item === "number" && !isNaN(item)
+  );
+  // return null if no valid numbers
+  if (numericList.length === 0) return null;
+
+  sortedList = [...numericList];
+
+  sortedList.sort((a, b) => a - b);
+  if (sortedList.length % 2 === 0) {
+    let middleIndex = sortedList.length / 2;
+    return (sortedList[middleIndex] + sortedList[middleIndex - 1]) / 2;
+  } else {
+    const middleIndex = Math.floor(sortedList.length / 2);
+    const median = sortedList.splice(middleIndex, 1)[0];
+    return median;
+  }
 }
 
 module.exports = calculateMedian;
