de4e1688f025fc914f8b5c7ebe9158d32aca0139
Ammad <anosidium@hotmail.com>
2025-11-10 14:05:15 +0000

    Add pause feature to pause the alarm clock and then resume it

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,28 +1,56 @@
+let intervalID = null;
+let seconds = 0;
+let isPaused = false;
+
 function setAlarm() {
   const alarmSetInputElement = document.getElementById("alarmSet");
-  const timeRemainingElement = document.getElementById("timeRemaining");
-
-  let seconds = Number(alarmSetInputElement.value);
+  const setAlarmButton = document.getElementById("set");
+  const delay = 1 * 1000;
 
   const updateTimeRemaining = () => {
-    seconds--;
-
-    if (seconds <= 0) {
+    if (!isPaused && seconds <= 0) {
       document.body.className = "flash";
       playAlarm();
       clearInterval(intervalID);
+      intervalID = null;
+      setAlarmButton.textContent = "Set Alarm";
+      return;
     }
 
-    const timeString = new Date(seconds * 1000).toLocaleTimeString([], {
-      minute: "numeric",
-      second: "numeric",
-    });
-
-    timeRemainingElement.innerHTML = `Time Remaining: ${timeString}`;
+    seconds--;
+    setRemainingTime(seconds);
   };
 
-  const delay = 1 * 1000;
-  const intervalID = setInterval(updateTimeRemaining, delay);
+  if (intervalID) {
+    clearInterval(intervalID);
+    intervalID = null;
+    isPaused = true;
+    setAlarmButton.textContent = "Resume Alarm";
+    return;
+  }
+
+  if (isPaused) {
+    isPaused = false;
+    intervalID = setInterval(updateTimeRemaining, delay);
+    setAlarmButton.textContent = "Pause Alarm";
+    return;
+  }
+
+  seconds = Number(alarmSetInputElement.value);
+  setRemainingTime(seconds);
+  intervalID = setInterval(updateTimeRemaining, delay);
+  setAlarmButton.textContent = "Pause Alarm";
+}
+
+function setRemainingTime(seconds) {
+  const timeString = new Date(seconds * 1000).toLocaleTimeString([], {
+    minute: "numeric",
+    second: "numeric",
+  });
+
+  document.getElementById(
+    "timeRemaining"
+  ).innerHTML = `Time Remaining: ${timeString}`;
 }
 
 // DO NOT EDIT BELOW HERE
