1e3daf27bc466e0472b0181d9659bb2aa32191ad
Ammad <anosidium@hotmail.com>
2025-11-11 20:56:16 +0000

    Completed contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,9 @@
-function contains() {}
+function contains(object, property) {
+  if (object === null || typeof object !== "object" || Array.isArray(object)) {
+    throw new Error("object is not an object type!");
+  }
+
+  return Object.prototype.hasOwnProperty.call(object, property);
+}
 
 module.exports = contains;
