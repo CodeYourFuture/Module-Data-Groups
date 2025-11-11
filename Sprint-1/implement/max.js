cb84ef6dff3dc001293c4d08a88c43f862e81644
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-11-11 09:33:27 +0000

    add comprehensive tests for finMax function and implement finMax to handle all test scenarios the scenarios are \n-empty array\n -Single elemnt array \n -Mixed positive and negative numbers\n-All negative numbers\n-Decimal values\n-Array with mixed (numeric/non-numeric)values \n- Array with only non-numeric values

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,5 +1,17 @@
 function findMax(elements) {
-    if(elements===0) return -Infinity;
+  const elementsClone = [...elements];
+
+  const numericElements = elementsClone.filter(
+    (item) => typeof item === "number" && !isNaN(item)
+  );
+  if (numericElements.length === 0) return -Infinity;
+
+  let max = numericElements[0];
+  for (let i = 0; i < numericElements.length; i++) {
+    if (max < numericElements[i]) max = numericElements[i];
+  }
+
+  return max;
 }
 
 module.exports = findMax;
