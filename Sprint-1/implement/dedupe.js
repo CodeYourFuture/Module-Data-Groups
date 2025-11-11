f16627ae2da3e03071328647fea87ed580c4695b
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-11-11 19:55:33 +0000

    Implement dedupe function with jest tests

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,17 @@
-function dedupe() {}
+function dedupe(elements) {
+  const elementsClone = [...elements];
+  if (elementsClone.length === 0) return elementsClone;
+
+  for (let i = 1; i < elementsClone.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (elementsClone[i] === elementsClone[j - 1]) {
+        elementsClone.splice(i, 1);
+        i--; // because I deleted one element from my array
+        break;
+      }
+    }
+  }
+  return elementsClone;
+}
+
+module.exports = dedupe;
--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,7 +16,35 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+describe("Dedupe", () => {
+    it("returns an empty array when it given an empty array", () => {
+        expect(dedupe([])).toEqual([]);
+    });
+    [
+        { input: [10, 12, 13], expected: [10, 12, 13] },
+        {
+            input: ["a", "b", "keke", 1, 21, 3],
+            expected: ["a", "b", "keke", 1, 21, 3],
+        },
+        { input: [null, undefined, 1, 3], expected: [null, undefined, 1, 3] },
+    ].forEach(({ input, expected }) =>
+        it(`returns a copy of the original array when given an array with no duplicates, for [${input}]`, () =>
+            expect(dedupe(input)).toEqual(expected))
+    );
+    [
+        { input: [10, 12, 10, 13], expected: [10, 12, 13] },
+        { input: [1, "a", "a", "b", 1, 1, "a"], expected: [1, "a", "b"] },
+        {
+            input: [null, undefined, 1, 3, 1, 1, 1, 1],
+            expected: [null, undefined, 1, 3],
+        },
+    ].forEach(({ input, expected }) => {
+        it(`remove duplicates from [${input}]`, () => expect(dedupe(input)).toEqual(expected));
+    });
+    it("treats different types separately", () => {
+        expect(dedupe([1, "1", true, "true", false, 0])).toEqual([1, "1", true, "true", false, 0]);
+    });
+});
 
 // Given an array with no duplicates
 // When passed to the dedupe function
