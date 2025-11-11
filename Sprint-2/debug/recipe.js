554168fc60010886854df0cfc5e3d038d8878af0
iswat <ibeetek@gmail.com>
2025-11-11 12:09:07 +0000

    Predict output and fix code to display each ingredient value on a new line

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-
+// When the code runs, `${recipe}` part of the console.log expression will output the string `[object Object]`
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -11,5 +11,8 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+  ingredients:`);
+
+for (const ingredient of recipe.ingredients) {
+  console.log(ingredient);
+}
