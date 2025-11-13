9bd3912d146fe3042a0443087e7a44221c8ca30c
Konvaly <konvaly@gmail.com>
2025-11-13 23:34:36 +0000

    Added solutions for not modifying the input array

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,10 +6,10 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  if (Array.isArray(list) === false || list.length === 0) {
+  if (!Array.isArray(list) || list.length === 0) {
     return null;
   }
-  const sortedArr = list.sort((a, b) => a - b);
+  const sortedArr = [...list].sort((a, b) => a - b);
   const middleIndex = Math.floor(sortedArr.length / 2);
   const median = sortedArr.splice(middleIndex, 1)[0];
   return median;
