d5f1bae2311df0e4c7d6d035b072cc9cb62acb42
iswat <ibeetek@gmail.com>
2025-11-13 00:43:58 +0000

    Implement 'contains' function:
    - returns true if object has property
    - returns false if property missing or object empty
    - throws TypeError for invalid inputs

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,9 @@
-function contains() {}
+function contains(obj, propertyName) {
+    if (typeof(obj) !== "object" || obj === null || Array.isArray(obj)) {
+        throw new TypeError("Invalid input: obj must be a plain object");
+    } 
+
+    return obj.hasOwnProperty(propertyName);
+}
 
 module.exports = contains;
