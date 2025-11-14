99df5b64585b6731e06fd880161a99a947f343bf
iswat <ibeetek@gmail.com>
2025-11-14 13:05:56 +0000

    Fix parseQueryString so key-value pairs with '=' in values are parsed correctly

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -6,11 +6,21 @@ function parseQueryString(queryString) {
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+    const index = pair.indexOf("=");
+    let key, value;
+
+    if (index === -1) {
+      key = pair;
+      value = "";
+    } else {
+      key = pair.slice(0, index);
+      value = pair.slice(index + 1);
+    }
     queryParams[key] = value;
   }
 
   return queryParams;
 }
+
 
 module.exports = parseQueryString;
