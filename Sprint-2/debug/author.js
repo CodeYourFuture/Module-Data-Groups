4ccd3dfe21bf72d4d3264dda738c0a15066ce64e
basel <adoumbasel@gmail.com>
2025-11-14 12:53:00 +0100

    new properties .value.key.entries

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -3,6 +3,9 @@
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
+    // i know it will not work because we cant access these values this way.
+    // what i learned new is the 3 properties
+
 const author = {
   firstName: "Zadie",
   lastName: "Smith",
@@ -11,6 +14,18 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+//for (const value of author) {
+  //console.log(value);}
+
+for (const key in author) {
+  console.log(author[key]);
+}
+for (const value of Object.values(author)) {
   console.log(value);
+}
+for (const key of Object.keys(author)) {
+  console.log(author[key]);
+}
+for (const [key, value] of Object.entries(author)) {
+  console.log(value); // or console.log(`${key}: ${value}`)
 }
