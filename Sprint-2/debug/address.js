109aaa076c61165f77093f4224f3f60781235bdc
basel <adoumbasel@gmail.com>
2025-11-14 12:08:45 +0100

    to het the value we need to use propertiy access via dot or prakets notation. index is for arrays

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,8 +1,12 @@
 // Predict and explain first...
+  // undefind is expect. because index access is for arrays not for object. 
+  // for object you mention the key to get the value.
+
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
+
 
 const address = {
   houseNumber: 42,
@@ -11,5 +15,7 @@ const address = {
   country: "England",
   postcode: "XYZ 123",
 };
-
-console.log(`My house number is ${address[0]}`);
+// either access via (.) properties
+console.log(`My house number is ${address.houseNumber}`);
+//OR prakets notation
+console.log(`My house number is ${address['houseNumber']}`);
