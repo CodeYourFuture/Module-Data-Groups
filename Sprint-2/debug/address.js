aeabc14b14d4745f580b105a8801b761968dc267
Fares Bakhet <faresak47roo@gamil.com>
2025-11-14 12:05:06 +0000

    Fix the bug in address.js file

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+//Prediction: The will be an error at ${address[0]}.
+//Explanation: calling an object using the index as in array. Instead we should use the dot method(.address) or the brackets["address"].
+
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +15,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
