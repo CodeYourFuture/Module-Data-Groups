156dfd953f7b9eba73981f728ab76db768b18083
iswat <ibeetek@gmail.com>
2025-11-14 19:51:32 +0000

    Add function implementation to count array items and handle invalid input

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,19 @@
-function tally() {}
+function tally(arr) {
+  if (!Array.isArray(arr)) {
+    throw new TypeError("Invalid input: Input must be an array");
+  }
+
+  const result = {};
+
+  for (const item of arr) {
+    if (result[item]) {
+      result[item] += 1;
+    } else {
+      result[item] = 1;
+    }
+  }
+
+  return result;
+}
 
 module.exports = tally;
