a6a37dcf5d75dbc4ba5ca16d0bff83b225d3d8ae
iswat <ibeetek@gmail.com>
2025-11-16 18:46:31 +0000

    Add test to verify invert throws a TypeError when given invalid input

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -11,3 +11,9 @@ test("Given a single key-value pair object, returns a swap value", () => {
 test("Given multiple key-value pairs object, returns a swap value", () => {
   expect(invert({ a: 1, b: 2 })).toEqual({ 1: "a", 2: "b" });
 });
+
+test("Given an input that is not a plain object, throws an error", () => {
+  expect(() => invert(null)).toThrowError(
+    new TypeError("Invalid input: Input must be a plain object")
+  );
+});
