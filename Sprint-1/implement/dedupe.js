86aff0760f48d7c84dbde91b63d38d17ca0bd17f
UbunMen <mahtem23@gmail.com>
2025-11-16 21:18:31 +0000

    Dedupe function implemented, test cases.. performed.

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,8 @@
-function dedupe() {}
+function dedupe(array) {
+  if (!Array.isArray(array)) throw new Error("Input must be an array");
+  return array.filter((item, index) => array.indexOf(item) === index);
+}
+console.log(dedupe(['a', 'a', 'a', 'b', 'b', 'c']));
+module.exports = dedupe;
+
+// Dedupe function implemented. 
