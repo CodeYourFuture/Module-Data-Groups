1babce8a07a48bbec54a6b61372224d90261d32b
Fares Bakhet <faresak47roo@gamil.com>
2025-11-16 15:15:42 +0000

    Fix the implement function in querysrting.js file

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,12 +1,33 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
+
+  if (!queryString) {
     return queryParams;
   }
+
+  if (queryString.startsWith("?")) {
+    queryString = queryString.slice(1);
+  }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+    if (!pair) continue;
+
+    const firstEq = pair.indexOf("=");
+
+    let key;
+    let value;
+
+    if (firstEq === -1) {
+      
+      key = pair;
+      value = "";
+    } else {
+      key = pair.slice(0, firstEq);
+      value = pair.slice(firstEq + 1);
+    }
+
     queryParams[key] = value;
   }
 
