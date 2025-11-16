7ab4f2da44442cf69b79b717ca8e519acb537f26
iswat <ibeetek@gmail.com>
2025-11-16 18:22:34 +0000

    Add test to verify invert correctly swaps a single key-value pair

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -4,3 +4,6 @@ test("Given an empty object, return an empty object", () => {
   expect(invert({})).toEqual({});
 });
 
+test("Given a single key-value pair object, returns a swap value", () => {
+  expect(invert({ a: 1 })).toEqual({ 1: "a" });
+});
