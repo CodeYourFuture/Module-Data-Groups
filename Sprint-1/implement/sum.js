49e91ddaeab86dc7f06b8b2f661c21874b08b35b
zilinskyte <zilinskyterogers@gmail.com>
2025-11-16 12:29:13 +0000

    sum function written and passes all tests

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,14 @@
-function sum(elements) {
-}
+function sum(arr) {
+  let total = 0; // variable keeps track of the sum
 
+  for (let element of arr) {
+    // iterate through each element in the array
+    if (typeof element === "number") {
+      // check if the element is a number
+      total += element; // add the number to the total sum
+    }
+  }
+  
+  return total; // return the final sum
+}
 module.exports = sum;
