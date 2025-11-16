a31ecb50cc7b938625783fe0f9eb315407610c77
Alaa Tagi <alaa3litagi1994@gmail.com>
2025-11-16 16:07:11 +0000

    implement a function called tally

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,16 @@
-function tally() {}
+function tally(items) {
+    if (!Array.isArray(items)) {
+        throw new TypeError('Input must be an array');
+    }
+    const tallyResult = {};
+    for (const item of items) {
+        if (tallyResult[item] === undefined) {
+            tallyResult[item] = 1;
+        } else {
+            tallyResult[item]++;
+        }
+    }
+    return tallyResult;
+}
 
 module.exports = tally;
