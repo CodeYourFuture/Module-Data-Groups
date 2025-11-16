92e74343c6bf976bf5f80fb6b52d2436ab8d47be
iswat <ibeetek@gmail.com>
2025-11-16 18:22:03 +0000

    Add test to verify invert returns an empty object for empty input

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -1,6 +1,6 @@
 const invert = require("./invert.js");
 
 test("Given an empty object, return an empty object", () => {
-    expect(invert({})).toEqual({});
-})
+  expect(invert({})).toEqual({});
+});
 
