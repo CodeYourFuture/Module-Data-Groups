8163ad3f172253adb62c4721bc261f7e347ff7ca
saff-coder <sophi.m.80@gmail.com>
2025-11-17 12:57:20 +0000

    fixed the error in the code

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-
+// for .. of only works with iterable objects like arrays or strings. but author is an object.
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +11,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {//This extracts the values into an array, and arrays ARE iterable, so the loop works.
   console.log(value);
 }
