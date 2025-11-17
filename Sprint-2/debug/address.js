39993007e36a5a446fdf94b02385599d4edd21c4
saff-coder <sophi.m.80@gmail.com>
2025-11-17 12:07:53 +0000

    corrected te Address function

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,8 @@
 // Predict and explain first...
 
+//because this not an array we can not use index to access the value
+// we need to use the key name instead
+
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +15,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address["houseNumber"] }`);
