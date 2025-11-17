b8ae2b948347a50e9023da21fe9ab5cfe5410621
saff-coder <sophi.m.80@gmail.com>
2025-11-17 14:40:28 +0000

    implement a function and run the tests

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,14 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+  const lookup = {};//start with an empty object
+
+  for (const pair of pairs) {
+    const country = pair[0];// first element is country code
+    const currency = pair[1];// second element is currency code
+    lookup[country] = currency;
+  }
+
+  return lookup;
+
 }
 
 module.exports = createLookup;
