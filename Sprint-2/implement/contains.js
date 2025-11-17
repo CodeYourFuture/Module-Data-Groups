53492c8e4031c243cbf3250e85461248fc86ef24
saff-coder <sophi.m.80@gmail.com>
2025-11-17 14:23:46 +0000

    Update the function and run the tests

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,11 @@
-function contains() {}
+function contains(obj, key) {
+  if (!obj || typeof obj !== "object" || Array.isArray(obj)) {//check if we have an object and makes sure it's not an array
+    return false;
+  }
+  if (Object.keys(obj).length === 0) {
+    return false;
+  }
+  return key in obj;
+}
 
 module.exports = contains;
