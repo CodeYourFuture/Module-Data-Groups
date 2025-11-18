d532bac8243245fa254f703917a04d4a0c4249f7
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-11-18 08:07:10 +0000

    implement a contain function and ensure all test cases pass

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,11 @@
-function contains() {}
+function isObject(value) {
+  return typeof value === "object" && value !== null && !Array.isArray(value);
+}
+
+function contains(obj, prop) {
+    if(!isObject(obj)) throw  new Error ("Invalid input");
+    
+  return Object.keys(obj).includes(prop);
+}
 
 module.exports = contains;
