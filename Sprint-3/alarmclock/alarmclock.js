4131d89c49b3416e39d912456548a922efebe8bd
iswat <ibeetek@gmail.com>
2025-11-19 12:56:00 +0000

    Add pseudocode for setAlarm implementation

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,22 @@
-function setAlarm() {}
+function setAlarm() {
+  // When I press the “Set Alarm” button:
+    // Read the number typed in the input field (in seconds)
+    // Store this value in a variable called timeRemaining
+    // Convert timeRemaining into minutes and seconds
+    // Format minutes and seconds so they always have 2 digits (mm:ss)
+    // Display "Time Remaining: mm:ss" on the UI
+    // Start a repeating timer using setInterval that runs every 1 second:
+    // Decrease timeRemaining by 1
+    // Convert the new time into mm:ss
+    // Update the display
+    // If timeRemaining reaches 0:
+    // Stop the interval
+    // Display "00:00"
+    // Call playAlarm()
+    
+  // When I press the “Stop Alarm” button:
+    // Call pauseAlarm() to stop the sound
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -23,3 +41,4 @@ function pauseAlarm() {
 }
 
 window.onload = setup;
+
