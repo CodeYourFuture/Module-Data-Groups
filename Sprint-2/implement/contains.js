5cf0ffbad72fb32e41c75d17ae39bc6407dc9a58
AdnaanA <thetriplex247@gmail.com>
2025-11-19 11:50:01 +0000

    changed parameter

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,12 +1,12 @@
-function contains(arr, value) {
+function contains(arr, item) {
   /**
    * Checks if the given array contains the specified value.
    * @param {Array} arr - The array to check.
-   * @param {*} value - The value to search for.
+   * @param {*} item - The item to search for.
    * @returns {boolean} - Returns true if the value is found, otherwise false.
    */
   if (!Array.isArray(arr)) return false;
-  return arr.includes(value);
+  return arr.includes(item);
 }
 
 module.exports = contains;
