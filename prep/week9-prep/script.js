2a86fe4038ed78e287814d820c1fbef3de0e483e
basel <adoumbasel@gmail.com>
2025-11-19 09:34:13 +0100

    character counter exercise-preparation part of DOM

---
+++ prep/week9-prep/script.js
@@ -0,0 +1,26 @@
+// const textarea = document.querySelector("textarea");
+// const remainingCharacters = textarea.maxLength - textarea.value.length;
+
+// const charactersLeftP = document.querySelector("#character-limit-info");
+// charactersLeftP.innerText = `You have ${remainingCharacters} characters remaining`;
+
+// function updateCharacterLimit() {
+//   const remainingCharacters = textarea.maxLength - textarea.value.length;
+//   const charactersLeftP = document.querySelector("#character-limit-info");
+//   charactersLeftP.innerText = `You have ${remainingCharacters} characters remaining`;
+// }
+
+// textarea.addEventListener("keyup", updateCharacterLimit);
+
+// refactor version 
+const textarea = document.querySelector("textarea");
+
+updateCharacterLimit();
+
+function updateCharacterLimit() {
+  const remainingCharacters = textarea.maxLength - textarea.value.length;
+  const charactersLeftP = document.querySelector("#character-limit-info");
+  charactersLeftP.innerText = `You have ${remainingCharacters} characters remaining`;
+}
+
+textarea.addEventListener("keyup", updateCharacterLimit);
