954737e858bda5d54ebaf6961c85182c00ffab7f
Abrsh100 <abrdbr2008@gmail.com>
2025-11-19 20:18:34 +0000

    it was a valuable comment and i communicate with the comment to modify the code.

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,16 +1,5 @@
 function dedupe(groupNumber) {
-  const seen = new Set()
-  const deduped = []
-
-  for (const item of groupNumber) {
-    if (!seen.has(item)) {
-      seen.add(item)
-      deduped.push(item)
-    }
-  }
-
-  return deduped
+  return [...new Set(groupNumber)];
 }
-   
 
-module.exports = dedupe
+module.exports = dedupe;
--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,6 +1,6 @@
 function sum(elements) {
   return elements.reduce((accumulator, currentValue) => {
-    if (typeof currentValue === "number") {
+    if (typeof currentValue === "number" && Number.isFinite(currentValue)) {
       return accumulator + currentValue;
     }
     return accumulator;
