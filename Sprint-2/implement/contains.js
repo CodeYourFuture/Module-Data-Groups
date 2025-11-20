60de6c2b97e9b99d03666f1800b436553c511832
enjoy15 <narumoljoy@gmail.com>
2025-11-20 20:26:35 +0000

    update contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -4,12 +4,11 @@ function contains(obj, prop) {
 	if (typeof obj !== 'object' || obj === null || Array.isArray(obj)) {
 		return false;
 	}
-	for (const key in obj) {
-		if (key === prop) {
-			return true;
-		}
-	}
-	return false;
+	// Use built‑in instead of manual for..in loop
+    // Object.hasOwn (Node 16+) can be used; fallback to hasOwnProperty for wider support
+    // return Object.hasOwn ? Object.hasOwn(obj, prop) : Object.prototype.hasOwnProperty.call(obj, prop);
+
+    return Object.prototype.hasOwnProperty.call(obj, prop);
 }
 
 module.exports = contains;
