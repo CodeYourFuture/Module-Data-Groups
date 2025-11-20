392cf81c3ad871843bc9acdec2d01f60b5b9fd15
Ali <naru.a@yahoo.com>
2025-11-20 20:51:36 +0000

    Fix: update for...of loop to use Object.values for iterating over author object

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,9 @@
 // Predict and explain first...
+// Prediction: This code will throw a TypeError saying that author is not iterable.
+// Explanation: The for...of loop only works on iterable objects (arrays, strings, maps, etc.).
+// Regular objects like "author" are NOT iterable, so JavaScript cannot loop over them using for...of.
+// To loop through an object's values, we need to use Object.values(author), Object.keys(author),
+// or Object.entries(author).
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +16,12 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
+// Expected output:
+// Zadie
+// Smith
+// writer
+// 40
+// true 
