2999e89d4b04787d34acf2ff02c6d7c14f4bb6c9
ahmadehsas <ahmadehsas14345@gmail.com>
2025-11-20 22:49:54 +0000

    Created a function to display a random quote.

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -496,3 +496,16 @@ const quotes = [
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const newQuoteButton = document.getElementById("new-quote");
+
+// quoteText.innerText = "Smile Life is beautiful";
+// authorText.innerText = "Ahmad Ehsas";
+
+// Show a random quote
+
+function showRandomQuote () {
+  const randomQuote = pickFromArray(quotes);// pick a random quote from the quote array.
+  quoteText.innerText = randomQuote.quote; // display the quote text in the HTML
+  authorText.innerText = randomQuote.quote; // display the author's in the HTML
+}
+// Display quote on page load
+showRandomQuote();
