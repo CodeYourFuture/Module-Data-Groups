527ecda0f8612cfcce3b1d8281452c24f5938dfb
zilinskyte <zilinskyterogers@gmail.com>
2025-11-21 14:23:47 +0000

    lookup function wirtten and tested

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,7 +1,20 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+test("creates a country currency code lookup for multiple codes", () => {
+  const countryCurrencyPairs = [
+    ["US", "USD"],
+    ["CA", "CAD"],
+    ["GB", "GBP"],
+  ];
 
+  const expectedLookup = {
+    US: "USD",
+    CA: "CAD",
+    GB: "GBP",
+  };
+
+  expect(createLookup(countryCurrencyPairs)).toEqual(expectedLookup);
+});
 /*
 
 Create a lookup object of key value pairs from an array of code pairs
