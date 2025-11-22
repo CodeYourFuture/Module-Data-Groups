fe14e35375ae9f27fb445b350d1b9f41ed2eec48
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-11-22 09:54:57 +0000

    Updated function contains() in contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,6 +1,7 @@
 function contains(obj, find) {
-  if (obj[find]) return true;
+  if (obj.hasOwnProperty(find)) return true;
   return false;
 }
 
+//console.log(contains({a: 1, b: 2}, 'toString'));
 module.exports = contains;
