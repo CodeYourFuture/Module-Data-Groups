db5390c77125ec882fd8ad55743160741132fea3
Declan Williams <declanwilliams793@gmail.com>
2025-11-22 14:44:22 +0000

    added tests to check if the dedupe function makes an new array and not using the original array

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -17,21 +17,27 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // When passed to the dedupe function
 // Then it should return an empty array
 test("given an empty array, it returns an empty array", () => {
-expect(dedupe([])).toEqual([]);
+  expect(dedupe([])).toEqual([]);
 });
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
 test("given an array with no duplicates, it returns a copy of the original array", () => {
   expect(dedupe([1, 2, 3])).toEqual([1, 2, 3]);
-  expect(dedupe(['a', 'b', 'c'])).toEqual(['a', 'b', 'c']);
+  expect(dedupe(["a", "b", "c"])).toEqual(["a", "b", "c"]);
+
+  const original = [1, 2, 3];
+  const result = dedupe(original);
+
+  expect(result).toEqual(original);
+  expect(result).not.toBe(original);
 });
 
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
 test("given an array with duplicates it removes any duplicates and preserves the first instance of each element", () => {
-  expect(dedupe(['a','a','a','b','b','c'])).toEqual(['a','b','c']);
+  expect(dedupe(["a", "a", "a", "b", "b", "c"])).toEqual(["a", "b", "c"]);
   expect(dedupe([5, 1, 1, 2, 3, 2, 5, 8])).toEqual([5, 1, 2, 3, 8]);
   expect(dedupe([1, 2, 1])).toEqual([1, 2]);
 });
