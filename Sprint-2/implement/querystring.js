ff9852e4ad3ed546ddeeb345440e9eb95f4b81ae
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-11-22 10:23:39 +0000

    Updated querystring.js

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,15 +1,17 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) { 
-    return queryParams;
-  }
-    let firstPart = queryString.slice(0, queryString.indexOf("="));
-    let secondPart = queryString.slice(queryString.indexOf("=") + 1);
-    queryParams[firstPart] = secondPart;
+  if (!queryString) return queryParams;
+
+  const eqIndex = queryString.indexOf("=");
+
+  let firstPart = decodeURIComponent(queryString.slice(0, eqIndex));
+  let secondPart = decodeURIComponent(queryString.slice(eqIndex + 1));
+
+  queryParams[firstPart] = secondPart;
 
   return queryParams;
 }
 
 module.exports = parseQueryString;
 
-console.log(parseQueryString("equation=x=y+1"));
+//console.log(parseQueryString("equation=x=y+1"));
