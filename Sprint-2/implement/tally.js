3033e55a8d870946b72d9a9c00dee036b6fc3ca5
Mohammed Abdoon <mohammedabdoon1996@gmail.com>
2025-11-22 14:00:24 +0000

    tally.js updated

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -7,7 +7,7 @@ function tally(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (!obj[arr[i]])
             obj[arr[i]] = 1;
-        else if (obj[arr[i]])
+        else
             obj[arr[i]]++;
     }
     return obj;
