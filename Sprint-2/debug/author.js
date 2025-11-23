b1aa75d4cb441a99d7a5e43a363232717018a08c
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-23 17:53:59 +0000

    changed loop to log just the values

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -11,6 +11,6 @@ const author = {
   alive: true,
 };
 
-for (const value in author) {
-  console.log(`${value}:${author[value]}`);
+for (const value of Object.values(author)) {
+  console.log(value);
 }
