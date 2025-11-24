b8bdddf4084eca215a6b85837b1c7887b1f9091b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-24 09:04:30 +0000

    removed redundant code

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -2,9 +2,6 @@ function contains(obj, property) {
   if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
     throw new Error("Parameter is not an object literal");
   }
-  if (Object.keys(obj).length === 0) {
-    return false;
-  }
 
   if (property in obj) {
     return true;
