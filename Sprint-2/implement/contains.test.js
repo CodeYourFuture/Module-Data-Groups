da2a8c53556ef58ea23fb1002ca41e15dbe068eb
ahmadehsas <ahmadehsas14345@gmail.com>
2025-11-24 21:42:09 +0000

    Modified the discription of test cases on line 48 and 53.

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -45,12 +45,12 @@ test("contains on an array returns false", () => {
   expect(contains([1, 2, 3], 4)).toBe(false);
 });
 
-test("contains on an array of length returns true", () => {
-  // expect(contains([1, 2, 3, 4], "length")).toBe(true);
-  expect(contains(null, "a", "length")).toBe(true);
+test("contains on array with existing property returns true", () => {
+ expect(contains([1, 2, 3, 4], "length")).toBe(true);
+  //expect(contains(null, "a", "length")).toBe(true);
 }); // the test returns true because 'length' is a property of the array object
 
-test("contains given on array as input return false", () => {
+test("contains on array with non-existent property returns false", () => {
   // expect(contains([1, 2, 3], "a")).toBe(false);
   expect(contains(1234, "a")).toBe(false);
 });
