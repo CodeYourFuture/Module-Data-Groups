0e5bd642d35c5e7aeddc546d3c8be1f0665f8f7f
Tarawally <41825140+Tarawally@users.noreply.github.com>
2025-11-26 11:32:31 +0000

    fix(debug): use dot notation for address.houseNumber access

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,12 @@
 // Predict and explain first...
 
+// The code will likely output undefined.
+// This is because `address[0]` tries to access an object property using an array index.
+// However, `address` is an object, not an array. Objects use keys (property names) to access values. 
+// References: 
+// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors#dot_notation
+// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors#bracket_notation
+
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +19,8 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+// Access the property using dot notation or bracket notation with the key name:
+// - Dot notation: `address.houseNumber` 
+// - Bracket notation: `address["houseNumber"]` 
+
+console.log(`My house number is ${address.houseNumber}`);
