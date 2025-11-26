9dd1769d03dff7c644fc7a5c132823e515711db1
UbunMen <mahtem23@gmail.com>
2025-11-26 19:32:32 +0000

    In lookup.js function createLookup implemented.

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,27 @@
-function createLookup() {
-  // implementation here
+function createLookup(input) {
+  if (!Array.isArray(input)) {
+    throw new Error("Input must be an array of [country, currency] pairs");
+  }
+
+  const lookup = {};
+
+  for (const pair of input) {
+    if (!Array.isArray(pair) || pair.length !== 2) {
+      throw new Error("Each item must be an array of [country, currency]");
+    }
+
+    const [country, currency] = pair;
+
+    if (typeof country !== "string" || typeof currency !== "string") {
+      throw new Error("Each item must be an array of [country, currency]");
+    }
+
+    lookup[country] = currency;
+  }
+
+  return lookup;
 }
 
 module.exports = createLookup;
+
+// In lookup.js function createLookup implemented. 
