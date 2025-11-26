99836eb50078ed42cd149097654bcc9e09621462
Alaa Tagi <alaa3litagi1994@gmail.com>
2025-11-26 16:48:40 +0000

    fix one line in the code and add two line to print the output

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -2,7 +2,9 @@ function tally(items) {
     if (!Array.isArray(items)) {
         throw new TypeError('Input must be an array');
     }
-    const tallyResult = {};
+
+    const tallyResult = Object.create(null);
+
     for (const item of items) {
         if (tallyResult[item] === undefined) {
             tallyResult[item] = 1;
@@ -10,7 +12,11 @@ function tally(items) {
             tallyResult[item]++;
         }
     }
+
     return tallyResult;
 }
 
 module.exports = tally;
+
+console.log("Test : tally with 'toString':");
+console.log(tally(["toString", "toString"]));
