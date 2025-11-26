bc33d58fdab820eadf3ad95b3628ab74832a2c6a
Abrsh100 <abrdbr2008@gmail.com>
2025-11-26 07:26:19 +0000

    decoding and indentation adjastement

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -3,20 +3,21 @@ function parseQueryString(queryString) {
     const queryParams = {};
     if (!queryString ) return queryParams;
 
-    const keyValuePairs = queryString.split("&");
+     const keyValuePairs = queryString.split("&");
 
-    for (const pair of keyValuePairs) {
-    const index = pair.indexOf("=");
-    if (index === -1) {
+     for (const pair of keyValuePairs) {
+     const index = pair.indexOf("=");
+     if (index === -1) {
       queryParams[decodeURIComponent(pair)] = undefined
-    } else {
-        const key = pair.slice(0, index);
-        const value = pair.slice (index + 1);
+     } else {
+        const key = decodeURIComponent (pair.slice(0, index));
+        const value = decodeURIComponent (pair.slice (index + 1));
         queryParams[key] = value;
-    }
-    return queryParams;
+     }
+     
+  return queryParams;
 
-    }
+  }
 }
 
  module.exports = parseQueryString;
