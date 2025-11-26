088c50ab2cca63504c5b47d74faa67656f0e146f
Alaa Tagi <alaa3litagi1994@gmail.com>
2025-11-26 19:17:42 +0000

    fix alarm function as requested

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,32 +1,30 @@
+function updateDisplay(heading, time) {
+  const minutes = String(Math.floor(time / 60)).padStart(2, "0");
+  const seconds = String(time % 60).padStart(2, "0");
+  heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
+}
+
 function setAlarm() {
   let time = Number(document.getElementById("alarmSet").value);
   const heading = document.getElementById("timeRemaining");
 
   audio.loop = true;
-
   document.body.style.backgroundColor = "";
 
-  function updateDisplay(t) {
-    const minutes = String(Math.floor(t / 60)).padStart(2, "0");
-    const seconds = String(t % 60).padStart(2, "0");
-    heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
-  }
-
-  updateDisplay(time);
+  updateDisplay(heading, time);
 
   const intervalId = setInterval(() => {
     time--;
 
     if (time <= 0) {
       clearInterval(intervalId);
-      updateDisplay(0);
+      updateDisplay(heading, 0);
 
       document.body.style.backgroundColor = "red";
 
       playAlarm();
-
     } else {
-      updateDisplay(time);
+      updateDisplay(heading, time);
     }
   }, 1000);
 }
