420e2983d7de5859959fc6fdb63f043300648d52
Tarawally <41825140+Tarawally@users.noreply.github.com>
2025-11-26 22:43:54 +0000

    fix: resolve TypeError by using Object.values() to iterate over author object properties

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,11 @@
 // Predict and explain first...
 
+// The code will throw a `TypeError` because `author` is a plain object, and plain objects are not iterable (cannot be used with `for...of`).
+// References:
+// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of#iterating_over_plain_objects
+// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+
+
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +17,7 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+// Get an array of values to iterate over
+for (const value of Object.values(author)) {
   console.log(value);
 }
