8bdd6c52ea25bea9394464da8e4335a9d181fce4
UbunMen <mahtem23@gmail.com>
2025-11-26 19:33:31 +0000

    In lookup.test.js test cases added and passed.

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,6 +1,6 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+//test.todo("creates a country currency code lookup for multiple codes");
 
 /*
 
@@ -33,3 +33,50 @@ It should return:
    'CA': 'CAD'
  }
 */
+// Case 1: Valid input
+
+test("returns an object with country codes as keys and currency codes as values", () => {
+  const input = [
+    ["US", "USD"],
+    ["CA", "CAD"],
+  ];
+  const expected = { US: "USD", CA: "CAD" };
+
+  expect(createLookup(input)).toEqual(expected);
+});
+
+// Case 2: Empty array
+
+test("returns an empty object when an empty array is passed", () => {
+  expect(createLookup([])).toEqual({});
+});
+
+// Case 3: Input not an array
+
+test("throws an error when input is not an array", () => {
+  expect(() => createLookup("not an array")).toThrow(
+    "Input must be an array of [country, currency] pairs"
+  );
+});
+
+// Case 4: Input null or undefined
+
+test("throws an error when input is null or undefined", () => {
+  expect(() => createLookup(null)).toThrow(
+    "Input must be an array of [country, currency] pairs"
+  );
+  expect(() => createLookup(undefined)).toThrow(
+    "Input must be an array of [country, currency] pairs"
+  );
+});
+
+// Case 5: type of Input elements are not strings 
+
+  test("throws an error if inner array elements are not valid strings", () => {
+    const invalidInput = [['US', 'USD'], ['CA', 123], ['JP', null]];
+    expect(() => createLookup(invalidInput)).toThrow(
+      "Each item must be an array of [country, currency]"
+    );
+  });
+
+// In lookup.test.js test cases added and passed. 
