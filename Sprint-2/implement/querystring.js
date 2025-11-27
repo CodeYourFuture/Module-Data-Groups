2c0305694ff438705ec4b1d58999a9a2c218d10d
UbunMen <mahtem23@gmail.com>
2025-11-27 11:57:15 +0000

    In querystring.js function implemented.

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,45 @@
 function parseQueryString(queryString) {
-  const queryParams = {};
+  const parsedParams = {}; // Gets the final key-value pairs
+
+  if (!queryString) return parsedParams;
+
+  // Removes leading '?' if present
+
+  if (queryString.startsWith("?")) {
+    queryString = queryString.slice(1);
+  }
+
   if (queryString.length === 0) {
-    return queryParams;
-  }
-  const keyValuePairs = queryString.split("&");
-
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    return parsedParams;
   }
 
-  return queryParams;
+  // Split the string into individual key-value pairs
+  const pairs = queryString.split("&");
+
+  for (const pair of pairs) {
+    if (!pair) continue; // skip empty segments (like from && or trailing &) eg "name=John&&age=30" 
+
+    const equalSignIndex = pair.indexOf("=");
+
+    let paramKey, paramValue;
+
+    if (equalSignIndex === -1) {
+
+      // If '=' not found we have a key exists but value is empty
+
+      paramKey = pair;
+      paramValue = "";
+    } else {
+      paramKey = decodeURIComponent(pair.slice(0, equalSignIndex));
+      paramValue = decodeURIComponent(pair.slice(equalSignIndex + 1));
+    }
+
+    parsedParams[paramKey] = paramValue; // overwrite previous value if key repeats
+  }
+
+  return parsedParams;
 }
 
 module.exports = parseQueryString;
+
+// In querystring.js function implemented. 
