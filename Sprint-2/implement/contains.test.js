396306258e5b22edf025d8e24fc6c52d559a8bf3
Fares Bakhet <faresak47roo@gmail.com>
2025-11-29 18:35:32 +0000

    Revert "Adding tests in contains.test.js file"
    This reverts commit 19ca88dea3971087ee3ef1939d6f3ef158c739ce.

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -17,63 +17,19 @@ as the object doesn't contains a key of 'c'
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
 
-test("given an object contains a property name, returns true", function (){
-    const input = {a: 1, b: 2};
-    const propertyName = 'a';
-    const currentOutput = contains(input, propertyName);
-    const targetOutput = true;
-
-    expect(currentOutput).toEqual(targetOutput);
-});
-
 // Given an empty object
 // When passed to contains
 // Then it should return false
-
-test("given an empty object, returns false", function (){
-    const input = {};
-    const propertyName = 'a';
-    const currentOutput = contains(input, propertyName);
-    const targetOutput = false;
-
-    expect(currentOutput).toEqual(targetOutput);
-});
+test.todo("contains on empty object returns false");
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
 
-test("given an object contains a property name, returns true", function (){
-    const input = {a: 1, b: 2};
-    const propertyName = 'a';
-    const currentOutput = contains(input, propertyName);
-    const targetOutput = true;
-
-    expect(currentOutput).toEqual(targetOutput);
-});
-
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
 
-test("given an object does not contain a property name, returns false", function (){
-    const input = {a: 1, b: 2};
-    const propertyName = 'c';
-    const currentOutput = contains(input, propertyName);
-    const targetOutput = false;
-
-    expect(currentOutput).toEqual(targetOutput);
-});
-
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
-
-test("given invalid parameters like an array, returns false", function (){
-    const input = [1, 2, 3];
-    const propertyName = 'a';
-    const currentOutput = contains(input, propertyName);
-    const targetOutput = false;
-
-    expect(currentOutput).toEqual(targetOutput);
-});
