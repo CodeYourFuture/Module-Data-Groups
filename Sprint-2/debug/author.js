b5500ee45fc47f88c3d1250d2f4ba602eb7ade5c
Tarawally <41825140+Tarawally@users.noreply.github.com>
2025-11-29 00:36:01 +0000

    fix: use Object.values() to iterate over object

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,10 +1,9 @@
 // Predict and explain first...
 
-// The code will throw a `TypeError` because `author` is a plain object, and plain objects are not iterable (cannot be used with `for...of`).
+// This throws a TypeError because plain objects cannot be used with for...of directly.
 // References:
-// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of#iterating_over_plain_objects
+// - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
 // - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
-
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -17,7 +16,7 @@ const author = {
   alive: true,
 };
 
-// Get an array of values to iterate over
+// Fix: Object.values() returns an array that can be looped over
 for (const value of Object.values(author)) {
   console.log(value);
 }
