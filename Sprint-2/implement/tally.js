4141123a771de16a9f24da21b7abc6fe2714f8cf
Declan Williams <declanwilliams793@gmail.com>
2025-11-29 15:14:20 +0000

    Refactor: change itemCount initialization to use Object.create(null) for a cleaner object creation

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,20 +1,21 @@
 function tally(array) {
-    // ensures that the input is an array.
-    if (!Array.isArray(array)){
-        // throw an error if input is not an array
-        throw new Error("expected input to be an array");
-    }
+  // ensures that the input is an array.
+  if (!Array.isArray(array)) {
+    // throw an error if input is not an array
+    throw new Error("expected input to be an array");
+  }
 
-    // make an empty object to store items counted
-    const itemCount = {};
+  // make an empty object to store items counted
+  const itemCount = Object.create(null);
 
-    // loop over each item in the array
-    for (const item of array){
-        // if the item hasn't been counted yet treat it as 0
-        itemCount[item] = (itemCount[item] || 0) + 1; // then adds 1 to either start or increment the count for this item
-    }
+  // loop over each item in the array
+  for (const item of array) {
+    // if the item hasn't been counted yet treat it as 0
+    itemCount[item] = (itemCount[item] || 0) + 1; // then adds 1 to either start or increment the count for this item
+  }
 
-    return itemCount;
+  return itemCount;
 }
+console.log(tally(["toString", "toString"]));
 
 module.exports = tally;
