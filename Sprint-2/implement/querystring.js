7a17d2620c826397d00f2d8ada9f2ad7503cc4e7
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-29 22:03:33 +0000

    Refactor parseQueryString function for clarity and consistency in handling key-value pairs

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -10,17 +10,17 @@ function parseQueryString(queryString) {
     if (!pair) {
       continue;
     }
-    const equalIndex = pair.indexOf("=");
-    if (equalIndex === -1) {
+    const index = pair.indexOf("=");
+    if (index === -1) {
       queryParams[pair] = "";
-      continue;
+    } else {
+      const key = pair.slice(0, index);
+      const value = pair.slice(index + 1);
+      queryParams[key] = value;
     }
-    const key = pair.substring(0, equalIndex);
-    const value = pair.substring(equalIndex + 1);
-    queryParams[key] = value;
   }
-
   return queryParams;
 }
+console.log(parseQueryString("equation=x=y+1&sound=none"));
 
 module.exports = parseQueryString;
