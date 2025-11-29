6f7ef9c882b31a17b7e06aafd19bb1c4a7bc17fa
Declan Williams <declanwilliams793@gmail.com>
2025-11-29 13:10:56 +0000

    Refactor: improve readability of the contains function by adjusting indentation and formatting and handles null safety

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,14 +1,14 @@
 function contains(obj, key) {
-    // check if "obj" is an object and not an array and we are working with plain objects
-    if (typeof obj !== "object" || Array.isArray(obj)) return false;
+  // check if "obj" is an object and not an array and we are working with plain objects
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj))
+    return false;
 
-    // loop over each property name in the object
-    for (let properties in obj){
+  // loop over each property name in the object
+  for (let properties in obj) {
+    // check that if each of the properties names matches what were looking for in the key and return true if matches
+    if (properties === key) return true;
+  }
 
-        // check that if each of the properties names matches what were looking for in the key and return true if matches
-        if (properties === key) return true;
-    }
-
-    return false; // if no object or key is found return false
+  return false; // if no object or key is found return false
 }
 module.exports = contains;
