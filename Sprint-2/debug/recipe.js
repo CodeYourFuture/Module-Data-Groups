324a6b38c5bd0d75864172054553ae35435308f3
Fares Bakhet <faresak47roo@gmail.com>
2025-11-29 18:35:47 +0000

    Revert "Fix the bug in recipe.js file"
    This reverts commit ab693e347811f9c459b3643a69bc61c6e0213db4.

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,6 +1,4 @@
 // Predict and explain first...
-// Prediction: An error in logging out the ingredients.
-// Explanation: Because of the way to get the value or an element from an array should used the dot notation for example and reach each element using its index.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -14,7 +12,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe.ingredients[0]}
-${recipe.ingredients[1]}
-${recipe.ingredients[2]}
-${recipe.ingredients[3]}`);
+${recipe}`);
