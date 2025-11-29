ea7fdc583f98b053bd6597ff41aef694066e514f
Fares Bakhet <faresak47roo@gmail.com>
2025-11-29 18:34:22 +0000

    Revert "Answering questions and fix the implemention and adding tests"
    This reverts commit 8465e56c93abf81706f0a6731bf1e0d7cdbd6e12.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -16,52 +16,14 @@ function invert(obj) {
   return invertedObj;
 }
 
-// a) What is the current return value when invert is called with { a : 1 } >> { key: 1 }
+// a) What is the current return value when invert is called with { a : 1 }
 
-// b) What is the current return value when invert is called with { a: 1, b: 2 } >> { key: 2 }
+// b) What is the current return value when invert is called with { a: 1, b: 2 }
 
-// c) What is the target return value when invert is called with {a : 1, b: 2} >> { "1": "a", "2": "b" }
+// c) What is the target return value when invert is called with {a : 1, b: 2}
 
-// c) What does Object.entries return? Why is it needed in this program? >> Object.entries returns an array of key-value paris.
+// c) What does Object.entries return? Why is it needed in this program?
 
-// d) Explain why the current return value is different from the target output >> Because invertedObj.key = value creates a property called (key) instead of using hte variable key.
+// d) Explain why the current return value is different from the target output
 
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
-// The fixed code:
-function invert(obj) {
-  const invertedObj = {};
-
-  for (const [key, value] of Object.entries(obj)) {
-    invertedObj[value] = key;
-  }
-
-  return invertedObj;
-}
-
-module.exports = invert;
-
-// Tests:
-const invert = require("./invert.js");
-
-// Single key
-test("invert works for one key-value pair", () => {
-  expect(invert({ a: 1 })).toEqual({ "1": "a" });
-});
-
-// Multiple keys
-test("invert swaps keys and values for multiple pairs", () => {
-  expect(invert({ a: 1, b: 2 })).toEqual({
-    "1": "a",
-    "2": "b",
-  });
-});
-
-// Empty object
-test("invert of an empty object returns empty", () => {
-  expect(invert({})).toEqual({});
-});
-
-// Values become string keys
-test("invert always returns string keys", () => {
-  expect(invert({ x: 10 })).toEqual({ "10": "x" });
-});
