e5876dbdae0ef8881c7cc283c7f95229596835d7
Alaa Tagi <alaa3litagi1994@gmail.com>
2025-11-29 18:01:54 +0000

    fix the code

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,11 +1,18 @@
 function findMax(elements) {
-    let max = -Infinity;
-    for (let i = 0; i < elements.length; i++) {
-        if (elements[i] > max) {
-            max = elements[i];
-        }
+  let max = -Infinity;
+
+  for (let i = 0; i < elements.length; i++) {
+    const value = elements[i];
+
+    // Only consider real finite numbers 
+    if (typeof value === "number" && Number.isFinite(value)) {
+      if (value > max) {
+        max = value;
+      }
     }
-    return max; 
+  }
+
+  return max; 
 }
 
 module.exports = findMax;
