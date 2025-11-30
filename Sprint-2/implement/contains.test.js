85c361c604f40c0f29c2cb5ea31c7e12171720d6
Fithi Teklom <fthiteklom@gmail.com>
2025-11-30 18:20:54 +0000

    tested my tests

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -8,7 +8,7 @@ E.g. contains({a: 1, b: 2}, 'a') // returns true
 as the object contains a key of 'a'
 
 E.g. contains({a: 1, b: 2}, 'c') // returns false
-as the object doesn't contains a key of 'c'
+as the object doesn't contain a key of 'c'
 */
 
 // Acceptance criteria:
@@ -20,16 +20,41 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
 
-// Given an object with properties
-// When passed to contains with an existing property name
-// Then it should return true
-
-// Given an object with properties
-// When passed to contains with a non-existent property name
-// Then it should return false
+test.todo("contains an empty object returns false", () => {
+  expect(contains({}, "a")).toBe(false);
+});
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+
+test("contains array-like object,invalid object and null  returns false",() => {
+    expect(contains([], "a")).toBe(false);
+    expect(contains(123, "a")).toBe(false);
+    expect(contains(null, "a")).toBe(false);
+});
+
+// Given an object with properties
+// When passed to contains with an existing property name
+// Then it should return true
+test("returns true for existent property", () => {
+    const obj = {a: 1, b: 2}
+    expect(contains("a")).toBe(true);   
+});
+
+
+
+// Given an object with properties
+// When passed to contains with a non-existent property name
+// Then it should return false
+test("returns false for non existent properties", () => {
+    const obj1= {a: 1, b:2}
+    expect(contains("c")).toBe(false);
+});
+
+
+
+
+
+
