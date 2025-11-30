9547061575d796c1fcfb81b4c30ad24aa9162294
Fithi Teklom <fthiteklom@gmail.com>
2025-11-30 19:37:10 +0000

    done lookup.test.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,10 @@
-function createLookup() {
+function createLookup(pairs) {
   // implementation here
+  const lookup = {};
+  for(const [countryCode, currencyCode]of pairs){
+    lookup[countryCode] = currencyCode;
+  }
+  return lookup;
 }
 
 module.exports = createLookup;
