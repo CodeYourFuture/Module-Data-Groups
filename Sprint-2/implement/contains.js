8875e1c9509d713edc3e6b99c8d7f629f8b5ba21
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-11-30 18:01:39 +0000

    Update the contains function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -5,7 +5,25 @@ function isObject(value) {
 function contains(obj, prop) {
     if(!isObject(obj)) throw  new Error ("Invalid input");
     
-  return Object.keys(obj).includes(prop);
+ // return Object.keys(obj).includes(prop);
+ return Object.hasOwn(obj, prop);
 }
 
+ console.log( contains({ a: 1, b: 2 }, 'toString'));
 module.exports = contains;
+
+
+//Does the following function call returns what you expect?
+
+ // contains({a: 1, b: 2}, 'toString');
+// returns false for inherited properties
+
+
+//How to determine if an object's property is its own property or an inherited property in JavaScript?
+// by using hasown method.
+
+//Does your test correctly check whether the function can detect the given object is an array?
+//Yes, the test correctly checks that the function can detect when the input is an array.
+//it("throws an error when the first parameter is not an object",()=>{
+ //       expect(()=> contains([],"a")).toThrow("Invalid input")
+ //   });
