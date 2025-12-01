4fa4c91ab62df75b022ce68fe55e5baea6ab7279
Konvaly <konvaly@gmail.com>
2025-12-01 01:13:28 +0000

    Explained and fixed debug/author.js

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// for...of only works on arrays. An object isn’t an array, so we can’t loop over it directly.
+// To find values from the object we need to convert it to an array of values first.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +13,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
