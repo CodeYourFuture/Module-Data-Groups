67ecaaaf70ac40cf270d39ac9fae0a00debafaaa
Fares Bakhet <faresak47roo@gmail.com>
2025-12-01 15:01:09 +0000

    Revert "Answering till.js questions"
    This reverts commit f5a92f8841a3ab888ea04efe6b354dac25109b9e.

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -22,32 +22,10 @@ const till = {
 };
 const totalAmount = totalTill(till);
 
-// a) What is the target output when totalTill is called with the till object >> "£4.40"
+// a) What is the target output when totalTill is called with the till object
 
-// b) Why do we need to use Object.entries inside the for...of loop in this function? >> To iterate over both the keys (coin types) and values (quantities) of the till object.
+// b) Why do we need to use Object.entries inside the for...of loop in this function?
 
-// c) What does coin * quantity evaluate to inside the for...of loop? >> It evaluates to NaN because coin is a string (e.g., "1p") and cannot be directly multiplied by a number (quantity).
+// c) What does coin * quantity evaluate to inside the for...of loop?
 
 // d) Write a test for this function to check it works and then fix the implementation of totalTill
-
-// Fixed implementation
-function totalTillFixed(till) {
-  let total = 0;
-
-  for (const [coin, quantity] of Object.entries(till)) {
-    const coinValue = parseInt(coin);
-    total += coinValue * quantity;
-  }
-
-  return `£${(total / 100).toFixed(2)}`;
-}
-
-// Test
-const tillTest = {
-  "1p": 10,
-  "5p": 6,
-  "50p": 4,
-  "20p": 10,
-};
-const totalAmountTest = totalTillFixed(tillTest);
-console.log(totalAmountTest);
