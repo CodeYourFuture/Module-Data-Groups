c7e49b65d2e81e660b2639d99495d54c90b5c34c
Konvaly <konvaly@gmail.com>
2025-12-01 01:22:41 +0000

    Explained and fixed debug/recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// I think it incorrectly tries to log ingredients - this should be an array,
+// but there's missed .ingredients after the object name, so it will likely log something like "[object Object]" instead of the actual ingredients.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -12,4 +14,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients.join(", ")}`);
