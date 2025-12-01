cca85e965fff097ff834d85797f0adfd867bd411
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-12-01 08:36:28 +0000

    remove unncessary clone

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,25 +6,23 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  // must be an array
+ 
   if (!Array.isArray(list)) return null;
-  //filter out non-numeric values
-
   const numericList = list.filter(
     (item) => typeof item === "number" && !isNaN(item)
   );
-  // return null if no valid numbers
+
   if (numericList.length === 0) return null;
 
-  sortedList = [...numericList];
 
-  sortedList.sort((a, b) => a - b);
-  if (sortedList.length % 2 === 0) {
-    let middleIndex = sortedList.length / 2;
-    return (sortedList[middleIndex] + sortedList[middleIndex - 1]) / 2;
+
+  numericList.sort((a, b) => a - b);
+  if (numericList.length % 2 === 0) {
+    let middleIndex = numericList.length / 2;
+    return (numericList[middleIndex] + numericList[middleIndex - 1]) / 2;
   } else {
-    const middleIndex = Math.floor(sortedList.length / 2);
-    const median = sortedList.splice(middleIndex, 1)[0];
+    const middleIndex = Math.floor(numericList.length / 2);
+    const median = numericList.splice(middleIndex, 1)[0];
     return median;
   }
 }
