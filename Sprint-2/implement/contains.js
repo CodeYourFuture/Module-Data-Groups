a2bb8bfacba0b4f62f775c53de039808e072bd71
Konvaly <konvaly@gmail.com>
2025-12-01 01:43:47 +0000

    Added tests and solved implement/contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,6 @@
-function contains() {}
+function contains(obj, prop) {
+  if (Object.keys(obj).length === 0) return false;
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -17,19 +17,32 @@ as the object doesn't contains a key of 'c'
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
 
-// Given an empty object
-// When passed to contains
-// Then it should return false
-test.todo("contains on empty object returns false");
+describe("contains", () => {
+  // Given an empty object
+  // When passed to contains
+  // Then it should return false
+  test("contains on empty object returns false", () => {
+    expect(contains({}, "a")).toBe(false);
+  });
 
-// Given an object with properties
-// When passed to contains with an existing property name
-// Then it should return true
+  // Given an object with properties
+  // When passed to contains with an existing property name
+  // Then it should return true
+  test("contains on object with existing property returns true", () => {
+    expect(contains({ a: 1, d: 4 }, "d")).toBe(true);
+  });
 
-// Given an object with properties
-// When passed to contains with a non-existent property name
-// Then it should return false
+  // Given an object with properties
+  // When passed to contains with a non-existent property name
+  // Then it should return false
+  test("contains on object with non-existent property returns false", () => {
+    expect(contains({ a: 1, b: 4 }, "c")).toBe(false);
+  });
 
-// Given invalid parameters like an array
-// When passed to contains
-// Then it should return false or throw an error
+  // Given invalid parameters like an array
+  // When passed to contains
+  // Then it should return false or throw an error
+  test("contains on invalid parameters returns false", () => {
+    expect(contains([], "a")).toBe(false);
+  });
+});
