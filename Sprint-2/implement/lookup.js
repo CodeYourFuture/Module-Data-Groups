97848e5022456eb5972ca946f1aa7dfb32601fd6
zilinskyte <zilinskyterogers@gmail.com>
2025-12-01 20:13:07 +0000

    indentation improved

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -6,14 +6,13 @@ function createLookup(pairs) {
 
   const lookup = {}; // Initialize empty lookup object, key-value pairs stored in this object
   for (const pair of pairs) {
-   // Iterate over each pair in the input array
-  if (Array.isArray(pair) && pair.length >= 2) {
-    const [countryCode, currencyCode] = pair; // Destructuring first two elements of pair array and assigning them to country and currency
-    lookup[countryCode] = currencyCode; // Add country-currency pair to the lookup object - later entries overwrite earlier ones if duplicate country codes exist.
+    // Iterate over each pair in the input array
+    if (Array.isArray(pair) && pair.length >= 2) {
+      const [countryCode, currencyCode] = pair; // Destructuring first two elements of pair array and assigning them to country and currency
+      lookup[countryCode] = currencyCode; // Add country-currency pair to the lookup object - later entries overwrite earlier ones if duplicate country codes exist.
+    }
   }
-}
   return lookup;
 }
-
 
 module.exports = createLookup;
