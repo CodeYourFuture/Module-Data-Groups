75e0482be0b1f1b703e642a441d32345c63571a4
Fares Bakhet <faresak47roo@gmail.com>
2025-12-01 15:01:39 +0000

    Revert "Adding tests in tally.test.js file"
    This reverts commit 1c6ccc3cc5656df997c0b0e8a321942efe66ff0f.

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -19,37 +19,16 @@ const tally = require("./tally.js");
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
-test("tally returns counts for each unique item", () => {
-  expect(tally(["a", "a", "b", "c"])).toEqual({
-    a: 2,
-    b: 1,
-    c: 1,
-  });
-});
 
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test("tally on an empty array returns an empty object", () => {
-  expect(tally([])).toEqual({});
-});
+test.todo("tally on an empty array returns an empty object");
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
-test("tally counts each unique item", () => {
-  expect(tally(["a", "a", "b", "c", "a"])).toEqual({
-    a: 3,
-    b: 1,
-    c: 1,
-  });
-});
 
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
-test("tally throws an error when given a non-array", () => {
-  expect(() => tally("not an array")).toThrow();
-  expect(() => tally(123)).toThrow();
-  expect(() => tally({})).toThrow();
-});
