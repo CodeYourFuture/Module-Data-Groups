a3a1636a5afac1b56c4b7b3c8940a4c9b128722c
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-12-01 10:09:21 +0000

    remove unnecessary clone and update fliter by using Number.isFinite

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,8 +1,6 @@
 function sum(elements) {
   if (!Array.isArray(elements)) return 0;
-  const numericElements = [...elements].filter(
-    (item) => typeof item === "number" && !isNaN(item)
-  );
+  const numericElements = elements.filter(Number.isFinite)
   if (numericElements.length === 0) return 0;
   let sum = 0;
   for (let i = 0; i < numericElements.length; i++) {
