387c8410f7829b70da9e8e8face4f4383a307764
Konvaly <konvaly@gmail.com>
2025-12-01 01:05:40 +0000

    Explained and fixed debug/address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+//I guess that the code will log "My house number is undefined" because address[0] isn't a valid way
+// to access the houseNumber property. When we do address[0], JS looks for a property literally
+// named "0", because bracket notation treats 0 as the string "0"
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +15,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address["houseNumber"]}`);
