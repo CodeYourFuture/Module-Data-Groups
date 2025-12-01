89482b0369f299222b812b1235d37d9d63eb7cff
Fithi Teklom <fthiteklom@gmail.com>
2025-12-01 18:17:30 +0000

    tally.test.js is done

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -6,11 +6,20 @@ function parseQueryString(queryString) {
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+
+    const idx = pair.indexOf("=");
+   
+    if (idx === -1) {
+      // No '=' found, treat entire pair as key with empty value
+      queryParams[pair] = "";
+    } else {
+    const key = pair.substring(0, idx);
+      const value = pair.substring(idx + 1);
     queryParams[key] = value;
   }
 
   return queryParams;
+}
 }
 
 module.exports = parseQueryString;
--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -23,12 +23,34 @@ const tally = require("./tally.js");
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
 
+
+// Empty array → empty object
+test("tally on an empty array returns an empty object", () => {
+  expect(tally([])).toEqual({});
+});
+
+
+test("tally counts occurrences of items", () => {
+  expect(tally(["a", "a", "b", "c"])).toEqual({
+    a: 2,
+    b: 1,
+    c: 1
+  });
+});
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+test("tally works for a single repeated item", () => {
+  expect(tally(["x", "x", "x"])).toEqual({ x: 3 });
+});
 
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("tally throws an error for invalid input", () => {
+  expect(() => tally("not an array")).toThrow("Input must be an array");
+});
+
+
+
