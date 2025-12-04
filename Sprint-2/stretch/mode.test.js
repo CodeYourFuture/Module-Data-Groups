0088e751e9d5ef949eeb1ab19c7515ed920b8c3f
UbunMen <mahtem23@gmail.com>
2025-12-04 20:09:37 +0000

    In mode.test.js test cases passed

--- Sprint-2/stretch/mode.test.js
+++ Sprint-2/stretch/mode.test.js
@@ -13,20 +13,29 @@ const calculateMode = require("./mode.js");
 
 describe("calculateMode()", () => {
   test("returns the most frequent number in an array", () => {
-    const nums = [2, 4, 1, 2, 3, 2, 1];
+    const arr = [2, 4, 1, 2, 3, 2, 1];
 
-    expect(calculateMode(nums)).toEqual(2);
+    expect(calculateMode(arr)).toEqual(2);
   });
 
   test("returns the first mode in case of multiple modes", () => {
-    const nums = [1, 2, 2, 3, 3];
+    const arr = [1, 2, 2, 3, 3];
 
-    expect(calculateMode(nums)).toEqual(2);
+    expect(calculateMode(arr)).toEqual(2);
   });
 
   test("ignores non-number values", () => {
-    const nums = [1, 3, "2", 2, 3, null];
+    const arr = [1, 3, "2", 2, 3, null];
 
-    expect(calculateMode(nums)).toEqual(3);
+    expect(calculateMode(arr)).toEqual(3);
   });
+
+  test("returns NaN for an empty array", () => {
+  expect(calculateMode([])).toEqual();
 });
+
+});
+
+// In mode.test.js test cases passed
+
+
