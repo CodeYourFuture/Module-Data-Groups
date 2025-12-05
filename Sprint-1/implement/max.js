8aae3f8351b014d06259df8e88d1e250a64f7fd3
zilinskyte <zilinskyterogers@gmail.com>
2025-12-05 13:48:33 +0000

    Refactor findMax to eliminate unnecessary array filtering

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -2,18 +2,16 @@ function findMax(elements) {
   if (!Array.isArray(elements) || elements.length === 0) {
     return -Infinity;
   }
-    const numbers = elements.filter(
-      (x) => typeof x === "number" && Number.isFinite(x)
-    );
-    // Filters out all non-numeric values
-    if (numbers.length === 0) return -Infinity;
-    // Matches empty array behavior
-    let max = -Infinity; // All negative and positive numbers are greater than this minimum starting value
-    for (const n of numbers) {
-      // Loops through each n in array, if greater than max, assigns n to max
-      if (n > max) max = n;
-    }
-    return max;
+    let max = -Infinity;
+
+for (const x of elements) {
+  if (typeof x === "number" && Number.isFinite(x)) {
+    if (x > max) max = x;
+  }
+}
+
+return max;
+// removed .filter as this creates a new array so this avoids unnecessary clones
   
 }
 
