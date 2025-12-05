7a4e7d371ec806c727e51b413e7bdd23a72e5da1
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-05 19:06:53 +0000

    complete implement/max.js and tests

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,13 @@
 function findMax(elements) {
+  if (elements.length === 0) {
+    return -Infinity;
+  }
+
+  const numbers = elements
+    .filter((val) => typeof val === "number")
+    .sort((a, b) => a - b);
+
+  return Math.max(...numbers);
 }
 
 module.exports = findMax;
