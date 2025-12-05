e501543fdd6c7da91a768d550de6a26773cef123
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-05 19:18:23 +0000

    complete implement/sum.js and tests

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,10 @@
 function sum(elements) {
+  const total = elements
+    .filter((val) => typeof val === "number")
+    // Returns initial value(0) for empty arrays
+    .reduce((total, current) => total + current, 0);
+
+  return total;
 }
 
 module.exports = sum;
