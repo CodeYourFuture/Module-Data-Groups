1d983a0a4f95d5b6a86fe273516c8b02e55765f2
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-05 19:31:32 +0000

    refactor: simplify sum function by returning the result directly

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,10 +1,10 @@
 function sum(elements) {
-  const total = elements
-    .filter((val) => typeof val === "number")
-    // Returns initial value(0) for empty arrays
-    .reduce((total, current) => total + current, 0);
-
-  return total;
+  return (
+    elements
+      .filter((val) => typeof val === "number")
+      // Returns initial value(0) for empty arrays
+      .reduce((total, current) => total + current, 0)
+  );
 }
 
 module.exports = sum;
