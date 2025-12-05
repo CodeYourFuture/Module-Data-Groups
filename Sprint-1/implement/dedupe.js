339a9324b89068ecce783b760e89585d3c3e5f33
Ahmad Hmedan <ahmadhmedann@gmail.com>
2025-12-05 09:57:46 +0000

    use built-in function

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,17 +1,8 @@
-function dedupe(elements) {
-  const elementsClone = [...elements];
-  if (elementsClone.length === 0) return elementsClone;
 
-  for (let i = 1; i < elementsClone.length; i++) {
-    for (let j = i; j > 0; j--) {
-      if (elementsClone[i] === elementsClone[j - 1]) {
-        elementsClone.splice(i, 1);
-        i--; // because I deleted one element from my array
-        break;
-      }
-    }
-  }
-  return elementsClone;
-}
+ function dedupe(elements) {
+   return [...new Set(elements)];
+ }
+
+
 
 module.exports = dedupe;
