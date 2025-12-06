e1c9439ea19152479dc2784b8b34022b60b55d9a
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-06 11:38:31 +0000

    implement contains function with tests

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,18 @@
-function contains() {}
+// Implement a function called contains that checks an object contains a
+// particular property
+
+// E.g. contains({a: 1, b: 2}, 'a') // returns true
+// as the object contains a key of 'a'
+
+// E.g. contains({a: 1, b: 2}, 'c') // returns false
+// as the object doesn't contains a key of 'c'
+
+function contains(obj, property) {
+  if (obj === null || obj === undefined) {
+    return false;
+  }
+
+  return property in obj;
+}
 
 module.exports = contains;
