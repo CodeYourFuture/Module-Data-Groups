bd20fa115b6d3fb94d35542ff1362f86b6cebc94
UbunMen <mahtem23@gmail.com>
2025-12-06 14:16:32 +0000

    In till.js function implemented.

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -5,27 +5,35 @@
 // Then it should return the total amount in pounds
 
 function totalTill(till) {
+  if (typeof till !== "object" || till === null || Array.isArray(till)) {
+    throw new Error("Input should be an object");
+  }
+
   let total = 0;
 
   for (const [coin, quantity] of Object.entries(till)) {
-    total += coin * quantity;
+    const value = parseInt(coin); // extracts the numeric part
+
+    if (isNaN(value)) continue;
+
+    total += value * quantity;
   }
 
-  return `£${total / 100}`;
+  const pounds = Math.floor(total / 100);
+  const pence = String(total % 100).padStart(2, "0");
+
+  return `£${pounds}.${pence}`;
 }
 
-const till = {
-  "1p": 10,
-  "5p": 6,
-  "50p": 4,
-  "20p": 10,
-};
-const totalAmount = totalTill(till);
+module.exports = totalTill;
 
 // a) What is the target output when totalTill is called with the till object
-
+// --> The target output should be £4.40
 // b) Why do we need to use Object.entries inside the for...of loop in this function?
-
+// --> To get both the key and value of the till object inside the loop.
 // c) What does coin * quantity evaluate to inside the for...of loop?
-
+// --> It is supposed to calculate the total number of pence
 // d) Write a test for this function to check it works and then fix the implementation of totalTill
+
+
+// In till.js function implemented. 
