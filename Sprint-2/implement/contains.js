e1377fcea1c70cc99fc06848debf4a997921deed
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-06 11:38:53 +0000

    refactor: remove unnecessary comments from contains function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,12 +1,3 @@
-// Implement a function called contains that checks an object contains a
-// particular property
-
-// E.g. contains({a: 1, b: 2}, 'a') // returns true
-// as the object contains a key of 'a'
-
-// E.g. contains({a: 1, b: 2}, 'c') // returns false
-// as the object doesn't contains a key of 'c'
-
 function contains(obj, property) {
   if (obj === null || obj === undefined) {
     return false;
