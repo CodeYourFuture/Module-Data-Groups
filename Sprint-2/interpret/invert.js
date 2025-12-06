08af2c857309462a19aef76e78bcb0a77e6de812
Hadi Vahidi <hadivahidi20@gmail.com>
2025-12-06 14:13:28 +0000

    done sprint 2

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -6,7 +6,7 @@
 
 // E.g. invert({x : 10, y : 20}), target output: {"10": "x", "20": "y"}
 
-function invert(obj) {
+/*function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
@@ -14,16 +14,56 @@ function invert(obj) {
   }
 
   return invertedObj;
+}*/
+
+//fixed code:
+function invert(obj) {
+  const invertedObj = {};
+
+  for (const [key, value] of Object.entries(obj)) {
+    invertedObj[value] = key; // use the value as key, key as value
+  }
+
+  return invertedObj;
 }
 
+module.exports = invert;
+
 // a) What is the current return value when invert is called with { a : 1 }
+  //  { key: 1 }
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
+  // { key: 2 }
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
+  //{ "1": "a", "2": "b" }
+
 
 // c) What does Object.entries return? Why is it needed in this program?
+  /*Object.entries(obj) returns an array of [key, value] pairs.
+Needed because we want to loop through keys and values at the same time.*/
 
 // d) Explain why the current return value is different from the target output
 
+/*Because the code is wrong in one place:
+
+invertedObj.key = value;
+
+
+This uses the word "key" as a property name instead of using the variable key.
+
+So instead of creating:
+
+{ 1: "a" }
+
+
+it always creates:
+
+{ key: 1 }
+
+
+and for multiple items, it keeps overwriting the same "key" */
+
+
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
+    // I did write upt there.
