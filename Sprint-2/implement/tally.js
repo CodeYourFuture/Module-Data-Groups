9559666cd2ff3e847236177510d97e3d2fa30eb5
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2025-12-07 16:32:53 +0000

    implement tally function with tests

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,18 @@
-function tally() {}
+function tally(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+
+  let result = {};
+
+  for (const element of arr) {
+    if (element in result) {
+      result[element] += 1;
+    } else {
+      result[element] = 1;
+    }
+  }
+  return result;
+}
 
 module.exports = tally;
