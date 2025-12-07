0aa1999f49b5c403999660e98659fc60ba10e65a
Hadi Vahidi <hadivahidi20@gmail.com>
2025-12-07 17:03:24 +0000

    alarm clock done

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,6 +1,39 @@
 function setAlarm() {}
 
 // DO NOT EDIT BELOW HERE
+let countdown;
+
+function setAlarm() {
+  let seconds = Number(document.getElementById("alarmSet").value);
+
+  function updateDisplay() {
+    let mins = Math.floor(seconds / 60);
+    let secs = seconds % 60;
+    let mm = String(mins).padStart(2, "0");
+    let ss = String(secs).padStart(2, "0");
+    document.getElementById("timeRemaining").innerText =
+      "Time Remaining: " + mm + ":" + ss;
+  }
+
+  updateDisplay();
+  clearInterval(countdown);
+
+  countdown = setInterval(function () {
+    seconds--;
+    updateDisplay();
+
+    if (seconds <= 0) {
+      clearInterval(countdown);
+      playAlarm();
+    }
+  }, 1000);
+}
+
+function pauseAlarm() {
+  audio.pause();
+  clearInterval(countdown);
+}
+
 
 var audio = new Audio("alarmsound.mp3");
 
