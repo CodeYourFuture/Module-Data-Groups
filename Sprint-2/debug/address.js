9819f2c63e51f6077ae0765db0560e958def53a3
asaniDev <asanidev9@gmail.com>
2025-12-08 10:56:12 +0000

    Revert "predicted the result of each file before fixing them"
    This reverts commit f21c96e6e26ba3ce80b0bf42afe82a6ec0937e1b.

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,10 +1,8 @@
 // Predict and explain first...
-//the original code is using the wrong notation to access our object this will return undefined as we do not have a 0 property.
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
-//fixed it by puting "houseNumber" in the square brackets.
 
 const address = {
   houseNumber: 42,
@@ -14,4 +12,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address["houseNumber"]}`);
+console.log(`My house number is ${address[0]}`);
