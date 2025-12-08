972539973fdfbc2d63b5e59de6952e8571379afe
asaniDev <asanidev9@gmail.com>
2025-12-08 10:56:12 +0000

    Revert "fixed invert function,answered questions and added tests to check it works as expected"
    This reverts commit e17bf3024c0beb907faf24b371e3997fec46f2aa.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -9,54 +9,21 @@
 function invert(obj) {
   const invertedObj = {};
 
-  if (Object.prototype.toString.call(obj) === "[object Object]") {
-    for (const [key, value] of Object.entries(obj)) {
-      invertedObj[value] = key;
-    }
-  } else {
-    throw new Error("error invalid input entered, expecting an object");
+  for (const [key, value] of Object.entries(obj)) {
+    invertedObj.key = value;
   }
 
   return invertedObj;
 }
 
-module.exports = invert;
-
-console.log(
-  `the return value when invert is called with ${invert({
-    cars: { toyota: 2, bmw: 1, benz: 4 },
-  })}`
-);
-
-/*
-for (const property in obj) {
-      if (typeof obj[property] !== "string") {
-        throw new Error(
-          "error invalid input entered, expecting an object to have only strings as values"
-        );
-      } else {
-        
-      }
-    }
-
-*/
-
 // a) What is the current return value when invert is called with { a : 1 }
-//it returns a string describing the object.
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
-//it aso returns a string describing the object.
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
-//the target return value is {"1": "a", "2": "b"}.
 
 // c) What does Object.entries return? Why is it needed in this program?
-//it returns an array made up of arrays of the original objects key - value pairs.
-//it allows us to unpack the contents of the object into an array which can then be used to create the new object
 
 // d) Explain why the current return value is different from the target output
-//because we used dot notation to assign a value to our key, this creates a property called key
-//and assigns it a value. we want our key to get its name from a variable so we need to use bracket notation.
 
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
-//we can fix it by using invertedObj[value] = key.
