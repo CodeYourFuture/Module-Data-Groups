8d1cad5699ad1de57be845b0506206096043fcf5
khalidbih <kain19m@hotmail.com>
2025-12-09 18:37:41 +0000

    Fix function and tests

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,11 @@
 function sum(elements) {
+  let total = 0;
+  for (const el of elements) {
+    if (typeof el === "number") {
+      total += el;
+    }
+  }
+  return total;
 }
-
 module.exports = sum;
+
