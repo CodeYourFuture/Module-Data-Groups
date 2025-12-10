161b8710a9ef72210f47c266276748bf585f9f13
UbunMen <mahtem23@gmail.com>
2025-12-10 12:42:24 +0000

    In contains.js modified return function as return Object.hasOwn(obj, key);

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -2,10 +2,17 @@ function contains(obj, key) {
   if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
     return false;
 }
-return key in obj;
+//return key in obj;
+return Object.hasOwn(obj, key);
 
 }
 
 module.exports = contains;
 
 // In contains.js function implemented and tested. 
+
+  let obj = {}, propertyName = "toString";
+  console.log( propertyName in obj );                // true
+  console.log( Object.hasOwn(obj, propertyName) );   // false
+
+  // In contains.js modified return function as return Object.hasOwn(obj, key);
