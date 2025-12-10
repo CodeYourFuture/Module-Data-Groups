8e4807e51132b7742bad2dbea0229e77c918f665
UbunMen <mahtem23@gmail.com>
2025-12-10 17:29:54 +0000

    Test case description updated

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -52,11 +52,13 @@ test("Should return false when object does not have the property", () => {
 // When passed to contains
 // Then it should return false or throw an error
 
-test("Should return false when invalid parameters are used", () => {
+test("Should correctly detect keys in arrays", () => {
   expect(contains([1, 2, 3], "1")).toEqual(true); // "1" is a key
   expect(contains([1, 2, 3], "3")).toEqual(false); // "3" is not a key
-  //expect(contains([], "a")).toEqual(false);      // array
-  expect(contains(null, "a")).toEqual(false);    // null 
+});
+
+test("Should return false when invalid parameters are used", () => {
+   expect(contains(null, "a")).toEqual(false);    // null 
   expect(contains(5, "a")).toEqual(false);       // number 
   expect(contains("hello", "a")).toEqual(false); // string 
 })
@@ -64,3 +66,5 @@ test("Should return false when invalid parameters are used", () => {
 // In contains.test.js tests for contains.test.js passed 
 
 // contains.test.js re-tested.
+
+// test case description differentiated. 
