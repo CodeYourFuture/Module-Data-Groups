6d5cccc13e3c7adf1d1cef354e1f10666a75d9dd
Hadi Vahidi <hadivahidi20@gmail.com>
2025-12-13 14:14:26 +0000

    solve the stop button issue

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,6 +1,33 @@
 function setAlarm() {}
 
 // DO NOT EDIT BELOW HERE
+let countdown;
+
+function setAlarm() {
+  let seconds = Number(document.getElementById("alarmSet").value);
+
+  function updateDisplay() {
+    let mins = Math.floor(seconds / 60);
+    let secs = seconds % 60;
+    let mm = String(mins).padStart(2, "0");
+    let ss = String(secs).padStart(2, "0");
+    document.getElementById("timeRemaining").innerText =
+      "Time Remaining: " + mm + ":" + ss;
+  }
+
+  updateDisplay();
+  clearInterval(countdown);
+
+  countdown = setInterval(function () {
+    seconds--;
+    updateDisplay();
+
+    if (seconds <= 0) {
+      clearInterval(countdown);
+      playAlarm();
+    }
+  }, 1000);
+}
 
 var audio = new Audio("alarmsound.mp3");
 
@@ -19,7 +46,10 @@ function playAlarm() {
 }
 
 function pauseAlarm() {
+  /* Stop the bell and freeze the timer */
+  clearInterval(countdown);
   audio.pause();
+  audio.currentTime = 0;
 }
 
 window.onload = setup;
