dd2c422881672ba0a8f70208e783238bd181c863
asaniDev <asanidev9@gmail.com>
2025-12-17 02:09:39 +0000

    removed redundant code from contantains to remain with one if else statement

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -4,25 +4,10 @@ function contains(checkObj, checkProp) {
     checkObj !== null &&
     !Array.isArray(checkObj)
   ) {
-    const checkKeys = Object.keys(checkObj);
-    let isProperty = false;
-
-    if (checkKeys.length === 0) {
-      return false;
-    }
-
     if (Object.hasOwn(checkObj, checkProp)) {
       return true;
     } else {
-      for (let key of checkKeys) {
-        if (key === checkProp) {
-          isProperty = true;
-        }
-      }
-
-      if (isProperty === false) {
-        return false;
-      }
+      return false;
     }
   } else {
     throw new Error("error invalid parameter please provide an object");
