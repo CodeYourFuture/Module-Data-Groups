d36586d1e73c71d30917708b09b32082af72f8ec
Mohsen Zamani <mohsenzamanist@gmail.com>
2026-02-04 20:13:09 +0000

    Complete feature/sprint-3-alarmclock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,27 @@
-function setAlarm() {}
+function setScreenTimer(totalSeconds) {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  const timeRemainingSpan = document.querySelector("#timeRemaining span");
+  timeRemainingSpan.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+}
+
+function setAlarm() {
+  const alarmSet = document.getElementById("alarmSet");
+  let totalSeconds = Number(alarmSet.value);
+
+  setScreenTimer(totalSeconds);
+
+  const timer = setInterval(() => {
+    totalSeconds--;
+    if (totalSeconds <= 0) {
+      setScreenTimer(0);
+      clearInterval(timer);
+      playAlarm();
+    } else {
+      setScreenTimer(totalSeconds);
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
