06339fc07627a0e162a484a322de8498c9df374b
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-23 00:20:00 +0200

    Implement dedupe

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,10 @@
-function dedupe() {}
+function dedupe(list) {
+  if (!Array.isArray(list))
+    throw new Error("Please input an array e.g. [1, 3, 6]");
+
+  if (list.length === 0) return [];
+
+  return [...new Set(list)];
+}
+
+module.exports = dedupe;
