ebbad7ea46c90190efbf855473a75fbd9b8e6a56
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-25 23:07:11 +0200

    Fix: reduce number of decimal places returned for sum

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,11 @@
 function sum(elements) {
+  const filteredElements = elements.filter((element) =>
+    Number.isFinite(element)
+  );
+
+  return parseFloat(
+    filteredElements.reduce((total, num) => total + num, 0).toFixed(10)
+  );
 }
 
 module.exports = sum;
