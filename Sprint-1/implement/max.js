cc952dd8584f5babe6f00fee76077f25ce176ccf
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-02-25 18:06:54 +0200

    Implement findMax

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,7 @@
 function findMax(elements) {
+  const numbersOnly = elements.filter((element) => Number.isFinite(element));
+
+  return numbersOnly.reduce((a, b) => Math.max(a, b), -Infinity);
 }
 
 module.exports = findMax;
