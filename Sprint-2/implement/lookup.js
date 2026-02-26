e62810cecf0e274659801e724cf84384e67648bd
Raihan Sharif <raihansharif92@gmail.com>
2026-02-26 12:22:33 +0000

    implement: lookup and tests

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,25 @@
-function createLookup() {
-  // implementation here
+// check if arry is 2d, and each element is a k,v pair and k,v both strings
+function is2dKeyValStringArray(arr) {
+  return (
+    Array.isArray(arr) &&
+    arr.every((element) => {
+      return (
+        Array.isArray(element) &&
+        element.length === 2 &&
+        typeof element[0] === "string" &&
+        typeof element[1] === "string"
+      );
+    })
+  );
+}
+
+function createLookup(countryAndCurrency) {
+  // bit lazy, should have tests for type, and content seperately,
+  // to throw more specific errors
+  if (!is2dKeyValStringArray(countryAndCurrency)) {
+    throw new Error("Input is not valid type and/or format");
+  }
+  return Object.fromEntries(countryAndCurrency);
 }
 
 module.exports = createLookup;
