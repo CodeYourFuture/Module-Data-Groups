925a44ce5f9a3eb23cd97a3720a97cac3437deca
Raihan Sharif <raihansharif92@gmail.com>
2026-02-26 11:10:43 +0000

    Fix: author

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,11 @@
 // Predict and explain first...
+/*
+Prediction: TypeError: author is not iterable
+
+Cause: Plain Js objects aren't iterable the way an array's elements are.
+To iterate over keys and/or values use the Object.keys(), Object.values(), 
+or Object.entries() methods to covert the data into an array first.
+*/
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +18,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
