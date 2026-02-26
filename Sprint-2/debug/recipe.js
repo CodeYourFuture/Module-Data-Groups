b65a94d7b4b8de35151eac20d7838e6d81c472d5
Raihan Sharif <raihansharif92@gmail.com>
2026-02-26 11:16:47 +0000

    Fix: recipe

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,13 @@
 // Predict and explain first...
 
+/*
+Prediction: the ingredients: ${recipe} part will only print something like [Object object]
+
+Cause: The code is attempting to print the recipe object itself, rather than the ingredients.
+In Js, when printing objects, it doesn't print a string of key, pair values of its 
+properties. Should be printing the ingredients property, which is an array, which does print out its values.
+*/
+
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -12,4 +20,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients}`);
