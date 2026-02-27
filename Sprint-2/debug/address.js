a5f25c773d2954ed3b7dc7265c667654187834a9
pathywang <pathywang@gmail.com>
2026-02-27 21:37:59 +0000

    commit

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,8 +1,9 @@
 // Predict and explain first...
+// it will print out My house number is undefined because the address is object rather than array so address[0] will look for property 0 which does not exit
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
-// Fix anything that isn't working
+// Fix anything that isn't working, it should be address.houseNumber in order to get value 42
 
 const address = {
   houseNumber: 42,
@@ -12,4 +13,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,7 +1,9 @@
 // Predict and explain first...
+// it will throw error or just print out undefined because 
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
+// Because author is not array rather object so we can not use for loop, we use object.values to get loop
 
 const author = {
   firstName: "Zadie",
@@ -11,6 +13,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
