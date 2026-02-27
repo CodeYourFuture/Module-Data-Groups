53ef6bbc9de1405460d520edd902b3392181de3c
Elisabeth-Matulian <elisabethmatulian@gmail.com>
2026-02-27 06:35:21 +0000

    completed-fix

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,30 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  if (Array.isArray(list)) {
+    let newList = list.filter(item => typeof item === "number");
+    newList = newList.sort(function (a, b) {
+      return a - b;
+    });
+    if (newList.length > 0){
+      const middleIndex = Math.floor(newList.length / 2);
+      const median = newList[middleIndex]
+      if (newList.length % 2 !== 0) {
+        return median;
+    }
+      else return (newList[middleIndex - 1] + median) / 2}
+    else return null;}
+  else return null;
 }
 
 module.exports = calculateMedian;
+
+
+
+
+
+// function arr(item) {
+//   if (typeof item === "number")
+//     return true;
+//   return false;
+// }
