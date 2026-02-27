ca26a21e0048f36027128bc2b50f9794f634f38f
pathywang <pathywang@gmail.com>
2026-02-27 22:01:50 +0000

    commit

---
+++ Sprint-2/debug/recipe.js
@@ -0,0 +1,19 @@
+// Predict and explain first...
+// It might print out undefined or something else
+
+// This program should log out the title, how many it serves and the ingredients.
+// Each ingredient should be logged on a new line
+// How can you fix it? i will loop through ingredient to get each single ingredient
+
+const recipe = {
+  title: "bruschetta",
+  serves: 2,
+  ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
+};
+
+console.log(`${recipe.title} serves ${recipe.serves}
+ingredients:`);
+
+for (const ingredient of recipe.ingredients) {
+  console.log(ingredient);
+}
