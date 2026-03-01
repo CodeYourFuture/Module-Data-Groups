0503c00a486f4e8c4b5e61f380ab09a58e7cb739
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-03-01 21:37:39 +0000

    Predicting why the program for author.js will not work

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+// The issue with this program is that for.. of only works on iterable objects.
+// Plain JavaScript objects are not iterable, so this will throw a "TypeError".
+// To fix this, we can use Object.values(author) which converts the object's value into an array.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
