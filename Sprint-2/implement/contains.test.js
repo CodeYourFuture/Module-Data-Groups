b8b5c2ca7213ccc2b38ddf2b8f843fd811a6b072
pathywang <pathywang@gmail.com>
2026-03-01 12:11:38 +0000

    commit

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,16 +20,30 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
+object={}
+console.log(object,'')
 test.todo("contains on empty object returns false");
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+object={name:'jin', age:13}
+console.log (contains(object, 'age'))
+test.todo("contains age property in object return true")
+
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+object={name:'jin', age:13}
+console.log (contains(object, 'nice'))
+test.todo("contains no nice property  in object return false")
+
+
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+object={name:'jin', age:13}
+console.log (contains(object, '[1,2,5]'))
+test.todo("contains no array property  in object return false")
