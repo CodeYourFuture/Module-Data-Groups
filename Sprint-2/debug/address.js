d1481372d3b39c36abc987b86579faeea2d73179
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-03-01 21:28:01 +0000

    Updating the file with my rediction

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+//The issue with this program is that console.log is logging the address but is an obkect. address[0] only works for arrays. 
+//As address is an object, we must use a key rather than numeric indexes. 
+//Also there is no property called "0" inside address.
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
