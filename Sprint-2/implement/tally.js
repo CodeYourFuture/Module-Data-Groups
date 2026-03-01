4b2cca7d66a22226c86882765b942077864f02b4
pathywang <pathywang@gmail.com>
2026-03-01 17:54:55 +0000

    complete tally

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,27 @@
-function tally() {}
+function tally(array) {
+    if (!Array.isArray(array)) {
+    throw new Error("Invalid input: expected an array");
+  }
+
+  const count={}
+  for(item of array){
+    if (count[item]){
+        count[item] += 1
+    }
+    else{
+        count[item] = 1
+    }
+  }
+  return count
+  }
+
+console.log(tally([ 2,"bee",2,"apple","apple",2, "banana"]))
+
+try {
+  console.log(tally('morning'));
+} catch (err) {
+  console.error(err.message);
+}
+
 
 module.exports = tally;
