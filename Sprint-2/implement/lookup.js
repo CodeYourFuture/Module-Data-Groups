f440646e707cc74046c1956d9a4d5bb4cd91ba2c
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-03-01 23:25:54 +0000

    Adding the program implementation for lookup.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,12 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+const result = {};
+for (const pair of pairs) {
+  const countryCode = pair[0];
+  const currencyCode = pair[1];
+  result[countryCode] = currencyCode;
+}
+
+return result;
 }
 
 module.exports = createLookup;
