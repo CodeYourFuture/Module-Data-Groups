1f294322f5f0b198717152904fd20851a2a5802e
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-03-01 21:51:12 +0000

    Updating my prediction for recipe.js file

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,9 @@
 // Predict and explain first...
+//The issue with this program is that ${recipe} inserts the entire object into the template string.
+// When an object is converted to a string, it becomes "[object Object]".
+//Instead we would need to access the ingredients array.
+//We can loop through recipe.ingredients or use join("\n")
+//This will print each ingredients on a new line.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
