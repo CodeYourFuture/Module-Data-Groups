a0f6c7ed419ef38388d9dec3dcf3dcc38bb2f296
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-03-02 12:14:34 +0000

    updates to Sprint1/fix dedupe function and test

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,8 +6,24 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
+  // only operate on non-empty arrays
+  if (!Array.isArray(list) || list.length === 0) {
+    return null;
+  }
+
+  const numbers = list.filter((item) => typeof item === "number");
+  if (numbers.length === 0) {
+    return null;
+  }
+
+  numbers.sort((a, b) => a - b);
+  if (numbers.length % 2 === 0) {
+    const mid1 = numbers.length / 2 - 1;
+    const mid2 = numbers.length / 2;
+    return (numbers[mid1] + numbers[mid2]) / 2;
+  }
+  const middleIndex = Math.floor(numbers.length / 2);
+  const median = numbers[middleIndex];
   return median;
 }
 
