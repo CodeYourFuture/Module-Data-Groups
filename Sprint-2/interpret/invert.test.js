c885402388bbd83f3d29463955564a59a77c2f70
KayanatSuleman <kayanat.suleman@yahoo.co.uk>
2026-03-02 23:13:34 +0000

    Adding more test to cover other instances

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -3,3 +3,14 @@ const invert = require("./invert.js");
 test("inverts a single key value pair", () => {
     expect(invert({ a: 1 })).toEqual({ "1": "a" });
 });
+
+test("inverts multiple key value pairs", () => {
+    expect(invert({ a: 1, b: 2 })).toEqual({ 
+        "1": "a", 
+        "2": "b"
+    });
+});
+
+test("returns empty object when given empty object", () => {
+  expect(invert({})).toEqual({});
+});
