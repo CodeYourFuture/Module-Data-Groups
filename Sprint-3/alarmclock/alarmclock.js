d8b6542a8f14d7d2b3eef505f6f052dec0e1f0e1
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-03-02 21:09:17 +0000

    Implemented intial alarm set and time display update

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,20 @@
-function setAlarm() {}
+function setAlarm() {
+  // Find the input box by its id
+  const input = document.getElementById("alarmSet");
+  // Get whatever number the user typed,
+  // and convert it to a real number
+  const minutes = parseInt(input.value, 10);
+  // If nothing useful was entered, do nothing
+  if (isNaN(minutes) || minutes <= 0) {
+    return;
+  }
+  // Turn minutes to mm:ss format
+  const displayMinutes = minutes.toString().padStart(2, "0");
+  const display = `Time Remaining: ${displayMinutes}:00`;
+  // Find the <h1> and update it
+  const heading = document.getElementById("timeRemaining");
+  heading.textContent = display;
+}
 
 // DO NOT EDIT BELOW HERE
 
