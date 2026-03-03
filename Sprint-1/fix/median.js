b4e3fb41fef6978e3669c2a6a2099ee3a2f5c16c
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-03-03 11:56:33 +0000

    median

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -2,13 +2,26 @@
 // Start by running the tests for this function
 // If you're in the Sprint-1 directory, you can run `npm test -- fix` to run the tests in the fix directory
 
+
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
+
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+
+  if (!Array.isArray(list)) return null;
+
+  const numbers = list.filter(item => typeof item === 'number');
+  if (numbers.length === 0) return null;
+
+  numbers.sort((a, b) => a - b);
+  const middleIndex = Math.floor(numbers.length / 2);
+
+  if (numbers.length % 2 === 1) {
+    return numbers[middleIndex];
+  }
+  return (numbers[middleIndex - 1] + numbers[middleIndex]) / 2;
 }
 
 module.exports = calculateMedian;
+
