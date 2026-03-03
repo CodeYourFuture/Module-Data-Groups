282b0a76c7c32231989694bd857adc1e8cf4c6bb
Laura C <laura.csaki.uk@gmail.com>
2026-03-03 13:37:43 +0000

    Implement sum function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,19 @@
 function sum(elements) {
+  if (!Array.isArray(elements)) return null;
+
+  const numbers = elements.filter(
+    (x) => typeof x === "number" && Number.isFinite(x)
+  );
+
+  if (numbers.length === 0) return 0;
+
+  let total = 0;
+
+  for (const number of numbers) {
+    total += number;
+  }
+
+  return total;
 }
 
 module.exports = sum;
