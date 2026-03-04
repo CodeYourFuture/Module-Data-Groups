e1211e98ebff154c0a373e29834346b49a872d5d
Laura C <laura.csaki.uk@gmail.com>
2026-03-04 14:45:56 +0000

    Sprint 2 debug: add prediction for author.js

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,10 @@
 // Predict and explain first...
 
+// Prediction: This code will throw an error.
+// The error happens because "author is an object and objects are not iterable.
+// The "for...of" loop only works with iterable objects like arrays or strings
+// So the program will throw a type error
+
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +16,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
-  console.log(value);
+for (const key in author) {
+  console.log(author[key]);
 }
