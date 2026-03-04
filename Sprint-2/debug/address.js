14a2f59166995ae34054a984268fa4aefa8654d7
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-03-04 00:28:16 +0000

    Fixed object property access using dot notation + added prediction and explanation

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,8 @@
 // Predict and explain first...
+// address is an object, not an array
+// address[0] looks for a property named "0",
+// which doesn't exist → returns undefined
+// So the log will say: My house number is undefined
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +16,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
