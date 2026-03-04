9e7a052695ff7a177ae1b1477642844751367d04
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-03-04 23:51:18 +0000

    Implemented lookup.js function

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,16 @@
-function createLookup() {
+function createLookup(pairs) {
   // implementation here
+  // Creates empty object that will store our,
+  // country → currency
+  const lookup = {};
+  // Loop through each pair in the input array
+  for (const [country, currency] of pairs) {
+    // Use the country code as they key
+    // Assigns the correspoding currency code as the value
+    lookup[country] = currency;
+  }
+  // returns the completed lookup object
+  return lookup;
 }
 
 module.exports = createLookup;
