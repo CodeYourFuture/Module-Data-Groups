5cf1d409951b9765c64c117a44e57a83293ef04b
Fayaz <fayaz.sadat551@yahoo.com>
2026-03-05 23:40:53 +0000

    fixed the issue with the code in address, the objects are accessed using their property names by (dot). instead of index

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+// The code is trying to access the houseNumber property of the address object using an index, which is not correct.
+// In JavaScript, objects are accessed using their property names, not indices. 
+// To fix this, we should use the property name 'houseNumber' instead of an index.
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +15,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
