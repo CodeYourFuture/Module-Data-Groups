bbd644995fe52750e9546ef8e4574ec8c76e0c0e
Fayaz <fayaz.sadat551@yahoo.com>
2026-03-06 23:57:39 +0000

    wrote the function lookup to look for country: currency pair and wrote test for it

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,16 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+let lookup ={};
+for(let pair of countryCurrencyPairs){
+let key = pair[0];
+let value = pair[1];
+lookup[key] = value;
 }
+return lookup;
+
+
+
+}
+
+  
 
 module.exports = createLookup;
