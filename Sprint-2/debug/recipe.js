da8a73ddce49df9dbe0e8a2974baae0119b38fbf
Fayaz <fayaz.sadat551@yahoo.com>
2026-03-06 00:04:18 +0000

    used join method to make all the items of ingredients array in one array and then used line break to make each of them in new line

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -3,6 +3,7 @@
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
+// The issue with the code is that it is trying to log the entire recipe object directly, which will not format the ingredients as desired. Instead, we need to access the properties of the recipe object and format the output correctly. We can use template literals to format the string and join the ingredients array with new line characters.
 
 const recipe = {
   title: "bruschetta",
@@ -12,4 +13,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients.join('\n')}`);
