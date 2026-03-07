08758cb1b549a5dc5175c9a0b4c9033030ecdd86
Fida H Ali Zada <fidahussainalizada@gmail.com>
2026-03-07 20:37:11 +0000

    fixed sum function and wrote tests

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,9 @@
 function sum(elements) {
+
+    // const filteredArr = elements.filter(x => typeof x === 'number');
+    // return filteredArr.reduce((a, b) => a + b, 0);
+
+    return elements.filter(x => typeof x === 'number').reduce((a, b) => a + b, 0);
 }
 
 module.exports = sum;
