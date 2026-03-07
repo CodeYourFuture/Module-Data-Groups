0c5f55ec6cced9cdd7c848b795280b022840e1a3
Fida H Ali Zada <fidahussainalizada@gmail.com>
2026-03-07 20:53:34 +0000

    fixed dedupe function and wrote tests

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,13 @@
-function dedupe() {}
+function dedupe(array) {
+    let arr = array.filter((item, index) => {
+
+        // indexOf() returns the first index where that value appears in the array.
+        if (array.indexOf(item) === index) {
+            return item;
+        }
+    })
+
+    return arr;
+}
+
+module.exports = dedupe;
--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,12 +16,23 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+
+test("return an empty array for an empty array", () => {
+    expect(dedupe([])).toEqual([]);
+})
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
 
+test("if no duplicates, return the copy of the original array", () => {
+    expect(dedupe([1, 3, 5, 7])).toEqual([1, 3, 5, 7]);
+})
+
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
+
+test("an array with strings strings or numbers", () => {
+    expect(dedupe([2, 2, 3, 3, 3, "Black", "Black", "Black", "Green"])).toEqual([2, 3, "Black", "Green"]);
+})
