67cf526a05c80ad2c99c92dba3b4ffa629547a2f
Fida H Ali Zada <fidahussainalizada@gmail.com>
2026-03-09 14:19:00 +0000

    minor changes to querystring.js file

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,23 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
-    return queryParams;
-  }
+
+  if (!queryString) return queryParams;
 
   const keyValuePairs = queryString.split("&");
 
+  // 1. with RegEx
+  // for (const pair of keyValuePairs) {
+  //   const [key, value] = pair.split(/=(.*)/s);
+  //   queryParams[key] = value;
+  // }
+
+  // 2. with substring method
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split(/=(.*)/s);
+    const index = pair.indexOf("=");
+
+    const key = pair.substring(0, index);
+    const value = pair.substring(index + 1);
+
     queryParams[key] = value;
   }
 
