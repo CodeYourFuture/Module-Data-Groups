a0c1cedcfc3ade8abff0139cef111e616cbb1d13
AngelaMcLeary <angela.mcleary@outlook.com>
2026-03-10 22:33:33 +0000

    implemented first version of max.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,14 @@
 function findMax(elements) {
+  if (!Array.isArray(elements)) return null;
+
+  const numbers = elements.filter((num) => typeof num === "number");
+  if (numbers.length === 0) return null;
+
+  const sortedElements = [...numbers].sort((a, b) => a - b);
+
+  return Math.max(...sortedElements);
 }
+
+//console.log(findMax([200, 5, 8, 15, 90, 12]));
 
 module.exports = findMax;
