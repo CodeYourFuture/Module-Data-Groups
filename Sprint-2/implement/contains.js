95b92bac9957098b4a10485203fd640bdb7a003e
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-03-10 16:32:48 +0000

    updates to sprint 2 implement Contains and tested ok

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,10 @@
-function contains() {}
+function contains(obj, prop) {
+ return (
+   !!obj &&
+   typeof obj === "object" &&
+   !Array.isArray(obj) &&
+   Object.prototype.hasOwnProperty.call(obj, prop)
+ );
+}
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,20 +16,47 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
+test("contains returns true for existing property", () => {
+  const obj = { a: 1, b: 2 };
+  expect(contains(obj, "a")).toBe(true);
+  expect(contains(obj, "b")).toBe(true);
+  expect(contains(obj, "c")).toBe(false);
+});
 
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+  const emptyObj = {};
+  expect(contains(emptyObj, "a")).toBe(false);
+  expect(contains(emptyObj, "b")).toBe(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test("contains returns true for existing property", () => {
+  const obj2 = { x: 10, y: 20 };
+  expect(contains(obj2, "x")).toBe(true);
+  expect(contains(obj2, "y")).toBe(true);
+  expect(contains(obj2, "z")).toBe(false);
+});
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("contains returns false for non-existent property", () => {
+  const obj3 = { name: "Alice", age: 30 };
+  expect(contains(obj3, "name")).toBe(true);
+  expect(contains(obj3, "age")).toBe(true);
+  expect(contains(obj3, "gender")).toBe(false);
+});
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("contains handles invalid parameters", () => {
+  expect(contains([], "a")).toBe(false);
+  expect(contains(null, "a")).toBe(false);
+  expect(contains(undefined, "a")).toBe(false);
+});
