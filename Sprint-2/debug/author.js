bc4aae52617bf70136bcb0dd86d7bec280eb8400
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-03-10 15:20:29 +0000

    updates to sprint 2 debug author

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -11,6 +11,7 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
+// The problem is that the code is trying to log out the values of the object using a for...of loop, but it is not using the correct syntax to access the values. The Object.values() method returns an array of the object's values, so we need to use a for...of loop to iterate over that array and log each value.
