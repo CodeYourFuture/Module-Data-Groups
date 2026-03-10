f59026c5cc6956e22c3443cb948f283b9f547c77
Arunkumar Akilan <arunkumarakilanarun@gmail.com>
2026-03-10 20:51:53 +0000

    Implement sum function

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,19 @@
 function sum(elements) {
+    if (elements.length === 0)
+    {
+        return 0;
+    }
+      let total = 0 ;
+    for (const item of elements)
+{
+    if (typeof item === 'number')
+    {
+     total =  total + item;
+    }
+    
+
+}
+  return total;
 }
 
 module.exports = sum;
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,7 +13,34 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test("given an empty array should return -Infinity", () => {
+  const list = [];
+  expect(sum(list)).toEqual(0);
+});
+test("Array with one number should return that number", () => {
+    const list =[7];
+    expect(sum(list)).toEqual(7);
+});
+test("An array with both positive and negative number should return the largest overall", () =>{
+    const list = [-10, 5, 30, -3];
+    expect(sum(list)).toEqual(22);
+});
+test("Array with just negative number should return the closest on to zero", () =>{
+    const list = [-10, -3, -20,-7,-4]
+    expect(sum(list)).toEqual(-44);
+});
+test("An array with decimal number should return largest decimal number", ()=>{
+   const list = [1.2, 3.5, 2.8]
+    expect(sum(list)).toEqual(7.5);
+});
+test("An array with non-numeric values should return the max and ignore non-numeric", () =>{
+    const list = ['hey', 10, 'hi', 60, 50];
+    expect(sum(list)).toEqual(120);
+});
+test("An array with non numeric value should  return the least surprising value given how it behaves for all other inputs", ()=> {
+    const list = ['a', 'b', 'hello'];
+    expect(sum(list)).toEqual(0);
+})
 
 // Given an array with just one number
 // When passed to the sum function
