d56b0999d23b405de87a6d5c37163ccb92c3808a
RomanSanaye <sanaye143@gmail.com>
2026-03-11 14:16:53 +0000

    sum function is developed and jest test implemented

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,12 @@
 function sum(elements) {
+  if (elements.length === 0) return 0;
+  let numberElement = elements.filter((el) => typeof el === "number");
+  if (numberElement.length === 0) return Infinity;
+  let total = 0;
+  for (let num of numberElement) {
+    total += num;
+  }
+  return Math.floor(total);
 }
 
 module.exports = sum;
--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,24 +13,47 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test("Empty array should be returned Zero", () => {
+  let elements = [];
+  expect(sum(elements)).toEqual(0);
+});
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
+test("given one number should be returned same as original", () => {
+  let elements = [5];
+  expect(sum(elements)).toEqual(5);
+});
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
+test("negative number should return total with no effect", () => {
+  let elements = [1, 3, -2];
+  expect(sum(elements)).toEqual(2);
+});
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
+test("decimal number should return the correct total", () => {
+  let elements = [1, 2.3, 4.5, 6];
+  expect(sum(elements)).toEqual(13);
+});
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+test("non-number values be ignored, function still should return total", () => {
+  let elements = [1, 2, 3, "hi", "b"];
+  expect(sum(elements)).toEqual(6);
+});
 
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test("non-number values should return Infinity", () => {
+  let elements = ["a", "d", "hi"];
+  expect(sum(elements)).toEqual(Infinity);
+});
