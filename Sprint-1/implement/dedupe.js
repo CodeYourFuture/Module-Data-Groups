ed6b0966698ecf00a3c14b21ee1db88ddf1b244b
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-03-11 14:00:10 +0000

    Added dedupe implementation

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,9 @@
-function dedupe() {}
+function dedupe(arr) {
+  // Return null if the input is not an array
+  if (!Array.isArray(arr)) return null;
+  // Use Set to remove duplicates,
+  // then spread back into a new array
+  return [...new Set(arr)];
+}
+
+module.exports = dedupe;
