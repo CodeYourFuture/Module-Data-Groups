f9f7dc04aac38f6225b70fa6c4253fe4a663b957
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-03-11 21:38:05 +0000

    Add comments to includes tests

--- Sprint-1/refactor/includes.test.js
+++ Sprint-1/refactor/includes.test.js
@@ -2,6 +2,9 @@
 
 const includes = require("./includes.js");
 
+// Given an array containing the target value
+// When passed to the includes function
+// Then it should return true
 test("returns true when target is in array", () => {
   const currentOutput = includes(["a", "b", "c", "d"], "c");
   const targetOutput = true;
@@ -9,6 +12,9 @@ test("returns true when target is in array", () => {
   expect(currentOutput).toEqual(targetOutput);
 });
 
+// Given an array that does not contain the target value
+// When passed to the includes function
+// Then it should return false
 test("returns false when target not in array", () => {
   const currentOutput = includes([1, 2, 3, 4], "a");
   const targetOutput = false;
@@ -16,6 +22,9 @@ test("returns false when target not in array", () => {
   expect(currentOutput).toEqual(targetOutput);
 });
 
+// Given an array where the target appears more than once
+// When passed to the includes function
+// Then it should still return true
 test("returns true when the target is in array multiple times", () => {
   const currentOutput = includes([1, 2, 2, 3], 2);
   const targetOutput = true;
@@ -23,6 +32,9 @@ test("returns true when the target is in array multiple times", () => {
   expect(currentOutput).toEqual(targetOutput);
 });
 
+// Given an empty array
+// When passed to the includes function
+// Then it should return false
 test("returns false for empty array", () => {
   const currentOutput = includes([]);
   const targetOutput = false;
@@ -30,6 +42,9 @@ test("returns false for empty array", () => {
   expect(currentOutput).toEqual(targetOutput);
 });
 
+// Given an array containing null as an element
+// When passed to the includes function with null as the target
+// Then it should return true
 test("searches for null", () => {
   const currentOutput = includes(["b", "z", null, "a"], null);
   const targetOutput = true;
