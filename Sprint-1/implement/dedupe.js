77b6546ea20e47ccf4ebb3b7c9c08a4834ff0462
RomanSanaye <sanaye143@gmail.com>
2026-03-11 11:39:54 +0000

    dedupe function is developed and jest test have been added

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,11 @@
-function dedupe() {}
+function dedupe(arr) {
+  if (arr.length === 0) return [];
+  let cleanArr = [];
+  for (let item of arr) {
+    if (!cleanArr.includes(item)) {
+      cleanArr.push(item);
+    }
+  }
+  return cleanArr;
+}
+module.exports = dedupe;
