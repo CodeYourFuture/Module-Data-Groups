799078c6897862a7e9d9cb8b25739c3652cc9fd6
Karla G. <k.grajalesluna@gmail.com>
2026-03-12 21:40:01 +0000

    refactor: simplify dedupe function by removing unnecessary conditional check

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,11 +1,7 @@
 
 
 function dedupe(val) {
-    if (val.length === new Set(val).size) {
-      return val;
-    } else {
-      return [...new Set(val)];
-    }
+  return [...new Set(val)];
 }
 
 module.exports = dedupe;
