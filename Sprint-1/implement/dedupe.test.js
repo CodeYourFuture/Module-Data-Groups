7b9820087e5a0f651523a8eedd4e239dadc08763
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-03-12 19:19:12 +0000

    Refactor dedupe tests to remove todo and implement test cases for empty array and unique values

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,7 +16,6 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
 
 // Given an array with no duplicates
 // When passed to the dedupe function
@@ -25,3 +24,33 @@ test.todo("given an empty array, it returns an empty array");
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
+
+describe("dedupe", () => {
+  it("returns an empty array if passed an empty array", () =>
+    expect(dedupe([])).toEqual([]));
+
+  [
+    { input: [1, 2, 3], expected: [1, 2, 3] },
+    {
+      input: [4542543, 65756756, 433254],
+      expected: [4542543, 65756756, 433254],
+    },
+  ].forEach(({ input, expected }) =>
+    it(
+      "returns a copy of original array when passed array with no duplicates ",
+      () => expect(dedupe(input)).toEqual(expected),
+      expect(dedupe(input)).not.toBe(input)
+    )
+  );
+
+  [
+    { input: [1, 2, 3, "apple", 3, 2, {}], expected: [1, 2, 3, "apple", {}] },
+    {
+      input: [4542543, undefined, 4542543, 4542543, null, [], null],
+      expected: [4542543, undefined, null, []],
+    },
+  ].forEach(({ input, expected }) =>
+    it("returns array wit unique values in their first occurrence index when passed an array with numbers and non-number values ", () =>
+      expect(dedupe(input)).toEqual(expected))
+  );
+});
