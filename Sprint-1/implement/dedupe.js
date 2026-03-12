efb99b96e52bbe843d647bcd0837f1b2bf8747bd
Karla G. <k.grajalesluna@gmail.com>
2026-03-12 21:37:46 +0000

    feat: implement deduplication function to remove duplicate values from an array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,14 @@
-function dedupe() {}
+
+
+function dedupe(val) {
+    if (val.length === 0) return [];
+    if (val.length === new Set(val).size) {
+      return val;
+    } else {
+      return [...new Set(val)];
+    }
+}
+
+module.exports = dedupe;
+
+
