15a213d2f6bae351483f326674980711d9083a72
RomanSanaye <sanaye143@gmail.com>
2026-03-12 18:08:04 +0000

    files have been debugged in debug folder of sprint-2

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,8 +1,10 @@
 // Predict and explain first...
+// ==> this code is not correct as the ingredients act as string not an object key therefore we wont have the value.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
+// ==> to fix the issue and print each ingredients in new line we need to loop through the array inside our recipe object.
 
 const recipe = {
   title: "bruschetta",
@@ -11,5 +13,7 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:`);
+for (let fruits of recipe.ingredients) {
+  console.log(fruits);
+}
