4ee9bf8efd19993b53f06617761a81af933febf6
Richard Frimpong <richardfrimpongalx@gmail.com>
2026-03-13 21:22:28 +0000

    Implement sprint 1 array exercises

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,19 @@
+// Find the largest numerical value in an array.
+// Non-number values should be ignored.
+
 function findMax(elements) {
+  let maxValue = -Infinity;
+
+  for (const element of elements) {
+    // Only compare values that are real numbers
+    if (typeof element === "number" && !Number.isNaN(element)) {
+      if (element > maxValue) {
+        maxValue = element;
+      }
+    }
+  }
+
+  return maxValue;
 }
 
 module.exports = findMax;
