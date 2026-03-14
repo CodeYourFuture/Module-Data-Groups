00cbd0933891eb315c2be60791fb74737ea4bdd8
Arunkumar Akilan <arunkumarakilanarun@gmail.com>
2026-03-14 23:52:09 +0000

    fix: iterate through ingredients array to print each ingredient

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-
+//The title and serves values print correctly because the properties are accessed directly using 
+// recipe.title and recipe.serves.
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -10,6 +11,11 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves}`);
+for(const ingredients of recipe.ingredients)
+{
+  console.log(`*${ingredients}`)
+}
+
+//The program prints the recipe title and number of servings. 
+// Then it loops through the ingredients array and logs each ingredient on a new line so they are displayed individually instead of printing the whole object.
