4fb239658eae0acb241d572e3eb1e7c12e5b10db
mervereis <mervereis78@gmail.com>
2026-03-16 22:45:24 +0000

    sum function created & test cases created

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,12 @@
 function sum(elements) {
+  const numbers = elements.filter((el) => typeof el === "number");
+
+  if (numbers.length === 0) return 0;
+
+  if (numbers.length === 1) return numbers[0];
+
+  const sumNumbers = numbers.reduce((a, b) => a + b, 0);
+  return sumNumbers;
 }
 
 module.exports = sum;
