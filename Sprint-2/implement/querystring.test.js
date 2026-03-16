f886f54ebf2ae8947ba25257a9ed32bb19426f84
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-03-16 14:36:07 +0000

    updates to querystringtest

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -5,10 +5,12 @@
 
 const parseQueryString = require("./querystring.js")
 
-test("parses querystring values containing =", () => {
-  expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
-  });
+test("given a query string with one pair of query params, returns them in object form", function () {
+  const input = "fruit=banana";
+  const currentOutput = parseQueryString(input);
+  const targetOutput = { fruit: "banana" };
+
+  expect(currentOutput).toEqual(targetOutput);
 });
 
 test("parses querystring with multiple key-value pairs", () => {
@@ -28,5 +30,10 @@ test("parses querystring with keys but no values", () => {
 
 test ("parse querystring with empty string", () => {
   expect(parseQueryString("")).toEqual({});
+  const input=""
+const currentOutput=parseQueryString(input)
+const targetOutput={}
+expect(currentOutput).toEqual(targetOutput)   
+
 }); 
 
