7bc29b05de29534c962c5af566f7c90e0b5d2015
Liban Jama <jliban756@gmail.com>
2026-03-16 13:00:28 +0000

    completed implement

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,12 @@
-function dedupe() {}
+function dedupe(elements) {
+  const result = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (!result.includes(elements[i])) {
+      result.push(elements[i]);
+    }
+  }
+
+  return result;
+}
+
+module.exports = dedupe;
