188f02b4a5835860f0757a76fd966f10cce5523f
Elisabeth-Matulian <elisabethmatulian@gmail.com>
2026-03-16 19:36:22 +0000

    copmleted alarm clock app

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,42 @@
-function setAlarm() {}
+const timeRemaining = document.querySelector("#timeRemaining");
+
+let intervalId;
+
+
+
+function setAlarm() {
+  // I will use the function showTime twice, one for showing the time just after the user clicks the set button and another one for updating the time every second in countdown function
+  function showTime() {
+  // getting te mins and seconds from the inputValue in appropriate format
+      const minutes = Math.floor(inputValue / 60)
+        .toString()
+        .padStart(2, "0");
+      const seconds = (inputValue % 60).toString().padStart(2, "0");
+      //Gathering everything together and outputing remaining time
+      timeRemaining.innerText = `Time Remaining: ${minutes}:${seconds}`;
+};
+  // I added the next line in case if we want to reset the time for alarm
+  clearInterval(intervalId);
+  // getting the input value in number format
+  let inputValue = Number(document.querySelector("#alarmSet").value);
+  showTime();
+  intervalId = setInterval(countdown, 1000);
+
+  function countdown() {
+    if (inputValue > 0) {
+      //decrease the entered number by 1
+      inputValue--;
+      showTime();
+    }
+    // When the inputValue is 0, play the alarm and clear interval so that the function stops initialising 
+    else {
+      playAlarm();
+      clearInterval(intervalId);
+    }
+  }
+}
+
+
 
 // DO NOT EDIT BELOW HERE
 
@@ -20,6 +58,8 @@ function playAlarm() {
 
 function pauseAlarm() {
   audio.pause();
+  //I know it's said to not edit this code, I wonder if it's ok to add this line below in order to pause the time before the time runs out. 
+  clearInterval(intervalId);
 }
 
 window.onload = setup;
