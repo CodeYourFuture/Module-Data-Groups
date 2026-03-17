88e8c1bfacf5e5bfcb1837db80be8ed8a7847239
Karla G. <k.grajalesluna@gmail.com>
2026-03-17 19:26:53 +0000

    test: fix expected assertions in unit tests for sum function

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -14,40 +14,40 @@ const sum = require("./sum.js");
 // When passed to the sum function
 // Then it should return 0
 test("Should return 0 for empty array", () => {
-  expected(sum([]).toEqual(0));
+  expect(sum([])).toEqual(0);
 });
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
 test("Should return the number itself for an array with one number", () => {
-  expected(sum([5]).toEqual(5));
+  expect(sum([5])).toEqual(5);
 });
 
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
 test("Should return the correct sum even when the array contains negative numbers", () => {
-  expected(sum([-1, -2, -3])).toEqual(-6);
+  expect(sum([-1, -2, -3])).toEqual(-6);
 });
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
 test("Should return the correct sum even when the array contains decimal numbers", () => {
-  expected(sum([1.4, 1.6, 5.8])).toEqual(8.8);
+  expect(sum([1.4, 1.6, 5.8])).toEqual(8.8);
 });
 
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
 test("should return the sum of the numerical elements and ignore non-numerical values", () => {
-    expected(sum(['hey', 10, 'hi', 60, 10]).toEqual(80));
+    expect(sum(['hey', 10, 'hi', 60, 10])).toEqual(80);
 })
 
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
 test("Should return 0 for an array with only non-numbers", () => {
-    expected(sum(['hey', 'hi', 'hello']).toEqual(0));
+    expect(sum(['hey', 'hi', 'hello'])).toEqual(0);
 })
