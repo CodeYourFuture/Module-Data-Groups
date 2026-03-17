0667ae6e23cf7152f8a2c933aa6491b79aad5805
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-03-17 19:52:52 +0000

    added unit tests for contains()

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,16 +20,29 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () =>
+  expect(contains({}, "key1")).toEqual(false));
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test("contains returns true when object contains the given property", () =>
+  expect(contains({ key1: "value1", key2: "value2" }, "key1")).toEqual(true));
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("contains returns false when object does not contain the given property", () =>
+  expect(contains({ key1: "value1", key2: "value2" }, "key4")).toEqual(false));
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+it("contains returns false or throws an error if given parameter is not a valid object", () => {
+  expect(contains([], "key1")).toEqual(false);
+  expect(contains([1, 2], 1)).toEqual(false);
+  expect(contains("key1:value1", "1")).toEqual(false);
+  expect(contains(5235, "key1")).toEqual(false);
+  expect(contains(undefined, "key1")).toEqual(false);
+  expect(contains(null, "key1")).toEqual(false);
+});
