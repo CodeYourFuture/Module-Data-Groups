ae69f89201260667b1ca8c6a3599e16d693f820b
KK Tech <dyxj6n786p@privaterelay.appleid.com>
2026-03-17 22:21:37 +0000

    completed the rest of the tasks for Sprint 1

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,13 @@
-function dedupe() {}
+function dedupe(array) {
+  let seen = new Set();
+  let result = [];
+    for (let item of array) {
+        if (!seen.has(item)) {
+            seen.add(item);
+            result.push(item);
+        }
+    }
+    return result;
+}
+
+module.exports = dedupe;
