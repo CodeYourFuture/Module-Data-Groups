7ad5748a060ced4a347f702b6bc7dade7fa50d08
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-03-18 01:01:23 +0200

    Test: add test for Case 2

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,9 +1,44 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+// Case 1: Returns country currency code lookup for a single country-currency pair
+test("creates a country currency code lookup for a single code pair", () => {
+  const countryCurrencyPairs = [["US", "USD"]];
+
+  const currencyObj = createLookup(countryCurrencyPairs);
+
+  expect(currencyObj).toEqual({
+    US: "USD",
+  });
+});
+
+// Case 2: Returns country currency codes lookup for multiple country-currency pairs
+test("creates a country currency code lookup for multiple codes", () => {
+  try {
+    const countryCurrencyPairs = [
+      ["US", "USD"],
+      ["CA", "CAD"],
+      ["GB", "GBP"],
+      ["ZA", "ZAR"],
+      ["NG", "NGN"],
+    ];
+
+    const currencyObj = createLookup(countryCurrencyPairs);
+
+    expect(currencyObj).toEqual({
+      US: "USD",
+      CA: "CAD",
+      GB: "GBP",
+      ZA: "ZAR",
+      NG: "NGN",
+    });
+  } catch (error) {
+    throw new Error(
+      `Failed to create the currency lookup object for ${JSON.stringify(countryCurrencyPairs)}: ${error.message}`
+    );
+  }
+});
 
 /*
-
 Create a lookup object of key value pairs from an array of code pairs
 
 Acceptance Criteria:
