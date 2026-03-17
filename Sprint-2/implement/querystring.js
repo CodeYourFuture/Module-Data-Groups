167b175dda64f3c80613fd09cae31c8052c5cfda
mervereis <mervereis78@gmail.com>
2026-03-17 22:09:44 +0000

    queryString function created & test cases added

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,31 @@
+
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
+
+  if (!queryString || typeof queryString !== "string") {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    if (!pair) continue;
+
+    const index = pair.indexOf("=");
+
+    let key, value;
+
+    if (index === -1) {
+      key = pair;
+      value = "";
+    } else {
+      key = pair.slice(0, index);
+      value = pair.slice(index + 1);
+    }
+
+    if (key) {
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
