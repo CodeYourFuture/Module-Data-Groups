25e375418de91b0fe331a420454ffa41e737c190
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-03-17 17:15:40 +0000

    add utility to check if object contains a key

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,15 @@
-function contains() {}
+function contains(object, name) {
+  if (object == null || Object.keys(object).length === 0) {
+    return false;
+  }
+
+  for (let key in object) {
+    if (key === name) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 module.exports = contains;
