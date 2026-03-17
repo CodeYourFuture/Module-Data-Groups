6dbb61d7c902859a68000691b114f24d6f893493
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-03-17 10:53:23 +0000

    debugged recipe.js and explained error

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -4,12 +4,18 @@
 // Each ingredient should be logged on a new line
 // How can you fix it?
 
+// I predict this code will log: bruschetta serves 2 followed by ingredients: and then [object Object]. It won't show the actual list of ingredients.
+// There are two main reasons this isn't working as intended:
+//      When you put ${recipe} inside a template literal, JavaScript doesn't know how to read the whole object into text, so it defaults to the placeholder [object Object]. We must point to the specific property, recipe.ingredients, instead.
+//      In addition to this; even if you point to recipe.ingredients, it's an array. If we want each ingredient to appear on a new line to meet the requirements, we will need to use the .join("\n") method. The \n acts as a newline character.
+
 const recipe = {
   title: "bruschetta",
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
+const formattedIngredients = recipe.ingredients.join("\n");
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${formattedIngredients}`);
