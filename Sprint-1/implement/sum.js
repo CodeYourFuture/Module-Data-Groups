b490a39d20bb29c0866381f1cdcc43d239b1d611
Karla G. <k.grajalesluna@gmail.com>
2026-03-17 19:27:32 +0000

    feat: implement sum function with filtering for non-numeric values

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,10 @@
 function sum(elements) {
+
+    let initialValue = 0;
+    const onlyNumbers = elements.filter((element) => typeof element === "number");
+    let totalSum = onlyNumbers.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+    
+    return totalSum;
 }
 
 module.exports = sum;
