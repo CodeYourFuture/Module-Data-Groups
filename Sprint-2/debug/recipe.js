2af5e5273ef3588677183e7fe53bf3b809743b15
Karla G. <k.grajalesluna@gmail.com>
2026-03-18 20:51:58 +0000

    feat: add for of to print the ingredients logged on a new line

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -4,12 +4,20 @@
 // Each ingredient should be logged on a new line
 // How can you fix it?
 
+// it won't work because the last line is calling the object property recipe and we
+// need the ingredientes, the console.log is trying to log the whole array of ingredients 
+// as a string, but we want to log each ingredient on a new line. We can fix this by 
+// using a for loop to iterate over the ingredients array and log each ingredient separately.
+
 const recipe = {
-  title: "bruschetta",
+  title: "Bruschetta",
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`Recipe: ${recipe.title} \nserves: ${recipe.serves}`);
+for(const ingredient of recipe.ingredients) {
+  console.log(`${ingredient}`);
+}
+
+
