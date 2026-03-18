576cd1fabf89e26c8131d9ac95ccacef5967cf92
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-03-18 10:36:32 +0000

    implemented function for contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,11 @@
-function contains() {}
+function contains(obj, propertyName) {
+  // 1. Check if 'obj' is a valid object and NOT an array
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    return false;
+  }
+
+  // 2. Check if the propertyName exists as a key in the object
+  return propertyName in obj;
+}
 
 module.exports = contains;
