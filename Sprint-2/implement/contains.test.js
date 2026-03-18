431ad8dccf53f49b2002319dd30ec12f9621448c
Karla G. <k.grajalesluna@gmail.com>
2026-03-18 21:27:19 +0000

    test: enhance contains function tests for various scenarios

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,20 +16,35 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
+test("return true when an object has the property name", () => {
+    expect(contains({a: 1, b: 2}, "a")).toBe(true);
+})
 
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("returns false on empty object ", () => {
+    expect(contains({}, "a")).toBe(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test("return true when pass an object that contains the property name", () => {
+    expect(contains({name: "John", age: 30}, "name")).toBe(true);
+})
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("return false when pass an object that does not contain the property name", () => {
+    expect(contains({id: 12, status: "active"}, "age")).toBe(false);
+})
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("return false when pass an array instead of an object", () => {
+    expect(contains([1, 2, 3], "name")).toBe(false);
+    expect(contains(null, "prop")).toBe(false);
+})
