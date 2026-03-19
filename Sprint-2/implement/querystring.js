66308a3fd8a4c279db8c758483d5f02e5abea2a8
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-03-19 12:26:21 +0000

    implement

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -3,14 +3,32 @@ function parseQueryString(queryString) {
   if (queryString.length === 0) {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+    if (pair === "") {
+      continue;
+    }
+
+    const indexOfEquals = pair.indexOf("=");
+    let key;
+    let value;
+
+    if (indexOfEquals === -1) {
+      // no "=", treat whole pair as key with empty value
+      key = pair;
+      value = "";
+    } else {
+      key = pair.slice(0, indexOfEquals);
+      value = pair.slice(indexOfEquals + 1);
+    }
+
     queryParams[key] = value;
   }
 
   return queryParams;
 }
+
 
 module.exports = parseQueryString;
