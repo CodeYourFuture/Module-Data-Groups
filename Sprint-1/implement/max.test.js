ecc365df7b528f09650d84301a46a4435113e355
marthak1 <marthaogunbiyi196@gmail.com>
2026-03-19 18:40:34 +0000

    update findMax test cases with jest function naming

--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -16,7 +16,7 @@ const findMax = require("./max.js");
 // When passed to the max function
 // Then it should return -Infinity
 // Delete this test.todo and replace it with a test.
-it("given an empty array, returns -Infinity", () => {
+test("given an empty array, returns -Infinity", () => {
     const input = []
     const result = findMax(input)
     expect(result).toBe(-Infinity);
@@ -25,7 +25,7 @@ it("given an empty array, returns -Infinity", () => {
 // Given an array with one number
 // When passed to the max function
 // Then it should return that number
-it('given an array with one number, its should return that number', () => {
+test('given an array with one number, its should return that number', () => {
     const input = [5]
     const result = findMax(input)
     expect(result).toBe(5);
@@ -34,7 +34,7 @@ it('given an array with one number, its should return that number', () => {
 // Given an array with both positive and negative numbers
 // When passed to the max function
 // Then it should return the largest number overall
-it('given an array with both positive and negative, it should return largest overall', () => {
+test('given an array with both positive and negative, it should return largest overall', () => {
     const input = [5, -5, -10, -1, 6, 8, 10]
     const result = findMax(input)
     expect(result).toBe(10);
@@ -43,7 +43,7 @@ it('given an array with both positive and negative, it should return largest ove
 // Given an array with just negative numbers
 // When passed to the max function
 // Then it should return the closest one to zero
-it('given an array with just negative numbers, it should return the closest one to zero', () => {
+test('given an array with just negative numbers, it should return the closest one to zero', () => {
     const input = [-10, -5, -2, -8];
     const result = findMax(input);
     expect(result).toBe(-2);
@@ -52,7 +52,7 @@ it('given an array with just negative numbers, it should return the closest one 
 // Given an array with decimal numbers
 // When passed to the max function
 // Then it should return the largest decimal number
-it("given an array with just decimal numbers, it should return the largest decimal number", () => {
+test("given an array with just decimal numbers, it should return the largest decimal number", () => {
   const input = [1.0, 2.5, 2.2, 5.8];
   const result = findMax(input);
   expect(result).toBe(5.8);
@@ -61,7 +61,7 @@ it("given an array with just decimal numbers, it should return the largest decim
 // Given an array with non-number values
 // When passed to the max function
 // Then it should return the max and ignore non-numeric values
-it("given an array with non-number value, it should return the max and ignore non-numeric value", () => {
+test("given an array with non-number value, it should return the max and ignore non-numeric value", () => {
   const input = [4, 5, 'arr', 2, []," ", 15, 8];
   const result = findMax(input);
   expect(result).toBe(15);
@@ -70,7 +70,7 @@ it("given an array with non-number value, it should return the max and ignore no
 // Given an array with only non-number values
 // When passed to the max function
 // Then it should return the least surprising value given how it behaves for all other inputs
-it("given an array with only non-number value, it should return the least surprising value given how it behaves for all other inputs", () => {
+test("given an array with only non-number value, it should return the least surprising value given how it behaves for all other inputs", () => {
   const input = [true, {}, "arr", null, [], " ", NaN];
   const result = findMax(input);
   expect(result).toBe(-Infinity);
