0fbe0321c790d99d0b3498072af4f9433d3529d9
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-03-19 12:47:44 +0000

    stretch

--- Sprint-2/stretch/mode.test.js
+++ Sprint-2/stretch/mode.test.js
@@ -1,15 +1,19 @@
 const calculateMode = require("./mode.js");
 
+
 // Acceptance criteria for calculateMode function
+
 
 // Given an array of numbers
 // When calculateMode is called on the array
 // Then it should return the number that appears most frequently in the array
 
+
 // Example:
 // Given [2,4,1,2,3,2,1]
 // When calculateMode is called on [2,4,1,2,3,2,1]
 // Then it should return 2 */
+
 
 describe("calculateMode()", () => {
   test("returns the most frequent number in an array", () => {
