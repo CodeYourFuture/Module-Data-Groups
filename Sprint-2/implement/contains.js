ff8cfe0b829864df4685a8df53d5334ae1c3f0c5
Daniel Aderibigbe <danieladeribigbe07@gmail.com>
2026-03-19 10:55:19 +0000

    test update

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,5 @@
-function contains() {}
+function contains(object, property) {
+  return Object.prototype.hasOwnProperty.call(object, property);
+}
 
 module.exports = contains;
--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,9 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrency) {
+  const lookup = {};
+  for (const [country, currency] of countryCurrency) {
+    lookup[country] = currency;
+  }
+  return lookup;
 }
 
 module.exports = createLookup;
