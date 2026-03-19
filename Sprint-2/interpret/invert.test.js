827f5b3016f5ed82d3833433cffe6ad931eeadf1
Eugenie Ahangama <eugenieahangama@gmail.com>
2026-03-19 01:50:33 +0000

    Fix: Add more invalid input types to invert tests

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -26,9 +26,13 @@ test("invert on an empty object returns an empty object", () => {
   expect(invert({})).toEqual({});
 });
 
-// Given an invalid input like an array
+// Given an invalid input like an array, string, number, null or undefined
 // When passed to invert
 // Then it should throw an error
 test("invert throws an error for invalid input", () => {
   expect(() => invert([])).toThrow("Input must be an object");
+  expect(() => invert("hello")).toThrow("Input must be an object");
+  expect(() => invert(123)).toThrow("Input must be an object");
+  expect(() => invert(null)).toThrow("Input must be an object");
+  expect(() => invert(undefined)).toThrow("Input must be an object");
 });
