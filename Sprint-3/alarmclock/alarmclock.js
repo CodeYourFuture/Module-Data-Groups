a6cd0fdfffd21add0a14b3c6dce369fe91a910cd
Martin <majamc@outlook.com>
2026-03-19 09:33:42 +0000

    happy with my clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,17 +1,36 @@
-let timeIn = document.getElementById("alarmSet").value;
-let countDown = document.getElementById("timeRemaining");
+botton = document.getElementById("set");
+//begins the count down
+botton.addEventListener("click", setAlarm);
 
+// document.getElementById("set").addEventListener("click", setAlarm);
+const timeRemaining = document.getElementById("timeRemaining");
 function setAlarm() {
-  timeIn--;
-  countDown.innerHTML = "timeRemaining " + ":" + timeIn;
-  if (timeIn < 1) {
-    playAlarm;
-    clearInterval(timer);
-  }
-}
-const timer = setInterval(setAlarm, 1000);
+  // gets the value of the input field and stores it in a variable
+  let timeInSeconds = document.getElementById("alarmSet").value;
 
-// setTimeout(count, 1000);
+  let clock = setInterval(() => {
+    function pad(num) {
+      return num.toString().padStart(2, "0");
+    }
+
+    function formatTimeDisplay(timeInSeconds) {
+      const remainingSeconds = timeInSeconds % 60;
+      const totalMinutes = (timeInSeconds - remainingSeconds) / 60;
+      const remainingMinutes = totalMinutes % 60;
+      const totalHours = (totalMinutes - remainingMinutes) / 60;
+
+      return `${pad(totalHours)}:${pad(remainingMinutes)}:${pad(remainingSeconds)}`;
+    }
+
+    timeInSecond = timeInSeconds--;
+    if (timeInSeconds == 0) {
+      clearInterval(clock);
+      playAlarm();
+    }
+    timeRemaining.innerHTML =
+      "time remaining  " + formatTimeDisplay(timeInSeconds);
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
