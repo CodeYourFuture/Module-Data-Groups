55bda6049819dcb97b1338b3ce1bfaf462c28358
tasleemadedokun <adedokuntasleem@gmail.com>
2026-03-20 05:07:37 +0000

    Fix the function to obtain the object  information instead of an array.

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,8 @@
 // Predict and explain first...
 
+// The result will be undefine. The reason we are trying to get an information using an array index method for and object.
+
+
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +15,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
