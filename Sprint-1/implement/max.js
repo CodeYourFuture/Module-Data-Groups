e2214bf727ff91fcb811fa800559afd86cbfb4e7
tasleemadedokun <adedokuntasleem@gmail.com>
2026-03-20 03:12:11 +0000

    Implement function and test to max.js and max.test.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,11 @@
 function findMax(elements) {
+  if (!Array.isArray(elements)) return -Infinity;
+
+  const nums = elements.filter((n) => typeof n === "number" && !isNaN(n));
+
+  if (nums.length === 0) return -Infinity;
+
+  return nums.reduce((max, curr) => (curr > max ? curr : max), -Infinity);
 }
 
 module.exports = findMax;
