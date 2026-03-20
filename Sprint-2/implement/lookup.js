a6011d20123191641efaadadcb8f506723c09eed
tasleemadedokun <adedokuntasleem@gmail.com>
2026-03-20 16:17:20 +0000

    Effect changes to lookup.js and lookup.test.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,23 @@
-function createLookup() {
+
   // implementation here
+  function createLookup(pairs) {
+
+  if (!Array.isArray(pairs)) {
+    throw new Error('Input must be an array of pairs');
+  }
+
+  const lookup = {};
+
+  for (const pair of pairs) {
+    
+    if (!Array.isArray(pair) || pair.length !== 2) {
+      throw new Error('Each element must be an array of [countryCode, currencyCode]');
+    }
+    const [country, currency] = pair;
+    lookup[country] = currency;
+  }
+
+  return lookup;
 }
 
 module.exports = createLookup;
