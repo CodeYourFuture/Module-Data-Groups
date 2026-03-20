85ddd3dae5538a8976b5fe4847c3426152cc8a74
tasleemadedokun <adedokuntasleem@gmail.com>
2026-03-20 16:42:58 +0000

    Implement changes.

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,17 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
+  if (!queryString || queryString.length === 0) {
     return queryParams;
   }
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
+    const [key, ...rest] = pair.split("=");
+    const value = rest.join("="); // this join back the remaining parts
     queryParams[key] = value;
   }
 
   return queryParams;
 }
-
+h;
 module.exports = parseQueryString;
