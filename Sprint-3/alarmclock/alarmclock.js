f5da05e4bb8f652f1dd392bc6a4c2d8ea5ed907f
Ihor Taradaiko <igor.taradaiko@gmail.com>
2026-03-20 14:15:45 +0000

    code implemented

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,25 @@
-function setAlarm() {}
+function setAlarm() {
+  const input = document.getElementById('alarmSet');
+  const heading = document.getElementById('timeRemaining');
+  let totalSeconds = parseInt(input.value) || 0;
+  
+  function updateDisplay() {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    heading.innerText = `Time Remaining: ${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    
+    if (totalSeconds > 0) {
+      totalSeconds--;
+    } else {
+      playAlarm();
+      clearInterval(intervalId);  // Stop interval after alarm
+    }
+  }
+  
+  updateDisplay();  // Set initial time
+  const intervalId = setInterval(updateDisplay, 1000);
+}
+
 
 // DO NOT EDIT BELOW HERE
 
