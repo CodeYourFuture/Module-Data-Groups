e347daa8837aadfdf496119d4a218a3ced7b7dca
tasleemadedokun <adedokuntasleem@gmail.com>
2026-03-21 16:08:38 +0000

    Fix the code and implement the test function changes.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -8,22 +8,43 @@
 
 function invert(obj) {
   const invertedObj = {};
-
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+     invertedObj[value] = key;
   }
-
   return invertedObj;
 }
 
 // a) What is the current return value when invert is called with { a : 1 }
+console.log(invert({ a: 1 }));
+//Result: { key: 1 }
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
+console.log(invert({ a: 1, b: 2 }));
+// Result: { key: 2 }
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
 
+console.log(invert({ a: 1, b: 2 }));
+// Result should be{"1": a, "2":"b"}
+
 // c) What does Object.entries return? Why is it needed in this program?
+
+// It takes an object and returns an array of key-value pairs, where each pair is itself an array:
+const obj = { a: 1, b: 2 };
+console.log(Object.entries(obj));
+//Result:[ [ 'a', 1 ], [ 'b', 2 ] ]
 
 // d) Explain why the current return value is different from the target output
 
+// The line invertedObj.key = value; creates a "key" and assign the value to it. With variables, we need to use bracket notation and dot notation.
+
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
+
+// After fixing the code i ran node and got the following accordingly:
+//{ '1': 'a' }
+//{ '1': 'a', '2': 'b' }
+//{ '1': 'a', '2': 'b' }
+//[ [ 'a', 1 ], [ 'b', 2 ] ]
+
+
+module.exports = invert;
