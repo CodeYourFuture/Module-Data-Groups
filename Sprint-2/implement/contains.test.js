a25d8cf398605d9d3694146fb7ff5c71a4efaff5
pathywang <pathywang@gmail.com>
2026-03-22 10:24:58 +0000

    correction

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,30 +20,50 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-object={}
-console.log(object,'')
 test.todo("contains on empty object returns false");
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-object={name:'jin', age:13}
-console.log (contains(object, 'age'))
-test.todo("contains age property in object return true")
-
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
-object={name:'jin', age:13}
-console.log (contains(object, 'nice'))
-test.todo("contains no nice property  in object return false")
 
 
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
-object={name:'jin', age:13}
-console.log (contains(object, '[1,2,5]'))
-test.todo("contains no array property  in object return false")
+describe('contains function', () => {
+
+  test('returns false for an empty object', () => {
+    const object = {};
+    expect(contains({}, 'name')).toBe(false);
+  });
+
+  test('returns true when property exists', () => {
+    const object = { name: 'jin', age: 13 };
+    expect(contains(object, 'age')).toBe(true);
+  });
+
+  test('returns false when property does not exist', () => {
+    const object = { name: 'jin', age: 13 };
+    expect(contains(object, 'nice')).toBe(false);
+  });
+
+  test('returns false when input is an array', () => {
+    const object = [1, 2, 5];
+    expect(contains(object, '0')).toBe(false);
+  });
+
+  test('returns false when input is null', () => {
+    expect(contains(null, 'age')).toBe(false);
+  });
+
+  test('returns false when input is not an object', () => {
+    expect(contains(123, 'age')).toBe(false);
+    expect(contains('hello', 'length')).toBe(false);
+  });
+
+});
--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,12 +1,14 @@
 function createLookup(countryCurrencyPairs) {
-  const countryCodeCurrency={}
-  for (const[country, currency] of countryCurrencyPairs){
-   countryCodeCurrency[country]=currency
+  const countryCodeCurrency = {}
+
+  for (const[country, currency] of countryCurrencyPairs) {
+   countryCodeCurrency[country] = currency
   }
 
   return countryCodeCurrency
 }
 
-console.log(createLookup([['GB','GBP']]))
+console.log(createLookup([['GB', 'GBP']]));
+
 
 module.exports = createLookup;
