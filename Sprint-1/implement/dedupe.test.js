3d8c9020506ec1ca611c2417abfed10d7041eb67
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-22 07:34:32 +0000

    I created different test cases for dedupe empty array, arry with dublications and to return the same values if the array has no dublications.

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,13 +16,35 @@ E.g. dedupe([1, 2, 1]) returns [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+describe('dedupe', () => {
+
+  test("given an empty array, it returns an empty array", () => {
+    expect(dedupe([])).toEqual([]);
+  });
+});
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
 
+  test("given an array with no duplication, it returns a copy of the array", () => {
+    const input = [1, 2, 3];
+    const result = dedupe(input);
+
+    expect(result).toEqual([1, 2, 3]);
+    expect(result).not.toBe(input);
+  });
 // Given an array of strings or numbers
 // When passed to the dedupe function
 // Then it should return a new array with duplicates removed while preserving the 
 // first occurrence of each element from the original array.
+  test("removes duplicates and preserves first occurrence", () => {
+    expect(dedupe(['a','a','a','b','b','b','c']))
+      .toEqual(['a','b','c']);
+
+    expect(dedupe([5,1,1,2,3,2,5,8]))
+      .toEqual([5,1,2,3,8]);
+
+    expect(dedupe([1,2,1]))
+      .toEqual([1,2]);
+  });
