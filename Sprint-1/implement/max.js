5728131ff31b1fdbe1542d6a9ae8af3f9f1af828
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-22 08:00:42 +0000

    implemented a find max function.

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,6 @@
-function findMax(elements) {
+function findMax(arr) {
+    return arr.filter(item=> typeof item === 'number')
+    .reduce((acc, num) =>(num > acc ? num : acc) , -Infinity);
 }
 
 module.exports = findMax;
