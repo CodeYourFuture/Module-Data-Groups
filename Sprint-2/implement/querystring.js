233d1e53b3802da743a98b330a636a528e730569
khalidbih <kain19m@hotmail.com>
2026-03-23 23:50:57 +0000

    Fix querystring parser and add minimal tests

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,33 @@
 function parseQueryString(queryString) {
   const queryParams = {};
+
+  if (queryString.startsWith("?")) {
+    queryString = queryString.slice(1);
+  }
+
   if (queryString.length === 0) {
     return queryParams;
   }
-  const keyValuePairs = queryString.split("&");
 
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+  const pairs = queryString.split("&");
+
+  for (const pair of pairs) {
+    const index = pair.indexOf("=");
+    let key, value;
+
+    if (index === -1) {
+      key = pair;
+      value = undefined;
+    } else {
+      key = pair.slice(0, index);
+      value = pair.slice(index + 1);
+    }
+
+    if (value === undefined || value === "") {
+      queryParams[key] = undefined;
+    } else {
+      queryParams[key] = decodeURIComponent(value);
+    }
   }
 
   return queryParams;
