e48f6ac38d68aebb513d3eddabb90d725c3f9d4b
khalidbih <kain19m@hotmail.com>
2026-03-23 15:49:39 +0000

    Log recipe ingredients

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-
+// `recipe` is an object. `${recipe}` prints [object Object], not the ingredients.
+// To log ingredients, loop over `recipe.ingredients` or join them into a string.
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -11,5 +12,8 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:`);
+
+for (const ingredient of recipe.ingredients) {
+  console.log(ingredient);
+}
