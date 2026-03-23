ba1d60ca85c6dfa599b44a3548adc2a514e57ccd
Liban Jama <jliban756@gmail.com>
2026-03-23 11:03:50 +0000

    Completed alarm clock task

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,8 +1,30 @@
-function setAlarm() {}
-
-// DO NOT EDIT BELOW HERE
-
 var audio = new Audio("alarmsound.mp3");
+
+let countdown;
+
+function formatTime(seconds) {
+  let mins = Math.floor(seconds / 60);
+  let secs = seconds % 60;
+  return String(mins).padStart(2, "0") + ":" + String(secs).padStart(2, "0");
+}
+
+function setAlarm() {
+  let seconds = parseInt(document.getElementById("alarmSet").value);
+  clearInterval(countdown);
+  document.getElementById("timeRemaining").textContent =
+    "Time Remaining: " + formatTime(seconds);
+
+  countdown = setInterval(() => {
+    seconds--;
+    document.getElementById("timeRemaining").textContent =
+      "Time Remaining: " + formatTime(seconds);
+
+    if (seconds <= 0) {
+      clearInterval(countdown);
+      playAlarm();
+    }
+  }, 1000);
+}
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
@@ -19,6 +41,7 @@ function playAlarm() {
 }
 
 function pauseAlarm() {
+  clearInterval(countdown);
   audio.pause();
 }
 
