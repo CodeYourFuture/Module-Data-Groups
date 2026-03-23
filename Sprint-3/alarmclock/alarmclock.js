ec4441a04507c896f46f1ed95f4d0ebd3f44ad79
Fayaz <fayaz.sadat551@yahoo.com>
2026-03-23 22:11:01 +0000

    implemented the code to make the alarmclock work properly with test cases

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,31 @@
-function setAlarm() {}
+function setAlarm() {
+  const time = document.getElementById("alarmSet").value;
+  const heading = document.getElementById("timeRemaining");
+
+  let remainingTime = Number(time);
+
+  const timer = setInterval(() => {
+    if (remainingTime <= 0) {
+      clearInterval(timer);
+      heading.textContent = "Time Remaining: 00:00";
+      playAlarm();
+      return;
+    }
+
+    remainingTime--;
+
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+
+    heading.textContent = `Time Remaining: ${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  }, 1000);
+
+  
+  const minutes = Math.floor(remainingTime / 60);
+  const seconds = remainingTime % 60;
+
+  heading.textContent = `Time Remaining: ${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
