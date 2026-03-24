66aa3ba223a978221c7e3f67d25bb3be40e51889
khalidbih <kain19m@hotmail.com>
2026-03-24 23:23:56 +0000

    Add tally function and tests for empty, duplicates, and invalid input

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,15 @@
-function tally() {}
+function tally(array) {
+  if (!Array.isArray(array)) {
+    throw new Error("Input must be an array");
+  }
+
+  const counts = {};
+
+  for (const item of array) {
+    counts[item] = (counts[item] || 0) + 1;
+  }
+
+  return counts;
+}
 
 module.exports = tally;
