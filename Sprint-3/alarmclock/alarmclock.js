8cd966d2c69694f71300df0dc4deec436340b648
Angela McLeary <angela.mcleary@outlook.com>
2026-03-24 19:26:25 +0000

    Implement alarmclock.js

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,35 @@
-function setAlarm() {}
+let alarmInterval;
+
+function setAlarm() {
+  clearInterval(alarmInterval);
+  let timeRemaining = document.getElementById("alarmSet");
+  const display = document.getElementById("timeRemaining");
+  //what ever we input is turned into seconds
+  let totalSeconds = parseInt(timeRemaining.value);
+
+  if (isNaN(totalSeconds) || totalSeconds <= 0) return;
+
+  const formattedTime = (seconds) => {
+    const mins = Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, "0");
+    const secs = (seconds % 60).toString().padStart(2, "0");
+    return `${mins}:${secs}`;
+  };
+
+  display.innerHTML = `Time Remaining: ${formattedTime(totalSeconds)}`;
+
+  alarmInterval = setInterval(() => {
+    totalSeconds--;
+
+    display.innerHTML = `Time Remaining: ${formattedTime(totalSeconds)}`;
+
+    if (totalSeconds <= 0) {
+      clearInterval(alarmInterval);
+      playAlarm();
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
