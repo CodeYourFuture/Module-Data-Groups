3698abb9e259704fc82988e81e0b4f7884d92c9c
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-03-24 14:43:16 +0000

    updated contains.js replacing Object.prototype.hasOwnProperty() with just Object.hasOwnProperty() to simplify code

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -3,7 +3,7 @@ function contains(obj, prop) {
    !!obj && // confirm that obj is not null or undefined. If undefined or null, return false.
    typeof obj === "object" && // confirm that obj is an object. If not an object, return false.
    !Array.isArray(obj) && // confirm that obj is not an array. If an array, return false.
-   Object.prototype.hasOwnProperty.call(obj, prop)// check if the object has the property. If it does, return true; otherwise, return false.
+   Object.hasOwnProperty.call(obj, prop)// check if the object has the property. If it does, return true; otherwise, return false.
  );
 }
 
