2fe30d99292e595a9be4886dd5032933c521836a
Zadri Abdule <zadriabdule@gmail.com>
2026-03-24 21:03:57 +0000

    fix(address): show houseNumber

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,5 +1,7 @@
 // Predict and explain first...
-
+// With the code as written, running the file will print: My house number is undefined
+// Why is this happening?
+// Because the address object does not have a property with the key 0 - which is what address[0] is trying to access. The houseNumber is a property of the address object, so it should be accessed using dot notation (address.houseNumber) instead of bracket notation with an index (address[0]).
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
@@ -12,4 +14,11 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
+// debug info
+console.log("address object:", address);
+console.log(
+  "address.houseNumber (type):",
+  typeof address.houseNumber,
+  address.houseNumber
+);
