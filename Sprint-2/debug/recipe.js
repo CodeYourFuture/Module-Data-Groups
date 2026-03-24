7adcbd89147be807ccaebf48a43f8032ec4fcbea
Damian_Dunkley <damian.dunkley@btinternet.com>
2026-03-24 14:34:49 +0000

    updated recipe.js replacing for loop with .join to simplify code

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -9,9 +9,14 @@ const recipe = {
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
-
+/* Replacing the for loop with Array.prototype.join() to log the ingredients in a single line, separated by new lines. 
 console.log(`${recipe.title} serves ${recipe.serves}`);
 for (const ingredient of recipe.ingredients) {
   console.log(ingredient);
 }
+*/
+console.log(`${recipe.title} serves ${recipe.serves}`);
+console.log (recipe.ingredients.join("\n"));
+
+
 // The problem is that the code is trying to log out the ingredients using a for...of loop, but it is not using the correct syntax to access the ingredients. The recipe.ingredients is an array, so we need to use a for...of loop to iterate over that array and log each ingredient.
