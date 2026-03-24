f51817c013dd33eb860950ab6e44e51ab4607c33
Fida Ali Zada <fidahussainalizada@gmail.com>
2026-03-24 11:44:51 +0000

    Nodejs File System (fs) issue solved

--- Sprint-1/stretch/aoc-2018-day1/solution.js
+++ Sprint-1/stretch/aoc-2018-day1/solution.js
@@ -1,3 +1,12 @@
-function sum(list) {
-    return list.reduce((a, b) => a + b, 0);
-}
+const fs = require('fs');
+
+const data = fs.readFileSync('input.txt', 'utf-8');
+
+// split by spaces or new lines
+// filter out empty values
+// convert strings into numbers
+const numbers = data.split(/\s+/).filter(Boolean).map(Number);
+
+const sum = numbers.reduce((a, b) => a + b, 0);
+
+console.log(sum); // 529
