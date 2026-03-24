a8cdaadc20adabe1e98980861c1e4a3ad863c1fc
Zadri Abdule <zadriabdule@gmail.com>
2026-03-24 21:21:47 +0000

    fix(author): iterate object values

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+// Running the file will result in a TypeError: author is not iterable. This is because the for...of loop is designed to work with iterable objects (like arrays, strings, etc.), but 'author' is a plain JavaScript object, which is not iterable by default. To iterate over the properties of an object, we can use a for...in loop or Object.values() method instead.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
@@ -11,6 +12,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
