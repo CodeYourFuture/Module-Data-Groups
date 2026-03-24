b63ad42c73449a906c84ed705476cd6be55507a9
Zadri Abdule <zadriabdule@gmail.com>
2026-03-24 17:56:42 +0000

    refactor(includes): use for...of; add tests and sum implementation

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,13 @@
 function sum(elements) {
+  if (!Array.isArray(elements)) return 0;
+
+  let total = 0;
+  for (const el of elements) {
+    if (typeof el === "number") {
+      total += el;
+    }
+  }
+  return total;
 }
 
 module.exports = sum;
