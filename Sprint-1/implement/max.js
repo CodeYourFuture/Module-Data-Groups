83a0c0d789bb68882aedbec0f7769c99fb23eb8b
Mona-Eltantawy <karamrafat@gmail.com>
2026-03-25 21:54:12 +0000

    I fixed the function to return the expected values for vlaues like NaN

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,7 +1,8 @@
 function findMax(arr) {
   return arr
-    .filter((item) => typeof item === "number")
-    .reduce((acc, num) => (num > acc ? num : acc), -Infinity);
+    .filter((item) => typeof item === "number" && Number.isFinite(item));
+    if(numbers.length=== 0) return null; 
+   return numbers.reduce((acc, num) => (num > acc ? num : acc), -Infinity);
 }
 
 module.exports = findMax;
