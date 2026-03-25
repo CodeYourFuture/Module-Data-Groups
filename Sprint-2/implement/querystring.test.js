51b68ebe41d58907fea74409a32232f8d72c2d88
Maryanne Mosonik <chmosonik@gmail.com>
2026-03-26 01:34:57 +0300

    Added edge test cases

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -10,3 +10,22 @@ test("parses querystring values containing =", () => {
     "equation": "x=y+1",
   });
 });
+test("parses multiple parameters", () => {
+  expect(parseQueryString("a=1&b=2")).toEqual({ a: "1", b: "2" });
+});
+test("parses empty querystring", () => {
+  expect(parseQueryString("")).toEqual({});
+});
+test("parses parameter without value", () => {
+  expect(parseQueryString("flag")).toEqual({ flag: "" });
+});
+test("parses parameter with empty value", () => {
+  expect(parseQueryString("empty=")).toEqual({ empty: "" });
+}); 
+test("parses parameter with multiple =", () => {
+  expect(parseQueryString("data=a=b=c")).toEqual({ data: "a=b=c" });
+});
+test("parses parameter with special characters", () => {
+  expect(parseQueryString("name=John%20Doe&age=30")).toEqual({ name: "John%20Doe", age: "30" });
+});
+
