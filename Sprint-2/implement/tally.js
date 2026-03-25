20f1d05d41869c5320c3c9d42ba8773fbf5431ac
Maryanne Mosonik <chmosonik@gmail.com>
2026-03-26 01:53:27 +0300

    Completed tally implementation

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,16 @@
-function tally() {}
+function tally(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error("Input must be an array");
+    }
+    const counts = {};
+    for (const item of arr) {
+        if (counts[item]) {
+            counts[item] += 1;
+        } else {
+            counts[item] = 1;
+        }
+    }
+    return counts;
+}
 
 module.exports = tally;
