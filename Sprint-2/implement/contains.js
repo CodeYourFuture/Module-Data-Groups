066d615fb4c7cc6835bb738e51333d8d19430d4e
Hayriye Saricicek <mshayriyesaricicek@gmail.com>
2026-03-25 13:05:05 +0000

    wrote function and tests for contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(obj, key) {
+  if (typeof obj !== "object" || Array.isArray(obj)) {
+    throw new Error("Input must not be an array");
+  }
+  return key in obj;
+}
 
 module.exports = contains;
