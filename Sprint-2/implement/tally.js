49f0e67abc12f1570152a820a34d261c563d01eb
Angela McLeary <angela.mcleary@outlook.com>
2026-03-25 17:31:34 +0000

    add test to tally.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,8 +1,11 @@
 function tally(list) {
   var counts = {};
+  if (!Array.isArray(list)) {
+    throw new Error("Input must be an array");
+  }
 
   for (var item of list) {
-    if (counts[item]){
+    if (counts[item]) {
       counts[item] += 1;
     } else {
       counts[item] = 1;
@@ -12,5 +15,5 @@ function tally(list) {
   return counts;
 }
 
-console.log(tally(['a', 'a', 'c', 'a', 'd']))
+console.log(tally(["a", "a", "c", "a", "d"]));
 module.exports = tally;
