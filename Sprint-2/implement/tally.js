77f4b74d1f7e0d9af2ec419766975f4cd4bc1df6
Zadri Abdule <zadriabdule@gmail.com>
2026-03-25 12:08:37 +0000

    feat(tally): add implementation

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,9 @@
-function tally() {}
+function tally(items) {
+  if (!Array.isArray(items)) throw new Error("Invalid input");
+  return items.reduce((acc, it) => {
+    acc[it] = (acc[it] || 0) + 1;
+    return acc;
+  }, {});
+}
 
 module.exports = tally;
