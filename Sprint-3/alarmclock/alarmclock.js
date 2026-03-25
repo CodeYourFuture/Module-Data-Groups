fb904b2d3aab4b90fa17b0a7d972ebe0cc203dd5
Arunkumar Akilan <arunkumarakilanarun@gmail.com>
2026-03-25 09:19:28 +0000

    Implement alarm clock countdown logic with setInterval and update UI formatting

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,32 @@
-function setAlarm() {}
+function setAlarm() {
+  const input = document.querySelector("#alarmSet");
+  let seconds = input.value%60;
+  let minutes = (input.value - seconds)/60;
+  const timeRemaining = document.querySelector("#timeRemaining");
+  timeRemaining.textContent = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+   givenTime = Number(input.value);
+    clearInterval(timerId);
+    timerId =setInterval(()=>
+   {
+    givenTime = givenTime - 1;
+    updateTime();
+     if (givenTime === 0) {
+       clearInterval(timerId);
+       playAlarm();
+     } 
+  }, 1000);
+
+}
+  let timerId;
+ let givenTime;
+
+function updateTime(){
+  let seconds = givenTime%60;
+  let minutes = (givenTime - seconds)/60; 
+  const remainingTime = document.querySelector("#timeRemaining");
+  remainingTime.textContent = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`; 
+}
+
 
 // DO NOT EDIT BELOW HERE
 
