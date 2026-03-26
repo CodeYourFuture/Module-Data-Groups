3c7d55443f3d799ec8123fa43262c4b23c8d1544
Pretty Taruvinga <pretty548@gmail.com>
2026-03-26 22:36:22 +0200

    Implement tally function with error handling and tests"

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,19 @@
-function tally() {}
+function tally(items) {
+  if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
+
+  const result = {};
+
+  items.forEach((item) => {
+    if (result[item]) {
+      result[item]++;
+    } else {
+      result[item] = 1;
+    }
+  });
+
+  return result;
+}
 
 module.exports = tally;
