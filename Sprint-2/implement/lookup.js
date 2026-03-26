067f3e1eaae63456d6223b7b9b85881a620feb16
Pretty Taruvinga <pretty548@gmail.com>
2026-03-26 16:02:05 +0200

    Implement createLookup function for country-currency pairs

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,12 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+  const result = {};
+  for (let i = 0; i < pairs.length; i++) {
+    const country = pairs[i][0];
+    const currency = pairs[i][1];
+
+    result[country] = currency;
+  }
+  return result;
 }
 
 module.exports = createLookup;
