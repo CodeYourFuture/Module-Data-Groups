caa05c07f72c4c8f64378cb3bddb5e5469e07996
Carlos Abreu <carlos.abreu@zoho.com>
2026-03-26 15:28:03 +0000

    Committed sum.js and sum.test.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,9 @@
 function sum(elements) {
+    // Filter out non-numeric values
+    const numbers = elements.filter(element => typeof element === 'number');
+
+    // Sum all the numbers using reduce
+    return numbers.reduce((total, current) => total + current, 0);
 }
 
 module.exports = sum;
