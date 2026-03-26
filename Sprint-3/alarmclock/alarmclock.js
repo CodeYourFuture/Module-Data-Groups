ac9e9324e8fdde13fe78e5e85a0388f5034ccb46
Elisabeth-Matulian <elisabethmatulian@gmail.com>
2026-03-26 21:50:00 +0000

    Fix countdown display and create var to avoid bloating

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,44 +1,48 @@
 const timeRemaining = document.querySelector("#timeRemaining");
 
-let intervalId; //declared here so that I can use it in line-17, before asigning on line 21. 
+let intervalId; //declared here so that I can use it in line-17, before asigning on line 21.
 
 function setAlarm() {
+  const alarmSetInput = document.querySelector("#alarmSet");
+
   // I will use the function showTime twice, one for showing the time just after the user clicks the set button and another one for updating the time every second in countdown function
   function showTime(inputValue) {
-    if (inputValue >= 0) {
-  // getting the mins & sec from the inputValue(line19) in appropriate format
-      const minutes = Math.floor(inputValue / 60)
-        .toString()
-        .padStart(2, "0");
-      const seconds = (inputValue % 60).toString().padStart(2, "0");
-      //Gathering everything together and outputing remaining time
-      timeRemaining.innerText = `Time Remaining: ${minutes}:${seconds}`;
-}}
+    // getting the mins & sec from the inputValue(line19) in appropriate format
+    const minutes = Math.floor(inputValue / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (inputValue % 60).toString().padStart(2, "0");
+    //Gathering everything together and outputing remaining time
+    timeRemaining.innerText = `Time Remaining: ${minutes}:${seconds}`;
+  }
   //the next line in case if we want to reset the time for alarm
   clearInterval(intervalId);
   // getting user's input value in number format
-  let inputValue = Number(document.querySelector("#alarmSet").value);
+  let inputValue = Number(alarmSetInput.value);
   if (inputValue <= 0) {
     timeRemaining.innerText = "Enter a positive number.";
-    document.querySelector("#alarmSet").value = '';
+    alarmSetInput.value = "";
     return;
   }
   showTime(inputValue);
   intervalId = setInterval(countdown, 1000);
 
   function countdown() {
-    if (inputValue >= 0) {
+    if (inputValue > 0) {
       inputValue--;
       showTime(inputValue);
     }
-    // When the inputValue is 0, play the alarm and clear interval so that the function stops initialising 
+    // When the inputValue is 0, play the alarm and clear interval so that the function stops initialising
     else {
       playAlarm();
       clearInterval(intervalId);
-      document.querySelector("#alarmSet").value = '';
+      alarmSetInput.value = "";
     }
   }
 }
+// document.getElementById("stop").addEventListener("click", () => {
+//     clearInterval(intervalId);
+//   });
 
 // DO NOT EDIT BELOW HERE
 
@@ -60,8 +64,6 @@ function playAlarm() {
 
 function pauseAlarm() {
   audio.pause();
-  //I know it's said to not edit this code, I wonder if it's ok to add this line below in order to pause the time before the time runs out. 
-  clearInterval(intervalId);
 }
 
 window.onload = setup;
