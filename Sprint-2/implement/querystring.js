4c7f534ce820f7fc9bc497892d07b64393be8de6
Ales-Os-Dev_Lab <alex.o.okorefe@gmail.com>
2026-03-26 09:34:35 +0000

    refactor parseQueryString to improve handling of query strings and ensure proper decoding of keys and values

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,44 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
+  if (typeof queryString !== "string") {
     return queryParams;
   }
+
+  if (queryString.startsWith("?")) {
+    queryString = queryString.substring(1);
+  }
+
+  if (queryString.trim() === "") {
+    return queryParams;
+  }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    if (!pair) continue;
+
+    const equalIndex = pair.indexOf("=");
+
+    let key = pair;
+    let value = "";
+
+    if (equalIndex !== -1) {
+      key = pair.substring(0, equalIndex);
+      value = pair.substring(equalIndex + 1);
+    }
+
+    key = decodeURIComponent(key);
+    value = decodeURIComponent(value);
+
+    if (queryParams.hasOwnProperty(key)) {
+      if (Array.isArray(queryParams[key])) {
+        queryParams[key].push(value);
+      } else {
+        queryParams[key] = [queryParams[key], value];
+      }
+    } else {
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
