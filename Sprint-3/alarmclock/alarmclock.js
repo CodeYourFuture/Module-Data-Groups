f6e14548bad4c0c69053ef9d47fc140cd43c7cbb
abduhasen <abdumussa1363@gmail.com>
2026-03-27 22:07:49 +0000

    wrote a code to implement alarmclock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,18 @@
-function setAlarm() {}
+function setAlarm() {
+  const userInput = document.querySelector("#alarmSet").value;
+  let timeRemaining = userInput;
+
+  const myInterval = setInterval(function () {
+    timeRemaining -= 1;
+    const charactersLeftP = document.querySelector("#timeRemaining");
+    charactersLeftP.innerText = `Time Remaining ${timeRemaining}`;
+
+    if (timeRemaining === 0) {
+      playAlarm();
+      clearInterval(myInterval);
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
