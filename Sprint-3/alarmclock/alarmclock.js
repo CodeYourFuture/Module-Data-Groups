f5b39bd891741390e47d39cf90b6991dce8c85bf
dcostaprakash <starfinhub@gmail.com>
2026-03-27 12:21:41 +0000

    GLASGOW | Jan-26-ITP | Prakash Dcosta | Sprint 3 | Quote Generator

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,36 @@
-function setAlarm() {}
+let timeRemaining;
+let timerId;
+function setAlarm() {
+  const input = document.getElementById("alarmSet").value;
+  timeRemaining = Number(input);
+
+  clearInterval(timerId);
+
+  updateDisplay();
+
+  timerId = setInterval(() => {
+    timeRemaining--;
+    updateDisplay();
+
+    if (timeRemaining <= 0) {
+      timeRemaining = 0;
+      updateDisplay();
+      clearInterval(timerId);
+      playAlarm();
+    }
+  }, 1000);
+}
+function updateDisplay() {
+  const display = document.getElementById("timeRemaining");
+
+  const minutes = Math.floor(timeRemaining / 60);
+  const seconds = timeRemaining % 60;
+
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(seconds).padStart(2, "0");
+
+  display.textContent = `Time Remaining: ${formattedMinutes}:${formattedSeconds}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
