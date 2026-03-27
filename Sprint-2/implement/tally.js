458dd9415f8dd9c55d75def6ff707b53247dd617
Asha Ahmed <asha.m.ahmed252@gmail.com>
2026-03-27 22:18:51 +0000

    Added tally function to count occurrences of items in an array

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,19 @@
-function tally() {}
+function tally(itemsList) {
+  const counts = {};
+
+  if (itemsList.length === 0) {
+    return {};
+  }
+
+  if (!Array.isArray(itemsList)) {
+    throw new Error("Input must be an array");
+  }
+
+  for (const item of itemsList) {
+    counts[item] = (counts[item] || 0) + 1;
+  }
+
+  return counts;
+}
 
 module.exports = tally;
