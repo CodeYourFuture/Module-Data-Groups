59c72d3d13e52c96ff930894928ba05b8466a815
mervereis <mervereis78@gmail.com>
2026-03-27 12:53:26 +0000

    use Object.hasOwn instead of in

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,21 +1,9 @@
 function contains(obj, prop) {
-  if (obj === null) {
+  if (obj === null || typeof obj !== "object" || Array.isArray(obj)) {
     return false;
   }
 
-  if (typeof obj !== "object") {
-    return false;
-  }
-
-  if (Array.isArray(obj)) {
-    return false;
-  }
-
-  if (prop in obj) {
-    return true;
-  } else {
-    return false;
-  }
+  return Object.hasOwn(obj, prop);
 }
 
 module.exports = contains;
