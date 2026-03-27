d0ec9a3d2960bd22772b73553e81eb9a732a76ab
Pretty Taruvinga <pretty548@gmail.com>
2026-03-27 23:32:12 +0200

    Return -Infinity for empty array to match spec

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,21 +1,12 @@
-function findMax(elements) {
-  if (!Array.isArray(elements) || elements.length === 0) {
-    return null;
-  }
-
+function max(numbers) {
   let max = -Infinity;
 
-  for (let i = 0; i < elements.length; i++) {
-    if (typeof elements[i] === "number" && !isNaN(elements[i])) {
-      if (elements[i] > max) {
-        max = elements[i];
-      }
+  for (let num of numbers) {
+    if (num > max) {
+      max = num;
     }
-  }
-  if (max === -Infinity) {
-    return null;
   }
   return max;
 }
 
-module.exports = findMax;
+module.exports = max;
