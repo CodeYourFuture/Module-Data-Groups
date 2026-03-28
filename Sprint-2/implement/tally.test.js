eb1dee9a79991f4a3fe5eddfd4342a51b5a7e52d
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-03-28 19:34:49 +0200

    Test: array without duplicates

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -1,8 +1,23 @@
 const tally = require("./tally.js");
 
-describe("tally", () => {
+describe("tally()", () => {
   // Case 1: Returns an empty object when an empty array is passed to tally
   test("tally on an empty array returns an empty object", () => {
     expect(tally([])).toEqual({});
+  });
+
+  // Case 2: Returns counts for each unique item when given an array without duplicate items
+  test("tally on an array without duplicate items returns counts for each unique item", () => {
+    const tallyWithUniqueItems = [
+      [["a"], { a: 1 }],
+      [["a", "b", "c"], { a: 1, b: 1, c: 1 }],
+      [[1, 2, 3], { 1: 1, 2: 1, 3: 1 }],
+      [[true, false], { true: 1, false: 1 }],
+      [[null, undefined], { null: 1, undefined: 1 }],
+    ];
+
+    tallyWithUniqueItems.forEach(([inputArray, targetOutput]) => {
+      expect(tally(inputArray)).toEqual(targetOutput);
+    });
   });
 });
