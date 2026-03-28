50e80fda0e74eb3328ffa584a461214128414066
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-03-28 20:30:29 +0200

    Test: strings with multiple key-value pairs

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -25,4 +25,61 @@ describe("parseQueryString", () => {
       equation: "x=y+1",
     });
   });
+
+  // Case 4: String contains multiple key-value pairs separated by '&' with one '=' character each
+  test("parses multiple key-value pairs", () => {
+    const multiplePairs = [
+      ["name=John&age=30", { name: "John", age: "30" }],
+      ["city=New York&country=USA", { city: "New York", country: "USA" }],
+      [
+        "key1=value1&key2=value2&key3=value3",
+        { key1: "value1", key2: "value2", key3: "value3" },
+      ],
+      [
+        "Nicaragua=Managua&Honduras=Tegucigalpa&El Salvador=San Salvador&Costa Rica=San José",
+        {
+          Nicaragua: "Managua",
+          Honduras: "Tegucigalpa",
+          "El Salvador": "San Salvador",
+          "Costa Rica": "San José",
+        },
+      ],
+    ];
+
+    multiplePairs.forEach(([input, targetOutput]) => {
+      expect(parseQueryString(input)).toEqual(targetOutput);
+    });
+  });
+
+  // Case 5: String contains multiple key-value pairs separated by '&' with multiple '=' character each
+  test("parses multiple key-value pairs with multiple =", () => {
+    const multiplePairsWithEquals = [
+      [
+        "equation1=x=y+1&equation2=a=b+c",
+        {
+          equation1: "x=y+1",
+        },
+      ],
+      [
+        "equation2=a!=b+c&equation3=p=q*r",
+        {
+          equation2: "a!=b+c",
+          equation3: "p=q*r",
+        },
+      ],
+      [
+        "equation3=p!=q*r&equation4=m=n+o&equation5=x=y/1",
+        {
+          equation3: "p!=q*r",
+          equation4: "m=n+o",
+          equation5: "x=y/1",
+        },
+      ],
+    ];
+
+    expect(parseQueryString("equation1=x=y+1&equation2=a=b+c")).toEqual({
+      equation1: "x=y+1",
+      equation2: "a=b+c",
+    });
+  });
 });
