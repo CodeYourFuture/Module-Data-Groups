340015c9e46dae6e101431b3cf4a70aaec7da816
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-03-28 19:37:27 +0200

    Implementation of tally function

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,19 @@
-function tally() {}
+function tally(array) {
+  if (!Array.isArray(array)) {
+    throw new Error("Input must be an array");
+  }
+
+  const counts = {};
+
+  for (const prop of array) {
+    if (Object.hasOwn(tally, prop)) {
+      counts[prop] += 1;
+    } else {
+      counts[prop] = 1;
+    }
+  }
+
+  return counts;
+}
 
 module.exports = tally;
