2872c2fc0c27fd40a9cf9c658831f138b2f42052
ofonimeedak <ofonimeedak@gmail.com>
2026-03-28 15:03:59 +0000

    remove dead codes

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,6 +1,5 @@
 function findMax(elements) {
   if (!Array.isArray(elements)) return "invalid elements";
-  if (elements.every((e) => typeof e === "string")) return -Infinity;
   const number = [];
   for (let i = 0; i < elements.length; i++) {
     if (typeof elements[i] === "number" && !Number.isNaN(elements[i])) {
