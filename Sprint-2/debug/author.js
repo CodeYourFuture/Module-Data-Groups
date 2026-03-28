de0570daa6030baf04be4b3dfb6fe5e1e05b2f23
XiaoQuark <kris.oldrini@gmail.com>
2026-03-28 08:25:54 +0000

    coursework: complete debug exercises

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,9 @@
 // Predict and explain first...
 
+// for.. of loops are used with Arrays, not Objects.
+// I'm expecting an error
+// For Objects, use for.. in loops
+
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +15,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value in author) {
   console.log(value);
 }
