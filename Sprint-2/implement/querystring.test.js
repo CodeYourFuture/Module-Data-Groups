21df2c264fdd66b94b33d9eb7bb2bba1c753990e
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-03-28 20:06:14 +0200

    Refactor: test for additional cases

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -1,12 +1,28 @@
-// In the prep, we implemented a function to parse query strings.
-// Unfortunately, it contains several bugs!
-// Below is one test case for an edge case the implementation doesn't handle well.
-// Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
+const parseQueryString = require("./querystring.js");
 
-const parseQueryString = require("./querystring.js")
+describe("parseQueryString", () => {
+  // Case 1: Returns an empty object when given an empty query string
+  test("parses an empty querystring", () => {
+    expect(parseQueryString("")).toEqual({});
+  });
 
-test("parses querystring values containing =", () => {
-  expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+  // Case 2: String contains one key-value pair and 1 '=' character
+  test("parses querystring values containing =", () => {
+    const singlePair = [
+      ["name=John", { name: "John" }],
+      ["age=30", { age: "30" }],
+      ["city=New York", { city: "New York" }],
+    ];
+
+    singlePair.forEach(([input, targetOutput]) => {
+      expect(parseQueryString(input)).toEqual(targetOutput);
+    });
+  });
+
+  // Case 3: String contains one key-value pair and multiple '=' character
+  test("parses querystring values containing multiple =", () => {
+    expect(parseQueryString("equation=x=y+1")).toEqual({
+      equation: "x=y+1",
+    });
   });
 });
