c547fdcb32a5c2757ebe7227374f51565219c6fc
Pretty Taruvinga <pretty548@gmail.com>
2026-03-28 22:14:00 +0200

    Separate invert tests from implementation into invert.test.js

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -27,27 +27,3 @@ function invert(obj) {
 // d) Explain why the current return value is different from the target output
 
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
-const invert = require("./invert");
-
-test("inverts a simple object", () => {
-  expect(invert({ a: 1 })).toEqual({ 1: "a" });
-});
-
-test("inverts an object with multiple key-value pairs", () => {
-  expect(invert({ a: 1, b: 2 })).toEqual({
-    1: "a",
-    2: "b",
-  });
-});
-
-test("returns empty object when given an empty object", () => {
-  expect(invert({})).toEqual({});
-});
-
-test("handles non-string values as keys in the inverted object", () => {
-  expect(invert({ a: 1, b: true, c: null })).toEqual({
-    1: "a",
-    true: "b",
-    null: "c",
-  });
-});
