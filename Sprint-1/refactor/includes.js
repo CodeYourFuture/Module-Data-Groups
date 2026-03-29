78292e842cff5b4e4dff6dcfdea47a0b2fcf2e18
marthak1 <marthaogunbiyi196@gmail.com>
2026-03-29 22:02:16 +0100

    removed unused comment for cleaner code practice

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -11,9 +11,4 @@ function includes(list, target) {
 
 module.exports = includes;
 
-// const array = ["a", "b", "c"];
-
-// for (const element of array) {
-//   console.log(element);
-// } for (variable of iterable) statement
 
