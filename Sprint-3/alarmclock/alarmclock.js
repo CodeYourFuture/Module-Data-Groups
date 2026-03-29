f9ffdd6fbc0a76d0b6840d8bfead35f0019e2597
khalidbih <kain19m@hotmail.com>
2026-03-30 00:27:57 +0100

    Add working alarm clock with countdown and sound

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,34 @@
-function setAlarm() {}
+function setAlarm() {
+  let timer;
+
+  const input = document.getElementById("alarmSet").value;
+  let timeLeft = Number(input);
+
+  updateDisplay(timeLeft);
+
+  clearInterval(timer);
+
+  timer = setInterval(() => {
+    timeLeft--;
+
+    updateDisplay(timeLeft);
+
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      playAlarm();
+      document.body.style.backgroundColor = "red";
+    }
+  }, 1000);
+}
+
+function updateDisplay(secondsLeft) {
+  const heading = document.getElementById("timeRemaining");
+
+  const minutes = Math.floor(secondsLeft / 60);
+  const seconds = secondsLeft % 60;
+
+  heading.innerText = `Time Remaining: ${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
