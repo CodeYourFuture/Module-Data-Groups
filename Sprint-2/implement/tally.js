a874ffd1afddec75b745891de09330ecc1145aa1
Zadri <zadriabdule@gmail.com>
2026-03-29 21:57:53 +0100

    Refactor tally function for item counting
    Refactor tally function to use a for-of loop instead of reduce for counting items.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,9 +1,17 @@
 function tally(items) {
   if (!Array.isArray(items)) throw new Error("Invalid input");
-  return items.reduce((acc, it) => {
-    acc[it] = (acc[it] || 0) + 1;
-    return acc;
-  }, {});
+  const counts = Object.create(null);
+    for (const it of items) {
+      counts[it] = (counts[it] || 0) + 1;
+  }
+  return counts;
 }
+
+console.log(tally(['a', 'a', 'b']));
+// -> { a: 2, b: 1 }
+
+console.log(tally(['toString', 'toString']));
+// -> { toString: 2 }
+
 
 module.exports = tally;
