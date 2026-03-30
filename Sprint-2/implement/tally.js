437c7adefe4329bfca52b3c6ec4ef8c62a8c9def
Maryanne Mosonik <chmosonik@gmail.com>
2026-03-30 23:36:46 +0300

    Refactor tally function to use Object.create for counts initialisation

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -2,12 +2,12 @@ function tally(arr) {
     if (!Array.isArray(arr)) {
         throw new Error("Input must be an array");
     }
-    const counts = {};
+    const counts = Object.create(null);
     for (const item of arr) {
-        if (counts[item]) {
-            counts[item] += 1;
-        } else {
+        if (counts[item] === undefined) {
             counts[item] = 1;
+        } else {
+            counts[item] = counts[item] + 1;
         }
     }
     return counts;
