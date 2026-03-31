71fdf6e3aa8070bd7f5c4731089373c0e63895ca
ofonimeedak <ofonimeedak@gmail.com>
2026-04-01 00:21:20 +0100

    write alarm clock function

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,47 @@
-function setAlarm() {}
+const displayRemainingTime = document.querySelector("#timeRemaining");
+
+function setAlarm(alarmTime) {
+  if (!alarmTime) {
+    displayRemainingTime.textContent = "Please select a time";
+    return;
+  }
+  const [h, m, s = 0] = alarmTime.split(":").map(Number);
+  const totalSeconds = h * 3600 + m * 60 + s;
+
+  const interval = setInterval(() => {
+    const now = new Date();
+    let currentTimeInSec =
+      now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+
+    let remainingTime = totalSeconds - currentTimeInSec;
+
+    const minutes = Math.floor(remainingTime / 60)
+      .toString()
+      .padStart(2, "0");
+
+    const seconds = (remainingTime % 60).toString().padStart(2, "0");
+
+    displayRemainingTime.textContent = `Time Remaining: ${minutes}:${seconds}`;
+
+    if (remainingTime < 0) {
+      remainingTime += 24 * 3600;
+    }
+    let trigger = false;
+    if (!trigger && remainingTime === 10) {
+      trigger = true;
+      document.getElementById("main").style.backgroundColor = "red";
+      displayRemainingTime.textContent = "⏰";
+      playAlarm();
+    }
+    if (remainingTime === 0) {
+      document.getElementById("main").style.backgroundColor = "red";
+      displayRemainingTime.textContent = "⏰";
+      playAlarm();
+      clearInterval(interval);
+      return;
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
@@ -6,7 +49,8 @@ var audio = new Audio("alarmsound.mp3");
 
 function setup() {
   document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
+    const alarmTime = document.querySelector("#alarmSet").value;
+    setAlarm(alarmTime);
   });
 
   document.getElementById("stop").addEventListener("click", () => {
