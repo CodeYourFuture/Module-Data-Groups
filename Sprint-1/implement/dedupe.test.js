c1b088d0f786c94e29e096bf40f3b9eb799e5dee
Pretty Taruvinga <pretty548@gmail.com>
2026-03-31 09:48:10 +0200

    Improve dedupe test to check immutability and order

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -13,25 +13,16 @@ E.g. dedupe([1, 2, 1]) returns [1, 2]
 
 // Acceptance Criteria:
 
-// Given an empty array
-// When passed to the dedupe function
-// Then it should return an empty array
 test("Given an empty array, when passed to the dedupe function, then it should return an empty array", () => {
   expect(dedupe([])).toEqual([]);
 });
 
-// Given a non-array value
-// When passed to the dedupe function
-// Then it should return null
 test("Given a non-array value, when passed to the dedupe function, then it should return null", () => {
   expect(dedupe("not an array")).toBeNull();
   expect(dedupe(123)).toBeNull();
   expect(dedupe({})).toBeNull();
 });
 
-// Given an array with no duplicates
-// When passed to the dedupe function
-// Then it should return a copy of the original array
 test("Given an array with no duplicates, when passed to the dedupe function, then it should return a copy of the original array", () => {
   const input = ["a", "b", "c"];
   const copyOfInput = ["a", "b", "c"];
@@ -43,9 +34,6 @@ test("Given an array with no duplicates, when passed to the dedupe function, the
   expect(input).toEqual(copyOfInput);
 });
 
-// Given an array of strings or numbers
-// When passed to the dedupe function
-// Then it should remove the duplicate values, preserving the first occurrence of each element
 test("Given an array with strings or numbers, when passed to the dedupe function, then it should remove the duplicate values, preserving the first occurrence of each element", () => {
   expect(dedupe(["a", "a", "b", "c", "c"])).toEqual(["a", "b", "c"]);
   expect(dedupe([1, 2, 2, 3, 4, 4])).toEqual([1, 2, 3, 4]);
