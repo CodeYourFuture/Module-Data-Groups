0a6a3519dea81727a4f6b41ee5dffc8f1dfd8fd9
Karla G. <k.grajalesluna@gmail.com>
2026-04-01 00:37:50 +0100

    fix: correct coin value calculation and format total output in totalTill function

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -8,24 +8,28 @@ function totalTill(till) {
   let total = 0;
 
   for (const [coin, quantity] of Object.entries(till)) {
-    total += coin * quantity;
+     const coinValue = parseInt(coin);
+    total += coinValue * quantity;
   }
 
-  return `£${total / 100}`;
+  return `£${(total / 100).toFixed(2)}`;
 }
 
-const till = {
-  "1p": 10,
-  "5p": 6,
-  "50p": 4,
-  "20p": 10,
-};
-const totalAmount = totalTill(till);
 
-// a) What is the target output when totalTill is called with the till object
+module.exports = totalTill;
 
-// b) Why do we need to use Object.entries inside the for...of loop in this function?
+/*
 
-// c) What does coin * quantity evaluate to inside the for...of loop?
+a) What is the target output when totalTill is called with the till object
+  ----£4.40 because the total value of the coins in the till is 440 pence, which is equivalent to £4.40.
 
-// d) Write a test for this function to check it works and then fix the implementation of totalTill
+b) Why do we need to use Object.entries inside the for...of loop in this function?
+  ----Object.entries allows us to iterate over the key-value pairs of the till object, 
+  otherwise is an object and we cannot directly iterate over it with a for...of loop.
+
+c) What does coin * quantity evaluate to inside the for...of loop?
+    ----£NaN because the coin values are strings multiplied by numbers, which results in NaN.
+
+d) Write a test for this function to check it works and then fix the implementation of totalTill
+
+**/
