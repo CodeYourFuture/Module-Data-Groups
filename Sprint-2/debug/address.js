edfc2fb6b34df57a2ca98260438580d942d92dc7
Seti Mussa <Mussasti@gmail.com>
2026-04-01 14:38:06 +0100

    Fixed the  object property access to houseNumber

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+//Its will say undefined. This is because the address object does not have an index of 0.
+// To access the houseNumber property of the address object, we need to use dot notation or bracket notation with the property name as a string
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +14,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
