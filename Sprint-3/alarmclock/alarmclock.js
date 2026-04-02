e6141bc40f51e4f9ce9d34d14fa739596a0fd6da
KK Tech <dyxj6n786p@privaterelay.appleid.com>
2026-04-02 19:41:44 +0100

    uptaded my code

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,30 @@
-function setAlarm() {}
+function setAlarm() {
+  const input = document.getElementById("alarmSet");
+  const heading = document.getElementById("timeRemaining");
 
+  let totalSeconds = Number(input.value);
+
+  let minutes = String(Math.floor(totalSeconds / 60)).padStart(2, "0");
+  let seconds = String(totalSeconds % 60).padStart(2, "0");
+
+  heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
+
+  const timer = setInterval(() => {
+    totalSeconds--;
+
+    let minutes = String(Math.floor(totalSeconds / 60)).padStart(2, "0");
+    let seconds = String(totalSeconds % 60).padStart(2, "0");
+
+    heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
+
+    if (totalSeconds <= 0) {
+      clearInterval(timer);
+      playAlarm();
+    }
+
+    console.log(totalSeconds);
+  }, 1000);
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
