9786cba22aa82abc82521f8718f6ecb9300cc7f5
Seti Mussa <Mussasti@gmail.com>
2026-04-02 21:27:39 +0100

    Add contain function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,12 @@
-function contains() {}
+const object = {};
+object.name = "Tayle";
 
+function contains(object, prop) {
+  if (typeof object !== "object" || object === null || Array.isArray(object)) {
+    return false;
+  }
+  return object.hasOwnProperty(prop);
+}
+
+console.log(contains(object, "name"));
 module.exports = contains;
