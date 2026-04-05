648ae2883ecbb0f8768b955676ca3bc0711cccc9
boladjebsoft <djebbour1985@gmail.com>
2026-04-05 16:31:07 +0100

    implements a function that returns true if the object has a given property, and false otherwise

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,6 @@
-function contains() {}
+function contains(obj, name) {
+  if (typeof obj == "object" || obj !== null || Object.keys(obj).length !== 0)
+    return Object.hasOwn(obj, name);
+}
 
 module.exports = contains;
