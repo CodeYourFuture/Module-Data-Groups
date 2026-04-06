eeb1c243fdca0f9a2afb293113fb6252bd086b97
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-04-06 08:21:47 +0200

    Refactor: improve readability of formatTime() function

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -2,11 +2,8 @@ let timeRemaining;
 let timerInterval;
 
 function formatTime(time) {
-  const minutes =
-    Math.floor(time / 60) < 10
-      ? "0" + Math.floor(time / 60)
-      : Math.floor(time / 60);
-  const seconds = time % 60 < 10 ? "0" + (time % 60) : time % 60;
+  const minutes = String(Math.floor(time / 60)).padStart(2, "0");
+  const seconds = String(time % 60).padStart(2, "0");
 
   return `${minutes}:${seconds}`;
 }
