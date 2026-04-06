5fcb0b1560b9241fa69cb362937d9557e74905eb
Isaac Abodunrin <isaacabodunrin05@gmail.com>
2026-04-06 07:37:15 +0200

    Fix: add 0 in front of minutes when single digit

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -2,9 +2,13 @@ let timeRemaining;
 let timerInterval;
 
 function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
-  return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  const minutes =
+    Math.floor(time / 60) < 10
+      ? "0" + Math.floor(time / 60)
+      : Math.floor(time / 60);
+  const seconds = time % 60 < 10 ? "0" + (time % 60) : time % 60;
+
+  return `${minutes}:${seconds}`;
 }
 
 function displayAlarm(time) {
