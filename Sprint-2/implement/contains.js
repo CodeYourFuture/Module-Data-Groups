85d62d3ad9672a0f46e31a4432d45817afc7bf70
marthak1 <marthaogunbiyi196@gmail.com>
2026-04-06 18:49:43 +0100

    object proptotype removed from validation in contains.js file

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -2,12 +2,10 @@ function contains(obj, targetKey) {
   if (
     obj === null ||
     typeof obj !== "object" ||
-    Array.isArray(obj) ||
-    Object.getPrototypeOf(obj) !== Object.prototype
+    Array.isArray(obj)
   ) {
     throw new Error("Invalid Parameter");
   }
   return Object.hasOwn(obj, targetKey);
 }
-
 module.exports = contains;
