c171be8b6b193fdcaf70b60c0ff013d5896007cd
Mouawia Elkhalifa <moawia115@hotmail.com>
2026-04-06 12:25:27 +0100

    fix: address reviewer feedback for reset and stop logic

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,36 +1,47 @@
-let countdown = null; 
+let countdown = null;
+
+function updateDisplay(time) {
+  const timeDisplay = document.getElementById("timeRemaining");
+  let minutes = Math.floor(time / 60)
+    .toString()
+    .padStart(2, "0");
+  let seconds = (time % 60).toString().padStart(2, "0");
+  timeDisplay.innerText = "Time Remaining: " + minutes + ":" + seconds;
+}
+
+function resetAlarm() {
+  if (countdown !== null) {
+    clearInterval(countdown);
+    countdown = null;
+  }
+
+  pauseAlarm();
+  audio.currentTime = 0;
+  updateDisplay(0);
+}
 
 function setAlarm() {
-  let secondsLeft = parseInt(document.getElementById("alarmSet").value, 10); 
-  const timeDisplay = document.getElementById("timeRemaining"); 
+  resetAlarm();
 
-  if (isNaN(secondsLeft) || secondsLeft <= 0) { 
-    timeDisplay.innerText = "Time Remaining: 00:00";
+  let secondsLeft = parseInt(document.getElementById("alarmSet").value, 10);
+
+  if (isNaN(secondsLeft) || secondsLeft <= 0) {
+    updateDisplay(0);
     return;
   }
 
-  if (countdown !== null) { 
-    clearInterval(countdown);
-  }
+  updateDisplay(secondsLeft);
 
-  const updateDisplay = (time) => { 
-    let minutes = Math.floor(time / 60).toString().padStart(2, "0"); 
-    let seconds = (time % 60).toString().padStart(2, "0"); 
-    timeDisplay.innerText = "Time Remaining: " + minutes + ":" + seconds; 
-  };
+  countdown = setInterval(() => {
+    secondsLeft = secondsLeft - 1;
+    updateDisplay(secondsLeft);
 
-  updateDisplay(secondsLeft); 
-
-  countdown = setInterval(() => { 
-    secondsLeft = secondsLeft - 1; 
-    updateDisplay(secondsLeft); 
-
-    if (secondsLeft <= 0) { 
-      clearInterval(countdown); 
-      countdown = null; 
-      playAlarm(); 
+    if (secondsLeft <= 0) {
+      clearInterval(countdown);
+      countdown = null;
+      playAlarm();
     }
-  }, 1000); 
+  }, 1000);
 }
 
 // DO NOT EDIT BELOW HERE
@@ -44,6 +55,13 @@ function setup() {
 
   document.getElementById("stop").addEventListener("click", () => {
     pauseAlarm();
+  });
+
+  document.getElementById("stop").addEventListener("click", () => {
+    if (countdown !== null) {
+      clearInterval(countdown);
+      countdown = null;
+    }
   });
 }
 
