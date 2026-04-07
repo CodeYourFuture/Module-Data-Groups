b1c7edb7c919aefcb9159290bd7a493dba5cb2cf
Fayaz <fayaz.sadat551@yahoo.com>
2026-04-07 22:29:47 +0100

    fixed indentation

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,16 +1,11 @@
 function createLookup(countryCurrencyPairs) {
-let lookup ={};
-for(let pair of countryCurrencyPairs){
-let key = pair[0];
-let value = pair[1];
-lookup[key] = value;
+  let lookup = {};
+  for (let pair of countryCurrencyPairs) {
+    let key = pair[0];
+    let value = pair[1];
+    lookup[key] = value;
+  }
+  return lookup;
 }
-return lookup;
-
-
-
-}
-
-  
 
 module.exports = createLookup;
