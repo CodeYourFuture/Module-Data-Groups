6f6c8357c43207b635d9ff8e47c125595e512fa0
Samual-Hu <h2206506@gmail.com>
2026-04-07 14:46:01 +0100

    Remove leftover unused auto-play code

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -2,10 +2,6 @@ window.onload = () => {
   const quoteText = document.getElementById("quote");
   const authorText = document.getElementById("author");
   const newQuoteBtn = document.getElementById("new-quote");
-  const autoPlayCheckbox = document.getElementById("auto-play");
-  const autoPlayStatus = document.getElementById("auto-play-txt");
-
-  let autoPlayInterval;
 
   function generateQuote() {
     const randomQuote = pickFromArray(quotes);
@@ -16,16 +12,6 @@ window.onload = () => {
   generateQuote();
 
   newQuoteBtn.addEventListener("click", generateQuote);
-
-  autoPlayCheckbox.addEventListener("change", (event) => {
-    if (event.target.checked) {
-      autoPlayStatus.innerText = "Auto-Play: ON";
-      autoPlayInterval = setInterval(generateQuote, 5000);
-    } else {
-      autoPlayStatus.innerText = "Auto-Play: OFF";
-      clearInterval(autoPlayInterval);
-    }
-  });
 };
 // DO NOT EDIT BELOW HERE
 
