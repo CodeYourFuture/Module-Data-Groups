e629a8560e766eb9d1cc1b07fcc57b1569c8caaa
boladjebsoft <djebbour1985@gmail.com>
2026-04-07 13:01:54 +0100

    storing time in one variable ans implment required chanes.

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -6,8 +6,7 @@ const userInput = document.querySelector("#alarmSet");
 const timeRemaining = document.querySelector("#timeRemaining");
 // get access to the "stop" button
 const stop = document.querySelector("#stop");
-let seconds = 0;
-let minutes = 0;
+let totalSeconds = 0;
 
 function main() {
   // add an event listener to the "input" element that will call the counterUpdate function when clicked on or when the user types in the input field.
@@ -22,24 +21,25 @@ function counterUpdate() {
   const userInputValue = userInput.value;
   // check if the input is equal to or greater than 0
   if (userInputValue >= 0) {
-    minutes = Math.floor(userInputValue / 60);
-    seconds = userInputValue % 60;
-    // if it is, update the TimeRemaining element to show the real time remaining in minutes and seconds.
-    timeRemaining.innerText = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    totalSeconds = userInputValue;
+    displyUpdate();
   }
+}
+
+// Update the TimeRemaining element to show the real time remaining in minutes and seconds.
+function displyUpdate() {
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+  timeRemaining.innerText = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
 // define the function that will count down the time remaining every second and update the remaining time on the page
 function setAlarm() {
   // use setInterval to call a function every second that will decrease the time remaining by 1 second.
   const interval = setInterval(() => {
-    if (seconds > 0) {
-      seconds--;
-      timeRemaining.innerText = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-    } else if (minutes > 0) {
-      minutes--;
-      seconds = 59;
-      timeRemaining.innerText = `Time Remaining: ${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    if (totalSeconds > 0) {
+      totalSeconds--;
+      displyUpdate();
     } else {
       clearInterval(interval);
       userInput.value = "";
