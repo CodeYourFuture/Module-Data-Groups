f5fa49398279c68dc60d78dbc34bdced344a9892
Mona-Eltantawy <karamrafat@gmail.com>
2026-04-11 08:29:48 +0100

    I fixed the console.log function access to ingridents to log out the correct output

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+I expect that the program will not log out the right output because the ingridents are not being accssed correctly.
+as the recipe object is being logged out rather than the ingridents. 
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -12,4 +14,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe}`);
+${recipe.ingredients .join("\n") }`);
