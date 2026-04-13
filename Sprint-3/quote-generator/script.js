fcf0c91c87441377d2b00e0e9e673b4c9009e113
Karla G. <k.grajalesluna@gmail.com>
2026-04-14 00:08:43 +0100

    feat: add auto-play functionality for random quotes

--- Sprint-3/quote-generator/script.js
+++ Sprint-3/quote-generator/script.js
@@ -15,3 +15,27 @@ window.onload = () => {
 button.addEventListener("click", () => {
   randomQuoteGenerate();
 });
+
+
+const container = document.querySelector("#container-quote");
+const secondButton = document.createElement("button");
+secondButton.textContent = "Play Auto-Quotes";
+secondButton.classList.add("btn");
+container.appendChild(secondButton);
+
+let quoteInterval;
+
+secondButton.addEventListener("click", () => {
+  if (quoteInterval) {
+    clearInterval(quoteInterval);
+    quoteInterval = null;
+    secondButton.textContent = "Play Auto-Quotes";
+  } else {
+    quoteInterval = setInterval(randomQuoteGenerate, 5000);
+    secondButton.textContent = "Stop (auto-play:ON)";
+  }
+});
+
+
+
+
