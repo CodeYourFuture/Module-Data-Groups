f2dd47777ee37e5e14ba62f7969dd1d879298e95
Seti Mussa <Mussasti@gmail.com>
2026-04-13 17:15:21 +0100

    Add random quote generator

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -491,3 +491,21 @@ const quotes = [
 ];
 
 // call pickFromArray with the quotes array to check you get a random quote
+
+const quoteElement = document.getElementById("quote");
+const authorElement = document.getElementById("author");
+
+function displayQuote() {
+  const randomQuote = pickFromArray(quotes);
+
+  quoteElement.textContent = randomQuote.quote;
+  authorElement.textContent = randomQuote.author;
+}
+
+// Show a quote when page loads
+displayQuote();
+
+// Change quote when button is clicked
+document
+  .getElementById("new-quote")
+  .addEventListener("click", displayQuote);
