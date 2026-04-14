7847481c35b93aa128ae122b9597be106ed10282
Karla G. <k.grajalesluna@gmail.com>
2026-04-14 01:05:03 +0100

    feat: add autoplay functionality for background sound and improve quote display logic

--- Sprint-3/quote-generator/script.js
+++ Sprint-3/quote-generator/script.js
@@ -1,14 +1,17 @@
+// added extra functionality to play music when the user click on autoplay
 let backgroundSound = new Audio("assets/SPACE.mp3");
 let isSoundStarted = false;
 
+//avoid auto-playing sound in test environment and jsdom which does not implement audio playback
 function playSound() {
   if (!navigator.userAgent.includes("jsdom")) {
     backgroundSound.play().catch((error) => {
-      console.log("Esperando interacción para el audio");
+      console.log("waiting for user interaction to start sound");
     });
   }
 }
 
+//generated random quote from the array of quotes
 function pickFromArray(quotes) {
   return quotes[Math.floor(Math.random() * quotes.length)];
 }
@@ -18,16 +21,19 @@ const quoteText = document.querySelector("#quote");
 const quoteAuthor = document.querySelector("#author");
 const button = document.querySelector("#new-quote");
 
+// updates the quote and author text in the HTML
 function randomQuoteGenerate() {
   const randomArr = pickFromArray(quotes);
   quoteText.textContent = randomArr.quote;
   quoteAuthor.textContent = randomArr.author;
 }
 
+// triggers the first random quote display once the window is fully loaded
 window.onload = () => {
   randomQuoteGenerate();
 };
 
+// listens for clicks to change quotes manually and start the background sound
 button.addEventListener("click", () => {
   randomQuoteGenerate();
   if (!isSoundStarted) {
@@ -44,6 +50,7 @@ container.appendChild(secondButton);
 
 let quoteInterval;
 
+// toggles the automatic quote generator and manages the play/stop state
 secondButton.addEventListener("click", () => {
   if (quoteInterval) {
     clearInterval(quoteInterval);
