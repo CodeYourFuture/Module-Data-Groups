74e551f70bdcc9608b48df61b6e910dc1a93de0b
Mona-Eltantawy <karamrafat@gmail.com>
2026-04-14 21:19:11 +0100

    I fixed the function.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,12 +1,19 @@
 function tally(items) {
-    if (!Array.isArray(items)) {
-        throw new Error("Input must be an array");
-    }
-    const result ={ };
-    for (let item of items){
-        result[item]=(result[item] || 0)+1;
-    }
-    return result;
+  if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
+
+  const result = {};
+
+  for (let item of items) {
+    if (Object.hasOwn(result, item)) {
+      result[item] += 1;
+    } else {
+      result[item] = 1;
+    }npm TextDecoderStream
+  }
+
+  return result;
 }
 
 module.exports = tally;
