46724ecf9d5456079808855bc807d60c32b99fd6
Seti Mussa <Mussasti@gmail.com>
2026-04-16 18:56:00 +0100

    Add setup function foe page load

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -492,8 +492,12 @@ const quotes = [
 
 // call pickFromArray with the quotes array to check you get a random quote
 
-const quoteElement = document.getElementById("quote");
+// this runs when the page loads
+
+function setup() {
+  const quoteElement = document.getElementById("quote");
 const authorElement = document.getElementById("author");
+
 
 function displayQuote() {
   const randomQuote = pickFromArray(quotes);
@@ -509,3 +513,7 @@ displayQuote();
 document
   .getElementById("new-quote")
   .addEventListener("click", displayQuote);
+}
+
+// run setup after page loads
+window.addEventListener("load",setup);
