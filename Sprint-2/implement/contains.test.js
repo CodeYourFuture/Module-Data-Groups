5dccf8a0f890ad9ccdc62c28cda6a849efd3d0ab
Maryanne Mosonik <chmosonik@gmail.com>
2026-04-16 22:06:48 +0300

    Modified the tests.

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,29 +16,35 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
-test("contains returns true for existing property");
-expect (contains({a: 1, b: 2}, 'a')).toBe(true);
+test("contains returns true for existing property", () => {
+  expect(contains({a: 1, b: 2}, 'a')).toBe(true);
+});
 
-test("contains returns false for non-existent property");
-expect (contains({a: 1, b: 2}, 'c')).toBe(false);
+test("contains returns false for non-existent property", () => {
+  expect(contains({a: 1, b: 2}, 'c')).toBe(false);
+});
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test("contains returns false for empty object");
-expect (contains({}, 'a')).toBe(false);
+test("contains returns false for empty object", () => {
+  expect(contains({}, 'a')).toBe(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-test("contains returns true for existing property");
-expect (contains({a: 1, b: 2}, 'a')).toBe(true);
+test("contains returns true for existing property", () => {
+  expect(contains({a: 1, b: 2}, 'a')).toBe(true);
+});
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
-test("contains returns false for non-existent property");
-expect (contains({a: 1, b: 2}, 'c')).toBe(false);
+test("contains returns false for non-existent property", () => {
+  expect(contains({a: 1, b: 2}, 'c')).toBe(false);
+});
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
-test("contains with invalid parameters returns false or throws error");
-expect (contains([1, 2, 3], 'a')).toBe(false);
+test("contains with invalid parameters returns false or throws error", () => {
+  expect(contains([1, 2, 3], 'a')).toBe(false);
+});
