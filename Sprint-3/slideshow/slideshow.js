bf6aa6cb53a5dd6c0785a9ea2679ca0749a77b34
miriamjorna <hoimirextra@gmail.com>
2026-04-19 00:57:31 +0100

    Slideshow Level 1

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,30 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "https://unsplash.com/photos/three-giraffes-on-brown-grass-field-during-daytime-eQdDUdi5qLk",
+  "https://unsplash.com/photos/a-group-of-giraffes-standing-on-a-dirt-road-nUwKN_3PQ7c",
+  "https://unsplash.com/photos/giraffe-drinking-water-q73Ehqc4OqY",
+  "https://unsplash.com/photos/a-giraffe-standing-next-to-a-tree-in-a-field-aWNGjRw4QpM",
 ];
 
+let currentIndex = 0;
 
-// Write your code here
+const slide = document.getElementById("slide");
+const backButton = document.getElementById("back");
+const forwardButton = document.getElementById("forward");
+
+function updateSlide() {
+  slide.src = images[currentIndex];
+}
+
+backButton.addEventListener("click", () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateSlide();
+});
+
+forwardButton.addEventListener("click", () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  // makes a loop so that the last pic leads to the first pic and vv
+  updateSlide();
+});
+
+// show first image on load
+updateSlide();
