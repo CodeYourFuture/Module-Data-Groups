4d7b72ce4baea1cf13048e8be2ed86e4f7d7cb82
Jacknguyen4438 <jacknguyen4438@gmail.com>
2026-04-18 02:18:37 +0100

    finish the tally course work

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,17 @@
-function tally() {}
+function tally(sumArray) {
+  if (sumArray.length === 0) return {} ;
+  if (!Array.isArray(sumArray)){
+    throw new Error("Input musk be an array")
+  }
+  let totalSum = {};
+  for (const item of sumArray) {
+    if (totalSum[item] === undefined) {
+      totalSum[item] = 1;
+    } else {
+      totalSum[item] += 1;
+    }
+  }
+  return totalSum;
+}
 
 module.exports = tally;
