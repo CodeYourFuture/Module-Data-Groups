aa2e1e62618f3667123b9c8c9b2329af8364e944
Raihan Sharif <raihansharif92@gmail.com>
2026-05-07 17:48:38 +0100

    guard against non-array input. more idiomatic parameter name.

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,9 +1,14 @@
-function findMax(elements) {
-  return elements.reduce((acc, curr) => {
+function findMax(nums) {
+  if (!Array.isArray(nums)) {
+    throw new TypeError("Expected an array");
+  }
+
+  return nums.reduce((acc, curr) => {
     // ignores non-num values
-    if (typeof curr !== "number") {
+    if (typeof curr !== "number" || isNaN(curr)) {
       return acc;
     }
+
     return curr > acc ? curr : acc;
   }, -Infinity); // returns -Infinity for empty elements arr, as reduce callback never runs.
 }
