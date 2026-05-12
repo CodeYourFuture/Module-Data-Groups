bd846e8a910bec155d542667f1133ae272acd196
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-13 00:58:36 +0100

    debugged address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+// This will fail because to parse from an object requires a key, not an index value
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
@@ -12,4 +13,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.street}, ${address.city}, ${address.country}, ${address.postcode}`);
