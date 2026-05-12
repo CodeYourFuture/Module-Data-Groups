4e0a556f0b913b6a148ff1ada875ec9519194c59
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-12 23:44:48 +0100

    changed includes.js code to add for...of loop

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,6 +1,15 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
+for (item of list) {if (item === target) {return true;}}
+    return false;
+}
+
+module.exports = includes;
+
+
+/* Old code:
+
   for (let index = 0; index < list.length; index++) {
     const element = list[index];
     if (element === target) {
@@ -8,6 +17,4 @@ function includes(list, target) {
     }
   }
   return false;
-}
-
-module.exports = includes;
+*/
