0d3fb031f89a495fa7e359980c0680c55f96b1d8
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-12 23:20:25 +0100

    added solutions to max.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,7 @@
-function findMax(elements) {
+function findMax(list) {
+    const numbers = list.filter(n => Number.isFinite(n));
+    if (numbers.length == 0) { return "-Infinity"}
+    else {return Math.max(...numbers)};
 }
 
 module.exports = findMax;
