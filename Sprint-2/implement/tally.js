f928482bcd57d69823d7c02c72b6a02133a22c63
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-13 02:36:23 +0100

    added solution to tally.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,12 @@
-function tally() {}
+function tally(array) {
+    if (!Array.isArray(array)) {throw new Error("Invalid array");}
+    tally = {}
+    array.forEach((item) =>{
+        if (item in tally) {tally[item] += 1}
+        else tally[item] = 1;
+    })
+    return tally
+}
 
 module.exports = tally;
+
