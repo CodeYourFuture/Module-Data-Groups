5761063572afc6425739d2a1be45a58d6f94472f
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-13 01:12:46 +0100

    debugged recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,6 @@
 // Predict and explain first...
+// the title and serves looks like it would work, the ingredients list is just calling the recipe object
+// that won't list any values and may return an error or undefined.
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -11,5 +13,5 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:
+${recipe.ingredients.join("\n")}`);
