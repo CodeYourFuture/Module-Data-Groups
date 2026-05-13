556787567fef6c47edefa3c4c143d2cb7d201202
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-13 01:58:28 +0100

    added solution to lookup.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,9 @@
-function createLookup() {
-  return {'US': 'USD', 'CA': 'CAD', 'UK': 'GBP'}
+function createLookup(arrayOfArrays) {
+  const lookup = {}
+  for (array of arrayOfArrays) {
+    lookup[array[0]] = array[1];
+  }
+  return lookup
 }
 
 module.exports = createLookup;
