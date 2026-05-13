59b776367b8c97540efcd1c32d89e6519f4e4d3a
Alex Jamshidi <alexjamshidi@Alexs-MacBook-Air.local>
2026-05-13 01:43:38 +0100

    added solution to contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,5 +1,8 @@
-function contains() {
-    return true
+function contains(object, property) {
+    if (typeof object !== 'object' || Array.isArray(object)) {return false;}
+    if (typeof property !== 'string') {return false;}
+
+    return Object.hasOwn(object, property)
 }
 
 module.exports = contains;
