914cc03826f30fe881c5ccb5398117ed086224cc
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-07-04 14:42:34 +0100

    Update median.js

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -4,14 +4,15 @@ function calculateMedian(arr) {
 
     const nums = [];
 
-    for (const x of arr) {
+    for (let i = 0; i < arr.length; i++) {
+      const x = arr[i];
+
+      if (!(i in arr)) continue;
+
       if (typeof x === "number" && Number.isFinite(x)) {
         nums.push(x);
-        continue;
-      }
-
-      if (typeof x === "string") {
-        const n = Number(x.trim());
+      } else if (typeof x === "string") {
+        const n = Number(x);
         if (Number.isFinite(n)) nums.push(n);
       }
     }
