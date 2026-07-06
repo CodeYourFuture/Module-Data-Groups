c82a59f71f86ec70a5086c1b1cb02837e9f17f87
Sandani Kannangara <sandz.kannangara@gmail.com>
2026-07-06 02:36:48 +0100

    Fix for...of loop in author.js using Object.values()

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -3,7 +3,7 @@
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
-const author = {
+/*const author = {
   firstName: "Zadie",
   lastName: "Smith",
   occupation: "writer",
@@ -12,5 +12,31 @@ const author = {
 };
 
 for (const value of author) {
+  console.log(value);
+}*/
+
+//------------------------------------------------------------------------------------------
+
+// Prediction and explanation
+
+/*This gives an error because:
+author is a plain object
+for...of only works with iterables
+Objects are not iterable by default
+So JavaScript throws:
+TypeError: author is not iterable
+ */
+
+//fixed code
+
+const author = {
+  firstName: "Zadie",
+  lastName: "Smith",
+  occupation: "writer",
+  age: 40,
+  alive: true,
+};
+
+for (const value of Object.values(author)) {
   console.log(value);
 }
