be1cf59022e5187eaa8e6ebdd75cc685b471428d
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-06 20:24:38 +0100

    Updated refactor/includes.js to use for of loop

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,7 +1,10 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
-  return list.includes(target);
+  for (const number of list) {
+    if (number === target) return true;
+  }
+  return false;
 }
 
 module.exports = includes;
