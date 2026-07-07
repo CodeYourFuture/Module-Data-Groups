839ea3f2a16d26d2a53b0e4fea4baddba5c068a7
Yonatan Teklemariam <jante26uk@gmail.com>
2026-07-07 08:46:26 +0100

    fixed the dedupe implementation

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,20 @@
-function dedupe() {}
+function dedupe(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+
+  const newVersion = new Set(arr);
+
+  // If no duplicates, return the original array
+  if (newVersion.size === arr.length) {
+    return arr;
+  }
+
+  // Otherwise return the deduped array
+  const newArray = [...newVersion];
+  return newArray;
+}
+
+console.log(dedupe([1, 2, 3]));
+console.log(dedupe([10, 30, 20, 50, 20, 10]));
+console.log(dedupe(5, 1, 1, 2, 3, 2, 5, 8));
