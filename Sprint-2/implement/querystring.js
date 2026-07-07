dcc86626295dcfcf226132e0a72b9d3feaa2aeef
Sandani Kannangara <sandz.kannangara@gmail.com>
2026-07-07 01:18:12 +0100

    implement parseQueryString with edge case handling

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -3,14 +3,45 @@ function parseQueryString(queryString) {
   if (queryString.length === 0) {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    if (pair === "") {
+      continue;
+    }
+    const pairWithoutAdd = pair.replace(/\+/g, " ");
+    const indexOfEqual = pairWithoutAdd.indexOf("=");
+
+    let keyVal;
+    let val;
+
+    if (indexOfEqual === -1) {
+      keyVal = pairWithoutAdd;
+      val = "";
+    } else {
+      keyVal = pairWithoutAdd.slice(0, indexOfEqual);
+      val = pairWithoutAdd.slice(indexOfEqual + 1);
+    }
+    keyVal = decodeURIComponent(keyVal);
+    val = decodeURIComponent(val);
+
+    if (Object.hasOwn(queryParams, keyVal)) {
+      if (!Array.isArray(queryParams[keyVal])) {
+        queryParams[keyVal] = [queryParams[keyVal]];
+      }
+      queryParams[keyVal].push(val);
+    } else {
+      queryParams[keyVal] = val;
+    }
   }
 
   return queryParams;
 }
 
 module.exports = parseQueryString;
+
+/*
+let a = { color: "red" };
+a.color = [a.color];
+console.log(a);*/
