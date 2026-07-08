14896e8eb23d0498372f6e87bae8d7c0e1e2d32b
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-07-08 14:42:21 +0100

    Update max.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,18 @@
 function findMax(elements) {
+  const floats = elements
+    .filter(x => {
+      const str = String(x).replace("-", "").replace(".", "");
+      return !isNaN(str) && str !== "";
+    })
+    .map(x => Number(x));
+
+  if (floats.length === 0) {
+    return -Infinity;
+  } else {
+    return Math.max(...floats);
+  }
+}
+  
 }
 
 module.exports = findMax;
