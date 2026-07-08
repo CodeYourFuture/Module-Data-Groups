91bb6480972f9f2f7733e66c4fe89335ce27bf59
Zadri Abdule <zadriabdule@gmail.com>
2026-07-08 20:03:41 +0100

    implemented findMax and added tests

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,3 +1,14 @@
-function findMax(elements) {}
+function findMax(elements) {
+    let max = -Infinity;
+    for (const value of elements) {
+        if (typeof value === "number") {
+            max = Math.max(max, value);
+        }
+    }
+    return max;
+}
+
+
+
 
 module.exports = findMax;
