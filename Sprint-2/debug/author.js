7ed691127c1fdaa567ef80497411f8d750285574
Yonatan Teklemariam <jante26uk@gmail.com>
2026-07-08 19:46:41 +0100

    added some comments for highlighting purposes.

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -2,6 +2,7 @@
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
+// Because objects are not iterable, so they need to be converted to arrays so as we can loop through them so, with Object.entries() method as this method converts the objects into key and value pairs within an array.
 
 const author = {
   firstName: "Zadie",
@@ -11,6 +12,8 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+let authorEntries = Object.entries(author);
+for (const value of authorEntries) {
   console.log(value);
 }
+//
--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-
+// so the first recipe.title prints the correct title, the second recipe.serves also prints the correct number of serves but the last recipe in template literals will throw error or perhaps undefined as recipe is an object and needs different method to access it. so basically inorder to access the key and values inside an object, it should be accessed by using object name followed by a dot and one of the values or keys inside it (whatever we want to use).
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
