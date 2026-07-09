0eed554d3306314cfe89708e811b952adc747e96
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-07-09 13:22:25 +0100

    Update querystring.js

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,34 @@
 function parseQueryString(queryString) {
-  const queryParams = {};
-  if (queryString.length === 0) {
-    return queryParams;
-  }
-  const keyValuePairs = queryString.split("&");
+  if (queryString === "") {
+    return {};
+  } else {
+    queryString = decodeURIComponent(queryString)
+      .replace(/\+/g, " ")
+      .replace(/&&/g, "&")
+      .replace(/&+$/, "");
 
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
-  }
+    const splitByAnds = queryString.split("&");
 
-  return queryParams;
+    const splitByEquals = splitByAnds.map(x => {
+      const parts = x.split("=", 2);
+      return parts.length === 1 ? [parts[0], ""] : parts;
+    });
+
+    return Object.fromEntries(splitByEquals);
+  }
+}
+  // const queryParams = {};
+  // if (queryString.length === 0) {
+  //   return queryParams;
+  // }
+  // const keyValuePairs = queryString.split("&");
+
+  // for (const pair of keyValuePairs) {
+  //   const [key, value] = pair.split("=");
+  //   queryParams[key] = value;
+  // }
+
+  // return queryParams;
 }
 
 module.exports = parseQueryString;
