caa36799abf08c4e22526b6d2ddf08e800721b68
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-07-09 11:38:34 +0100

    Update lookup.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,10 @@
 function createLookup() {
   // implementation here
+      for (const [country, currency] of country_currency_pairs) {
+        lookup[country] = currency;
+    }
+
+    return lookup;
 }
 
 module.exports = createLookup;
