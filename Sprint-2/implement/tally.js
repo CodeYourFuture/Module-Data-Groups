aaeb462d2a39f408a22f8313ffc0ae147e72cedb
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-07-09 15:27:05 +0100

    Update tally.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,16 @@
-function tally() {}
+function tally(items) {
+  if (!Array.isArray(items)) {
+    throw new TypeError("Input must be an array");
+  }
+
+  const uniqueItems = [...new Set(items)].sort();
+
+  const counts = {};
+  for (const item of uniqueItems) {
+    counts[item] = items.filter(x => x === item).length;
+  }
+
+  return counts;
+}
 
 module.exports = tally;
