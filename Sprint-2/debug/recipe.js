4a01f99bd53b847337a46382d688b528f65a5742
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-09 02:16:20 +0100

    Fix error in debug/recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+// [MM] - I expect this programme to log out recipe object rather than the ingredients
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -10,6 +11,5 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+console.log(`${recipe.title} serves ${recipe.serves} ingredients:
+${recipe.ingredients.join("\n")}`);
