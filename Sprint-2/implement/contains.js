3ce1f746a9c995d4df804988f98989f4d253f144
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-09 14:54:24 +0100

    complete function in implement/contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,16 @@
-function contains() {}
+function contains(object, value) {
+  const isNotObject =
+    typeof object !== "object" ||
+    object === null ||
+    Array.isArray(object) ||
+    Object.keys(object).length < 1;
+
+  if (isNotObject) return false;
+
+  return Object.keys(object).includes(value);
+}
+
+console.log(contains({ a: 1, b: 2 }, "a"));
+console.log(contains(["a", "b"], "a"));
 
 module.exports = contains;
