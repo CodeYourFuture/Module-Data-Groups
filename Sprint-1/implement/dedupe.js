d4a5843c96f2f3fa29d265b624e29a53dec18af5
Dipa-Sarker <dipasarker696@gmail.com>
2026-07-10 01:21:17 +0100

    implement dedupe function and tests

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,10 @@
-function dedupe() {}
+function dedupe(array) {
+  const result = [];
+  if (array.length === 0) {
+    return result;
+  }
+  const set = new Set(array);
+  const newArray = Array.from(set);
+  return newArray;
+}
+module.exports = dedupe;
--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -1,28 +1,40 @@
 const dedupe = require("./dedupe.js");
-/*
-Dedupe Array
 
-📖 Dedupe means **deduplicate**
+describe("dedupe", () => {
+  test("given an empty array, it should return an empty array", () => {
+    expect(dedupe([])).toEqual([]);
+  });
+  test.each([
+    {
+      input: [1, 2, 3, 4],
+      expected: [1, 2, 3, 4],
+    },
+    {
+      input: ["a", "b", "c"],
+      expected: ["a", "b", "c"],
+    },
+  ])("returns a copy of an array with no duplicates", ({ input, expected }) => {
+    const result = dedupe(input);
 
-In this kata, you will need to deduplicate the elements of an array
+    expect(result).toEqual(expected);
+    expect(result).not.toBe(input);
+  });
+  test.each([
+    {
+      input: ["a", "c", "b", "d", "c", "d"],
+      expected: ["a", "c", "b", "d"],
+    },
+    {
+      input: [1, 2, 3, 2, 4, 3],
+      expected: [1, 2, 3, 4],
+    },
+  ])(
+    "given an array with duplicates, it should remove duplicates and preserve first occurrence",
+    ({ input, expected }) => {
+      const result = dedupe(input);
 
-E.g. dedupe(['a','a','a','b','b','c']) returns ['a','b','c']
-E.g. dedupe([5, 1, 1, 2, 3, 2, 5, 8]) returns [5, 1, 2, 3, 8]
-E.g. dedupe([1, 2, 1]) returns [1, 2]
-*/
-
-// Acceptance Criteria:
-
-// Given an empty array
-// When passed to the dedupe function
-// Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
-
-// Given an array with no duplicates
-// When passed to the dedupe function
-// Then it should return a copy of the original array
-
-// Given an array of strings or numbers
-// When passed to the dedupe function
-// Then it should return a new array with duplicates removed while preserving the 
-// first occurrence of each element from the original array.
+      expect(result).toEqual(expected);
+      expect(result).not.toBe(input);
+    }
+  );
+});
