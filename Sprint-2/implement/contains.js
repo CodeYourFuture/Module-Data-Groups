e0455d0d6e910f7dcf97e50ea24f2924e8e826f6
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-10 08:40:13 +0100

    Refactor implement/contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,13 +1,10 @@
 function contains(object, value) {
   const isNotObject =
-    typeof object !== "object" ||
-    object === null ||
-    Array.isArray(object) ||
-    Object.keys(object).length < 1;
+    typeof object !== "object" || object === null || Array.isArray(object);
 
   if (isNotObject) return false;
 
-  return Object.keys(object).includes(value);
+  return Object.hasOwn(object, value);
 }
 
 console.log(contains({ a: 1, b: 2 }, "a"));
