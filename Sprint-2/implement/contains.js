7abedd44b57dcd82cb6053c9922e5aad37fcd90a
Niangh Ciang <cciang462@gmail.com>
2026-07-11 16:05:24 +0100

    Implement contains function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,7 @@
-function contains() {}
-
+function contains(obj, prop) {
+  if (typeof obj !== "object" || obj === null) {
+    return false;
+  }
+  return obj.hasOwnProperty(prop);
+}
 module.exports = contains;
