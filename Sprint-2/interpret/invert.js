213eee29a4810bcd77611094878904df10c8d128
Arthur <>
2026-07-11 19:12:04 +0100

    invert.js

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -10,20 +10,30 @@ function invert(obj) {
   const invertedObj = {};
 
   for (const [key, value] of Object.entries(obj)) {
-    invertedObj.key = value;
+    invertedObj[value] = key;
   }
 
   return invertedObj;
 }
 
+console.log(invert({ a: 1 }));
 // a) What is the current return value when invert is called with { a : 1 }
-
+//{1: a }
+{a : 1}
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
+{a :1, b: 2 }
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
+{1 :a, 2: b }
 
 // c) What does Object.entries return? Why is it needed in this program?
+because for... loop doesn't recognize object insides, so by turning objects into objects.entries, it will become an array format that the for...loop function can recognize.
+
 
 // d) Explain why the current return value is different from the target output
+because we didn't switch the position of key and value insides the function.
+Also we forgot to add [ ], it is to recognize the data insides item.
+
+
 
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
