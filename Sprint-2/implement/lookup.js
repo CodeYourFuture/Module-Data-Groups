db23e2ce79657bc28b4f8e95786effa7aef4f4ca
Arthur <>
2026-07-11 18:58:32 +0100

    lookup.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,13 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+  if (!Array.isArray(pairs)) {
+    throw Error;
+  }
+  const myLookup = {};
+
+  for (let pair of pairs) {
+    myLookup[pair[0]] = pair[1];
+  }
+  return myLookup;
 }
 
 module.exports = createLookup;
