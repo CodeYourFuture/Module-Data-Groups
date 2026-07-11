5d6fe57f242cef75f25d24b56b4aca79ca08d11f
Arthur <>
2026-07-11 18:55:32 +0100

    contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,33 @@
-function contains() {}
+const object = {
+  a: 1,
+  b: 2,
+};
+
+function contains(object, key) {
+  if (Array.isArray(object)) {
+    throw new Error();
+  }
+
+  if (!object) {
+    return false;
+  }
+
+  if (key in object) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = contains;
+
+/*
+Implement a function called contains that checks an object contains a
+particular property
+
+E.g. contains({a: 1, b: 2}, 'a') // returns true
+as the object contains a key of 'a'
+
+E.g. contains({a: 1, b: 2}, 'c') // returns false
+as the object doesn't contains a key of 'c'
+*/
