777a9a43edcb5bc721b40d64470ad174ec9cd3c1
Tomislav Dukez <tomdu3@ymail.com>
2026-07-13 21:44:06 +0100

    add remove duplicate elements function

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,12 @@
-function dedupe() {}
+function dedupe(inputArray) {
+  const uniqueElements = [];
+  for (const el of inputArray) {
+    if (uniqueElements.includes(el)) {
+      continue;
+    }
+    uniqueElements.push(el);
+  }
+  return uniqueElements;
+}
+
+module.exports = dedupe;
