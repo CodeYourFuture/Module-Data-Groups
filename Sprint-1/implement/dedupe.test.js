4b21149a232aad69ad654b5677d46d290554217c
Tomislav Dukez <tomdu3@ymail.com>
2026-07-13 21:44:22 +0100

    add tests for dedupe function

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -13,16 +13,32 @@ E.g. dedupe([1, 2, 1]) returns [1, 2]
 
 // Acceptance Criteria:
 
-// Given an empty array
-// When passed to the dedupe function
-// Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+describe("dedupe", () => {
+  // Given an empty array
+  // When passed to the dedupe function
+  // Then it should return an empty array
+  it("returns an empty array when given empty array", () => {
+    expect(dedupe([])).toStrictEqual([]);
+  });
 
-// Given an array with no duplicates
-// When passed to the dedupe function
-// Then it should return a copy of the original array
-
-// Given an array of strings or numbers
-// When passed to the dedupe function
-// Then it should return a new array with duplicates removed while preserving the 
-// first occurrence of each element from the original array.
+  // Given an array with no duplicates
+  // When passed to the dedupe function
+  // Then it should return a copy of the original array
+  it("returns a copy of the array is the one given without duplicates", () => {
+    expect(dedupe([1, 2, 3, 4])).toStrictEqual([1, 2, 3, 4]);
+    expect(dedupe(["a", "b", "c", "d"])).toStrictEqual(["a", "b", "c", "d"]);
+  });
+  // Given an array of strings or numbers
+  // When passed to the dedupe function
+  // Then it should return a new array with duplicates removed while preserving the
+  // first occurrence of each element from the original array.
+  it("returns an array of unique elements with order preserving the first occurence of each element", () => {
+    expect(dedupe(["a", "a", "a", "b", "b", "c"])).toStrictEqual([
+      "a",
+      "b",
+      "c",
+    ]);
+    expect(dedupe([5, 1, 1, 2, 3, 2, 5, 8])).toStrictEqual([5, 1, 2, 3, 8]);
+    expect(dedupe([1, 2, 1])).toStrictEqual([1, 2]);
+  });
+});
