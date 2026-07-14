ca97ed235c9999dc4059c34e1917e35b6481c866
Tomislav Dukez <tomdu3@ymail.com>
2026-07-14 14:28:45 +0100

    add findMax function solution

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,17 @@
 function findMax(elements) {
+  // filter out only numbers to the new array
+  const numElements = elements.filter(
+    (el) => !isNaN(el) && typeof el === "number"
+  );
+
+  return numElements.reduce(
+    (acc, curr) => (acc > curr ? acc : curr),
+    -Infinity
+  );
 }
 
 module.exports = findMax;
+
+console.log(findMax([-10, 20, 0, 100, 1]));
+console.log(findMax([]));
+console.log(findMax(["I", 2, "am", -1000, "Lord", "200", "Voldemort", 10]));
