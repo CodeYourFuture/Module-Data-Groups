6474d852611bcbc8c18e9053d92452e45f689d51
Tomislav Dukez <tomdu3@ymail.com>
2026-07-14 23:14:47 +0100

    add solution to aoc 2018 day1 part1

--- Sprint-1/stretch/aoc-2018-day1/solution.js
+++ Sprint-1/stretch/aoc-2018-day1/solution.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
 let frequencies;
+
+// read the input file and convert the input elements into numbers
 try {
   const data = fs.readFileSync("./input.txt", "utf8");
   const inputData = data.split("\n").filter((line) => line.trim() !== "");
@@ -8,3 +10,8 @@ try {
 } catch (err) {
   console.error("Error reading file:", err);
 }
+
+function getFrequenciesSum(frequencies) {
+  return frequencies.reduce((acc, curr) => acc + curr, 0);
+}
+console.log(getFrequenciesSum(frequencies)); // result is 529
