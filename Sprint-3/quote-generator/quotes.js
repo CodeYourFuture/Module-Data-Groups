24a446368a38f1e7077d2679973ce4959a8b9672
joanne342 <130585013+joanne342@users.noreply.github.com>
2026-07-14 05:09:52 +0100

    Update quotes.js

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -1,23 +1,3 @@
-const quoteElem = document.getElementById("quote");
-const authorElem = document.getElementById("author");
-const newQuoteBtn = document.getElementById("new-quote");
-
-function getRandomQuote() {
-  return pickFromArray(quotes);
-}
-
-function displayQuote(quote) {
-  quoteElem.textContent = quote.quote;
-  authorElem.textContent = quote.author;
-}
-
-function showRandomQuote() {
-  displayQuote(getRandomQuote());
-}
-
-newQuoteBtn.addEventListener("click", showRandomQuote);
-showRandomQuote();
-
 // DO NOT EDIT BELOW HERE
 
 // pickFromArray is a function which will return one item, at
@@ -511,3 +491,23 @@ const quotes = [
 ];
 
 // call pickFromArray with the quotes array to check you get a random quote
+
+const quoteElem = document.getElementById("quote");
+const authorElem = document.getElementById("author");
+const newQuoteBtn = document.getElementById("new-quote");
+
+function getRandomQuote() {
+  return pickFromArray(quotes);
+}
+
+function displayQuote(quote) {
+  quoteElem.textContent = quote.quote;
+  authorElem.textContent = quote.author;
+}
+
+function showRandomQuote() {
+  displayQuote(getRandomQuote());
+}
+
+newQuoteBtn.addEventListener("click", showRandomQuote);
+showRandomQuote();
