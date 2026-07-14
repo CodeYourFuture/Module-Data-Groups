975a820c9c5970ba0e7002101c0754f069528190
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-14 15:08:41 +0100

    Write tests for lookup.test.js

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -31,14 +31,53 @@ It should return:
    'CA': 'CAD'
  }
 */
-test("maps country codes to currency codes", () => {
-  expect(
-    createLookup([
-      ["US", "USD"],
-      ["CA", "CAD"],
-    ])
-  ).toEqual({
-    US: "USD",
-    CA: "CAD",
+
+describe("createLookup", () => {
+  test("maps country codes to currency codes", () => {
+    expect(
+      createLookup([
+        ["US", "USD"],
+        ["CA", "CAD"],
+      ])
+    ).toEqual({
+      US: "USD",
+      CA: "CAD",
+    });
+  });
+
+  test.each([
+    [
+      "throws when input array is empty",
+      [],
+      "Input must not be an empty array",
+    ],
+    ["throws when input is not an array", "hello", "Input must be an array"],
+    [
+      "throws when input pairs are invalid",
+      [1, 2, 3],
+      "Input must be valid text pairs",
+    ],
+    [
+      "throws when duplicate pairs are provided",
+      [
+        ["US", "USD"],
+        ["US", "USD"],
+      ],
+      "Input must not be duplicated",
+    ],
+  ])("%s", (_title, data, expecteMessage) => {
+    expect(() => createLookup(data).toThrow(expecteMessage));
+  });
+
+  test("throws when country code format is incorrect", () => {
+    expect(() => createLookup([["us", "USD"]])).toThrow(
+      "Country code must be 2 uppercase letters"
+    );
+  });
+
+  test("throws when currency code format is incorrect", () => {
+    expect(() => createLookup([["US", "usd"]])).toThrow(
+      "Currency code must be 3 uppercase letters"
+    );
   });
 });
