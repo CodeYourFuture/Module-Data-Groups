dab2b2470603ea96dc014b097e4ed1a1f9921a41
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-15 18:13:24 +0100

    Complete implement/querysting.js so that all tests pass

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -6,10 +6,22 @@ function parseQueryString(queryString) {
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
-  }
+    if (pair === "") continue;
 
+    const [keyPart, ...valueParts] = pair.split("=");
+    const rawKey = keyPart.trim("").replace(/\+/g, " ");
+    const rawValue = valueParts.join("=").trim("").replace(/\+/g, " ");
+    const key = decodeURIComponent(rawKey);
+    const value = decodeURIComponent(rawValue);
+
+    if (queryParams[key] === undefined) {
+      queryParams[key] = value;
+    } else if (Array.isArray(queryParams[key])) {
+      queryParams[key].push(value);
+    } else {
+      queryParams[key] = [queryParams[key], value];
+    }
+  }
   return queryParams;
 }
 
