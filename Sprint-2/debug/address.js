d5927fbce24afebf4c4ac14347a719e0b1c4af69
Dipa-Sarker <dipasarker696@gmail.com>
2026-07-15 23:50:59 +0100

    predicted and fixed address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,8 +1,10 @@
 // Predict and explain first...
-
+// I think, for object we have to use key as property value instead of array index in console.log.
 // This code should log out the houseNumber from the address object
 // but it isn't working...
 // Fix anything that isn't working
+
+//Correct Code:
 
 const address = {
   houseNumber: 42,
@@ -12,4 +14,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
