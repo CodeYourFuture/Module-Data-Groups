d1f7aea367f55d3dbd85f50fb1d7b60a8d26f843
Dipa-Sarker <dipasarker696@gmail.com>
2026-07-16 00:52:22 +0100

    predicted and fixed code for recipe.js

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,5 +1,6 @@
 // Predict and explain first...
-
+// My prediction is for log out the title, how many it serves and the ingridients we have to use several console.log for each of the property 
+// value. Besides, for each ingredient should be logged on a new line, we can use array index for the ingredients array.
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -10,6 +11,10 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+
+console.log(recipe.title);
+console.log(recipe.serves);
+console.log(recipe.ingredients[0]);
+console.log(recipe.ingredients[1]);
+console.log(recipe.ingredients[2]);
+console.log(recipe.ingredients[3]);
