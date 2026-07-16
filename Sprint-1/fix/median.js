c9145dfc0a9be944a44642f290176cd6a3fffe52
russom <russom.g.meskel@gmail.cpm>
2026-07-16 20:57:19 +0100

    CalculateMedian function implemented

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,16 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
+  for (let i = 0; i < list.length; i++) {
+    if (typeof list[i] === "number" && typeof list[i] === " ") {
+    return null;
+  }
   const middleIndex = Math.floor(list.length / 2);
   const median = list.splice(middleIndex, 1)[0];
   return median;
 }
+}
+let nums = [1, 2, 3];
+console.log(calculateMedian(nums));
 
 module.exports = calculateMedian;
