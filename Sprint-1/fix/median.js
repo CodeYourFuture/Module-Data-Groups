256ef50be642483dee5284a511cdcdc005e71358
russom <russom.g.meskel@gmail.cpm>
2026-07-16 23:01:36 +0100

    Function calculateMedian updated so it could calculate even elements of arrays.

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -7,15 +7,17 @@
 
 function calculateMedian(list) {
   for (let i = 0; i < list.length; i++) {
-    if (typeof list[i] === "number" && typeof list[i] === " ") { // List is checked whether it is numbers and strings.
-    return null; // Null is returned if list is not numbers or has mixed values(numbers and strings)
+    if (typeof list[i] === "number" && typeof list[i] === " ") {
+      // List is checked whether it is numbers and strings.
+      return null; // Null is returned if list is not numbers or has mixed values(numbers and strings)
+    }
+    const middleIndex = Math.floor(list.length / 2);
+
+    if (list.length % 2 === 0) {
+      return (list[middleIndex - 1] + list[middleIndex]) / 2;
+    }
+    return list[middleIndex];
   }
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
 }
-}
-let nums = [1, 2, 3];
-console.log(calculateMedian(nums));
 
 module.exports = calculateMedian;
