1cf01db33917126bc42de35c6a6f4768590f6276
russom <russom.g.meskel@gmail.cpm>
2026-07-16 20:58:11 +0100

    Comment updated on return null

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -7,8 +7,8 @@
 
 function calculateMedian(list) {
   for (let i = 0; i < list.length; i++) {
-    if (typeof list[i] === "number" && typeof list[i] === " ") {
-    return null;
+    if (typeof list[i] === "number" && typeof list[i] === " ") { // List is checked whether it is numbers and strings.
+    return null; // Null is returned if list is not numbers or has mixed values(numbers and strings)
   }
   const middleIndex = Math.floor(list.length / 2);
   const median = list.splice(middleIndex, 1)[0];
