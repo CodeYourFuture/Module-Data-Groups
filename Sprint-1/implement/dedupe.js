43561d7133910213fafa8e3889402f8b8d9a9174
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-16 20:31:27 +0100

    Refactor dedupe.js

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,10 +1,7 @@
 function dedupe(array) {
   // return empty array if empty input
   if (array.length === 0) return [];
-  // check if array has duplicates
-  const hasDuplicates = new Set(array).size !== array.length;
-  // if no duplicates return original array else return new array with duplicates removed (string or numbers)
-  return !hasDuplicates ? array : [...new Set(array)];
+  return Array.from(new Set(array));
 }
 
 module.exports = dedupe;
