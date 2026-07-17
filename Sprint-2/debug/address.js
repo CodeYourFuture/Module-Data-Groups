f504eb1efb0696d70bfb82841407dcf07392d909
Tomislav Dukez <tomdu3@ymail.com>
2026-07-17 08:34:49 +0100

    add address solution

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -4,6 +4,20 @@
 // but it isn't working...
 // Fix anything that isn't working
 
+// const address = {
+//   houseNumber: 42,
+//   street: "Imaginary Road",
+//   city: "Manchester",
+//   country: "England",
+//   postcode: "XYZ 123",
+// };
+//
+// console.log(`My house number is ${address[0]}`);
+
+// => in line 15, the object is being called, but the index, instead of a key
+// is put inside the square brackets. This should result in undefined value, since
+// we are treating object as an array.
+
 const address = {
   houseNumber: 42,
   street: "Imaginary Road",
@@ -12,4 +26,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
