39e7f31c1e8bd53c9aa5a915024121315b9c968a
Tomislav Dukez <tomdu3@ymail.com>
2026-07-17 08:48:56 +0100

    add recipe solution

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -4,6 +4,19 @@
 // Each ingredient should be logged on a new line
 // How can you fix it?
 
+// const recipe = {
+//   title: "bruschetta",
+//   serves: 2,
+//   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
+// };
+//
+// console.log(`${recipe.title} serves ${recipe.serves}
+//   ingredients:
+// ${recipe}`);
+
+// there's an error in line 15. Instead of logging all the ingredients, line by line, the code is telling to give the whole object
+// and the representation of it will be logged into the console.
+
 const recipe = {
   title: "bruschetta",
   serves: 2,
@@ -11,5 +24,8 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+ingredients:`);
+
+for (const ingredient of recipe.ingredients) {
+  console.log(ingredient);
+}
