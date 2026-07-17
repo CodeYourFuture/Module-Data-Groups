c60f88b3255c7a2c856e76ba5556d130f6beef4b
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-17 20:59:33 +0100

    my sum solution

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,16 @@
 function sum(elements) {
+    if (!Array.isArray(elements) || elements.length === 0) {
+    return 0;
+  }
+
+  const numbersOnly = elements.filter(item => typeof item === "number" && !isNaN(item));
+
+  if (numbersOnly.length === 0) {
+    return 0;
+  }
+
+  return numbersOnly.reduce((acc, curr) => acc + curr, 0);
+
 }
 
 module.exports = sum;
