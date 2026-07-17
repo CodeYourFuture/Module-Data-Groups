529da6af5318eeb92859453a6a710ef98a3d3675
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-17 20:51:26 +0100

    my max test

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,12 @@
 function findMax(elements) {
+    if (!Array.isArray(elements) || elements.length === 0) {
+        return -Infinity;
+    }
+    const numbersOnly = elements.filter(item => typeof item === 'number' && !isNaN(item));
+    if (numbersOnly.length === 0) {
+        return -Infinity;
+    }
+    return Math.max(...numbersOnly);
 }
 
 module.exports = findMax;
