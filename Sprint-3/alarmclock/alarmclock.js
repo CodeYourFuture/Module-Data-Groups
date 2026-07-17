c12f5446550a9f1e9409daf3cd9b95aa9615c12b
Martin Mwaka <martinmwaka@hotmail.com>
2026-07-17 17:42:45 +0100

    write code to get alarm clock working in alarmclock.js

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,28 @@
-function setAlarm() {}
+const heading = document.querySelector("#timeRemaining");
+const input = document.querySelector("#alarmSet");
+const startBtn = document.querySelector("#set");
+const stopBtn = document.querySelector("#stop");
+
+function setAlarm() {
+  let totalSeconds = Number(input.value);
+  let timer = null;
+
+  timer = setInterval(() => {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    const mm = String(minutes).padStart(2, "0");
+    const ss = String(seconds).padStart(2, "0");
+    heading.innerText = `Time Remaining: ${mm}:${ss}`;
+    if (totalSeconds === 0) {
+      timer = null;
+      playAlarm();
+      return;
+    }
+    totalSeconds--;
+  }, 1000);
+
+  input.value = "";
+}
 
 // DO NOT EDIT BELOW HERE
 
