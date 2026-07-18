e32b9449db502d7a44f226308b8ba4f3bb274699
Alina Sofragiu <sofragiualina@gmail.com>
2026-07-19 00:13:58 +0100

    Keep only Sprint-1

--- Sprint-2/implement/contains.js
+++
@@ -1,9 +0,0 @@
-function contains(object, property) {
-  if (typeof object !== "object" || object === null || Array.isArray(object)) {
-    return false;
-  }
-
-  return object.hasOwnProperty(property);
-}
-
-module.exports = contains;
