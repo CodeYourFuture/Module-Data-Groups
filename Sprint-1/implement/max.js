973df9183eb199b3914b6b9318a71f4303964343
russom <russom.g.meskel@gmail.cpm>
2026-07-18 22:30:59 +0100

    findMax function implemented to check for the largest number in a given array.

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,11 @@
 function findMax(elements) {
+    let largestNum = [];
+    for (let i = 0; i <= elements.length; i++) {
+        if (elements[i] > largestNum) {
+            largestNum = elements[i];
+        }
+    }
+    return largestNum;
 }
 
 module.exports = findMax;
