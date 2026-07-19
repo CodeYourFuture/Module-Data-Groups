83993fd918544190394e2d712036baebe3d958c7
Niangh Ciang <cciang462@gmail.com>
2026-07-19 16:40:39 +0100

    Implement alarm clock countdown logic

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,23 @@
-function setAlarm() {}
+function setAlarm() {
+  const timeInput = document.getElementById("alarmSet");
+  let remainingTime = Number(timeInput.value);
+  const timeRemainingDisplay = document.getElementById("timeRemaining");
+
+  timeRemainingDisplay.innerText = `Time Remaining: ${formatTime(remainingTime)}`;
+  const countdownInterval = setInterval(() => {
+    remainingTime -= 1;
+    if (remainingTime === 0) {
+      playAlarm();
+      clearInterval(countdownInterval);
+    }
+    timeRemainingDisplay.innerText = `Time Remaining: ${formatTime(remainingTime)}`;
+  }, 1000);
+}
+function formatTime(seconds) {
+  const mm = String(Math.floor(seconds / 60)).padStart(2, "0");
+  const ss = String(seconds % 60).padStart(2, "0");
+  return `${mm}:${ss}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
