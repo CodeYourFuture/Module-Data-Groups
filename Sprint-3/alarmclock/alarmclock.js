35d8c2fd5eaded9293756f2762ec18e9a8789693
Alina Sofragiu <sofragiualina@gmail.com>
2026-07-19 11:44:27 +0100

    alarm clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,46 @@
-function setAlarm() {}
+let timer = null;
+
+function updateHeading(secondsRemaining) {
+  const minutes = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(seconds).padStart(2, "0");
+
+  document.getElementById("timeRemaining").innerText =
+    `Time Remaining: ${formattedMinutes}:${formattedSeconds}`;
+}
+
+function setAlarm() {
+  if (timer) {
+    clearInterval(timer);
+  }
+
+  let secondsRemaining = Number(document.getElementById("alarmSet").value);
+
+  updateHeading(secondsRemaining);
+
+  timer = setInterval(() => {
+    secondsRemaining--;
+
+    if (secondsRemaining >= 0) {
+      updateHeading(secondsRemaining);
+    }
+
+    if (secondsRemaining === 0) {
+      playAlarm();
+      clearInterval(timer);
+
+      // Optional extra task - flash the background
+      let flash = false;
+
+      setInterval(() => {
+        document.body.style.backgroundColor = flash ? "white" : "red";
+        flash = !flash;
+      }, 500);
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
