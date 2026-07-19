35e0652bfebc84c6c904c14c88f85feaa24f7df0
dona-shehu <shehu.donna@gmail.com>
2026-07-19 15:07:14 +0100

    Implement max function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,11 @@
 function findMax(elements) {
+  if (elements.length === 0) {
+    return -Infinity;
+  }
+  let filteredNumericValues = elements.filter((item) => Number.isFinite(item));
+  console.log(filteredNumericValues);
+
+  return Math.max(...filteredNumericValues);
 }
 
 module.exports = findMax;
