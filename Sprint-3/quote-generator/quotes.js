bfa2f25c040cf974a28ebde9d691f002f6cf0246
Alina Sofragiu <sofragiualina@gmail.com>
2026-07-19 12:57:13 +0100

    quotes generator

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -491,3 +491,17 @@ const quotes = [
 ];
 
 // call pickFromArray with the quotes array to check you get a random quote
+const quoteElement = document.querySelector("#quote");
+const authorElement = document.querySelector("#author");
+const newQuoteButton = document.querySelector("#new-quote");
+
+function displayRandomQuote() {
+  const randomQuote = pickFromArray(quotes);
+
+  quoteElement.innerText = `"${randomQuote.quote}"`;
+  authorElement.innerText = randomQuote.author;
+}
+
+displayRandomQuote();
+
+newQuoteButton.addEventListener("click", displayRandomQuote);
