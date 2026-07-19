59abaeff53b88ea7bf354f78a78dfe643f8f5103
Tomislav Dukez <tomdu3@ymail.com>
2026-07-19 07:39:00 +0100

    add querystring solution

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -6,11 +6,35 @@ function parseQueryString(queryString) {
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    if (!pair) continue;
+    const equalIndex = pair.indexOf("=");
+    if (equalIndex === -1) {
+      const key = pair;
+      queryParams[decodeURIComponent(key) ?? ""] = "";
+      continue;
+    }
+
+    let key = pair.substring(0, equalIndex).replace("+", " ");
+    let value = pair.substring(equalIndex + 1).replace("+", " ");
+
+    key = decodeURIComponent(key);
+    value = decodeURIComponent(value);
+    if (queryParams[key] !== undefined) {
+      if (Array.isArray(queryParams[key])) {
+        queryParams[key].push(value);
+      } else {
+        queryParams[key] = [queryParams[key], value];
+      }
+    } else {
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
 }
 
+console.log(parseQueryString("=value"));
+console.log(parseQueryString("key"));
+console.log(parseQueryString("key="));
+console.log(parseQueryString("="));
 module.exports = parseQueryString;
