d57add3962dd24e90393199b101197e4cb9ba53f
JorvanW <your.email@example.com>
2026-07-20 20:27:43 +0100

    added  code for max.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,8 @@
 function findMax(elements) {
+ elements = elements.filter(element => typeof element === 'number');
+      if (elements.length === 0){
+    return -Infinity;
+   }
+  return Math.max(...elements);
 }
-
 module.exports = findMax;
