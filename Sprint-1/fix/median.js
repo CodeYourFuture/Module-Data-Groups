3234e577da7aa96d9b3e88991acfb11f27f3bedc
russom <russom.g.meskel@gmail.cpm>
2026-07-21 20:17:07 +0100

    If statement for filtering through non numeric value implemented for the last set of test.

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,26 +6,21 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  //for (let i = 0; i < list.length; i++) {
-    //if (typeof list[i] !== "number" && typeof list[i] === " ") {
-      // List is checked whether it is numbers and strings.
-      //return null; // Null is returned if list is not numbers or has mixed values(numbers and strings)
-    //}
-   
-   if (!Array.isArray(list) || list.length === 0) {
+  if (!Array.isArray(list) || list.length === 0) {
     return null;
-   }
-
-  const newArray = [...list];
-  newArray.sort((a, b) => a - b);
-  const middleIndex = Math.floor(newArray.length / 2);
-  // const median = list.splice(middleIndex, 1)[0];
-  //return median;
-
-  if (newArray.length % 2 === 0) {
-    return (newArray[middleIndex - 1] + newArray[middleIndex]) / 2;
   }
-  return newArray[middleIndex];
+
+  const numbers = list.filter((item) => typeof item === "number");
+  if (numbers.length === 0) {
+    return null;
+  }
+
+  numbers.sort((a, b) => a - b);
+  const middleIndex = Math.floor(numbers.length / 2);
+
+  if (numbers.length % 2 === 0) {
+    return (numbers[middleIndex - 1] + numbers[middleIndex]) / 2;
+  }
+  return numbers[middleIndex];
 }
-console.log(calculateMedian([3, 1, 2]));
 module.exports = calculateMedian;
