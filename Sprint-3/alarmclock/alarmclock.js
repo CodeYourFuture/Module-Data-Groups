be8fbab1f213b81a45f05dd68c202e28a2064129
Sandani Kannangara <sandz.kannangara@gmail.com>
2026-07-21 20:48:41 +0100

    Implement alarm clock countdown functionality

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,31 @@
-function setAlarm() {}
+const alarmTime = document.querySelector("#alarmSet");
+const timeRemaining = document.querySelector("#timeRemaining");
+let remainingSeconds;
+
+function setAlarm() {
+  remainingSeconds = Number(alarmTime.value);
+  displayTimeRemaining();
+
+  let timer = setInterval(() => {
+    remainingSeconds--;
+    displayTimeRemaining();
+    if (remainingSeconds === 0) {
+      clearInterval(timer);
+      window.playAlarm();
+    }
+  }, 1000);
+}
+
+function displayTimeRemaining() {
+  let minutes = Math.floor(remainingSeconds / 60);
+  let seconds = remainingSeconds % 60;
+  const formatTime =
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
+
+  timeRemaining.textContent = `Time Remaining: ${formatTime}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
