5e42552367c58bbaeec441df456fa64627cf660b
Edina Kurdi <edina.kurdi@gmail.com>
2026-07-22 21:23:16 +0100

    refactor includes function

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,21 @@
 // Refactor the implementation of includes to use a for...of loop
 
+// function includes(list, target) {
+//   for (let index = 0; index < list.length; index++) {
+//     const element = list[index];
+//     if (element === target) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
+  for (element of list) {
     if (element === target) {
       return true;
     }
   }
   return false;
 }
-
 module.exports = includes;
