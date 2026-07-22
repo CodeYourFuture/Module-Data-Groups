d2cd254867f7e89f0816ebff8f39d2c5caa0fd05
Khaliun <khaliun13@hotmail.com>
2026-07-22 16:27:01 +0100

    Implement findMax and add tests

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,13 @@
 function findMax(elements) {
+  let max = -Infinity;
+
+  for (const element of elements) {
+    if (typeof element === "number" && element > max) {
+      max = element;
+    }
+  }
+
+  return max;
 }
 
 module.exports = findMax;
