452383ba0f8e12cd5c41d69efe2c3763e82e2b39
Tobias <amaechinatobias@gmail.com>
2026-07-22 16:40:14 +0100

    Wrote a test case to return an empty array

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,11 +16,20 @@ E.g. dedupe([1, 2, 1]) returns [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+//test.todo("given an empty array, it returns an empty array");
+const dedupe = require("./dedupe.js");
+
+describe("dedupe given an empty array, it returns an empty array ", () => {
+  [{ input: [], expected: [] }].forEach(({ input, expected }) =>
+    it(`returns the empty array for [${input}]`, () =>
+      expect(dedupe(input)).toEqual(expected))
+  );
+});
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
+
 
 // Given an array of strings or numbers
 // When passed to the dedupe function
