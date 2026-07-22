0010e7a3ec90303f81858831b0ea7d97c226bd86
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-22 19:00:58 +0100

    the change

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,11 @@
-function contains() {}
+function contains(obj, property) {
+  // Check that 'obj' is a valid, non-array object
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    return false;
+  }
+
+  // Object.hasOwn checks if the key directly exists on the object
+  return Object.hasOwn(obj, property);
+}
 
 module.exports = contains;
