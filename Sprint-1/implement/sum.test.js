93daaa086753d792c8a6c3ed3dee12e76948e0aa
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-22 19:35:25 +0100

    my change for sum -2

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,7 +13,7 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
+test.todo("given an empty array, returns 0");
 
 // Given an array with just one number
 // When passed to the sum function
@@ -34,3 +34,42 @@ test.todo("given an empty array, returns 0")
 // Given an array with only non-number values
 // When passed to the sum function
 // Then it should return the least surprising value given how it behaves for all other inputs
+
+describe("sum function", () => {
+  // Given an empty array
+  test("given an empty array, returns 0", () => {
+    expect(sum([])).toBe(0);
+  });
+
+  // Given an array with just one number
+  test("given an array with just one number, returns that number", () => {
+    expect(sum([42])).toBe(42);
+  });
+
+  // Given an array containing negative numbers
+  test("given an array containing negative numbers, returns the correct total sum", () => {
+    expect(sum([10, -5, 20, -15])).toBe(10);
+  });
+
+  // Given an array with decimal/float numbers
+  test("given an array with decimal/float numbers, returns the correct total sum", () => {
+    expect(sum([1.5, 2.25, 3.25])).toBe(7);
+  });
+
+  // Given an array containing non-number values
+  test("given an array containing non-number values, ignores non-numerical values", () => {
+    expect(sum(["hey", 10, "hi", 60, 10])).toBe(80);
+    expect(sum([10, true, null, undefined, NaN, 20])).toBe(30);
+  });
+
+  // Given an array with only non-number values
+  test("given an array with only non-number values, returns 0", () => {
+    expect(sum(["apple", "banana", true, null])).toBe(0);
+  });
+
+  // Edge case: Non-array inputs
+  test("given non-array inputs, returns 0", () => {
+    expect(sum(null)).toBe(0);
+    expect(sum("not an array")).toBe(0);
+  });
+});
