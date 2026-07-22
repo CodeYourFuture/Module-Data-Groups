cf98ba8c8d33f88c067c965ddb6fb9fc3ce0f439
Edina Kurdi <edina.kurdi@gmail.com>
2026-07-22 21:13:42 +0100

    implement deduplicating function

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,17 @@
-function dedupe() {}
+function dedupe(array) {
+  //create a duplicate,to avoid changing the array
+  const copiedArray = Array.from(array);
+
+  //Initialise an empty array (to collect non-duplicates in)
+  const dedupedArray = [];
+
+  //create a for loop to collect non-duplicates
+  for (element of copiedArray) {
+    if (!dedupedArray.includes(element)) {
+      dedupedArray.push(element);
+    }
+  }
+  return dedupedArray;
+}
+
+module.exports = dedupe;
