ab5d98d26d31e1d3bfb9ed81452351aabc629383
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-23 00:59:28 +0100

    my commit for query_1

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,54 @@
 function parseQueryString(queryString) {
-  const queryParams = {};
-  if (queryString.length === 0) {
-    return queryParams;
-  }
-  const keyValuePairs = queryString.split("&");
+  const result = {};
 
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+  if (!queryString) return result;
+  if (queryString.startsWith("?")) {
+    queryString = queryString.slice(1);
   }
 
-  return queryParams;
+  const decode = (str) => {
+    try {
+      return decodeURIComponent(str.replace(/\+/g, " "));
+    } catch {
+      return str;
+    }
+  };
+
+  const pairs = queryString.split("&");
+
+  for (const pair of pairs) {
+    if (!pair) continue;
+
+    const equalIndex = pair.indexOf("=");
+    let rawKey, rawValue;
+
+    if (equalIndex === -1) {
+      rawKey = pair;
+      rawValue = "";
+    } else {
+      rawKey = pair.slice(0, equalIndex);
+      rawValue = pair.slice(equalIndex + 1);
+    }
+
+    const key = decode(rawKey);
+    const value = decode(rawValue);
+
+    // Safeguard against prototype pollution while setting keys safely
+    if (!Object.hasOwn(result, key)) {
+      Object.defineProperty(result, key, {
+        value: value,
+        writable: true,
+        enumerable: true,
+        configurable: true,
+      });
+    } else if (Array.isArray(result[key])) {
+      result[key].push(value);
+    } else {
+      result[key] = [result[key], value];
+    }
+  }
+
+  return result;
 }
 
 module.exports = parseQueryString;
