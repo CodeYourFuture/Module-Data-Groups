3cd77220fd662ea07e8b8c300bf6c9a5f054f90e
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-07-22 20:11:32 +0100

    Fix recipe output formatting in console log
    Updated console log to display ingredients correctly.

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -13,3 +13,19 @@ const recipe = {
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
 ${recipe}`);
+
+//Prediction:  
+//output will be, [object Object]. Because recipe is an object, when js converts objects to a string (template literals can only insert strings) it calls on toString() method, which 
+//by default returns [Object Object]. We can fix this code by using join(\n) method. 
+
+//fixed code:
+
+const recipe = {
+  title: "bruschetta",
+  serves: 2,
+  ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
+};
+
+console.log(`${recipe.title} serves ${recipe.serves}
+Ingredients:
+${recipe.ingredients.join("\n")}`);
