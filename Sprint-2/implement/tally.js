ef554d37e92bb6bad92cc2b918a9eddc31b30c93
Zadri Abdule <zadriabdule@gmail.com>
2026-07-22 21:00:12 +0100

    implement tally and add tests

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,16 @@
-function tally() {}
+function tally(items) {
+	if (!Array.isArray(items)) {
+		throw new Error('tally expects an array');
+	}
+
+	const counts = {};
+	for (const item of items) {
+		// Use the item as the key; this will stringify non-string keys
+		const key = String(item);
+		counts[key] = (counts[key] || 0) + 1;
+	}
+
+	return counts;
+}
 
 module.exports = tally;
