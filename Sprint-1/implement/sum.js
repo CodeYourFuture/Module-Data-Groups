3773eb140fee33b960cc2a7404258fbc680dcf35
JorvanW <your.email@example.com>
2026-07-22 14:31:42 +0100

    Updated code of sum.js  to return null if there are only non-numeric inputs only.

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,8 +1,11 @@
 function sum(elements) {
   elements = elements.filter(element => typeof element === 'number');
-  let total = 0;
-
-  for (let element of elements) {
+   if (elements.length === 0) {
+    return null;}
+  
+    let total = 0;
+  
+    for (let element of elements) {
     total += element;
     // += means to add to the current value and assign as result
     // elements are individual items inside a collection (not just string)
