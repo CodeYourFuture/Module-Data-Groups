d165c4f3148d699fcacfc82a3b90bbd505d0825f
Edina Kurdi <edina.kurdi@gmail.com>
2026-07-22 19:50:28 +0100

    implement function to find the maximum value

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,10 @@
 function findMax(elements) {
+  // filter out non-number items from elements
+  elements = elements.filter((element) => typeof element === "number");
+
+  // find maximum
+  const max = Math.max(...elements);
+  return max;
 }
 
 module.exports = findMax;
