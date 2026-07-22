f075a6e9186a7a82928a5a5994b1909125dcb2f7
russom <russom.g.meskel@gmail.cpm>
2026-07-22 20:56:19 +0100

    Function implemented to check for duplicate elements in a given array.

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,5 +1,13 @@
 function dedupe(arr) {
-  if (arr.length === 0) return arr;
+  const elements = [];
+  for (const item of arr) {
+    if (!elements.includes(item)) {
+      elements.push(item);
+    }
+  }
+  return elements;
 }
+console.log(dedupe([1, 2, 2, 3, 4, 4, 5, 6]));
 
 module.exports = dedupe;
+
