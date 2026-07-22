d336151275b8810b61cbf446e1f3f40141c1bec4
russom <russom.g.meskel@gmail.cpm>
2026-07-22 20:33:52 +0100

    Test for mixed numbers and strings created.

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -36,8 +36,8 @@ test("given non duplicate array, it returns a copy of the array", function () {
 // When passed  to the dedupe function
 // Then it should return a new array with duplicates removed while preserving the 
 // first occurrence of each element from the original array.
-test("given an empty array, it returns an empty array", function () {
-  const array = [1, 2, 3];
+test("given an array with, it returns an empty array", function () {
+  const array = [1, c, 2, 3, d];
   const result = dedupe(array);
   expect(result()).toEqual(array);
 });
