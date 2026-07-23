9681bc630ec7b09e265f0a55b5b00da4ab90e1b6
Tobias <amaechinatobias@gmail.com>
2026-07-23 12:57:45 +0100

    implement the findMax function to filter non-numeric data

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,7 +1,11 @@
 function findMax(elements) {
+    // filter out the numeric numbers 
+const numbers = elements.filter(
+  (item) => typeof item === "number" && !Number.isNaN(item)
+);
 
-    const maxiNum = Math.max(...elements);
-    return maxiNum;
+const maxiNum = Math.max(...numbers);
+return maxiNum;
 
 }
 
