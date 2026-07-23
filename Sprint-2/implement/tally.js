a0bf22b11f5b71cfb86c8d87294c5f3b429f6afc
dona-shehu <shehu.donna@gmail.com>
2026-07-23 17:21:26 +0100

    Implemented tally()

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,14 @@
-function tally() {}
+function tally(items) {
+  if (!Array.isArray(items)) {
+    throw new Error("Invalid input");
+  }
+  const itemsObj = {};
+  for (const item of items) {
+    if (itemsObj[item] === undefined) {
+      itemsObj[item] = 1;
+    } else itemsObj[item]++;
+  }
+  return itemsObj;
+}
 
 module.exports = tally;
