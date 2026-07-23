1a697cc6847882f2e5ba69aa0a8d96bc5992e9e0
russom <russom.g.meskel@gmail.cpm>
2026-07-23 22:39:22 +0100

    function implemented for checking calculating sum of numeric array

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,12 @@
 function sum(elements) {
+    let sum = 0;
+    for (let i = 0; i < elements.length; i++) {
+        sum += elements[i];
+    }
+    return sum
 }
+
+let numbers = ["c", 3, 4, "hi", 7];
+console.log(sum(numbers));
 
 module.exports = sum;
