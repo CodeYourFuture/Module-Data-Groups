71ed164d4bea8699ce59163529d7bc57cf471736
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-07-23 22:42:25 +0100

    Enhance parseQueryString to handle empty pairs

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,43 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (queryString.length === 0) {
+
+  if (queryString === "") {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    // Ignore empty pairs
+    if (pair === "") continue;
+
+   
+    const equalsIndex = pair.indexOf("=");
+
+    let key, value;
+
+    if (equalsIndex === -1) {
+      key = pair;
+      value = "";
+    } else {
+      key = pair.slice(0, equalsIndex);
+      value = pair.slice(equalsIndex + 1);
+    }
+
+    
+    key = decodeURIComponent(key.replace(/\+/g, " "));
+    value = decodeURIComponent(value.replace(/\+/g, " "));
+
+    
+    if (queryParams.hasOwnProperty(key)) {
+      if (Array.isArray(queryParams[key])) {
+        queryParams[key].push(value);
+      } else {
+        queryParams[key] = [queryParams[key], value];
+      }
+    } else {
+      queryParams[key] = value;
+    }
   }
 
   return queryParams;
