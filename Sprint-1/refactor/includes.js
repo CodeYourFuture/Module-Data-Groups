4d15a8d1b66942bdf1d2657cbca46c5bbcd2bc71
Tobias <amaechinatobias@gmail.com>
2026-07-23 16:53:47 +0100

    Refactored the Implementing function includes to still check the test accordingly

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,12 +1,12 @@
 // Refactor the implementation of includes to use a for...of loop
 
-function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
+function includes(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
       return true;
     }
   }
+
   return false;
 }
 
