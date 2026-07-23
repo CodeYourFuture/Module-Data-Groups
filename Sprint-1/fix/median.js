3ac6be3d1d7cb98e4b531175aea8b0ebbd57a6a5
d-odumosu <dt.odumosu@gmail.com>
2026-07-23 22:11:47 +0100

    wrote the function to make the tests pas in median.js

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -5,10 +5,31 @@
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
+
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+    if (!Array.isArray(list)) {
+        return null
+    }
+    const arrCopy = [...list]
+    const filteredNumbers = arrCopy.filter((num => Number.isFinite(num)));
+
+    if (filteredNumbers.length === 0) {
+        return null
+    }
+
+    const sortedNumbers = filteredNumbers.sort((a, b) => a - b);
+    let middleIndex
+    if (sortedNumbers.length % 2 === 0) {
+        middleIndex = sortedNumbers.length / 2
+        const firstMiddleIndex = sortedNumbers[middleIndex - 1];
+        const secondMiddleIndex = sortedNumbers[middleIndex];
+        return (firstMiddleIndex + secondMiddleIndex) / 2
+    } else {
+        middleIndex = Math.floor(sortedNumbers.length / 2);
+        return sortedNumbers[middleIndex];
+    }
 }
+
+// console.log(calculateMedian([100, 2, 3, 'f', 4, 9, 9, 0, "hey", 'g']))
 
 module.exports = calculateMedian;
