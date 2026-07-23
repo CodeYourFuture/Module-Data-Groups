c2dd7bacc022fab3a31263979728860988cd59dd
Tobias <amaechinatobias@gmail.com>
2026-07-23 16:22:14 +0100

    updated the function Implementation to pass the  test of when non- numerical value and numerical values as pass as array into the fn

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,6 +1,8 @@
 function sum(elements) {
-    const addNum = elements.reduce((item, n) => item + n, 0);
+      const onlyNumbers = elements.filter(elem=>typeof elem==="number"&&!Number.isNaN(elem));
+    const addNum = onlyNumbers.reduce((item,n)=>item +n,0);
     return Number(addNum.toFixed(10));
 }
+
 
 module.exports = sum;
