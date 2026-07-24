d8493efd6ffa9d4c493fd772d852f30a827ac898
Tobias <amaechinatobias@gmail.com>
2026-07-24 12:23:02 +0100

    made my  prediction as to what will happen when the program runs and the explanations

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+/* my prediction is that  this program will print the title and how many it services but not the ingrientes as there is no dot notation in the recipe expression so the properties are never accessed */
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
