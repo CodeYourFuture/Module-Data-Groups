101957dea87fc0f3f687dfe2fab91411142fe602
Tomislav Dukez <tomdu3@ymail.com>
2026-07-24 17:28:29 +0100

    add integration of deadline in main

--- Sprint-3/todo-list/script.mjs
+++ Sprint-3/todo-list/script.mjs
@@ -29,13 +29,16 @@ function deleteCompletedTodos() {
 // append a new task to the todo list.
 function addNewTodo() {
   const taskInput = document.getElementById("new-task-input");
+  const deadlineInput = document.getElementById("new-task-deadline");
   const task = taskInput.value.trim();
+  const deadline = deadlineInput.value || null;
   if (task) {
-    Todos.addTask(todos, task, false);
+    Todos.addTask(todos, task, false, deadline);
     render();
   }
 
   taskInput.value = "";
+  deadlineInput.value = "";
 }
 
 // Note:
@@ -66,6 +69,15 @@ function createListItem(todo, index) {
   const li = todoListItemTemplate.cloneNode(true); // true => Do a deep copy of the node
 
   li.querySelector(".description").textContent = todo.task;
+
+  const deadlineBadge = li.querySelector(".deadline-badge");
+  const deadlineDate = li.querySelector(".deadline-date");
+  if (todo.deadline) {
+    deadlineDate.textContent = `Deadline: ${todo.deadline}`;
+  } else {
+    deadlineBadge.remove();
+  }
+
   if (todo.completed) {
     li.classList.add("completed");
   }
