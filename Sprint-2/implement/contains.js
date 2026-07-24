0ace00d6e50acb082fa4dcbc0e47e12035693249
Tobias <amaechinatobias@gmail.com>
2026-07-24 14:35:04 +0100

    implements a function to pass the test

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,10 @@
-function contains() {}
+
+function contains(obj, target) {
+      for (const key in obj) {
+        if (key === target) return true;
+      }
+      return false;
+    }
+
 
 module.exports = contains;
