5a946c6560961974084a719533a570c92d0c414b
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-07-24 08:48:04 +0100

    Fix invert function implementation and add tests
    Implemented the invert function to swap keys and values of an object and added tests to verify its functionality.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -18,12 +18,61 @@ function invert(obj) {
 
 // a) What is the current return value when invert is called with { a : 1 }
 
+{ key: 1 }
+
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
+
+{ key: 2 }
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
 
+{ 1: "a", 2: "b" }
+
 // c) What does Object.entries return? Why is it needed in this program?
+
+//Object.entries() returns an array of an object's key-value pairs, and it is needed in this program
+//so the for...of loop can access both each key and its corresponding value to swap them.
 
 // d) Explain why the current return value is different from the target output
 
+//It is different because invertObj.key creates a property literally calley "key". If we want to use the value of 
+//a variable as the property name, (invertedObj[value] = key) must be used instead.
+
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
+
+function invert(obj) {
+  const invertedObj = {};
+
+  for (const [key, value] of Object.entries(obj)) {
+    invertedObj[value] = key;
+  }
+
+  return invertedObj;
+}
+
+
+
+describe("invert", () => {
+  test("inverts an empty object", () => {
+    expect(invert({})).toEqual({});
+  });
+
+  test("inverts a single property", () => {
+    expect(invert({ a: 1 })).toEqual({ 1: "a" });
+  });
+
+  test("inverts multiple properties", () => {
+    expect(invert({ a: 1, b: 2 })).toEqual({
+      1: "a",
+      2: "b",
+    });
+  });
+
+  test("works with other values", () => {
+    expect(invert({ x: 10, y: 20 })).toEqual({
+      10: "x",
+      20: "y",
+    });
+  });
+});
+
