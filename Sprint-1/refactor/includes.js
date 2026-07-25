5fdea5848c2cbb8cd7ee9545c8c73098db805ced
d-odumosu <dt.odumosu@gmail.com>
2026-07-25 15:21:18 +0100

    refactored this file from using array method .includes to using a for of loop. all tests pass.

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,21 @@
 // Refactor the implementation of includes to use a for...of loop
 
+//my comments
+//make a copy of the array
+//check if target is in the array
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
-      return true;
+  const targetFoundInArr = [];
+  for (const li of list) {
+    if (li === target) {
+      targetFoundInArr.push(li);
     }
   }
-  return false;
+  if (targetFoundInArr.length === 0) {
+    return false;
+  }
+  if (targetFoundInArr.length >= 1) {
+    return true;
+  }
 }
-
+console.log(includes([1, 2, 3, 4, 55, 6, null, 6, 7], 6));
 module.exports = includes;
