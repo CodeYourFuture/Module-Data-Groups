e46277c29c33fd320af637e6f68b61bf2839cd8e
eyob tech <eyobfitsum07@gmail.com>
2026-07-25 16:12:10 +0100

    Implement findMax and add tests for empty, single, negative, decimal, and non-numeric arrays

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,7 @@
-function findMax(elements) {
+function findMax(list) {
+  const numbers = list.filter((item) => typeof item === "number" && !Number.isNaN(item));
+
+  return numbers.reduce((max, current) => (current > max ? current : max), -Infinity);
 }
 
 module.exports = findMax;
