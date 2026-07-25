79e9066325fb4eca6350411c081ca21a76ada7aa
eyob tech <eyobfitsum07@gmail.com>
2026-07-25 16:21:42 +0100

    Implement sum and add tests for empty, single, negative, decimal, and non-numeric arrays

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -1,36 +1,25 @@
-/* Sum the numbers in an array
-
-In this kata, you will need to implement a function that sums the numerical elements of an array
-
-E.g. sum([10, 20, 30]), target output: 60
-E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical elements)
-*/
-
 const sum = require("./sum.js");
 
-// Acceptance Criteria:
+test("given an empty array, returns 0", () => {
+  expect(sum([])).toEqual(0);
+});
 
-// Given an empty array
-// When passed to the sum function
-// Then it should return 0
-test.todo("given an empty array, returns 0")
+test("given an array with just one number, returns that number", () => {
+  expect(sum([7])).toEqual(7);
+});
 
-// Given an array with just one number
-// When passed to the sum function
-// Then it should return that number
+test("given an array containing negative numbers, returns the correct total sum", () => {
+  expect(sum([10, -5, 3])).toEqual(8);
+});
 
-// Given an array containing negative numbers
-// When passed to the sum function
-// Then it should still return the correct total sum
+test("given an array with decimal numbers, returns the correct total sum", () => {
+  expect(sum([1.5, 2.25, 0.25])).toEqual(4);
+});
 
-// Given an array with decimal/float numbers
-// When passed to the sum function
-// Then it should return the correct total sum
+test("given an array containing non-number values, ignores them and sums the rest", () => {
+  expect(sum(["hey", 10, "hi", 60, 10])).toEqual(80);
+});
 
-// Given an array containing non-number values
-// When passed to the sum function
-// Then it should ignore the non-numerical values and return the sum of the numerical elements
-
-// Given an array with only non-number values
-// When passed to the sum function
-// Then it should return the least surprising value given how it behaves for all other inputs
+test("given an array with only non-number values, returns 0", () => {
+  expect(sum(["hey", "hi", "hello"])).toEqual(0);
+});
