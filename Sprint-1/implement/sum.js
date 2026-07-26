7d6ad3c4642d6ee9378cd4393da80af0bf39c98d
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-07-26 12:32:52 +0100

    Refactor sum function to filter non-numeric values and ensure correct summation

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,6 @@
 function sum(elements) {
+  const numbers = elements.filter((item) => typeof item === "number");
+  return numbers.reduce((total, current) => total + current, 0);
 }
 
 module.exports = sum;
