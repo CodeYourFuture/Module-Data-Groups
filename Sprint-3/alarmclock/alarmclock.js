c225fb22daf8eff257faeab9c58db5b3992cb73d
dona-shehu <shehu.donna@gmail.com>
2026-07-26 14:29:16 +0100

    Implement setAlarm()

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,35 @@
-function setAlarm() {}
+let remainingSeconds = 0;
+let intervalId;
+function setAlarm() {
+  clearInterval(intervalId);
+
+  const initialSeconds = Number(document.getElementById("alarmSet").value);
+  remainingSeconds = initialSeconds;
+
+  let minutesDisplay = Math.floor(initialSeconds / 60)
+    .toString()
+    .padStart(2, "0");
+  let secondsDisplay = Math.floor(initialSeconds % 60)
+    .toString()
+    .padStart(2, "0");
+  let timeRemainingHeading = document.getElementById("timeRemaining");
+  timeRemainingHeading.innerText = `Time Remaining: ${minutesDisplay}:${secondsDisplay}`;
+
+  intervalId = setInterval(() => {
+    remainingSeconds--;
+    let currentMinutes = Math.floor(remainingSeconds / 60)
+      .toString()
+      .padStart(2, "0");
+    let currentSeconds = Math.floor(remainingSeconds % 60)
+      .toString()
+      .padStart(2, "0");
+    timeRemainingHeading.innerText = `Time Remaining: ${currentMinutes}:${currentSeconds}`;
+    if (remainingSeconds === 0) {
+      clearInterval(intervalId);
+      playAlarm();
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
