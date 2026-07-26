71029b2f8f0488b645387e423073b0e3e56a73a1
Zadri Abdule <zadriabdule@gmail.com>
2026-07-26 16:49:36 +0100

    add implemention for count-words.js

--- Sprint-2/stretch/count-words.js
+++ Sprint-2/stretch/count-words.js
@@ -26,3 +26,15 @@
 
 3. Order the results to find out which word is the most common in the input
 */
+function countWords(str) {
+  if (typeof str !== 'string') throw new Error('countWords expect a string');
+
+  const cleaned = str.toLowerCase().replace(/[^a-z0-9'\s]+/gi, " ").trim();
+  if (cleaned === '') return 0;
+
+  return cleaned.split(/\s+/).length;
+
+
+}
+
+module.exports = countWords;
