64b9210b50417b72c5d27d24670d1d7aa061ffb9
d-odumosu <dt.odumosu@gmail.com>
2026-07-26 13:38:28 +0100

    Implemented the find maximum function and all tests pass.

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,23 @@
 function findMax(elements) {
+  if (elements.length === 0) {
+    return -Infinity;
+  }
+  const elementLists = elements.filter((number) => {
+    return typeof number === "number";
+  });
+  if (elementLists.length === 0) {
+    return undefined;
+  }
+  if (elementLists.length === 1) {
+    return elementLists[0];
+  }
+  let max = elementLists[0];
+  for (let i = 1; i < elementLists.length; i++) {
+    if (elementLists[i] > max) {
+      max = elementLists[i];
+    }
+  }
+  return max;
 }
-
+console.log(findMax([-1, 0, -2]));
 module.exports = findMax;
