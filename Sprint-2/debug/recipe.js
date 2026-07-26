ffa88babf7fc72de29a1468a335ab8d948bb5c97
Dagim-Daniel <dagimdan19@gmail.com>
2026-07-26 18:55:20 +0100

    Revert "Sprint 2 debug exercises are done"
    This reverts commit f6cb741a432fc7f9b367deae2ed2e21f7df3ff5e.

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,9 +1,5 @@
 // Predict and explain first...
-/*
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);// here it misses recipe.ingredients 
-    */
+
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
 // How can you fix it?
@@ -16,4 +12,4 @@ const recipe = {
 
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
-${recipe.ingredients.join("\n")}`); // this make the list to be logged on a new line - not predicted at first
+${recipe}`);
