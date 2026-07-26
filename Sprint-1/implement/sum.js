f1f59acf50cdfa81e9f8be497997b4027d14cac2
d-odumosu <dt.odumosu@gmail.com>
2026-07-26 14:32:10 +0100

    Implemented the sum function and all tests pass.

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,17 @@
 function sum(elements) {
+  if (elements.length === 0) {
+    return 0;
+  }
+  const elementsList = elements.filter((element) => {
+    return typeof element === "number";
+  });
+  if (elementsList.length === 1) {
+    return elementsList[0];
+  }
+  let sum = 0;
+  for (let i = 0; i < elementsList.length; i++) {
+    sum += elementsList[i];
+  }
+  return sum;
 }
-
 module.exports = sum;
