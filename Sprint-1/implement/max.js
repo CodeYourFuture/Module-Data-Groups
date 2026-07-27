d612becf816316a1388b04eb088a9999c678f7ff
dona-shehu <shehu.donna@gmail.com>
2026-07-27 21:04:38 +0100

    Updated the function using for loop.

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -2,10 +2,22 @@ function findMax(elements) {
   if (elements.length === 0) {
     return -Infinity;
   }
-  let filteredNumericValues = elements.filter((item) => Number.isFinite(item));
-  console.log(filteredNumericValues);
 
-  return Math.max(...filteredNumericValues);
+  let filteredNumericValues = elements.filter((item) => Number.isFinite(item));
+
+  if (filteredNumericValues.length === 0) {
+    return -Infinity;
+  }
+
+  let maximum = filteredNumericValues[0];
+  for (const item of filteredNumericValues) {
+    if (item > maximum) {
+      maximum = item;
+    }
+  }
+
+  // return Math.max(...filteredNumericValues);
+  return maximum;
 }
 
 module.exports = findMax;
