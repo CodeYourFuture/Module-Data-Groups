6ef60024064f1cf1de04ea8edec8378632097909
eyob tech <eyobfitsum07@gmail.com>
2026-07-27 23:27:16 +0100

    Implement setAlarm() with countdown timer and fix template literal syntax error

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,29 @@
-function setAlarm() {}
+function setAlarm() {
+  const input = document.getElementById("alarmSet");
+  const heading = document.getElementById("timeRemaining");
+
+  let secondsRemaining = Number(input.value);
+
+  function updateDisplay() {
+    const minutes = Math.floor(secondsRemaining / 60);
+    const seconds = secondsRemaining % 60;
+    const paddedMinutes = String(minutes).padStart(2, "0");
+    const paddedSeconds = String(seconds).padStart(2, "0");
+    heading.innerText = `Time Remaining: ${paddedMinutes}:${paddedSeconds}`;
+  }
+
+  updateDisplay();
+
+  const intervalId = setInterval(() => {
+    secondsRemaining--;
+    updateDisplay();
+
+    if (secondsRemaining <= 0) {
+      clearInterval(intervalId);
+      playAlarm();
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
