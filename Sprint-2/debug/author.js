b6eb718b95dff3358d675695f52844148f7a48f3
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-07-27 23:17:10 +0100

    Fix TypeError in author.js by using Object.values() to iterate over object properties

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,5 +1,11 @@
 // Predict and explain first...
 
+/* Prediction - TypeError: author is not iterable.
+
+Explaination - The "for... of" loop is designed fir iterable objects like arrays, strings, maps or sets,
+which have built in protocols. plain javascript objects like "author" are not iterable by default, 
+which would cause the error when the "for... of" loop is used. */
+
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
@@ -11,6 +17,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.Values(author)) {
   console.log(value);
 }
