bb247fd85a200cb06047f8fe029766a8c81c3e7d
ChinweP <exponentialresults1973@gmail.com>
2026-07-27 17:05:14 +0100

    Implement findMax and add full test coverage for all input scenarios

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,9 @@
 function findMax(elements) {
+  const numbers = elements.filter((item) => typeof item === "number");
+  if (numbers.length === 0) {
+    return -Infinity;
+  }
+  return Math.max(...numbers);
 }
 
 module.exports = findMax;
