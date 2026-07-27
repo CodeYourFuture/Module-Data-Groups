4ea0c563bb64b436c7650b508f41637fb9c9f20b
Tobias <amaechinatobias@gmail.com>
2026-07-27 14:06:48 +0100

    replace const with let to allow for reassigment in the for .. of loop , removed the comme and changed the correct variables

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,14 +1,36 @@
 function parseQueryString(queryString) {
   const queryParams = {};
-  if (!queryString) {
-    return queryParams;
-  }
-  const keyValuePairs = queryString.split("&").filter(item=>item!=='');
+  if (!queryString) return queryParams;
 
-  for (const pair of keyValuePairs) {
-     pair = pair.replace(/\+/g,' '),// replace any occurrences of plus sign with spaces 
+  const keyValuePairs = queryString.split("&").filter((item) => item !== "");
 
-     let 
+  for (let pair of keyValuePairs) {
+    // Replace '+' with spaces
+    pair = pair.replace(/\+/g, " ");
+
+    const eqIdx = pair.indexOf("=");
+
+    let keyPair, valuePair;
+
+    if (eqIdx === -1) {
+      keyPair = pair;
+      valuePair = "";
+    } else {
+      keyPair = pair.slice(0, eqIdx);
+      valuePair = pair.slice(eqIdx + 1);
+    }
+
+    const key = decodeURIComponent(keyPair);
+    const value = decodeURIComponent(valuePair);
+
+    // Handle duplicates
+    if (!(key in queryParams)) {
+      queryParams[key] = value;
+    } else if (Array.isArray(queryParams[key])) {
+      queryParams[key].push(value);
+    } else {
+      queryParams[key] = [queryParams[key], value];
+    }
   }
 
   return queryParams;
