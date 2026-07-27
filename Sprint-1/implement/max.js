8283c106b67cca1fa9dd9712f9a030ace7f0f598
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-27 18:16:37 +0100

    commit using the loop function instead of the build in max function

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -2,13 +2,17 @@ function findMax(elements) {
   if (!Array.isArray(elements) || elements.length === 0) {
     return -Infinity;
   }
-  const numbersOnly = elements.filter(
-    (item) => typeof item === "number" && !isNaN(item)
-  );
-  if (numbersOnly.length === 0) {
-    return -Infinity;
+  let max = -Infinity;
+
+  for (const item of elements) {
+    if (typeof item === "number" && !Number.isNaN(item)) {
+      if (item > max) {
+        max = item;
+      }
+    }
   }
-  return Math.max(...numbersOnly);
+
+  return max;
 }
 
 module.exports = findMax;
