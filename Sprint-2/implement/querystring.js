1e8578b3e4fdff5eb5fac0656ffba27597d123e2
Tobias <amaechinatobias@gmail.com>
2026-07-27 13:55:23 +0100

    separated the key, value at the first occurance of  "="

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -6,7 +6,9 @@ function parseQueryString(queryString) {
   const keyValuePairs = queryString.split("&").filter(item=>item!=='');
 
   for (const pair of keyValuePairs) {
-     pair = pair.replace(/\+/g,'')
+     pair = pair.replace(/\+/g,' '),// replace any occurrences of plus sign with spaces 
+
+     let 
   }
 
   return queryParams;
