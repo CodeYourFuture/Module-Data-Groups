9256b7f5bf8b60cc4f65bdceab3e80d26282a18a
vmoratti <vmoratti@gmail.com>
2026-07-27 13:11:03 +0100

    debug the code

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -3,13 +3,18 @@
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
-const author = {
-  firstName: "Zadie",
-  lastName: "Smith",
-  occupation: "writer",
-  age: 40,
-  alive: true,
-};
+// I think the issue with the original code is it was trying to loop over the object directly,
+//  which is not iterable. Instead, we should loop over the array that contains the object.
+
+const author = [
+  {
+    firstName: "Zadie",
+    lastName: "Smith",
+    occupation: "writer",
+    age: 40,
+    alive: true,
+  },
+];
 
 for (const value of author) {
   console.log(value);
