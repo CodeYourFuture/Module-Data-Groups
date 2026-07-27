488225543172081a4eba9e02801cd2f59732d46c
B3-Bisrat <bisratttt1919@gmail.com>
2026-07-27 21:46:21 +0100

    Implement function for alarm clock.

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,37 @@
-function setAlarm() {}
+let timer;
+function setAlarm() {
+  const input = document.getElementById("alarmSet");
+  let seconds = Number(input.value);
+
+  const timeRemaining = document.getElementById("timeRemaining");
+
+  clearInterval(timer);
+
+  function updateDisplay() {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedTime =
+      String(minutes).padStart(2, "0") +
+      ":" +
+      String(remainingSeconds).padStart(2, "0");
+
+    timeRemaining.textContent = `Time Remaining: ${formattedTime}`;
+  }
+
+  updateDisplay();
+
+  timer = setInterval(() => {
+    if (seconds > 0) {
+      seconds--;
+      updateDisplay();
+    } else {
+      clearInterval(timer);
+      playAlarm();
+      document.body.style.backgroundColor = "red";
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
