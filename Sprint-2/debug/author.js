ea0a850c92450034c9a9bde9859f9cb06cbdf995
Dagim-Daniel <dagimdan19@gmail.com>
2026-07-27 15:31:45 +0100

    all tasks under sprint 2  debug section  has been done

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,8 +1,8 @@
 // Predict and explain first...
-
+//i think its because value is not used as a parameter of an object, and author is an object not an ordered list Array.
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
-
+// i found that object literals are not iterative because they have no orders
 const author = {
   firstName: "Zadie",
   lastName: "Smith",
@@ -11,6 +11,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
