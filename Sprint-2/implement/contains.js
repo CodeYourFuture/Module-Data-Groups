c7406b2bd48d4b0e5f6c5c3a4a47457bc02f7f32
TTiamiyu <tiamiyutolu@rocketmail.com>
2026-07-27 23:37:51 +0100

    Implement contains function to check for object properties

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains() {
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    return false;
+  }
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
 
 module.exports = contains;
