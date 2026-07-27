16e8e897c9c6ff34bda5eecfe9e7c1ec0bd484bc
B3-Bisrat <bisratttt1919@gmail.com>
2026-07-27 21:10:28 +0100

    Quote-generator is done

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -491,3 +491,18 @@ const quotes = [
 ];
 
 // call pickFromArray with the quotes array to check you get a random quote
+
+const quoteElement = document.getElementById("quote");
+const authorElement = document.getElementById("author");
+const button = document.getElementById("new-quote");
+
+function showRandomQuote() {
+  const randomQuote = pickFromArray(quotes);
+
+  quoteElement.textContent = randomQuote.quote;
+  authorElement.textContent = randomQuote.author;
+}
+
+showRandomQuote();
+
+button.addEventListener("click", showRandomQuote);
