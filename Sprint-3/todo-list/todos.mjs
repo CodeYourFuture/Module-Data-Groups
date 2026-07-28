3c084ca90f6eeb44f5aef44b4bf6091d04eb72ab
Tobias <amaechinatobias@gmail.com>
2026-07-28 16:03:44 +0100

    implement the function deleteCompleted to handle the deletion of mass completed task

--- Sprint-3/todo-list/todos.mjs
+++ Sprint-3/todo-list/todos.mjs
@@ -27,3 +27,6 @@ export function toggleCompletedOnTask(todos, taskIndex) {
     todos[taskIndex].completed = !todos[taskIndex].completed;
   }
 }
+export function deleteCompleted(todos) {
+  return todos.filter((todo) => !todo.completed);
+}
