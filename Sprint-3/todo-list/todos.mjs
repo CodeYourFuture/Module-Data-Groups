902445b3e3e9beaaa4e63ba8f0e5734d1f3f5f75
Tobias <amaechinatobias@gmail.com>
2026-07-28 16:58:17 +0100

    extended the addtask function to include deadline

--- Sprint-3/todo-list/todos.mjs
+++ Sprint-3/todo-list/todos.mjs
@@ -10,9 +10,10 @@
 */
 
 // Append a new task to todos[]
-export function addTask(todos, task, completed = false) {
-  todos.push({ task, completed });
+export function addTask(todos, task, completed = false, deadline = null) {
+  todos.push({ task, completed, deadline });
 }
+
 
 // Delete todos[taskIndex] if it exists
 export function deleteTask(todos, taskIndex) {
