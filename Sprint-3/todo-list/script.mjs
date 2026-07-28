9455547c47d590cbbad7955a3b95bd2a371e4bc4
Tobias <amaechinatobias@gmail.com>
2026-07-28 16:09:27 +0100

    Add a click listener to the window button in script.mjs file

--- Sprint-3/todo-list/script.mjs
+++ Sprint-3/todo-list/script.mjs
@@ -7,6 +7,9 @@ const todos = [];
 // Set up tasks to be performed once on page load
 window.addEventListener("load", () => {
   document.getElementById("add-task-btn").addEventListener("click", addNewTodo);
+  document.getElementById("delete-completed-btn")
+        .addEventListener("click", deleteCompletedTodos);
+
 
   // Populate sample data
   Todos.addTask(todos, "Wash the dishes", false); 
