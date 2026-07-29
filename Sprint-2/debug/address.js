013e56da657df0d240c163b6bc1dbd7a7ffb40b0
cywong <zenobiatony@gmail.com>
2026-07-29 15:22:15 +0100

    Update address.js

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -12,4 +12,9 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
+// console.log(`My house number is ${address[0]}`);
+
+// The original syntax works for array and not for object.
+// it will show undefine message
+// Instead use the correct . operator to access the object field values
