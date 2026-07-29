b79d1b695eee0b8900a6f1f111ac83537eb42772
Ogbemi mene <your.meneogbemi42@gmail.com>
2026-07-29 09:48:15 +0100

    commit for quote app

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -491,3 +491,20 @@ const quotes = [
 ];
 
 // call pickFromArray with the quotes array to check you get a random quote
+function displayRandomQuote() {
+  const quoteP = document.getElementById("quote");
+  const authorP = document.getElementById("author");
+
+  const selectedQuote = pickFromArray(quotes);
+
+  quoteP.textContent = selectedQuote.quote;
+  authorP.textContent = selectedQuote.author;
+}
+
+// Setup event listener and display initial quote on load
+window.addEventListener("DOMContentLoaded", () => {
+  displayRandomQuote();
+
+  const newQuoteBtn = document.getElementById("new-quote");
+  newQuoteBtn.addEventListener("click", displayRandomQuote);
+});
