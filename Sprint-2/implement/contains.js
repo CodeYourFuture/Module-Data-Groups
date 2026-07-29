16db858debcd46138397cbf9719200a000dc81db
d-odumosu <dt.odumosu@gmail.com>
2026-07-29 14:04:25 +0100

    completed all requirements for contains and written tests to pass.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,12 @@
-function contains() {}
-
+function contains(object, property) {
+  if (Array.isArray(object)) {
+    throw new Error("Expected an object, received an array");
+  }
+  const obj = Object.keys(object);
+  if (obj.length === 0) {
+    return false;
+  }
+  return obj.includes(property);
+}
+console.log(contains({ 1: "o", 2: "k" }, 1));
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -8,7 +8,7 @@ E.g. contains({a: 1, b: 2}, 'a') // returns true
 as the object contains a key of 'a'
 
 E.g. contains({a: 1, b: 2}, 'c') // returns false
-as the object doesn't contains a key of 'c'
+as the object doesn't contain a key of 'c'
 */
 
 // Acceptance criteria:
@@ -17,19 +17,65 @@ as the object doesn't contains a key of 'c'
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
 
+test(
+  "when passed an object and a property name, should return true if the object " +
+    "contains the property",
+  () => {
+    const obj1 = {
+      name: "dami",
+      age: 34,
+      height: 5.3,
+      location: "London",
+    };
+    const propertyNameCheck1 = "name";
+    const expected1 = true;
+    const result1 = contains(obj1, propertyNameCheck1);
+    expect(result1).toBe(expected1);
+  }
+);
+
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
 
+test("given an empty object, should return false", () => {
+  const obj2 = {};
+  const propertyNameCheck2 = "a";
+  const expected2 = false;
+  const result2 = contains(obj2, propertyNameCheck2);
+  expect(result2).toBe(expected2);
+});
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+//this test requirement is covered on line 21
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test(
+  "when passed an object and a property name, should return false if the object " +
+    "does not contain the property.",
+  () => {
+    const obj3 = {
+      title: "Things fall apart",
+      year: 1958,
+      publisher: "pan-macmillan",
+    };
+    const propertyNameCheck3 = "location";
+    const expected3 = false;
+    const result3 = contains(obj3, propertyNameCheck3);
+    expect(result3).toBe(expected3);
+  }
+);
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("when passed a data type other than an object, throw an error", () => {
+  const arr = [];
+  const propertyNameCheck4 = "name";
+  expect(() => {
+    contains(arr, propertyNameCheck4);
+  }).toThrow("Expected an object, received an array");
+});
