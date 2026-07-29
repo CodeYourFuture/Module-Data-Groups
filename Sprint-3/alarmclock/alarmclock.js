3c5b7b97627e473b9262d1a6e45bcd9a9b2fb241
ChinweP <exponentialresults1973@gmail.com>
2026-07-29 23:13:30 +0100

    Complete alarm clock countdown and flashing background

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,38 @@
-function setAlarm() {}
+function setAlarm() {
+  const input = document.getElementById("alarmSet");
+  const heading = document.getElementById("timeRemaining");
+
+  let remainingSeconds = Number(input.value);
+
+  function formatTime(totalSeconds) {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    const mm = String(minutes).padStart(2, "0");
+    const ss = String(seconds).padStart(2, "0");
+
+    return `${mm}:${ss}`;
+  }
+
+  heading.innerText = `Time Remaining: ${formatTime(remainingSeconds)}`;
+
+  const intervalId = setInterval(() => {
+    remainingSeconds -= 1;
+
+    if (remainingSeconds > 0) {
+      heading.innerText = `Time Remaining: ${formatTime(remainingSeconds)}`;
+    } else {
+      heading.innerText = "Time Remaining: 00:00";
+      clearInterval(intervalId);
+      playAlarm();
+      document.body.classList.add("flash");
+    }
+  }, 1000);
+}
+
+function stopFlashingBackground() {
+  document.body.classList.remove("flash");
+}
 
 // DO NOT EDIT BELOW HERE
 
