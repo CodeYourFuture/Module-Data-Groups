33c5b4266bddef5960fedf457a718c9cd5d892dc
abduhasen <abdumussa1363@gmail.com>
2026-07-29 08:59:23 +0100

    wrote a function for the alarm

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,6 +1,26 @@
-function setAlarm() {}
-
-// DO NOT EDIT BELOW HERE
+let myInterval;
+function setAlarm() {
+  clearInterval(myInterval);
+  const userInput = document.querySelector("#alarmSet").value;
+  let timeRemaining = Number(userInput);
+  if (isNaN(timeRemaining) || timeRemaining <= 0) {
+    alert("Please enter a valid number.");
+    return;
+  }
+  myInterval = setInterval(() => {
+    timeRemaining -= 1;
+    const minutes = Math.floor(timeRemaining / 60);
+    const seconds = timeRemaining % 60;
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(seconds).padStart(2, "0");
+    const heading = document.querySelector("#timeRemaining");
+    heading.innerText = `Time Remaining:${formattedMinutes}:${formattedSeconds}`;
+    if (timeRemaining === 0) {
+      playAlarm();
+      clearInterval(myInterval);
+    }
+  }, 1000);
+}
 
 var audio = new Audio("alarmsound.mp3");
 
