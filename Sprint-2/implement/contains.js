4c6a9e4fe30a609363b829c2d4c081f5b25555ad
Francesco Romano Monda <fr.monda@gmail.com>
2026-07-29 21:50:23 +0100

    Implement contains and lookup functions

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,13 @@
-function contains() {}
+function contains(obj, property) {
+     if (
+    obj === null ||
+    typeof obj !== "object" ||
+    Array.isArray(obj)
+  ) {
+    return false;
+}
+}
+
+return property in obj;
 
 module.exports = contains;
