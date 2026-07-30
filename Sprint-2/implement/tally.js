ec2b1d080a4b5324d5a7563e7caee4f6f8d9dc98
d-odumosu <dt.odumosu@gmail.com>
2026-07-30 19:18:42 +0100

    completed all requirements of tally

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,13 @@
-function tally() {}
-
+function tally(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('Invalid data type entered"');
+  }
+  if (arr.length === 0) {
+    return {};
+  }
+  return arr.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
+}
 module.exports = tally;
