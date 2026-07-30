989dc411d50b5a1e48226983c6cf629e85570530
Edina Kurdi <edina.kurdi@gmail.com>
2026-07-30 15:48:41 +0100

    implement function to find the max without using a built in method

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -2,8 +2,17 @@ function findMax(elements) {
   // filter out non-number items from elements
   elements = elements.filter((element) => typeof element === "number");
 
+  if (elements.length === 0) {
+    return -Infinity;
+  }
+
   // find maximum
-  const max = Math.max(...elements);
+  let max = elements[0];
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i] > max) {
+      max = elements[i];
+    }
+  }
   return max;
 }
 
