7633409301c34820d88c917179366fdff229a645
eyob tech <eyobfitsum07@gmail.com>
2026-07-30 13:35:12 +0100

    Clear existing interval before starting a new one on repeated clicks

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,30 +1,37 @@
+let currentIntervalId = null;
+
 function setAlarm() {
   const input = document.getElementById("alarmSet");
   const heading = document.getElementById("timeRemaining");
 
   let secondsRemaining = Number(input.value);
 
-if (!secondsRemaining || secondsRemaining <= 0) {
-  heading.innerText = "Please enter a number of seconds greater than 0";
-  return;
-}
+  if (!secondsRemaining || secondsRemaining <= 0) {
+    heading.innerText = "Please enter a number of seconds greater than 0";
+    return;
+  }
 
   function updateDisplay() {
     const minutes = Math.floor(secondsRemaining / 60);
     const seconds = secondsRemaining % 60;
     const paddedMinutes = String(minutes).padStart(2, "0");
     const paddedSeconds = String(seconds).padStart(2, "0");
-    heading.innerText = `Time Remaining: ${paddedMinutes}:${paddedSeconds}`;
+    heading.innerText = Time Remaining: ${paddedMinutes}:${paddedSeconds};
   }
 
   updateDisplay();
 
-  const intervalId = setInterval(() => {
+  if (currentIntervalId !== null) {
+    clearInterval(currentIntervalId);
+  }
+
+  currentIntervalId = setInterval(() => {
     secondsRemaining--;
     updateDisplay();
 
     if (secondsRemaining <= 0) {
-      clearInterval(intervalId);
+      clearInterval(currentIntervalId);
+      currentIntervalId = null;
       playAlarm();
     }
   }, 1000);
@@ -53,3 +60,4 @@ function pauseAlarm() {
 }
 
 window.onload = setup;
+
