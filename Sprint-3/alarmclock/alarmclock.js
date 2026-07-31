ee6a0e5fd71f915a39b76feb9ea275c4d377f88c
Francesco Romano Monda <fr.monda@gmail.com>
2026-07-31 19:52:17 +0100

    Implement alarm countdown functionality

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,43 @@
-function setAlarm() {}
+let timer;
+
+function setAlarm() {
+  let input = document.getElementById("alarmSet");
+  let heading = document.getElementById("timeRemaining");
+
+  let timeRemaining = parseInt(input.value);
+
+  let minutes = Math.floor(timeRemaining / 60);
+  let seconds = timeRemaining % 60;
+
+  heading.innerText =
+    "Time Remaining: " +
+    String(minutes).padStart(2, "0") +
+    ":" +
+    String(seconds).padStart(2, "0");
+
+  clearInterval(timer);
+
+  function countdown() {
+    timeRemaining = timeRemaining - 1;
+
+    let minutes = Math.floor(timeRemaining / 60);
+    let seconds = timeRemaining % 60;
+
+    heading.innerText =
+      "Time Remaining: " +
+      String(minutes).padStart(2, "0") +
+      ":" +
+      String(seconds).padStart(2, "0");
+
+    if (timeRemaining <= 0) {
+      clearInterval(timer);
+      document.body.style.backgroundColor = "red";
+      playAlarm();
+    }
+  }
+
+  timer = setInterval(countdown, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
