3a1925a480744b66f4270196167a5128652e899c
Vitalii Kmit <vitalikkmita@gmail.com>
2026-08-01 08:59:16 +0100

    Set up looping alarm

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,30 @@
-function setAlarm() {}
+const oneSecondInMilliseconds = 1000;
+const lengthOfAlarmSound = 20000;
+
+function setAlarm() {
+  // set up time
+  let now = Date.now();
+  let timeLeft = document.getElementById("alarmSet").value;
+
+  // validate result
+  const result = parseInt(timeLeft);
+  if (isNaN(result)) {
+    return;
+  }
+
+  setTimeout(alarmFinished, timeLeft * oneSecondInMilliseconds);
+  audio.loop = true;
+  playAlarm();
+}
+
+function stopAlarm() {
+  pauseAlarm();
+}
+
+function alarmFinished() {
+  console.log("Alarm finished");
+  stopAlarm();
+}
 
 // DO NOT EDIT BELOW HERE
 
