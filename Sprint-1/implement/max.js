0f21769c0354ed52721a64dd873f75d2da57203a
Monsur Abdulrahman <monsuromotunde.abdulrahman@gmail.com>
2026-08-01 22:54:17 +0100

    Finding the max value

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,14 @@
 function findMax(elements) {
+    // Filtering out non-numeric values    
+    const numericElements = elements.filter(element => typeof element === 'number' && !isNaN(element));
+
+    // If there are no numeric values, return -Infinity
+    if (numericElements.length === 0) {
+        return -Infinity;
+    }
+
+    // Using Math.max to find the maximum value in the numeric elements
+    return Math.max(...numericElements);    
 }
 
 module.exports = findMax;
