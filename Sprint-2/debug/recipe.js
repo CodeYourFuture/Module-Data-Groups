99e811b47f1d33534e1e22255cb6a7251132d5fe
ChinweP <exponentialresults1973@gmail.com>
2026-08-01 14:24:16 +0100

    Fix revert files for Sprint 2

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -4,7 +4,7 @@
 // Each ingredient should be logged on a new line
 // How can you fix it?
 
-/*const recipe = {
+const recipe = {
   title: "bruschetta",
   serves: 2,
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
@@ -13,26 +13,3 @@
 console.log(`${recipe.title} serves ${recipe.serves}
   ingredients:
 ${recipe}`);
-*/
-
-// Prediction and Explanation
-/* 
-The line inside the template string: ${recipe} does not print the ingredients. 
-Instead, JavaScript converts the entire recipe object into a string, which becomes "[object Object]". This happens 
-because plain objects {} are not automatically converted into readable text. The program should print each ingredient on a new line, but the current code 
-never loops through recipe.ingredients, so nothing is printed correctly.
-*/
-
-// Corrected code
-const recipe = {
-  title: "bruschetta",
-  serves: 2,
-  ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
-};
-
-console.log(`${recipe.title} serves ${recipe.serves}
-ingredients:`);
-
-for (const ingredient of recipe.ingredients) {
-  console.log(ingredient);
-}
