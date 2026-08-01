1b55921d064995cf50e77e6709db91be31d277cf
eyob tech <eyobfitsum07@gmail.com>
2026-08-01 16:32:33 +0100

    Implement remaining tally test cases: no duplicates, invalid input, and empty array

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -1,7 +1,7 @@
 const tally = require("./tally.js");
 
 /**
- * tally array
+ * tally array.
  *
  * In this task, you'll need to implement a function called tally
  * that will take a list of items and count the frequency of each item
@@ -23,13 +23,24 @@ const tally = require("./tally.js");
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
+
 test("tally on an empty array returns an empty object", () => {
   expect(tally([])).toEqual({});
 });
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+test("tally on an array with duplicate items returns counts for each unique item", () => {
+  expect(tally(["a", "a", "b", "c"])).toEqual({ a: 2, b: 1, c: 1 });
+});
+
+test("tally on an array with no duplicates returns a count of 1 for each item", () => {
+  expect(tally(["a", "b", "c"])).toEqual({ a: 1, b: 1, c: 1 });
+});
 
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("tally on an invalid input like a string throws an error", () => {
+  expect(() => tally("abc")).toThrow();
+});
