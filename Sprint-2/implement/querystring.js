78041d6228ba22ca5dc00f9d8c5f5038862cddfd
vmoratti <vmoratti@gmail.com>
2026-08-02 18:24:17 +0100

    write complete function to meet the test requirements

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -3,11 +3,31 @@ function parseQueryString(queryString) {
   if (queryString.length === 0) {
     return queryParams;
   }
+  queryString = queryString.replace(/\+/g, " ");
   const keyValuePairs = queryString.split("&");
-
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    if (pair === "") {
+      continue;
+    }
+    let key;
+    let value;
+    const index = pair.indexOf("=");
+    if (index === -1) {
+      key = decodeURIComponent(pair);
+      value = "";
+    } else {
+      key = decodeURIComponent(pair.slice(0, index));
+      value = decodeURIComponent(pair.slice(index + 1));
+    }
+    if (!queryParams[key]) {
+      queryParams[key] = value;
+    } else {
+      if (Array.isArray(queryParams[key])) {
+        queryParams[key].push(value);
+      } else {
+        queryParams[key] = [queryParams[key], value];
+      }
+    }
   }
 
   return queryParams;
