6d8eefd2c004e51889fc5668d0efb75e8db69e90
vmoratti <vmoratti@gmail.com>
2026-08-02 21:35:42 +0100

    write complete function to taly

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,20 @@
-function tally() {}
+function tally(arr) {
+  let countOfItemsObj = {};
+  if (!Array.isArray(arr)) {
+    throw new Error("Invalid input");
+  } else if (arr.length < 1) {
+    return countOfItemsObj;
+  } else {
+    for (let element = 0; element < arr.length; element++) {
+      const exist = Object.hasOwn(countOfItemsObj, arr[element]);
+      if (!exist) {
+        countOfItemsObj[arr[element]] = 1;
+      } else {
+        countOfItemsObj[arr[element]] = countOfItemsObj[arr[element]] + 1;
+      }
+    }
+  }
+  return countOfItemsObj;
+}
 
 module.exports = tally;
