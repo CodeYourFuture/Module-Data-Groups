605f05904c1242043a8416ac2e90423e90ee8272
d-odumosu <dt.odumosu@gmail.com>
2026-08-02 20:23:54 +0100

    Implemented the set alarm funtion.

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,28 @@
-function setAlarm() {}
+const input = document.getElementById("alarmSet");
+const timeDisplay = document.querySelector("span");
+
+function setAlarm() {
+  let timeInput = Number(input.value);
+  let minutes = Math.floor(timeInput / 60);
+  let seconds = timeInput % 60;
+
+  minutes = minutes.toString().padStart(2, "0");
+  seconds = seconds.toString().padStart(2, "0");
+  timeDisplay.textContent = `${minutes}:${seconds}`;
+
+  const alarm = setInterval(() => {
+    timeInput--;
+    let remainingMinutes = Math.floor(timeInput / 60);
+    let remainingSeconds = timeInput % 60;
+    remainingMinutes = remainingMinutes.toString().padStart(2, "0");
+    remainingSeconds = remainingSeconds.toString().padStart(2, "0");
+    timeDisplay.textContent = `${remainingMinutes}:${remainingSeconds}`;
+    if (timeInput === 0) {
+      playAlarm();
+      clearInterval(alarm);
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
