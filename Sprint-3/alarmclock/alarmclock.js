8eae09967b599d1b6be76e98f4690ebf0dcf4dfd
Dipa-Sarker <dipasarker696@gmail.com>
2026-08-02 12:23:12 +0100

    commented for conditions

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -33,30 +33,32 @@ const alarmSoundElement = document.querySelector("#alarmSound");
 
 let remainingTime;
 let intervalID;
+
 function triggerAlarm() {
   bodyElement.style.backgroundColor = "yellow";
   alarmSoundElement.play();
 }
+
 function setAlarm() {
   clearInterval(intervalID);
   const alarmTime = alarmSetElement.value;
   if (alarmTime === "") {
     return;
-  }
+  } //input validation (for emptry input)
   remainingTime = Number(alarmTime);
   if (remainingTime < 0) {
     return;
-  }
+  } //input validation(for -ve number input)
   const showTimeMinutes = Math.floor(remainingTime / 60)
     .toString()
     .padStart(2, "0");
   const showTimeSeconds = (remainingTime % 60).toString().padStart(2, "0");
   timeRemainingElement.innerText = showTimeMinutes + ":" + showTimeSeconds;
   if (remainingTime === 0) {
-    triggerAlarm();
+    triggerAlarm(); //valid input, when input = 0 then immediately triggers alarm
   } else {
     intervalID = setInterval(countDown, 1000);
-  }
+  } //valid input, when input = 10 then countdown starts
 }
 setButtonElement.addEventListener("click", setAlarm);
 function countDown() {
@@ -64,7 +66,9 @@ function countDown() {
     clearInterval(intervalID);
     triggerAlarm();
     return;
-  }
+  } /*to avoid any chance of -ve counting after countdown reached 0 from 10. 
+   when countdown = 0 alarm triggers immediately and returns from the code */
+
   remainingTime -= 1;
   const remainingMinutes = Math.floor(remainingTime / 60)
     .toString()
@@ -72,6 +76,7 @@ function countDown() {
   const remainingSeconds = (remainingTime % 60).toString().padStart(2, "0");
   timeRemainingElement.innerText = remainingMinutes + ":" + remainingSeconds;
 }
+
 function stopAlarm() {
   clearInterval(intervalID);
   alarmSoundElement.pause();
