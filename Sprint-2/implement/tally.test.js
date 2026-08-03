720be29c6cd56359c9158fa4e3f9b844f87d576b
cywong <zenobiatony@gmail.com>
2026-08-03 13:58:49 +0100

    Update tally.test.js

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -19,16 +19,30 @@ const tally = require("./tally.js");
 // Given a function called tally
 // When passed an array of items
 // Then it should return an object containing the count for each unique item
+test("returns counts for each unique item", () => {
+    expect(tally(["a"])).toEqual({ a: 1 });
+    expect(tally(["a", "b", "c"])).toEqual({ a: 1, b: 1, c: 1 });
+  });
 
 // Given an empty array
 // When passed to tally
 // Then it should return an empty object
-test.todo("tally on an empty array returns an empty object");
+test("tally on an empty array returns an empty object", () => {
+    expect(tally([])).toEqual({});
+  });
 
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+test("returns counts for each unique item", () => {
+    expect(tally(["a", "a", "a"])).toEqual({ a: 3 });
+    expect(tally(["a", "a", "b", "c"])).toEqual({ a: 2, b: 1, c: 1 });
+  });
 
 // Given an invalid input like a string
 // When passed to tally
 // Then it should throw an error
+test("throws an error when passed an invalid input like a string", () => {
+    expect(() => tally("string")).toThrow(TypeError);
+    expect(() => tally("string")).toThrow("Expected an array as input");
+  });
