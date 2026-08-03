34394f6d70c9beea51f5df56a670b6a53aeb663e
Khaliun Baatarkhuu <khaliun13@hotmail.com>
2026-08-03 19:17:54 +0100

    Remove tests for invert function
    Removed unit tests for the invert function from invert.js.

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -50,29 +50,4 @@ function invert(obj) {
   return invertedObj;
 }
 
-
-
-describe("invert", () => {
-  test("inverts an empty object", () => {
-    expect(invert({})).toEqual({});
-  });
-
-  test("inverts a single property", () => {
-    expect(invert({ a: 1 })).toEqual({ 1: "a" });
-  });
-
-  test("inverts multiple properties", () => {
-    expect(invert({ a: 1, b: 2 })).toEqual({
-      1: "a",
-      2: "b",
-    });
-  });
-
-  test("works with other values", () => {
-    expect(invert({ x: 10, y: 20 })).toEqual({
-      10: "x",
-      20: "y",
-    });
-  });
-});
-
+module.exports = invert;
