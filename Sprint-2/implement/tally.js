03af0029717cd8b1cd64b0ec133630576feb679b
Tobias <amaechinatobias@gmail.com>
2026-08-03 14:58:49 +0100

    refactored the function tally

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,20 +1,12 @@
 function tally(items) {
-    if(!Array.isArray(items)){
-        throw new Error("Input must me and array"); 
-        // validate  and throw error if input is not an array . 
-    }
-     const frequency = {};
-     for (let item of items){
-       if (item in frequency){
-        frequency[item] +=1;
-       }else{
-         frequency[item] = 1
-       }
-      
-     }
-    
-    
-   return frequency;
-}
+  if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
 
+  const frequency = {};
+  for (const item of items) {
+    frequency[item] = (frequency[item] || 0) + 1;
+  }
+  return frequency;
+}
 module.exports = tally;
