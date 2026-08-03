eeb63cdf25d65b4a8266a281fc9b4f09706af5bf
Dagim-Daniel <dagimdan19@gmail.com>
2026-08-03 15:36:48 +0100

    cleaning code and fixing pre validation

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -5,7 +5,6 @@ const setButton = document.querySelector("#set");
 const stopButton = document.querySelector("#stop");
 let conuntdownInterval;
 
-// time management function
 function formatTime(totalSeconds) {
   const minutes = Math.floor(totalSeconds / 60);
   const seconds = totalSeconds % 60;
@@ -15,49 +14,50 @@ function formatTime(totalSeconds) {
   return `Time Remaining: ${paddedMinutes}:${paddedSeconds}`;
 }
 
-// to clear every change made with conditions
-function clearEverything() {
+function reset() {
+  clearInterval(conuntdownInterval);
+  display.textContent = "Time Remaining: 00:00";
   display.style.color = "black";
   setButton.disabled = false;
   document.body.style.background = "";
 }
-
-// button event handler
+let countdownInterval;
 setButton.addEventListener("click", () => {
   let timeleft = Number(totalSeconds.value);
-  setButton.disabled = true;
 
-  // time 0
-
-  if (timeleft <= 0) {
-    alert("please enter a number greater than 0!");
+  if (timeleft <= 0 || isNaN(timeleft)) {
+    alert("please enter a number greater than zero (0)!");
     return;
   }
+  setButton.disabled = true;
 
   display.textContent = formatTime(timeleft);
 
+  const warningTime = 10; // seconds
+  clearInterval(conuntdownInterval);
+
   conuntdownInterval = setInterval(() => {
-    if (timeleft == 0) {
+    timeleft--;
+
+    if (timeleft >= 0) {
+      display.textContent = formatTime(timeleft);
+    }
+
+    if (timeleft <= warningTime) {
+      display.style.color = "red";
+    }
+
+    if (timeleft <= 0) {
       clearInterval(conuntdownInterval);
       document.body.style.backgroundColor = "grey";
       playAlarm();
     }
-
-    if (timeleft <= 10) {
-      display.style.color = "red";
-    }
-
-    if (timeleft >= 0) {
-      display.textContent = formatTime(timeleft);
-      timeleft--;
-    }
   }, 1000);
 });
 
-//stop button clicked
 stopButton.addEventListener("click", () => {
   clearInterval(conuntdownInterval);
-  clearEverything();
+  reset();
   pauseAlarm();
 });
 
