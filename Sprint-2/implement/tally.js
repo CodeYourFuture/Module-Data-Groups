2d7c3ad688bf564eeaeefab5dfd4f328b29b2377
cywong <zenobiatony@gmail.com>
2026-08-03 13:54:52 +0100

    Implement tally function with input validation
    Added input validation and tallying functionality.

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,12 @@
-function tally() {}
+function tally(items) {
+    if (!Array.isArray(items)) {
+        throw new TypeError("Expected an array as input");
+    }
+
+    return items.reduce((acc, item) => {
+        acc[item] = (acc[item] || 0) + 1;
+        return acc;
+    }, {});
+}
 
 module.exports = tally;
