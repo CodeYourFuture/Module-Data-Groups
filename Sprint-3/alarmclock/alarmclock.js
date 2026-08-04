e0849c04b707b0c57e0905d7d0647b56e6e516ea
russom <russom.g.meskel@gmail.cpm>
2026-08-04 20:53:26 +0100

    Minutes and seconds declared. heading.innerText changed to include remaining minutes and seconds.

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,7 +1,15 @@
 function setAlarm() {
+  
   let time = document.getElementById("alarmSet");
+  
   let heading = document.getElementById("timeRemaining");
-
+  
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  
+  heading.innerText = "Time Remaining: " + 
+  String(minutes).padStart(2, "0") + ":" + 
+  String(seconds.padStart(2, "0"));
 }
 
 // DO NOT EDIT BELOW HERE
