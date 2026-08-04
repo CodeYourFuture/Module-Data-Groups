3ecea9e614ae40c211ac8321dc02d18d355bd697
mandip sanger <mandipsanger1@gmail.com>
2026-08-04 20:59:10 +0100

    max test

--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -11,34 +11,71 @@ We have set things up already so that this file can see your function from the o
 */
 
 const findMax = require("./max.js");
-findMax();
 
 // Given an empty array
 // When passed to the max function
 // Then it should return -Infinity
 // Delete this test.todo and replace it with a test.
-test.todo("given an empty array, returns -Infinity");
+
+test("returns -Infinity for an empty array", () => {
+  const currentOutput = max([]);
+  const targetOutput = -Infinity;
+
+  expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an array with one number
 // When passed to the max function
 // Then it should return that number
 
+test("returns with one number", () => {
+  const currentOutput = max([3]);
+  const targetOutput = 3;
+  expect(currentOutput).toEqual(targetOutput);
+});
+
 // Given an array with both positive and negative numbers
 // When passed to the max function
 // Then it should return the largest number overall
+test("returns largest number", () => {
+  const currentOutput = max([3, -10]);
+  const targetOutput = 3;
+  expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an array with just negative numbers
 // When passed to the max function
 // Then it should return the closest one to zero
+test("return closest to 0", () => {
+  const currentOutput = max([-7, -3, -10]);
+  const targetOutput = -3;
+  expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an array with decimal numbers
 // When passed to the max function
 // Then it should return the largest decimal number
+test("return to largest decimal number", () => {
+  const currentOutput = max([2.5, 5.6]);
+  const targetOutput = 5.6;
+  expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an array with non-number values
 // When passed to the max function
 // Then it should return the max and ignore non-numeric values
 
+test("return to max and ignore non-numeric values", () => {
+  const currentOutput = max([7, 4, -10], "b", "f");
+  const targetOutput = -7;
+  expect(currentOutput).toEqual(targetOutput);
+});
+
 // Given an array with only non-number values
 // When passed to the max function
 // Then it should return the least surprising value given how it behaves for all other inputs
+test("return the least surprising value", () => {
+  const currentOutput = max([NaN, "hi", true]);
+  const targetOutput = null;
+  expect(currentOutput).toEqual(targetOutput);
+});
