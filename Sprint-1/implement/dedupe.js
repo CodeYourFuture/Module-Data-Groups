1792362a496e8537a670aa0da58d322118fb4da4
mandip sanger <mandipsanger1@gmail.com>
2026-08-04 20:29:33 +0100

    dedupe

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,8 @@
-function dedupe() {}
+function dedupe(arr) {
+  return [...new Set(arr)];
+}
+
+module.exports = dedupe;
+
+console.log(dedupe(["a", "a", "a", "b", "b", "c"])); // ['a', 'b', 'c']
+console.log(dedupe([5, 1, 1, 2, 3, 2, 5, 8])); // [5, 1, 2, 3, 8]
