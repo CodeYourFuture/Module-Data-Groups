b91442c3ad5759847552046862ae55c4c81cd910
ChinweP <exponentialresults1973@gmail.com>
2026-08-04 16:07:49 +0100

    Remove placeholder test for empty array in max function tests

--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -15,8 +15,6 @@ const findMax = require("./max.js");
 // Given an empty array
 // When passed to the max function
 // Then it should return -Infinity
-// Delete this test.todo and replace it with a test.
-//test.todo("given an empty array, returns -Infinity");
 test("given an empty array, returns -Infinity", () => {
   expect(findMax([])).toBe(-Infinity);
 });
