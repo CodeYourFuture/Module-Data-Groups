c2e4f8f5a1f475d6c3ab2af9ea6458cac5852a32
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-08-05 01:22:57 +0100

    fixed the median function upholding with the test requirements

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,30 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  // validate the array 
+  if (!Array.isArray(list)){
+    return null;
+  }
+  // keep only the real number by filtering the array( excluding string, null, undefined )
+  const numbersOnly = list.filter(item => typeof item === 'number')
+  // after filtering return null if nothing is valid 
+  if (numbersOnly.length === 0){
+    return null ;
+  }
+  const sorted =[...numbersOnly].sort((a,b) => a-b)
+  const middleIndex = Math.floor(list.length / 2)
+
+  
+  if (sorted.length %2 == 0){
+    return (sorted[middleIndex-1] + sorted[middleIndex])/2;
+  }
+  else{
+    return sorted[middleIndex]
+  }
+  
+  
+  
+  
 }
 
 module.exports = calculateMedian;
