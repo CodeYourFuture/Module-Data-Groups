e8e1de0ca1c5d0487dde2960eb80c2a2b76a082a
mandip sanger <mandipsanger1@gmail.com>
2026-08-05 22:41:34 +0100

    too many errors replacing original

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,60 +16,69 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
- describe("contains", () => {
-  test("returns true if the object contains the property", () => {
-    expect(contains({ a: 1, b: 2 }, "a")).toBe(true);
-    expect(contains({ a: 1, b: 2 }, "b")).toBe(true);
-    
-  });
-  test("returns false when the object does not contain the property", () => {
-    expect(contains({ a: 1, b: 2 }, "c")).toBe(false);
-    expect(contains({ a: 1, b: 2 }, "d")).toBe(false);
-  });
 
 // Given an empty object
 // When passed to contains
 // Then it should return false
-  test("returns false for an empty object", () => {
-    expect(contains({}, "a")).toBe(false);
-  });
-});
-
-
-
-describe("contains", () => {
-  test("returns false if the object is missing", () => {
-    expect(contains({a: 1, b:2 })).toBe(false);
-  });
-
-  test("returns false for an empty object", () => {
-    expect(contains({}).toBe(false);
-  });
-});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
-test("returns true if the object contains the existing property name", () => {
-    expect(contains({ a: 1, b: 2 }, "a")).toBe(true);
-    expect(contains({ a: 1, b: 2 }, "b")).toBe(true);
-    expect(contains({ a: 1, b: 2 }, "c")).toBe(false);
-  });
-
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
 
-test("returns false when given an invalid object", () => {
-    expect(contains(null, "a")).toBe(false);
-    expect(contains(undefined, "a")).toBe(false);
-     });
-     
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
-test("returns false or throw an error when given an invalid object", () => {
-    expect(contains(null, "a")).toBe(false);
-    expect(contains(undefined, "a")).toBe(false);
-  });
+
+// Given an empty object
+// When passed to contains
+// Then it should return false
+test("returns false for an empty object", () => {
+  expect(contains({}, "a")).toEqual(false);
+});
+
+// Given an object with properties
+// When passed to contains with an existing property name
+// Then it should return true
+test("returns true when the property exists", () => {
+  expect(contains({ a: 1, b: 2 }, "a")).toEqual(true);
+  expect(contains({ a: 1, b: 2 }, "b")).toEqual(true);
+});
+
+// Given an object with properties
+// When passed to contains with a non-existent property name
+// Then it should return false
+test("returns false when the property does not exist", () => {
+  expect(contains({ a: 1, b: 2 }, "c")).toEqual(false);
+});
+
+// Given an object with several properties
+// When checking another existing property
+// Then it should return true
+test("returns true for another existing property", () => {
+  expect(contains({ name: "Alice", age: 25 }, "age")).toEqual(true);
+});
+
+// Given an array
+// When passed to contains
+// Then it should return false
+test("returns false when given an array", () => {
+  expect(contains([1, 2, 3], "0")).toEqual(false);
+});
+
+// Given a null value
+// When passed to contains
+// Then it should return false
+test("returns false when given null", () => {
+  expect(contains(null, "a")).toEqual(false);
+});
+
+// Given an undefined value
+// When passed to contains
+// Then it should return false
+test("returns false when given undefined", () => {
+  expect(contains(undefined, "a")).toEqual(false);
+});
