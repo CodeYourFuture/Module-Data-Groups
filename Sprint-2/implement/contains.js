e544c0fe2732aa97e73c88a2f5efde96c5fc1ef4
Rizqah <popoolarisquat@gmail.com>
2026-08-05 11:30:20 +0100

    used the built in method has own for  contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,9 +1,4 @@
 function contains(object, target) {
-  for (let index = 0; index < Object.keys(object).length; index++) {
-    if (Object.keys(object).includes(target)) {
-      return true;
-    }
-  }
-  return false;
+  return Object.hasOwn(object, target);
 }
 module.exports = contains;
