b23acdbc935a42fbaf1f28f570acc1167aa3aaf0
Rizqah <popoolarisquat@gmail.com>
2026-08-05 10:22:35 +0100

    dedupe nested loop removed

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -3,9 +3,11 @@ function dedupe(input) {
   if (input.length === 0) {
     return [];
   }
-  for (const i in input)
-    for (const j in input)
-      if (i !== j && !newArray.includes(input[i])) newArray.push(input[i]);
+  for (const item of input) {
+    if (!newArray.includes(item)) {
+      newArray.push(item);
+    }
+  }
   return newArray;
 }
 module.exports = dedupe;
