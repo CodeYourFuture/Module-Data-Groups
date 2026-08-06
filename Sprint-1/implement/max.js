01d183154150d8662003a8dd719b023432f694d5
mandip sanger <mandipsanger1@gmail.com>
2026-08-06 20:44:24 +0100

    implemented max

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,7 +1,13 @@
 function findMax(elements) {
-  console.log(elements);
-  console.log("Hello from Abdi");
-  console.log("Hello from Mandy");
+  const numbersOnly = elements.filter(
+    (item) => typeof item === "number" && !Number.isNaN(item)
+  );
+  if (numbersOnly.length === 0) {
+    return -Infinity;
+  }
+  const sorted = [...numbersOnly].sort((a, b) => a - b);
+  const max = sorted[sorted.length - 1];
+  return max;
 }
 
 module.exports = findMax;
