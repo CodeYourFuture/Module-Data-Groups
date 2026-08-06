f4a3df713ac9a54443a57b5d535ad4102dd0282a
Enice-Codes <mutandaeunice@94gmail.com>
2026-08-07 01:24:12 +0200

    all tasks modified

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,5 +1,6 @@
 function sum(arr) {
-  return arr
+  const list = Array.isArray(arr) ? arr : [arr];
+  return list
     .filter(item => typeof item === 'number' && !isNaN(item))
     .reduce((total, num) => total + num, 0);
 }
