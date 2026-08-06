56d823777b40bc72c7616d2408dc6cad4dea327e
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-08-06 16:26:11 +0100

    committing the max function built from the max.test cases

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,15 @@
 function findMax(elements) {
+    const numbersOnly = elements.filter((item)=> typeof item === 'number' && !Number.isNaN(item));
+
+    if (numbersOnly.length ===0 ){
+        return -Infinity 
+    }
+    const sorted = [...numbersOnly].sort((a,b) => a-b)
+    const max = sorted[sorted.length-1]
+
+    return max
+
+
 }
 
 module.exports = findMax;
