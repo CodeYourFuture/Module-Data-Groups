248a1e642ccb05aabc19532b92caa1836961b7c4
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2026-08-08 00:47:07 +0100

    implement contains function and tests

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,16 +20,18 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+  expect(contains({}, "a")).toEqual(false);
+});
 
-// Given an object with properties
-// When passed to contains with an existing property name
-// Then it should return true
+test("returns true when object has the property", () => {
+  expect(contains({ a: 1, b: 2 }, "a")).toEqual(true);
+});
 
-// Given an object with properties
-// When passed to contains with a non-existent property name
-// Then it should return false
+test("returns false when object does not have the property", () => {
+  expect(contains({ a: 1, b: 2 }, "c")).toEqual(false);
+});
 
-// Given invalid parameters like an array
-// When passed to contains
-// Then it should return false or throw an error
+test("returns false when passed an array", () => {
+  expect(contains([1, 2, 3], "0")).toEqual(false);
+});
