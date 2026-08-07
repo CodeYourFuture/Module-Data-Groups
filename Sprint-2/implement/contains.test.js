351dfc63ac68b4d1b8385cca27c9d7a2c750eee5
Enice-Codes <mutandaeunice@94gmail.com>
2026-08-07 14:50:12 +0200

    Complete contains project

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -1,4 +1,4 @@
-const contains = require("./contains.js");
+const contains = require("./contains")
 
 /*
 Implement a function called contains that checks an object contains a
@@ -16,20 +16,44 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
+test("contains returns true for existing property", () => {
+const currentOutput = contains({a:1,b:2}, 'a');
+const targetOutput = true;
+expect(currentOutput).toEqual(targetOutput);    
+});
 
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+const currentOutput = contains({}, 'a');
+const targetOutput = false;
+expect(currentOutput).toEqual(targetOutput);    
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test("contains returns true for existing property", () => {
+const currentOutput = contains({a:1,b:2}, 'b');
+const targetOutput = true;
+expect(currentOutput).toEqual(targetOutput);    
+}); 
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
 // Then it should return false
+test("contains returns false for non-existent property", () => {
+const currentOutput = contains({a:1,b:2}, 'c');
+const targetOutput = false;
+expect(currentOutput).toEqual(targetOutput);    
+});
 
 // Given invalid parameters like an array
 // When passed to contains
 // Then it should return false or throw an error
+test("contains returns false for invalid parameters", () => {
+const currentOutput = contains([1,2,3], 'a');
+const targetOutput = false;
+expect(currentOutput).toEqual(targetOutput);    
+});
