2dbe395b23f853e92edb18ad7aaea62479ef9e0b
vmoratti <vmoratti@gmail.com>
2026-08-09 14:05:10 +0100

    Fix author iteration by using Object.entries

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -6,16 +6,15 @@
 // I think the issue with the original code is it was trying to loop over the object directly,
 //  which is not iterable. Instead, we should loop over the array that contains the object.
 
-const author = [
+const author = 
   {
     firstName: "Zadie",
     lastName: "Smith",
     occupation: "writer",
     age: 40,
     alive: true,
-  },
-];
+  }
 
-for (const value of author) {
-  console.log(value);
+for (let [key, value] of Object.entries(author)) {
+    console.log(key, value);
 }
