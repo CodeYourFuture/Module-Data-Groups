47367c6547836a6300fed3c88c9559b584fcd4ad
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2026-08-10 20:35:44 +0100

    fix: resolve contains syntax and tally prototype bug

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,35 +1,13 @@
 const createLookup = require("./lookup.js");
 
-test.todo("creates a country currency code lookup for multiple codes");
+test("creates a country currency code lookup for multiple codes", () => {
+  expect(createLookup([["US", "USD"], ["CA", "CAD"]])).toEqual({ US: "USD", CA: "CAD" });
+});
 
-/*
+test("returns empty object for empty array", () => {
+  expect(createLookup([])).toEqual({});
+});
 
-Create a lookup object of key value pairs from an array of code pairs
-
-Acceptance Criteria:
-
-Given
- - An array of arrays representing country code and currency code pairs
-   e.g. [['US', 'USD'], ['CA', 'CAD']]
-
-When
- - createLookup function is called with the country-currency array as an argument
-
-Then
- - It should return an object where:
- - The keys are the country codes
- - The values are the corresponding currency codes
-
-Example
-Given: [['US', 'USD'], ['CA', 'CAD']]
-
-When
-createLookup(countryCurrencyPairs) is called
-
-Then
-It should return:
- {
-   'US': 'USD',
-   'CA': 'CAD'
- }
-*/
+test("creates lookup for a single pair", () => {
+  expect(createLookup([["GB", "GBP"]])).toEqual({ GB: "GBP" });
+});
