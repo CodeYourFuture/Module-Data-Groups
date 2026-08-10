34f16f3f24a6480fa9be12728221cda6e2783eaa
vmoratti <vmoratti@gmail.com>
2026-08-10 11:41:49 +0100

    Refactor contains function to improve type checks

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,7 +1,14 @@
-function contains(obj, prop) {
-    const notArray = !Array.isArray(obj);
-    if (obj === null || Array.isArray(obj) || obj === Number || obj === undefined) {
-        return false; 
-    }  
-  return obj.hasOwnProperty(prop);
+function contains(value, prop) {
+    const isObject = (value) => {
+        return typeof value === 'object'
+        && value !== null
+        && !Array.isArray(value)
+        && !(value instanceof RegExp)
+        && !(value instanceof Date)
+        && !(value instanceof Set)
+        && !(value instanceof Map)
+    }
+    if (!isObject(value)) {
+        return false;
+    } return value.hasOwnProperty(prop);
 }
