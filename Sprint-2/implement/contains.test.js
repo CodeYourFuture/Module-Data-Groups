fd5c9ca5e3d252143a51c7acb4729011e0fd80b8
Dagim-Daniel <dagimdan19@gmail.com>
2026-08-10 16:19:24 +0100

    removing the same test from contains.test.js .

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -8,10 +8,6 @@ test("Give a contains with empty object and  when passed to contains it returns 
   expect(contains({}, "y")).toEqual(false);
 });
 
-test("Give a contains with object and property and if the object contains the property returns true", () => {
-  expect(contains({ a: 1, b: 2 }, "a")).toEqual(true);
-});
-
 test("Give a contains with object and property and if the property is non-existent returns false", () => {
   expect(contains({ a: 1, b: 2 }, "z")).toEqual(false);
 });
