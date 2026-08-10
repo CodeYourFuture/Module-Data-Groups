fe018ee13a399d5179256a4a25de9c19288dee49
mandip sanger <mandipsanger1@gmail.com>
2026-08-10 20:04:38 +0100

    removing the test cases

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -3,6 +3,3 @@ function dedupe(arr) {
 }
 
 module.exports = dedupe;
-
-console.log(dedupe(["a", "a", "a", "b", "b", "c"])); // ['a', 'b', 'c']
-console.log(dedupe([5, 1, 1, 2, 3, 2, 5, 8])); // [5, 1, 2, 3, 8]
