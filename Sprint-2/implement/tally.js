2a62f584df9c397d6d64a5bf47bed55d6c9a1473
Edina Kurdi <edina.kurdi@gmail.com>
2026-08-11 10:17:10 +0100

    implement tally function

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,18 @@
-function tally() {}
+function tally(array) {
+  if (typeof array === "string") {
+    throw new Error("Input should be an array");
+  }
+
+  let tallySet = {};
+
+  for (let item of array) {
+    if (!tallySet[item]) {
+      tallySet[item] = 1;
+    } else {
+      tallySet[item] += 1;
+    }
+  }
+  return tallySet;
+}
 
 module.exports = tally;
