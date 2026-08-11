1e8685b3faf0546dde84a74e010d8f40bc28d170
Edina Kurdi <edina.kurdi@gmail.com>
2026-08-11 21:10:13 +0100

    add another test

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -2,9 +2,17 @@ const invert = require("./invert.js");
 // Given an object
 // When invert is passed this object
 // Then it should swap the keys and values in the object
+describe("invert()", () => {
+  describe("invert function", () => {
+    test("should swap the keys and values in the object", () => {
+      expect(invert({ a: 1 })).toEqual({ 1: "a" });
+      expect(invert({ x: 10, y: 20 })).toEqual({ 10: "x", 20: "y" });
+    });
+  });
 
-describe("invert function", () => {
-  test("should swap the keys and values in the object", () => {
-    expect(invert({ a: 1 })).toEqual({ 1: "a" });
+  describe("given an empty object is passed into the function", () => {
+    test("should return and empty object", () => {
+      expect(invert({})).toEqual({});
+    });
   });
 });
