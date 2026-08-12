5da7c38823cd40f681d6f3892ff8bd00341ea9f8
Edina Kurdi <edina.kurdi@gmail.com>
2026-08-12 22:17:05 +0100

    grab html elements

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -19,6 +19,9 @@
 function pickFromArray(choices) {
   return choices[Math.floor(Math.random() * choices.length)];
 }
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const quoteButton = document.getElementById("new-quote");
 
 // A list of quotes you can use in your app.
 // DO NOT modify this array, otherwise the tests may break!
