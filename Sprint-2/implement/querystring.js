edb970dbed28d81ccc0a2f8a77c11f90d06a3540
JorvanW <your.email@example.com>
2026-08-13 09:46:58 +0100

    removed console.log in code

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,33 +1,26 @@
-
 function parseQueryString(queryString) {
   const queryParams = {};
   if (queryString.length === 0) {
     return queryParams;
   }
   let keyValuePairs = queryString.split("&");
-  
-  
-  for (const pair of keyValuePairs) {
-  if (!pair) continue; // continue is to skip empty strings
-    let [key,...values] = pair.split("=");
-    
-  key = decodeURIComponent(key.replace(/\+/g, " "));
-  const value = decodeURIComponent(values.join("=").replace(/\+/g, " "));
 
-    
-  /* decodeURIComponent function decodes percent encoded characters 
+  for (const pair of keyValuePairs) {
+    if (!pair) continue; // continue is to skip empty strings
+    let [key, ...values] = pair.split("=");
+
+    key = decodeURIComponent(key.replace(/\+/g, " "));
+    const value = decodeURIComponent(values.join("=").replace(/\+/g, " "));
+
+    /* decodeURIComponent function decodes percent encoded characters 
      "replace" swaps one character with another 
      (/../) means the begining and end of a regex pattern (better for characters)
-     '\+' is an escaped '+' because it has its own function in coding  */ 
-    
-    queryParams[key] = value;
+     '\+' is an escaped '+' because it has its own function in coding  */
 
-    
-    console.log(pair)
+    queryParams[key] = value;
   }
 
   return queryParams;
 }
-
 
 module.exports = parseQueryString;
