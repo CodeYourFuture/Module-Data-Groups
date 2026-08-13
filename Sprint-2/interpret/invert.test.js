37deb3c5fca15fc42c1e1cbf51b5a28c740f09ec
JorvanW <your.email@example.com>
2026-08-13 10:47:10 +0100

    removed excess comments in invert.js. fixed the code and added more test cases for invert

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -1,5 +1,13 @@
 const invert = require("./invert.js");
 
-test("When invert is passed, keys and values  in the object should be swapped ",() => {
-  expect({x : 10, y : 20}).toEqual({x : 10, y : 20});
+test("when passed invert swaps single key-value pair", () => {
+  expect(invert({ a: 1 })).toEqual({ "1": "a" });
+});
+
+test("When invert is passed, multiple keys and values in the object should be swapped ", () => {
+  expect(invert({ x: 10, y: 20 })).toEqual({ "10": "x", "20": "y" })
+});
+
+test("when passed invert swaps string values", () => {
+  expect(invert({ first: "hello",second: "world" })).toEqual({ hello: "first", world: "second" });
 });
