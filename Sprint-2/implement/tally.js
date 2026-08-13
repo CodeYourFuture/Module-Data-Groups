a9c191f34c871c0a572451d46a4abefb573e992b
Sophia <zadriabdule@gmail.com>
2026-08-13 19:40:42 +0100

    Update tally.js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -2,14 +2,12 @@ function tally(items) {
 	if (!Array.isArray(items)) {
 		throw new Error('tally expects an array');
 	}
-
-	const counts = {};
+	const counts = Object.create(null);
 	for (const item of items) {
 		// Use the item as the key; this will stringify non-string keys
 		const key = String(item);
 		counts[key] = (counts[key] || 0) + 1;
 	}
-
 	return counts;
 }
 
