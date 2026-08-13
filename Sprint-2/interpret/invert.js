bdcc0fc3c35189b106ad6d3bd6a1f43c97618b67
JorvanW <your.email@example.com>
2026-08-13 11:12:59 +0100

    updated answers for invert.js

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -25,11 +25,11 @@ function invert(obj) {
 // {"1": "a", "2":"b"}
 
 // c) What does Object.entries return? Why is it needed in this program?
-// It returns an array of property into the object
+// Object Entries return an array  of key value pairs and it's needed so the for...of loop goes through each key and value individually
 
 // d) Explain why the current return value is different from the target output
 // The current return value only shows {key: 2}. It doesn't show the first key and value only the second,
-// and it doesn't specify the second key. Its just defined as 'key'.
+// and it doesn't specify the second key. Its just defined as 'key'. It also doesn't swap the key and value around as intended
 
 // e) Fix the implementation of invert (and write tests to prove it's fixed!)
 
