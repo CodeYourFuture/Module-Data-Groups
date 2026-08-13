d22745d5d0dde368fdfa4a86d485450c87486084
Edina Kurdi <edina.kurdi@gmail.com>
2026-08-13 12:25:19 +0100

    start implementing setAlarm()

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,11 @@
-function setAlarm() {}
+function setAlarm() {
+  let timeInput = document.getElementById("alarmSet");
+  // console.log(timeInput.value);
+  const timeRemaining = document.getElementById("timeRemaining");
+  const minutes = String(Math.floor(timeInput.value / 60)).padstart(2, "0");
+  const seconds = String(timeInput.value - minutes * 60).padStart(2, "0");
+  timeRemaining.textContent = `Time remaining: ${minutes}:${seconds}`;
+}
 
 // DO NOT EDIT BELOW HERE
 
