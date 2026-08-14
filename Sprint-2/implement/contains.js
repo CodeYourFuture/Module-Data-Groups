3266e73cc6e462e78fcb365d5555f68e181e76c7
Enice-Codes <mutandaeunice@94gmail.com>
2026-08-14 20:40:00 +0200

    remove Sprint-2 files not in scope for this Sprint-1 PR

--- Sprint-2/implement/contains.js
+++
@@ -1,6 +0,0 @@
-function contains(obj, prop) {
-  if (typeof obj !== "object" || obj === null) return false;
-  return Object.prototype.hasOwnProperty.call(obj, prop);
-}
-
-module.exports = contains;
--- Sprint-2/implement/lookup.js
+++
@@ -1,9 +0,0 @@
-function createLookup(pairs) {
-  const lookup = {};
-  for (const [countryCode, currencyCode] of pairs) {
-    lookup[countryCode] = currencyCode;
-  }
-  return lookup;
-}
-
-module.exports = createLookup;
