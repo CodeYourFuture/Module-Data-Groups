2e32afc6f747155f6baebffc7ca13dc81e8a9bab
Edina Kurdi <edina.kurdi@gmail.com>
2026-08-14 16:05:10 +0100

    implement time conversion function to be re-used in the function

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,3 +1,9 @@
+function timeConvert(sec) {
+  let minutes = String(Math.floor(sec / 60)).padStart(2, "0");
+  let seconds = String(sec % 60).padStart(2, "0");
+  return `${minutes}:${seconds}`;
+}
+
 function setAlarm() {
   const timeInput = document.getElementById("alarmSet");
   let remainingSeconds = parseInt(timeInput.value);
@@ -7,16 +13,14 @@ function setAlarm() {
   }
 
   const timeRemaining = document.getElementById("timeRemaining");
-  const minutes = String(Math.floor(remainingSeconds / 60)).padStart(2, "0");
-  const seconds = String(remainingSeconds - minutes * 60).padStart(2, "0");
 
-  timeRemaining.textContent = `Time Remaining: ${minutes}:${seconds}`;
+  timeRemaining.textContent = `Time Remaining: ${timeConvert(remainingSeconds)}`;
 
   const timer = setInterval(() => {
     remainingSeconds--;
     const minutes = String(Math.floor(remainingSeconds / 60)).padStart(2, "0");
     const seconds = String(remainingSeconds - minutes * 60).padStart(2, "0");
-    timeRemaining.textContent = `Time Remaining: ${minutes}:${seconds}`;
+    timeRemaining.textContent = `Time Remaining: ${timeConvert(remainingSeconds)}`;
     if (remainingSeconds === 0) {
       playAlarm();
       clearInterval(timer);
