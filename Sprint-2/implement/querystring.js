7cb9ffadece4ba5ae01ab6fb06f138e5fc7606f3
Vitalii Kmit <vitalikkmita@gmail.com>
2026-08-14 17:49:50 +0100

    Pass all tests on querystring.js

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,13 +1,36 @@
 function parseQueryString(queryString) {
   const queryParams = {};
+
+  // return if input is 0
   if (queryString.length === 0) {
     return queryParams;
   }
+
+  // replace all + by ' '
+  queryString = queryString.replaceAll("+", " ");
+
+  // split by &
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    if (pair.length > 0) {
+      const parts = pair.split("=");
+      // first part as the key
+      const key = decodeURIComponent(parts[0]);
+      // second to last parts as the value
+      const value = decodeURIComponent(parts.slice(1).join("="));
+
+      // if no key then assign new value
+      if (!(key in queryParams)) {
+        queryParams[key] = value;
+        // if array then push a new value to it
+      } else if (Array.isArray(queryParams[key])) {
+        queryParams[key].push(value);
+        // if not array then turn into array with old and new values
+      } else {
+        queryParams[key] = [queryParams[key], value];
+      }
+    }
   }
 
   return queryParams;
