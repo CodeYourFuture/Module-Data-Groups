8cd6d481fb7e3048c0322e14612cfd3c435b7251
mandip sanger <mandipsanger1@gmail.com>
2026-08-14 21:32:36 +0100

    alarmclock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,41 @@
-function setAlarm() {}
+let timer;
+
+function setAlarm() {
+  const input = document.getElementById("alarmSet");
+  const heading = document.getElementById("timeRemaining");
+
+  let timeRemaining = Number(input.value);
+
+  function formatTime(totalSeconds) {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    return `Time Remaining: ${String(minutes).padStart(2, "0")}:${String(
+      seconds
+    ).padStart(2, "0")}`;
+  }
+
+  // Show starting time
+  heading.innerText = formatTime(timeRemaining);
+
+  // Stop an existing timer
+  clearInterval(timer);
+
+  timer = setInterval(() => {
+    timeRemaining--;
+
+    if (timeRemaining <= 0) {
+      timeRemaining = 0;
+      heading.innerText = formatTime(timeRemaining);
+
+      clearInterval(timer);
+      playAlarm();
+      return;
+    }
+
+    heading.innerText = formatTime(timeRemaining);
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
