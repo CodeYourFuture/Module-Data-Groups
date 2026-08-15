58a5073ca11dd8e71c30166d3e5dfa4a44fedf1e
LiyemaMfengwana <mfengwanaliyema@gmail.com>
2026-08-15 21:15:10 +0200

    contain.js implement

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,19 @@
-function contains() {}
+function contains(toCheck, input) {
+  if (
+    typeof toCheck !== "object" ||
+    Array.isArray(toCheck) ||
+    toCheck === null
+  ) {
+    return false;
+  }
 
-module.exports = contains;
+  for (const key in toCheck) {
+    if (key === input) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+console.log(contains({ a: 1, b: 2 }, "a"));
