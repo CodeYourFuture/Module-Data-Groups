25e5affe6bc5d08212ecbd63a7e58470952a33b5
JorvanW <your.email@example.com>
2026-08-18 11:45:47 +0100

    added more test cases for lookup.test and test involving various currency pairs

--- Sprint-2/implement/lookup.test.js
+++ Sprint-2/implement/lookup.test.js
@@ -1,6 +1,14 @@
 const createLookup = require("./lookup.js");
 
-test("creates a country currency code lookup for multiple codes", () => {
+test("creates a lookup from one country currency pair", () => {
+  const countryCurrencyPairs = [["JP", "JPY"]];
+
+  expect(createLookup(countryCurrencyPairs)).toEqual({
+    JP: "JPY",
+  });
+});
+
+test("creates a lookup for multiple country currency pairs", () => {
   const countryCurrencyPairs = [
     ["US", "USD"],
     ["CA", "CAD"],
@@ -12,6 +20,28 @@ test("creates a country currency code lookup for multiple codes", () => {
     CA: "CAD",
     EN: "GBP",
   });
+});
+
+test("creates a lookup for a larger list of country currency pairs", () => {
+  const countryCurrencyPairs = [
+    ["FR", "EUR"],
+    ["AU", "AUD"],
+    ["CH", "CHF"],
+    ["MX", "MXN"],
+    ["SE", "SEK"],
+  ];
+
+  expect(createLookup(countryCurrencyPairs)).toEqual({
+    FR: "EUR",
+    AU: "AUD",
+    CH: "CHF",
+    MX: "MXN",
+    SE: "SEK",
+  });
+});
+
+test("creates an empty lookup when given an empty array", () => {
+  expect(createLookup([])).toEqual({});
 });
 
 /*
