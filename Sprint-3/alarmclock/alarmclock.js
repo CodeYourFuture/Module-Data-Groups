f848caee9e6b0951221381698f46369a0b9c276a
mandip sanger <mandipsanger1@gmail.com>
2026-08-18 17:12:12 +0100

    add validation

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -3,6 +3,27 @@ let timer;
 function setAlarm() {
   const input = document.getElementById("alarmSet");
   const heading = document.getElementById("timeRemaining");
+  // step 1 retrieve input value
+  const inputValue = input.value;
+
+  // step 2 validate if input is a number
+  const numberValue = Number(inputValue);
+  if (isNaN(numberValue)) {
+    alert("only enter a valid number.");
+    return; // exit a function if not a number
+  }
+
+  //step 3 validate if it is an integer
+  if (!Number.isInteger(numberValue)) {
+    alert("only enter an integer value.");
+    return; // exit a function if not an integer
+  }
+
+  //step 4  validate if it is greater than 0
+  if (numberValue <= 0) {
+    alert("only enter a number which is bigger than 0");
+    return; //Exit a funciton if not greater than 0
+  }
 
   let timeRemaining = Number(input.value);
 
