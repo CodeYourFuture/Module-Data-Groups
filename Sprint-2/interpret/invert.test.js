ec4725cda830200abcd7c4117b355a1258166691
JorvanW <your.email@example.com>
2026-08-18 11:29:24 +0100

    added more test cases for invert.test.js

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -1,13 +1,38 @@
 const invert = require("./invert.js");
 
 test("when passed invert swaps single key-value pair", () => {
-  expect(invert({ a: 1 })).toEqual({ "1": "a" });
+  expect(invert({ a: 1 })).toEqual({ 1: "a" });
+});
+
+test("when invert is passed two key-value pairs, it swaps both keys and values", () => {
+  expect(invert({ a: 1, b: 2 })).toEqual({
+    1: "a",
+    2: "b",
+  });
 });
 
 test("When invert is passed, multiple keys and values in the object should be swapped ", () => {
-  expect(invert({ x: 10, y: 20 })).toEqual({ "10": "x", "20": "y" })
+  expect(invert({ x: 10, y: 20 })).toEqual({ 10: "x", 20: "y" });
+});
+
+test("when invert is passed a larger object, it swaps all keys and values", () => {
+  expect(invert({ a: 1, b: 2, c: 3, d: 4 })).toEqual({
+    1: "a",
+    2: "b",
+    3: "c",
+    4: "d",
+  });
 });
 
 test("when passed invert swaps string values", () => {
-  expect(invert({ first: "hello",second: "world" })).toEqual({ hello: "first", world: "second" });
+  expect(invert({ first: "hello", second: "world" })).toEqual({
+    hello: "first",
+    world: "second",
+  });
+});
+
+test("when invert is passed duplicate values, the last key is kept", () => {
+  expect(invert({ a: 1, b: 1 })).toEqual({
+    1: "b",
+  });
 });
