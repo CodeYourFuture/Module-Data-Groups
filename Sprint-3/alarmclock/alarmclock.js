ece8ebf1a5629ecfd7fae6c4f8ed13a9fbd27dee
mandip sanger <mandipsanger1@gmail.com>
2026-08-19 14:36:08 +0100

    add pause alarm

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,31 +1,31 @@
 let timer;
 
+const audio = new Audio("alarmsound.mp3");
+
 function setAlarm() {
+  const form = document.getElementById("ShowForm");
   const input = document.getElementById("alarmSet");
   const heading = document.getElementById("timeRemaining");
-  // step 1 retrieve input value
+
+  // Step 1: retrieve input value
   const inputValue = input.value;
 
-  // step 2 validate if input is a number
+  // Step 2: validate if input is a number
   const numberValue = Number(inputValue);
-  if (isNaN(numberValue)) {
-    alert("only enter a valid number.");
-    return; // exit a function if not a number
-  }
 
-  //step 3 validate if it is an integer
+  // Step 3: validate if it is an integer
   if (!Number.isInteger(numberValue)) {
-    alert("only enter an integer value.");
-    return; // exit a function if not an integer
+    alert("Only enter an integer value.");
+    return;
   }
 
-  //step 4  validate if it is greater than 0
+  // Step 4: validate if it is greater than 0
   if (numberValue <= 0) {
-    alert("only enter a number which is bigger than 0");
-    return; //Exit a funciton if not greater than 0
+    alert("Only enter a number which is bigger than 0.");
+    return;
   }
 
-  let timeRemaining = Number(input.value);
+  let timeRemaining = numberValue;
 
   function formatTime(totalSeconds) {
     const minutes = Math.floor(totalSeconds / 60);
@@ -41,6 +41,7 @@ function setAlarm() {
 
   // Stop an existing timer
   clearInterval(timer);
+  pauseAlarm();
 
   timer = setInterval(() => {
     timeRemaining--;
@@ -58,26 +59,22 @@ function setAlarm() {
   }, 1000);
 }
 
-// DO NOT EDIT BELOW HERE
-
-var audio = new Audio("alarmsound.mp3");
-
-function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
-  });
-
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
-  });
-}
-
+// Play alarm
 function playAlarm() {
   audio.play();
 }
 
+// Stop alarm
 function pauseAlarm() {
   audio.pause();
+  audio.currentTime = 0;
+}
+
+// Setup buttons
+function setup() {
+  document.getElementById("set").addEventListener("click", setAlarm);
+
+  document.getElementById("stop").addEventListener("click", pauseAlarm);
 }
 
 window.onload = setup;
