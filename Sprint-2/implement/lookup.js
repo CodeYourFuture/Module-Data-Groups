fd9d025228645b1f367b08088923dd45cd46e769
Asanda <asandagwala@gmail.com>
2026-08-20 14:43:42 +0200

    add lookup.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,15 @@
-function createLookup() {
+function createLookup(countryCurrencyPairs) {
   // implementation here
+  const lookup = {};
+
+  for (const pair of countryCurrencyPairs) {
+    const [countryCode, currencyCode] = pair;
+    lookup[countryCode] = currencyCode;
+  }
+
+  return lookup;
 }
+
+module.exports = createLookup;
 
 module.exports = createLookup;
