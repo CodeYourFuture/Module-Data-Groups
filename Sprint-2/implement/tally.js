9e4acfd6d637c40ebac56cdb09025ab9aef2902a
Asanda <asandagwala@gmail.com>
2026-08-20 14:55:47 +0200

    add tally .js

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,21 @@
-function tally() {}
+function tally(items) {
+  // Check that the input is an array
+  if (!Array.isArray(items)) {
+    throw new Error("Input must be an array");
+  }
+
+  const counts = {};
+
+  // Go through each item in the array
+  for (const item of items) {
+    if (counts[item] === undefined) {
+      counts[item] = 1;
+    } else {
+      counts[item] = counts[item] + 1;
+    }
+  }
+
+  return counts;
+}
 
 module.exports = tally;
