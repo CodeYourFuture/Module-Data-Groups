33e50c2ca8b509f1881d85dbf3a078ffe81e9d11
Asanda <asandagwala@gmail.com>
2026-08-20 14:40:48 +0200

    add contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,11 @@
-function contains() {}
+function contains(object, property) {
+  if (typeof object !== "object" || object === null || Array.isArray(object)) {
+    return false;
+  }
+
+  return Object.hasOwn(object, property);
+}
 
 module.exports = contains;
+
+
