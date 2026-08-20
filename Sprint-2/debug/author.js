59f99a821e3945bd3c95b01265b4e18f65d749c9
Asanda <asandagwala@gmail.com>
2026-08-20 13:33:58 +0200

    add authors.js

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,8 +1,12 @@
 // Predict and explain first...
+// TypeError: author is not iterable because A for...of loop is used for iterable things such as arrays, strings, Maps, and Sets.
+
+// But author is a plain object. Plain objects aren't directly iterable with for...of.
+
+// To loop through an object's values, use:
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
-
 const author = {
   firstName: "Zadie",
   lastName: "Smith",
@@ -11,6 +15,6 @@ const author = {
   alive: true,
 };
 
-for (const value of author) {
+for (const value of Object.values(author)) {
   console.log(value);
 }
