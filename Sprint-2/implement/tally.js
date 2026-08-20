95db99756bb6927d8212db95ed19f9104829e417
Maryam Janjua <maryamjanjua65@gmail.com>
2026-08-20 11:36:08 +0100

    updated tally code

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,14 +1,15 @@
 function tally(arr) {
     const result = arr.reduce((obj, item) => {
-        if (obj [item]) {
+        if (Object.hasOwn(obj, item)) {
             obj[item] = obj[item] + 1;
-        }
-        else{
+        } else {
             obj[item] = 1;
         }
-        return obj; 
+
+        return obj;
     }, {});
-    
+
     return result;
 }
+
 module.exports = tally;
