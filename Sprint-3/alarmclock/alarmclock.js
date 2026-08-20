d281f821eccadf1bbc96843b775fbfb5e28b0b7a
Ebrahim-Moqbel <ebra72himuni@gmail.com>
2026-08-20 15:32:03 +0100

    implemented the function  format time

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,16 @@
-function setAlarm() {}
+let intervalId = null;
+let totalSeconds = 0;
+
+function formatTime(seconds){
+  const minutes = Math.floor(seconds/ 60);
+  const secs = seconds % 60;
+  return `${string(minutes).padStart(2,"0")}:${String(secs).padStart(2,"0")}`;
+
+}
+function setAlarm() {
+  
+
+}
 
 // DO NOT EDIT BELOW HERE
 
