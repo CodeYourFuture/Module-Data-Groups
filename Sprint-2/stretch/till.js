18d34ecdf458e482cd1c2a76cb583581c0224c85
LiyemaMfengwana <mfengwanaliyema@gmail.com>
2026-08-20 22:15:00 +0200

    till js

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -8,24 +8,23 @@ function totalTill(till) {
   let total = 0;
 
   for (const [coin, quantity] of Object.entries(till)) {
-    total += coin * quantity;
+    const coinValue = parseInt(coin, 10);
+    total += coinValue * quantity;
   }
 
   return `£${total / 100}`;
 }
 
-const till = {
-  "1p": 10,
-  "5p": 6,
-  "50p": 4,
-  "20p": 10,
-};
-const totalAmount = totalTill(till);
-
 // a) What is the target output when totalTill is called with the till object
+// a) The target output is £4.4
 
 // b) Why do we need to use Object.entries inside the for...of loop in this function?
+// b) Object.entries converts the object into key-value pairs,
+//    allowing us to access both the coin and its quantity.
 
 // c) What does coin * quantity evaluate to inside the for...of loop?
+// c) coinValue * quantity calculates the total value
+//    of that type of coin.
 
 // d) Write a test for this function to check it works and then fix the implementation of totalTill
+module.exports = totalTill;
