a4bba13a322e42b134ca03ab85b41c2effdcd79d
LiyemaMfengwana <mfengwanaliyema@gmail.com>
2026-08-20 21:04:40 +0200

    querystringjs

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -6,10 +6,29 @@ function parseQueryString(queryString) {
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
-  }
+    if (pair === "") {
+      continue;
+    }
 
+    const separatorIndex = pair.indexOf("=");
+
+    const encodedKey =
+      separatorIndex === -1 ? pair : pair.slice(0, separatorIndex);
+
+    const encodedValue =
+      separatorIndex === -1 ? "" : pair.slice(separatorIndex + 1);
+
+    const key = decodeURIComponent(encodedKey.replaceAll("+", " "));
+    const value = decodeURIComponent(encodedValue.replaceAll("+", " "));
+
+    if (!Object.hasOwn(queryParams, key)) {
+      queryParams[key] = value;
+    } else if (Array.isArray(queryParams[key])) {
+      queryParams[key].push(value);
+    } else {
+      queryParams[key] = [queryParams[key], value];
+    }
+  }
   return queryParams;
 }
 
