5253409562cd46c8abfdcde3ffaf58ecdcd29a00
d-odumosu <dt.odumosu@gmail.com>
2026-08-21 19:47:59 +0100

    further simplified sum.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,11 +1,5 @@
 function sum(elements) {
-  const elementsList = elements.filter((element) => {
-    return (
-      typeof element === "number" &&
-      !Number.isNaN(element) &&
-      Number.isFinite(element)
-    );
-  });
+  const elementsList = elements.filter((element) => Number.isFinite(element));
 
   let sum = 0;
   for (let i = 0; i < elementsList.length; i++) {
