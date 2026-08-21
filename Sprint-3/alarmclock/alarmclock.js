57b4289e751e3a3abfa80be49df8eadf6f2e306a
Asanda <asandagwala@gmail.com>
2026-08-21 13:14:17 +0200

    Alarm clock

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,5 +1,27 @@
-function setAlarm() {}
+function setAlarm() {
+  let time = Number(document.getElementById("alarmSet").value);
+  let heading = document.getElementById("timeRemaining");
 
+  function updateClock() {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+
+    heading.innerText = `Time Remaining: ${minutes}:${seconds}`;
+
+    if (time === 0) {
+      clearInterval(timer);
+      playAlarm();
+    }
+
+    time--;
+  }
+
+  updateClock();
+  let timer = setInterval(updateClock, 1000);
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
