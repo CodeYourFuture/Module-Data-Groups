e6024c186a471c42293437a0155d5c3e5b629a76
LiyemaMfengwana <mfengwanaliyema@gmail.com>
2026-08-21 19:22:32 +0200

    fix: handle non-object values in contains

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,9 +1,8 @@
 function contains(object, propertyName) {
-  if (Array.isArray(object)) {
+  if (object === null || typeof object !== "object" || Array.isArray(object)) {
     return false;
   }
 
   return Object.hasOwn(object, propertyName);
 }
-
 module.exports = contains;
