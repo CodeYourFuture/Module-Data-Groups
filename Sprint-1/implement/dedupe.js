b6cce2a0b7c9f81430bf7e27612db481ee117de1
Vitalii Kmit <vitalikkmita@gmail.com>
2026-08-21 21:10:21 +0100

    Implement dedupe

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,15 @@
-function dedupe() {}
+function dedupe(list) {
+  const seen = {};
+  const result = [];
+
+  for (const item of list) {
+    if (seen[item] === undefined) {
+      seen[item] = true;
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+module.exports = dedupe;
