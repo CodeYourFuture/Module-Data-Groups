0c301ed61300956662fbe2148841a58ab9e97a51
LiyemaMfengwana <mfengwanaliyema@gmail.com>
2026-08-21 19:39:46 +0200

    fix: handle inherited object properties in tally

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -2,7 +2,7 @@ function tally(items) {
   if (!Array.isArray(items)) {
     throw new TypeError("Expected an array");
   }
-  const counts = {};
+  const counts = Object.create(null);
 
   for (const item of items) {
     counts[item] = (counts[item] || 0) + 1;
