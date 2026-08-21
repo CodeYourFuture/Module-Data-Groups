aba332ccc657e1c06467adc9ef8ec16ccf858c55
Vitalii Kmit <vitalikkmita@gmail.com>
2026-08-21 21:21:54 +0100

    Implement findMax

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,13 @@
 function findMax(elements) {
+  let max = -Infinity;
+
+  for (i of elements) {
+    if (typeof i === "number" && !isNaN(i) && i > max) {
+      max = i;
+    }
+  }
+
+  return max;
 }
 
 module.exports = findMax;
