c42fcdd94e09e2cbc651a2977a789d1ce9f65e67
Maryam Janjua <maryamjanjua65@gmail.com>
2026-09-05 12:12:59 +0100

    update counter

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,35 +1,32 @@
-function setAlarm() {
-  const input = document.getElementById("alarmSet");
-  const heading = document.getElementById("timeRemaining");
-  let timeRemaining = Number(input.value);
+let alarmTimer;
 
-  function updateHeading() {
-    const minutes = Math.floor(timeRemaining / 60);
-    const seconds = timeRemaining % 60;
-    const formattedMinutes = String(minutes).padStart(2, "0");
-    const formattedSeconds = String(seconds).padStart(2, "0");
-    heading.innerText =
-      `Time Remaining: ${formattedMinutes}:${formattedSeconds}`;
-  }
-  updateHeading();
-  // If the alarm is set to 00:00
-  if (timeRemaining === 0) {
-    playAlarm();
-    return;
-  }
-  
-  const timer = setInterval(() => {
-    timeRemaining--;
-    updateHeading();
-    // When the countdown reaches zero
-    if (timeRemaining === 0) {
-      clearInterval(timer);
+function setAlarm() {
+  clearTimeout(alarmTimer);
+
+  let secondsRemaining = Number(document.getElementById("alarmSet").value);
+
+  function updateTimer() {
+    const minutes = Math.floor(secondsRemaining / 60);
+    const seconds = secondsRemaining % 60;
+
+    document.getElementById("timeRemaining").innerText =
+      `Time Remaining: ${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
+    if (secondsRemaining === 0) {
       playAlarm();
-      document.body.style.backgroundColor = "lightcoral";
+      return;
     }
-  }, 1000);
+
+    secondsRemaining--;
+
+    alarmTimer = setTimeout(updateTimer, 1000);
+  }
+
+  updateTimer();
 }
+
 // DO NOT EDIT BELOW HERE
+
 var audio = new Audio("alarmsound.mp3");
 
 function setup() {
