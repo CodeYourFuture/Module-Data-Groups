60354c4080c44fed00064f9bf6fbcfe6f583ce66
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-09-10 16:13:20 +0100

    refactoring the last two cases of the sum file

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,41 +13,44 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test("given an empty array, returns 0", ()=>{
-    expect(sum([])).toBe(0)
-})
+test("given an empty array, returns 0", () => {
+  expect(sum([])).toBe(0);
+});
 
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
-test('Given an array with just one number, return that number', ()=>{
-    expect(sum([3])).toBe(3)
-})
+test("Given an array with just one number, return that number", () => {
+  expect(sum([3])).toBe(3);
+});
 // Given an array containing negative numbers
 // When passed to the sum function
 // Then it should still return the correct total sum
-test('Given an array with negative numbers, return the correct total', ()=>{
-    expect(sum([-2,-4,-1])).toBe(-7)
-})
+test("Given an array with negative numbers, return the correct total", () => {
+  expect(sum([-2, -4, -1])).toBe(-7);
+});
 
 // Given an array with decimal/float numbers
 // When passed to the sum function
 // Then it should return the correct total sum
-test('Given an array with decimal/float numbers, returns the correct total sum',()=>{
-    expect(sum([3.5,6.1,])).toBe(9.6)
-} )
+test("Given an array with decimal/float numbers, returns the correct total sum", () => {
+  expect(sum([3.5, 6.1])).toBe(9.6);
+});
 
 // Given an array containing a value that isn't a number
 // When passed to the sum function
 // Then it should throw Error("sum requires an array of numbers")
-test("throws an error when given an array contains a non-number value", () =>{
-  expect(() => sum(['4',"Holla",6,9,1])).toThrow(
-    new Error("sum requires an array of numbers"));
- 
+test("throws an error when given an array contains a non-number value", () => {
+  expect(() => sum(["4", "Holla", 6, 9, 1])).toThrow(
+    new Error("sum requires an array of numbers")
+  );
+});
 
 // Given something that isn't an array at all, such as "hey", 42 or no argument
 // When passed to the sum function
 // Then it should throw Error("sum requires an array of numbers")
-  test("throws an error when given a non array value", () =>{
-  expect(() => sum([null,undefined,NaN,true])).toThrow(
-    new Error("sum requires an array of numbers"));
+test("throws an error when given a non array value", () => {
+  expect(() => sum([null, undefined, NaN, true])).toThrow(
+    new Error("sum requires an array of numbers")
+  );
+});
